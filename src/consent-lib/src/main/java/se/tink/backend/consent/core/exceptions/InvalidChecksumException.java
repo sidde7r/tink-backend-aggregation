82b4ff26cfedda5fe9fd4429187c75f0d1e617de
Jonas Renold <jonas.renold@tink.se>
2018-06-03 18:49:57 +0200

    ref(consent-lib) remove completely

--- src/consent-lib/src/main/java/se/tink/backend/consent/core/exceptions/InvalidChecksumException.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.consent.core.exceptions;
-
-public class InvalidChecksumException extends Exception {
-    public InvalidChecksumException(String left, String right) {
-        super(String.format("Checksums do not match (Left = '%s', Right = '%s')", left, right));
-    }
-}
