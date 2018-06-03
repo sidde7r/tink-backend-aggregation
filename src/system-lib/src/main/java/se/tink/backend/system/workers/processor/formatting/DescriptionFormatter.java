8895a8d82b7fa69f8499fbbf61d3b02181df18f4
Jonas Renold <jonas.renold@tink.se>
2018-06-03 11:10:42 +0200

    ref(system-lib) remove completely

--- src/system-lib/src/main/java/se/tink/backend/system/workers/processor/formatting/DescriptionFormatter.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.system.workers.processor.formatting;
-
-public interface DescriptionFormatter {
-    String clean(String description);
-
-    // Move extrapolation out of formatter? Is it an orthogonal use case?
-    String extrapolate(String description);
-}
--- src/system-lib/src/main/java/se/tink/backend/system/workers/processor/transfers/scoring/TransferDetectionScorer.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.system.workers.processor.transfers.scoring;
-
-import se.tink.backend.core.Transaction;
-
-public interface TransferDetectionScorer {
-    double getScore(Transaction left, Transaction right);
-}
