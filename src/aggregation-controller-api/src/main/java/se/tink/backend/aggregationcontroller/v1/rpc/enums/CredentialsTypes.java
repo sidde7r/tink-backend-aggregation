a4c553b0ee18716ff297334d57ca06898e25f12c
Jonas Renold <jonas.renold@tink.se>
2018-06-05 09:02:51 +0200

    ref(aggregation-controller-api) remove completely
    The aggregation controller RPC is already mirrored in aggregation-lib.

--- src/aggregation-controller-api/src/main/java/se/tink/backend/aggregationcontroller/v1/rpc/enums/CredentialsTypes.java
+++
@@ -1,5 +0,0 @@
-package se.tink.backend.aggregationcontroller.v1.rpc.enums;
-
-public enum CredentialsTypes {
-    PASSWORD, MOBILE_BANKID, KEYFOB, FRAUD
-}
--- src/aggregation-controller-api/src/main/java/se/tink/backend/aggregationcontroller/v1/rpc/enums/TransactionTypes.java
+++
@@ -1,5 +0,0 @@
-package se.tink.backend.aggregationcontroller.v1.rpc.enums;
-
-public enum TransactionTypes {
-    DEFAULT, CREDIT_CARD, TRANSFER, PAYMENT, WITHDRAWAL, TRAINING
-}
