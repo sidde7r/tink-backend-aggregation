68b00b6508a579003e920d9aa66f2e95dc33d487
Lars Almgren <lars.almgren@tink.se>
2018-06-08 10:01:44 +0200

    ref(swedbank) add abstract executor transaction entity
    Will be used for sharing common fields.

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/executors/rpc/TransactionEntity.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/executors/rpc/TransactionEntity.java
@@ -1,48 +1,12 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.executors.rpc;
 
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.rpc.LinksEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
-public class TransactionEntity {
-    private LinksEntity links;
-    private String id;
-    private String type;
-    private String currencyCode;
-    private boolean selected;
-    private String amount;
-    private TransferEntity transfer;
+public class TransactionEntity extends AbstractExecutorTransactionEntity {
     private String noteToSender;
     private boolean canSign;
     private boolean counterSigning;
-
-    public LinksEntity getLinks() {
-        return links;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getCurrencyCode() {
-        return currencyCode;
-    }
-
-    public boolean isSelected() {
-        return selected;
-    }
-
-    public String getAmount() {
-        return amount;
-    }
-
-    public TransferEntity getTransfer() {
-        return transfer;
-    }
 
     public String getNoteToSender() {
         return noteToSender;
