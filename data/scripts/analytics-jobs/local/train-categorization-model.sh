e6a7919050ddc2f78e9c357a592204fa2d9949f8
Johannes Elgh <johannes.elgh@tink.se>
2018-06-08 17:28:00 +0200

    ref(data): removed a bunch of stuff from data and third_party folders

--- data/scripts/analytics-jobs/local/train-categorization-model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Sync settings with pre process categorization model script
-
-java -classpath src/analytics-jobs/target/analytics-jobs.jar:src/analytics-jobs/target/lib/* se.tink.analytics.jobs.categorization.commands.TrainCategorizationModelCommand --market NL --categoryTypes EXPENSES,INCOME --unhandledCategory expenses:misc.uncategorized
