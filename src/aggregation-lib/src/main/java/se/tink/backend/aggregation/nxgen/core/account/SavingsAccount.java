2150fca18b7d846cbf19b8e7565f7c511d6d253c
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-06-14 09:58:10 +0200

    sty(account) Remove old code and indentation

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/SavingsAccount.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/SavingsAccount.java
@@ -4,70 +4,57 @@ import se.tink.backend.aggregation.rpc.AccountTypes;
 import se.tink.backend.core.Amount;
 
 public class SavingsAccount extends TransactionalAccount {
-  private final Double interestRate;
+    private final Double interestRate;
 
-  //    private SavingsAccount(String name, String accountNumber, Amount balance,
-  // List<AccountIdentifier> identifiers,
-  //            String tinkId, String bankIdentifier, Double interestRate, HolderName holderName,
-  //            Map<String, String> temporaryStorage) {
-  //        super(name, accountNumber, balance, identifiers, tinkId, bankIdentifier, holderName,
-  // temporaryStorage);
-  //        this.interestRate = interestRate;
-  //    }
+    private SavingsAccount(Builder<?, ?> builder) {
+        super(builder);
+        this.interestRate = builder.getInterestRate();
+    }
 
-  private SavingsAccount(Builder<?, ?> builder) {
-    super(builder);
-    this.interestRate = builder.getInterestRate();
-  }
+    public static Builder<?, ?> builder() {
+        return new DefaultSavingAccountsBuilder();
+    }
 
-  //    public static Builder builder(String accountNumber, Amount balance) {
-  //        return new Builder(accountNumber, balance);
-  //    }
+    public static Builder<?, ?> builder(String accountNumber, Amount balance) {
+        DefaultSavingAccountsBuilder defaultSavingAccountsBuilder = new DefaultSavingAccountsBuilder();
+        defaultSavingAccountsBuilder.setBalance(balance).setAccountNumber(accountNumber);
+        return defaultSavingAccountsBuilder;
+    }
 
-  public static Builder<?, ?> builder() {
-    return new DefaultSavingAccountsBuilder();
-  }
-
-  public static Builder<?, ?> builder(String accountNumber, Amount balance) {
-    DefaultSavingAccountsBuilder defaultSavingAccountsBuilder = new DefaultSavingAccountsBuilder();
-    defaultSavingAccountsBuilder.setBalance(balance).setAccountNumber(accountNumber);
-    return defaultSavingAccountsBuilder;
-  }
-
-  @Override
-  public AccountTypes getType() {
-    return AccountTypes.SAVINGS;
-  }
-
-  public Double getInterestRate() {
-    return this.interestRate;
-  }
-
-  public abstract static class Builder<
-          A extends SavingsAccount, T extends SavingsAccount.Builder<A, T>>
-      extends TransactionalAccount.Builder<SavingsAccount, Builder<A, T>> {
-    private Double interestRate;
+    @Override
+    public AccountTypes getType() {
+        return AccountTypes.SAVINGS;
+    }
 
     public Double getInterestRate() {
-      return this.interestRate;
+        return this.interestRate;
     }
 
-    public Builder<A, T> setInterestRate(Double interestRate) {
-      self().interestRate = interestRate;
-      return self();
-    }
-  }
+    public abstract static class Builder<
+            A extends SavingsAccount, T extends SavingsAccount.Builder<A, T>>
+            extends TransactionalAccount.Builder<SavingsAccount, Builder<A, T>> {
+        private Double interestRate;
 
-  private static class DefaultSavingAccountsBuilder
-      extends SavingsAccount.Builder<SavingsAccount, DefaultSavingAccountsBuilder> {
-    @Override
-    protected DefaultSavingAccountsBuilder self() {
-      return this;
+        public Double getInterestRate() {
+            return this.interestRate;
+        }
+
+        public Builder<A, T> setInterestRate(Double interestRate) {
+            self().interestRate = interestRate;
+            return self();
+        }
     }
 
-    @Override
-    public SavingsAccount build() {
-      return new SavingsAccount(this);
+    private static class DefaultSavingAccountsBuilder
+            extends SavingsAccount.Builder<SavingsAccount, DefaultSavingAccountsBuilder> {
+        @Override
+        protected DefaultSavingAccountsBuilder self() {
+            return this;
+        }
+
+        @Override
+        public SavingsAccount build() {
+            return new SavingsAccount(this);
+        }
     }
-  }
 }
