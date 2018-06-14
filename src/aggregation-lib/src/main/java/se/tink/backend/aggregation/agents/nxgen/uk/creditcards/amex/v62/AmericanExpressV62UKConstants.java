75b3048e7338dacf92b31e5fc2876b25fca9a306
ovesan <ove.sandfeldt@tink.se>
2018-06-14 21:44:00 +0200

    fea(AmexAgent UK) Add Amex UK implementation of Amex agent

---
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/creditcards/amex/v62/AmericanExpressV62UKConstants.java
@@ -0,0 +1,8 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.creditcards.amex.v62;
+
+public class AmericanExpressV62UKConstants {
+    public static final String LOCALE = "en_GB";
+    public static final String APP_ID = "uk.co.americanexpress.amexservice";
+    public static final String USER_AGENT = "Amex%20UK/32 CFNetwork/811.4.18 Darwin/16.5.0";
+    public static final String GBP = "GBP";
+}
