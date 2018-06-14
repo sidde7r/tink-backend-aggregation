a88bb1ea04dfb051ac6df454a6eeae32fa5c424e
ovesan <ove.sandfeldt@tink.se>
2018-06-14 21:37:19 +0200

    ref(AmexAgent) Remove warnings and unused imports

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/amex/v62/fetcher/AmericanExpressV62CreditCardFetcher.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/amex/v62/fetcher/AmericanExpressV62CreditCardFetcher.java
@@ -5,8 +5,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.AmericanExpressV62Configuration;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.AmericanExpressV62Constants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.fetcher.entities.CardEntity;
@@ -19,7 +17,7 @@ public class AmericanExpressV62CreditCardFetcher implements AccountFetcher<Credi
     private final SessionStorage sessionStorage;
     private final AmericanExpressV62Configuration configuration;
 
-    public AmericanExpressV62CreditCardFetcher(
+    private AmericanExpressV62CreditCardFetcher(
             SessionStorage sessionStorage, AmericanExpressV62Configuration configuration) {
         this.sessionStorage = sessionStorage;
         this.configuration = configuration;
@@ -33,12 +31,9 @@ public class AmericanExpressV62CreditCardFetcher implements AccountFetcher<Credi
     @Override
     public Collection<CreditCardAccount> fetchAccounts() {
         List<CardEntity> cardEntities =
-                sessionStorage
-                        .get(
-                                AmericanExpressV62Constants.Tags.CARD_LIST,
-                                new TypeReference<List<CardEntity>>() {
-                                })
-                        .orElse(Collections.EMPTY_LIST);
+            sessionStorage
+                .get(AmericanExpressV62Constants.Tags.CARD_LIST, new TypeReference<List<CardEntity>>() {})
+                .orElse(Collections.emptyList());
 
         return cardEntities
                 .stream()
