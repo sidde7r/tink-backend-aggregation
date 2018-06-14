976c7f0d0f50e5d20b7f9373a6a7606dd188b0f6
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-06-14 09:55:28 +0200

    sty(account) Fix indentation

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/InvestmentAccount.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/InvestmentAccount.java
@@ -8,67 +8,67 @@ import se.tink.backend.core.Amount;
 import se.tink.backend.system.rpc.Portfolio;
 
 public class InvestmentAccount extends Account {
-  public static final ImmutableList<AccountTypes> ALLOWED_ACCOUNT_TYPES =
-      ImmutableList.<AccountTypes>builder()
-          .add(AccountTypes.INVESTMENT)
-          .add(AccountTypes.PENSION)
-          .build();
+    public static final ImmutableList<AccountTypes> ALLOWED_ACCOUNT_TYPES =
+            ImmutableList.<AccountTypes>builder()
+                    .add(AccountTypes.INVESTMENT)
+                    .add(AccountTypes.PENSION)
+                    .build();
 
-  private List<Portfolio> portfolios;
-
-  private InvestmentAccount(Builder<InvestmentAccount, DefaultInvestmentAccountsBuilder> builder) {
-    super(builder);
-    this.portfolios = builder.getPortfolios();
-  }
-
-  public static Builder<InvestmentAccount, DefaultInvestmentAccountsBuilder> builder(
-      String accountNumber, Amount balance) {
-    DefaultInvestmentAccountsBuilder defaultInvestmentAccountsBuilder =
-        new DefaultInvestmentAccountsBuilder();
-    defaultInvestmentAccountsBuilder.setAccountNumber(accountNumber).setBalance(balance);
-    return defaultInvestmentAccountsBuilder;
-  }
-
-  public static Builder<InvestmentAccount, DefaultInvestmentAccountsBuilder> builder() {
-    return new DefaultInvestmentAccountsBuilder();
-  }
-
-  public List<Portfolio> getPortfolios() {
-    return this.portfolios != null
-        ? ImmutableList.copyOf(this.portfolios)
-        : Collections.emptyList();
-  }
-
-  @Override
-  public AccountTypes getType() {
-    return AccountTypes.INVESTMENT;
-  }
-
-  public abstract static class Builder<
-          A extends InvestmentAccount, T extends InvestmentAccount.Builder<A, T>>
-      extends Account.Builder<InvestmentAccount, Builder<A, T>> {
     private List<Portfolio> portfolios;
 
+    private InvestmentAccount(Builder<InvestmentAccount, DefaultInvestmentAccountsBuilder> builder) {
+        super(builder);
+        this.portfolios = builder.getPortfolios();
+    }
+
+    public static Builder<InvestmentAccount, DefaultInvestmentAccountsBuilder> builder(
+            String accountNumber, Amount balance) {
+        DefaultInvestmentAccountsBuilder defaultInvestmentAccountsBuilder =
+                new DefaultInvestmentAccountsBuilder();
+        defaultInvestmentAccountsBuilder.setAccountNumber(accountNumber).setBalance(balance);
+        return defaultInvestmentAccountsBuilder;
+    }
+
+    public static Builder<InvestmentAccount, DefaultInvestmentAccountsBuilder> builder() {
+        return new DefaultInvestmentAccountsBuilder();
+    }
+
     public List<Portfolio> getPortfolios() {
-      return this.portfolios != null ? this.portfolios : Collections.emptyList();
-    }
-
-    public Builder<A, T> setPortfolios(List<Portfolio> portfolios) {
-      this.portfolios = portfolios;
-      return self();
-    }
-  }
-
-  private static class DefaultInvestmentAccountsBuilder
-      extends InvestmentAccount.Builder<InvestmentAccount, DefaultInvestmentAccountsBuilder> {
-    @Override
-    protected DefaultInvestmentAccountsBuilder self() {
-      return this;
+        return this.portfolios != null
+                ? ImmutableList.copyOf(this.portfolios)
+                : Collections.emptyList();
     }
 
     @Override
-    public InvestmentAccount build() {
-      return new InvestmentAccount(this);
+    public AccountTypes getType() {
+        return AccountTypes.INVESTMENT;
     }
-  }
+
+    public abstract static class Builder<
+            A extends InvestmentAccount, T extends InvestmentAccount.Builder<A, T>>
+            extends Account.Builder<InvestmentAccount, Builder<A, T>> {
+        private List<Portfolio> portfolios;
+
+        public List<Portfolio> getPortfolios() {
+            return this.portfolios != null ? this.portfolios : Collections.emptyList();
+        }
+
+        public Builder<A, T> setPortfolios(List<Portfolio> portfolios) {
+            this.portfolios = portfolios;
+            return self();
+        }
+    }
+
+    private static class DefaultInvestmentAccountsBuilder
+            extends InvestmentAccount.Builder<InvestmentAccount, DefaultInvestmentAccountsBuilder> {
+        @Override
+        protected DefaultInvestmentAccountsBuilder self() {
+            return this;
+        }
+
+        @Override
+        public InvestmentAccount build() {
+            return new InvestmentAccount(this);
+        }
+    }
 }
