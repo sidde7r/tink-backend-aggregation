950399a86edd9720377847617d6e712d6d7fefa2
Jonas Renold <jonas.renold@tink.se>
2018-06-15 08:56:33 +0200

    ref(refreshableItems) move refreshableItems sorting and groups to refreshableItems
    The sorting order and groups (accounts, transactions) were previously only defined in workerOperationFactory. However, more users of refreshableItems need to access these (tests for example).

--- src/aggregation-api/src/main/java/se/tink/backend/aggregation/rpc/RefreshableItem.java
+++ src/aggregation-api/src/main/java/se/tink/backend/aggregation/rpc/RefreshableItem.java
@@ -1,7 +1,12 @@
 package se.tink.backend.aggregation.rpc;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Ordering;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public enum RefreshableItem {
     ACCOUNTS, TRANSACTIONAL_ACCOUNTS_AND_TRANSACTIONS, EINVOICES, TRANSFER_DESTINATIONS,
@@ -23,7 +28,60 @@ public enum RefreshableItem {
         accountTypes = Arrays.asList(types);
     }
 
+    private static final ImmutableList<RefreshableItem> LEGACY_ITEMS = ImmutableList.<RefreshableItem>builder()
+            .add(ACCOUNTS)
+            .add(TRANSACTIONAL_ACCOUNTS_AND_TRANSACTIONS)
+            .build();
+
+    // Explicit order of refreshable items.
+    private static final Ordering<RefreshableItem> REFRESHABLE_ITEM_ORDERING = Ordering.explicit(ImmutableList.of(
+            RefreshableItem.CHECKING_ACCOUNTS,
+            RefreshableItem.SAVING_ACCOUNTS,
+            RefreshableItem.CREDITCARD_ACCOUNTS,
+            RefreshableItem.LOAN_ACCOUNTS,
+            RefreshableItem.INVESTMENT_ACCOUNTS,
+
+            RefreshableItem.CHECKING_TRANSACTIONS,
+            RefreshableItem.SAVING_TRANSACTIONS,
+            RefreshableItem.CREDITCARD_TRANSACTIONS,
+            RefreshableItem.LOAN_TRANSACTIONS,
+            RefreshableItem.INVESTMENT_TRANSACTIONS,
+
+            RefreshableItem.EINVOICES,
+            RefreshableItem.TRANSFER_DESTINATIONS
+    ));
+
+    private static final ImmutableSet<RefreshableItem> REFRESHABLE_ITEMS_ACCOUNTS = ImmutableSet.<RefreshableItem>builder()
+            .add(RefreshableItem.CHECKING_ACCOUNTS)
+            .add(RefreshableItem.SAVING_ACCOUNTS)
+            .add(RefreshableItem.CREDITCARD_ACCOUNTS)
+            .add(RefreshableItem.LOAN_ACCOUNTS)
+            .add(RefreshableItem.INVESTMENT_ACCOUNTS)
+            .build();
+
+    private static final ImmutableSet<RefreshableItem> REFRESHABLE_ITEMS_TRANSACTIONS = ImmutableSet.<RefreshableItem>builder()
+            .add(RefreshableItem.CHECKING_TRANSACTIONS)
+            .add(RefreshableItem.SAVING_TRANSACTIONS)
+            .add(RefreshableItem.CREDITCARD_TRANSACTIONS)
+            .add(RefreshableItem.LOAN_TRANSACTIONS)
+            .add(RefreshableItem.INVESTMENT_TRANSACTIONS)
+            .build();
+
     public boolean isAccountType(AccountTypes type) {
         return accountTypes.contains(type);
+    }
+
+    public static List<RefreshableItem> sort(Set<RefreshableItem> items) {
+        // Remove the legacy items before ordering (cannot order items that are not included in the Oredering<>).
+        items.removeAll(LEGACY_ITEMS);
+        return REFRESHABLE_ITEM_ORDERING.sortedCopy(items);
+    }
+
+    public static boolean hasAccounts(List<RefreshableItem> items) {
+        return !Collections.disjoint(items, REFRESHABLE_ITEMS_ACCOUNTS);
+    }
+
+    public static boolean hasTransactions(List<RefreshableItem> items) {
+        return !Collections.disjoint(items, REFRESHABLE_ITEMS_TRANSACTIONS);
     }
 }
