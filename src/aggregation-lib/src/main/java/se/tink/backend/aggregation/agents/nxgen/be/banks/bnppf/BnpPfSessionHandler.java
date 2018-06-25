011dee49d6cec142ddda5e2955f824a5e633eda4
Johan Haukka <johan.haukka@gmail.com>
2018-06-25 13:15:10 +0200

    fea(bnppf) implement session handler for bnppf

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/bnppf/BnpPfSessionHandler.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/bnppf/BnpPfSessionHandler.java
@@ -1,6 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.be.banks.bnppf;
 
 import se.tink.backend.aggregation.agents.exceptions.SessionException;
+import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
 
 public class BnpPfSessionHandler implements SessionHandler {
@@ -9,5 +10,7 @@ public class BnpPfSessionHandler implements SessionHandler {
     public void logout() { }
 
     @Override
-    public void keepAlive() throws SessionException { }
+    public void keepAlive() throws SessionException {
+        throw SessionError.SESSION_EXPIRED.exception();
+    }
 }
