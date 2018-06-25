83d2cf383f44dad002f86b5e759ae0df92139f36
apete <anders.peterson@tink.se>
2018-06-25 15:32:15 +0200

    fix(example) no longer static field
    Having a static field is not a good idea:
    
    1) Exceptions from the bulder class will prevet the class from loading.
    
    2) That builder code will run even if the test case is ignored/disabled

--- src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/demo/NewDemoAgentTest.java
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/demo/NewDemoAgentTest.java
@@ -9,22 +9,25 @@ import se.tink.credentials.demo.DemoCredentials;
 
 @Ignore
 public class NewDemoAgentTest {
-    private static final AgentIntegrationTest.Builder builder = new AgentIntegrationTest.Builder("se", "demo")
-            .setCredentialType(CredentialsTypes.PASSWORD)
-            .addCredentialField(Field.Key.PASSWORD, "demo")
-            .loadCredentialsBefore(false)
-            .saveCredentialsAfter(false);
+
+    private static AgentIntegrationTest.Builder builder() {
+        return new AgentIntegrationTest.Builder("se", "demo")
+                .setCredentialType(CredentialsTypes.PASSWORD)
+                .addCredentialField(Field.Key.PASSWORD, "demo")
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false);
+    }
 
     @Test
     public void testDemoSupplementalInfo() throws Exception {
-        builder.addCredentialField(Field.Key.USERNAME, DemoCredentials.USER8.getUsername())
+        builder().addCredentialField(Field.Key.USERNAME, DemoCredentials.USER8.getUsername())
                 .build()
                 .testRefresh();
     }
 
     @Test
     public void testDemoSwe() throws Exception {
-        builder.addCredentialField(Field.Key.USERNAME, DemoCredentials.USER4.getUsername())
+        builder().addCredentialField(Field.Key.USERNAME, DemoCredentials.USER4.getUsername())
                 .build()
                 .testRefresh();
     }
