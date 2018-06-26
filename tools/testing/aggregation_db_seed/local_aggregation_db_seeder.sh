3b86da319307836dd5feefaf739fa3a6239fcd55
Mathew Clegg <mathew.clegg@live.se>
2018-06-26 11:42:38 +0200

    (Fix) Removed expecting output from the execute command in the script

--- tools/testing/aggregation_db_seed/local_aggregation_db_seeder.sh
+++ tools/testing/aggregation_db_seed/local_aggregation_db_seeder.sh
@@ -2,4 +2,4 @@
 #   Script to seed aggregation with local testing configurations #
 ##################################################################
 aggregation_pod=$( kubectl get --no-headers=true pods --selector=app=aggregationdb -o name | awk -F "/" '{print $2}')
-kubectl exec -it  $aggregation_pod mysql <./script.sql
+kubectl exec -i  $aggregation_pod mysql <./script.sql
