a43a0b24041401ec1304d4081773799e24d1adb6
Xiaoqiang Chen <xiaoqiang.chen@tink.se>
2018-06-28 10:35:32 +0200

    fix(danske) update client secret

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/danskebank/filters/DanskeBankHttpFilter.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/danskebank/filters/DanskeBankHttpFilter.java
@@ -6,7 +6,7 @@ public abstract class DanskeBankHttpFilter extends Filter {
     protected static final String CLIENT_ID_KEY = "x-ibm-client-id";
     protected static final String CLIENT_ID_VALUE = "5ec4b8ad-a93d-43e1-831c-8e78ee6e661a";
     protected static final String CLIENT_SECRET_KEY = "x-ibm-client-secret";
-    protected static final String CLIENT_SECRET_VALUE = "lJFXVTCZ3nAVyPXfsZ7aXzwsD41WdYT2y4rxICt5mSEe9xMgPh";
+    protected static final String CLIENT_SECRET_VALUE = "xHciA0wZGWg9UCwSuA0PTMCBHfSjvfFu54ykNhpP0asMTg06NS";
     protected static final String APP_CULTURE_KEY = "x-app-culture";
     protected static final String ADRUM_KEY = "ADRUM";
     protected static final String ADRUM_VALUE = "isAjax:true";
