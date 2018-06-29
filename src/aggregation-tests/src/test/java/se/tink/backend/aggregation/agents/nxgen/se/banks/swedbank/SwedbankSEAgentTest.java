da7286769e4a5428be656b3803db496ddf480049
ovesan <ove.sandfeldt@tink.se>
2018-06-29 10:54:17 +0200

    fea(Swedbank) Add providers for SE and test

---
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/SwedbankSEAgentTest.java
@@ -0,0 +1,22 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.rpc.CredentialsTypes;
+import se.tink.backend.aggregation.rpc.Field;
+
+@Ignore
+public class SwedbankSEAgentTest {
+    @Test
+    public void testRegisterAndRefresh() throws Exception {
+        AgentIntegrationTest.Builder builder = new AgentIntegrationTest.Builder("se", "swedbank-bankid")
+                .setCredentialType(CredentialsTypes.MOBILE_BANKID)
+                .addCredentialField(Field.Key.USERNAME, "yyyyMMddnnnn")
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(true);
+
+        builder.build()
+                .testRefresh();
+    }
+}
