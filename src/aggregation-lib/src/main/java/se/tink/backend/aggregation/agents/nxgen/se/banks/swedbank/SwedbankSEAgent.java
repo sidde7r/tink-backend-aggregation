33c0c1c20f1de27c368d38672179bd6d76794543
ovesan <ove.sandfeldt@tink.se>
2018-06-29 10:29:21 +0200

    fea(Swedbank) Add Swedbank agent for Sweden

---
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/SwedbankSEAgent.java
@@ -0,0 +1,18 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.SwedbankAbstractAgent;
+import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
+import se.tink.backend.aggregation.rpc.CredentialsRequest;
+
+public class SwedbankSEAgent extends SwedbankAbstractAgent {
+
+    public SwedbankSEAgent(CredentialsRequest request, AgentContext context) {
+        super(request, context, new SwedbankSEConfiguration(request.getProvider().getPayload()));
+    }
+
+    @Override
+    protected void configureHttpClient(TinkHttpClient client) {
+        super.configureHttpClient(client);
+    }
+}
