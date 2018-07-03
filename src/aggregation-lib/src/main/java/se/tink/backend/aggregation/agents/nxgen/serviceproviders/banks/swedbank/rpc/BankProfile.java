30dd42b896f79c9d68b95b53ba2f42d297ac00f9
ovesan <ove.sandfeldt@tink.se>
2018-07-03 16:46:09 +0200

    ref(Swedbank) Classes must be serializable, used in temporary storage

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/rpc/BankProfile.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/rpc/BankProfile.java
@@ -3,12 +3,17 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank
 import com.google.common.base.Preconditions;
 import java.util.Map;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.fetchers.transferdestination.rpc.PaymentBaseinfoResponse;
+import se.tink.backend.aggregation.annotations.JsonObject;
 
+@JsonObject
 public class BankProfile {
-    private final BankEntity bank;
-    private final Map<String, MenuItemLinkEntity> menuItems;
-    private final EngagementOverviewResponse engagementOverViewResponse;
-    private final PaymentBaseinfoResponse paymentBaseinfoResponse;
+    private BankEntity bank;
+    private Map<String, MenuItemLinkEntity> menuItems;
+    private EngagementOverviewResponse engagementOverViewResponse;
+    private PaymentBaseinfoResponse paymentBaseinfoResponse;
+
+    public BankProfile() {
+    }
 
     public BankProfile(BankEntity bank,
             Map<String, MenuItemLinkEntity> menuItems,
