f6bbd972bb9b03a2c5b7c29155f9968c4bfcc96d
Alexander Jamm <alexander.jamm@tink.se>
2018-07-04 15:55:41 +0200

    ref(Fidor) removed sandbox credentials & cleanup

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fidor/FidorConstants.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fidor/FidorConstants.java
@@ -4,20 +4,15 @@ public class FidorConstants {
 
     public static final String BEARER_TOKEN = "Bearer ";
     public static final String BASIC = "Basic ";
-    public static final int STATUS_API_RATE_LIMIT_EXCEEDED = 429;
 
     public static final String SANBOX_CLIENT_ID = "4c7b5996af0ee5db";
-    public static final String SANDBOX_CLIENT_SECRET = "6e581c440cf99f17625a2ec6bc404a80";
     public static final String SANDBOX_BASE64_BASIC_AUTH = "NGM3YjU5OTZhZjBlZTVkYjo2ZTU4MWM0NDBjZjk5ZjE3NjI1YTJlYzZiYzQwNGE4MA=="; //client_id:client_secret
-    public static final String SANDBOX_USER_USERNAME = "api_tester@example.com";
-    public static final String SANDBOX_USER_PASSWORD = "password";
     public static final String SANDBOX_REDIRECT_URL = "http://google.com";
     public static final String STATE = "99MagiCat99";
 
     public class URL {
 
         public class OPENAPI {
-            public static final String CUSTOMERS = "/customers";
             public static final String ACCOUNTS = "/accounts";
             public static final String TRANSACTIONS = "/transactions";
             public static final String RATELIMIT = "/rate_limit";
@@ -26,9 +21,7 @@ public class FidorConstants {
             public static final String UPCOMING_TRANSACTIONS = "/preauths";
 
             public static final String SANDBOX_BASE = "https://aps.fidor.de";
-            public static final String SANDBOX_OAUTH = "/oauth/authorize";
         }
-
     }
 
     public class STORAGE {
@@ -40,9 +33,6 @@ public class FidorConstants {
         public class OPENAPI {
             public static final String CLIENT_ID = "client_id";
             public static final String REDIRECT_URI = "redirect_uri";
-            public static final String STATE = "state";
-            public static final String RESPONSE_TYPE = "response_type";
-            public static final String RESPONSE_TYPE_CODE = "code";
             public static final String GRANT_TYPE = "grant_type";
             public static final String GRANT_TYPE_AUTHORIZATION_CODE = "authorization_code";
             public static final String GRANT_TYPE_REFRESH_TOKEN = "refresh_token";
@@ -52,7 +42,6 @@ public class FidorConstants {
             public static final String PER_PAGE_MAXVALUE = "100";
             public static final String REFRESH_TOKEN = "refresh_token";
         }
-
     }
 
     public class FORM {
@@ -63,7 +52,6 @@ public class FidorConstants {
 
     public class HEADERS {
         public class OPENAPI {
-            public static final String LOCATION = "Location";
             public static final String APPLICATION_JSON_FIDOR_V1 = "application/vnd.fidor.de; version=1,text/json";
         }
     }
