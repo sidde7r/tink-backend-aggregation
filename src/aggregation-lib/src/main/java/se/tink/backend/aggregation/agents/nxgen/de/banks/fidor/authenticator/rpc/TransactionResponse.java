6f6a17369f0c0964af46a646c76183e6fcc88c24
Alexander Jamm <alexander.jamm@tink.se>
2018-07-05 11:41:58 +0200

    fix(Fidor) TransactionResponse now implements PaginatorResponse

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fidor/authenticator/rpc/TransactionResponse.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fidor/authenticator/rpc/TransactionResponse.java
@@ -2,15 +2,16 @@ package se.tink.backend.aggregation.agents.nxgen.de.banks.fidor.authenticator.rp
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.de.banks.fidor.authenticator.entities.CollectionEntity;
 import se.tink.backend.aggregation.agents.nxgen.de.banks.fidor.authenticator.entities.TransactionEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionPagePaginatorResponse;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 
 @JsonObject
-public class TransactionResponse {
+public class TransactionResponse implements TransactionPagePaginatorResponse {
 
     @JsonProperty("data")
     private ArrayList<TransactionEntity> transactions;
@@ -26,13 +27,15 @@ public class TransactionResponse {
         return transactions;
     }
 
-    public List<Transaction> toTinkTransactions(){
+    @Override
+    public Collection<? extends Transaction> getTinkTransactions() {
         return transactions.stream()
                 .map(TransactionEntity::toTinkTransaction)
                 .collect(Collectors.toList());
     }
 
-    public boolean canFetchMore(int page){
-        return page < (collectionEntity.getTotalPages() - 1);
+    @Override
+    public boolean canFetchMore() {
+        return collectionEntity.getCurrentPage() < collectionEntity.getTotalPages();
     }
 }
