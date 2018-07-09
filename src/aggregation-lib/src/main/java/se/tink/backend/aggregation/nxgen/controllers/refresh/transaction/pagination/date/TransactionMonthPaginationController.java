020349d7c365120e0c2136b2e911835da0e8aa26
apete <anders.peterson@tink.se>
2018-07-09 16:50:00 +0200

    ref Improved the TransactionMonthPaginator
    It now, potentially, fetches more data

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/date/TransactionMonthPaginationController.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/date/TransactionMonthPaginationController.java
@@ -4,24 +4,33 @@ import com.google.common.base.Preconditions;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Year;
+import java.time.ZoneId;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Objects;
-import java.util.TimeZone;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.TransactionPaginator;
 import se.tink.backend.aggregation.nxgen.core.account.Account;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 
 public class TransactionMonthPaginationController<A extends Account> implements TransactionPaginator<A> {
-    static final int MAX_CONSECUTIVE_EMPTY_PAGES = 3;
-    private final LocalDate nowInLocalDate = LocalDate.now(TimeZone.getTimeZone("Europe/Stockholm").toZoneId());
+    protected static final int MAX_CONSECUTIVE_EMPTY_PAGES = 4;
+    protected static final int MAX_TOTAL_EMPTY_PAGES = 24;
+    private static final ZoneId DEFAULT_ZONE_ID = ZoneId.of("Europe/Stockholm");
+    private final LocalDate nowInLocalDate;
     private final TransactionMonthPaginator paginator;
     private A currentAccount;
     private LocalDate dateToFetch;
     private int consecutiveEmptyFetches = 0;
+    private int totalEmptyFetches = 0;
+    private boolean haveFoundSomething;
 
     public TransactionMonthPaginationController(TransactionMonthPaginator paginator) {
+        this(paginator, DEFAULT_ZONE_ID);
+    }
+
+    public TransactionMonthPaginationController(TransactionMonthPaginator paginator, ZoneId zoneId) {
         this.paginator = Preconditions.checkNotNull(paginator);
+        nowInLocalDate = LocalDate.now(zoneId);
     }
 
     @Override
@@ -37,10 +46,12 @@ public class TransactionMonthPaginationController<A extends Account> implements 
 
         if (transactions == null || transactions.isEmpty()) {
             consecutiveEmptyFetches++;
+            totalEmptyFetches++;
             return Collections.emptyList();
         }
 
         consecutiveEmptyFetches = 0;
+        haveFoundSomething = true;
         return transactions;
     }
 
@@ -48,7 +59,7 @@ public class TransactionMonthPaginationController<A extends Account> implements 
     public boolean canFetchMoreFor(A account) {
         resetStateIfAccountChanged(account);
 
-        return consecutiveEmptyFetches < MAX_CONSECUTIVE_EMPTY_PAGES;
+        return haveFoundSomething ? consecutiveEmptyFetches < MAX_CONSECUTIVE_EMPTY_PAGES : totalEmptyFetches < MAX_TOTAL_EMPTY_PAGES;
     }
 
     private void resetStateIfAccountChanged(A account) {
@@ -60,6 +71,8 @@ public class TransactionMonthPaginationController<A extends Account> implements 
 
         currentAccount = account;
         consecutiveEmptyFetches = 0;
+        totalEmptyFetches = 0;
+        haveFoundSomething = false;
         dateToFetch = nowInLocalDate;
     }
 }
