75025ead7c35f635ae7c99ec448a3abf2f882a9c
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-07-10 16:01:26 +0200

    fix(cic) Add error logging when error from investment endpoint

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/euroinformation/fetcher/EuroInformationInvestmentAccountFetcher.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/euroinformation/fetcher/EuroInformationInvestmentAccountFetcher.java
@@ -12,6 +12,7 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinfor
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.fetcher.entities.SecurityAccountListEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.fetcher.rpc.InvestmentAccountOverviewResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.fetcher.rpc.InvestmentAccountsListResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.utils.EuroInformationErrorCodes;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.utils.EuroInformationUtils;
 import se.tink.backend.aggregation.agents.utils.log.LogTag;
 import se.tink.backend.aggregation.log.AggregationLogger;
@@ -46,7 +47,13 @@ public class EuroInformationInvestmentAccountFetcher implements AccountFetcher<I
                 .get(EuroInformationConstants.Tags.INVESTMENT_ACCOUNTS, InvestmentAccountsListResponse.class)
                 .orElseGet(() -> apiClient.requestInvestmentAccounts());
 
-        if (!EuroInformationUtils.isSuccess(investmentAccountsListResponse.getReturnCode())) {
+        String returnCode = investmentAccountsListResponse.getReturnCode();
+        if (!EuroInformationUtils.isSuccess(returnCode)) {
+            if (!EuroInformationErrorCodes.NO_ACCOUNT.equals(EuroInformationErrorCodes.getByCodeNumber(returnCode))) {
+                LOGGER.error("Problem while fetching investment accounts: " + investmentAccountsListResponse);
+            } else {
+                LOGGER.info("No investment accounts");
+            }
             return Collections.emptyList();
         }
 
