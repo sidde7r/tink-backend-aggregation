32ce088941f7c6c2e29f7e8f6a2be4ff4915b485
Johan Haukka <johan.haukka@gmail.com>
2018-07-10 14:17:05 +0200

    fea(be/ing) add agent test for ing

---
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/ing/IngAgentTest.java
@@ -0,0 +1,30 @@
+package se.tink.backend.aggregation.agents.nxgen.be.banks.ing;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.rpc.CredentialsTypes;
+import se.tink.backend.aggregation.rpc.Field;
+
+@Ignore
+public class IngAgentTest {
+
+    private static final String USERNAME = "";
+    private static final String CARD_ID = "";
+    private static final String OTP = "";
+
+    private static final AgentIntegrationTest.Builder builder =
+            new AgentIntegrationTest.Builder("be", "be-ing-cardreader")
+                    .setCredentialType(CredentialsTypes.PASSWORD)
+                    .addCredentialField(Field.Key.USERNAME, USERNAME)
+                    .addCredentialField("cardId", CARD_ID)
+                    .addCredentialField("otp", OTP)
+                    .loadCredentialsBefore(true)
+                    .saveCredentialsAfter(true);
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder.build()
+                .testRefresh();
+    }
+}
