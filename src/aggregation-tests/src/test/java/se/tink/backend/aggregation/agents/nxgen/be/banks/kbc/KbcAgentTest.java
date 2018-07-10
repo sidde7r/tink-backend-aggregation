da9018406b5c94827f0c75f5aea720e0f96b2c70
Johan Haukka <johan.haukka@gmail.com>
2018-07-10 13:17:02 +0200

    fea(kbc/test) add agent test for kbc

---
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/KbcAgentTest.java
@@ -0,0 +1,26 @@
+package se.tink.backend.aggregation.agents.nxgen.be.banks.kbc;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.rpc.CredentialsTypes;
+import se.tink.backend.aggregation.rpc.Field;
+
+@Ignore
+public class KbcAgentTest {
+
+    private static final String USERNAME = "";
+
+    private static final AgentIntegrationTest.Builder builder =
+            new AgentIntegrationTest.Builder("be", "be-kbc-cardreader")
+                    .setCredentialType(CredentialsTypes.PASSWORD)
+                    .addCredentialField(Field.Key.USERNAME, USERNAME)
+                    .loadCredentialsBefore(true)
+                    .saveCredentialsAfter(true);
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder.build()
+                .testRefresh();
+    }
+}
