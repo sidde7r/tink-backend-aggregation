95f9d25dd5beec0562d8b8cbcbe34764419ebaac
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-07-10 16:31:48 +0200

    fea(bmce) Add BMCE Direct configuration

---
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/banks/bmcedirect/BmceDirectConfiguration.java
@@ -0,0 +1,15 @@
+package se.tink.backend.aggregation.agents.nxgen.fr.banks.bmcedirect;
+
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.EuroInformationConfiguration;
+
+public class BmceDirectConfiguration implements EuroInformationConfiguration {
+    @Override
+    public String getUrl() {
+        return BmceDirectConstants.URL;
+    }
+
+    @Override
+    public String getTarget() {
+        return BmceDirectConstants.TARGET;
+    }
+}
---
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/banks/bmcedirect/BmceDirectConstants.java
@@ -0,0 +1,10 @@
+package se.tink.backend.aggregation.agents.nxgen.fr.banks.bmcedirect;
+
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.EuroInformationConstants;
+
+public class BmceDirectConstants extends EuroInformationConstants {
+    // TODO: The login request goes to the `fr/wsmobile`, but the logcrash request goes to `wsmobile/fr` so this should be doublechecked when possible
+    // TODO: Check if endpoint can be found in the android app code
+    public static final String URL = "https://www.bmcedirect.ma/fr/wsmobile/";
+    public static final String TARGET = "CM";
+}
