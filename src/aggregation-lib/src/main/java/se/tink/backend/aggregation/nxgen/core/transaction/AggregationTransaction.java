ba00c91424f1cfa6fe59a4cc5adadf209f48cc6d
Johan Haukka <johan.haukka@gmail.com>
2018-07-10 13:10:27 +0200

    fea(transactions) move raw details setter to base class

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/nxgen/core/transaction/AggregationTransaction.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/nxgen/core/transaction/AggregationTransaction.java
@@ -1,20 +1,25 @@
 package se.tink.backend.aggregation.nxgen.core.transaction;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import java.util.Date;
 import se.tink.backend.core.Amount;
+import se.tink.backend.system.rpc.TransactionPayloadTypes;
 import se.tink.backend.system.rpc.TransactionTypes;
 import se.tink.libraries.date.DateUtils;
+import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public abstract class AggregationTransaction {
     private final Amount amount;
     private final String description;
     private final Date date;
+    private final String rawDetails;
 
-    protected AggregationTransaction(Amount amount, Date date, String description) {
+    protected AggregationTransaction(Amount amount, Date date, String description, String rawDetails) {
         this.amount = amount;
         this.date = date;
         this.description = description;
+        this.rawDetails = rawDetails;
     }
 
     public Amount getAmount() {
@@ -29,6 +34,10 @@ public abstract class AggregationTransaction {
         return date;
     }
 
+    public String getRawDetails() {
+        return rawDetails;
+    }
+
     public TransactionTypes getType() {
         return TransactionTypes.DEFAULT;
     }
@@ -41,6 +50,10 @@ public abstract class AggregationTransaction {
         transaction.setDate(getDate());
         transaction.setType(getType());
 
+        if (!Strings.isNullOrEmpty(getRawDetails())) {
+            transaction.setPayload(TransactionPayloadTypes.DETAILS, getRawDetails());
+        }
+
         return transaction;
     }
 
@@ -48,6 +61,7 @@ public abstract class AggregationTransaction {
         private Amount amount;
         private String description;
         private Date date;
+        private String rawDetails;
 
         public Builder setAmount(Amount amount) {
             this.amount = amount;
@@ -74,6 +88,21 @@ public abstract class AggregationTransaction {
 
         Date getDate() {
             return date != null ? DateUtils.flattenTime(date) : null;
+        }
+
+        public Builder setRawDetails(Object rawDetails) {
+            if (rawDetails != null) {
+                if (rawDetails instanceof String) {
+                    this.rawDetails = (String)rawDetails;
+                } else {
+                    this.rawDetails = SerializationUtils.serializeToString(rawDetails);
+                }
+            }
+            return this;
+        }
+
+        String getRawDetails() {
+            return rawDetails;
         }
 
         public abstract AggregationTransaction build();
