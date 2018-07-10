7d3cb232e3e3f5ed1807cb3f4b8f25264755e1c5
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-07-10 09:47:05 +0200

    fix(targo) revert accidental URL rename

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/targo/TargoBankDEConstants.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/targo/TargoBankDEConstants.java
@@ -3,6 +3,6 @@ package se.tink.backend.aggregation.agents.nxgen.de.banks.targo;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroInformation.EuroInformationConstants;
 
 public class TargoBankDEConstants extends EuroInformationConstants {
-    public static final String URL = "https://m.euroInformation.de/wsmobile/en/";
+    public static final String URL = "https://m.euroinformation.de/wsmobile/en/";
     public static final String TARGET = "TARGO";
 }
