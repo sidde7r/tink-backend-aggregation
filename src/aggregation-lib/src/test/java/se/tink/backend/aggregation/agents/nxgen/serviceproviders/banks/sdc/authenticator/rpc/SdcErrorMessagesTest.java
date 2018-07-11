06089b1f1e30102c5c01f7c3ae38ac253cd6bab0
ovesan <ove.sandfeldt@tink.se>
2018-07-11 14:25:24 +0200

    fix(SDC) Add handling of not a customer error during authentication

---
+++ src/aggregation-lib/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/authenticator/rpc/SdcErrorMessagesTest.java
@@ -0,0 +1,31 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.authenticator.rpc;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.SdcNoConfiguration;
+import se.tink.backend.aggregation.rpc.Provider;
+
+public class SdcErrorMessagesTest {
+    private SdcNoConfiguration agentConfiguration;
+
+    @Before
+    public void setUp() throws Exception {
+        Provider provider = new Provider();
+        provider.setPayload("1234");
+
+        agentConfiguration = new SdcNoConfiguration(provider);
+    }
+
+    @Test
+    public void noCustomerErrorMessage() {
+        String errorMessage = "You have no agreement";
+        Assert.assertTrue(agentConfiguration.isNotCustomer(errorMessage));
+    }
+
+    @Test
+    public void notNoCustomerErrorMessage() {
+        String errorMessage = "I am a little teapot";
+        Assert.assertFalse(agentConfiguration.isNotCustomer(errorMessage));
+    }
+}
