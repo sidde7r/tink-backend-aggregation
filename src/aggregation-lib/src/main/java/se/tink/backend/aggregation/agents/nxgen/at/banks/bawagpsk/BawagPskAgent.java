b9d4f89011f549e8e7fc1407b9d2fa0ab8025bec
Alexander Jamm <alexander.jamm@tink.se>
2018-07-11 11:09:07 +0200

    ref(Bawag) Removed debug output

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/banks/bawagpsk/BawagPskAgent.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/banks/bawagpsk/BawagPskAgent.java
@@ -35,7 +35,6 @@ public class BawagPskAgent extends NextGenerationAgent {
 
     @Override
     protected void configureHttpClient(TinkHttpClient client) {
-        client.setDebugOutput(true);
     }
 
     @Override
