3c0285cd19ee2cba97565aa7adc415248e04539b
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-07-12 10:25:48 +0200

    sty(bmce) Remove TODO comment after performing the check

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/banks/bmcedirect/BmceDirectConstants.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/banks/bmcedirect/BmceDirectConstants.java
@@ -2,9 +2,8 @@ package se.tink.backend.aggregation.agents.nxgen.fr.banks.bmcedirect;
 
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.EuroInformationConstants;
 
+// Banque Marocaine du Commerce Extérieur
 public class BmceDirectConstants extends EuroInformationConstants {
-    // TODO: The login request goes to the `fr/wsmobile`, but the logcrash request goes to `wsmobile/fr` so this should be doublechecked when possible
-    // TODO: Check if endpoint can be found in the android app code
     public static final String URL = "https://www.bmcedirect.ma/fr/wsmobile/";
     public static final String TARGET = "CM";
     // TODO: In the bank app this version is set in the body of the message, not in header like for different agents using same serviceProvider
