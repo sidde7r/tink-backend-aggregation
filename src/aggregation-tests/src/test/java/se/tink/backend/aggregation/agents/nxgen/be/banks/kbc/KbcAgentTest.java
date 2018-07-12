03e1357c126610ad1d1cc4d58db51afd8952f98d
Annica Alstermark <annica.alstermark@tink.se>
2018-07-12 17:24:09 +0200

    fix(tests) Remove call to nonexisting method in KBC agent test

--- src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/KbcAgentTest.java
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/KbcAgentTest.java
@@ -3,7 +3,6 @@ package se.tink.backend.aggregation.agents.nxgen.be.banks.kbc;
 import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
-import se.tink.backend.aggregation.rpc.CredentialsTypes;
 import se.tink.backend.aggregation.rpc.Field;
 
 @Ignore
@@ -13,7 +12,6 @@ public class KbcAgentTest {
 
     private static final AgentIntegrationTest.Builder builder =
             new AgentIntegrationTest.Builder("be", "be-kbc-cardreader")
-                    .setCredentialType(CredentialsTypes.PASSWORD)
                     .addCredentialField(Field.Key.USERNAME, USERNAME)
                     .loadCredentialsBefore(true)
                     .saveCredentialsAfter(true);
