6b0faadbf7323d1ac11f67c19cde1b0c0099b8b2
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-07-12 09:02:21 +0200

    fea(mona) Adding MonaBanq configuration

---
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/banks/monabankq/MonaBanqConfiguration.java
@@ -0,0 +1,20 @@
+package se.tink.backend.aggregation.agents.nxgen.fr.banks.monabankq;
+
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.EuroInformationConfiguration;
+
+public class MonaBanqConfiguration implements EuroInformationConfiguration {
+    @Override
+    public String getUrl() {
+        return MonaBanqConstants.URL;
+    }
+
+    @Override
+    public String getTarget() {
+        return MonaBanqConstants.TARGET;
+    }
+
+    @Override
+    public String getAppVersion() {
+        return MonaBanqConstants.APP_VERSION;
+    }
+}
---
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/banks/monabankq/MonaBanqConstants.java
@@ -0,0 +1,9 @@
+package se.tink.backend.aggregation.agents.nxgen.fr.banks.monabankq;
+
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.EuroInformationConstants;
+
+public class MonaBanqConstants extends EuroInformationConstants {
+    public static final String URL = "https://m.monabanq.com/fr/wsmobile/";
+    public static final String TARGET = "MONA";
+    public static final String APP_VERSION = "V4.46.0";
+}
