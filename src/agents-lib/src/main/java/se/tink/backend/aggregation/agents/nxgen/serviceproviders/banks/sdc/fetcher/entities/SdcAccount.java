d499bd79b26b6a881931e28c400a8fd9ef2287a1
Jonas Renold <jonas.renold@tink.se>
2018-07-13 16:09:17 +0200

    fix(sdc) set account uniqueId explictly
    Set it explicitly to accountNumber which it was before instead of relying on the previous logic of accountNumber -> uniqueId.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/fetcher/entities/SdcAccount.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/fetcher/entities/SdcAccount.java
@@ -29,6 +29,7 @@ public class SdcAccount {
     public TransactionalAccount toTinkAccount(SdcConfiguration agentConfiguration) {
         return TransactionalAccount.builder(convertAccountType(name, agentConfiguration.getTypeGuesser()), id,
                 amount.toTinkAmount())
+                .setUniqueIdentifier(id)
                 .setName(name)
                 .setBankIdentifier(normalizedBankId())
                 .build();
