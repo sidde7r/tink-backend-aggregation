55b84899a1ae6a704ba2715c80ead94d2ebc5b69
Mathew Clegg <mathew.clegg@live.se>
2018-07-13 10:53:15 +0200

    (Fix) Fix for fields in providers due to encoding issues

--- tools/testing/aggregation_db_seed/seed_local.py
+++ tools/testing/aggregation_db_seed/seed_local.py
@@ -54,7 +54,9 @@ def join(columns, rowdict):
 def formatInput(rowdict):
     for row in rowdict:
         if isinstance(rowdict[row], list) or isinstance(rowdict[row], dict): 
-            rowdict[row] = "'" + json.dumps(rowdict[row]) + "'"
+            #rowdict[row] = "'" + json.dumps(rowdict[row], ensure_ascii=False) + "'"
+            rowdict[row] = json.dumps(rowdict[row], ensure_ascii=False)
+            print rowdict[row]
 
 def findPrimaryKey(definedColumns):
     for a in definedColumns:
