aa386b34d98ab9d01bcbbe4dc9336cc1c1f6c0dd
Mathew Clegg <mathew.clegg@live.se>
2018-07-17 14:05:04 +0200

    fix(AggregationDB) Changed seeding of aggregationDB
    Changed columns in the aggregation database to match the ones in mainDB.
    
    Columns that need to change type in aggregation
    +------------------------+--------------+------+-----+---------+-------+
    | Field | Type | Null | Key | Default | Extra |
    +------------------------+--------------+------+-----+---------+-------+
    | capabilities | varchar(255) | YES | | NULL | |
    | payload | varchar(255) | YES | | NULL | |
    | status | varchar(255) | YES | | NULL | |
    | type | varchar(255) | YES | | NULL | |
    +------------------------+--------------+------+-----+---------+-------+
    
    and should be changed to:
    +------------------------+--------------+------+-----+---------+-------+
    | Field | Type | Null | Key | Default | Extra |
    +------------------------+--------------+------+-----+---------+-------+
    | capabilities | longtext | YES | | NULL | |
    | payload | longtext | YES | | NULL | |
    | status | varchar(255) | YES | MUL | NULL | |
    | type | varchar(255) | YES | MUL | NULL | |
    
    and added support for null fields

--- src/main-api/src/main/java/se/tink/backend/core/ProviderConfiguration.java
+++ src/main-api/src/main/java/se/tink/backend/core/ProviderConfiguration.java
@@ -2,10 +2,17 @@ package se.tink.backend.core;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.MoreObjects;
 import com.google.common.base.Objects;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
+import io.protostuff.Exclude;
+import io.protostuff.Tag;
+import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +23,7 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import org.hibernate.annotations.Type;
 import se.tink.libraries.serialization.utils.SerializationUtils;
 
@@ -23,51 +31,183 @@ import se.tink.libraries.serialization.utils.SerializationUtils;
 @Table(name = "provider_configurations")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ProviderConfiguration {
+    private static final String DEMO_AGENT_CLASS_NAME = "demo.DemoAgent";
+
     @SuppressWarnings("serial")
-    private static class CapabilityList extends ArrayList<ProviderConfiguration.Capability> {}
+    private static class CapabilityList extends ArrayList<Capability> {}
     @SuppressWarnings("serial")
     private static class FieldsList extends ArrayList<Field> {}
 
-    @Column(name = "capabilities")
+    @Tag(15)
+    @Column(name = "`capabilities`")
+    @Type(type = "text")
+    @ApiModelProperty(name = "capabilitiesSerialized", hidden = true)
     private String capabilitiesSerialized;
+    @Exclude
+    @ApiModelProperty(name = "className", hidden = true)
     private String className;
     @Enumerated(EnumType.STRING)
+    @Tag(5)
     private CredentialsTypes credentialsType;
+    @Exclude
     private String currency;
+    @Tag(2)
     private String displayName;
-    private String displayDescription;
     @JsonProperty("fields")
-    @Column(name = "fields")
+    @Column(name = "`fields`")
     @Type(type = "text")
+    @Tag(9)
     private String fieldsSerialized;
+    @Tag(10)
     private String groupDisplayName;
+    @Exclude
     private String market;
+    @Exclude
     private boolean multiFactor;
     @Id
+    @Tag(1)
     private String name;
     @Type(type = "text")
+    @Tag(6)
     private String passwordHelpText;
+    @Type(type = "text")
+    @Exclude
+    @ApiModelProperty(name = "payload", hidden = true)
     private String payload;
+    @Tag(7)
     private boolean popular;
+    @JsonIgnore
+    @Exclude
+    @ApiModelProperty(name = "refreshFrequency", hidden = true)
     private double refreshFrequency;
+    @JsonIgnore
+    @Exclude
     private double refreshFrequencyFactor;
     @Enumerated(EnumType.STRING)
+    @Tag(4)
     private ProviderStatuses status;
+    @Tag(8)
     private boolean transactional;
     @Enumerated(EnumType.STRING)
+    @Tag(3)
     private ProviderTypes type;
-    @Column(name = "refreshschedule")
+    @Tag(13)
+    @Transient
+    private ImageUrls images;
+    @Tag(14)
+    private String displayDescription;
+    @Column(name = "`refreshschedule`")
     @Type(type = "text")
+    @Exclude
     private String refreshScheduleSerialized;
 
+
+    /*
+   Difference between fields in main and fields in aggregation.
+   These fields can be added later on to match the fields in main
+
+    @Exclude
+    private String email;
+
+    @Exclude
+    private String url;
+
+    @Tag(11)
+    @ApiModelProperty(name = "tutorialUrl", hidden = true)
+    private String tutorialUrl;
+
+    @Exclude
+    private String phone;
+
+
+    @ApiModelProperty(name = "email", value="The contact information email to the provider")
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTutorialUrl() {
+        return tutorialUrl;
+    }
+
+    public void setTutorialUrl(String tutorialUrl) {
+        this.tutorialUrl = tutorialUrl;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @ApiModelProperty(name = "url", value="The contact information URL to the provider")
+    public String getUrl() {
+        return url;
+    }
+
+    @ApiModelProperty(name = "phone", value="The contact information phone number to the provider")
+    public String getPhone() {
+        return phone;
+    }*/
+
+    public ProviderConfiguration() {
+        setFields(Lists.<Field> newArrayList());
+    }
+
+    @Override
+    public Provider clone() {
+        try {
+            return (Provider) super.clone();
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // Generated using Eclipse
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        ProviderConfiguration other = (ProviderConfiguration) obj;
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Deprecated
+    @JsonProperty
+    @ApiModelProperty(name = "additionalInformationCaption", hidden = true)
+    public String getAdditionalInformationCaption() {
+        Field field = getField(Field.Key.ADDITIONAL_INFORMATION);
+
+        return (field != null ? field.getDescription() : null);
+    }
+
+    @ApiModelProperty(name = "capabilities", hidden = true)
     @JsonProperty("capabilities")
-    public Set<ProviderConfiguration.Capability> getCapabilities() {
+    public Set<Capability> getCapabilities() {
         if (Strings.isNullOrEmpty(capabilitiesSerialized)) {
             return Sets.newHashSet();
         }
 
-        return Sets.newConcurrentHashSet(
-                SerializationUtils.deserializeFromString(capabilitiesSerialized, ProviderConfiguration.CapabilityList.class));
+        return Sets.newConcurrentHashSet(SerializationUtils.deserializeFromString(capabilitiesSerialized, CapabilityList.class));
     }
 
     public String getCapabilitiesSerialized() {
@@ -78,18 +218,22 @@ public class ProviderConfiguration {
         return className;
     }
 
+    @ApiModelProperty(name = "credentialsType", value="The type of credentials the provider creates", example = "MOBILE_BANKID", allowableValues = CredentialsTypes.DOCUMENTED)
     public CredentialsTypes getCredentialsType() {
         return credentialsType;
     }
 
+    @ApiModelProperty(name = "currency", value="The default currency of the provider", example = "SEK")
     public String getCurrency() {
         return currency;
     }
 
+    @ApiModelProperty(name = "displayName", value="The display name of the provider", example = "Handelsbanken")
     public String getDisplayName() {
         return displayName;
     }
 
+    @ApiModelProperty(name = "displayDescription", value="The display description of the provider", example = "Mobilt BankID")
     public String getDisplayDescription() {
         return displayDescription;
     }
@@ -97,7 +241,7 @@ public class ProviderConfiguration {
     private Field getField(final String name) {
         List<Field> fields = getFields();
 
-        if (fields == null || fields.isEmpty()) {
+        if (fields == null || fields.size() == 0) {
             return null;
         }
 
@@ -111,23 +255,61 @@ public class ProviderConfiguration {
     }
 
     public List<Field> getFields() {
-        return SerializationUtils.deserializeFromString(fieldsSerialized, ProviderConfiguration.FieldsList.class);
+        return SerializationUtils.deserializeFromString(fieldsSerialized, FieldsList.class);
     }
 
+    @ApiModelProperty(name = "groupDisplayName", value="The grouped display name of the provider")
     public String getGroupDisplayName() {
         return groupDisplayName;
     }
 
+    @ApiModelProperty(name = "market", value="The market of the provider")
     public String getMarket() {
         return market;
     }
 
+    @ApiModelProperty(name = "name", value="The short name of the provider")
     public String getName() {
         return name;
     }
 
+    @Deprecated
+    @JsonProperty
+    @ApiModelProperty(name = "passwordCaption", hidden = true)
+    public String getPasswordCaption() {
+        Field field = getField(Field.Key.PASSWORD);
+
+        return (field != null ? field.getDescription() : null);
+    }
+
     public String getPasswordHelpText() {
         return passwordHelpText;
+    }
+
+    @Deprecated
+    @JsonProperty
+    @ApiModelProperty(name = "passwordIsPIN", hidden = true)
+    public boolean getPasswordIsPIN() {
+        Field field = getField(Field.Key.PASSWORD);
+
+        if (field == null) {
+            return false;
+        }
+
+        if (Objects.equal("PIN-kod", field.getDescription())) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Deprecated
+    @JsonProperty
+    @ApiModelProperty(name = "passwordLength", hidden = true)
+    public int getPasswordLength() {
+        Field field = getField(Field.Key.PASSWORD);
+
+        return ((field != null && field.getMaxLength() != null) ? field.getMaxLength() : 0);
     }
 
     public String getPayload() {
@@ -142,28 +324,68 @@ public class ProviderConfiguration {
         return refreshFrequencyFactor;
     }
 
+    @ApiModelProperty(name = "status", value="The current status of the provider")
     public ProviderStatuses getStatus() {
         return status;
     }
 
+    @ApiModelProperty(name = "type", value="The type of the provider")
     public ProviderTypes getType() {
         return type;
     }
 
+    @Deprecated
+    @JsonProperty
+    @ApiModelProperty(name = "usernameCaption", hidden = true)
+    public String getUsernameCaption() {
+        Field field = getField(Field.Key.USERNAME);
+
+        return (field != null ? field.getDescription() : null);
+    }
+
+    @Deprecated
+    @JsonProperty
+    @ApiModelProperty(name = "usernameIsPersonnummer", hidden = true)
+    public boolean getUsernameIsPersonnummer() {
+        Field field = getField(Field.Key.USERNAME);
+
+        if (field == null) {
+            return false;
+        }
+
+        if (Objects.equal(field.getHint(), "ÅÅÅÅMMDDNNNN") || Objects.equal(field.getHint(), "ÅÅMMDDNNNN")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        // Generated using Eclipse
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @ApiModelProperty(name = "multiFactor", value="Flag to indicate if the provider requires multi-factor authentication")
     public boolean isMultiFactor() {
         return multiFactor;
     }
 
+    @ApiModelProperty(name = "popular", value="Flag to indicate if the provider is popular")
     public boolean isPopular() {
         return popular;
     }
 
+    @ApiModelProperty(name = "transactional", value="Flag to indicate if the provider provides transactional data")
     public boolean isTransactional() {
         return transactional;
     }
 
     @JsonProperty("capabilities")
-    public void setCapabilities(Set<ProviderConfiguration.Capability> capabilities) {
+    public void setCapabilities(Set<Capability> capabilities) {
         if (capabilities == null) {
             this.capabilitiesSerialized = null;
         }
@@ -247,6 +469,39 @@ public class ProviderConfiguration {
         this.type = type;
     }
 
+    public ImageUrls getImages() {
+        return images;
+    }
+
+    public void setImages(ImageUrls images) {
+        this.images = images;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(Provider.class).add("name", name).toString();
+    }
+
+    @JsonIgnore
+    @Transient
+    public double getCurrentRefreshFrequency() {
+        return refreshFrequency * refreshFrequencyFactor;
+    }
+
+    @JsonIgnore
+    @Transient
+    public String getCleanDisplayName() {
+        // Some of our display names have the authetication method in a parenthesis after.
+        // e.g. Handelsbanken (Mobilt BankID)
+        return displayName.replaceAll(" \\([\\w \\-]+\\)", "");
+    }
+
+    @JsonIgnore
+    @Transient
+    public boolean isUsingDemoAgent() {
+        return DEMO_AGENT_CLASS_NAME.equals(getClassName());
+    }
+
     /**
      * Used on providers to indicate different tasks it can handle in terms of agents, since it's not possible now in
      * main to know if an agent implements an interface e.g. TransferExecutor.
@@ -261,6 +516,7 @@ public class ProviderConfiguration {
     }
 
     @JsonIgnore
+    @Transient
     public Optional<ProviderRefreshSchedule> getRefreshSchedule() {
         if (Strings.isNullOrEmpty(refreshScheduleSerialized)) {
             return Optional.empty();
@@ -270,3 +526,5 @@ public class ProviderConfiguration {
                 SerializationUtils.deserializeFromString(refreshScheduleSerialized, ProviderRefreshSchedule.class));
     }
 }
+
+
