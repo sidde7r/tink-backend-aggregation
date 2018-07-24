8dbc530c58d3b76a1dac36f58681fc00df944498
evchen <minjia.chen@tink.se>
2018-07-24 15:54:24 +0200

    fea(credit safe) move to credit-safe-lib
    move credit safe related code from agents-lib:...fraud to
    credit-safe-lib

---
+++ src/credit-safe-lib/src/main/java/se/tink/backend/aggregation/credit/safe/creditsafe/soap/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "https://webservice.creditsafe.se/getdata/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package se.tink.backend.aggregation.credit.safe.creditsafe.soap;
