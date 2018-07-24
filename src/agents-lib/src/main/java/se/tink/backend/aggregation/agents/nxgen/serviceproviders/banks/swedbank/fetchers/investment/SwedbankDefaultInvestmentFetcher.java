58f24a82da93482459e95edeb69db06483822f85
ovesan <ove.sandfeldt@tink.se>
2018-07-24 09:45:07 +0200

    fix(Swedbank) Catch and log errors when fetching portfolio details
    This is specific for endowment insurances since they are failing

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/fetchers/investment/SwedbankDefaultInvestmentFetcher.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/fetchers/investment/SwedbankDefaultInvestmentFetcher.java
@@ -18,6 +18,7 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.fetchers.investment.rpc.InvestmentSavingsAccountEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.fetchers.investment.rpc.PortfolioHoldingsResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.rpc.BankProfile;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.rpc.EngagementOverviewResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.rpc.LinksEntity;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.core.account.InvestmentAccount;
@@ -57,8 +58,9 @@ public class SwedbankDefaultInvestmentFetcher implements AccountFetcher<Investme
 
             investmentAccounts.addAll(fundAccountsToInvestmentAccounts(
                     portfolioHoldings.getFundAccounts()));
+            // temporary changed to debug
             investmentAccounts.addAll(endowmentInsurancesToTinkInvestmentAccounts(
-                    portfolioHoldings.getEndowmentInsurances()));
+                    portfolioHoldings.getEndowmentInsurances(), bankProfile.getEngagementOverViewResponse()));
             investmentAccounts.addAll(equityTradersToTinkInvestmentAccounts(
                     portfolioHoldings.getEquityTraders()));
             investmentAccounts.addAll(investmentSavingsToTinkInvestmentAccounts(
@@ -90,14 +92,28 @@ public class SwedbankDefaultInvestmentFetcher implements AccountFetcher<Investme
     }
 
     private List<InvestmentAccount> endowmentInsurancesToTinkInvestmentAccounts(
-            List<EndowmentInsuranceEntity> endowmentInsurances) {
+            List<EndowmentInsuranceEntity> endowmentInsurances, EngagementOverviewResponse engagementOverviewResponse) {
         if (endowmentInsurances == null) {
             log.warn(LIST_INPUT_ERROR_FORMAT_MESSAGE, ENDOWMENT_INSURANCE_STRING);
             return Collections.emptyList();
         }
 
-        return defaultAccountToInvestmentAccount(getDetailedPortfolioResponseList(
-                endowmentInsurances, ENDOWMENT_INSURANCE_STRING));
+        // debug logging for Swedbank investments
+        // log errors for endowment insurance details, these are too common
+        // especially try to find out what type of insurance we get errors from and if possible try to figure out
+        // how to retrieve that data
+        try {
+            return defaultAccountToInvestmentAccount(getDetailedPortfolioResponseList(
+                    endowmentInsurances, ENDOWMENT_INSURANCE_STRING));
+        } catch (Exception e) {
+            String investmentInEngagement = "";
+            if (engagementOverviewResponse != null && engagementOverviewResponse.getSavingAccounts() != null) {
+                investmentInEngagement = SerializationUtils.serializeToString(engagementOverviewResponse.getSavingAccounts());
+            }
+            log.warn("Failed to portfolio fetch details for(engagement): " + investmentInEngagement, e);
+            log.warn("Failed to portfolio fetch details for(holdings): " + SerializationUtils.serializeToString(endowmentInsurances), e);
+            return Collections.emptyList();
+        }
     }
 
     private List<InvestmentAccount> defaultAccountToInvestmentAccount(
