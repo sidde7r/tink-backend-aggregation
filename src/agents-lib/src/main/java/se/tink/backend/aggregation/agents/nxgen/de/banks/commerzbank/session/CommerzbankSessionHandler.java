00a0c7b9f91b7512334c68ca430c01ec49726896
michpalm <michpalm95@gmail.com>
2018-07-25 14:28:28 +0200

    fea(Commerzbank DE) Implements logout for session handler

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/commerzbank/session/CommerzbankSessionHandler.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/commerzbank/session/CommerzbankSessionHandler.java
@@ -3,6 +3,7 @@ package se.tink.backend.aggregation.agents.nxgen.de.banks.commerzbank.session;
 import se.tink.backend.aggregation.agents.exceptions.SessionException;
 import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
 import se.tink.backend.aggregation.agents.nxgen.de.banks.commerzbank.CommerzbankApiClient;
+import se.tink.backend.aggregation.agents.nxgen.de.banks.commerzbank.CommerzbankConstants;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
 import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
 
@@ -16,6 +17,13 @@ public class CommerzbankSessionHandler implements SessionHandler {
 
     @Override
     public void logout() {
+        String textLogout = apiClient.logout().getResult().getItems().get(0).getMessage();
+        System.out.println("Text is: " + textLogout);
+        if (apiClient.logout().getResult().getItems().get(0).getMsgKey()
+                .equalsIgnoreCase(CommerzbankConstants.VALUES.LOGOUT_OK)) {
+            return;
+        }
+        throw new IllegalStateException("Error when logging out");
     }
 
     @Override
