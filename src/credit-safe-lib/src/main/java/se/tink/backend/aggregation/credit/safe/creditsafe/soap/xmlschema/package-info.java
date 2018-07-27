158d119b3e6b6cca885c937ee4ce063f3578d104
evchen <minjia.chen@tink.se>
2018-07-27 15:43:16 +0200

    ref(create agent package) move credit safe agent

--- src/credit-safe-lib/src/main/java/se/tink/backend/aggregation/credit/safe/creditsafe/soap/xmlschema/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.w3.org/2001/XMLSchema", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package se.tink.backend.aggregation.credit.safe.creditsafe.soap.xmlschema;
