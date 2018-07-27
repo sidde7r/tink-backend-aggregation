9ab0bbf012cf0d75bee9b5533e97a6f6854daa57
Jonas Renold <jonas.renold@tink.se>
2018-07-27 17:23:07 +0200

    ref(paginator) use PaginatorResponse in PagePaginator
    The old response was identical to the new one. Pagees must define canFetchMore (like the old one).

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/page/TransactionPagePaginationController.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/page/TransactionPagePaginationController.java
@@ -1,17 +1,16 @@
 package se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page;
 
-import java.util.Collection;
+import com.google.common.base.Preconditions;
 import java.util.Objects;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.TransactionPaginator;
 import se.tink.backend.aggregation.nxgen.core.account.Account;
-import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 
 public class TransactionPagePaginationController<A extends Account> implements TransactionPaginator<A> {
     private final TransactionPagePaginator<A> paginator;
     private A currentAccount;
     private int startPage;
     private int currentPage;
-    private boolean canFetchMore;
 
     public TransactionPagePaginationController(TransactionPagePaginator<A> paginator, int startPage) {
         this.paginator = paginator;
@@ -20,19 +19,15 @@ public class TransactionPagePaginationController<A extends Account> implements T
     }
 
     @Override
-    public Collection<? extends Transaction> fetchTransactionsFor(A account) {
+    public PaginatorResponse fetchTransactionsFor(A account) {
         resetStateIfAccountChanged(account);
 
-        TransactionPagePaginatorResponse response = paginator.getTransactionsFor(account, currentPage);
-        this.canFetchMore = response.canFetchMore();
+        PaginatorResponse response = paginator.getTransactionsFor(account, currentPage);
+        Preconditions.checkState(response.canFetchMore().isPresent(), "canFetchMore must be defined.");
+
         currentPage++;
 
-        return response.getTinkTransactions();
-    }
-
-    @Override
-    public boolean canFetchMoreFor(A account) {
-        return canFetchMore;
+        return response;
     }
 
     private void resetStateIfAccountChanged(A account) {
