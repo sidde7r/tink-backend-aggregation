ac13b71b1b2a315d33ba4a9a890433ef7d1a2ffe
Alice Heavey <alice.heavey@tink.se>
2018-07-31 16:17:29 +0200

    ref(se/swedbank) use new annotation @JsonDouble
    Swedbank formats amounts as comma separated strings. Instead of having custom method inside request class for transforming the double value into a String we can use @JsonDouble which has the ability to serialize a double into a String, and set custom decimal separators.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/executors/transfer/rpc/RegisterTransferRequest.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/executors/transfer/rpc/RegisterTransferRequest.java
@@ -1,37 +1,23 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.executors.transfer.rpc;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import java.math.BigDecimal;
+import se.tink.backend.aggregation.annotations.JsonDouble;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import static se.tink.backend.aggregation.annotations.JsonDouble.JsonType;
 
 @JsonObject
 public class RegisterTransferRequest {
     @JsonIgnore
     private static final String EMPTY_DATE = "";
 
-    private final String amount;
+    @JsonDouble(outputType=JsonType.STRING, decimalSeparator=',')
+    private final double amount;
     private final String recipientId;
     private final String fromAccountId;
     private final String date;
 
-    public String getAmount() {
-        return amount;
-    }
-
-    public String getRecipientId() {
-        return recipientId;
-    }
-
-    public String getFromAccountId() {
-        return fromAccountId;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
     private RegisterTransferRequest(double amount, String recipientId, String fromAccountId) {
-        this.amount = formatAmount(amount);
+        this.amount = amount;
         this.recipientId = recipientId;
         this.fromAccountId = fromAccountId;
         this.date = EMPTY_DATE;
@@ -39,10 +25,5 @@ public class RegisterTransferRequest {
 
     public static RegisterTransferRequest create(double amount, String recipientId, String fromAccountId) {
         return new RegisterTransferRequest(amount, recipientId, fromAccountId);
-    }
-
-    private static String formatAmount(double amount) {
-        BigDecimal bigDecimalAmount = new BigDecimal(amount).setScale(2, BigDecimal.ROUND_HALF_UP);
-        return bigDecimalAmount.toString().replace(".", ","); // Swedbank uses comma-separator
     }
 }
