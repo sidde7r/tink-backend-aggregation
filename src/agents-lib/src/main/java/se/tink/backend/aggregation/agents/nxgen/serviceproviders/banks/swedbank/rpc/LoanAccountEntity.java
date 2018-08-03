33c572775b7d804002cfb0f710bb8daeeb683d0f
ovesan <ove.sandfeldt@tink.se>
2018-08-03 12:57:20 +0200

    fix(Swedbank) Parse interest to double value as percent/100
    Since this is the way Tink backend expects it

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/rpc/LoanAccountEntity.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/rpc/LoanAccountEntity.java
@@ -1,5 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.rpc;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Date;
 import java.util.Optional;
 import se.tink.backend.aggregation.annotations.JsonObject;
@@ -26,8 +28,14 @@ public class LoanAccountEntity extends AccountEntity {
                 LoanAccount.builder(fullyFormattedNumber, new Amount(currency, StringUtils.parseAmount(balance)))
                         .setAccountNumber(fullyFormattedNumber)
                         .setDetails(loanDetails)
-                        .setInterestRate(StringUtils.parseAmount(interest))
+                        .setInterestRate(parsePercentageToDouble(interest))
                         .setName(name)
                         .build());
+    }
+
+    private double parsePercentageToDouble(String interestString) {
+        // Using BigDecimal for the division to not end up with stuff like 0.016200000000000003
+        BigDecimal interest = new BigDecimal(StringUtils.parseAmount(interestString));
+        return interest.divide(new BigDecimal(100)).setScale(6, RoundingMode.HALF_UP).doubleValue();
     }
 }
