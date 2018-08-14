d2e68bc7beee4a44c914dbe1f1ccd2617b24659b
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-08-14 16:40:29 +0200

    fix(Swedbank) Add missing JsonObject annotation

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/loan/entities/BorrowerEntity.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/loan/entities/BorrowerEntity.java
@@ -1,5 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.loan.entities;
 
+import se.tink.backend.aggregation.annotations.JsonObject;
+
+@JsonObject
 public class BorrowerEntity {
     private String name;
     private String customerNumber;
