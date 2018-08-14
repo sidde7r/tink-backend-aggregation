7b2d8238800a2cc97d128f0f38c248de74845677
Annica <annicaal@c64.localdomain>
2018-08-14 11:11:06 +0200

    fix(sdcDK) Catch HttpResponseException when fetching custody accounts
    Looks like the logic for determining if we can fetch custody accounts or not is faulty, we seem to try to fetch custody accounts for users that don't have these accounts.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/fetcher/SdcInvestmentFetcher.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/fetcher/SdcInvestmentFetcher.java
@@ -3,6 +3,8 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.fetc
 import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcConfiguration;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcSessionStorage;
@@ -10,8 +12,10 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.authe
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.authenticator.entities.SessionStorageAgreements;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.core.account.InvestmentAccount;
+import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
 
 public class SdcInvestmentFetcher extends SdcAgreementFetcher implements AccountFetcher<InvestmentAccount> {
+    private static final Logger log = LoggerFactory.getLogger(SdcInvestmentFetcher.class);
 
     private final SdcConfiguration agentConfiguration;
 
@@ -23,15 +27,20 @@ public class SdcInvestmentFetcher extends SdcAgreementFetcher implements Account
 
     @Override
     public Collection<InvestmentAccount> fetchAccounts() {
-        if (this.agentConfiguration.canRetrieveInvestmentData()) {
-            SessionStorageAgreements agreements = getAgreements();
-            return agreements.stream().map(agreement -> selectAgreement(agreement, agreements))
-                    .filter(SdcServiceConfigurationEntity::isInvestmentDeposit)
-                    .flatMap(serviceConfiguration ->
-                            this.bankClient.fetchCustodyOverview()
-                                    .toInvestmentAccounts(this.bankClient)
-                    )
-                    .collect(Collectors.toList());
+        try {
+            if (this.agentConfiguration.canRetrieveInvestmentData()) {
+                SessionStorageAgreements agreements = getAgreements();
+                return agreements.stream()
+                        .map(agreement -> selectAgreement(agreement, agreements))
+                        .filter(SdcServiceConfigurationEntity::isInvestmentDeposit)
+                        .flatMap(serviceConfiguration ->
+                                this.bankClient.fetchCustodyOverview()
+                                        .toInvestmentAccounts(this.bankClient)
+                        )
+                        .collect(Collectors.toList());
+            }
+        } catch (HttpResponseException e) {
+            log.warn("Could not fetch custody accounts.", e);
         }
 
         return Collections.emptyList();
