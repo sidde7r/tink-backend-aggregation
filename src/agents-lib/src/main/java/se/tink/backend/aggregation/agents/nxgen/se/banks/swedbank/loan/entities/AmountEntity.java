b0e806a1016accf641ef214427db7bcbfb64e0ed
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-08-14 15:14:03 +0200

    fix(Swedbank) Add missing @JsonObject annotation

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/loan/entities/AmountEntity.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/loan/entities/AmountEntity.java
@@ -3,8 +3,10 @@ package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.loan.entities
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.loan.SwedbankSeSerializationUtils;
+import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.core.Amount;
 
+@JsonObject
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AmountEntity {
     protected String amount;
