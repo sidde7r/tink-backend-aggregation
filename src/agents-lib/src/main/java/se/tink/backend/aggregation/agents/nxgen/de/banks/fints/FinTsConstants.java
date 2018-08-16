4b506f4b9f3a7f0f7be71ee4d15332df33f27c51
Alexander Jamm <alexander.jamm@tink.se>
2018-08-16 12:26:42 +0200

    fix(FinTs) Added errorcode for credential format

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/FinTsConstants.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/FinTsConstants.java
@@ -19,6 +19,7 @@ public class FinTsConstants {
         public static final String LOGIN_FAILED = "9210";
         public static final String ACTION_LOCKED = "9010";
         public static final String ACCOUNT_NOT_ASSIGNED = "9010";
+        public static final String INVALID_USERNAME_FORMAT = "9130";
     }
 
     public static class StatusMessage {
