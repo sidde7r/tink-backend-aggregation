6f3cfde84870cccb0bddcf92038c16893b67b0a6
evchen <minjia.chen@tink.se>
2018-08-17 16:10:39 +0200

    fea(service provider) create service provider

---
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/clients/ProviderServiceFactoryProvider.java
@@ -0,0 +1,64 @@
+package se.tink.backend.aggregation.clients;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.inject.name.Named;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.ClientRequest;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.filter.ClientFilter;
+import org.apache.curator.framework.CuratorFramework;
+import se.tink.backend.aggregation.provider.configuration.client.ClientProviderServiceFactory;
+import se.tink.backend.aggregation.provider.configuration.client.ProviderServiceFactory;
+import se.tink.backend.guice.annotations.ProviderConfiguration;
+import se.tink.libraries.endpoints.EndpointConfiguration;
+
+public class ProviderServiceFactoryProvider implements Provider<ProviderServiceFactory> {
+    private final EndpointConfiguration endpointConfiguration;
+    private final String clusterName;
+    private final String clusterEnvironment;
+
+    protected static final String CLUSTER_NAME_HEADER = "x-tink-cluster-name";
+    protected static final String CLUSTER_ENVIRONMENT_HEADER = "x-tink-cluster-environment";
+
+    @Inject
+    public ProviderServiceFactoryProvider(@ProviderConfiguration EndpointConfiguration providerConfiguration,
+                                          CuratorFramework coordinationClient,
+                                          @Named("clusterName") String clusterName,
+                                          @Named("clusterEnvironment") String clusterEnvironment){
+        this.endpointConfiguration = providerConfiguration;
+        this.clusterName = clusterName;
+        this.clusterEnvironment = clusterEnvironment;
+    }
+
+    @Override
+    public ProviderServiceFactory get() {
+        if (endpointConfiguration == null) {
+            throw new IllegalStateException("no endpoint found, did we configure correctly?");
+        }
+
+        return new ClientProviderServiceFactory(
+                endpointConfiguration.getPinnedCertificates(),
+                endpointConfiguration.getUrl(),
+                new ClusterIdFilter(clusterName, clusterEnvironment),
+                endpointConfiguration.getAccessToken()
+        );
+    }
+
+    public class ClusterIdFilter extends ClientFilter {
+        private final String clusterName;
+        private final String clusterEnvironment;
+
+        public ClusterIdFilter(String clusterName, String clusterEnvironment) {
+            this.clusterName = clusterName;
+            this.clusterEnvironment = clusterEnvironment;
+        }
+
+        @Override
+        public ClientResponse handle(ClientRequest cr) throws ClientHandlerException {
+            cr.getHeaders().add(CLUSTER_NAME_HEADER, clusterName);
+            cr.getHeaders().add(CLUSTER_ENVIRONMENT_HEADER, clusterEnvironment);
+            return getNext().handle(cr);
+        }
+    }
+}
