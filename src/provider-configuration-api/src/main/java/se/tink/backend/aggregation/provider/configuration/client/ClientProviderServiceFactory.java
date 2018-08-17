01f15e9c7354215c0b5921f614a07f840de0cf86
evchen <minjia.chen@tink.se>
2018-08-17 16:08:20 +0200

    fix(client in aggregation) change to intercontainer client

--- src/provider-configuration-api/src/main/java/se/tink/backend/aggregation/provider/configuration/client/ClientProviderServiceFactory.java
+++ src/provider-configuration-api/src/main/java/se/tink/backend/aggregation/provider/configuration/client/ClientProviderServiceFactory.java
@@ -6,6 +6,9 @@ import com.google.common.base.Strings;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.ClientFilter;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 import se.tink.backend.aggregation.provider.configuration.api.MonitoringService;
 import se.tink.backend.aggregation.provider.configuration.api.ProviderService;
@@ -16,19 +19,18 @@ import se.tink.libraries.jersey.utils.JerseyUtils;
 public class ClientProviderServiceFactory implements ProviderServiceFactory{
     private ServiceClassBuilder builder;
 
-    public ClientProviderServiceFactory(byte[] pinnedCertificates, String host, ClientFilter clientFilter, String accessToken){
+    public ClientProviderServiceFactory(List<String> pinnedCertificates, String host, ClientFilter clientFilter, String accessToken){
         this(new BasicWebServiceClassBuilder(createResource(pinnedCertificates, host, clientFilter, accessToken)));
     }
 
-    private static WebResource createResource(byte[] pinnedCertificates,
+    private static WebResource createResource(List<String> pinnedCertificates,
             String host, ClientFilter clientFilter, String accessToken){
 
         Preconditions.checkState(!Strings.isNullOrEmpty(host),
                 "Host cannot be empty");
         Preconditions.checkState(!Objects.isNull(clientFilter),
                 "The provider service requires");
-
-        Client client = JerseyUtils.getClusterClient(pinnedCertificates, "", false);
+        Client client = JerseyUtils.getClient(pinnedCertificates);
         client.addFilter(clientFilter);
         JerseyUtils.registerAPIAccessToken(client, accessToken);
         return client.resource(host);
