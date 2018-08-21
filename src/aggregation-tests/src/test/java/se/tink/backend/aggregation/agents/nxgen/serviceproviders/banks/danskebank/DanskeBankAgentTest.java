26fb802d8bdbdf8f472a85eafe19ce58718e4536
Gustaf Svanerud <gustaf.svanerud@tink.se>
2018-08-21 16:02:14 +0200

    tes(danskebank) Added test for DanskeBank provider
    Added a test for DanskeBank that can be configured with serviceprovider
    to test DanskeBank in any market.

---
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/danskebank/DanskeBankAgentTest.java
@@ -0,0 +1,37 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.rpc.Field;
+
+@Ignore
+public class DanskeBankAgentTest {
+
+    private final String FI_MARKET = "fi";
+    private final String FI_PROVIDER_NAME = "fi-danskebank-codecard";
+
+    private final String DK_MARKET = "dk";
+    private final String DK_PROVIDER_NAME = "dk-danskebank-servicecode";
+
+    private final String TEST_USERNAME = "NNNN";
+    private final String TEST_PASSWORD = "NNNN";
+
+    private AgentIntegrationTest.Builder builder;
+
+    @Before
+    public void setup(){
+        builder = new AgentIntegrationTest.Builder(DK_MARKET, DK_PROVIDER_NAME)
+                .addCredentialField(Field.Key.USERNAME, TEST_USERNAME)
+                .addCredentialField(Field.Key.PASSWORD, TEST_PASSWORD)
+                .loadCredentialsBefore(true)
+                .saveCredentialsAfter(true);
+    }
+
+    @Test
+    public void testRefresh() throws Exception{
+
+        builder.build().testRefresh();
+    }
+}
