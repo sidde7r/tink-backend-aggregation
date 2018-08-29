01430f7e3c55c8372256ef3fad573676600b15b1
Sebastian Olsson <sebastian.olsson@tink.se>
2018-08-29 10:05:10 +0200

    fea(bawagpsk/test) Added support for supplying credentials as CLI params

--- src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/at/banks/easybank/bawagpsk/BawagPskAgentTest.java
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/at/banks/easybank/bawagpsk/BawagPskAgentTest.java
@@ -1,20 +1,36 @@
 package se.tink.backend.aggregation.agents.nxgen.at.banks.easybank.bawagpsk;
 
 import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
 import se.tink.backend.aggregation.rpc.Field;
 
 @Ignore
 public class BawagPskAgentTest {
-    private static final AgentIntegrationTest.Builder builder = new AgentIntegrationTest.Builder("at", "at-bawagpsk-password")
-            .addCredentialField(Field.Key.PASSWORD, TestConfig.PASSWORD)
+    private final ArgumentHelper helper = new ArgumentHelper("tink.username", "tink.password");
+
+    private static final AgentIntegrationTest.Builder builder = new AgentIntegrationTest.Builder("at",
+            "at-bawagpsk-password")
             .loadCredentialsBefore(false)
             .saveCredentialsAfter(false);
 
+    @Before
+    public void before() {
+        helper.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentHelper.afterClass();
+    }
+
     @Test
     public void testLoginAndRefresh() throws Exception {
-        builder.addCredentialField(Field.Key.USERNAME, TestConfig.USERNAME)
+        builder.addCredentialField(Field.Key.USERNAME, helper.get("tink.username"))
+                .addCredentialField(Field.Key.PASSWORD, helper.get("tink.password"))
                 .build()
                 .testRefresh();
     }
