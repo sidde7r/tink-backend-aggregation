0a0b5a994312235489feb6177dd2e357962265da
ovesan <ove.sandfeldt@tink.se>
2018-09-10 11:00:15 +0200

    fix(SparbankenSyd) Fetch credit card and transactions for SparbankenSyd
    Sparbanken syd has its own implementation of fetching credit cards. It
    will still use the service providers transaction fetching for credit cards.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/fetcher/SdcCreditCardFetcher.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/fetcher/SdcCreditCardFetcher.java
@@ -3,7 +3,6 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.fetc
 import com.google.common.collect.Lists;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
@@ -16,7 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcConfiguration;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcSessionStorage;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.authenticator.entities.SdcServiceConfigurationEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.authenticator.entities.SessionStorageAgreement;
@@ -36,7 +34,6 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.paginat
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginator;
 import se.tink.backend.aggregation.nxgen.core.account.CreditCardAccount;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
-import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class SdcCreditCardFetcher extends SdcAgreementFetcher implements AccountFetcher<CreditCardAccount>,
         TransactionDatePaginator<CreditCardAccount> {
@@ -45,8 +42,8 @@ public class SdcCreditCardFetcher extends SdcAgreementFetcher implements Account
     private static final int ONE_WEEK_AGO_IN_DAYS = -7;
 
     private final SdcTransactionParser transactionParser;
-    private final SdcConfiguration agentConfiguration;
-    private final Map<String, SdcAccountKey> creditCardAccounts = new HashMap<>();
+    protected final SdcConfiguration agentConfiguration;
+    protected final Map<String, SdcAccountKey> creditCardAccounts = new HashMap<>();
 
     public SdcCreditCardFetcher(SdcApiClient bankClient, SdcSessionStorage sessionStorage,
             SdcTransactionParser transactionParser, SdcConfiguration agentConfiguration) {
@@ -65,20 +62,19 @@ public class SdcCreditCardFetcher extends SdcAgreementFetcher implements Account
 
             serviceConfigurationEntity.ifPresent(configurationEntity -> {
 
-                // different provider service config use different endpoints
+                // different provider service config use different endpoints:
+                // SparbankenSyd accounts (of type KKPD) handled by SdcSeCreditcardFetcher
+                // Eika(NO) provider list (config isCreditcard)
+                // Storebrand(NO) and DK credit card list (config isBlockcard)
                 if (configurationEntity.isCreditCard()) {
-                    log.info("Fetch credit cards using: fetchCreditCardProviderAccountList");
                     fetchCreditCardProviderAccountList(creditCards, agreement);
-                }
-
-                if (SdcConstants.BANK_CODE_SPARBANKEN_SYD.equals(agentConfiguration.getBankCode())) {
-                    getCreditCardAccountsForSparbankenSyd(creditCards, agreement);
+                    if (creditCards.size() > 0) {
+                        log.info("Fetch credit cards using: fetchCreditCardProviderAccountList: " + creditCards.size());
+                    }
                 } else if (configurationEntity.isBlockCard()) {
-                    try {
-                        log.info("Fetch credit cards using: fetchCreditCardList");
-                        fetchCreditCardList(creditCards, agreement);
-                    } catch (Exception e) {
-                        log.info("Failed to fetch credit card for user", e);
+                    fetchCreditCardList(creditCards, agreement);
+                    if (creditCards.size() > 0) {
+                        log.info("Fetch credit cards using: fetchCreditCardList: " + creditCards.size());
                     }
                 }
             });
@@ -111,21 +107,21 @@ public class SdcCreditCardFetcher extends SdcAgreementFetcher implements Account
                 .setTransactionsFrom(formatDate(fromDate))
                 .setTransactionsTo(formatDate(toDate));
 
-        if (SdcConstants.BANK_CODE_SPARBANKEN_SYD.equals(agentConfiguration.getBankCode())) {
-            transactions = getSparbankenSydTransactions(account,
-                    searchTransactionsRequest);
+        SearchTransactionsResponse creditCardTransactions = this.bankClient
+                .searchCreditCardTransactions(searchTransactionsRequest);
 
-        } else {
-            SearchTransactionsResponse creditCardTransactions = this.bankClient
-                    .searchCreditCardTransactions(searchTransactionsRequest);
+        transactions = creditCardTransactions.getTinkCreditCardTransactions(account,
+                this.transactionParser);
 
-            transactions = creditCardTransactions.getTinkCreditCardTransactions(account,
-                    this.transactionParser);
+        // keep an eye on number of transactions returned for credit cards for Sdc clients
+        if (transactions != null && transactions.size() > 0) {
+            log.debug(String.format("Num credit cards transaction: %d", transactions.size()));
         }
 
         return PaginatorResponseImpl.create(transactions);
     }
 
+    // isCreditcard
     private void fetchCreditCardProviderAccountList(List<CreditCardAccount> creditCards,
             SessionStorageAgreement agreement) {
         FilterAccountsResponse creditCardProviderAccounts = this.bankClient.listCreditCardProviderAccounts();
@@ -147,6 +143,7 @@ public class SdcCreditCardFetcher extends SdcAgreementFetcher implements Account
         }
     }
 
+    // isBlockcard
     private void fetchCreditCardList(List<CreditCardAccount> creditCards, SessionStorageAgreement agreement) {
         FilterAccountsResponse accounts = retrieveAccounts();
 
@@ -174,7 +171,7 @@ public class SdcCreditCardFetcher extends SdcAgreementFetcher implements Account
         }
     }
 
-    private FilterAccountsResponse retrieveAccounts() {
+    protected FilterAccountsResponse retrieveAccounts() {
         FilterAccountsRequest request = new FilterAccountsRequest()
                 .setOnlyFavorites(false)
                 .setIncludeCreditAccounts(true)
@@ -195,88 +192,5 @@ public class SdcCreditCardFetcher extends SdcAgreementFetcher implements Account
     private String formatDate(Date aDate) {
         LocalDate date = new java.sql.Date(aDate.getTime()).toLocalDate();
         return date.format(DateTimeFormatter.ISO_DATE);
-    }
-
-
-    ////////////////// code to see what works for sparbanken syd when fetching credit cards. Earlier the used to work
-    ////////////////// using "BlockCard"
-
-    // sweden used to use BlockCard style, but it doesn't work This is a test and code cleanup will
-    // be done when the correct way defined
-    private void getCreditCardAccountsForSparbankenSyd(List<CreditCardAccount> creditCards,
-            SessionStorageAgreement agreement) {
-        List<CreditCardAccount> creditCardAccounts = new ArrayList<>();
-
-        try {
-            log.info("Fetch credit cards using: fetchCreditCardAccounts");
-            fetchCreditCardAccounts(creditCardAccounts, agreement);
-            if (creditCardAccounts.size() > 0) {
-                log.info("Sparbanken syd - Fetch credit cards using: fetchCreditCardAccounts: "
-                        + creditCardAccounts.size());
-            }
-        } catch (Exception e) {
-            log.info("Failed to fetch credit card for user", e);
-        }
-
-        creditCards.addAll(creditCardAccounts);
-    }
-
-    // Sparbanken syd uses standard account as credit card account
-    private void fetchCreditCardAccounts(List<CreditCardAccount> creditCards, SessionStorageAgreement agreement) {
-        FilterAccountsResponse accounts = retrieveAccounts();
-
-        // Convert accounts of type credit card to credit card
-        accounts.stream()
-                .filter(SdcAccount::isCreditCardAccount)
-                .forEach(account -> {
-                    CreditCardAccount creditCardAccount = account.toTinkCreditCardAccount(this.agentConfiguration);
-
-                    // return value
-                    creditCards.add(creditCardAccount);
-
-                    String creditCardAccountId = creditCardAccount.getBankIdentifier();
-                    // keep BankIdentifier to be able to fetch transactions
-                    this.creditCardAccounts.put(creditCardAccountId, account.getEntityKey());
-                    // add credit card bankId to agreement for fetching transactions later
-                    agreement.addAccountBankId(creditCardAccountId);
-                });
-    }
-
-    // credit cards are not working for sparbanken syd, Try fetch transactions in different ways and see what works
-    private Collection<? extends Transaction> getSparbankenSydTransactions(CreditCardAccount account,
-            SearchTransactionsRequest searchTransactionsRequest) {
-        Collection<? extends Transaction> transactions = Collections.emptyList();
-
-        try {
-            SearchTransactionsResponse creditCardTransactions = this.bankClient
-                    .searchCreditCardTransactions(searchTransactionsRequest);
-
-            if (creditCardTransactions != null &&
-                    creditCardTransactions.getTransactions() != null &&
-                    creditCardTransactions.getTransactions().size() > 0) {
-                log.info("SparbankenSyd - credit card transactions " + SerializationUtils.serializeToString(creditCardTransactions));
-                transactions = creditCardTransactions.getTinkCreditCardTransactions(account, this.transactionParser);
-            }
-        } catch (Exception e) {
-            log.info("SparbankenSyd - Failed to fetch credit card transactions", e);
-        }
-
-        try {
-            SearchTransactionsResponse creditCardTransactions = this.bankClient
-                    .searchTransactions(searchTransactionsRequest);
-
-            if (creditCardTransactions != null &&
-                    creditCardTransactions.getTransactions() != null &&
-                    creditCardTransactions.getTransactions().size() > 0) {
-                log.info("SparbankenSyd - account transactions for credit card " + SerializationUtils.serializeToString(creditCardTransactions));
-                if (transactions.size() == 0) {
-                    transactions = creditCardTransactions.getTinkCreditCardTransactions(account, this.transactionParser);
-                }
-            }
-        } catch (Exception e) {
-            log.info("SparbankenSyd - Failed to fetch account transactions for credit card", e);
-        }
-
-        return transactions;
     }
 }
