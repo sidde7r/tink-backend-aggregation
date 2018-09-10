056ee98fe9c64bbc2e3003f3aa930162d15e6390
Sebastian Olsson <sebastian.olsson@tink.se>
2018-09-10 15:25:51 +0200

    fea(hvb) Added support for guessing the account type from the title

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/hvb/fetcher/rpc/AccountResponse.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/hvb/fetcher/rpc/AccountResponse.java
@@ -38,12 +38,10 @@ public final class AccountResponse {
      * @throws NullPointerException if the account type was not found
      */
     private static boolean validateAndLogAccountType(final AccountEntity accountEntity) {
-        final String accountTypeString = Preconditions.checkNotNull(accountEntity.getType()).trim();
-        final Optional<AccountTypes> accountType = stringToAccountType(accountTypeString);
+        final Optional<AccountTypes> accountType = entityToAccountType(accountEntity);
         if (!accountType.isPresent()) {
-            logger.warn("{} - Received unknown account type: {} for account entity {}",
-                    HVBConstants.LogTags.HVB_UNKNOWN_ACCOUNT_TYPE.toTag(),
-                    accountTypeString,
+            logger.error("{} - Could not figure out account type for account entity {}",
+                    HVBConstants.LogTags.HVB_UNRECOGNIZED_ACCOUNT_TYPE.toTag(),
                     SerializationUtils.serializeToString(accountEntity)
             );
             return false;
@@ -52,8 +50,34 @@ public final class AccountResponse {
     }
 
     private static AccountTypes getAccountType(final AccountEntity accountEntity) {
+        return entityToAccountType(accountEntity).orElseThrow(IllegalStateException::new);
+    }
+
+    private static Optional<AccountTypes> entityToAccountType(final AccountEntity accountEntity) {
         final String accountTypeString = Preconditions.checkNotNull(accountEntity.getType()).trim();
-        return stringToAccountType(accountTypeString).orElseThrow(IllegalStateException::new);
+        final Optional<AccountTypes> type = stringToAccountType(accountTypeString);
+        if (!type.isPresent()) {
+            logger.warn("{} - Received unknown account type: {} for account entity {}",
+                    HVBConstants.LogTags.HVB_UNKNOWN_ACCOUNT_TYPE.toTag(),
+                    accountTypeString,
+                    SerializationUtils.serializeToString(accountEntity)
+            );
+            final String title = Optional.ofNullable(accountEntity.getTitle())
+                    .map(String::trim)
+                    .orElse("");
+            return titleToAccountType(title);
+        }
+        return type;
+    }
+
+    private static Optional<AccountTypes> titleToAccountType(@Nonnull final String accountTitle) {
+        final String upperCaseTitle = accountTitle.toUpperCase();
+        if (HVBConstants.CHECKING_ACCOUNT_TITLE_SUBSTRINGS.stream().anyMatch(upperCaseTitle::contains)) {
+            return Optional.of(AccountTypes.CHECKING);
+        } else if (HVBConstants.SAVINGS_ACCOUNT_TITLE_SUBSTRINGS.stream().anyMatch(upperCaseTitle::contains)) {
+            return Optional.of(AccountTypes.SAVINGS);
+        }
+        return Optional.empty();
     }
 
     private static Optional<AccountTypes> stringToAccountType(@Nonnull final String accountType) {
@@ -72,8 +96,7 @@ public final class AccountResponse {
         return string == null ? null : string.trim();
     }
 
-    private static TransactionalAccount.Builder<?, ?> toTransactionalAccount(
-            final AccountEntity accountEntity) {
+    private static TransactionalAccount.Builder<?, ?> toTransactionalAccount(final AccountEntity accountEntity) {
         final String currency = trimNullable(accountEntity.getCurrency());
         final Double balance = accountEntity.getCurrentBalance();
         final String bic = Preconditions.checkNotNull(accountEntity.getBic());
