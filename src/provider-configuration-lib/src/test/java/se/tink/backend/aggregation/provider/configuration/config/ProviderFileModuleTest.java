e735c123c8af1a6e9d2f697e51933553fdcb33a0
evchen <minjia.chen@tink.se>
2018-09-12 16:22:00 +0200

    fea(FileModuleTest) adding test for loading files

--- src/provider-configuration-lib/src/test/java/se/tink/backend/aggregation/provider/configuration/config/ProviderFileModuleTest.java
+++ src/provider-configuration-lib/src/test/java/se/tink/backend/aggregation/provider/configuration/config/ProviderFileModuleTest.java
@@ -1,18 +1,41 @@
 package se.tink.backend.aggregation.provider.configuration.config;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.core.ProviderConfiguration;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ProviderFileModuleTest {
+    ProviderFileModule module;
+
+    @Before
+    public void createModule(){
+        module = new ProviderFileModule();
+    }
 
     @Test
-    public void loadProviderConfigurationFromJsonTest() throws IOException{
-        Map<String, ProviderConfiguration> map = new ProviderFileModule().loadProviderConfigurationFromJson();
+    public void loadProviderConfigurationFromJsonTest() throws Exception{
+        Map<String, ProviderConfiguration> map = module.loadProviderConfigurationFromJson();
         Assert.assertNotNull(map);
         Assert.assertFalse(map.isEmpty());
+    }
+
+    @Test
+    public void testLoaderLoadsFileSuccess() throws IOException{
+        module.loadClusterSpecificProviderConfigurationFromJson();
+    }
+
+    @Test
+    public void testLoadEnabledProviders() throws IOException{
+        Map<String, List<String>> clusterProvider = module.loadClusterEnabledProviderNamesFromJson();
+        List<List<String>> emptyProviders = clusterProvider.values().stream()
+                .filter(List::isEmpty)
+                .collect(Collectors.toList());
+        Assert.assertTrue(emptyProviders.isEmpty());
     }
 }
