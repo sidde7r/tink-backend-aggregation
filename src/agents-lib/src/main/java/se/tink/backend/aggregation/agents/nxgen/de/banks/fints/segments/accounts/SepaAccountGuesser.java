3b0407b470a908882ae9d812d499c8477070ece7
Alexander Jamm <alexander.jamm@tink.se>
2018-09-12 14:06:50 +0200

    ref(FinTs) Added more known account types to SepaAccountGuesser

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/segments/accounts/SepaAccountGuesser.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/segments/accounts/SepaAccountGuesser.java
@@ -12,18 +12,28 @@ public class SepaAccountGuesser {
             accountName = accountName.toLowerCase();
 
             for (String savingsAccountName : FinTsConstants.SepaAccountIdentifiers.KNOWN_SAVINGS_ACCOUNT_NAMES) {
-                if (accountName.equals(savingsAccountName)) {
+                if (accountName.equalsIgnoreCase(savingsAccountName)) {
                     return FinTsConstants.AccountType.SAVINGS_ACCOUNT_CURSOR;
                 }
             }
 
             for (String investmentAccountName : FinTsConstants.SepaAccountIdentifiers.KNOWN_INVESTMENT_ACCOUNT_NAMES) {
-                if (accountName.equals(investmentAccountName)) {
+                if (accountName.equalsIgnoreCase(investmentAccountName)) {
                     return FinTsConstants.AccountType.FUND_DEPOSIT_ACCOUNT_CURSOR;
                 }
             }
 
-            LOGGER.info("{} Account type is missing, product name: {}", FinTsConstants.LogTags.PRODUCTNAME_FOR_MISSING_ACCOUNT_TYPE.toString(), accountName);
+            for (String creditAccountName : FinTsConstants.SepaAccountIdentifiers.KNOWN_CREDIT_ACCOUNT_NAMES) {
+                if (accountName.equalsIgnoreCase(creditAccountName)) {
+                    return FinTsConstants.AccountType.CREDIT_CARD_CURSOR;
+                }
+            }
+
+            for (String creditAccountName : FinTsConstants.SepaAccountIdentifiers.KNOWN_CHECKING_ACCOUNT_NAMES) {
+                if (accountName.equalsIgnoreCase(creditAccountName)) {
+                    return FinTsConstants.AccountType.CHECKING_ACCOUNT_CURSOR;
+                }
+            }
 
             for (String savingsToken : FinTsConstants.SepaAccountIdentifiers.ACCOUNT_TYPE_SAVINGS_TOKENS) {
                 if (accountName.contains(savingsToken)) {
@@ -31,13 +41,21 @@ public class SepaAccountGuesser {
                 }
             }
 
-
             for (String investmentToken : FinTsConstants.SepaAccountIdentifiers.ACCOUNT_TYPE_INVESTMENT_TOKENS) {
                 if (accountName.contains(investmentToken)) {
                     return FinTsConstants.AccountType.FUND_DEPOSIT_ACCOUNT_CURSOR;
                 }
             }
+
+            for (String creditToken : FinTsConstants.SepaAccountIdentifiers.ACCOUNT_TYPE_CREDIT_TOKENS) {
+                if (accountName.contains(creditToken)) {
+                    return FinTsConstants.AccountType.CREDIT_CARD_CURSOR;
+                }
+            }
         }
+
+        LOGGER.info("{} Account type is missing, product name: {}",
+                FinTsConstants.LogTags.PRODUCTNAME_FOR_MISSING_ACCOUNT_TYPE.toString(), accountName);
 
         return FinTsConstants.AccountType.CHECKING_ACCOUNT_CURSOR;
     }
