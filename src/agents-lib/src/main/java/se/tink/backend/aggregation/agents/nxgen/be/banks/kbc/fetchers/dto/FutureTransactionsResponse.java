71d4500bf5e1b03f116f07eee358886372aaf22b
Xiaoqiang Chen <xiaoqiang.chen@tink.se>
2018-09-14 15:41:05 +0200

    fix(kbc-upcoming) filter on executiondate == null cases

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/fetchers/dto/FutureTransactionsResponse.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/fetchers/dto/FutureTransactionsResponse.java
@@ -17,7 +17,11 @@ public class FutureTransactionsResponse extends HeaderResponse {
     private TypeValuePair repositioningKey;
 
     public Collection<UpcomingTransaction> getUpcomingTransactions() {
+
         return Optional.ofNullable(transactions).orElse(Collections.emptyList()).stream()
+                // When the user has no enough fund, the bank will reserve a transaction to reverse the transaction in
+                // the future, this transaction is without execution date
+                .filter(futureTransactionDto -> futureTransactionDto.getExecutionDate() != null)
                 .map(FutureTransactionDto::toUpcomingTransaction)
                 .collect(Collectors.toList());
     }
