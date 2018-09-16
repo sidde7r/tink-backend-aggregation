7ffbc733f995e328a111c8f3e00523eccbab7b9c
Babis Kiosidis <babis.kiosidis@tink.se>
2018-09-16 13:59:39 +0200

    fea(provider, http): convert core object to http impl

--- src/provider-configuration-lib/src/main/java/se/tink/backend/aggregation/provider/configuration/http/converter/HttpProviderConfigurationConverter.java
+++ src/provider-configuration-lib/src/main/java/se/tink/backend/aggregation/provider/configuration/http/converter/HttpProviderConfigurationConverter.java
@@ -8,8 +8,29 @@ import java.util.stream.Collectors;
 
 public class HttpProviderConfigurationConverter {
     public static ProviderConfigurationDTO translate(ProviderConfiguration providerConfiguration) {
-        // TODO: implementation
-        return null;
+        ProviderConfigurationDTO dto = new ProviderConfigurationDTO();
+        dto.setCapabilitiesSerialized(providerConfiguration.getCapabilitiesSerialized());
+        dto.setClassName(providerConfiguration.getClassName());
+        dto.setCredentialsType(providerConfiguration.getCredentialsType());
+        dto.setCurrency(providerConfiguration.getCurrency());
+        dto.setDisplayName(providerConfiguration.getDisplayName());
+        dto.setDisplayDescription(providerConfiguration.getDisplayDescription());
+        dto.setFields(providerConfiguration.getFields());
+        dto.setGroupDisplayName(providerConfiguration.getGroupDisplayName());
+        dto.setMarket(providerConfiguration.getMarket());
+        dto.setMultiFactor(providerConfiguration.isMultiFactor());
+        dto.setName(providerConfiguration.getName());
+        dto.setPasswordHelpText(providerConfiguration.getPasswordHelpText());
+        dto.setPayload(providerConfiguration.getPayload());
+        dto.setPopular(providerConfiguration.isPopular());
+        dto.setRefreshFrequency(providerConfiguration.getRefreshFrequency());
+        dto.setRefreshFrequencyFactor(providerConfiguration.getRefreshFrequencyFactor());
+        dto.setStatus(providerConfiguration.getStatus());
+        dto.setTransactional(providerConfiguration.isTransactional());
+        dto.setType(providerConfiguration.getType());
+        dto.setRefreshSchedule(providerConfiguration.getRefreshSchedule().orElse(null));
+        dto.setStatus(providerConfiguration.getStatus());
+        return dto;
     }
 
     public static List<ProviderConfigurationDTO> translate(List<ProviderConfiguration> providerConfigurationList) {
