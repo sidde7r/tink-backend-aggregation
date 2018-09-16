dfc2552112d8c9e743952740b4cc2c7d342406eb
Babis Kiosidis <babis.kiosidis@tink.se>
2018-09-16 13:51:46 +0200

    ref(provider, repo): removed unused code

--- src/provider-configuration-lib/src/main/java/se/tink/backend/aggregation/provider/configuration/repositories/mysql/ProviderConfigurationRepository.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.provider.configuration.repositories.mysql;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.tink.backend.aggregation.provider.configuration.repositories.ProviderConfiguration;
-
-public interface ProviderConfigurationRepository extends JpaRepository<ProviderConfiguration, String>,
-        ProviderConfigurationRepositoryCustom {
-}
