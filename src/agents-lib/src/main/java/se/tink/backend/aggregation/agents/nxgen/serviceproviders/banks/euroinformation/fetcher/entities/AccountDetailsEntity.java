42947d9de7f502a4fb734be818ff452bc7fbcc9d
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-09-17 13:41:34 +0200

    fea(euroInf) adding @JsonObject for logging all fields

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/euroinformation/fetcher/entities/AccountDetailsEntity.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/euroinformation/fetcher/entities/AccountDetailsEntity.java
@@ -8,9 +8,11 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import org.assertj.core.util.Strings;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.euroinformation.utils.EuroInformationUtils;
+import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.account.Account;
 import se.tink.backend.core.Amount;
 
+@JsonObject
 @XmlRootElement(name = "compte")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class AccountDetailsEntity {
