9f2485db7625ac53c3a19c0dd0b2611a4a476c3d
Babis Kiosidis <babis.kiosidis@tink.se>
2018-09-17 09:51:13 +0200

    ref(provider, cli): commented commands that the implementation isn't certain

--- src/provider-configuration-service/src/main/java/se/tink/backend/aggregation/provider/configuration/cli/ChangeProviderRefreshFrequencyFactorCommand.java
+++ src/provider-configuration-service/src/main/java/se/tink/backend/aggregation/provider/configuration/cli/ChangeProviderRefreshFrequencyFactorCommand.java
@@ -1,77 +1,77 @@
 package se.tink.backend.aggregation.provider.configuration.cli;
-
-import com.google.common.base.Preconditions;
-import com.google.inject.Injector;
-import io.dropwizard.setup.Bootstrap;
-import net.sourceforge.argparse4j.inf.Namespace;
-import se.tink.backend.aggregation.provider.configuration.repositories.mysql.ProviderConfigurationRepository;
-import se.tink.backend.aggregation.provider.configuration.repositories.ProviderConfiguration;
-import se.tink.backend.common.config.ServiceConfiguration;
-
-import java.util.List;
-import java.util.Optional;
-
-public class ChangeProviderRefreshFrequencyFactorCommand extends
-        ProviderConfigurationCommand<ServiceConfiguration> {
-    private static final String REFRESH_FREQUENCY_INTERVAL_ERROR_MESSAGE = "'refreshFrequencyFactor' must be 0 <= x <= 1.";
-
-    public ChangeProviderRefreshFrequencyFactorCommand() {
-        super("change-refresh-frequency-factor",
-                "Change refresh frequency factor for one, or all providers.");
-    }
-
-    @Override
-    protected void run(Bootstrap<ServiceConfiguration> bootstrap, Namespace namespace,
-            ServiceConfiguration configuration, Injector injector) throws Exception {
-
-        // Input handling
-
-        final Optional<String> providerName = Optional.ofNullable(System.getProperty("providerName"));
-        final Double refreshFrequency = getRefreshFrequencyFromSystemProperty();
-
-        // Print parameters.
-
-        System.out.println("'providerName' to search for is: " + providerName);
-        System.out.println("New 'refreshFrequencyFactor' is: " + refreshFrequency);
-
-        // Make the actual change.
-
-        ProviderConfigurationRepository providerConfigurationRepository = injector.getInstance(
-                ProviderConfigurationRepository.class);
-
-        if (providerName.isPresent()) {
-            ProviderConfiguration providerConfiguration = providerConfigurationRepository.findByName(providerName.get());
-            Preconditions.checkNotNull(providerConfiguration,
-                    String.format("Could not find provider with the given name: %s", providerName.get()));
-
-            providerConfiguration.setRefreshFrequency(refreshFrequency);
-
-            providerConfigurationRepository.save(providerConfiguration);
-
-            System.out.println("Provider modified.");
-        } else {
-            List<ProviderConfiguration> providers = providerConfigurationRepository.findAll();
-
-            for (ProviderConfiguration provider : providers) {
-                provider.setRefreshFrequencyFactor(refreshFrequency);
-            }
-
-            providerConfigurationRepository.save(providers);
-
-            System.out.println("Providers modified.");
-        }
-
-    }
-
-    private Double getRefreshFrequencyFromSystemProperty() {
-        final Double refreshFrequency;
-        String refreshFrequencyString = System.getProperty("refreshFrequency");
-        refreshFrequency = Double.parseDouble(Preconditions.checkNotNull(refreshFrequencyString,
-                "'refreshFrequency' must be given as Java system property."));
-
-        Preconditions.checkArgument(refreshFrequency >= 0, REFRESH_FREQUENCY_INTERVAL_ERROR_MESSAGE);
-        Preconditions.checkArgument(refreshFrequency <= 1, REFRESH_FREQUENCY_INTERVAL_ERROR_MESSAGE);
-        return refreshFrequency;
-    }
-
-}
+//
+//import com.google.common.base.Preconditions;
+//import com.google.inject.Injector;
+//import io.dropwizard.setup.Bootstrap;
+//import net.sourceforge.argparse4j.inf.Namespace;
+//import se.tink.backend.aggregation.provider.configuration.repositories.mysql.ProviderConfigurationRepository;
+//import se.tink.backend.aggregation.provider.configuration.repositories.ProviderConfiguration;
+//import se.tink.backend.common.config.ServiceConfiguration;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public class ChangeProviderRefreshFrequencyFactorCommand extends
+//        ProviderConfigurationCommand<ServiceConfiguration> {
+//    private static final String REFRESH_FREQUENCY_INTERVAL_ERROR_MESSAGE = "'refreshFrequencyFactor' must be 0 <= x <= 1.";
+//
+//    public ChangeProviderRefreshFrequencyFactorCommand() {
+//        super("change-refresh-frequency-factor",
+//                "Change refresh frequency factor for one, or all providers.");
+//    }
+//
+//    @Override
+//    protected void run(Bootstrap<ServiceConfiguration> bootstrap, Namespace namespace,
+//            ServiceConfiguration configuration, Injector injector) throws Exception {
+//
+//        // Input handling
+//
+//        final Optional<String> providerName = Optional.ofNullable(System.getProperty("providerName"));
+//        final Double refreshFrequency = getRefreshFrequencyFromSystemProperty();
+//
+//        // Print parameters.
+//
+//        System.out.println("'providerName' to search for is: " + providerName);
+//        System.out.println("New 'refreshFrequencyFactor' is: " + refreshFrequency);
+//
+//        // Make the actual change.
+//
+//        ProviderConfigurationRepository providerConfigurationRepository = injector.getInstance(
+//                ProviderConfigurationRepository.class);
+//
+//        if (providerName.isPresent()) {
+//            ProviderConfiguration providerConfiguration = providerConfigurationRepository.findByName(providerName.get());
+//            Preconditions.checkNotNull(providerConfiguration,
+//                    String.format("Could not find provider with the given name: %s", providerName.get()));
+//
+//            providerConfiguration.setRefreshFrequency(refreshFrequency);
+//
+//            providerConfigurationRepository.save(providerConfiguration);
+//
+//            System.out.println("Provider modified.");
+//        } else {
+//            List<ProviderConfiguration> providers = providerConfigurationRepository.findAll();
+//
+//            for (ProviderConfiguration provider : providers) {
+//                provider.setRefreshFrequencyFactor(refreshFrequency);
+//            }
+//
+//            providerConfigurationRepository.save(providers);
+//
+//            System.out.println("Providers modified.");
+//        }
+//
+//    }
+//
+//    private Double getRefreshFrequencyFromSystemProperty() {
+//        final Double refreshFrequency;
+//        String refreshFrequencyString = System.getProperty("refreshFrequency");
+//        refreshFrequency = Double.parseDouble(Preconditions.checkNotNull(refreshFrequencyString,
+//                "'refreshFrequency' must be given as Java system property."));
+//
+//        Preconditions.checkArgument(refreshFrequency >= 0, REFRESH_FREQUENCY_INTERVAL_ERROR_MESSAGE);
+//        Preconditions.checkArgument(refreshFrequency <= 1, REFRESH_FREQUENCY_INTERVAL_ERROR_MESSAGE);
+//        return refreshFrequency;
+//    }
+//
+//}
--- src/provider-configuration-service/src/main/java/se/tink/backend/aggregation/provider/configuration/cli/SeedProvidersForMarketCommand.java
+++ src/provider-configuration-service/src/main/java/se/tink/backend/aggregation/provider/configuration/cli/SeedProvidersForMarketCommand.java
@@ -1,162 +1,164 @@
 package se.tink.backend.aggregation.provider.configuration.cli;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Objects;
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
-import com.google.inject.Injector;
-import io.dropwizard.setup.Bootstrap;
-import net.sourceforge.argparse4j.inf.Namespace;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import se.tink.backend.aggregation.provider.configuration.storage.module.clusterprovider.ProviderConfigModel;
-import se.tink.backend.aggregation.provider.configuration.repositories.mysql.ProviderConfigurationRepository;
-import se.tink.backend.aggregation.provider.configuration.repositories.ProviderConfiguration;
-import se.tink.backend.core.ProviderStatuses;
-import se.tink.backend.common.config.ServiceConfiguration;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.google.common.base.Objects;
+//import com.google.common.base.Preconditions;
+//import com.google.common.base.Strings;
+//import com.google.inject.Injector;
+//import io.dropwizard.setup.Bootstrap;
+//import net.sourceforge.argparse4j.inf.Namespace;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import se.tink.backend.aggregation.provider.configuration.storage.models.ProviderConfiguration;
+//import se.tink.backend.aggregation.provider.configuration.storage.module.clusterprovider.ProviderConfigModel;
+//import se.tink.backend.common.repository.mysql.aggregation.providerconfiguration.ProviderConfigurationRepository;
+//import se.tink.backend.core.ProviderStatuses;
+//import se.tink.backend.common.config.ServiceConfiguration;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.List;
+//import java.util.Optional;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
 
 
+// FIXME: implementation unclear for Provider Service
 /**
  * Try this locally.
  * bazel build :all
  * java -Dmarket=se -jar bazel-bin/provider-configuration_deploy.jar seed-providers-for-market etc/development-provider-configuration-server.yml
  */
-public class SeedProvidersForMarketCommand extends ProviderConfigurationCommand<ServiceConfiguration> {
-    private static final Logger log = LoggerFactory.getLogger(SeedProvidersForMarketCommand.class);
-    private static ObjectMapper mapper = new ObjectMapper();
-    private ProviderConfigurationRepository providerRepository;
-    private final double DEFAULT_INITIAL_REFRESH_FREQUENCY = 1.0;
-    private final double DEFAULT_INITIAL_REFRESH_FREQUENCY_FACTOR = 1.0;
-
-    public SeedProvidersForMarketCommand() {
-        super("seed-providers-for-market", "Seed database with providers for given market, has optional providerName"
-                + "argument for seeding/updating only the specified provider.");
-    }
-
-    private void seedProviders(String providerName, String market) throws IOException {
-        String providersFilePath = "data/seeding/providers-" + escapeMarket(market).toLowerCase() + ".json";
-        File providersFile = new File(providersFilePath);
-        ProviderConfigModel providerConfig = mapper.readValue(providersFile, ProviderConfigModel.class);
-
-        String marketInProvidersConfig = providerConfig.getMarket();
-
-        if (!Objects.equal(marketInProvidersConfig.toLowerCase(), market.toLowerCase())) {
-            throw new IllegalStateException(String.format(
-                    "Supplied market ( %s ) does not match market in the providers config file ( %s ).",
-                    market.toLowerCase(), marketInProvidersConfig.toLowerCase()));
-        }
-
-        String currencyInProvidersConfig = providerConfig.getCurrency();
-        List<ProviderConfiguration> providers = providerConfig.getProviders();
-
-        if (Strings.isNullOrEmpty(providerName)) {
-            // No providerName argument supplied, seed all providers in market's providers config file.
-            seedAllProvidersInConfigFile(marketInProvidersConfig, currencyInProvidersConfig, providers,
-                    providersFile.getName());
-        } else {
-            // providerName argument supplied, find provider in market's providers config file and seed it.
-            seedProviderByProviderName(providerName, marketInProvidersConfig, currencyInProvidersConfig,
-                    providers, providersFile.getName());
-        }
-    }
-
-    private String escapeMarket(String market) {
-        return market.replaceAll("[^a-zA-Z]", "");
-    }
-
-    private void seedAllProvidersInConfigFile(String marketInProvidersConfig, String currencyInProvidersConfig,
-            List<ProviderConfiguration> providers, String providersFileName) {
-        log.info(String.format("Seeding all providers for %s (from %s)",
-                marketInProvidersConfig, providersFileName));
-
-        providers.forEach(provider -> seedProvider(provider, marketInProvidersConfig, currencyInProvidersConfig));
-
-        log.info(String.format("Done seeding %d providers for %s", providers.size(), marketInProvidersConfig));
-    }
-
-    private void seedProviderByProviderName(String providerName, String marketInProvidersConfig,
-            String currencyInProvidersConfig, List<ProviderConfiguration> providers, String providersFileName) {
-        Optional<ProviderConfiguration> providerInConfig = providers.stream()
-                .filter(provider -> Objects.equal(provider.getName(), providerName))
-                .findFirst();
-
-        if (!providerInConfig.isPresent()) {
-            throw new IllegalStateException(String.format(
-                    "Supplied provider, %s, does not exist in the %s providers config file.",
-                    providerName, marketInProvidersConfig));
-        }
-
-        log.info(String.format("Seeding provider '%s' for %s (from %s)",
-                providerName, marketInProvidersConfig, providersFileName));
-
-        seedProvider(providerInConfig.get(), marketInProvidersConfig, currencyInProvidersConfig);
-
-        log.info(String.format("Done seeding provider '%s'", providerName));
-    }
-
-    private void seedProvider(ProviderConfiguration providerToSeed, String market, String currency) {
-        providerToSeed.setCurrency(currency);
-        providerToSeed.setMarket(market);
-        providerToSeed.setRefreshFrequency(DEFAULT_INITIAL_REFRESH_FREQUENCY);
-        providerToSeed.setRefreshFrequencyFactor(DEFAULT_INITIAL_REFRESH_FREQUENCY_FACTOR);
-
-        ProviderConfiguration checkedProviderToSeed = checkPotentiallyModifiedValuesInDatabase(providerToSeed);
-        providerRepository.save(checkedProviderToSeed);
-    }
-
-    private ProviderConfiguration checkPotentiallyModifiedValuesInDatabase(ProviderConfiguration providerToSeed) {
-        ProviderConfiguration providerInDatabase = providerRepository.findOne(providerToSeed.getName());
-
-        if (providerInDatabase == null) {
-            return providerToSeed;
-        }
-
-        ProviderStatuses statusInDatabase = providerInDatabase.getStatus();
-        double refreshFrequencyInDatabase = providerInDatabase.getRefreshFrequency();
-        double refreshFrequencyFactorInDatabase = providerInDatabase.getRefreshFrequencyFactor();
-
-        if (!Objects.equal(statusInDatabase, providerToSeed.getStatus())) {
-            log.warn(String.format("Status in database for provider '%s', does not match status in the "
-                            + "providers config file. Status in the database ( '%s' ) has not been updated.",
-                    providerToSeed.getName(), statusInDatabase));
-            providerToSeed.setStatus(statusInDatabase);
-        }
-
-        if (!Objects.equal(refreshFrequencyInDatabase, providerToSeed.getRefreshFrequency())) {
-            log.warn(String.format("Refresh frequency in database for provider '%s' does not match frequency in"
-                            + " the providers config file. Frequency in the database ( %s ) has not been updated.",
-                    providerToSeed.getName(), refreshFrequencyInDatabase));
-            providerToSeed.setRefreshFrequency(refreshFrequencyInDatabase);
-        }
-
-        if (!Objects.equal(refreshFrequencyFactorInDatabase, providerToSeed.getRefreshFrequencyFactor())) {
-            log.warn(String.format("Refresh frequency factor in database for provider '%s' does not match "
-                            + "factor in the providers config file. Factor in the database ( %s ) "
-                            + "has not been updated.",
-                    providerToSeed.getName(), refreshFrequencyFactorInDatabase));
-            providerToSeed.setRefreshFrequencyFactor(refreshFrequencyFactorInDatabase);
-        }
-
-        return providerToSeed;
-    }
-
-    @Override
-    protected void run(Bootstrap<ServiceConfiguration> bootstrap, Namespace namespace,
-            ServiceConfiguration configuration, Injector injector) throws Exception {
-        final String providerName = System.getProperty("providerName");
-        final String market = System.getProperty("market");
-        Preconditions.checkNotNull(market, "market can't be null.");
-
-        providerRepository = injector.getInstance(ProviderConfigurationRepository.class);
-        try {
-            seedProviders(providerName, market);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
+//public class SeedProvidersForMarketCommand extends ProviderConfigurationCommand<ServiceConfiguration> {
+//    private static final Logger log = LoggerFactory.getLogger(SeedProvidersForMarketCommand.class);
+//    private static ObjectMapper mapper = new ObjectMapper();
+//    private ProviderConfigurationRepository providerRepository;
+//    private final double DEFAULT_INITIAL_REFRESH_FREQUENCY = 1.0;
+//    private final double DEFAULT_INITIAL_REFRESH_FREQUENCY_FACTOR = 1.0;
+//
+//    public SeedProvidersForMarketCommand() {
+//        super("seed-providers-for-market", "Seed database with providers for given market, has optional providerName"
+//                + "argument for seeding/updating only the specified provider.");
+//    }
+//
+//    private void seedProviders(String providerName, String market) throws IOException {
+//        String providersFilePath = "data/seeding/providers-" + escapeMarket(market).toLowerCase() + ".json";
+//        File providersFile = new File(providersFilePath);
+//        ProviderConfigModel providerConfig = mapper.readValue(providersFile, ProviderConfigModel.class);
+//
+//        String marketInProvidersConfig = providerConfig.getMarket();
+//
+//        if (!Objects.equal(marketInProvidersConfig.toLowerCase(), market.toLowerCase())) {
+//            throw new IllegalStateException(String.format(
+//                    "Supplied market ( %s ) does not match market in the providers config file ( %s ).",
+//                    market.toLowerCase(), marketInProvidersConfig.toLowerCase()));
+//        }
+//
+//        String currencyInProvidersConfig = providerConfig.getCurrency();
+//        List<ProviderConfiguration> providers = providerConfig.getProviders();
+//
+//        if (Strings.isNullOrEmpty(providerName)) {
+//            // No providerName argument supplied, seed all providers in market's providers config file.
+//            seedAllProvidersInConfigFile(marketInProvidersConfig, currencyInProvidersConfig, providers,
+//                    providersFile.getName());
+//        } else {
+//            // providerName argument supplied, find provider in market's providers config file and seed it.
+//            seedProviderByProviderName(providerName, marketInProvidersConfig, currencyInProvidersConfig,
+//                    providers, providersFile.getName());
+//        }
+//    }
+//
+//    private String escapeMarket(String market) {
+//        return market.replaceAll("[^a-zA-Z]", "");
+//    }
+//
+//    private void seedAllProvidersInConfigFile(String marketInProvidersConfig, String currencyInProvidersConfig,
+//            List<ProviderConfiguration> providers, String providersFileName) {
+//        log.info(String.format("Seeding all providers for %s (from %s)",
+//                marketInProvidersConfig, providersFileName));
+//
+//        providers.forEach(provider -> seedProvider(provider, marketInProvidersConfig, currencyInProvidersConfig));
+//
+//        log.info(String.format("Done seeding %d providers for %s", providers.size(), marketInProvidersConfig));
+//    }
+//
+//    private void seedProviderByProviderName(String providerName, String marketInProvidersConfig,
+//            String currencyInProvidersConfig, List<ProviderConfiguration> providers, String providersFileName) {
+//        Optional<ProviderConfiguration> providerInConfig = providers.stream()
+//                .filter(provider -> Objects.equal(provider.getName(), providerName))
+//                .findFirst();
+//
+//        if (!providerInConfig.isPresent()) {
+//            throw new IllegalStateException(String.format(
+//                    "Supplied provider, %s, does not exist in the %s providers config file.",
+//                    providerName, marketInProvidersConfig));
+//        }
+//
+//        log.info(String.format("Seeding provider '%s' for %s (from %s)",
+//                providerName, marketInProvidersConfig, providersFileName));
+//
+//        seedProvider(providerInConfig.get(), marketInProvidersConfig, currencyInProvidersConfig);
+//
+//        log.info(String.format("Done seeding provider '%s'", providerName));
+//    }
+//
+//    private void seedProvider(ProviderConfiguration providerToSeed, String market, String currency) {
+//        providerToSeed.setCurrency(currency);
+//        providerToSeed.setMarket(market);
+//        providerToSeed.setRefreshFrequency(DEFAULT_INITIAL_REFRESH_FREQUENCY);
+//        providerToSeed.setRefreshFrequencyFactor(DEFAULT_INITIAL_REFRESH_FREQUENCY_FACTOR);
+//
+//        ProviderConfiguration checkedProviderToSeed = checkPotentiallyModifiedValuesInDatabase(providerToSeed);
+//        providerRepository.save(checkedProviderToSeed);
+//    }
+//
+//    private ProviderConfiguration checkPotentiallyModifiedValuesInDatabase(ProviderConfiguration providerToSeed) {
+//        ProviderConfiguration providerInDatabase = providerRepository.findOne(providerToSeed.getName());
+//
+//        if (providerInDatabase == null) {
+//            return providerToSeed;
+//        }
+//
+//        ProviderStatuses statusInDatabase = providerInDatabase.getStatus();
+//        double refreshFrequencyInDatabase = providerInDatabase.getRefreshFrequency();
+//        double refreshFrequencyFactorInDatabase = providerInDatabase.getRefreshFrequencyFactor();
+//
+//        if (!Objects.equal(statusInDatabase, providerToSeed.getStatus())) {
+//            log.warn(String.format("Status in database for provider '%s', does not match status in the "
+//                            + "providers config file. Status in the database ( '%s' ) has not been updated.",
+//                    providerToSeed.getName(), statusInDatabase));
+//            providerToSeed.setStatus(statusInDatabase);
+//        }
+//
+//        if (!Objects.equal(refreshFrequencyInDatabase, providerToSeed.getRefreshFrequency())) {
+//            log.warn(String.format("Refresh frequency in database for provider '%s' does not match frequency in"
+//                            + " the providers config file. Frequency in the database ( %s ) has not been updated.",
+//                    providerToSeed.getName(), refreshFrequencyInDatabase));
+//            providerToSeed.setRefreshFrequency(refreshFrequencyInDatabase);
+//        }
+//
+//        if (!Objects.equal(refreshFrequencyFactorInDatabase, providerToSeed.getRefreshFrequencyFactor())) {
+//            log.warn(String.format("Refresh frequency factor in database for provider '%s' does not match "
+//                            + "factor in the providers config file. Factor in the database ( %s ) "
+//                            + "has not been updated.",
+//                    providerToSeed.getName(), refreshFrequencyFactorInDatabase));
+//            providerToSeed.setRefreshFrequencyFactor(refreshFrequencyFactorInDatabase);
+//        }
+//
+//        return providerToSeed;
+//    }
+//
+//    @Override
+//    protected void run(Bootstrap<ServiceConfiguration> bootstrap, Namespace namespace,
+//            ServiceConfiguration configuration, Injector injector) throws Exception {
+//        final String providerName = System.getProperty("providerName");
+//        final String market = System.getProperty("market");
+//        Preconditions.checkNotNull(market, "market can't be null.");
+//
+//        providerRepository = injector.getInstance(ProviderConfigurationRepository.class);
+//        try {
+//            seedProviders(providerName, market);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
