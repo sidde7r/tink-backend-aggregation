3a07503223c77258c8fa0cad91c2abacea11f9f9
Jonas Renold <jonas.renold@tink.se>
2018-09-18 09:46:19 +0200

    ref(http) move trust all certificates strategy to own class file
    Move the inlined class to the dedicated truststrategy package.

---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/nxgen/http/truststrategy/TrustAllCertificatesStrategy.java
@@ -0,0 +1,13 @@
+package se.tink.backend.aggregation.nxgen.http.truststrategy;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import tink.org.apache.http.conn.ssl.TrustStrategy;
+
+// good site to test this: https://badssl.com/
+public class TrustAllCertificatesStrategy implements TrustStrategy {
+    @Override
+    public boolean isTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+        return true;
+    }
+}
