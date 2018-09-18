ac46b104ff166eb90de08d355e111f2a803c6683
Lars Almgren <lars.almgren@tink.se>
2018-09-18 15:50:05 +0200

    fea(credit safe service) will never run outside of agg cluster

--- src/credit-safe-service/src/main/java/se/tink/backend/aggregation/credit/safe/CreditSafeServiceContainer.java
+++ src/credit-safe-service/src/main/java/se/tink/backend/aggregation/credit/safe/CreditSafeServiceContainer.java
@@ -1,15 +1,9 @@
 package se.tink.backend.aggregation.credit.safe;
 
-
-import com.google.common.base.Predicate;
-import com.google.common.base.Predicates;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import se.tink.backend.common.AbstractServiceContainer;
 import se.tink.backend.common.config.ServiceConfiguration;
-import se.tink.libraries.auth.ApiTokenAuthorizationHeaderPredicate;
-import se.tink.libraries.auth.ContainerAuthorizationResourceFilterFactory;
-import se.tink.libraries.auth.YubicoAuthorizationHeaderPredicate;
 import se.tink.libraries.dropwizard.DropwizardObjectMapperConfigurator;
 
 public class CreditSafeServiceContainer extends AbstractServiceContainer {
@@ -31,16 +25,6 @@ public class CreditSafeServiceContainer extends AbstractServiceContainer {
     @SuppressWarnings("unchecked")
     @Override
     protected void build(ServiceConfiguration configuration, Environment environment) throws Exception {
-        if (!configuration.isAggregationCluster() && configuration.getServiceAuthentication() != null
-                && !configuration.getServiceAuthentication().getServerTokens().isEmpty()) {
-            Predicate<String> authorizationAuthorizers = Predicates.or(
-                    new ApiTokenAuthorizationHeaderPredicate(configuration.getServiceAuthentication()
-                            .getServerTokens()),
-                    new YubicoAuthorizationHeaderPredicate(
-                            configuration.getYubicoClientId(),
-                            configuration.getServiceAuthentication().getYubikeys()));
-            environment.jersey().getResourceConfig().getResourceFilterFactories()
-                    .add(new ContainerAuthorizationResourceFilterFactory(authorizationAuthorizers));
-        }
+
     }
 }
