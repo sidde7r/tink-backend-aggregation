7e5f3b4ab93050d3b1f123989e654e65db97857b
ovesan <ove.sandfeldt@tink.se>
2018-09-19 09:37:26 +0200

    fea(BanquePopulaire) Add agent test

---
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/fr/banks/banquepopulaire/BanquePopulaireAgentTest.java
@@ -0,0 +1,25 @@
+package se.tink.backend.aggregation.agents.nxgen.fr.banks.banquepopulaire;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.rpc.Field;
+
+@Ignore
+public class BanquePopulaireAgentTest {
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+
+    @Test
+    public void testRegisterAndRefresh() throws Exception {
+        AgentIntegrationTest.Builder builder = new AgentIntegrationTest.Builder("fr",
+                "fr-banquepopulaire-password")
+                .addCredentialField(Field.Key.USERNAME, USERNAME)
+                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                .loadCredentialsBefore(true)
+                .saveCredentialsAfter(true);
+
+        builder.build()
+                .testRefresh();
+    }
+}
