ff2c501b8773b419f4c59942bf98e279739b0dfa
Björn Nyberg <bjorn.nyberg@tink.se>
2018-09-20 13:37:30 +0200

    fix(BankAustria) Accept RTA messages.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/banks/bankaustria/fetcher/BankAustriaTransactionalAccountFetcher.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/banks/bankaustria/fetcher/BankAustriaTransactionalAccountFetcher.java
@@ -1,6 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.at.banks.bankaustria.fetcher;
 
+import java.util.Collection;
+import java.util.Date;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.at.banks.bankaustria.BankAustriaApiClient;
+import se.tink.backend.aggregation.agents.nxgen.at.banks.bankaustria.entities.RtaMessage;
 import se.tink.backend.aggregation.agents.nxgen.at.banks.bankaustria.otml.OtmlResponseConverter;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
@@ -8,11 +13,6 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.paginat
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginator;
 import se.tink.backend.aggregation.nxgen.core.account.TransactionalAccount;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
-
-import java.util.Collection;
-import java.util.Date;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class BankAustriaTransactionalAccountFetcher implements
         AccountFetcher<TransactionalAccount>,
@@ -32,12 +32,16 @@ public class BankAustriaTransactionalAccountFetcher implements
         Collection<TransactionalAccount> accountsFromSettings = otmlResponseConverter
                 .getAccountsFromSettings(apiClient.getAccountsFromSettings().getDataSources());
 
-        // If we did not get any accounts we might have been shown a pop up screen instead, retry
-        // loading the settings
+        // If we did not get any accounts we might have been shown a pop up screen instead,
+        // retry loading the settings and accepting RTA message
         if (accountsFromSettings.isEmpty()) {
+            otmlResponseConverter.anyRtaMessageToAccept(apiClient.getAccountsFromSettings().getDataSources())
+                    .ifPresent(rtaMessage -> acceptRtaMessage(rtaMessage));
+
             accountsFromSettings = otmlResponseConverter
                     .getAccountsFromSettings(apiClient.getAccountsFromSettings().getDataSources());
         }
+
         return accountsFromSettings
                 .stream()
                 .map(account -> otmlResponseConverter
@@ -46,6 +50,10 @@ public class BankAustriaTransactionalAccountFetcher implements
                                         account.getBankIdentifier()).getDataSources(), account))
                 .filter(Objects::nonNull)
                 .collect(Collectors.toList());
+    }
+
+    private void acceptRtaMessage(RtaMessage rtaMessage) {
+        apiClient.acceptRtaMessage(rtaMessage.getRtaMessageID());
     }
 
     @Override
