3bf46d20e260c89a3336e158760ec12197ba466f
evchen <minjia.chen@tink.se>
2018-09-21 13:29:04 +0200

    fix(Persistence) remove unused persistence and module

--- src/provider-configuration-lib/src/main/java/se/tink/backend/aggregation/provider/configuration/config/ProviderRepositoryModule.java
+++ src/provider-configuration-lib/src/main/java/se/tink/backend/aggregation/provider/configuration/config/ProviderRepositoryModule.java
@@ -1,14 +1,7 @@
 package se.tink.backend.aggregation.provider.configuration.config;
 
-import com.google.inject.Provides;
-import com.google.inject.Singleton;
-import com.google.inject.name.Named;
-import java.util.Map;
-import java.util.stream.Collectors;
 import se.tink.backend.aggregation.provider.configuration.storage.repositories.ProviderStatusConfigurationRepository;
 import se.tink.backend.common.config.DatabaseConfiguration;
-import se.tink.backend.common.repository.mysql.aggregation.clusterhostconfiguration.ClusterHostConfigurationRepository;
-import se.tink.backend.core.ClusterHostConfiguration;
 import se.tink.backend.guice.configuration.RepositoryModule;
 
 public class ProviderRepositoryModule extends RepositoryModule {
@@ -17,16 +10,6 @@ public class ProviderRepositoryModule extends RepositoryModule {
     }
 
     protected void bindRepositories() {
-        bindSpringBean(ClusterHostConfigurationRepository.class);
         bindSpringBean(ProviderStatusConfigurationRepository.class);
-    }
-
-    @Provides
-    @Singleton
-    @Named("clusterHostConfigurations")
-    public Map<String, ClusterHostConfiguration> provideCategoryCodesById(ClusterHostConfigurationRepository repository) {
-        return repository.findAll().stream().collect(
-                Collectors.toMap(ClusterHostConfiguration::getClusterId, x -> x)
-        );
     }
 }
