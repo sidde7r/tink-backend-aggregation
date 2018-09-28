fcb67720bf30068996c2f4677a0fb646712eb2ba
ovesan <ove.sandfeldt@tink.se>
2018-09-28 14:57:42 +0200

    fix(ES ImaginBank) Add ignore annotation and rename test method

--- src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/imaginbank/ImaginBankAgentTest.java
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/imaginbank/ImaginBankAgentTest.java
@@ -1,15 +1,17 @@
 package se.tink.backend.aggregation.agents.nxgen.es.banks.imaginbank;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.rpc.Field;
 
+@Ignore
 public class ImaginBankAgentTest {
     private final String USERNAME = "";
     private final String PASSWORD = "";
 
     @Test
-    public void testLogin() throws Exception {
+    public void testRefresh() throws Exception {
         new AgentIntegrationTest.Builder("es", "es-imaginbank-password")
                 .addCredentialField(Field.Key.USERNAME, USERNAME)
                 .addCredentialField(Field.Key.PASSWORD, PASSWORD)
