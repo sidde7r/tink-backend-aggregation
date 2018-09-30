1664f07e1749afb252496bbcef99947ed4a02a85
Jonas Renold <jonas.renold@tink.se>
2018-09-30 21:10:35 +0200

    fix(openid/test) update tests after ukob logic split
    Also removed http specific tests that had previously been moved to the http tests.

--- src/agents-lib/src/test/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/thirdpartyapp/openid/OpenIdApiClientTest.java
+++ src/agents-lib/src/test/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/thirdpartyapp/openid/OpenIdApiClientTest.java
@@ -1,27 +1,26 @@
 package se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Strings;
-import java.io.IOException;
-import org.apache.http.HttpStatus;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.ProviderConfiguration;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.SoftwareStatement;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.UkOpenBankingConfiguration;
-import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.rpc.AccountPermissionResponse;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.rpc.RegistrationResponse;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.rpc.WellKnownResponse;
 import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.http.URL;
-import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
 import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class OpenIdApiClientTest {
 
     private TinkHttpClient httpClient;
     private OpenIdApiClient apiClient;
+    private SoftwareStatement softwareStatement;
+    private ProviderConfiguration providerConfiguration;
 
-    private static final UkOpenBankingConfiguration UKOB_TEST_CONFIG = fromJson("",
+    private static final UkOpenBankingConfiguration UKOB_TEST_CONFIG = SerializationUtils.deserializeFromString("",
             UkOpenBankingConfiguration.class);
 
     @Before
@@ -33,19 +32,14 @@ public class OpenIdApiClientTest {
         httpClient.trustRootCaCertificate(UKOB_TEST_CONFIG.getRootCAData(),
                 UKOB_TEST_CONFIG.getRootCAPassword());
 
-        apiClient = new OpenIdApiClient(httpClient,
-                UKOB_TEST_CONFIG.getSoftwareStatement("tink"),
-                UKOB_TEST_CONFIG.getSoftwareStatement("tink")
-                        .getProviderConfiguration("modelo"));
-    }
 
-    private static <T> T fromJson(String json, Class<T> type) {
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            return mapper.readValue(json, type);
-        } catch (IOException e) {
-            throw new IllegalStateException(e);
-        }
+        softwareStatement = UKOB_TEST_CONFIG.getSoftwareStatement("tink")
+                .orElseThrow(AssertionError::new);
+
+        providerConfiguration = softwareStatement.getProviderConfiguration("modelo")
+                .orElseThrow(AssertionError::new);
+
+        apiClient = new OpenIdApiClient(httpClient, softwareStatement, providerConfiguration);
     }
 
     @Test
@@ -75,38 +69,9 @@ public class OpenIdApiClientTest {
     }
 
     @Test
-    public void TestAccountPermissionRequest() {
-        AuthenticationToken clientCredentials = apiClient.requestClientCredentials();
-        AccountPermissionResponse response = apiClient.requestAccountsApi(clientCredentials);
-        Assert.assertNotNull(response);
-    }
-
-    @Test
-    public void testAuthorizeConsent(){
-        AuthenticationToken clientCredentials = apiClient.requestClientCredentials();
-        AccountPermissionResponse response = apiClient.requestAccountsApi(clientCredentials);
-        URL authUrl = apiClient.buildAuthorizeUrl("test", response.getData().getAccountRequestId());
+    public void testAuthorizeConsent() {
+        URL authUrl = apiClient.buildAuthorizeUrl("state-test", "nonce-test");
         System.out.println(authUrl.toString());
         Assert.assertTrue(!Strings.isNullOrEmpty(authUrl.toString()));
-    }
-
-    @Test(expected = HttpResponseException.class)
-    public void testTrustRootCA() {
-        httpClient.request("https://modelobank2018.o3bank.co.uk:4201/token").get(String.class);
-    }
-
-    @Test
-    public void testClientCertificate() {
-        byte[] clientP12 = UKOB_TEST_CONFIG.getSoftwareStatement("tink").getTransportKeyP12();
-
-        httpClient.setSslClientCertificate(clientP12, "");
-        try {
-            httpClient.request("https://modelobank2018.o3bank.co.uk:4201/token").get(String.class);
-        } catch (HttpResponseException hre) {
-            Assert.assertEquals(hre.getResponse().getStatus(), HttpStatus.SC_NOT_FOUND);
-            return;
-        }
-
-        Assert.fail("should not reach this code");
     }
 }
