017ced5b0d4d06e1f142abe78eb635e75b3f94d4
Jonas Renold <jonas.renold@tink.se>
2018-10-02 09:00:11 +0200

    tes(ukob/ignore) ignore manual tests

--- src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/UkOpenBankingAgentTest.java
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/UkOpenBankingAgentTest.java
@@ -1,8 +1,10 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
+@Ignore
 public class UkOpenBankingAgentTest {
     @Test
     public void test() throws Exception {
