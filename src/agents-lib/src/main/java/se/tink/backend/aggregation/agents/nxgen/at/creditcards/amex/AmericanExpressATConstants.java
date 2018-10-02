366da3c2896f5c632b533544c7d88c1c1fc06e04
michpalm <michpalm95@gmail.com>
2018-10-02 14:07:56 +0200

    fix(Amex AT) Remove unused constants

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/creditcards/amex/AmericanExpressATConstants.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/creditcards/amex/AmericanExpressATConstants.java
@@ -4,9 +4,6 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.ame
 
 public class AmericanExpressATConstants extends AmericanExpressConstants {
 
-    public static final String MARKET = "AT";
-    public static final String PROVIDER_NAME = "at-americanexpress-password";
-
     public static class HeaderValues {
         public static final String APP_ID = "at.co.americanexpress.amexservice";
         public static final String USER_AGENT = "Amex%20AT/4.4.1 CFNetwork/808.2.16 Darwin/16.3.0";
