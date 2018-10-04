ad0cf357bf36478da2cc58a0b5d76f13a452f14c
michpalm <michpalm95@gmail.com>
2018-10-04 11:16:37 +0200

    feat(Amex BE): Adds Amex agent for BE

---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/creditcards/amex/AmericanExpressBEConfiguration.java
@@ -0,0 +1,62 @@
+package se.tink.backend.aggregation.agents.nxgen.be.creditcards.amex;
+
+import java.util.Date;
+import se.tink.backend.aggregation.agents.nxgen.be.creditcards.amex.fetcher.rpc.TimelineBERequest;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v45.AmericanExpressConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v45.AmericanExpressConstants;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v45.authenticator.entities.CardEntity;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v45.fetcher.rpc.TimelineRequest;
+import se.tink.backend.core.Amount;
+import se.tink.libraries.date.ThreadSafeDateFormat;
+
+public class AmericanExpressBEConfiguration implements AmericanExpressConfiguration {
+
+    @Override
+    public String getAppId() {
+        return AmericanExpressBEConstants.HeaderValues.APP_ID;
+    }
+
+    @Override
+    public String getUserAgent() {
+        return AmericanExpressBEConstants.HeaderValues.USER_AGENT;
+    }
+
+    @Override
+    public String getFace() {
+        return AmericanExpressBEConstants.HeaderValues.FACE;
+    }
+
+    @Override
+    public String getLocale() {
+        return AmericanExpressBEConstants.BodyValue.LOCALE;
+    }
+
+    @Override
+    public String getClientVersion() {
+        return AmericanExpressBEConstants.BodyValue.CLIENT_VERSION;
+    }
+
+    @Override
+    public String getBankId(CardEntity cardEntity) {
+        return cardEntity.getCardNumberDisplay();
+    }
+
+    @Override
+    public Amount toAmount(Double amount) {
+        return Amount.inEUR(amount);
+    }
+
+    @Override
+    public TimelineRequest createTimelineRequest(Integer cardIndex) {
+        TimelineBERequest request = new TimelineBERequest();
+        request.setTimeZone(AmericanExpressConstants.RequestValue.TIME_ZONE);
+        request.setTimeZoneOffset(AmericanExpressConstants.RequestValue.TIME_ZONE_OFFSET);
+        request.setSortedIndex(cardIndex);
+        request.setLocalTime(new ThreadSafeDateFormat("MM-dd-YYYY'T'HH:mm:ss").format(new Date()));
+        request.setPendingChargeEnabled(true);
+        request.setCmlEnabled(true);
+        request.setTimestamp(Long.toString(System.currentTimeMillis()));
+        return request;
+    }
+
+}
---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/creditcards/amex/AmericanExpressBEConstants.java
@@ -0,0 +1,21 @@
+package se.tink.backend.aggregation.agents.nxgen.be.creditcards.amex;
+
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v45.AmericanExpressConstants;
+
+public class AmericanExpressBEConstants extends AmericanExpressConstants {
+
+    public static final String MARKET = "BE";
+    public static final String PROVIDER_NAME = "be-americanexpress-password";
+
+    public static class HeaderValues {
+        public static final String APP_ID = "be.co.americanexpress.amexservice";
+        public static final String USER_AGENT = "Amex%20BE/4.4.1 CFNetwork/808.2.16 Darwin/16.3.0";
+        public static final String FACE = "fr_BE";
+    }
+
+    public static class BodyValue {
+        public static final String LOCALE = "en_BE";
+        public static final String CLIENT_VERSION = "4.4.1";
+    }
+
+}
