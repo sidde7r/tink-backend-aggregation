f066b09d369cb83505fa28bda8ff800c920d46e4
Gustaf Svanerud <gustaf.svanerud@tink.se>
2018-10-04 10:17:36 +0200

    tes(agent) Added expectLoggedIn flag to agent test
    Added expectLoggedIn that allows agent tests to opt-out of the
    `Assert.assertTrue("Expected to be logged in.", keepAlive(agent));`
    check in AgentIntegrationTest by setting this flag to `false`.
    
    This allows agents to implement a SessionHandeler that always
    throws `SessionError.SESSION_EXPIRED` if this is required by the
    authentication flow and still pass the test.
    
    The flag defaults to `true`.

--- src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/UkOpenBankingAgentTest.java
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/UkOpenBankingAgentTest.java
@@ -11,6 +11,7 @@ public class UkOpenBankingAgentTest {
         new AgentIntegrationTest.Builder("uk", "uk-modelo-oauth2")
                 .loadCredentialsBefore(true)
                 .saveCredentialsAfter(true)
+                .expectLoggedIn(false)
                 .build()
                 .testRefresh();
     }
