ca832542599a3eccf644a9da64cc61d95e2b6be1
Mathew Clegg <mathew.clegg@live.se>
2018-10-05 10:30:22 +0200

    fix(AgentDebug) Agent Debug Logs will return both CLI and HTTP for AWS

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/storage/AgentDebugS3Storage.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/storage/AgentDebugS3Storage.java
@@ -60,6 +60,9 @@ public class AgentDebugS3Storage implements AgentDebugStorageHandler {
 
     public String putObject(String content, String file) throws AmazonServiceException {
             awsStorageClient.putObject(bucketName, file, content);
-            return String.format("s3://%s/%s", configuration.getAgentDebugBucketName(), file);
+            return String.format("AWS CLI: s3://%s/%s \n "
+                            + "AWS HTTP: %s",
+                    configuration.getAgentDebugBucketName(), file,
+                    awsStorageClient.getUrl(bucketName, file).toExternalForm());
     }
 }
