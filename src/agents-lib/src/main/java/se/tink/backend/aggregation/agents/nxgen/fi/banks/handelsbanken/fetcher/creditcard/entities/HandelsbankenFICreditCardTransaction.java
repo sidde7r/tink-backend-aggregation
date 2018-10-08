ee158db05d6c979ccbb70d635c00257cd016eee7
ovesan <ove.sandfeldt@tink.se>
2018-10-08 14:51:10 +0200

    fix(FI Handelsbanken) Negate amount for transaction
    Nordea shows them as positive amount for purchase and negative amount
    for 'amortization'

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/handelsbanken/fetcher/creditcard/entities/HandelsbankenFICreditCardTransaction.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/handelsbanken/fetcher/creditcard/entities/HandelsbankenFICreditCardTransaction.java
@@ -3,11 +3,11 @@ package se.tink.backend.aggregation.agents.nxgen.fi.banks.handelsbanken.fetcher.
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.util.Date;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.deserializers.StringCleaningDeserializer;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.entities.HandelsbankenAmount;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.account.CreditCardAccount;
 import se.tink.backend.aggregation.nxgen.core.transaction.CreditCardTransaction;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.deserializers.StringCleaningDeserializer;
 import se.tink.backend.core.Amount;
 
 @JsonObject
@@ -21,7 +21,7 @@ public class HandelsbankenFICreditCardTransaction {
 
     public CreditCardTransaction toTinkTransaction(CreditCardAccount account) {
         return CreditCardTransaction.builder()
-                .setAmount(Amount.inEUR(amount.asDouble()))
+                .setAmount(Amount.inEUR(amount.asDouble()).negate())
                 .setDate(date)
                 .setCreditAccount(account)
                 .setDescription(description)
