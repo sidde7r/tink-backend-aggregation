9d9ca59387319b0431b713cfb94fa0c9a9c4bb6e
ovesan <ove.sandfeldt@tink.se>
2018-10-11 17:02:22 +0200

    fea(SE VolvoFinans) Add type for savings accounts

---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/volvofinans/fetcher/transactionalaccounts/entities/SavingsAccountEntity.java
@@ -0,0 +1,59 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.volvofinans.fetcher.transactionalaccounts.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.volvofinans.VolvoFinansConstants;
+import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.nxgen.core.account.SavingsAccount;
+import se.tink.backend.aggregation.nxgen.core.account.entity.HolderName;
+import se.tink.backend.core.Amount;
+
+@JsonObject
+public class SavingsAccountEntity {
+    @JsonProperty("kontoId")
+    private String accountId;
+    @JsonProperty("kontonummer")
+    private String accountNumber;
+    @JsonProperty("produkt")
+    private String product;
+    @JsonProperty("namn")
+    private String name;
+    @JsonProperty("saldo")
+    private double balance;
+    @JsonProperty("rantesats")
+    private double interestRate;
+    @JsonProperty("kontoRoll")
+    private String accountRole;
+    @JsonProperty("intressenter")
+    private List<AccountHolderEntity> accountHolders;
+
+    private double upplupenRanta;
+    private boolean kanOverfora;
+    private String bankgiroInbetalning;
+    private String ocrnummerInbetalning;
+
+    public SavingsAccount toTinkAccount() {
+        return SavingsAccount.builder(accountNumber, Amount.inSEK(balance))
+                .setBankIdentifier(accountId)
+                .setAccountNumber(accountNumber)
+                .setInterestRate(interestRate)
+                .setName(name)
+                .setHolderName(getHolderName())
+                .build();
+    }
+
+    public boolean isAccountHolder() {
+        return VolvoFinansConstants.Fetcher.ACCOUNT_ROLE_MAIN_APPLICANT.equalsIgnoreCase(accountRole);
+    }
+
+    private HolderName getHolderName() {
+        return Optional.ofNullable(accountHolders).orElse(Collections.emptyList()).stream()
+                .filter(holder ->
+                        VolvoFinansConstants.Fetcher.ACCOUNT_ROLE_MAIN_APPLICANT.equalsIgnoreCase(holder.getRole()))
+                .findFirst()
+                .map(AccountHolderEntity::getName)
+                .map(HolderName::new).orElse(null);
+    }
+}
---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/volvofinans/fetcher/transactionalaccounts/rpc/SavingsAccountsResponse.java
@@ -0,0 +1,20 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.volvofinans.fetcher.transactionalaccounts.rpc;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.volvofinans.fetcher.transactionalaccounts.entities.SavingsAccountEntity;
+import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.nxgen.core.account.TransactionalAccount;
+
+@JsonObject
+public class SavingsAccountsResponse extends ArrayList<SavingsAccountEntity> {
+
+    public Collection<TransactionalAccount> getTinkAccounts() {
+        return stream()
+                // only fetch accounts where user is account holder/main applicant (HUVUDSOKANDE)
+                .filter(SavingsAccountEntity::isAccountHolder)
+                .map(SavingsAccountEntity::toTinkAccount)
+                .collect(Collectors.toList());
+    }
+}
