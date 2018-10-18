b9db9c2dc1219a9e39429222102f764a2eef64f5
apete <anders.peterson@tink.se>
2018-10-18 11:08:17 +0200

    ref(OAuth2Authenticator) Add checked exceptions

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/thirdpartyapp/oauth2/OAuth2Authenticator.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/thirdpartyapp/oauth2/OAuth2Authenticator.java
@@ -1,14 +1,25 @@
 package se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2;
 
+import se.tink.backend.aggregation.agents.exceptions.BankServiceException;
+import se.tink.backend.aggregation.agents.exceptions.SessionException;
 import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
 import se.tink.backend.aggregation.nxgen.http.URL;
 
 public interface OAuth2Authenticator {
-    URL buildAuthorizeUrl(String state);
-    OAuth2Token exchangeAuthorizationCode(String code);
-    OAuth2Token refreshAccessToken(String refreshToken);
 
-    // When the authentication is done this method will be invoked to let the agent authenticator use the access
-    // token (e.g. when building requests or attached in a authentication filter).
+    URL buildAuthorizeUrl(String state);
+
+    OAuth2Token exchangeAuthorizationCode(String code) throws BankServiceException;
+
+    /**
+     * If refreshing doesn't work you must throw a {@link SessionException} or {@link BankServiceException}
+     */
+    OAuth2Token refreshAccessToken(String refreshToken) throws SessionException, BankServiceException;
+
+    /**
+     * When the authentication is done this method will be invoked to let the agent authenticator use the access
+     * token (e.g. when building requests or attached in a authentication filter).
+     */
     void useAccessToken(OAuth2Token accessToken);
+
 }
