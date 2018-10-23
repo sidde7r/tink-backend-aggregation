5f7dd56428f1dc097becbf07a5c8427c9e90f77b
Erik Pettersson <eripe970@gmail.com>
2018-10-23 16:33:16 +0200

    fea(teams) deprecate the data team

--- src/api-annotations/src/main/java/se/tink/api/annotations/Team.java
+++ src/api-annotations/src/main/java/se/tink/api/annotations/Team.java
@@ -4,6 +4,7 @@ package se.tink.api.annotations;
  * Make sure that there are alerts defined for each team if you add or remove one.
  */
 public enum Team {
+    @Deprecated
     DATA,
     INTEGRATION,
     PFM
