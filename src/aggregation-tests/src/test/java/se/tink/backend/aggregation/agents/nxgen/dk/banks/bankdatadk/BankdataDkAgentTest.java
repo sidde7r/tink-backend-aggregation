16a44e1360ddc0c73216dfa092dd6504ab535ee9
ovesan <ove.sandfeldt@tink.se>
2018-10-30 14:30:52 +0100

    test(DK Bankdata) Add agent test

---
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/bankdatadk/BankdataDkAgentTest.java
@@ -0,0 +1,26 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.banks.bankdatadk;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.rpc.Field;
+
+@Ignore
+public class BankdataDkAgentTest {
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+
+    private AgentIntegrationTest.Builder builder() {
+        return new AgentIntegrationTest.Builder("dk", "dk-ringkjobinglandbobank-password")
+                .addCredentialField(Field.Key.USERNAME, USERNAME)
+                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                .expectLoggedIn(false)
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(true);
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder().build().testRefresh();
+    }
+}
