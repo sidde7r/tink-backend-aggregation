b3905635c96df17d88e255980dbcf566af7ec262
Mattias Hird <mattias.hird@tink.se>
2018-10-31 16:00:21 +0100

    refactor(LHV): Remove some code duplication from Account and Transaction fetchers

---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/ee/banks/aslhv/fetcher/AsLhvTransactionFetcher.java
@@ -0,0 +1,37 @@
+package se.tink.backend.aggregation.agents.nxgen.ee.banks.aslhv.fetcher;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+import se.tink.backend.aggregation.agents.nxgen.ee.banks.aslhv.AsLhvApiClient;
+import se.tink.backend.aggregation.agents.nxgen.ee.banks.aslhv.rpc.GetAccountTransactionsResponse;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponseImpl;
+import se.tink.backend.aggregation.nxgen.core.account.Account;
+import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
+
+public class AsLhvTransactionFetcher {
+    private final AsLhvApiClient apiClient;
+
+    public AsLhvTransactionFetcher(final AsLhvApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public <T extends Account> PaginatorResponse getTransactionsFor(
+            final T account,
+            final Date fromDate,
+            final Date toDate) {
+        final GetAccountTransactionsResponse response =
+                apiClient.getAccountTransactions(account.getBankIdentifier(), fromDate, toDate);
+        if (!response.requestSuccessful()) {
+            throw new IllegalStateException(String.format("Transaction fetch request failed: %s",
+                    response.getErrorMessage()));
+        }
+
+        final Collection<? extends Transaction> transactions = response.getTransactions(apiClient.getSessionStorage())
+                .stream()
+                .filter(transaction -> !transaction.getAmount().isZero())
+                .collect(Collectors.toSet());
+        return PaginatorResponseImpl.create(transactions);
+    }
+}
--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/ee/banks/aslhv/fetcher/creditcard/AsLhvCreditCardTransactionFetcher.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/ee/banks/aslhv/fetcher/creditcard/AsLhvCreditCardTransactionFetcher.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.ee.banks.aslhv.AsLhvApiClient;
+import se.tink.backend.aggregation.agents.nxgen.ee.banks.aslhv.fetcher.AsLhvTransactionFetcher;
 import se.tink.backend.aggregation.agents.nxgen.ee.banks.aslhv.rpc.GetAccountTransactionsResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponseImpl;
@@ -12,27 +13,15 @@ import se.tink.backend.aggregation.nxgen.core.account.CreditCardAccount;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 
 public class AsLhvCreditCardTransactionFetcher implements TransactionDatePaginator<CreditCardAccount> {
-    private final AsLhvApiClient apiClient;
+    private final AsLhvTransactionFetcher transactionFetcher;
 
     public AsLhvCreditCardTransactionFetcher (AsLhvApiClient apiClient) {
-        this.apiClient = apiClient;
+        transactionFetcher = new AsLhvTransactionFetcher(apiClient);
     }
 
     // TODO Remove code duplication (TransactionFetcher)
     @Override
     public PaginatorResponse getTransactionsFor(CreditCardAccount account, Date fromDate, Date toDate) {
-        final GetAccountTransactionsResponse response = apiClient.getAccountTransactions(account.getBankIdentifier(),
-                fromDate,
-                toDate);
-        if (!response.requestSuccessful()) {
-            throw new IllegalStateException(String.format("Transaction fetch request failed: %s",
-                    response.getErrorMessage()));
-        }
-
-        final Collection<? extends Transaction> transactions = response.getTransactions(apiClient.getSessionStorage())
-                .stream()
-                .filter(transaction -> !transaction.getAmount().isZero())
-                .collect(Collectors.toSet());
-        return PaginatorResponseImpl.create(transactions);
+        return transactionFetcher.getTransactionsFor(account, fromDate, toDate);
     }
 }
