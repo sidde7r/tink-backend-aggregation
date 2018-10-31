6be809e3a432bce0a6aafea32fb2cb3861f19ea3
Andre Hilsendeger <andre.hilsendeger@tink.se>
2018-10-31 16:40:21 +0100

    ci: change pipeline to use buildkite v3

---
+++ .buildkite/build-and-push-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export PATH="/usr/local/bin/google-cloud-sdk/bin:$PATH";
+echo "$GOOGLE_CLOUD_ACCOUNT_JSON" | base64 --decode | gcloud auth activate-service-account --key-file=-;
+
+bazel build docker:bundle.tar;
+docker load -i bazel-bin/docker/bundle.tar;
+
+docker tag gcr.io/tink-containers/tink-backend-provider-configuration:latest "gcr.io/tink-containers/tink-backend-provider-configuration:$VERSION";
+gcloud docker -- push "gcr.io/tink-containers/tink-backend-provider-configuration:$VERSION";
