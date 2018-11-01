b30b1a174d3777bf1c0dd31b21a344228052f883
ovesan <ove.sandfeldt@tink.se>
2018-11-01 11:44:18 +0100

    fix(SE Handelsbanken): Correct null check and set currency

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/fetcher/transactionalaccount/entities/HandelsbankenSETransaction.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/fetcher/transactionalaccount/entities/HandelsbankenSETransaction.java
@@ -39,7 +39,7 @@ public class HandelsbankenSETransaction extends BaseResponse {
 
     @JsonIgnore
     public LocalDate dueDateAsLocalDate() {
-        if (dueDate != null) {
+        if (dueDate == null) {
             return null;
         }
 
@@ -51,7 +51,7 @@ public class HandelsbankenSETransaction extends BaseResponse {
         if (amount == null) {
             return null;
         }
-        Amount positiveAmount = amount.asAmount();
+        Amount positiveAmount = Amount.inSEK(amount.asDouble());
 
         if (!positiveAmount.isPositive()) {
             return positiveAmount.negate();
