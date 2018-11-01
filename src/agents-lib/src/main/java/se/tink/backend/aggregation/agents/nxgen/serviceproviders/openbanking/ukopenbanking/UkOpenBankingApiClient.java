0b7d3f9d918886f9a2ceaf345c87f65996a547da
Gustaf Svanerud <gustaf.svanerud@tink.se>
2018-11-01 16:22:54 +0100

    fix(ukob/content-negotiation) ApiClient accepts application/json
    Created default request that adds `Accept: application/json` to header,
    this is the format we expect and some banks like RBS actually enforces
    content-negotiation and will throw `406 Not Acceptable` if none is provided.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/UkOpenBankingApiClient.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/UkOpenBankingApiClient.java
@@ -6,6 +6,7 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.uko
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.OpenIdApiClient;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.ProviderConfiguration;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.SoftwareStatement;
+import se.tink.backend.aggregation.nxgen.http.RequestBuilder;
 import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.http.URL;
 
@@ -20,21 +21,20 @@ public class UkOpenBankingApiClient extends OpenIdApiClient {
     }
 
     public AccountPermissionResponse createAccountIntentId() {
-        return httpClient.request(providerConfiguration.getAccountRequestsURL())
+        return createRequest(providerConfiguration.getAccountRequestsURL())
                 .type(MediaType.APPLICATION_JSON_TYPE)
-                .accept(MediaType.APPLICATION_JSON_TYPE)
                 .body(AccountPermissionRequest.create())
                 .post(AccountPermissionResponse.class);
     }
 
     public <T> T fetchAccounts(Class<T> responseType) {
-        return httpClient.request(
+        return createRequest(
                 UkOpenBankingConstants.ApiServices.getBulkAccountRequestURL(apiBaseUrl))
                 .get(responseType);
     }
 
     public <T> T fetchAccountBalance(String accountId, Class<T> responseType) {
-        return httpClient.request(
+        return createRequest(
                 UkOpenBankingConstants.ApiServices.getAccountBalanceRequestURL(apiBaseUrl, accountId))
                 .get(responseType);
     }
@@ -46,20 +46,25 @@ public class UkOpenBankingApiClient extends OpenIdApiClient {
         if (url.getScheme() == null)
             url = apiBaseUrl.concat(paginationKey);
 
-        return httpClient.request(url)
+        return createRequest(url)
                 .get(responseType);
     }
 
     public <T> T fetchUpcomingTransactions(String accountId, Class<T> responseType) {
         try {
 
-            return httpClient
-                    .request(UkOpenBankingConstants.ApiServices.getUpcomingTransactionRequestURL(apiBaseUrl, accountId))
+            return createRequest(
+                    UkOpenBankingConstants.ApiServices.getUpcomingTransactionRequestURL(apiBaseUrl, accountId))
                     .get(responseType);
         } catch (Exception e) {
             // TODO: Ukob testdata has an error in it which makes some transactions impossible to parse.
             // TODO: This combined with the null check in UpcomingTransactionFetcher discards those transactions to prevents crash.
             return null;
         }
+    }
+
+    private RequestBuilder createRequest(URL url) {
+        return httpClient.request(url)
+                .accept(MediaType.APPLICATION_JSON_TYPE);
     }
 }
