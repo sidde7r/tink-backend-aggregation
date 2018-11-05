6b245c548e73d39fcde2a16c2ac4969f92e8ea72
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2018-11-05 15:48:07 +0100

    Revert "fea(UpdateController): Create AccountFeaturesFactory"
    This reverts commit e08be82756626e9d9c4a53df4d71ce02b9e470cd.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/UpdateController.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/UpdateController.java
@@ -9,7 +9,6 @@ import se.tink.backend.aggregation.agents.AgentContext;
 import se.tink.backend.aggregation.agents.TransferDestinationsResponse;
 import se.tink.backend.aggregation.constants.MarketCode;
 import se.tink.backend.aggregation.log.AggregationLogger;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.utils.AccountFeaturesFactory;
 import se.tink.backend.aggregation.nxgen.core.account.Account;
 import se.tink.backend.aggregation.nxgen.core.account.InvestmentAccount;
 import se.tink.backend.aggregation.nxgen.core.account.LoanAccount;
@@ -36,7 +35,22 @@ public class UpdateController {
     }
 
     public <A extends Account> boolean updateAccount(A account) {
-        return updateAccount(account, AccountFeaturesFactory.createForAnAccount(account, loanInterpreter));
+        return updateAccount(account, getFeatures(account));
+    }
+
+    private <A extends Account> AccountFeatures getFeatures(A account) {
+        Class<? extends Account> accountClass = account.getClass();
+        if (LoanAccount.class.equals(accountClass)) {
+            LoanAccount loan = (LoanAccount) account;
+            return AccountFeatures.createForLoan(
+                    loan.getDetails().toSystemLoan(loan, loanInterpreter));
+        }
+        if (InvestmentAccount.class.equals(accountClass)) {
+            InvestmentAccount investment = (InvestmentAccount) account;
+            return AccountFeatures.createForPortfolios(
+                    investment.getPortfolios());
+        }
+        return AccountFeatures.createEmpty();
     }
 
     private boolean updateAccount(Account account, AccountFeatures accountFeatures) {
