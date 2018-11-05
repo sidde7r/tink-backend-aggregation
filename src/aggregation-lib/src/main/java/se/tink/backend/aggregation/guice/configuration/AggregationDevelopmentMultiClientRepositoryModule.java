a32aa0ac4ee4a9d1586ac06d859af98516233d4e
evchen <minjia.chen@tink.se>
2018-11-05 09:17:42 +0100

    fea(multiclientModule) add repo binding

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/guice/configuration/AggregationDevelopmentMultiClientRepositoryModule.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/guice/configuration/AggregationDevelopmentMultiClientRepositoryModule.java
@@ -1,6 +1,12 @@
 package se.tink.backend.aggregation.guice.configuration;
 
+import se.tink.backend.aggregation.configurations.repositories.AggregatorConfigurationsRepository;
+import se.tink.backend.aggregation.configurations.repositories.ClientConfigurationsRepository;
+import se.tink.backend.aggregation.configurations.repositories.ClusterConfigurationsRepository;
+import se.tink.backend.aggregation.configurations.repositories.CryptoConfigurationsRepository;
 import se.tink.backend.common.config.DatabaseConfiguration;
+import se.tink.backend.common.repository.mysql.aggregation.clustercryptoconfiguration.ClusterCryptoConfigurationRepository;
+import se.tink.backend.common.repository.mysql.aggregation.clusterhostconfiguration.ClusterHostConfigurationRepository;
 import se.tink.backend.guice.configuration.RepositoryModule;
 
 /*
@@ -10,5 +16,15 @@ import se.tink.backend.guice.configuration.RepositoryModule;
 public class AggregationDevelopmentMultiClientRepositoryModule extends RepositoryModule {
     public AggregationDevelopmentMultiClientRepositoryModule(DatabaseConfiguration databaseConfiguration) {
         super(databaseConfiguration);
+    }
+
+    @Override
+    protected void bindRepositories() {
+        bindSpringBean(ClusterHostConfigurationRepository.class);
+        bindSpringBean(ClusterCryptoConfigurationRepository.class);
+        bindSpringBean(CryptoConfigurationsRepository.class);
+        bindSpringBean(ClientConfigurationsRepository.class);
+        bindSpringBean(AggregatorConfigurationsRepository.class);
+        bindSpringBean(ClusterConfigurationsRepository.class);
     }
 }
