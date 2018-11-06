b95cf6f54e17198417da66856b063bcdbbaf33a9
Lars Almgren <lars.almgren@tink.se>
2018-11-06 13:22:33 +0100

    ref: remove service discovery logic from controller client

--- src/aggregationcontroller-api/src/main/java/se/tink/backend/aggregation/aggregationcontroller/AggregationControllerAggregationClient.java
+++ src/aggregationcontroller-api/src/main/java/se/tink/backend/aggregation/aggregationcontroller/AggregationControllerAggregationClient.java
@@ -5,7 +5,6 @@ import com.google.common.base.Strings;
 import com.google.inject.Inject;
 import com.sun.jersey.api.client.Client;
 import javax.ws.rs.core.Response;
-import org.apache.curator.framework.CuratorFramework;
 import se.tink.backend.aggregation.aggregationcontroller.v1.api.CredentialsService;
 import se.tink.backend.aggregation.aggregationcontroller.v1.api.ProcessService;
 import se.tink.backend.aggregation.aggregationcontroller.v1.api.UpdateService;
@@ -26,38 +25,15 @@ import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.UpdateTransfersR
 import se.tink.backend.aggregation.rpc.Account;
 import se.tink.backend.aggregation.rpc.Credentials;
 import se.tink.backend.core.signableoperation.SignableOperation;
-import se.tink.backend.guice.annotations.AggregationControllerConfiguration;
 import se.tink.backend.system.rpc.UpdateFraudDetailsRequest;
-import se.tink.libraries.discovered.DiscoveredWebServiceClassBuilder;
-import se.tink.libraries.endpoints.EndpointConfiguration;
 import se.tink.libraries.http.client.WebResourceFactory;
 import se.tink.libraries.jersey.utils.JerseyUtils;
 
 public class AggregationControllerAggregationClient {
-    private static final String SERVICE_NAME = "aggregation-controller";
     private static final String EMPTY_PASSWORD = "";
 
-    private final EndpointConfiguration endpointConfiguration;
-    private final CuratorFramework coordinationClient;
-
-    private ProcessService processService = null;
-    private UpdateService updateService = null;
-
     @Inject
-    public AggregationControllerAggregationClient(
-            @AggregationControllerConfiguration EndpointConfiguration endpointConfiguration,
-            CuratorFramework coordinationClient) {
-        this.endpointConfiguration = endpointConfiguration;
-        this.coordinationClient = coordinationClient;
-    }
-
-    // This enables us to start the service without the need for Aggregation to be up if we use service discovery
-    private <T> T buildServiceFromInterfaceUsingServiceDiscovery(Class<T> serviceInterface) {
-        Client client = JerseyUtils.getClient(endpointConfiguration.getPinnedCertificates());
-        JerseyUtils.registerAPIAccessToken(client, endpointConfiguration.getAccessToken());
-
-        return new DiscoveredWebServiceClassBuilder(coordinationClient, client, SERVICE_NAME)
-                .build(serviceInterface);
+    public AggregationControllerAggregationClient() {
     }
 
     private <T> T buildInterClusterServiceFromInterface(HostConfiguration hostConfiguration, Class<T> serviceInterface) {
@@ -76,13 +52,6 @@ public class AggregationControllerAggregationClient {
         return buildInterClusterServiceFromInterface(hostConfiguration, CredentialsService.class);
     }
 
-    private ProcessService getProcessServiceUsingServiceDiscovery() {
-        if (processService == null) {
-            processService = buildServiceFromInterfaceUsingServiceDiscovery(ProcessService.class);
-        }
-        return processService;
-    }
-
     private ProcessService getProcessService(HostConfiguration hostConfiguration) {
         return buildInterClusterServiceFromInterface(hostConfiguration, ProcessService.class);
     }
@@ -91,24 +60,9 @@ public class AggregationControllerAggregationClient {
         return buildInterClusterServiceFromInterface(hostConfiguration, UpdateService.class);
     }
 
-    private UpdateService getUpdateServiceUsingServiceDiscovery() {
-        if (updateService == null) {
-            updateService = buildServiceFromInterfaceUsingServiceDiscovery(UpdateService.class);
-        }
-        return updateService;
-    }
-
-    public Response generateStatisticsAndActivityAsynchronously(GenerateStatisticsAndActivitiesRequest request) {
-        return getProcessServiceUsingServiceDiscovery().generateStatisticsAndActivityAsynchronously(request);
-    }
-
     public Response generateStatisticsAndActivityAsynchronously(HostConfiguration hostConfiguration,
             GenerateStatisticsAndActivitiesRequest request) {
         return getProcessService(hostConfiguration).generateStatisticsAndActivityAsynchronously(request);
-    }
-
-    public Response updateTransactionsAsynchronously(UpdateTransactionsRequest request) {
-        return getProcessServiceUsingServiceDiscovery().updateTransactionsAsynchronously(request);
     }
 
     public Response updateTransactionsAsynchronously(HostConfiguration hostConfiguration,
@@ -116,16 +70,8 @@ public class AggregationControllerAggregationClient {
         return getProcessService(hostConfiguration).updateTransactionsAsynchronously(request);
     }
 
-    public String ping() {
-        return getUpdateServiceUsingServiceDiscovery().ping();
-    }
-
     public String ping(HostConfiguration hostConfiguration) {
         return getUpdateService(hostConfiguration).ping();
-    }
-
-    public SupplementalInformationResponse getSupplementalInformation(SupplementalInformationRequest request) {
-        return getUpdateServiceUsingServiceDiscovery().getSupplementalInformation(request);
     }
 
     public SupplementalInformationResponse getSupplementalInformation(HostConfiguration hostConfiguration,
@@ -133,16 +79,8 @@ public class AggregationControllerAggregationClient {
         return getUpdateService(hostConfiguration).getSupplementalInformation(request);
     }
 
-    public Account updateAccount(UpdateAccountRequest request) {
-        return getUpdateServiceUsingServiceDiscovery().updateAccount(request);
-    }
-
     public Account updateAccount(HostConfiguration hostConfiguration, UpdateAccountRequest request) {
         return getUpdateService(hostConfiguration).updateAccount(request);
-    }
-
-    public Response updateTransferDestinationPatterns(UpdateTransferDestinationPatternsRequest request) {
-        return getUpdateServiceUsingServiceDiscovery().updateTransferDestinationPatterns(request);
     }
 
     public Response updateTransferDestinationPatterns(HostConfiguration hostConfiguration,
@@ -150,48 +88,24 @@ public class AggregationControllerAggregationClient {
         return getUpdateService(hostConfiguration).updateTransferDestinationPatterns(request);
     }
 
-    public Response processAccounts(ProcessAccountsRequest request) {
-        return getUpdateServiceUsingServiceDiscovery().processAccounts(request);
-    }
-
     public Response processAccounts(HostConfiguration hostConfiguration, ProcessAccountsRequest request) {
         return getUpdateService(hostConfiguration).processAccounts(request);
-    }
-
-    public Response optOutAccounts(OptOutAccountsRequest request) {
-        return getUpdateServiceUsingServiceDiscovery().optOutAccounts(request);
     }
 
     public Response optOutAccounts(HostConfiguration hostConfiguration, OptOutAccountsRequest request) {
         return getUpdateService(hostConfiguration).optOutAccounts(request);
     }
 
-    public Response updateCredentials(UpdateCredentialsStatusRequest request) {
-        return getUpdateServiceUsingServiceDiscovery().updateCredentials(request);
-    }
-
     public Response updateCredentials(HostConfiguration hostConfiguration, UpdateCredentialsStatusRequest request) {
         return getUpdateService(hostConfiguration).updateCredentials(request);
-    }
-
-    public Response updateSignableOperation(SignableOperation signableOperation) {
-        return getUpdateServiceUsingServiceDiscovery().updateSignableOperation(signableOperation);
     }
 
     public Response updateSignableOperation(HostConfiguration hostConfiguration, SignableOperation signableOperation) {
         return getUpdateService(hostConfiguration).updateSignableOperation(signableOperation);
     }
 
-    public Response processEinvoices(UpdateTransfersRequest request) {
-        return getUpdateServiceUsingServiceDiscovery().processEinvoices(request);
-    }
-
     public Response processEinvoices(HostConfiguration hostConfiguration, UpdateTransfersRequest request) {
         return getUpdateService(hostConfiguration).processEinvoices(request);
-    }
-
-    public UpdateDocumentResponse updateDocument(UpdateDocumentRequest request) {
-        return getUpdateServiceUsingServiceDiscovery().updateDocument(request);
     }
 
     public UpdateDocumentResponse updateDocument(HostConfiguration hostConfiguration, UpdateDocumentRequest request) {
@@ -200,10 +114,6 @@ public class AggregationControllerAggregationClient {
 
     public Response updateFraudDetails(HostConfiguration hostConfiguration, UpdateFraudDetailsRequest request) {
         return getUpdateService(hostConfiguration).updateFraudDetails(request);
-    }
-
-    public Response updateFraudDetails(UpdateFraudDetailsRequest updateFraudRequest) {
-        return getUpdateServiceUsingServiceDiscovery().updateFraudDetails(updateFraudRequest);
     }
 
     public Response updateCredentialSensitive(HostConfiguration hostConfiguration, Credentials credentials,
