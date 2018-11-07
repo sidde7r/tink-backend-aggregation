3fad92ee4eac005e82133ca19b2abdd72dca0215
Lars Almgren <lars.almgren@tink.se>
2018-11-07 11:53:53 +0100

    fea(common conf) add missing configuration to provider service

--- src/provider-configuration-lib/src/main/java/se/tink/backend/aggregation/provider/configuration/config/ProviderServiceConfigurationModule.java
+++ src/provider-configuration-lib/src/main/java/se/tink/backend/aggregation/provider/configuration/config/ProviderServiceConfigurationModule.java
@@ -1,6 +1,12 @@
 package se.tink.backend.aggregation.provider.configuration.config;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+import se.tink.backend.common.VersionInformation;
+import se.tink.libraries.metrics.HeapDumpGauge;
+import se.tink.libraries.metrics.MetricCollector;
+import se.tink.libraries.metrics.MetricRegistry;
+import se.tink.libraries.metrics.PrometheusExportServer;
 
 public class ProviderServiceConfigurationModule extends AbstractModule {
     private final ProviderServiceConfiguration configuration;
@@ -11,6 +17,12 @@ public class ProviderServiceConfigurationModule extends AbstractModule {
 
     @Override
     protected void configure() {
+        bind(MetricRegistry.class).in(Scopes.SINGLETON);
+        bind(VersionInformation.class).in(Scopes.SINGLETON);
+        bind(MetricCollector.class).in(Scopes.SINGLETON);
+        bind(PrometheusExportServer.class).in(Scopes.SINGLETON);
+        bind(HeapDumpGauge.class).in(Scopes.SINGLETON);
+
         bind(ProviderServiceConfiguration.class).toInstance(configuration);
         bind(PrometheusConfiguration.class).toInstance(configuration.getPrometheus());
     }
