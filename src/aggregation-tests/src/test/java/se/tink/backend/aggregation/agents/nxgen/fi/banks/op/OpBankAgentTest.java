23f41241922d2ed940608e7875cd0acf5011a729
ovesan <ove.sandfeldt@tink.se>
2018-11-08 10:34:47 +0100

    test(FI OpBank): Add new style agent test

---
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/op/OpBankAgentTest.java
@@ -0,0 +1,37 @@
+package se.tink.backend.aggregation.agents.nxgen.fi.banks.op;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.rpc.Field;
+
+@Ignore
+public class OpBankAgentTest {
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+
+    private AgentIntegrationTest.Builder builder() {
+        return new AgentIntegrationTest.Builder("fi", "fi-op-codecard")
+                .addCredentialField(Field.Key.USERNAME, USERNAME)
+                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                .expectLoggedIn(false)
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(true);
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder()
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(true)
+                .build().testRefresh();
+    }
+
+    @Test
+    public void testAutoRefresh() throws Exception {
+        builder()
+                .loadCredentialsBefore(true)
+                .saveCredentialsAfter(true)
+                .build().testRefresh();
+    }
+}
