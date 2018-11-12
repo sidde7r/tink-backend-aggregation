b35cba4c366dd5d1fc3ae83eb4ddb7d839fdd3f7
Babis Kiosidis <babis.kiosidis@tink.se>
2018-11-12 09:34:54 +0100

    fea(capabilities, agents): define list of available capabilities

---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/constants/Capability.java
@@ -0,0 +1,10 @@
+package se.tink.backend.aggregation.constants;
+
+public enum Capability {
+    CHECKING_ACCOUNTS,
+    SAVINGS_ACCOUNTS,
+    CREDIT_CARDS,
+    LOANS,
+    INVESTMENTS,
+    PAYMENTS
+}
