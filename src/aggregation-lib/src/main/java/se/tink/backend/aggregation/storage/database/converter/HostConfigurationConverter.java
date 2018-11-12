ba0e9636501e99f8f9fa0ad8b99633ff8d214aff
evchen <minjia.chen@tink.se>
2018-11-12 14:10:52 +0100

    feat(Converter) create new converter for DAO
    1:1 mapping from ClusterConfiguration to HostConfiguration

---
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/storage/database/converter/HostConfigurationConverter.java
@@ -0,0 +1,17 @@
+package se.tink.backend.aggregation.storage.database.converter;
+
+import se.tink.backend.aggregation.aggregationcontroller.v1.core.HostConfiguration;
+import se.tink.backend.aggregation.storage.database.models.ClusterConfiguration;
+
+public class HostConfigurationConverter {
+
+    public static HostConfiguration convert(ClusterConfiguration clusterConfiguration) {
+        HostConfiguration hostConfiguration = new HostConfiguration();
+        hostConfiguration.setClusterId(clusterConfiguration.getClusterId());
+        hostConfiguration.setHost(clusterConfiguration.getHost());
+        hostConfiguration.setApiToken(clusterConfiguration.getApiToken());
+        hostConfiguration.setBase64encodedclientcert(clusterConfiguration.getBase64encodedclientcert());
+        hostConfiguration.setDisablerequestcompression(clusterConfiguration.isDisablerequestcompression());
+        return hostConfiguration;
+    }
+}
