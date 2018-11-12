b49d3e1e96aa7c6c911b2209378c3c40cb634416
Lars Almgren <lars.almgren@tink.se>
2018-11-12 12:03:52 +0100

    fea(multi tenant) add controller wrapper
    This wrapper will make the communication back to the clusters
    easier since it wrapps the aggregation controller aggregation client
    together with the host configuration.

---
+++ src/aggregationcontroller-api/src/main/java/se/tink/backend/aggregation/aggregationcontroller/ControllerWrapper.java
@@ -0,0 +1,91 @@
+package se.tink.backend.aggregation.aggregationcontroller;
+
+import javax.ws.rs.core.Response;
+import se.tink.backend.aggregation.aggregationcontroller.v1.core.HostConfiguration;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.GenerateStatisticsAndActivitiesRequest;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.OptOutAccountsRequest;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.ProcessAccountsRequest;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.SupplementalInformationRequest;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.SupplementalInformationResponse;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.UpdateAccountRequest;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.UpdateCredentialsStatusRequest;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.UpdateDocumentRequest;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.UpdateDocumentResponse;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.UpdateTransactionsRequest;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.UpdateTransferDestinationPatternsRequest;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.UpdateTransfersRequest;
+import se.tink.backend.aggregation.rpc.Account;
+import se.tink.backend.aggregation.rpc.Credentials;
+import se.tink.backend.core.signableoperation.SignableOperation;
+import se.tink.backend.system.rpc.UpdateFraudDetailsRequest;
+
+public class ControllerWrapper {
+    private final AggregationControllerAggregationClient client;
+    private final HostConfiguration configuration;
+
+    private ControllerWrapper(AggregationControllerAggregationClient client, HostConfiguration configuration) {
+        this.client = client;
+        this.configuration = configuration;
+    }
+
+    public static ControllerWrapper of(AggregationControllerAggregationClient client, HostConfiguration configuration) {
+        return new ControllerWrapper(client, configuration);
+    }
+
+    public Response generateStatisticsAndActivityAsynchronously(GenerateStatisticsAndActivitiesRequest request) {
+        return client.generateStatisticsAndActivityAsynchronously(configuration, request);
+    }
+
+    public Response updateTransactionsAsynchronously(UpdateTransactionsRequest request) {
+        return client.updateTransactionsAsynchronously(configuration, request);
+    }
+
+    public String ping() {
+        return client.ping(configuration);
+    }
+
+    public SupplementalInformationResponse getSupplementalInformation(SupplementalInformationRequest request) {
+        return client.getSupplementalInformation(configuration, request);
+    }
+
+    public Account updateAccount(UpdateAccountRequest request) {
+        return client.updateAccount(configuration, request);
+    }
+
+    public Response updateTransferDestinationPatterns(
+            UpdateTransferDestinationPatternsRequest request) {
+        return client.updateTransferDestinationPatterns(configuration, request);
+    }
+
+    public Response processAccounts(ProcessAccountsRequest request) {
+        return client.processAccounts(configuration, request);
+    }
+
+    public Response optOutAccounts(OptOutAccountsRequest request) {
+        return client.optOutAccounts(configuration, request);
+    }
+
+    public Response updateCredentials(UpdateCredentialsStatusRequest request) {
+        return client.updateCredentials(configuration, request);
+    }
+
+    public Response updateSignableOperation(SignableOperation signableOperation) {
+        return client.updateSignableOperation(configuration, signableOperation);
+    }
+
+    public Response processEinvoices(UpdateTransfersRequest request) {
+        return client.processEinvoices(configuration, request);
+    }
+
+    public UpdateDocumentResponse updateDocument(UpdateDocumentRequest request) {
+        return client.updateDocument(configuration, request);
+    }
+
+    public Response updateFraudDetails(UpdateFraudDetailsRequest request) {
+        return client.updateFraudDetails(configuration, request);
+    }
+
+    public Response updateCredentialSensitive(Credentials credentials, String sensitiveData) {
+        return client.updateCredentialSensitive(configuration, credentials, sensitiveData);
+    }
+}
