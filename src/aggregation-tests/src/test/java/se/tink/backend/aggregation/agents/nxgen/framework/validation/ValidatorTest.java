14bd6597ec6e4dba8efb0b160e7c1f0d3f2e1e79
Sebastian Olsson <sebastian.olsson@tink.se>
2018-11-13 13:40:33 +0100

    test(validator): Improved test

--- src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/framework/validation/ValidatorTest.java
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/framework/validation/ValidatorTest.java
@@ -1,9 +1,14 @@
 package se.tink.backend.aggregation.agents.nxgen.framework.validation;
 
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
 import org.junit.Test;
 import se.tink.backend.aggregation.rpc.Account;
+import se.tink.backend.system.rpc.Transaction;
 
 // TODO Move to one of the test jars
 public final class ValidatorTest {
@@ -11,10 +16,11 @@ public final class ValidatorTest {
     public void testValidator() {
         Account account = new Account();
         account.setBalance(999999999.0);
+        account.setHolderName("Test Testsson");
 
         AisData aisData = new AisData(Collections.singleton(account), Collections.emptySet());
 
-        final SilentAction action = new SilentAction();
+        SilentAction action = new SilentAction();
 
         AisValidator validator =
                 AisValidator.builder()
@@ -30,6 +36,10 @@ public final class ValidatorTest {
                                         String.format(
                                                 "Balance %f exceeds threshold %f for account %s",
                                                 acc.getBalance(), 10000000.0, acc))
+                        .ruleAccount(
+                                "Holder name is present",
+                                acc -> acc.getHolderName() != null,
+                                acc -> String.format("Account lacks a holder name: %s", acc))
                         .rule(
                                 "Account balance threshold",
                                 data ->
