fcc10e6838fcb0a3a48f7920e2b3375a09ff7c9f
Jonas Renold <jonas.renold@tink.se>
2018-11-14 16:00:01 +0100

    fea(ukobV11) add accountIdentifier to transactional accounts
    Add AccountIdentifier to transactional accounts, supports the two types documented in the UKOB v11 specification.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/v11/fetcher/entities/account/AccountIdentifierEntity.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/v11/fetcher/entities/account/AccountIdentifierEntity.java
@@ -1,13 +1,17 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v11.fetcher.entities.account;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.UkOpenBankingConstants;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.account.identifiers.IbanIdentifier;
+import se.tink.libraries.account.identifiers.SortCodeIdentifier;
 
 @JsonObject
 public class AccountIdentifierEntity {
 
     @JsonProperty("SchemeName")
-    private String schemeName;
+    private UkOpenBankingConstants.ExternalAccountIdentification2Code identifierType;
     @JsonProperty("Identification")
     private String identification;
     @JsonProperty("Name")
@@ -21,5 +25,18 @@ public class AccountIdentifierEntity {
 
     public String getName() {
         return name;
+    }
+
+    public AccountIdentifier toAccountIdentifier() {
+        switch (identifierType) {
+        case IBAN:
+            return new IbanIdentifier(null, identification);
+        case SORT_CODE_ACCOUNT_NUMBER:
+            return new SortCodeIdentifier(name, identification);
+        default:
+            throw new IllegalStateException(
+                    String.format("Unknown identifier type: %s", identifierType)
+            );
+        }
     }
 }
