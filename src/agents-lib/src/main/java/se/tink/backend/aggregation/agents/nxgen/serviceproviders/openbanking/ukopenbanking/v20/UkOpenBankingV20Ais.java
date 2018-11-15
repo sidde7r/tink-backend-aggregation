7a1dbcee46f4a72a697fac723afbf66b931b5c0e
Jonas Renold <jonas.renold@tink.se>
2018-11-15 11:43:53 +0100

    fea(ukob) ais implementations for all versions
    The code is copied from the version's agent code, just copy paste since the interface has the same methods as the agent abstracts.

---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/v20/UkOpenBankingV20Ais.java
@@ -0,0 +1,66 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v20;
+
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.UkOpenBankingAis;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.UkOpenBankingApiClient;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.fetcher.UkOpenBankingAccountFetcher;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.fetcher.UkOpenBankingTransactionPaginator;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.fetcher.UkOpenBankingUpcomingTransactionFetcher;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v20.fetcher.rpc.account.AccountBalanceV20Response;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v20.fetcher.rpc.account.AccountsV20Response;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v20.fetcher.rpc.transaction.AccountTransactionsV20Response;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.TransactionPaginator;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginationController;
+import se.tink.backend.aggregation.nxgen.core.account.CreditCardAccount;
+import se.tink.backend.aggregation.nxgen.core.account.TransactionalAccount;
+
+public class UkOpenBankingV20Ais implements UkOpenBankingAis {
+    @Override
+    public UkOpenBankingAccountFetcher<?, ?, TransactionalAccount> makeTransactionalAccountFetcher(
+            UkOpenBankingApiClient apiClient) {
+        return new UkOpenBankingAccountFetcher<>(apiClient,
+                AccountsV20Response.class,
+                AccountBalanceV20Response.class,
+                AccountsV20Response::toTransactionalAccount
+        );
+    }
+
+    @Override
+    public TransactionPaginator<TransactionalAccount> makeAccountTransactionPaginatorController(
+            UkOpenBankingApiClient apiClient) {
+        return new TransactionKeyPaginationController<>(
+                new UkOpenBankingTransactionPaginator<>(
+                        apiClient,
+                        AccountTransactionsV20Response.class,
+                        AccountTransactionsV20Response::toAccountTransactionPaginationResponse));
+    }
+
+    @Override
+    public Optional<UkOpenBankingUpcomingTransactionFetcher<?>> makeUpcomingTransactionFetcher(
+            UkOpenBankingApiClient apiClient) {
+        return Optional.empty();
+        //TODO: Enable when this feature is mandatory for the banks to implement
+        //        return Optional.of(new UkOpenBankingUpcomingTransactionFetcher<>(apiClient,
+        //                UpcomingTransactionsV20Response.class,
+        //                UpcomingTransactionsV20Response::toUpcomingTransactions));
+    }
+
+    @Override
+    public UkOpenBankingAccountFetcher<?, ?, CreditCardAccount> makeCreditCardAccountFetcher(
+            UkOpenBankingApiClient apiClient) {
+        return new UkOpenBankingAccountFetcher<>(apiClient,
+                AccountsV20Response.class,
+                AccountBalanceV20Response.class,
+                AccountsV20Response::toCreditCardAccount);
+    }
+
+    @Override
+    public TransactionPaginator<CreditCardAccount> makeCreditCardTransactionPaginatorController(
+            UkOpenBankingApiClient apiClient) {
+        return new TransactionKeyPaginationController<>(
+                new UkOpenBankingTransactionPaginator<>(
+                        apiClient,
+                        AccountTransactionsV20Response.class,
+                        AccountTransactionsV20Response::toCreditCardPaginationResponse));
+    }
+}
---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/v30/UkOpenBankingV30Ais.java
@@ -0,0 +1,65 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v30;
+
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.UkOpenBankingAis;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.UkOpenBankingApiClient;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.fetcher.UkOpenBankingAccountFetcher;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.fetcher.UkOpenBankingTransactionPaginator;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.fetcher.UkOpenBankingUpcomingTransactionFetcher;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v30.fetcher.rpc.account.AccountBalanceV30Response;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v30.fetcher.rpc.account.AccountsV30Response;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v30.fetcher.rpc.transaction.AccountTransactionsV30Response;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.TransactionPaginator;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginationController;
+import se.tink.backend.aggregation.nxgen.core.account.CreditCardAccount;
+import se.tink.backend.aggregation.nxgen.core.account.TransactionalAccount;
+
+public class UkOpenBankingV30Ais implements UkOpenBankingAis {
+    @Override
+    public UkOpenBankingAccountFetcher<?, ?, TransactionalAccount> makeTransactionalAccountFetcher(
+            UkOpenBankingApiClient apiClient) {
+        return new UkOpenBankingAccountFetcher<>(apiClient,
+                AccountsV30Response.class,
+                AccountBalanceV30Response.class,
+                AccountsV30Response::toTransactionalAccount);
+    }
+
+    @Override
+    public TransactionPaginator<TransactionalAccount> makeAccountTransactionPaginatorController(
+            UkOpenBankingApiClient apiClient) {
+        return new TransactionKeyPaginationController<>(
+                new UkOpenBankingTransactionPaginator<>(
+                        apiClient,
+                        AccountTransactionsV30Response.class,
+                        AccountTransactionsV30Response::toAccountTransactionPaginationResponse));
+    }
+
+    @Override
+    public Optional<UkOpenBankingUpcomingTransactionFetcher<?>> makeUpcomingTransactionFetcher(
+            UkOpenBankingApiClient apiClient) {
+        return Optional.empty();
+        //TODO: Enable when this feature is mandatory for the banks to implement
+        //        return Optional.of(new UkOpenBankingUpcomingTransactionFetcher<>(apiClient,
+        //                UpcomingTransactionsV30Response.class,
+        //                UpcomingTransactionsV30Response::toUpcomingTransactions));
+    }
+
+    @Override
+    public UkOpenBankingAccountFetcher<?, ?, CreditCardAccount> makeCreditCardAccountFetcher(
+            UkOpenBankingApiClient apiClient) {
+        return new UkOpenBankingAccountFetcher<>(apiClient,
+                AccountsV30Response.class,
+                AccountBalanceV30Response.class,
+                AccountsV30Response::toCreditCardAccount);
+    }
+
+    @Override
+    public TransactionPaginator<CreditCardAccount> makeCreditCardTransactionPaginatorController(
+            UkOpenBankingApiClient apiClient) {
+        return new TransactionKeyPaginationController<>(
+                new UkOpenBankingTransactionPaginator<>(
+                        apiClient,
+                        AccountTransactionsV30Response.class,
+                        AccountTransactionsV30Response::toCreditCardPaginationResponse));
+    }
+}
