18b8eb8eceb5a533909bbc0ffdae57a960f7add9
Babis Kiosidis <babis.kiosidis@tink.se>
2018-11-16 16:53:36 +0100

    ref(client info): removed api key from object

--- src/aggregation-api/src/main/java/se/tink/backend/aggregation/cluster/identification/ClientInfo.java
+++ src/aggregation-api/src/main/java/se/tink/backend/aggregation/cluster/identification/ClientInfo.java
@@ -2,27 +2,21 @@ package se.tink.backend.aggregation.cluster.identification;
 
 public class ClientInfo {
     private String clientName;
-    private String apiClientKey;
     private String clusterId;
     private String aggregatorId;
 
-    private ClientInfo(String clientName, String apiClientKey, String clusterId, String aggregatorId) {
+    private ClientInfo(String clientName, String clusterId, String aggregatorId) {
         this.clientName = clientName;
-        this.apiClientKey = apiClientKey;
         this.clusterId = clusterId;
         this.aggregatorId = aggregatorId;
     }
 
-    public static ClientInfo of(String clientName, String apiClientKey, String clusterId, String aggregatorId) {
-        return new ClientInfo(clientName, apiClientKey, clusterId, aggregatorId);
+    public static ClientInfo of(String clientName, String clusterId, String aggregatorId) {
+        return new ClientInfo(clientName, clusterId, aggregatorId);
     }
 
     public String getClientName() {
         return clientName;
-    }
-
-    public String getApiClientKey() {
-        return apiClientKey;
     }
 
     public String getClusterId() {
