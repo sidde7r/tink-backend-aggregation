101fa01da88977ea139705b2db4ca8283ec14ee3
Jonas Renold <jonas.renold@tink.se>
2018-11-19 13:33:47 +0100

    fea(ukob/pis) PIS interface
    PIS interface to make bank transfers, "person to person" or "party to party" according to ukob PIS documentation.
    
    The sourceIdentifier can be null. If it is null the agent should not specify it in the request, instead the user will pick a source account in the consent flow.

---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/UkOpenBankingPis.java
@@ -0,0 +1,28 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking;
+
+import javax.annotation.Nullable;
+import se.tink.backend.aggregation.agents.TransferExecutionException;
+import se.tink.backend.core.Amount;
+import se.tink.libraries.account.AccountIdentifier;
+
+public interface UkOpenBankingPis {
+    // Some banks do not allow us to specify a source account.
+    // I.e. the user MUST pick the source account him/her self in the bank's consent flow.
+    boolean canHaveSourceAccountSpecified();
+
+    String getBankTransferIntentId(
+            UkOpenBankingApiClient apiClient,
+            @Nullable AccountIdentifier sourceIdentifier,
+            AccountIdentifier destinationIdentifier,
+            Amount amount,
+            String referenceText) throws TransferExecutionException;
+
+    void executeBankTransfer(
+            UkOpenBankingApiClient apiClient,
+            String intentId,
+            @Nullable AccountIdentifier sourceIdentifier,
+            AccountIdentifier destinationIdentifier,
+            Amount amount,
+            String referenceText
+    ) throws TransferExecutionException;
+}
