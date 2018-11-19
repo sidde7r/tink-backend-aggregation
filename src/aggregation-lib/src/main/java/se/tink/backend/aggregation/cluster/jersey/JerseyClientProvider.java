aadae09d894b936e6c1511f0cb84089f6c2bf9f3
evchen <minjia.chen@tink.se>
2018-11-19 08:18:18 +0100

    feat(JerseyClientProvider) get clientInfo from apikey
    - inject ClientConfigurationProvider
    - validate apiKey in request header
    - convert from ClientConfiguration -> ClientInfo

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/cluster/jersey/JerseyClientProvider.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/cluster/jersey/JerseyClientProvider.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.cluster.jersey;
 
+import com.google.inject.Inject;
 import com.sun.jersey.api.core.HttpContext;
 import com.sun.jersey.api.core.HttpRequestContext;
 import com.sun.jersey.core.spi.component.ComponentContext;
@@ -13,6 +14,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.cluster.annotations.ClientContext;
 import se.tink.backend.aggregation.cluster.identification.ClientInfo;
+import se.tink.backend.aggregation.storage.database.models.ClientConfiguration;
+import se.tink.backend.aggregation.storage.database.providers.ClientConfigurationProvider;
+
 import static se.tink.backend.aggregation.cluster.jersey.JerseyClusterInfoProvider.CLUSTER_ENVIRONMENT_HEADER;
 import static se.tink.backend.aggregation.cluster.jersey.JerseyClusterInfoProvider.CLUSTER_NAME_HEADER;
 
@@ -21,6 +25,12 @@ public class JerseyClientProvider extends AbstractHttpContextInjectable<ClientIn
     private static final String CLIENT_API_KEY_HEADER = "X-Tink-Client-Api-Key";
 
     private final Logger logger = LoggerFactory.getLogger(JerseyClientProvider.class);
+    private final ClientConfigurationProvider clientConfigurationProvider;
+
+    @Inject
+    public JerseyClientProvider(ClientConfigurationProvider clientConfigurationProvider) {
+        this.clientConfigurationProvider = clientConfigurationProvider;
+    }
 
     @Override
     public Injectable<ClientInfo> getInjectable(ComponentContext ic, ClientContext a, Type c) {
@@ -36,12 +46,27 @@ public class JerseyClientProvider extends AbstractHttpContextInjectable<ClientIn
     public ClientInfo getValue(HttpContext c) {
         HttpRequestContext request = c.getRequest();
         String apiKey = request.getHeaderValue(CLIENT_API_KEY_HEADER);
+
+        // check if apikey is in header
         if (Strings.isNullOrEmpty(apiKey)) {
             String name = request.getHeaderValue(CLUSTER_NAME_HEADER);
             String environment = request.getHeaderValue(CLUSTER_ENVIRONMENT_HEADER);
             logger.error("Received a missing api key for {} {} .", name, environment);
         }
 
-        return null ;
+        // check if apikey is in storage
+        if (!clientConfigurationProvider.isValidClientKey(apiKey)) {
+            logger.error("can not find api key {} in database.", apiKey);
+        }
+
+        return convertFromClientConfiguration(clientConfigurationProvider.getClientConfiguration(apiKey));
+    }
+
+    private ClientInfo convertFromClientConfiguration(ClientConfiguration clientConfiguration) {
+        String clientName = clientConfiguration.getClientName();
+        String clusterId = clientConfiguration.getClusterId();
+        String aggregatorId = clientConfiguration.getAggregatorId();
+
+        return ClientInfo.of(clientName, clusterId, aggregatorId);
     }
 }
