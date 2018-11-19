7b93f14def3d19830d1ed89d94389acc25c6c2ba
Jonas Renold <jonas.renold@tink.se>
2018-11-19 14:56:26 +0100

    fea(ukob/pis) transferExecutor and payment consent authenticator
    The UKOB PIS is built upon the OpenId authentication flow. Each version that implements PIS must implement two methods:
    - initiation (create intent id)
    - execution
    
    The intentId will be used in the openId flow to get the user's consent (same as intentId in AIS). The user can only consent 100% or non at all.
    
    The PIS flow supports IBAN and SortCode identifiers.

---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/pis/UkOpenBankingBankTransferExecutor.java
@@ -0,0 +1,153 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.pis;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.TransferExecutionException;
+import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
+import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.UkOpenBankingApiClient;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.UkOpenBankingPis;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.fetcher.UkOpenBankingAccountFetcher;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.ThirdPartyAppAuthenticationController;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.OpenIdAuthenticationController;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.OpenIdConstants;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.ProviderConfiguration;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.SoftwareStatement;
+import se.tink.backend.aggregation.nxgen.controllers.transfer.BankTransferExecutor;
+import se.tink.backend.aggregation.nxgen.controllers.utils.SupplementalInformationController;
+import se.tink.backend.aggregation.nxgen.core.account.TransactionalAccount;
+import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
+import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
+import se.tink.backend.aggregation.rpc.Credentials;
+import se.tink.backend.core.Amount;
+import se.tink.backend.core.transfer.SignableOperationStatuses;
+import se.tink.backend.core.transfer.Transfer;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.i18n.Catalog;
+
+public class UkOpenBankingBankTransferExecutor implements BankTransferExecutor {
+    private final Catalog catalog;
+    private final Credentials credentials;
+    private final UkOpenBankingApiClient apiClient;
+    private final SupplementalInformationController supplementalInformationController;
+    private final SoftwareStatement softwareStatement;
+    private final ProviderConfiguration providerConfiguration;
+    private final UkOpenBankingAccountFetcher<?, ?, TransactionalAccount> ukOpenBankingAccountFetcher;
+    private final UkOpenBankingPis ukOpenBankingPis;
+
+    public UkOpenBankingBankTransferExecutor(
+            Catalog catalog,
+            Credentials credentials,
+            SupplementalInformationController supplementalInformationController,
+            SoftwareStatement softwareStatement,
+            ProviderConfiguration providerConfiguration,
+            TinkHttpClient httpClient,
+            UkOpenBankingAccountFetcher<?, ?, TransactionalAccount> ukOpenBankingAccountFetcher,
+            UkOpenBankingPis ukOpenBankingPis) {
+        this.catalog = catalog;
+        this.credentials = credentials;
+        this.supplementalInformationController = supplementalInformationController;
+        this.softwareStatement = softwareStatement;
+        this.providerConfiguration = providerConfiguration;
+        this.ukOpenBankingAccountFetcher = ukOpenBankingAccountFetcher;
+        this.ukOpenBankingPis = ukOpenBankingPis;
+
+        this.apiClient = new UkOpenBankingApiClient(
+                httpClient,
+                softwareStatement,
+                providerConfiguration,
+                OpenIdConstants.ClientMode.PAYMENTS
+        );
+    }
+
+    private List<TransactionalAccount> getTransactionalAccounts() {
+        return ukOpenBankingAccountFetcher.fetchAccounts();
+    }
+
+    private boolean matchingAccount(TransactionalAccount account, AccountIdentifier accountIdentifier) {
+        return account.getIdentifiers().stream().anyMatch(identifier -> identifier.equals(accountIdentifier));
+    }
+
+    private boolean hasAccountIdentifier(AccountIdentifier accountIdentifier) {
+        List<TransactionalAccount> tinkAccounts = getTransactionalAccounts();
+
+        return tinkAccounts.stream()
+                .anyMatch(account -> matchingAccount(account, accountIdentifier));
+    }
+
+    private boolean isImmediateTransfer(Transfer transfer) {
+        return Objects.isNull(transfer.getDueDate());
+    }
+
+    @Override
+    public Optional<String> executeTransfer(Transfer transfer) throws TransferExecutionException {
+        if (!isImmediateTransfer(transfer)) {
+            return Optional.of(catalog.getString("Cannot schedule future transfers."));
+        }
+
+        AccountIdentifier sourceIdentifier = ukOpenBankingPis.canHaveSourceAccountSpecified() ?
+                transfer.getSource() : null;
+        AccountIdentifier destinationIdentifier = transfer.getDestination();
+        Amount amount = transfer.getAmount();
+        String referenceText = transfer.getDestinationMessage();
+
+        if (!hasAccountIdentifier(sourceIdentifier)) {
+            throw TransferExecutionException.builder(SignableOperationStatuses.FAILED)
+                    .setMessage(TransferExecutionException.EndUserMessage.INVALID_SOURCE.getKey().get())
+                    .build();
+        }
+
+        UkOpenBankingPaymentsAuthenticator paymentAuthenticator = new UkOpenBankingPaymentsAuthenticator(
+                apiClient,
+                softwareStatement,
+                providerConfiguration,
+                ukOpenBankingPis,
+                sourceIdentifier,
+                destinationIdentifier,
+                amount,
+                referenceText
+        );
+
+        // Do not use the real PersistentStorage because we don't want to overwrite the AIS auth token.
+        PersistentStorage dummyStorage = new PersistentStorage();
+
+        OpenIdAuthenticationController openIdAuthenticationController = new OpenIdAuthenticationController(
+                dummyStorage,
+                supplementalInformationController,
+                apiClient,
+                paymentAuthenticator
+        );
+
+        ThirdPartyAppAuthenticationController<String> thirdPartyAppAuthenticationController =
+                new ThirdPartyAppAuthenticationController<>(openIdAuthenticationController,
+                        supplementalInformationController
+                );
+
+        try {
+            thirdPartyAppAuthenticationController.authenticate(credentials);
+        } catch (AuthenticationException | AuthorizationException e) {
+            throw TransferExecutionException.builder(SignableOperationStatuses.FAILED)
+                    .setMessage("Authentication error.")
+                    .build();
+        }
+
+        String intentId = paymentAuthenticator.getIntentId()
+                .orElseThrow(() ->
+                        TransferExecutionException.builder(SignableOperationStatuses.FAILED)
+                                .setMessage("No intentId.")
+                                .build()
+                );
+
+        ukOpenBankingPis.executeBankTransfer(
+                apiClient,
+                intentId,
+                sourceIdentifier,
+                destinationIdentifier,
+                amount,
+                referenceText
+        );
+
+        return Optional.empty();
+    }
+}
