7f93e11020b87982fe68eeee0733bd1148cae13f
evchen <minjia.chen@tink.se>
2018-11-19 12:48:48 +0100

    fix(aggregatorInfoProvider) throw client not cluster not valid

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/storage/database/providers/AggregatorInfoProvider.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/storage/database/providers/AggregatorInfoProvider.java
@@ -3,6 +3,7 @@ package se.tink.backend.aggregation.storage.database.providers;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import se.tink.backend.aggregation.api.AggregatorInfo;
+import se.tink.backend.aggregation.cluster.exceptions.ClientNotValid;
 import se.tink.backend.aggregation.cluster.exceptions.ClusterNotValid;
 import se.tink.backend.aggregation.cluster.identification.ClusterInfo;
 import se.tink.backend.aggregation.storage.database.converter.AggregatorConverter;
@@ -26,9 +27,9 @@ public class AggregatorInfoProvider {
     }
 
     // using aggregatorId (UUID) to get aggregator in multi client environment
-    public AggregatorInfo createAggregatorInfoFor(String aggregatorId) throws ClusterNotValid {
+    public AggregatorInfo createAggregatorInfoFor(String aggregatorId) throws ClientNotValid {
         if (!aggregatorConfigurations.containsKey(aggregatorId)) {
-            throw new ClusterNotValid();
+            throw new ClientNotValid();
         }
         return AggregatorConverter.convert(aggregatorConfigurations.get(aggregatorId));
     }
