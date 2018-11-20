7790b962b8ea999e39d71650bac3a94e2f9f0204
Jonas Renold <jonas.renold@tink.se>
2018-11-20 11:01:04 +0100

    ref(ukob/transfdest) changed field name

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/fetcher/UkOpenBankingTransferDestinationFetcher.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/fetcher/UkOpenBankingTransferDestinationFetcher.java
@@ -21,7 +21,7 @@ import se.tink.libraries.account.identifiers.SortCodeIdentifier;
 public class UkOpenBankingTransferDestinationFetcher implements TransferDestinationFetcher {
 
     // Whitelist source account types. Basically to avoid displaying credit cards as transfer sources.
-    private static final ImmutableList<AccountTypes> SOURCE_ACCOUNT_TYPES = ImmutableList.<AccountTypes>builder()
+    private static final ImmutableList<AccountTypes> WHITELISTED_ACCOUNT_TYPES = ImmutableList.<AccountTypes>builder()
             .add(AccountTypes.CHECKING)
             .add(AccountTypes.SAVINGS)
             .build();
@@ -44,7 +44,7 @@ public class UkOpenBankingTransferDestinationFetcher implements TransferDestinat
 
     private List<? extends GeneralAccountEntity> getSourceAccounts(Collection<Account> accounts) {
         return accounts.stream()
-                .filter(a -> SOURCE_ACCOUNT_TYPES.contains(a.getType()))
+                .filter(a -> WHITELISTED_ACCOUNT_TYPES.contains(a.getType()))
                 .map(GeneralAccountEntityImpl::createFromCoreAccount)
                 .filter(Optional::isPresent)
                 .map(Optional::get)
