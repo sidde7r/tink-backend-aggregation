a06a84eb4ac8391154dfd5d3860d667329dc9ea2
evchen <minjia.chen@tink.se>
2018-11-20 14:53:56 +0100

    feat(HostConfiguration) add missing functions from callbackConfig

--- src/aggregationcontroller-api/src/main/java/se/tink/backend/aggregation/aggregationcontroller/v1/core/HostConfiguration.java
+++ src/aggregationcontroller-api/src/main/java/se/tink/backend/aggregation/aggregationcontroller/v1/core/HostConfiguration.java
@@ -1,6 +1,12 @@
 package se.tink.backend.aggregation.aggregationcontroller.v1.core;
 
+import com.google.common.base.Splitter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.tink.libraries.metrics.MetricId;
+
 import java.util.Base64;
+import java.util.List;
 
 public class HostConfiguration {
     private String clusterId;
@@ -9,6 +15,8 @@ public class HostConfiguration {
     private String base64encodedclientcert;
     private boolean disablerequestcompression;
 
+
+    private static final Logger log = LoggerFactory.getLogger(HostConfiguration.class);
     private static final Base64.Decoder BASE64_DECODER = Base64.getDecoder();
 
     public String getClusterId() {
@@ -53,5 +61,29 @@ public class HostConfiguration {
 
     public byte[] getClientCert() {
         return BASE64_DECODER.decode(base64encodedclientcert);
+    }
+
+
+    public static HostConfiguration createForTesting(String clusterId) {
+        HostConfiguration hostConfiguration = new HostConfiguration();
+
+        hostConfiguration.setClusterId(clusterId);
+
+        return hostConfiguration;
+    }
+
+    // TODO: We should do this some other way. This is a hack we can use for now.
+    public MetricId.MetricLabels metricLabels() {
+        List<String> splitClusterId = Splitter.on("-").splitToList(clusterId);
+
+        if (splitClusterId.size() != 2) {
+            // The clusterId should be of the format <cluster name>-<environment>, i.e. oxford-staging
+            log.warn("SplitClusterId did not have size of exactly 2. ClusterId: {}", clusterId);
+            return MetricId.MetricLabels.createEmpty();
+        }
+
+        return new MetricId.MetricLabels()
+                .add("request_cluster", splitClusterId.get(0))
+                .add("request_environment", splitClusterId.get(1));
     }
 }
