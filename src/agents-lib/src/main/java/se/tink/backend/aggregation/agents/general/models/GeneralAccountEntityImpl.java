8e0a2aaeae2747eac93655043c3ba877ec2ae6a1
Jonas Renold <jonas.renold@tink.se>
2018-11-20 10:21:41 +0100

    fea(generalaccountentity) construct a generalAccountEntity from a core account
    This is needed to represent a source account, in transfer destinations, from a core account. Could not find this functionality elsewhere.

---
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/general/models/GeneralAccountEntityImpl.java
@@ -0,0 +1,49 @@
+package se.tink.backend.aggregation.agents.general.models;
+
+import java.util.Optional;
+import org.assertj.core.util.Preconditions;
+import se.tink.backend.aggregation.rpc.Account;
+import se.tink.libraries.account.AccountIdentifier;
+
+public class GeneralAccountEntityImpl implements GeneralAccountEntity {
+    private static final String EMPTY_BANK_NAME = "";
+
+    private final String bankName;
+    private final String accountName;
+    private final AccountIdentifier accountIdentifier;
+
+    private GeneralAccountEntityImpl(String bankName, String accountName,
+            AccountIdentifier accountIdentifier) {
+        this.bankName = bankName;
+        this.accountName = accountName;
+        this.accountIdentifier = accountIdentifier;
+    }
+
+    public static Optional<GeneralAccountEntityImpl> createFromCoreAccount(Account coreAccount) {
+
+        Preconditions.checkNotNull(coreAccount, "Account cannot be null.");
+
+        Optional<AccountIdentifier> accountIdentifier = coreAccount.getIdentifiers().stream().findFirst();
+        if (!accountIdentifier.isPresent()) {
+            return Optional.empty();
+        }
+
+        String accountName = coreAccount.getName();
+        return Optional.of(new GeneralAccountEntityImpl(EMPTY_BANK_NAME, accountName, accountIdentifier.get()));
+    }
+
+    @Override
+    public AccountIdentifier generalGetAccountIdentifier() {
+        return accountIdentifier;
+    }
+
+    @Override
+    public String generalGetBank() {
+        return bankName;
+    }
+
+    @Override
+    public String generalGetName() {
+        return accountName;
+    }
+}
