69b975915e39e94ff06db57ead8b7cdb1a8dca11
Babis Kiosidis <babis.kiosidis@tink.se>
2018-11-21 15:00:39 +0100

    ref(imports): removed unused imports from crypto conf dao

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/storage/database/daos/CryptoConfigurationDao.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/storage/database/daos/CryptoConfigurationDao.java
@@ -2,29 +2,17 @@ package se.tink.backend.aggregation.storage.database.daos;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import javax.inject.Inject;
-import se.tink.backend.aggregation.cluster.identification.ClusterId;
 import se.tink.backend.aggregation.storage.database.models.CryptoConfiguration;
-import se.tink.backend.aggregation.storage.database.models.CryptoConfigurationId;
-import se.tink.backend.aggregation.storage.database.repositories.ClusterCryptoConfigurationRepository;
 import se.tink.backend.aggregation.storage.database.repositories.CryptoConfigurationsRepository;
 import se.tink.backend.aggregation.wrappers.CryptoWrapper;
-import se.tink.backend.core.ClusterCryptoConfiguration;
-import se.tink.backend.core.CryptoId;
 
 public class CryptoConfigurationDao {
-    private ClusterCryptoConfigurationRepository clusterCryptoConfigurationRepository;
     private CryptoConfigurationsRepository cryptoConfigurationsRepository;
 
     @Inject
-    CryptoConfigurationDao(ClusterCryptoConfigurationRepository clusterCryptoConfigurationRepository,
-            CryptoConfigurationsRepository cryptoConfigurationsRepository) {
-        this.clusterCryptoConfigurationRepository = clusterCryptoConfigurationRepository;
+    CryptoConfigurationDao(CryptoConfigurationsRepository cryptoConfigurationsRepository) {
         this.cryptoConfigurationsRepository = cryptoConfigurationsRepository;
     }
 
