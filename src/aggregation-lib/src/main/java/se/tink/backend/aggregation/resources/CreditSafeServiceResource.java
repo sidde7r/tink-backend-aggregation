7769edd704cc7c99e662b371c24852f22cbd23b2
Babis Kiosidis <babis.kiosidis@tink.se>
2018-11-21 12:07:28 +0100

    fea(creditsafe): removed clusterinfo from creditsafe service

--- src/aggregation-lib/src/main/java/se/tink/backend/aggregation/resources/CreditSafeServiceResource.java
+++ src/aggregation-lib/src/main/java/se/tink/backend/aggregation/resources/CreditSafeServiceResource.java
@@ -6,10 +6,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import se.tink.backend.aggregation.api.CreditSafeService;
 import se.tink.backend.aggregation.cluster.annotations.ClientContext;
-import se.tink.backend.aggregation.cluster.annotations.ClusterContext;
 import se.tink.backend.aggregation.cluster.identification.ClientInfo;
-import se.tink.backend.aggregation.cluster.identification.ClusterId;
-import se.tink.backend.aggregation.cluster.identification.ClusterInfo;
 import se.tink.backend.aggregation.configuration.AgentsServiceConfiguration;
 import se.tink.backend.idcontrol.creditsafe.consumermonitoring.ConsumerMonitoringWrapper;
 import se.tink.backend.idcontrol.creditsafe.consumermonitoring.api.AddMonitoredConsumerCreditSafeRequest;
@@ -39,9 +36,7 @@ public class CreditSafeServiceResource implements CreditSafeService {
 
     @Override
     public void removeConsumerMonitoring(RemoveMonitoredConsumerCreditSafeRequest request,
-            @ClusterContext ClusterInfo clusterInfo, @ClientContext ClientInfo clientInfo) {
-        validateCluster(clusterInfo, clientInfo);
-
+            @ClientContext ClientInfo clientInfo) {
         SocialSecurityNumber.Sweden socialSecurityNumber = new SocialSecurityNumber.Sweden(request.getPnr());
         if (!socialSecurityNumber.isValid()) {
             HttpResponseHelper.error(Status.BAD_REQUEST);
@@ -52,9 +47,7 @@ public class CreditSafeServiceResource implements CreditSafeService {
 
     @Override
     public Response addConsumerMonitoring(AddMonitoredConsumerCreditSafeRequest request,
-            @ClusterContext ClusterInfo clusterInfo, @ClientContext ClientInfo clientInfo) {
-        validateCluster(clusterInfo, clientInfo);
-
+            @ClientContext ClientInfo clientInfo) {
         SocialSecurityNumber.Sweden socialSecurityNumber = new SocialSecurityNumber.Sweden(request.getPnr());
         if (!socialSecurityNumber.isValid()) {
             HttpResponseHelper.error(Status.BAD_REQUEST);
@@ -65,42 +58,19 @@ public class CreditSafeServiceResource implements CreditSafeService {
     }
 
     @Override
-    public PortfolioListResponse listPortfolios(@ClusterContext ClusterInfo clusterInfo, @ClientContext ClientInfo clientInfo) {
-        validateCluster(clusterInfo, clientInfo);
-
+    public PortfolioListResponse listPortfolios(@ClientContext ClientInfo clientInfo) {
         return consumerMonitoringWrapper.listPortfolios();
     }
 
     @Override
     public PageableConsumerCreditSafeResponse listChangedConsumers(ChangedConsumerCreditSafeRequest request,
-            @ClusterContext ClusterInfo clusterInfo, @ClientContext ClientInfo clientInfo) {
-        validateCluster(clusterInfo, clientInfo);
-
+            @ClientContext ClientInfo clientInfo) {
         return consumerMonitoringWrapper.listChangedConsumers(request);
     }
 
     @Override
     public PageableConsumerCreditSafeResponse listMonitoredConsumers(PageableConsumerCreditSafeRequest request,
-            @ClusterContext ClusterInfo clusterInfo, @ClientContext ClientInfo clientInfo) {
-        validateCluster(clusterInfo, clientInfo);
-
+            @ClientContext ClientInfo clientInfo) {
         return consumerMonitoringWrapper.listMonitoredConsumers(request);
-    }
-
-    private static void validateCluster(ClusterInfo clusterInfo,
-            ClientInfo clientInfo) {
-        // TODO: add clientInfo validation
-        // TODO: remove clusterInfo
-        ClusterId clusterId = clusterInfo.getClusterId();
-
-        if (!clusterId.isValidId()) {
-            HttpResponseHelper.error(Status.BAD_REQUEST);
-        }
-
-        if (VALID_CLUSTERS.contains(clusterId.getId())) {
-            return;
-        }
-
-        HttpResponseHelper.error(Status.BAD_REQUEST);
     }
 }
