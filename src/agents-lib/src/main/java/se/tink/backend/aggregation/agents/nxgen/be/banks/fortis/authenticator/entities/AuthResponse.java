e9f32b5487caf58dc74de40218037dda94610a91
Alexander Jamm <alexander.jamm@tink.se>
2018-11-28 12:24:14 +0100

    fix(Fortis): now handling UnsupportedEncodingException

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/fortis/authenticator/entities/AuthResponse.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/fortis/authenticator/entities/AuthResponse.java
@@ -19,36 +19,32 @@ public class AuthResponse {
     }
 
     public String getUrlEncodedFormat() {
-        StringBuilder xmlBuilder = new StringBuilder();
-        xmlBuilder.append("<DIST_ID>");
-        xmlBuilder.append(distId);
-        xmlBuilder.append("</DIST_ID><AUTH_PROC_ID>");
-        xmlBuilder.append(authProcId);
-        xmlBuilder
-                .append("</AUTH_PROC_ID><MEAN_ID>UCR</MEAN_ID><EAI_AUTH_TYPE>UCR</EAI_AUTH_TYPE><EBANKING_USER_ID><PERS_ID>12345678910000007</PERS_ID><SMID>");
-        xmlBuilder.append(smid);
-        xmlBuilder.append("</SMID><AGRE_ID>");
-        xmlBuilder.append(agreementId);
-        xmlBuilder
-                .append("</AGRE_ID></EBANKING_USER_ID><EBANKING_USER_AUTHENTICITY_VALIDATION><VALIDATION_DATE></VALIDATION_DATE><VALID></VALID><AUTHENTICATION_MEAN_ID>");
-        xmlBuilder.append(authenticationMeanId);
-        xmlBuilder
-                .append("</AUTHENTICATION_MEAN_ID></EBANKING_USER_AUTHENTICITY_VALIDATION><CHALLENGE_RESPONSE><VALUE>");
-        xmlBuilder.append(response);
-        xmlBuilder.append("</VALUE><CHALLENGE>");
-        xmlBuilder.append(challenge);
-        xmlBuilder.append("</CHALLENGE><AUTH_FACTOR_ID>");
-        xmlBuilder.append(cardNumber);
-        xmlBuilder.append("</AUTH_FACTOR_ID>");
-        xmlBuilder.append("</CHALLENGE_RESPONSE>");
-        xmlBuilder.append("<DEVICE_ID>");
-        xmlBuilder.append("<FINGER_PRINT>" + deviceFingerprint + "</FINGER_PRINT>");
-        xmlBuilder.append("<NAME>iPhone</NAME>");
-        xmlBuilder.append("</DEVICE_ID>");
-        String right = xmlBuilder.toString();
+        String right = "<DIST_ID>"
+                + distId
+                + "</DIST_ID><AUTH_PROC_ID>"
+                + authProcId
+                + "</AUTH_PROC_ID><MEAN_ID>UCR</MEAN_ID><EAI_AUTH_TYPE>UCR</EAI_AUTH_TYPE><EBANKING_USER_ID><PERS_ID>12345678910000007</PERS_ID><SMID>"
+                + smid
+                + "</SMID><AGRE_ID>"
+                + agreementId
+                + "</AGRE_ID></EBANKING_USER_ID><EBANKING_USER_AUTHENTICITY_VALIDATION><VALIDATION_DATE></VALIDATION_DATE><VALID></VALID><AUTHENTICATION_MEAN_ID>"
+                + authenticationMeanId
+                + "</AUTHENTICATION_MEAN_ID></EBANKING_USER_AUTHENTICITY_VALIDATION><CHALLENGE_RESPONSE><VALUE>"
+                + response
+                + "</VALUE><CHALLENGE>"
+                + challenge
+                + "</CHALLENGE><AUTH_FACTOR_ID>"
+                + cardNumber
+                + "</AUTH_FACTOR_ID>"
+                + "</CHALLENGE_RESPONSE>"
+                + "<DEVICE_ID>"
+                + "<FINGER_PRINT>" + deviceFingerprint + "</FINGER_PRINT>"
+                + "<NAME>iPhone</NAME>"
+                + "</DEVICE_ID>";
         try {
             right = URLEncoder.encode(URLEncoder.encode(right, "UTF-8"), "UTF-8");
         } catch (UnsupportedEncodingException e) {
+            throw new IllegalStateException("Unable to encode: " + e.toString());
         }
         return "AUTH=" + right;
     }
