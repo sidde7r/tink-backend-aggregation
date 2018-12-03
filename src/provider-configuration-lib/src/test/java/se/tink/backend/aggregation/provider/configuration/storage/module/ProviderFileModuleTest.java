c34513cff71ff1ef45ef40da3ba46442e5bc010b
Lars Almgren <lars.almgren@tink.se>
2018-12-03 13:27:06 +0100

    ref(capabilities) remove unused import

--- src/provider-configuration-lib/src/test/java/se/tink/backend/aggregation/provider/configuration/storage/module/ProviderFileModuleTest.java
+++ src/provider-configuration-lib/src/test/java/se/tink/backend/aggregation/provider/configuration/storage/module/ProviderFileModuleTest.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import se.tink.backend.aggregation.provider.configuration.storage.module.clusterprovider.AgentCapabilitiesMapModel;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProviderFileModuleTest {
