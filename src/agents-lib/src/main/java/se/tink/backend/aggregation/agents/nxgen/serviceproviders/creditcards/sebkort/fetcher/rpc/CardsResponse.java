45c8ce8def283e6c5bf262f662e615851c90c875
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2018-12-04 14:39:10 +0100

    refactor(SEBKort): Use Maps.newHashMap() instead of new HashMap<>()

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/sebkort/fetcher/rpc/CardsResponse.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/sebkort/fetcher/rpc/CardsResponse.java
@@ -1,11 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.sebkort.fetcher.rpc;
 
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import org.assertj.core.util.Maps;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.sebkort.fetcher.entity.CardAccountEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.sebkort.fetcher.entity.CardContractEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.sebkort.fetcher.entity.UserEntity;
@@ -31,7 +31,7 @@ public class CardsResponse {
                         .stream()
                         .collect(Collectors.toMap(CardAccountEntity::getId, account -> account));
 
-        return Optional.of(cardAccountsHashMap).orElse(new HashMap<>());
+        return Optional.of(cardAccountsHashMap).orElse(Maps.newHashMap());
     }
 
     public UserEntity getUser() {
