7c71632ded6336c14798a15aa5c1068a61dc68e2
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2018-12-04 15:39:51 +0100

    test(Avanza): Split up into separate test cases

--- src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/brokers/avanza/AvanzaV2AccountTypeMappersTest.java
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/brokers/avanza/AvanzaV2AccountTypeMappersTest.java
@@ -7,7 +7,7 @@ import se.tink.backend.aggregation.rpc.AccountTypes;
 
 public final class AvanzaV2AccountTypeMappersTest {
     @Test
-    public void testInferAccountType() {
+    public void testInferExactAccountType() {
         AvanzaV2AccountTypeMappers mappers = new AvanzaV2AccountTypeMappers();
 
         // No warning -- uses product code account type mapper
@@ -17,12 +17,22 @@ public final class AvanzaV2AccountTypeMappersTest {
         Assert.assertEquals(
                 Optional.of(AccountTypes.INVESTMENT),
                 mappers.inferAccountType("investeringssparkonto"));
+    }
+
+    @Test
+    public void testInferFallbackAccountType() {
+        AvanzaV2AccountTypeMappers mappers = new AvanzaV2AccountTypeMappers();
 
         // Warns about using predicate fallback
         Assert.assertEquals(Optional.of(AccountTypes.PENSION), mappers.inferAccountType("pension"));
         Assert.assertEquals(
                 Optional.of(AccountTypes.SAVINGS), mappers.inferAccountType("sparkonto"));
         Assert.assertEquals(Optional.of(AccountTypes.LOAN), mappers.inferAccountType("kredit"));
+    }
+
+    @Test
+    public void testInferMissingAccountType() {
+        AvanzaV2AccountTypeMappers mappers = new AvanzaV2AccountTypeMappers();
 
         // Warns about being unable to figure out the type
         Assert.assertEquals(Optional.empty(), mappers.inferAccountType("försäkring"));
