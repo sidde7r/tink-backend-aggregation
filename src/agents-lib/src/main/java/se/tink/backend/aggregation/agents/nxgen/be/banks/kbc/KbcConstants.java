bd35cf671db17fec75ce99aca8e9011d22b869b3
Xiaoqiang Chen <xiaoqiang.chen@tink.se>
2018-12-07 14:49:49 +0100

    fea(kbc-be) upgrade version to 18.10

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/KbcConstants.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/KbcConstants.java
@@ -69,7 +69,7 @@ public class KbcConstants {
         }
 
         public static final String HOST = "https://mobile.kbc-group.com";
-        public static final String VERSION = "1808";
+        public static final String VERSION = "1810";
 
         private static String createUrlWithHost(String uri) {
             return HOST + uri;
@@ -87,7 +87,7 @@ public class KbcConstants {
 
     public static class RequestInput {
         public static final String APP_FAMILY = "PHNIOSV1";
-        public static final String VERSION_NUMBER = "18.9.0";
+        public static final String VERSION_NUMBER = "18.10.1";
         public static final String APPLICATION_ID = "A031";
         public static final String COMPANY_ID = "0001";
 
