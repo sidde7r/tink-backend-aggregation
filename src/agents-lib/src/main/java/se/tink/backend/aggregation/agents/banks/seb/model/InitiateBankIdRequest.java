adb4770ef9eea57a792c441e78f9b17fdc17b98b
Gustaf Svanerud <gustaf.svanerud@tink.se>
2018-12-12 11:45:33 +0100

    fea(seb/auth) Updated SEB agent to use autostart token
    The wer notified that SEB will stop supporting ssn based BankId login
    in favour of `autostarttoken`.
    
    This commit updates our SEB agent to reflect this.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/banks/seb/model/InitiateBankIdRequest.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/banks/seb/model/InitiateBankIdRequest.java
@@ -5,23 +5,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class InitiateBankIdRequest {
-    private String uid;
+
     @JsonProperty("SEB_Referer")
     private String sebReferer;
 
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getSebReferer() {
-        return sebReferer;
-    }
-
-    public void setSebReferer(String sebReferer) {
+    public InitiateBankIdRequest(String sebReferer) {
         this.sebReferer = sebReferer;
     }
 }
