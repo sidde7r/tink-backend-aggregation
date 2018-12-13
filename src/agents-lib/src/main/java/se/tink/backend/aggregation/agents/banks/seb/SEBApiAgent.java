f0fdb5731cee7c75ce9bfc9242643645c9612550
Gustaf Svanerud <gustaf.svanerud@tink.se>
2018-12-13 09:45:27 +0100

    Revert "fea(seb/auth) Updated SEB agent to use autostart token"
    This reverts commit adb4770ef9eea57a792c441e78f9b17fdc17b98b.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/banks/seb/SEBApiAgent.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/banks/seb/SEBApiAgent.java
@@ -603,10 +603,13 @@ public class SEBApiAgent extends AbstractAgent implements RefreshableItemExecuto
     }
 
     private void loginWithBankId() throws BankIdException {
+        InitiateBankIdRequest initiateBankIdRequest = new InitiateBankIdRequest();
+        initiateBankIdRequest.setUid(credentials.getField(Field.Key.USERNAME));
+        initiateBankIdRequest.setSebReferer(SEB_REFERER_BANKID_LOGIN);
         InitiateBankIdResponse initiateBankIdResponse = resource(INITIATE_BANKID_URL)
                 .type(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .post(InitiateBankIdResponse.class, new InitiateBankIdRequest(SEB_REFERER_BANKID_LOGIN));
+                .post(InitiateBankIdResponse.class, initiateBankIdRequest);
 
 
         final String rfa = initiateBankIdResponse.getRfa().toLowerCase();
@@ -629,7 +632,7 @@ public class SEBApiAgent extends AbstractAgent implements RefreshableItemExecuto
                             rfa, status, initiateBankIdResponse.getMessage()));
         }
 
-        context.openBankId(initiateBankIdResponse.getAutostartToken(), false);
+        context.openBankId(null, false);
 
         collectBankId(initiateBankIdResponse);
     }
