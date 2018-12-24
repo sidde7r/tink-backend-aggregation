4ad38b3bde9ef7407360088fa21ac1e612a5bcfd
Sebastian Olsson <sebastian.olsson@tink.se>
2018-12-24 08:43:05 +0100

    fix(alandsbanken): Forced appVersion 1.4.0 in all requests
    The bank appears to have dropped support of 1.3.2
    entirely since every request is responded with an
    appeal to update to 1.4.0.

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/banks/se/alandsbanken/AlandsBankenAgent.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/banks/se/alandsbanken/AlandsBankenAgent.java
@@ -2,9 +2,8 @@ package se.tink.backend.aggregation.agents.banks.se.alandsbanken;
 
 import se.tink.backend.aggregation.agents.AgentContext;
 import se.tink.backend.aggregation.agents.banks.crosskey.CrossKeyAgent;
-import se.tink.backend.aggregation.rpc.CredentialsRequest;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
-import se.tink.backend.core.enums.FeatureFlags;
+import se.tink.backend.aggregation.rpc.CredentialsRequest;
 
 public class AlandsBankenAgent extends CrossKeyAgent {
 
@@ -15,11 +14,6 @@ public class AlandsBankenAgent extends CrossKeyAgent {
         apiClient.setErrorHandler(new AlandsBankenErrorHandler());
         apiClient.setLanguage("sv");
 
-        if (request.getUser().getFlags().contains(FeatureFlags.ALANDSBANKEN_SE_V4)) {
-            apiClient.setAppId("1.4.0-iOS");
-        } else {
-            apiClient.setAppId("1.3.2-iOS");
-        }
-
+        apiClient.setAppId("1.4.0-iOS");
     }
 }
