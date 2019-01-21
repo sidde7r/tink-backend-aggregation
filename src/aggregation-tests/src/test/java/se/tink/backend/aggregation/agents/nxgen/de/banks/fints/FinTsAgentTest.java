248517a12dffe73cbc56ba59c3aee62636f82510
Mattias Hird <mattias.hird@tink.se>
2019-01-21 14:45:02 +0100

    fix(fints): Use argument helper for agent test

--- src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/FinTsAgentTest.java
+++ src/aggregation-tests/src/test/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/FinTsAgentTest.java
@@ -1,19 +1,37 @@
 package se.tink.backend.aggregation.agents.nxgen.de.banks.fints;
 
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
-import se.tink.backend.aggregation.rpc.CredentialsTypes;
+import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
 import se.tink.backend.aggregation.rpc.Field;
 
-@Ignore
+//@Ignore
 public class FinTsAgentTest {
+
+    ArgumentHelper helper = new ArgumentHelper(
+            "tink.username",
+            "tink.password",
+            "tink.market",
+            "tink.provider");
+
+    @Before
+    public void before() {
+        helper.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentHelper.afterClass();
+    }
 
     @Test
     public void refresh() throws Exception {
-        new AgentIntegrationTest.Builder("", "")
-                .addCredentialField(Field.Key.USERNAME, "")
-                .addCredentialField(Field.Key.PASSWORD, "")
+        new AgentIntegrationTest.Builder(helper.get("tink.market"), helper.get("tink.provider"))
+                .addCredentialField(Field.Key.USERNAME, helper.get("tink.username"))
+                .addCredentialField(Field.Key.PASSWORD, helper.get("tink.password"))
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false)
                 .build()
