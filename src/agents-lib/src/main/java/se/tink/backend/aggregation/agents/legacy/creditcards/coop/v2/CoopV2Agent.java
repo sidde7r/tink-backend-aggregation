0ea4c24d5140b49551daef7d630897c964509bed
Alexander Jamm <alexander.jamm@tink.se>
2019-01-28 15:35:00 +0100

    refactor(CoopV2): Coop refresh refactor

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/legacy/creditcards/coop/v2/CoopV2Agent.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/legacy/creditcards/coop/v2/CoopV2Agent.java
@@ -3,22 +3,24 @@ package se.tink.backend.aggregation.agents.creditcards.coop.v2;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableSortedSet;
 import com.google.common.collect.Iterables;
+import java.util.HashMap;
 import java.util.List;
-import se.tink.backend.aggregation.agents.AbstractAgent;
-import se.tink.backend.aggregation.agents.AgentContext;
-import se.tink.backend.aggregation.agents.RefreshableItemExecutor;
-import se.tink.backend.aggregation.agents.creditcards.coop.v2.model.AccountEntity;
-import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
-import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
+import java.util.Map;
+import java.util.stream.Collectors;
 import se.tink.backend.agents.rpc.Account;
 import se.tink.backend.agents.rpc.Credentials;
-import se.tink.libraries.credentials.service.CredentialsRequest;
-import se.tink.libraries.credentials.service.RefreshableItem;
-import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.backend.aggregation.agents.AbstractAgent;
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
+import se.tink.backend.aggregation.agents.RefreshCreditCardAccountsExecutor;
+import se.tink.backend.aggregation.agents.creditcards.coop.v2.model.AccountEntity;
 import se.tink.backend.aggregation.agents.models.Transaction;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.libraries.credentials.service.CredentialsRequest;
 import se.tink.libraries.strings.StringUtils;
 
-public class CoopV2Agent extends AbstractAgent implements RefreshableItemExecutor {
+public class CoopV2Agent extends AbstractAgent implements RefreshCreditCardAccountsExecutor {
     private static ImmutableSortedSet<Integer> TRANSACTION_PAGE_SIZES = ImmutableSortedSet
             .<Integer>naturalOrder()
             .add(200)
@@ -36,70 +38,6 @@ public class CoopV2Agent extends AbstractAgent implements RefreshableItemExecuto
                 clientFactory.createBasicClient(context.getLogOutputStream()),
                 request.getCredentials(),
                 context.getMetricRegistry());
-    }
-
-    @Override
-    public boolean login() throws AuthenticationException, AuthorizationException {
-        return apiClient.loginWithPassword();
-    }
-
-    @Override
-    public void logout() throws Exception {
-
-    }
-
-    private List<AccountEntity> getAccounts() {
-        if (accounts != null) {
-            return accounts;
-        }
-
-        accounts = apiClient.getAccounts();
-        return accounts;
-    }
-
-    /**
-     * The transactions in the app API are not bound to any specific account, but rather
-     * to an account type. Though, users can have multiple accounts that map to the same
-     * account type: Type 7 and 9 are both named MedMera Efter, of which we think one of them are
-     * previous Visa card and the other is a regular MedMera. Since V2 we now start to separate these to two accounts
-     * instead of putting them together.
-     *
-     * The user probably cannot have two accounts on the same type since it's not possible to separate them in the API.
-     */
-    @Override
-    public void refresh(RefreshableItem item) {
-        switch (item) {
-        case CREDITCARD_ACCOUNTS:
-            getAccounts().forEach(accountEntity -> financialDataCacher.cacheAccount(parseAccount(accountEntity)));
-            break;
-        case CREDITCARD_TRANSACTIONS:
-            getAccounts().forEach(accountEntity -> {
-                Account account = parseAccount(accountEntity);
-                financialDataCacher.updateTransactions(account, getTransactions(account, accountEntity));
-            });
-            break;
-        }
-    }
-
-    private Account parseAccount(AccountEntity accountEntity) {
-        Account account = accountEntity.toAccount();
-        // TODO: Start using accountNumber as bankId (stop overriding it)
-        account.setBankId(hashLegacyBankId(accountEntity.getAccountTypeEnum()));
-
-        if (Strings.isNullOrEmpty(account.getAccountNumber())) {
-            // Before we start using accountNumber as bankId, we need to ensure that it's always present
-            log.warn("Found account without account number");
-        }
-
-        return account;
-    }
-
-    /**
-     * Preferrably we'd use the account number instead, but that'll require merging of data since V1 version of Coop
-     * used same kind of logic for hashing the BankId.
-     */
-    private String hashLegacyBankId(AccountType accountType) {
-        return StringUtils.hashAsStringMD5(credentials.getId() + accountType.getLegacyBankIdPart());
     }
 
     /**
@@ -131,5 +69,73 @@ public class CoopV2Agent extends AbstractAgent implements RefreshableItemExecuto
 
         // If we were not content with the refresh, return the last result
         return transactions;
+    }
+
+    private List<AccountEntity> getAccounts() {
+        if (accounts != null) {
+            return accounts;
+        }
+
+        accounts = apiClient.getAccounts();
+        return accounts;
+    }
+
+    /**
+     * Preferrably we'd use the account number instead, but that'll require merging of data since V1 version of Coop
+     * used same kind of logic for hashing the BankId.
+     */
+    private String hashLegacyBankId(AccountType accountType) {
+        return StringUtils.hashAsStringMD5(credentials.getId() + accountType.getLegacyBankIdPart());
+    }
+
+    private Account parseAccount(AccountEntity accountEntity) {
+        Account account = accountEntity.toAccount();
+        // TODO: Start using accountNumber as bankId (stop overriding it)
+        account.setBankId(hashLegacyBankId(accountEntity.getAccountTypeEnum()));
+
+        if (Strings.isNullOrEmpty(account.getAccountNumber())) {
+            // Before we start using accountNumber as bankId, we need to ensure that it's always present
+            log.warn("Found account without account number");
+        }
+
+        return account;
+    }
+
+    @Override
+    public boolean login() throws Exception {
+        return apiClient.loginWithPassword();
+    }
+
+    @Override
+    public void logout() throws Exception {
+
+    }
+
+    /**
+     * The transactions in the app API are not bound to any specific account, but rather
+     * to an account type. Though, users can have multiple accounts that map to the same
+     * account type: Type 7 and 9 are both named MedMera Efter, of which we think one of them are
+     * previous Visa card and the other is a regular MedMera. Since V2 we now start to separate these to two accounts
+     * instead of putting them together.
+     *
+     * The user probably cannot have two accounts on the same type since it's not possible to separate them in the API.
+     */
+    @Override
+    public FetchAccountsResponse fetchCreditCardAccounts() {
+        return new FetchAccountsResponse(getAccounts().stream()
+                .map(x -> parseAccount(x))
+                .collect(Collectors.toList()));
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchCreditCardTransactions() {
+        Map<Account, List<Transaction>> result = new HashMap<>();
+
+        for(AccountEntity entity : getAccounts()) {
+            Account account = entity.toAccount();
+            List<Transaction> transactions = getTransactions(account, entity);
+            result.put(account, transactions);
+        }
+        return new FetchTransactionsResponse(result);
     }
 }
