9358655e94a636164664e72340e671c64e9dd2a4
Alexander Jamm <alexander.jamm@tink.se>
2019-01-28 13:05:58 +0100

    refactor(Nordnet): Nordnet refresh refactor

--- src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/legacy/brokers/NordnetAgent.java
+++ src/agents-lib/src/main/java/se/tink/backend/aggregation/agents/legacy/brokers/NordnetAgent.java
@@ -1,26 +1,33 @@
-package se.tink.backend.aggregation.agents.brokers;
+package se.tink.backend.aggregation.agents.legacy.brokers;
 
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.Uninterruptibles;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+import se.tink.backend.agents.rpc.Account;
+import se.tink.backend.agents.rpc.AccountTypes;
 import se.tink.backend.agents.rpc.Credentials;
 import se.tink.backend.agents.rpc.CredentialsTypes;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.AbstractAgent;
 import se.tink.backend.aggregation.agents.AgentContext;
 import se.tink.backend.aggregation.agents.BankIdStatus;
-import se.tink.backend.aggregation.agents.RefreshableItemExecutor;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchInvestmentAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
+import se.tink.backend.aggregation.agents.RefreshInvestmentAccountsExecutor;
+import se.tink.backend.aggregation.agents.RefreshSavingsAccountsExecutor;
 import se.tink.backend.aggregation.agents.brokers.nordnet.NordnetApiClient;
 import se.tink.backend.aggregation.agents.brokers.nordnet.model.PositionEntity;
 import se.tink.backend.aggregation.agents.brokers.nordnet.model.PositionsResponse;
 import se.tink.backend.aggregation.agents.brokers.nordnet.model.Response.AccountResponse;
-import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
-import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
 import se.tink.backend.aggregation.agents.exceptions.BankIdException;
 import se.tink.backend.aggregation.agents.exceptions.LoginException;
 import se.tink.backend.aggregation.agents.exceptions.errors.BankIdError;
@@ -29,12 +36,10 @@ import se.tink.backend.aggregation.agents.models.AccountFeatures;
 import se.tink.backend.aggregation.agents.models.Instrument;
 import se.tink.backend.aggregation.agents.models.Portfolio;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
-import se.tink.backend.agents.rpc.Account;
-import se.tink.backend.agents.rpc.AccountTypes;
 import se.tink.libraries.credentials.service.CredentialsRequest;
-import se.tink.libraries.credentials.service.RefreshableItem;
 
-public class NordnetAgent extends AbstractAgent implements RefreshableItemExecutor {
+public class NordnetAgent extends AbstractAgent implements RefreshSavingsAccountsExecutor,
+        RefreshInvestmentAccountsExecutor {
     private static final int MAX_ATTEMPTS = 60;
 
     private final Credentials credentials;
@@ -51,120 +56,17 @@ public class NordnetAgent extends AbstractAgent implements RefreshableItemExecut
                 NordnetApiClient.REDIRECT_STRATEGY), DEFAULT_USER_AGENT);
     }
 
-    private void refreshSavingsAccounts(AccountResponse accountEntities) {
-        accountEntities.forEach(accountEntity -> {
-
-            // A sparkonto don't hold instruments
-            if (!"sparkonto".equalsIgnoreCase(accountEntity.getAccountCode())) {
-                return;
-            }
-            // accounts can be blocked, will have no further data
-            if (accountEntity.isBlocked()) {
-                log.debug(String.format("Account: %s blocked, reason: %s ",
-                        accountEntity.getAccountNumber(), accountEntity.getBlockedReason()));
-                return;
-            }
-
-            financialDataCacher.cacheAccount(accountEntity.toAccount(AccountTypes.SAVINGS));
-        });
-    }
-
-    private void refreshInvestmentAccounts(AccountResponse accountEntities) {
-        accountEntities.forEach(accountEntity -> {
-
-            // A sparkonto don't hold instruments
-            if (Objects.equals("sparkonto", Strings.nullToEmpty(accountEntity.getAccountCode()).toLowerCase())) {
-                return;
-            }
-            // accounts can be blocked, will have no further data
-            if (accountEntity.isBlocked()) {
-                log.debug(String.format("Account: %s blocked, reason: %s ",
-                        accountEntity.getAccountNumber(), accountEntity.getBlockedReason()));
-                return;
-            }
-
-            Account account = accountEntity.toAccount(AccountTypes.INVESTMENT);
-            Portfolio portfolio = accountEntity.toPortfolio();
-
-            // Contains all positions, regardless of account/portfolio
-            Optional<PositionsResponse> positions = apiClient.getPositions();
-
-            if (!positions.isPresent()) {
-                financialDataCacher.cacheAccount(account, AccountFeatures.createForPortfolios(portfolio));
-                return;
-            }
-
-            List<Instrument> instruments = Lists.newArrayList();
-            for (PositionEntity positionEntity : positions.get()) {
-                if (positionEntity.getAccountId().equalsIgnoreCase(accountEntity.getAccountId())) {
-                    // If, this position actually belongs to this account/portfolio
-                    positionEntity.toInstrument().ifPresent(instruments::add);
-                }
-            }
-            portfolio.setInstruments(instruments);
-
-            financialDataCacher.cacheAccount(account, AccountFeatures.createForPortfolios(portfolio));
-        });
-    }
-
-    private AccountResponse getAccounts() {
-
-        if (accounts != null) {
-            return accounts;
-        }
-
-        accounts = apiClient.fetchAccounts();
-        return accounts;
-    }
-
-    @Override
-    public void refresh(RefreshableItem item) {
-        switch (item) {
-        case SAVING_ACCOUNTS:
-            refreshSavingsAccounts(getAccounts());
-            break;
-
-        case SAVING_TRANSACTIONS:
-            // NOTE:
-            // Last commit SHA related to this agent with transaction aggregation: feb1e91f9028a0a76e997b12146e2e4f1867e6d2
-            // Though, since there are some transactions that are sent in other currencies we cannot get a complete view of
-            // the account transactions and we thus removed the aggregation of them since they turned up in search and ID-koll.
-            // If we want to reenable it we should investigate if amount is returned in SEK for all transactions in their
-            // newer API that their app is using.
-            break;
-
-        case INVESTMENT_ACCOUNTS:
-            refreshInvestmentAccounts(getAccounts());
-            break;
-        }
-    }
-
-    @Override
-    public boolean login() throws AuthenticationException, AuthorizationException {
-        resetToMobileBankIdIfNeeded();
-
-        switch (credentials.getType()) {
-        case PASSWORD:
-            return loginWithPassword();
-        case MOBILE_BANKID:
-            return loginWithBankID();
-        default:
-            throw new IllegalStateException(String.format(
-                    "Unsupported credentials type(%s)", credentials.getType()));
-        }
-    }
-
-    /**
-     * We've previously for BANKID and PASSWORD stored an access token for persistent login. Though it doesn't work
-     * anymore, and it also wasn't a long-term access token so gave not much value.
-     * <p>
-     * Probably a keepAlive would work if we persisted cookies, but at this point there is just a small value of it.
-     * <p>
-     * For legacy reason this method is needed. We used to change a credential from BANKID to PASSWORD if access token
-     * was present (as a kind of hack of not needing two factor on next login). Thus we need to check if we're password
-     * and double check that the credentials has no password --> originally BANKID credential that should be reset so
-     * that we can log it in again.
-     */
+/**
+ * We've previously for BANKID and PASSWORD stored an access token for persistent login. Though it doesn't work
+ * anymore, and it also wasn't a long-term access token so gave not much value.
+ * <p>
+ * Probably a keepAlive would work if we persisted cookies, but at this point there is just a small value of it.
+ * <p>
+ * For legacy reason this method is needed. We used to change a credential from BANKID to PASSWORD if access token
+ * was present (as a kind of hack of not needing two factor on next login). Thus we need to check if we're password
+ * and double check that the credentials has no password --> originally BANKID credential that should be reset so
+ * that we can log it in again.
+ */
     private void resetToMobileBankIdIfNeeded() {
         switch (credentials.getType()) {
         case PASSWORD:
@@ -184,19 +86,6 @@ public class NordnetAgent extends AbstractAgent implements RefreshableItemExecut
                 credentials.getField(Field.Key.PASSWORD));
 
         Preconditions.checkState(accessToken.isPresent());
-
-        return true;
-    }
-
-    private boolean loginWithBankID() throws BankIdException, LoginException {
-        String orderRef = apiClient.initBankID(credentials.getField(Field.Key.USERNAME));
-        openBankID();
-
-        Optional<String> accessToken = collectBankID(orderRef);
-
-        if (!accessToken.isPresent()) {
-            throw LoginError.INCORRECT_CREDENTIALS.exception();
-        }
 
         return true;
     }
@@ -225,8 +114,124 @@ public class NordnetAgent extends AbstractAgent implements RefreshableItemExecut
         throw BankIdError.TIMEOUT.exception();
     }
 
+    private boolean loginWithBankID() throws BankIdException, LoginException {
+        String orderRef = apiClient.initBankID(credentials.getField(Field.Key.USERNAME));
+        openBankID();
+
+        Optional<String> accessToken = collectBankID(orderRef);
+
+        if (!accessToken.isPresent()) {
+            throw LoginError.INCORRECT_CREDENTIALS.exception();
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean login() throws Exception {
+        resetToMobileBankIdIfNeeded();
+
+        switch (credentials.getType()) {
+        case PASSWORD:
+            return loginWithPassword();
+        case MOBILE_BANKID:
+            return loginWithBankID();
+        default:
+            throw new IllegalStateException(String.format(
+                    "Unsupported credentials type(%s)", credentials.getType()));
+        }
+    }
+
     @Override
     public void logout() throws Exception {
         // NOP
+    }
+
+    private AccountResponse getAccounts() {
+
+        if (accounts != null) {
+            return accounts;
+        }
+
+        accounts = apiClient.fetchAccounts();
+        return accounts;
+    }
+
+    @Override
+    public FetchAccountsResponse fetchSavingsAccounts() {
+        List<Account> accounts = new ArrayList<Account>();
+        getAccounts().forEach(accountEntity -> {
+
+            // A sparkonto don't hold instruments
+            if (!"sparkonto".equalsIgnoreCase(accountEntity.getAccountCode())) {
+                return;
+            }
+            // accounts can be blocked, will have no further data
+            if (accountEntity.isBlocked()) {
+                log.debug(String.format("Account: %s blocked, reason: %s ",
+                        accountEntity.getAccountNumber(), accountEntity.getBlockedReason()));
+                return;
+            }
+            accounts.add(accountEntity.toAccount(AccountTypes.SAVINGS));
+        });
+        return new FetchAccountsResponse(accounts);
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchSavingsTransactions() {
+        // NOTE:
+        // Last commit SHA related to this agent with transaction aggregation: feb1e91f9028a0a76e997b12146e2e4f1867e6d2
+        // Though, since there are some transactions that are sent in other currencies we cannot get a complete view of
+        // the account transactions and we thus removed the aggregation of them since they turned up in search and ID-koll.
+        // If we want to reenable it we should investigate if amount is returned in SEK for all transactions in their
+        // newer API that their app is using.
+        return null;
+    }
+
+    @Override
+    public FetchInvestmentAccountsResponse fetchInvestmentAccounts() {
+        Map<Account, AccountFeatures> accounts = new HashMap<Account, AccountFeatures>();
+        getAccounts().forEach(accountEntity -> {
+
+            // A sparkonto don't hold instruments
+            if (Objects.equals("sparkonto", Strings.nullToEmpty(accountEntity.getAccountCode()).toLowerCase())) {
+                return;
+            }
+            // accounts can be blocked, will have no further data
+            if (accountEntity.isBlocked()) {
+                log.debug(String.format("Account: %s blocked, reason: %s ",
+                        accountEntity.getAccountNumber(), accountEntity.getBlockedReason()));
+                return;
+            }
+
+            Account account = accountEntity.toAccount(AccountTypes.INVESTMENT);
+            Portfolio portfolio = accountEntity.toPortfolio();
+
+            // Contains all positions, regardless of account/portfolio
+            Optional<PositionsResponse> positions = apiClient.getPositions();
+
+            if (!positions.isPresent()) {
+                //financialDataCacher.cacheAccount(account, AccountFeatures.createForPortfolios(portfolio));
+                accounts.put(account, AccountFeatures.createForPortfolios(portfolio));
+                return;
+            }
+
+            List<Instrument> instruments = Lists.newArrayList();
+            for (PositionEntity positionEntity : positions.get()) {
+                if (positionEntity.getAccountId().equalsIgnoreCase(accountEntity.getAccountId())) {
+                    // If, this position actually belongs to this account/portfolio
+                    positionEntity.toInstrument().ifPresent(instruments::add);
+                }
+            }
+            portfolio.setInstruments(instruments);
+
+            accounts.put(account, AccountFeatures.createForPortfolios(portfolio));
+        });
+        return new FetchInvestmentAccountsResponse(accounts);
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchInvestmentTransactions() {
+        return null;
     }
 }
