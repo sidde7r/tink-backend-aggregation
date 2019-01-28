528d124dc2c761fb49fdfcb87e29561f35a61904
Kiril Videlov <kiril.videlov@tink.se>
2019-01-28 14:56:18 +0100

    build(ci): Remove --batch bazel flag from ci scripts
    The --batch flag is deprecated, from the bazel documentation: "If set,
    Bazel will be run as just a client process without a server, instead of
    in the standard client/server mode. This is deprecated and will be
    removed, please prefer shutting down the server explicitly if you wish
    to avoid lingering servers."
    Running in this mode is undesirable anyways because it does not allow us
    to reuse cache.

--- .buildkite/run-unit-tests.sh
+++ .buildkite/run-unit-tests.sh
@@ -3,7 +3,7 @@
 # Explicitly not setting `-e` here to be able to contain errors from bazel-wrapper.
 set -x
 
-./bazel-wrapper --batch test --deleted_packages=deb,docker --curses=yes --color=yes -- ...:all
+./bazel-wrapper test --deleted_packages=deb,docker --curses=yes --color=yes -- ...:all
 OUTCOME=$?
 .buildkite/upload-test-files.sh
 exit $OUTCOME
