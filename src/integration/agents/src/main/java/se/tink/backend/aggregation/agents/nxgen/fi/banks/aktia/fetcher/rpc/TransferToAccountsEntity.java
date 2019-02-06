6d49f233bb971fc0d2836c82c7b1a97ac88634cb
Jonas Renold <jonas.renold@tink.se>
2019-02-06 13:19:00 +0100

    fix(fi/aktia) remve old aktia agent
    The aktia agent has not been in use for a long time due to authentication issues. Deleting the code for a rewrite (they have updated their api).

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/aktia/fetcher/rpc/TransferToAccountsEntity.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.fi.banks.aktia.fetcher.rpc;
-
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class TransferToAccountsEntity extends AbstractAccountEntity {
-}
