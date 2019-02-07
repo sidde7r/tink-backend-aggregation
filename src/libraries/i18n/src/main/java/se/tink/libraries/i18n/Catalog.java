44169463e09f208d0c3318b64d1ed0b00d641f18
Lars Almgren <lars.almgren@tink.se>
2019-02-07 16:25:04 +0100

    ref: change logger in catalog

--- src/libraries/i18n/src/main/java/se/tink/libraries/i18n/Catalog.java
+++ src/libraries/i18n/src/main/java/se/tink/libraries/i18n/Catalog.java
@@ -8,14 +8,15 @@ import java.util.Map;
 import java.util.MissingResourceException;
 import java.util.Objects;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xnap.commons.i18n.I18n;
 import org.xnap.commons.i18n.I18nFactory;
-import se.tink.libraries.log.LogUtils;
 
 public class Catalog {
     private static final Map<Locale, Catalog> catalogs = Maps.newHashMap();
     private static final Object catalogsLock = new Object();
-    private static final LogUtils log = new LogUtils(Catalog.class);
+    private static final Logger log = LoggerFactory.getLogger(Catalog.class);
 
     private static final String I18N_MESSAGES_BUNDLE = "se.tink.libraries.i18n.Messages";
 
