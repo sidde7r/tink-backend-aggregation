20c0b72488b566cb8de55979795558f4c9711f46
Lars Almgren <lars.almgren@tink.se>
2019-02-07 16:30:51 +0100

    ref: change logger in http response helper and web app error utils

--- src/libraries/http/src/main/java/se/tink/libraries/http/utils/HttpResponseHelper.java
+++ src/libraries/http/src/main/java/se/tink/libraries/http/utils/HttpResponseHelper.java
@@ -2,12 +2,12 @@ package se.tink.libraries.http.utils;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import se.tink.libraries.log.LogUtils;
+import org.slf4j.Logger;
 
 public class HttpResponseHelper {
-    private final LogUtils log;
+    private final Logger log;
 
-    public HttpResponseHelper(LogUtils log) {
+    public HttpResponseHelper(Logger log) {
         this.log = log;
     }
 
