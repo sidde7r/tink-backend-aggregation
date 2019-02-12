98cb0c5d330df0ac8766eb6d70666bb2e804bac9
ovesan <ove.sandfeldt@tink.se>
2019-02-12 09:43:33 +0100

    feat(TransactionalAccount): Add transactional account of type OTHER

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/transactional/builder/OtherBuildStep.java
@@ -0,0 +1,5 @@
+package se.tink.backend.aggregation.nxgen.core.account.transactional.builder;
+
+import se.tink.backend.aggregation.nxgen.core.account.transactional.OtherAccount;
+
+public interface OtherBuildStep extends BuildStep<OtherAccount, OtherBuildStep> {}
