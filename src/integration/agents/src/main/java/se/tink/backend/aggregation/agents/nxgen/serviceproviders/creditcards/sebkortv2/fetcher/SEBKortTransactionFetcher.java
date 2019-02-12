2300b8a29ea20aa848d282b7cfa627a690e24c77
Lars Almgren <lars.almgren@tink.se>
2019-02-12 10:18:56 +0100

    fix(sebkortv2): fix handling of reservations
    Reservations are fetched in one big chunk. Doing it over and over
    again will fetch the same reservations.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/sebkortv2/fetcher/SEBKortTransactionFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/sebkortv2/fetcher/SEBKortTransactionFetcher.java
@@ -3,11 +3,12 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.se
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.sebkortv2.SebKortApiClient;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponseImpl;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginator;
-import se.tink.backend.aggregation.nxgen.core.account.CreditCardAccount;
+import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 
 public class SEBKortTransactionFetcher implements TransactionDatePaginator<CreditCardAccount> {
@@ -21,16 +22,23 @@ public class SEBKortTransactionFetcher implements TransactionDatePaginator<Credi
     public PaginatorResponse getTransactionsFor(CreditCardAccount account, Date fromDate, Date toDate) {
         List<Transaction> tinkTransactions = new ArrayList<>();
 
-        tinkTransactions.addAll(
-                apiClient.fetchReservations(account.getBankIdentifier(), fromDate, toDate)
-                        .getTinkTransactions()
-        );
+        List<Transaction> reservations = apiClient.fetchReservations(account.getApiIdentifier())
+                .getTinkTransactions();
 
         tinkTransactions.addAll(
-                apiClient.fetchTransactions(account.getBankIdentifier(), fromDate, toDate)
+                handleReservations(fromDate, toDate, reservations));
+
+        tinkTransactions.addAll(
+                apiClient.fetchTransactions(account.getApiIdentifier(), fromDate, toDate)
                         .getTinkTranscations()
         );
 
         return PaginatorResponseImpl.create(tinkTransactions);
+    }
+
+    private static List<Transaction> handleReservations(Date fromDate, Date toDate, List<Transaction> reservations) {
+        return reservations.stream()
+                .filter(transaction -> transaction.getDate().after(fromDate) && transaction.getDate().before(toDate))
+                .collect(Collectors.toList());
     }
 }
