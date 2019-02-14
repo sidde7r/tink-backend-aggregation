d9911f435f1b6a3cdb053e751d58830cc9b9708e
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2019-02-14 16:09:14 +0100

    feat(RO Raiffeisen): Create agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/ro/banks/raiffeisen/RaiffeisenAgent.java
@@ -0,0 +1,102 @@
+package se.tink.backend.aggregation.agents.nxgen.ro.banks.raiffeisen;
+
+import java.time.ZoneId;
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.ro.banks.raiffeisen.authenticator.RaiffeisenOAuth2Authenticator;
+import se.tink.backend.aggregation.agents.nxgen.ro.banks.raiffeisen.fetcher.RaiffeisenAccountFetcher;
+import se.tink.backend.aggregation.agents.nxgen.ro.banks.raiffeisen.fetcher.RaiffeisenTransactionFetcher;
+import se.tink.backend.aggregation.nxgen.agents.NextGenerationAgent;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.ThirdPartyAppAuthenticationController;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2AuthenticationController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.creditcard.CreditCardRefreshController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.einvoice.EInvoiceRefreshController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.investment.InvestmentRefreshController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.loan.LoanRefreshController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcherController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionMonthPaginationController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transfer.TransferDestinationRefreshController;
+import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
+import se.tink.backend.aggregation.nxgen.controllers.transfer.TransferController;
+import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+
+public class RaiffeisenAgent extends NextGenerationAgent {
+
+    private final RaiffeisenApiClient raiffeisenApiClient;
+
+    public RaiffeisenAgent(CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+        this.raiffeisenApiClient = new RaiffeisenApiClient(client, persistentStorage);
+    }
+
+    @Override
+    protected void configureHttpClient(TinkHttpClient client) {
+    }
+
+    @Override
+    protected Authenticator constructAuthenticator() {
+        RaiffeisenOAuth2Authenticator authenticator = new RaiffeisenOAuth2Authenticator(raiffeisenApiClient);
+        OAuth2AuthenticationController oAuth2AuthenticationController = new OAuth2AuthenticationController(
+                persistentStorage,
+                supplementalInformationHelper, authenticator);
+        return new AutoAuthenticationController(
+                request,
+                context,
+                new ThirdPartyAppAuthenticationController<>(
+                        oAuth2AuthenticationController,
+                        supplementalInformationHelper
+                ),
+                oAuth2AuthenticationController);
+    }
+
+    @Override
+    protected Optional<TransactionalAccountRefreshController> constructTransactionalAccountRefreshController() {
+        return Optional.of(new TransactionalAccountRefreshController(
+                metricRefreshController,
+                updateController,
+                new RaiffeisenAccountFetcher(raiffeisenApiClient),
+                new TransactionFetcherController<>(transactionPaginationHelper,
+                        new TransactionMonthPaginationController<>(
+                                new RaiffeisenTransactionFetcher(raiffeisenApiClient), ZoneId.of("Europe/Madrid")))));
+    }
+
+    @Override
+    protected Optional<CreditCardRefreshController> constructCreditCardRefreshController() {
+        return Optional.empty();
+    }
+
+    @Override
+    protected Optional<InvestmentRefreshController> constructInvestmentRefreshController() {
+        return Optional.empty();
+    }
+
+    @Override
+    protected Optional<LoanRefreshController> constructLoanRefreshController() {
+        return Optional.empty();
+    }
+
+    @Override
+    protected Optional<EInvoiceRefreshController> constructEInvoiceRefreshController() {
+        return Optional.empty();
+    }
+
+    @Override
+    protected Optional<TransferDestinationRefreshController> constructTransferDestinationRefreshController() {
+        return Optional.empty();
+    }
+
+    @Override
+    protected SessionHandler constructSessionHandler() {
+        return new RaiffeisenSessionHandler();
+    }
+
+    @Override
+    protected Optional<TransferController> constructTransferController() {
+        return Optional.empty();
+    }
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/ro/banks/raiffeisen/RaiffeisenConstants.java
@@ -0,0 +1,83 @@
+package se.tink.backend.aggregation.agents.nxgen.ro.banks.raiffeisen;
+
+import java.time.format.DateTimeFormatter;
+import java.util.regex.Pattern;
+
+public class RaiffeisenConstants {
+
+    //TODO: Move to development.yaml
+    // These are only for sandbox!
+    public static final String CLIENT_ID_VALUE = "52c20546-32c0-42b1-afa2-18770029ec80";
+    public static final String CLIENT_SECRET_VALUE = "P5cM0oP1sM5iV8uP3fF6dV3bU8uI0fD5kG2dO1sB1eF2pJ4pS4";
+    public static final String REDIRECT_URL_VALUE = "https://api.tink.se/api/v1/credentials/third-party/callback";
+    // for localhost: https://127.0.0.1:9100/api/v1/credentials/third-party/callback
+    // for staging: https://main.staging.oxford.tink.se/api/v1/credentials/third-party/callback
+
+    public static final class URL {
+        public static final String BASE_AUTH = "https://api-test.raiffeisen.ro/psd2-sandbox-oauth2-api";
+        public static final String BASE_API = "https://api-test.raiffeisen.ro/";
+        public static final String OAUTH = "/oauth2/authorize";
+        public static final String TOKEN = "/oauth2/token";
+        public static final String ACCOUNTS = "/v1/banks/rbro/sandbox/accounts";
+        public static final String TRANSACTIONS = "/v1/banks/rbro/sandbox/accounts/%s/transactions";
+    }
+
+    public static final class QUERY {
+        public static final String CLIENT_ID = "client_id";
+        public static final String RESPONSE_TYPE = "response_type";
+        public static final String SCOPE = "scope";
+        public static final String REDIRECT_URL = "redirect_uri";
+        public static final String STATE = "state";
+        public static final String WITH_BALANCE = "withBalance";
+        public static final String DATE_FROM = "dateFrom";
+        public static final String DATE_TO = "dateTo";
+        public static final String BOOKING_STATUS = "bookingStatus";
+        public static final String PAGE = "page";
+        public static final String PAGE_SIZE = "pageSize";
+
+        public static final String SCOPE_VALUE = "AISP";
+        public static final String RESPONSE_TYPE_CODE = "code";
+        public static final String WITH_BALANCE_TRUE = "true";
+        public static final String BOOKING_STATUS_BOTH = "both";
+    }
+
+    public static final class HEADER {
+        public static final String ACCEPT_TEXT_HTML = "text/html";
+        public static final String X_IBM_CLIENT_ID = "x-ibm-client-id";
+        public static final String X_REQUEST_ID = "x-request-id";
+        public static final String CLIENT_ID = "client_id";
+        public static final String CLIENT_SECRET = "client_secret";
+    }
+
+    public static final class BODY {
+        public static final String GRANT_TYPE_CODE = "authorization_code";
+        public static final String GRANT_TYPE_REFRESH_TOKEN = "refresh_token";
+    }
+
+    public static final class STORAGE {
+        public static final String TOKEN = "TOKEN";
+        public static final String BALANCE_URL = "BALANCE_URL";
+        public static final String TRANSACTIONS_URL = "TRANSACTIONS_URL";
+        public static final String ACCOUNT_ID = "ACCOUNT_ID";
+    }
+
+    public static final class DATE {
+        public static final String FORMAT = "yyyy-MM-dd";
+        public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(FORMAT);
+    }
+
+    public static final class FORM {
+        public static final String GRANT_TYPE = "grant_type";
+        public static final String CLIENT_SECRET = "client_secret";
+        public static final String CLIENT_ID = "client_id";
+        public static final String CODE = "code";
+        public static final String REDIRECT_URI = "redirect_uri";
+        public static final String REFRESH_TOKEN = "refresh_token";
+    }
+
+    public static final class REGEX {
+        public static final Pattern PAGE = Pattern.compile("page=(.*)");
+        public static final Pattern PATTERN_STRUCTURED_INFO = Pattern.compile(".*\\\"MerchantName\\\" ?: ?\\\"(.*)\\\".*");
+    }
+
+}
