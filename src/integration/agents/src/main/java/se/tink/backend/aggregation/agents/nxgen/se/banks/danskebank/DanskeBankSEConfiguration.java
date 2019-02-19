d85ddb2e4aeda91a44599bb708138013ee0740cb
ovesan <ove.sandfeldt@tink.se>
2019-02-19 08:46:40 +0100

    fix(DanskeBank SE): Update constants to new version

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/danskebank/DanskeBankSEConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/danskebank/DanskeBankSEConfiguration.java
@@ -3,22 +3,24 @@ package se.tink.backend.aggregation.agents.nxgen.se.banks.danskebank;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.DanskeBankConfiguration;
 import se.tink.backend.aggregation.agents.models.Loan;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.DanskeBankConfiguration;
 
 public class DanskeBankSEConfiguration implements DanskeBankConfiguration {
     // == START Standard configuration ==
     private static final String APP_CULTURE = "sv-SE";
     private static final String APP_NAME = "com.danskebank.mobilebank3se";
     private static final String APP_REFERER = "MobileBanking3 SE";
-    private static final String APP_VERSION = "0.41.0";
+    private static final String APP_VERSION = "0.43.0";
     private static final String BRAND = "OEB";
     private static final String LANGUAGE_CODE = "SV";
     private static final String MARKET_CODE = "SE";
     private static final String SECURITY_SYSTEM = "SV";
     private static final String CLIENT_ID = "5ec4b8ad-a93d-43e1-831c-8e78ee6e661a";
-    private static final String CLIENT_SECRET = "NRRM1W2ckjUdBwhbHtP38yIZevM9yr46v0wosfIWM4sYSFuCNy";
-    private static final String APP_VERSION_HEADER = "MobileBank ios SE 1121105";
+    private static final String CLIENT_SECRET = "Et7TLuUT6WfZbz9RLlwYKUQMVK99Gq4yU3FZqsebMO7MHFtSZ2";
+    private static final String APP_VERSION_HEADER = "MobileBank ios SE 1315097";
+    private static final String DEVICE_SERIAL_NO_KEY = "x-device-serial-no";
+    private static final String STEP_UP_TOKEN_KEY = "x-stepup-token";
     // == END Standard configuration ==
 
     @Override
@@ -91,10 +93,6 @@ public class DanskeBankSEConfiguration implements DanskeBankConfiguration {
         return Collections.emptyMap();
     }
 
-    public String getSecuritySystem() {
-        return SECURITY_SYSTEM; // Bank ID Security system
-    }
-
     public class BankIdStatus {
         public static final String ALREADY_IN_PROGRESS = "already_in_progress";
         public static final String CANCELLED = "cancelled";
@@ -103,5 +101,20 @@ public class DanskeBankSEConfiguration implements DanskeBankConfiguration {
         public static final String OUTSTANDING_TRANSACTION = "outstanding_transaction";
         public static final String USER_CANCEL = "user_cancel";
         public static final String USER_SIGN = "user_sign";
+    }
+
+    @Override
+    public String getStepUpTokenKey() {
+        return STEP_UP_TOKEN_KEY;
+    }
+
+    @Override
+    public String getDeviceSerialNumberKey() {
+        return DEVICE_SERIAL_NO_KEY;
+    }
+
+    @Override
+    public String getSecuritySystem() {
+        return SECURITY_SYSTEM; // Bank ID Security system
     }
 }
