e5aea40243f443558db257e799500ee355a7f7e1
Xiaoqiang Chen <xiaoqiang.chen@tink.se>
2019-02-20 16:49:25 +0100

    fix(raiffeisen-ro): fix the redirect url and provider name in test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/ro/banks/raiffeisen/RaiffeisenAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/ro/banks/raiffeisen/RaiffeisenAgentTest.java
@@ -1,6 +1,5 @@
 package se.tink.backend.aggregation.agents.nxgen.ro.banks.raiffeisen;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
@@ -9,7 +8,7 @@ public class RaiffeisenAgentTest {
 
     @Test
     public void refresh() throws Exception {
-        new AgentIntegrationTest.Builder("ro", "ro-raiffeisen-psd2")
+        new AgentIntegrationTest.Builder("ro", "ro-raiffeisen-oauth2")
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false)
                 .expectLoggedIn(false)
