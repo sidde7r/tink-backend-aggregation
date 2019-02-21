a0cdd3cdaf8617b2f2ae304ef71aac5199e7df36
Sebastian Olsson <sebastian.olsson@tink.se>
2019-02-21 14:33:03 +0100

    style(agent/utils): google-java-format compliance

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/authentication/vasco/digipass/Digipass.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/authentication/vasco/digipass/Digipass.java
@@ -2,14 +2,6 @@ package se.tink.backend.aggregation.agents.utils.authentication.vasco.digipass;
 
 import com.google.common.base.Preconditions;
 import com.google.common.primitives.Bytes;
-import java.security.KeyPair;
-import java.time.Instant;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 import org.bouncycastle.jce.interfaces.ECPrivateKey;
 import org.bouncycastle.jce.interfaces.ECPublicKey;
 import se.tink.backend.aggregation.agents.utils.authentication.vasco.digipass.models.DecryptActivationDataResponse;
@@ -25,6 +17,15 @@ import se.tink.backend.aggregation.agents.utils.encoding.EncodingUtils;
 import se.tink.backend.aggregation.agents.utils.random.RandomUtils;
 import se.tink.libraries.serialization.TypeReferences;
 import se.tink.libraries.serialization.utils.SerializationUtils;
+
+import java.security.KeyPair;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 public class Digipass {
     private int otpCounter = 0;
@@ -44,50 +45,54 @@ public class Digipass {
     private StaticVector staticVector;
 
     // First step
-    public InitializeRegistrationDataResponse initializeRegistrationData(String activationPassword) {
+    public InitializeRegistrationDataResponse initializeRegistrationData(
+            String activationPassword) {
         this.fingerPrint = FingerPrintUtils.generateFingerPrint();
-        this.ellipticCurveKeyPair = EllipticCurve.generateKeyPair(DigipassConstants.CommunicationCrypto.EC_CURVE_NAME);
+        this.ellipticCurveKeyPair =
+                EllipticCurve.generateKeyPair(DigipassConstants.CommunicationCrypto.EC_CURVE_NAME);
 
         this.activationPassword = activationPassword;
-        this.clientNonce = RandomUtils.secureRandom(DigipassConstants.CommunicationCrypto.NONCE_LENGTH);
+        this.clientNonce =
+                RandomUtils.secureRandom(DigipassConstants.CommunicationCrypto.NONCE_LENGTH);
 
         byte[] activationKey = CryptoUtils.deriveActivationKey(this.activationPassword);
         byte[] iv = RandomUtils.secureRandom(DigipassConstants.CommunicationCrypto.IV_LENGTH);
 
-        byte[] encryptedPublicKeyAndNonce = CryptoUtils.encryptPublicKeyAndNonce(
-                activationKey,
-                iv,
-                this.ellipticCurveKeyPair,
-                this.clientNonce
-        );
+        byte[] encryptedPublicKeyAndNonce =
+                CryptoUtils.encryptPublicKeyAndNonce(
+                        activationKey, iv, this.ellipticCurveKeyPair, this.clientNonce);
 
         return new InitializeRegistrationDataResponse(iv, encryptedPublicKeyAndNonce);
     }
 
     // Second step
-    public DecryptActivationDataResponse decryptActivationData(String serverInitialVector, String encryptedNonces,
-            String encryptedServerPublicKey, String xfad, String challenge) {
+    public DecryptActivationDataResponse decryptActivationData(
+            String serverInitialVector,
+            String encryptedNonces,
+            String encryptedServerPublicKey,
+            String xfad,
+            String challenge) {
         Preconditions.checkNotNull(this.fingerPrint, "Fingerprint has not been initialized.");
         Preconditions.checkNotNull(this.ellipticCurveKeyPair, "ECKey has not been initialized.");
-        Preconditions.checkNotNull(this.activationPassword, "ActivationPassword has not been initialized.");
+        Preconditions.checkNotNull(
+                this.activationPassword, "ActivationPassword has not been initialized.");
 
         byte[] requestIv = EncodingUtils.decodeHexString(serverInitialVector);
-        byte[] responseIv = RandomUtils.secureRandom(DigipassConstants.CommunicationCrypto.IV_LENGTH);
+        byte[] responseIv =
+                RandomUtils.secureRandom(DigipassConstants.CommunicationCrypto.IV_LENGTH);
 
         this.ecdhSharedSecret = calculateSharedSecret(requestIv, encryptedServerPublicKey);
 
-        byte[] encryptedServerNonce = CryptoUtils.reencryptServerNonce(
-                this.ecdhSharedSecret,
-                requestIv,
-                responseIv,
-                EncodingUtils.decodeHexString(encryptedNonces)
-        );
+        byte[] encryptedServerNonce =
+                CryptoUtils.reencryptServerNonce(
+                        this.ecdhSharedSecret,
+                        requestIv,
+                        responseIv,
+                        EncodingUtils.decodeHexString(encryptedNonces));
 
-        byte[] decryptedXfad = AES.decryptCbc(
-                this.ecdhSharedSecret,
-                requestIv,
-                EncodingUtils.decodeHexString(xfad)
-        );
+        byte[] decryptedXfad =
+                AES.decryptCbc(
+                        this.ecdhSharedSecret, requestIv, EncodingUtils.decodeHexString(xfad));
         // The decrypted Xfad is hex encoded (i.e. a string).
         loadXfad(new String(decryptedXfad));
 
@@ -116,17 +121,15 @@ public class Digipass {
 
         byte[] activationKey = CryptoUtils.deriveActivationKey(this.activationPassword);
 
-        ECPublicKey serverPublicKey = CryptoUtils.decryptPublicKey(
-                DigipassConstants.CommunicationCrypto.EC_CURVE_NAME,
-                activationKey,
-                iv,
-                EncodingUtils.decodeHexString(encryptedServerPublicKey)
-        );
+        ECPublicKey serverPublicKey =
+                CryptoUtils.decryptPublicKey(
+                        DigipassConstants.CommunicationCrypto.EC_CURVE_NAME,
+                        activationKey,
+                        iv,
+                        EncodingUtils.decodeHexString(encryptedServerPublicKey));
 
         return CryptoUtils.calculateSharedSecret(
-                (ECPrivateKey) ellipticCurveKeyPair.getPrivate(),
-                serverPublicKey
-        );
+                (ECPrivateKey) ellipticCurveKeyPair.getPrivate(), serverPublicKey);
     }
 
     private void calculateOtpKey() {
@@ -134,8 +137,10 @@ public class Digipass {
         Preconditions.checkNotNull(this.otpSeedIv, "otpSeedIv not initialized.");
         Preconditions.checkNotNull(this.otpSeedData, "otpSeedData not initialized.");
 
-        byte[] key = staticVector.getMandatoryField(DigipassConstants.StaticVectorFieldType.CRYPTO_KEY);
-        byte[] signature = staticVector.getMandatoryField(DigipassConstants.StaticVectorFieldType.SIGNATURE);
+        byte[] key =
+                staticVector.getMandatoryField(DigipassConstants.StaticVectorFieldType.CRYPTO_KEY);
+        byte[] signature =
+                staticVector.getMandatoryField(DigipassConstants.StaticVectorFieldType.SIGNATURE);
         byte[] iv = Bytes.concat(signature, this.otpSeedIv);
 
         this.otpKey = CryptoUtils.calculateOtpKey(key, iv, this.otpSeedData);
@@ -163,12 +168,12 @@ public class Digipass {
                 this.otpKey,
                 otpCounter,
                 epochTime,
-                challenges
-        );
+                challenges);
     }
 
     private String calculateOtp(int otpCounter, String challenge) {
-        return calculateOtp(otpCounter, Collections.singletonList(EncodingUtils.decodeHexString(challenge)));
+        return calculateOtp(
+                otpCounter, Collections.singletonList(EncodingUtils.decodeHexString(challenge)));
     }
 
     private String calculateDerivationCode(String otpResponse) {
@@ -180,30 +185,51 @@ public class Digipass {
 
         m.put(DigipassConstants.Serialization.OTP_COUNTER, Integer.toString(otpCounter));
         Optional.ofNullable(otpKey)
-                .ifPresent(v -> m.put(DigipassConstants.Serialization.OTP_KEY,
-                        EncodingUtils.encodeAsBase64String(v)));
+                .ifPresent(
+                        v ->
+                                m.put(
+                                        DigipassConstants.Serialization.OTP_KEY,
+                                        EncodingUtils.encodeAsBase64String(v)));
         Optional.ofNullable(clientNonce)
-                .ifPresent(v -> m.put(DigipassConstants.Serialization.CLIENT_NONCE,
-                        EncodingUtils.encodeAsBase64String(v)));
+                .ifPresent(
+                        v ->
+                                m.put(
+                                        DigipassConstants.Serialization.CLIENT_NONCE,
+                                        EncodingUtils.encodeAsBase64String(v)));
         Optional.ofNullable(ecdhSharedSecret)
-                .ifPresent(v -> m.put(DigipassConstants.Serialization.ECDH_SHARED_SECRET,
-                        EncodingUtils.encodeAsBase64String(v)));
+                .ifPresent(
+                        v ->
+                                m.put(
+                                        DigipassConstants.Serialization.ECDH_SHARED_SECRET,
+                                        EncodingUtils.encodeAsBase64String(v)));
         Optional.ofNullable(xfad)
-                .ifPresent(v -> m.put(DigipassConstants.Serialization.XFAD,
-                        EncodingUtils.encodeAsBase64String(v)));
+                .ifPresent(
+                        v ->
+                                m.put(
+                                        DigipassConstants.Serialization.XFAD,
+                                        EncodingUtils.encodeAsBase64String(v)));
         Optional.ofNullable(otpSeedIv)
-                .ifPresent(v -> m.put(DigipassConstants.Serialization.OTP_SEED_IV,
-                        EncodingUtils.encodeAsBase64String(v)));
+                .ifPresent(
+                        v ->
+                                m.put(
+                                        DigipassConstants.Serialization.OTP_SEED_IV,
+                                        EncodingUtils.encodeAsBase64String(v)));
         Optional.ofNullable(otpSeedData)
-                .ifPresent(v -> m.put(DigipassConstants.Serialization.OTP_SEED_DATA,
-                        EncodingUtils.encodeAsBase64String(v)));
+                .ifPresent(
+                        v ->
+                                m.put(
+                                        DigipassConstants.Serialization.OTP_SEED_DATA,
+                                        EncodingUtils.encodeAsBase64String(v)));
         Optional.ofNullable(fingerPrint)
                 .ifPresent(v -> m.put(DigipassConstants.Serialization.FINGERPRINT, v));
         Optional.ofNullable(activationPassword)
                 .ifPresent(v -> m.put(DigipassConstants.Serialization.ACTIVATION_PASSWORD, v));
         Optional.ofNullable(ellipticCurveKeyPair)
-                .ifPresent(v -> m.put(DigipassConstants.Serialization.EC_KEY_PAIR,
-                        SerializationUtils.serializeKeyPair(v)));
+                .ifPresent(
+                        v ->
+                                m.put(
+                                        DigipassConstants.Serialization.EC_KEY_PAIR,
+                                        SerializationUtils.serializeKeyPair(v)));
 
         return SerializationUtils.serializeToString(m);
     }
@@ -216,31 +242,29 @@ public class Digipass {
     }
 
     public void deserialize(String mstring) {
-        Map<String, String> m = SerializationUtils.deserializeFromString(
-                mstring,
-                TypeReferences.MAP_OF_STRING_STRING
-        );
+        Map<String, String> m =
+                SerializationUtils.deserializeFromString(
+                        mstring, TypeReferences.MAP_OF_STRING_STRING);
 
         getIfExists(m, DigipassConstants.Serialization.OTP_COUNTER)
-                .ifPresent(v-> otpCounter = Integer.valueOf(v));
+                .ifPresent(v -> otpCounter = Integer.valueOf(v));
         getIfExists(m, DigipassConstants.Serialization.OTP_KEY)
-                .ifPresent(v-> otpKey = EncodingUtils.decodeBase64String(v));
+                .ifPresent(v -> otpKey = EncodingUtils.decodeBase64String(v));
         getIfExists(m, DigipassConstants.Serialization.CLIENT_NONCE)
-                .ifPresent(v-> clientNonce = EncodingUtils.decodeBase64String(v));
+                .ifPresent(v -> clientNonce = EncodingUtils.decodeBase64String(v));
         getIfExists(m, DigipassConstants.Serialization.ECDH_SHARED_SECRET)
-                .ifPresent(v-> ecdhSharedSecret = EncodingUtils.decodeBase64String(v));
+                .ifPresent(v -> ecdhSharedSecret = EncodingUtils.decodeBase64String(v));
         getIfExists(m, DigipassConstants.Serialization.XFAD)
-                .ifPresent(v-> xfad = EncodingUtils.decodeBase64String(v));
+                .ifPresent(v -> xfad = EncodingUtils.decodeBase64String(v));
         getIfExists(m, DigipassConstants.Serialization.OTP_SEED_IV)
-                .ifPresent(v-> otpSeedIv = EncodingUtils.decodeBase64String(v));
+                .ifPresent(v -> otpSeedIv = EncodingUtils.decodeBase64String(v));
         getIfExists(m, DigipassConstants.Serialization.OTP_SEED_DATA)
-                .ifPresent(v-> otpSeedData = EncodingUtils.decodeBase64String(v));
-        getIfExists(m, DigipassConstants.Serialization.FINGERPRINT)
-                .ifPresent(v-> fingerPrint = v);
+                .ifPresent(v -> otpSeedData = EncodingUtils.decodeBase64String(v));
+        getIfExists(m, DigipassConstants.Serialization.FINGERPRINT).ifPresent(v -> fingerPrint = v);
         getIfExists(m, DigipassConstants.Serialization.ACTIVATION_PASSWORD)
-                .ifPresent(v-> activationPassword = v);
+                .ifPresent(v -> activationPassword = v);
         getIfExists(m, DigipassConstants.Serialization.EC_KEY_PAIR)
-                .ifPresent(v-> ellipticCurveKeyPair = SerializationUtils.deserializeKeyPair(v));
+                .ifPresent(v -> ellipticCurveKeyPair = SerializationUtils.deserializeKeyPair(v));
 
         if (Objects.nonNull(this.xfad)) {
             this.staticVector = StaticVector.createFromXfad(this.xfad);
