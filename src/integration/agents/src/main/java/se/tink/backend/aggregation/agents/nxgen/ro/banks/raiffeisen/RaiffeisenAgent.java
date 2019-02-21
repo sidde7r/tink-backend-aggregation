803bb5be99e79122134e45995f778888a5cf549c
Alexander Jamm <alexander.jamm@tink.se>
2019-02-21 14:31:42 +0100

    fix(Raiffeisen RO): moved redirectUrl to provider payload

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/ro/banks/raiffeisen/RaiffeisenAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/ro/banks/raiffeisen/RaiffeisenAgent.java
@@ -1,11 +1,13 @@
 package se.tink.backend.aggregation.agents.nxgen.ro.banks.raiffeisen;
 
+import com.google.common.base.Strings;
 import java.time.ZoneId;
 import java.util.Optional;
 import se.tink.backend.aggregation.agents.AgentContext;
 import se.tink.backend.aggregation.agents.nxgen.ro.banks.raiffeisen.authenticator.RaiffeisenOAuth2Authenticator;
 import se.tink.backend.aggregation.agents.nxgen.ro.banks.raiffeisen.fetcher.RaiffeisenAccountFetcher;
 import se.tink.backend.aggregation.agents.nxgen.ro.banks.raiffeisen.fetcher.RaiffeisenTransactionFetcher;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
 import se.tink.backend.aggregation.nxgen.agents.NextGenerationAgent;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
@@ -23,46 +25,54 @@ import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
 import se.tink.backend.aggregation.nxgen.controllers.transfer.TransferController;
 import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
 import se.tink.libraries.credentials.service.CredentialsRequest;
-import se.tink.backend.aggregation.configuration.SignatureKeyPair;
 
 public class RaiffeisenAgent extends NextGenerationAgent {
 
     private final RaiffeisenApiClient raiffeisenApiClient;
 
-    public RaiffeisenAgent(CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+    public RaiffeisenAgent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
         super(request, context, signatureKeyPair);
-        this.raiffeisenApiClient = new RaiffeisenApiClient(client, persistentStorage);
+        String redirectUrl = request.getProvider().getPayload();
+
+        if (Strings.isNullOrEmpty(redirectUrl)) {
+            throw new IllegalStateException("redirectUrl is null or empty!");
+        }
+
+        this.raiffeisenApiClient = new RaiffeisenApiClient(client, persistentStorage, redirectUrl);
     }
 
     @Override
-    protected void configureHttpClient(TinkHttpClient client) {
-    }
+    protected void configureHttpClient(TinkHttpClient client) {}
 
     @Override
     protected Authenticator constructAuthenticator() {
-        RaiffeisenOAuth2Authenticator authenticator = new RaiffeisenOAuth2Authenticator(raiffeisenApiClient);
-        OAuth2AuthenticationController oAuth2AuthenticationController = new OAuth2AuthenticationController(
-                persistentStorage,
-                supplementalInformationHelper, authenticator);
+        RaiffeisenOAuth2Authenticator authenticator =
+                new RaiffeisenOAuth2Authenticator(raiffeisenApiClient);
+        OAuth2AuthenticationController oAuth2AuthenticationController =
+                new OAuth2AuthenticationController(
+                        persistentStorage, supplementalInformationHelper, authenticator);
         return new AutoAuthenticationController(
                 request,
                 context,
                 new ThirdPartyAppAuthenticationController<>(
-                        oAuth2AuthenticationController,
-                        supplementalInformationHelper
-                ),
+                        oAuth2AuthenticationController, supplementalInformationHelper),
                 oAuth2AuthenticationController);
     }
 
     @Override
-    protected Optional<TransactionalAccountRefreshController> constructTransactionalAccountRefreshController() {
-        return Optional.of(new TransactionalAccountRefreshController(
-                metricRefreshController,
-                updateController,
-                new RaiffeisenAccountFetcher(raiffeisenApiClient),
-                new TransactionFetcherController<>(transactionPaginationHelper,
-                        new TransactionMonthPaginationController<>(
-                                new RaiffeisenTransactionFetcher(raiffeisenApiClient), ZoneId.of("Europe/Madrid")))));
+    protected Optional<TransactionalAccountRefreshController>
+            constructTransactionalAccountRefreshController() {
+        return Optional.of(
+                new TransactionalAccountRefreshController(
+                        metricRefreshController,
+                        updateController,
+                        new RaiffeisenAccountFetcher(raiffeisenApiClient),
+                        new TransactionFetcherController<>(
+                                transactionPaginationHelper,
+                                new TransactionMonthPaginationController<>(
+                                        new RaiffeisenTransactionFetcher(raiffeisenApiClient),
+                                        ZoneId.of("Europe/Madrid")))));
     }
 
     @Override
@@ -86,7 +96,8 @@ public class RaiffeisenAgent extends NextGenerationAgent {
     }
 
     @Override
-    protected Optional<TransferDestinationRefreshController> constructTransferDestinationRefreshController() {
+    protected Optional<TransferDestinationRefreshController>
+            constructTransferDestinationRefreshController() {
         return Optional.empty();
     }
 
