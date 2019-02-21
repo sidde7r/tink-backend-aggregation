5157c4fc1e40efbe576018550c189fd2132b9fb9
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2019-02-21 11:40:51 +0100

    fix(IntegrationsConfiguration): Add missing @JsonObject annotation on client configurations

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/monzo/configuration/MonzoConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/monzo/configuration/MonzoConfiguration.java
@@ -1,8 +1,10 @@
 package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.monzo.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.configuration.ClientConfiguration;
 
+@JsonObject
 public class MonzoConfiguration implements ClientConfiguration {
     @JsonProperty private String clientId;
     @JsonProperty private String clientSecret;
