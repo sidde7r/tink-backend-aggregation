0a76fec977388718f686ae1dade59013a24e78eb
Sebastian Olsson <sebastian.olsson@tink.se>
2019-02-27 15:51:50 +0100

    style(de-hvb): google-java-format formatting

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/hvb/worklight/fetcher/WLFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/hvb/worklight/fetcher/WLFetcher.java
@@ -1,17 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.de.banks.hvb.worklight.fetcher;
 
 import com.google.api.client.repackaged.com.google.common.base.Preconditions;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.security.Signature;
-import java.security.SignatureException;
-import java.util.Collections;
-import java.util.List;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
-import javax.xml.bind.DatatypeConverter;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
@@ -27,34 +16,47 @@ import se.tink.backend.aggregation.agents.nxgen.de.banks.hvb.worklight.fetcher.r
 import se.tink.backend.aggregation.agents.utils.crypto.AES;
 import se.tink.backend.aggregation.agents.utils.crypto.RSA;
 import se.tink.backend.aggregation.agents.utils.encoding.EncodingUtils;
-import se.tink.backend.aggregation.log.AggregationLogger;
 import se.tink.backend.aggregation.nxgen.http.Form;
 import se.tink.backend.aggregation.nxgen.http.HttpResponse;
 import se.tink.libraries.serialization.utils.SerializationUtils;
 
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.DatatypeConverter;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.util.Collections;
+import java.util.List;
+
 public final class WLFetcher {
     private static final Logger logger = LoggerFactory.getLogger(WLFetcher.class);
-    //private static final AggregationLogger longLogger = new AggregationLogger(WLFetcher.class);
+    // private static final AggregationLogger longLogger = new AggregationLogger(WLFetcher.class);
 
     private final WLApiClient apiClient;
     private final WLFetcherStorage storage;
     private final WLConfig config;
 
     public WLFetcher(
-            final WLApiClient client,
-            final WLFetcherStorage storage,
-            final WLConfig config) {
+            final WLApiClient client, final WLFetcherStorage storage, final WLConfig config) {
         this.apiClient = client;
         this.storage = storage;
         this.config = config;
     }
 
     public <T> T getAccounts(final Class<T> entityClass) {
-        return query(WLConstants.Procedure.fetch_FinancialStatusAccountDO.name(), entityClass, new JSONObject());
+        return query(
+                WLConstants.Procedure.fetch_FinancialStatusAccountDO.name(),
+                entityClass,
+                new JSONObject());
     }
 
     public <T> T getAccountHolders(final Class<T> entityClass) {
-        return query(WLConstants.Procedure.fetch_AdvisorsListDO.name(), entityClass, new JSONObject());
+        return query(
+                WLConstants.Procedure.fetch_AdvisorsListDO.name(), entityClass, new JSONObject());
     }
 
     public <T> T getTransactions(final Class<T> entityClass, final String accountNumber) {
@@ -71,30 +73,41 @@ public final class WLFetcher {
         return WLConstants.Url.API_ROOT + config.getModuleName() + slug;
     }
 
-    private <T> T query(final String procedure, final Class<T> entityClass, final JSONObject payload) {
-        final Form request = Form.builder()
-                .put(WLConstants.Forms.ADAPTER, WLConstants.Forms.ADAPTER_FACADE)
-                .put(WLConstants.Forms.PROCEDURE, WLConstants.Forms.FETCH_CRYPT)
-                .put(WLConstants.Forms.COMPRESS_RESPONSE)
-                .put(WLConstants.Forms.PARAMETERS,
-                        SerializationUtils.serializeToString(getParameters(procedure, payload)))
-                .build();
-        final HttpResponse httpResponse = apiClient.getClient()
-                .request(config.getEndpointUrl() + getApiPath(WLConstants.Url.QUERY))
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_TYPE)
-                .header(WLConstants.Headers.X_WL_APP_VERSION, WLConstants.WL_APP_VERSION)
-                .header(WLConstants.Storage.WL_INSTANCE_ID, storage.getWlInstanceId())
-                .body(request.serialize())
-                .post(HttpResponse.class);
+    private <T> T query(
+            final String procedure, final Class<T> entityClass, final JSONObject payload) {
+        final Form request =
+                Form.builder()
+                        .put(WLConstants.Forms.ADAPTER, WLConstants.Forms.ADAPTER_FACADE)
+                        .put(WLConstants.Forms.PROCEDURE, WLConstants.Forms.FETCH_CRYPT)
+                        .put(WLConstants.Forms.COMPRESS_RESPONSE)
+                        .put(
+                                WLConstants.Forms.PARAMETERS,
+                                SerializationUtils.serializeToString(
+                                        getParameters(procedure, payload)))
+                        .build();
+        final HttpResponse httpResponse =
+                apiClient
+                        .getClient()
+                        .request(config.getEndpointUrl() + getApiPath(WLConstants.Url.QUERY))
+                        .header(
+                                HttpHeaders.CONTENT_TYPE,
+                                MediaType.APPLICATION_FORM_URLENCODED_TYPE)
+                        .header(WLConstants.Headers.X_WL_APP_VERSION, WLConstants.WL_APP_VERSION)
+                        .header(WLConstants.Storage.WL_INSTANCE_ID, storage.getWlInstanceId())
+                        .body(request.serialize())
+                        .post(HttpResponse.class);
 
-        final QueryResponse responseBody = WLUtils.encasedJsonToEntity(httpResponse, QueryResponse.class);
+        final QueryResponse responseBody =
+                WLUtils.encasedJsonToEntity(httpResponse, QueryResponse.class);
 
         Preconditions.checkArgument(responseBody.getIsSuccessful());
 
-        final byte[] ciphertextBytes = EncodingUtils.decodeBase64String(responseBody.getCipherText());
+        final byte[] ciphertextBytes =
+                EncodingUtils.decodeBase64String(responseBody.getCipherText());
         final byte[] signatureBytes = EncodingUtils.decodeBase64String(responseBody.getSignature());
 
-        final boolean isSignatureValid = verifySignature(config.getPublicKey(), signatureBytes, ciphertextBytes);
+        final boolean isSignatureValid =
+                verifySignature(config.getPublicKey(), signatureBytes, ciphertextBytes);
         if (!isSignatureValid) {
             logger.error("Signature for symmetric encryption was found to be invalid");
             throw new IllegalStateException();
@@ -106,7 +119,8 @@ public final class WLFetcher {
 
         final String plaintextString = new String(plaintext);
         // TODO consider moving to caller
-        //longLogger.infoExtraLong(plaintextString, WLConstants.LogTags.WL_RECEIVED_PLAINTEXT.toTag());
+        // longLogger.infoExtraLong(plaintextString,
+        // WLConstants.LogTags.WL_RECEIVED_PLAINTEXT.toTag());
         return SerializationUtils.deserializeFromString(plaintextString, entityClass);
     }
 
@@ -136,7 +150,8 @@ public final class WLFetcher {
         return randomBytes(16);
     }
 
-    private List<QueryParameterEntity> getParameters(final String procedure, final JSONObject paylIWV) {
+    private List<QueryParameterEntity> getParameters(
+            final String procedure, final JSONObject paylIWV) {
         // Will later be found in the response dict within the 'z' field
         final String hash = generateHexstring();
         // Needed by the server to decrypt the message
@@ -148,12 +163,13 @@ public final class WLFetcher {
         // Client-side thingie; probably not important
         final byte[] z = randomBytes(32);
         // Actual message
-        final String plaintext = SerializationUtils.serializeToString(
-                new CFRequest(paylIWV.toString(), DatatypeConverter.printHexBinary(z), "0")
-        );
+        final String plaintext =
+                SerializationUtils.serializeToString(
+                        new CFRequest(
+                                paylIWV.toString(), DatatypeConverter.printHexBinary(z), "0"));
 
         // TODO consider moving to caller
-        //longLogger.infoExtraLong(plaintext, WLConstants.LogTags.WL_SENT_PLAINTEXT.toTag());
+        // longLogger.infoExtraLong(plaintext, WLConstants.LogTags.WL_SENT_PLAINTEXT.toTag());
 
         // Actual message, encrypted and base64 encoded
         final byte[] ciphertext = AES.encryptCbc(sharedAesKey, sharedAesIv, plaintext.getBytes());
@@ -171,7 +187,8 @@ public final class WLFetcher {
         return Collections.singletonList(parameter);
     }
 
-    private static boolean verifySignature(final PublicKey publicKey, byte[] signatureBytes, final byte[] ciphertext) {
+    private static boolean verifySignature(
+            final PublicKey publicKey, byte[] signatureBytes, final byte[] ciphertext) {
         final Signature signature;
         try {
             signature = Signature.getInstance("SHA1withRSA");
