ec584f234284bd07a40860593dd99831ecf09ddb
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2019-02-27 13:52:46 +0100

    fea(Alandsbanken): BankId authentication

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/crosskey/authenticator/CrossKeyBankIdAuthenticator.java
@@ -0,0 +1,115 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.crosskey.authenticator;
+
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.tink.backend.agents.rpc.Credentials;
+import se.tink.backend.aggregation.agents.BankIdStatus;
+import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
+import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
+import se.tink.backend.aggregation.agents.exceptions.errors.AuthorizationError;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.crosskey.CrossKeyApiClient;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.crosskey.CrossKeyConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.crosskey.CrossKeyConstants;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.crosskey.authenticator.rpc.BankiIdAutostartTokenResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.crosskey.authenticator.rpc.BankiIdResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.crosskey.authenticator.rpc.LoginProvidersResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.crosskey.rpc.CrossKeyResponse;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.bankid.BankIdAuthenticator;
+import se.tink.backend.aggregation.nxgen.storage.SessionStorage;
+import se.tink.libraries.serialization.utils.SerializationUtils;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.crosskey.CrossKeyConstants.MultiFactorAuthentication.AUTOSTART_TOKEN;
+
+public class CrossKeyBankIdAuthenticator
+        implements BankIdAuthenticator<BankiIdAutostartTokenResponse> {
+    public static final Logger LOGGER = LoggerFactory.getLogger(CrossKeyBankIdAuthenticator.class);
+
+    private final CrossKeyApiClient apiClient;
+    private final CrossKeyConfiguration agentConfiguration;
+    private final SessionStorage sessionStorage;
+    private final Credentials credentials;
+
+    public CrossKeyBankIdAuthenticator(
+            CrossKeyApiClient apiClient,
+            CrossKeyConfiguration agentConfiguration,
+            SessionStorage sessionStorage,
+            Credentials credentials) {
+        this.apiClient = apiClient;
+        this.agentConfiguration = agentConfiguration;
+        this.sessionStorage = sessionStorage;
+        this.credentials = credentials;
+    }
+
+    @Override
+    public BankiIdAutostartTokenResponse init(String ssn) throws AuthorizationException {
+        handleRequestFailure(apiClient.initSession());
+        handleRequestFailure(apiClient.getContent());
+
+        LoginProvidersResponse loginProviders = handleRequestFailure(apiClient.getLoginProviders());
+        if (!loginProviders.canUseBankId()) {
+            LOGGER.error(
+                    "Cannot use BankId authentication: "
+                            + SerializationUtils.serializeToString(loginProviders));
+            throw new RuntimeException(
+                    "Cannot use BankId login method: "
+                            + SerializationUtils.serializeToString(loginProviders));
+        }
+
+        BankiIdAutostartTokenResponse response = apiClient.initBankId();
+        sessionStorage.put(AUTOSTART_TOKEN, response.getAutoStartToken());
+        return response;
+    }
+
+    @Override
+    public BankIdStatus collect(BankiIdAutostartTokenResponse initBankId)
+            throws AuthenticationException, AuthorizationException {
+        BankiIdResponse bankiIdResponse = apiClient.collectBankId();
+
+        if (bankiIdResponse.getStatus().isSuccess()) {
+            return BankIdStatus.DONE;
+        }
+
+        Optional<BankIdStatus> bankIdStatus =
+                bankiIdResponse
+                        .getStatus()
+                        .getErrors()
+                        .stream()
+                        .map(
+                                s ->
+                                        CrossKeyConstants.MultiFactorAuthentication
+                                                .BANKID_ERROR_MAPPING.getOrDefault(
+                                                s, BankIdStatus.FAILED_UNKNOWN))
+                        .findAny();
+
+        bankIdStatus
+                .filter(s -> s == BankIdStatus.FAILED_UNKNOWN)
+                .ifPresent(
+                        b ->
+                                LOGGER.warn(
+                                        "Bank ID Failed with unknown error for response: "
+                                                + SerializationUtils.serializeToString(
+                                                        bankiIdResponse)));
+
+        // Should never use orElseGet as we do use FAILED_UNKNOWN and log it before if occurs
+        return bankIdStatus.orElseGet(() -> BankIdStatus.FAILED_UNKNOWN);
+    }
+
+    @Override
+    public Optional<String> getAutostartToken() {
+        return sessionStorage.get(AUTOSTART_TOKEN, String.class);
+    }
+
+    private <T extends CrossKeyResponse> T handleRequestFailure(T message)
+            throws AuthorizationException {
+        if (message.isFailure()) {
+            if (message.getStatus()
+                    .getErrors()
+                    .contains(CrossKeyConstants.MultiFactorAuthentication.NOT_AUTHORIZED_ERROR)) {
+                LOGGER.error(
+                        "Not authorized url: " + SerializationUtils.serializeToString(message));
+            }
+            throw AuthorizationError.UNAUTHORIZED.exception();
+        }
+        return message;
+    }
+}
---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/alandsbanken/AlandsBankenSEAgentTest.java
@@ -0,0 +1,29 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.alandsbanken;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+
+@Ignore
+public class AlandsBankenSEAgentTest {
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+
+    private AgentIntegrationTest.Builder builder;
+
+    @Before
+    public void setup() {
+        builder = new AgentIntegrationTest.Builder("se", "se-alandsbanken-bankid")
+                .addCredentialField(Field.Key.USERNAME, USERNAME)
+                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false);
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder.build().testRefresh();
+    }
+}
