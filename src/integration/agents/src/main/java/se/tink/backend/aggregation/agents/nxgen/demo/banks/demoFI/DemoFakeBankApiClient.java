33aec326b3c5088aa3d69a293d0ef0ede20bab4b
Melih Yalcin <melih.yalcin@tink.se>
2019-02-28 11:38:25 +0100

    Refactor: removed object mapper for serialization

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/demoFI/DemoFakeBankApiClient.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/demoFI/DemoFakeBankApiClient.java
@@ -1,8 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.demo.banks.demoFI;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import se.tink.backend.aggregation.agents.nxgen.demo.banks.demoFI.authenticator.DemoFakeBankAuthenticateResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import se.tink.backend.aggregation.agents.nxgen.demo.banks.demoFI.authenticator.rpc.DemoFakeBankAuthenticationBody;
 import se.tink.backend.aggregation.nxgen.http.RequestBuilder;
 import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
@@ -17,12 +15,9 @@ public class DemoFakeBankApiClient {
         this.client = client;
     }
 
-    public DemoFakeBankAuthenticateResponse authenticate(String username, String password) throws JsonProcessingException {
-        DemoFakeBankAuthenticationBody requestBody = new DemoFakeBankAuthenticationBody(username, password);
-        String serialized = new ObjectMapper().writeValueAsString(requestBody);
-
+    public DemoFakeBankAuthenticateResponse authenticate(DemoFakeBankAuthenticationBody authenticationBody) {
         return createRequest(DemoFakeBankConstants.Urls.AUTHENTICATE_URL)
-                .post(DemoFakeBankAuthenticateResponse.class, serialized);
+                .post(DemoFakeBankAuthenticateResponse.class, authenticationBody);
     }
 
     private RequestBuilder createRequest(URL url) {
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/demoFI/authenticator/DemoFakeBankAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/demoFI/authenticator/DemoFakeBankAuthenticator.java
@@ -1,12 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.demo.banks.demoFI.authenticator;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
-import se.tink.backend.aggregation.agents.nxgen.demo.banks.demoFI.DemoFakeBankApiClient;
 import se.tink.backend.aggregation.agents.exceptions.LoginException;
+import se.tink.backend.aggregation.agents.nxgen.demo.banks.demoFI.DemoFakeBankApiClient;
 import se.tink.backend.aggregation.agents.nxgen.demo.banks.demoFI.DemoFakeBankConstants;
-
+import se.tink.backend.aggregation.agents.nxgen.demo.banks.demoFI.authenticator.rpc.DemoFakeBankAuthenticationBody;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.password.PasswordAuthenticator;
 import se.tink.backend.aggregation.nxgen.http.HttpResponse;
 import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
@@ -24,15 +23,14 @@ public class DemoFakeBankAuthenticator implements PasswordAuthenticator {
     @Override
     public void authenticate(String username, String password) throws AuthenticationException, AuthorizationException {
         try {
-            DemoFakeBankAuthenticateResponse response = client.authenticate(username, password);
+            DemoFakeBankAuthenticateResponse response = client.authenticate(
+                    new DemoFakeBankAuthenticationBody(username, password));
 
             if(response.getStatus() == "SUCCEED") {
                 sessionStorage.put(DemoFakeBankConstants.Storage.AUTH_TOKEN, response.getToken());
             }
         } catch (HttpResponseException e) {
             HandleAuthenticationException(e);
-        } catch (JsonProcessingException e) {
-            throw new IllegalArgumentException("Credentials are not correct`");
         }
     }
 
