588ecb037e4054cbc6b542e87a23389f38413e95
Melih Yalcin <melih.yalcin@tink.se>
2019-02-28 16:48:46 +0100

    fix: Fixed the host

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/demofakebank/DemoFakeBankConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/demofakebank/DemoFakeBankConstants.java
@@ -4,8 +4,8 @@ import se.tink.backend.aggregation.nxgen.http.URL;
 
 public class DemoFakeBankConstants {
     public static class Urls {
-        //private static final URL BASE_URL = new URL("demo-financial-institute.internal.staging.aggregation.tink.network");
-        private static final URL BASE_URL = new URL("http://localhost:8090");
+        private static final URL BASE_URL = new URL("demo-financial-institute.internal.staging.aggregation.tink.network:32011");
+        //private static final URL BASE_URL = new URL("localhost:8090");
         public static final URL AUTHENTICATE_URL = BASE_URL.concatWithSeparator("authenticate");
     }
 
