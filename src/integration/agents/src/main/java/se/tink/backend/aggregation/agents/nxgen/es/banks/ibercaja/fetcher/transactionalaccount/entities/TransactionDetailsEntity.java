986386e5e18725098cf757681c6a09f17e93ea6a
Schahin Rajab <schahin.rajab@tink.se>
2019-03-01 16:34:46 +0100

    fix(ibercaja): set accurate tx descriptions

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/ibercaja/fetcher/transactionalaccount/entities/TransactionDetailsEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/ibercaja/fetcher/transactionalaccount/entities/TransactionDetailsEntity.java
@@ -1,8 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.es.banks.ibercaja.fetcher.transactionalaccount.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.ibercaja.IberCajaConstants;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
@@ -10,9 +13,8 @@ import se.tink.libraries.amount.Amount;
 
 @JsonObject
 public class TransactionDetailsEntity {
-
     @JsonProperty("ConceptoMovimiento")
-    private String description;
+    private String transactionType;
     @JsonProperty("FechaOperacion")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date dateOfTransaction;
@@ -20,12 +22,31 @@ public class TransactionDetailsEntity {
     private double amount;
     @JsonProperty("IdMovimientoPFM")
     private String idMovimientopfm;
+    @JsonProperty("Registros")
+    private List<String> descriptions;
 
-    public Transaction toTinkTransaction(){
-       return Transaction.builder().setAmount(new Amount(IberCajaConstants.currency, amount))
+    @JsonIgnore
+    public Transaction toTinkTransaction() {
+        return Transaction.builder().setAmount(new Amount(IberCajaConstants.currency, amount))
                 .setDate(dateOfTransaction)
-                .setDescription(description)
+                .setDescription(getDescription())
                 .setExternalId(idMovimientopfm)
                 .build();
+    }
+
+    @JsonIgnore
+    private String getDescription() {
+        if (descriptions != null) {
+            //some transaction descriptions have a lot of whitespace that needs to be trimmed
+            return descriptions.stream()
+                    .map(this::trimWhiteSpace)
+                    .collect(Collectors.joining(" "));
+        }
+        return transactionType;
+    }
+
+    @JsonIgnore
+    private String trimWhiteSpace(String description) {
+        return description.replaceAll("\\s+", " ");
     }
 }
