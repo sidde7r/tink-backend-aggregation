995877f8f5dcec5f31c1f2842362eb7e94f24872
gyslainzola <gyslain.zola@tink.se>
2019-03-04 14:48:44 +0100

    refactor(ErsteBankAgent): Use ArgumentManager
     Please enter the commit message for your changes. Lines starting

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/at/banks/ErsteBank/ErsteBankAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/at/banks/ErsteBank/ErsteBankAgentTest.java
@@ -1,21 +1,31 @@
 package se.tink.backend.aggregation.agents.nxgen.at.banks.ErsteBank;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
-@Ignore
 public class ErsteBankAgentTest {
 
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    private enum Arg {
+        USERNAME,
+        PASSWORD
+    }
+
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+
+    @Before
+    public void setUp() throws Exception {
+        manager.before();
+    }
 
     @Test
     public void testLoginAndRefresh() throws Exception {
         new AgentIntegrationTest.Builder("at", "at-erstebank-password")
-                .addCredentialField(Field.Key.USERNAME, USERNAME)
-                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
+                .addCredentialField(Field.Key.PASSWORD, manager.get(Arg.PASSWORD))
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false)
                 .build()
@@ -25,10 +35,15 @@ public class ErsteBankAgentTest {
     @Test
     public void testLoginAndRefreshSidentity() throws Exception {
         new AgentIntegrationTest.Builder("at", "at-erstebank-thirdparty")
-                .addCredentialField(Field.Key.USERNAME, USERNAME)
+                .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false)
                 .build()
                 .testRefresh();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
     }
 }
