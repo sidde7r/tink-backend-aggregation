5e35988568233cf36c9e1f3330fd3d14cfe24f2e
Alexander Jamm <alexander.jamm@tink.se>
2019-03-05 16:00:03 +0100

    fix(BBVA MX): BBVA agent test now using argumenthelper

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/mx/BBVAAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/mx/BBVAAgentTest.java
@@ -4,6 +4,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
 import se.tink.backend.aggregation.agents.nxgen.mx.banks.bbva.BBVAConstants;
 
 @Ignore
@@ -12,12 +13,18 @@ public class BBVAAgentTest {
     private final String PASSWORD = "";
     private final String CARD_NUMBER = "";
 
+    private final ArgumentHelper helper = new ArgumentHelper(
+            "tink.username",
+            "tink.password",
+            "tink.market",
+            "tink.provider");
+
     @Test
     public void testLogin() throws Exception {
         new AgentIntegrationTest.Builder("mx", "mx-bbva-password")
-                .addCredentialField(Field.Key.USERNAME, PHONE_NUMBER)
-                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
-                .addCredentialField(BBVAConstants.FIELDS.CARD_NUMBER, CARD_NUMBER)
+                .addCredentialField(Field.Key.USERNAME, helper.get("phoneNumber"))
+                .addCredentialField(Field.Key.PASSWORD, helper.get("password"))
+                .addCredentialField(BBVAConstants.FIELDS.CARD_NUMBER, helper.get("cardNumber"))
                 .loadCredentialsBefore(true)
                 .saveCredentialsAfter(true)
                 .build()
