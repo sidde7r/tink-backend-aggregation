ee95de55e7fcc0e991a06ad420c1da1f3f34eea3
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2019-03-06 08:53:42 +0100

    feat(BBVA): Expand AccountType mapper cases

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/BbvaConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/BbvaConstants.java
@@ -11,13 +11,38 @@ public final class BbvaConstants {
             TypeMapper.<AccountTypes>builder()
                     .put(
                             AccountTypes.CHECKING,
+                            "0000015109", // CUENTA PERSONALES - CUENTA VA CONTIGO
                             "0000011954", // CUENTA NEGOCIOS - "personal" business account
                             "0CA0000079", // CUENTA BLUE
                             "0CA0000245", // CUENTA ON LINE
                             "0000009340", // CUENTA TRADER - marked as personal account by BBVA
                             "0CA0000299", // CUENTO UNO
-                            "0000004272") // CUENTA BLUE ONLINE
-                    .put(AccountTypes.SAVINGS, "0000011102") // CUENTA METAS - goal account
+                            "0000004272", // CUENTA BLUE ONLINE
+                            "0GV0002307" // CUENTA BLUE ONLINE
+                            )
+                    .put(
+                            AccountTypes.SAVINGS, "0000011102" // CUENTA METAS - goal account
+                            )
+                    .put(
+                            AccountTypes.OTHER,
+                            "0000009156", // RECARGA - PAGO MOVIL - prepaid card
+                            "0000006412", // SEGURO PP. CONSUMO PUF - insurance
+                            "0000009719" // TARJETA AHORA MASTERCARD - debit card
+                            )
+                    .put(
+                            AccountTypes.PENSION,
+                            "0000010644" // BBVA PLAN JUBILACION 2040, PPI - pension
+                            )
+                    .put(
+                            AccountTypes.LOAN,
+                            "0GV0002475", // PRESTAMO - RESTO CONSUMO CREDICONSUMO - consumer credit
+                            "0GV0002307" // PRESTAMO - PRESTAMO 5,50% - loan
+                            )
+                    .put(
+                            AccountTypes.CREDIT_CARD,
+                            "0000010354", // CREDITO - REPSOL MAS CREDITO - credit card
+                            "0000001315" // CREDITO - TARJETA DESPUES BLUE BBVA - credit card
+                            )
                     .build();
 
     public enum Error {
