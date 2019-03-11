6fa9129cf7059b9345eebcebc6ff05956e99cb07
Sofia Johnsen <sofia.johnsen@tink.se>
2019-03-11 14:02:12 +0100

    refactor(fake-agent): Remove unnecessary annotation

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/demofakebank/fetcher/transactionalaccount/entities/AccountType.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/demofakebank/fetcher/transactionalaccount/entities/AccountType.java
@@ -1,8 +1,5 @@
 package se.tink.backend.aggregation.agents.nxgen.demo.banks.demofakebank.fetcher.transactionalaccount.entities;
 
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
 public enum AccountType {
     TRANSACTIONAL
 }
