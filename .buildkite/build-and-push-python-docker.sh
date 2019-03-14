ee329844c4db72744302e3c564237477fdbbcaa8
Gustav Westling <hello@gustav.tv>
2019-03-14 17:09:47 +0100

    chore: use gcloud wrapper
    Co-Authored-By: LarsAlmgren <lars.almgren@tink.se>

--- .buildkite/build-and-push-python-docker.sh
+++ .buildkite/build-and-push-python-docker.sh
@@ -6,6 +6,6 @@ export PATH="/usr/local/bin/google-cloud-sdk/bin:$PATH";
 echo "$GOOGLE_CLOUD_ACCOUNT_JSON" | base64 --decode | gcloud auth activate-service-account --key-file=-;
 
 echo "--- Build and push Statuspage Providers Cronjob image to gcr.io"
-docker build -t gcr.io/tink-containers/tink-backend-aggregation-statuspage-providers-cronjob jobs/cron/provider-status
+gcloud docker build -t gcr.io/tink-containers/tink-backend-aggregation-statuspage-providers-cronjob jobs/cron/provider-status
 docker tag gcr.io/tink-containers/tink-backend-aggregation-statuspage-providers-cronjob:latest "gcr.io/tink-containers/tink-backend-aggregation-statuspage-providers-cronjob:$VERSION";
 gcloud docker -- push "gcr.io/tink-containers/tink-backend-aggregation-statuspage-providers-cronjob:$VERSION";
