2ec2172ba452d17cf021ed40894a2d245ce0639c
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2019-03-14 16:39:23 +0100

    refactor(BBVA): Allow both String and CurrencyEntity currencies

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/entities/AmountEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/entities/AmountEntity.java
@@ -1,6 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.databind.JsonNode;
 import io.vavr.control.Option;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.BbvaConstants.Defaults;
 import se.tink.backend.aggregation.annotations.JsonObject;
@@ -9,21 +11,26 @@ import se.tink.libraries.amount.Amount;
 @JsonObject
 public class AmountEntity {
     private double amount;
-    private CurrencyEntity currency;
+    private String currency;
 
     public double getAmount() {
         return amount;
     }
 
-    public Option<CurrencyEntity> getCurrency() {
-        return Option.of(currency);
+    public String getCurrency() {
+        return currency;
+    }
+
+    @JsonSetter
+    public void setCurrency(JsonNode jsonNode) {
+        this.currency =
+                Option.of(jsonNode.get("id"))
+                        .map(JsonNode::asText)
+                        .getOrElse(jsonNode.asText(Defaults.CURRENCY));
     }
 
     @JsonIgnore
     public Amount toTinkAmount() {
-        final String currency =
-                getCurrency().map(CurrencyEntity::getId).getOrElse(Defaults.CURRENCY);
-
-        return new Amount(currency, amount);
+        return new Amount(getCurrency(), amount);
     }
 }
