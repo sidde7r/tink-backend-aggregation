b794fb2ef41a7a2e26127d8669c471551ba34371
Gustaf Svanerud <gustaf.svanerud@tink.se>
2019-03-14 11:29:22 +0100

    fix(starling): explicit null checks for starling configuration

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/starling/configuration/StarlingConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/starling/configuration/StarlingConfiguration.java
@@ -1,6 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Strings;
+import org.elasticsearch.common.Preconditions;
 import se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.configuration.entity.ClientConfigurationEntity;
 import se.tink.backend.aggregation.agents.utils.crypto.RSA;
 import se.tink.backend.aggregation.agents.utils.encoding.EncodingUtils;
@@ -18,18 +20,26 @@ public class StarlingConfiguration implements ClientConfiguration {
     @JsonProperty private String signingKey;
 
     public ClientConfigurationEntity getAisConfiguration() {
+
+        Preconditions.checkNotNull(aisConfiguration, "Starling AIS configuration could not load.");
         return aisConfiguration;
     }
 
     public ClientConfigurationEntity getPisConfiguration() {
+
+        Preconditions.checkNotNull(pisConfiguration, "Starling PIS configuration could not load.");
         return pisConfiguration;
     }
 
     public String getKeyUid() {
+        Preconditions.checkNotNull(
+                Strings.emptyToNull(keyUid), "Starling signing key UID could not load.");
         return keyUid;
     }
 
     public PrivateKey getSigningKey() {
+        Preconditions.checkNotNull(
+                Strings.emptyToNull(signingKey), "Starling signing key could not load.");
         return RSA.getPrivateKeyFromBytes(EncodingUtils.decodeBase64String(signingKey));
     }
 }
