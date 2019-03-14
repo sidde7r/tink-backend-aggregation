195df61835fbef50af24b11ec10a02a3eefcc204
Lars Almgren <lars.almgren@tink.se>
2019-03-14 17:30:59 +0100

    chore: add missing double dash for gcloud docker build

--- .buildkite/build-and-push-python-docker.sh
+++ .buildkite/build-and-push-python-docker.sh
@@ -6,5 +6,5 @@ export PATH="/usr/local/bin/google-cloud-sdk/bin:$PATH";
 echo "$GOOGLE_CLOUD_ACCOUNT_JSON" | base64 --decode | gcloud auth activate-service-account --key-file=-;
 
 echo "--- Build and push Statuspage Providers Cronjob image to gcr.io"
-gcloud docker build -t "gcr.io/tink-containers/tink-backend-aggregation-statuspage-providers-cronjob:$VERSION" jobs/cron/provider-status;
+gcloud docker -- build -t "gcr.io/tink-containers/tink-backend-aggregation-statuspage-providers-cronjob:$VERSION" jobs/cron/provider-status;
 gcloud docker -- push "gcr.io/tink-containers/tink-backend-aggregation-statuspage-providers-cronjob:$VERSION";
