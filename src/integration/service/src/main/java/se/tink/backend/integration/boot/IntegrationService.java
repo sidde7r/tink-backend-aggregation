fa5e785c5f5eea4e2b0ef92291d6d4349fcf9b24
Babis Kiosidis <babis.kiosidis@tink.se>
2019-03-15 08:55:41 +0100

    fea(grpc interceptor): added metrics module and bind instances

--- src/integration/service/src/main/java/se/tink/backend/integration/boot/IntegrationService.java
+++ src/integration/service/src/main/java/se/tink/backend/integration/boot/IntegrationService.java
@@ -4,24 +4,22 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import java.io.File;
 import java.util.concurrent.CountDownLatch;
-
 import com.google.common.base.Stopwatch;
 import com.google.common.collect.ImmutableList;
-import io.grpc.BindableService;
 import io.prometheus.client.hotspot.DefaultExports;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import se.tink.backend.integration.boot.configuration.Configuration;
 import se.tink.backend.integration.boot.configuration.ConfigurationUtils;
+import se.tink.backend.integration.boot.configuration.IntegrationServiceModule;
 import se.tink.backend.integration.boot.configuration.SensitiveConfiguration;
 import se.tink.backend.integration.gprcserver.configuration.GrpcServerModule;
 import se.tink.backend.libraries.healthcheckhandler.HealthCheckHandler;
 import se.tink.backend.libraries.httpserver.SimpleHTTPServer;
 import se.tink.backend.integration.gprcserver.GrpcServer;
+import se.tink.libraries.metrics.PrometheusExportServer;
 
 class IntegrationService {
     private static final Logger logger = LogManager.getLogger(IntegrationService.class);
@@ -48,11 +46,10 @@ class IntegrationService {
     private final CountDownLatch keepRunningLatch;
     private SimpleHTTPServer httpServer;
     private GrpcServer grpcServer;
-    private io.prometheus.client.exporter.HTTPServer prometheusServer;
     private Injector injector;
 
-    IntegrationService(Configuration config, SensitiveConfiguration sensitiveConfiguration) throws InterruptedException, IOException {
-        injector = Guice.createInjector(ImmutableList.of(new GrpcServerModule()));
+    IntegrationService(Configuration config, SensitiveConfiguration sensitiveConfiguration) throws Exception {
+        injector = Guice.createInjector(ImmutableList.of(new GrpcServerModule(), new IntegrationServiceModule(config, sensitiveConfiguration)));
         logger.debug("Starting Integration Service");
         logger.debug("Built with Java " + System.getProperty("java.version"));
 
@@ -64,7 +61,7 @@ class IntegrationService {
         stop();
     }
 
-    private void start(Configuration config, SensitiveConfiguration sensitiveConfiguration) throws IOException, InterruptedException {
+    private void start(Configuration config, SensitiveConfiguration sensitiveConfiguration) throws Exception {
         logger.info("Starting Servers");
 
         // Start HTTP health check service
@@ -78,9 +75,9 @@ class IntegrationService {
 
         // Start the Prometheus Exporter Server
         DefaultExports.initialize();
-        prometheusServer = new io.prometheus.client.exporter.HTTPServer(9130);
 
         grpcServer = injector.getInstance(GrpcServer.class);
+        injector.getInstance(PrometheusExportServer.class).start();
 
         // Serve over TLS if configured
         if (config.getGrpcTlsCertificatePath() != null) {
@@ -93,13 +90,15 @@ class IntegrationService {
         grpcServer.start();
     }
 
-    private void stop() throws InterruptedException {
+    private void stop() throws Exception {
         int duration = 10;
         TimeUnit unit = TimeUnit.SECONDS;
 
         Stopwatch sw = Stopwatch.createStarted();
 
-        prometheusServer.stop();
+        // stop prometheus
+        injector.getInstance(PrometheusExportServer.class).stop();
+
         final CountDownLatch shutdownLatch = new CountDownLatch(2); // same numbers as components to close
         httpServer.stop(shutdownLatch, duration, unit);
         grpcServer.stop(shutdownLatch, duration, unit);
