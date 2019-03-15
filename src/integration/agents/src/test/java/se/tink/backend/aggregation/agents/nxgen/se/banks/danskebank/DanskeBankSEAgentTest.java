64f157c3b1888bc574e248e0810fcac87e31d7c6
Sebastian Olsson <sebastian.olsson@tink.se>
2019-03-15 08:51:21 +0100

    test(se-danskebank): Added agent test

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/danskebank/DanskeBankSEAgentTest.java
@@ -0,0 +1,51 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.danskebank;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+
+public final class DanskeBankSEAgentTest {
+    private enum Arg {
+        SSN,
+        SERVICE_CODE,
+    }
+
+    private static final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+
+    private AgentIntegrationTest.Builder builder;
+
+    @Before
+    public void setup() {
+        manager.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentHelper.afterClass();
+    }
+
+    @Test
+    public void testRefreshWithBankId() throws Exception {
+        builder =
+                new AgentIntegrationTest.Builder("se", "se-danskebank-bankid")
+                        .addCredentialField(Field.Key.USERNAME, manager.get(Arg.SSN))
+                        .loadCredentialsBefore(false)
+                        .saveCredentialsAfter(false);
+        builder.build().testRefresh();
+    }
+
+    @Test
+    public void testRefreshWithServiceCode() throws Exception {
+        builder =
+                new AgentIntegrationTest.Builder("se", "se-danskebank-password")
+                        .addCredentialField(Field.Key.USERNAME, manager.get(Arg.SSN))
+                        .addCredentialField(Field.Key.PASSWORD, manager.get(Arg.SERVICE_CODE))
+                        .loadCredentialsBefore(false)
+                        .saveCredentialsAfter(false);
+        builder.build().testRefresh();
+    }
+}
