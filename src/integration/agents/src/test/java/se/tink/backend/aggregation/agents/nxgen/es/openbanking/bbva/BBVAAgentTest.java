163bc8d6075e1a33a26e143a4688d2f8eb728a9e
Ivan Furdek <i.furdek@vegaitsourcing.rs>
2019-03-20 19:19:51 +0100

    test(bbva): Added missing ignore annotation

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/openbanking/bbva/BBVAAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/openbanking/bbva/BBVAAgentTest.java
@@ -1,9 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.es.openbanking.bbva;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
+@Ignore
 public class BBVAAgentTest {
 
   private AgentIntegrationTest.Builder builder;
