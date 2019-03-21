17c2e1523f8f46c199f541ef1ac5409246d4dc7c
Babis Kiosidis <babis.kiosidis@tink.se>
2019-03-21 12:07:41 +0100

    ref(controller integration): handle checking accounts cmd

---
+++ src/integration/grpc-services/src/main/java/se/tink/backend/integration/fetchservice/controller/FetchCheckingAccountsCommand.java
@@ -0,0 +1,53 @@
+package se.tink.backend.integration.fetchservice.controller;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+
+public class FetchCheckingAccountsCommand {
+    private String operationId;
+    private AgentInfo agentInfo;
+    private Credentials credentials;
+    private AggregatorInfo aggregatorInfo;
+
+    FetchCheckingAccountsCommand(String operationId,
+            AgentInfo agentInfo, Credentials credentials,
+            AggregatorInfo aggregatorInfo) {
+        this.operationId = operationId;
+        this.agentInfo = agentInfo;
+        this.credentials = credentials;
+        this.aggregatorInfo = aggregatorInfo;
+    }
+
+    public static FetchCheckingAccountsCommand of(String operationId,
+            AgentInfo agentInfo, Credentials credentials,
+            AggregatorInfo aggregatorInfo) {
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(operationId));
+        Preconditions.checkNotNull(agentInfo);
+        Preconditions.checkNotNull(credentials);
+        Preconditions.checkNotNull(aggregatorInfo);
+
+        // FIXME: add more elaborate checks per command
+        return new FetchCheckingAccountsCommand(
+                operationId,
+                agentInfo,
+                credentials,
+                aggregatorInfo
+        );
+    }
+
+    public String getOperationId() {
+        return operationId;
+    }
+
+    public AgentInfo getAgentInfo() {
+        return agentInfo;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public AggregatorInfo getAggregatorInfo() {
+        return aggregatorInfo;
+    }
+}
---
+++ src/integration/grpc-services/src/main/java/se/tink/backend/integration/fetchservice/controller/FetchController.java
@@ -0,0 +1,31 @@
+package se.tink.backend.integration.fetchservice.controller;
+
+import com.google.inject.Inject;
+import se.tink.backend.aggregation.agents.AgentFactory;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
+import se.tink.backend.aggregation.agents.grpc.FakeIntegrationArgumentsCreator;
+import se.tink.libraries.metrics.MetricRegistry;
+
+public class FetchController {
+    private final AgentFactory agentFactory;
+    private final MetricRegistry metricRegistry;
+
+    @Inject
+    FetchController(AgentFactory agentFactory, MetricRegistry metricRegistry) {
+        this.agentFactory = agentFactory;
+        this.metricRegistry = metricRegistry;
+    }
+
+    public FetchAccountsResponse execute(FetchCheckingAccountsCommand command) throws Exception {
+        RefreshCheckingAccountsExecutor agent = (RefreshCheckingAccountsExecutor) agentFactory.createForIntegration(
+                command.getAgentInfo().getAgentClassName(),
+                FakeIntegrationArgumentsCreator.getCredReq(),
+                FakeIntegrationArgumentsCreator.getAgentContext(metricRegistry)
+        );
+        return agent.fetchCheckingAccounts();
+    }
+
+
+}
+
