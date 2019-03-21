a679b8c38df4758a5cfa8a39d8064e498c85f0a4
Babis Kiosidis <babis.kiosidis@tink.se>
2019-03-21 12:09:57 +0100

    ref(fetch service): comply with grpc models

--- src/integration/grpc-services/src/main/java/se/tink/backend/integration/fetchservice/FetchService.java
+++ src/integration/grpc-services/src/main/java/se/tink/backend/integration/fetchservice/FetchService.java
@@ -1,29 +1,35 @@
 package se.tink.backend.integration.fetchservice;
 
+import com.google.inject.Inject;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
-import java.util.Collections;
-import se.tink.backend.integration.api.models.AgentState;
-import se.tink.backend.integration.api.models.IntegrationAccounts;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
 import se.tink.backend.integration.api.rpc.CheckingAccountsResponse;
 import se.tink.backend.integration.api.rpc.IntegrationRequest;
-import se.tink.backend.integration.api.rpc.IntegrationResponse;
 import se.tink.backend.integration.api.services.FetchServiceGrpc;
+import se.tink.backend.integration.fetchservice.converters.CoreToGrpcConverter;
+import se.tink.backend.integration.fetchservice.converters.GrpcToCoreConverter;
+import se.tink.backend.integration.fetchservice.controller.FetchController;
 
 public class FetchService extends FetchServiceGrpc.FetchServiceImplBase {
+
+    private FetchController fetchController;
+
+    @Inject
+    FetchService(FetchController fetchController) {
+        this.fetchController = fetchController;
+    }
+
     @Override
     public void checkingAccounts(IntegrationRequest request,
             StreamObserver<CheckingAccountsResponse> responseObserver) {
-    responseObserver.onNext(
-        CheckingAccountsResponse.newBuilder().setIntegrationInfo(
-                IntegrationResponse.newBuilder()
-                        .setOperationId(request.getOperationId())
-                        .setState(
-                                AgentState.newBuilder()
-                                        .setState("STEP_1")
-                                        .build()))
-            .addAllAccounts(Collections.emptyList())
-            .addAccounts(IntegrationAccounts.newBuilder().build())
-            .build());
-        responseObserver.onCompleted();
+        try {
+            FetchAccountsResponse fetchAccountsResponse = fetchController.execute(GrpcToCoreConverter.convert(request));
+            responseObserver.onNext(CoreToGrpcConverter.convert(fetchAccountsResponse));
+            responseObserver.onCompleted();
+
+        } catch (Exception e) {
+            responseObserver.onError(Status.INTERNAL.withDescription(e.getMessage()).withCause(e.getCause()).asRuntimeException());
+        }
     }
 }
