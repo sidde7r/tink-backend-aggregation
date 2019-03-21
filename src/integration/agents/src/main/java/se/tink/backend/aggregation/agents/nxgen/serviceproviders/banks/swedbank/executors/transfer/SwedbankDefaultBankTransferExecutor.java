f20ad97f34bdc946c7552de50029410a284bb2b3
Annica <annica.alstermark@tink.se>
2019-03-21 10:57:24 +0100

    fix(Swedbank/transfer): Select profile based on source account

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/executors/transfer/SwedbankDefaultBankTransferExecutor.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/swedbank/executors/transfer/SwedbankDefaultBankTransferExecutor.java
@@ -14,11 +14,11 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.fetchers.transferdestination.rpc.PaymentBaseinfoResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.rpc.AbstractAccountEntity;
 import se.tink.backend.aggregation.nxgen.controllers.transfer.BankTransferExecutor;
-import se.tink.libraries.signableoperation.enums.SignableOperationStatuses;
-import se.tink.libraries.transfer.rpc.Transfer;
 import se.tink.libraries.account.AccountIdentifier;
 import se.tink.libraries.account.identifiers.SwedishIdentifier;
 import se.tink.libraries.i18n.Catalog;
+import se.tink.libraries.signableoperation.enums.SignableOperationStatuses;
+import se.tink.libraries.transfer.rpc.Transfer;
 
 public class SwedbankDefaultBankTransferExecutor extends BaseTransferExecutor implements BankTransferExecutor {
     private final Catalog catalog;
@@ -31,28 +31,21 @@ public class SwedbankDefaultBankTransferExecutor extends BaseTransferExecutor im
 
     @Override
     public Optional<String> executeTransfer(Transfer transfer) throws TransferExecutionException {
-        // this is the current implementation for transfers, only use the last profile for transfers
-        apiClient.selectTransferProfile();
+        // We'll go through all the profiles to find the one the source account belongs to.
+        // That profile will be selected so it's used going forward in the execution flow.
+        String sourceAccountId = this.getSourceAccountIdAndSelectProfile(transfer);
 
         RegisteredTransfersResponse registeredTransfers = apiClient.registeredTransfers();
         registeredTransfers.noUnsignedTransfersOrThrow();
-
-        RegisteredTransfersResponse registeredTransfersResponse = registerTransfer(transfer);
+        
+        RegisteredTransfersResponse registeredTransfersResponse = registerTransfer(transfer, sourceAccountId);
 
         signAndConfirmTransfer(registeredTransfersResponse);
         return Optional.empty();
     }
-
-    private RegisteredTransfersResponse registerTransfer(Transfer transfer) {
+    
+    private RegisteredTransfersResponse registerTransfer(Transfer transfer, String sourceAccountId) {
         PaymentBaseinfoResponse paymentBaseinfo = apiClient.paymentBaseinfo();
-
-        AccountIdentifier sourceAccount = SwedbankTransferHelper.getSourceAccount(transfer);
-        Optional<String> sourceAccountId = paymentBaseinfo.getSourceAccountId(sourceAccount);
-        if (!sourceAccountId.isPresent()) {
-            throw TransferExecutionException.builder(SignableOperationStatuses.FAILED)
-                    .setEndUserMessage(TransferExecutionException.EndUserMessage.SOURCE_NOT_FOUND)
-                    .setMessage(SwedbankBaseConstants.ErrorMessage.SOURCE_NOT_FOUND).build();
-        }
 
         AccountIdentifier destinationAccount = SwedbankTransferHelper.getDestinationAccount(transfer);
         Optional<String> destinationAccountId = paymentBaseinfo.getTransferDestinationAccountId(destinationAccount);
@@ -69,7 +62,7 @@ public class SwedbankDefaultBankTransferExecutor extends BaseTransferExecutor im
         RegisterTransferResponse registerTransfer = apiClient.registerTransfer(
                 transfer.getAmount().getValue(),
                 recipientAccountId,
-                sourceAccountId.get());
+                sourceAccountId);
 
         RegisteredTransfersResponse registeredTransfers = apiClient.registeredTransfers(
                 registerTransfer.getLinks().getNextOrThrow());
@@ -87,11 +80,13 @@ public class SwedbankDefaultBankTransferExecutor extends BaseTransferExecutor im
     }
 
     private AbstractAccountEntity createSignedRecipient(final Transfer transfer) {
+
         AccountIdentifier accountIdentifier = transfer.getDestination();
         if (accountIdentifier.getType() != AccountIdentifier.Type.SE) {
             throw TransferExecutionException.builder(SignableOperationStatuses.CANCELLED).setEndUserMessage(
                     catalog.getString("You can only make transfers to Swedish accounts")).build();
         }
+
         SwedishIdentifier destination = accountIdentifier.to(SwedishIdentifier.class);
 
         String recipientName = transferHelper.getDestinationName(transfer);
