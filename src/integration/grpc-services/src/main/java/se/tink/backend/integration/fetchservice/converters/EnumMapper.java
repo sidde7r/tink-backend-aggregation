cdf1c78a206fe97c7e9b786649b4074a8523f88b
Babis Kiosidis <babis.kiosidis@tink.se>
2019-03-21 12:08:05 +0100

    fea(integration): grpc - core converters

---
+++ src/integration/grpc-services/src/main/java/se/tink/backend/integration/fetchservice/converters/EnumMapper.java
@@ -0,0 +1,22 @@
+package se.tink.backend.integration.fetchservice.converters;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import java.util.Map;
+import se.tink.backend.integration.api.models.IntegrationCredentials;
+import se.tink.backend.integration.fetchservice.controller.Type;
+
+public class EnumMapper {
+
+
+
+    public static final Map<IntegrationCredentials.Type, Type> CREDENTIALS_TYPE = new ImmutableMap.Builder<IntegrationCredentials.Type, Type>()
+            .put(IntegrationCredentials.Type.TYPE_PASSWORD, Type.PASSWORD)
+            .put(IntegrationCredentials.Type.TYPE_THIRD_PARTY_AUTHENTICATION, Type.THIRD_PARTY_AUTHENTICATION)
+            .put(IntegrationCredentials.Type.TYPE_KEYFOB, Type.KEYFOB)
+            .put(IntegrationCredentials.Type.UNRECOGNIZED, Type.UNKNOWN)
+            .put(IntegrationCredentials.Type.TYPE_UNKNOWN, Type.UNKNOWN)
+            .build();
+
+
+}
---
+++ src/integration/grpc-services/src/main/java/se/tink/backend/integration/fetchservice/converters/GrpcToCoreConverter.java
@@ -0,0 +1,25 @@
+package se.tink.backend.integration.fetchservice.converters;
+
+import se.tink.backend.integration.api.rpc.IntegrationRequest;
+import se.tink.backend.integration.fetchservice.controller.AgentInfo;
+import se.tink.backend.integration.fetchservice.controller.AggregatorInfo;
+import se.tink.backend.integration.fetchservice.controller.Credentials;
+import se.tink.backend.integration.fetchservice.controller.FetchCheckingAccountsCommand;
+import se.tink.backend.integration.fetchservice.controller.Type;
+
+public class GrpcToCoreConverter {
+
+    public static FetchCheckingAccountsCommand convert(IntegrationRequest request) {
+
+        return FetchCheckingAccountsCommand.of(
+                request.getOperationId(),
+                AgentInfo.of(request.getAgentInfo().getAgentClassName(), request.getAgentInfo().getState().getState()),
+                Credentials.of(
+                        request.getCredentials().getId(),
+                        request.getCredentials().getUserId(),
+                        request.getCredentials().getFieldsSerialized(),
+                        EnumMapper.CREDENTIALS_TYPE.getOrDefault(request.getCredentials().getType(), Type.UNKNOWN)),
+                        AggregatorInfo.of(request.getAggregatorInfo().getClientId(), request.getAggregatorInfo().getAggregatorIdentifier()));
+
+    }
+}
