38e5aacbc89df7d68a2402945ffdb2ad4a9659c3
Sofia Johnsen <sofia.johnsen@tink.se>
2019-03-22 14:34:34 +0100

    refactor(nasa): Extract reusable http logic to methods

--- src/nasa/service/src/main/java/se/tink/backend/nasa/boot/AggregationClient.java
+++ src/nasa/service/src/main/java/se/tink/backend/nasa/boot/AggregationClient.java
@@ -2,6 +2,7 @@ package se.tink.backend.nasa.boot;
 
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
@@ -20,11 +21,37 @@ public class AggregationClient {
 
     private static final String X_TINK_CLIENT_API_KEY = "X-Tink-Client-Api-Key";
     private static final String CONTENT_TYPE = "Content-type";
+    private static final String AGGREGATION =
+            "https://192.168.99.100:31011"; // TODO: Fetch from config
+    private static final String REFRESH_ENDPOINT = "/aggregation/refresh";
 
     public static HttpResponse refreshInformation(
             String apiClientKey, RefreshInformationRequest request)
             throws IOException, KeyManagementException, NoSuchAlgorithmException {
 
+        CloseableHttpClient client = createCloseableHttpClientTrustingAllHosts();
+        HttpPost httpPost = createAggregationHttpPost(apiClientKey, request, REFRESH_ENDPOINT);
+
+        CloseableHttpResponse closeableHttpResponse = client.execute(httpPost);
+        closeableHttpResponse.close();
+        return closeableHttpResponse;
+    }
+
+    private static HttpPost createAggregationHttpPost(
+            String apiClientKey, RefreshInformationRequest request, String endpoint)
+            throws UnsupportedEncodingException {
+
+        HttpPost httpPost = new HttpPost(AGGREGATION + endpoint);
+
+        String jsonRequest = new Gson().toJson(request);
+        httpPost.setEntity(new StringEntity(jsonRequest));
+        httpPost.setHeader(CONTENT_TYPE, "application/json");
+        httpPost.setHeader(X_TINK_CLIENT_API_KEY, apiClientKey);
+        return httpPost;
+    }
+
+    private static CloseableHttpClient createCloseableHttpClientTrustingAllHosts()
+            throws NoSuchAlgorithmException, KeyManagementException {
         SSLContextBuilder builder = new SSLContextBuilder();
         try {
             builder.loadTrustMaterial(null, new TrustSelfSignedStrategy());
@@ -37,18 +64,6 @@ public class AggregationClient {
                 new SSLConnectionSocketFactory(
                         builder.build(), SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
 
-        CloseableHttpClient client = HttpClientBuilder.create().setSSLSocketFactory(sslsf).build();
-
-        // TODO: Fetch stuff from config instead of hardcoded
-        HttpPost httpPost = new HttpPost("https://192.168.99.100:31011" + "/aggregation/refresh");
-
-        String jsonRequest = new Gson().toJson(request);
-        httpPost.setEntity(new StringEntity(jsonRequest));
-        httpPost.setHeader(CONTENT_TYPE, "application/json");
-        httpPost.setHeader(X_TINK_CLIENT_API_KEY, apiClientKey);
-
-        CloseableHttpResponse closeableHttpResponse = client.execute(httpPost);
-        closeableHttpResponse.close();
-        return closeableHttpResponse;
+        return HttpClientBuilder.create().setSSLSocketFactory(sslsf).build();
     }
 }
