356db3b211a151ab6d975d5579a00709495de351
Lars Almgren <lars.almgren@tink.se>
2019-03-22 17:45:51 +0100

    feat(test): add demo credit card fetcher

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/agents/demo/fetchers/NextGenerationDemoCreditCardFetcher.java
@@ -0,0 +1,67 @@
+package se.tink.backend.aggregation.nxgen.agents.demo.fetchers;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import se.tink.backend.agents.rpc.Account;
+import se.tink.backend.aggregation.nxgen.agents.demo.DemoConstants;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoCreditCardAccount;
+import se.tink.backend.aggregation.nxgen.agents.demo.demogenerator.DemoAccountFactory;
+import se.tink.backend.aggregation.nxgen.agents.demo.demogenerator.PurchaseHistoryGenerator;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.TransactionPaginator;
+import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
+import se.tink.libraries.date.DateUtils;
+import se.tink.libraries.i18n.Catalog;
+
+public class NextGenerationDemoCreditCardFetcher implements AccountFetcher<CreditCardAccount>,
+        TransactionPaginator<CreditCardAccount> {
+
+    private static final String BASE_PATH = DemoConstants.BASE_PATH;
+    private final List<Account> accounts;
+    private static final int YEARS_BACK_TO_FETCH = -3;
+    private static final int CERTAIN_DATE_OFFSET_DAYS = 29;
+    private final PurchaseHistoryGenerator purchaseHistoryGenerator;
+    private final Catalog catalog;
+    private final List<DemoCreditCardAccount> creditCardAccountDefinition;
+
+    public NextGenerationDemoCreditCardFetcher(List<Account> accounts, Catalog catalog,
+            List<DemoCreditCardAccount> creditCardAccountDefinition) {
+        this.accounts = accounts;
+        this.purchaseHistoryGenerator = new PurchaseHistoryGenerator(BASE_PATH);
+        this.catalog = catalog;
+        this.creditCardAccountDefinition = creditCardAccountDefinition;
+    }
+
+    @Override
+    public Collection<CreditCardAccount> fetchAccounts() {
+        return DemoAccountFactory.createCreditCardAccounts(catalog, creditCardAccountDefinition);
+    }
+
+    @Override
+    public void resetState() {
+
+    }
+
+    @Override
+    public PaginatorResponse fetchTransactionsFor(CreditCardAccount account) {
+        return purchaseHistoryGenerator.generateTransactions(
+                getRefreshStartDate(account.getAccountNumber()),
+                DateUtils.getToday(),
+                account.getBalance().getCurrency());
+    }
+
+    private Date getRefreshStartDate(String accountId) {
+        Optional<Account> previouslyRefreshedAccount = accounts.stream()
+                .filter(account -> account.getAccountNumber().equals(accountId)
+                        && Objects.nonNull(account.getCertainDate()))
+                .findFirst();
+
+        return previouslyRefreshedAccount
+                .map(account -> DateUtils.addDays(account.getCertainDate(), CERTAIN_DATE_OFFSET_DAYS))
+                .orElseGet(() -> DateUtils.addYears(DateUtils.getToday(), YEARS_BACK_TO_FETCH));
+    }
+}
