d07274b0afca5be9b7e68d0daa249fbb58d7a8da
Annica <annica.alstermark@tink.se>
2019-03-25 11:38:35 +0100

    fix(SE/SDC): Ignore but log exception on init session
    Sparbanken Syd does the request as well and continues even though getting 500 in response.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/authenticator/SdcBankIdAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/authenticator/SdcBankIdAuthenticator.java
@@ -1,6 +1,10 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.authenticator;
 
 import java.util.Optional;
+import org.apache.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.tink.backend.agents.rpc.Credentials;
 import se.tink.backend.aggregation.agents.BankIdStatus;
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
@@ -10,9 +14,10 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcAp
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcSessionStorage;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.authenticator.rpc.AgreementsResponse;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.bankid.BankIdAuthenticator;
-import se.tink.backend.agents.rpc.Credentials;
+import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
 
 public class SdcBankIdAuthenticator implements BankIdAuthenticator<String> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SdcBankIdAuthenticator.class);
 
     private final SdcApiClient bankClient;
     private final SdcSessionStorage sessionStorage;
@@ -26,7 +31,16 @@ public class SdcBankIdAuthenticator implements BankIdAuthenticator<String> {
 
     @Override
     public String init(String ssn) throws BankIdException, AuthorizationException {
-        bankClient.initSession();
+
+        // Sparbanken Syd app does this request and receives 500 in response, this doesn't
+        // result in an error in the app. Logging a warning but continuing with authentication.
+        try {
+            bankClient.initSession();
+        } catch (HttpResponseException e) {
+            if (e.getResponse().getStatus() == HttpStatus.SC_INTERNAL_SERVER_ERROR) {
+                LOGGER.warn("SDC SE: Received 500 response on init session request.");
+            }
+        }
 
         bankClient.initBankId(ssn);
 
