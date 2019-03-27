72f8f99890826b607f2fee0fd55cf9ed6a98f1cd
Melih Yalcin <melih.yalcin@tink.se>
2019-03-27 11:36:49 +0100

    Feat(cronjob): Connect to NASA from cronjob to initiate test

--- jobs/cron/demo-bank/cronjob.py
+++ jobs/cron/demo-bank/cronjob.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 import sys
+import requests
 
-print("Hello World!")
-sys.exit(0)
+response = None
+
+try:
+    response = requests.get('https://192.168.99.100:32023/initiate', verify=False)
+except requests.exceptions.ConnectionError as error:
+    print("Failed to connect to NASA: " + format(error))
+    sys.exit(1)
+
+if(response.status_code is 200):
+    print(response.text)
+    sys.exit(0)
+else:
+    print("Some other error happened : " + str(response.status_code))
+    sys.exit(1)
