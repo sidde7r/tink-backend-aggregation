3a4cf9ca554d4287a67be15912a9f7b70fb44eb3
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2019-03-28 14:23:16 +0100

    doc(ukob): Add some comments

--- tools/prepare_ukob_secrets.sh
+++ tools/prepare_ukob_secrets.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+#Super dummy script that takes the UK Open Banking config as input
 FILE=$1;
 ONE_LINE="${FILE}_oneLine"
 ENCODED="${FILE}_encoded"
 
+#Remove all the new lines and then add one at the end (tr removes too much, not being compatible with unix standard)
 tr -d '\n ' < $FILE | sed '$s/ $/\n/'  > $ONE_LINE
 cat ${ONE_LINE} | base64 > ${ENCODED}
 echo "created files $ONE_LINE and $ENCODED"
