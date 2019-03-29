cfaceb54d0480b4a51f50aabe4bbac287ff127fe
Melih Yalcin <melih.yalcin@tink.se>
2019-03-29 13:23:11 +0100

    fix(nasa): enriched request object to not get 500 from aggregation

--- src/nasa/service/src/main/java/se/tink/backend/nasa/boot/NasaRequestHandler.java
+++ src/nasa/service/src/main/java/se/tink/backend/nasa/boot/NasaRequestHandler.java
@@ -10,10 +10,12 @@ import se.tink.backend.nasa.boot.rpc.Provider;
 import se.tink.backend.nasa.boot.rpc.RefreshInformationRequest;
 import se.tink.backend.nasa.boot.rpc.RefreshableItem;
 import se.tink.backend.nasa.boot.rpc.User;
+import se.tink.backend.nasa.boot.rpc.UserProfile;
 import spark.Request;
 import spark.Response;
 public class NasaRequestHandler implements NasaApi{
     private final String PROVIDER_NAME = "se-test-demo-fake-bank";
+    private final String API_CLIENT_KEY = "00000000-0000-0000-0000-000000000000";
 
     private NasaRequestHandler() {}
 
@@ -29,30 +31,27 @@ public class NasaRequestHandler implements NasaApi{
     @Override
     public Object initiate(Request request, Response response) {
         User user = new User();
+        UserProfile userProfile = new UserProfile();
+        userProfile.setLocale("en_GB");
+        user.setProfile(userProfile);
+
         Provider provider = new Provider();
         provider.setName(PROVIDER_NAME);
         provider.setClassName("nxgen.demo.banks.demofakebank.DemoFakeBankAgent");
+        provider.setMarket("SE");
 
         Credentials credentials = new Credentials();
         credentials.setProviderName(PROVIDER_NAME);
         credentials.setUsername("user");
         credentials.setPassword("password");
-        // credentialsID
-        // userID
 
-        RefreshInformationRequest refreshInformationRequest =
-                new RefreshInformationRequest(user, provider, credentials, false);
-        refreshInformationRequest.setItemsToRefresh(
-                ImmutableSet.of(RefreshableItem.CHECKING_ACCOUNTS));
-
-        HttpResponse httpResponse = null;
+        RefreshInformationRequest refreshInformationRequest = new RefreshInformationRequest(
+                user, provider, credentials, true);
+        refreshInformationRequest.setItemsToRefresh(ImmutableSet.of(RefreshableItem.CHECKING_ACCOUNTS));
 
         try {
-            httpResponse =
-                    AggregationClient.refreshInformation(
-                            "00000000-0000-0000-0000-000000000000",
-                            refreshInformationRequest); // TODO: Fetch apiKey from config
-            return httpResponse;
+            // TODO: Fetch apiKey from config
+            return AggregationClient.refreshInformation(API_CLIENT_KEY, refreshInformationRequest);
         } catch (Exception e) {
             return  e;
         }
