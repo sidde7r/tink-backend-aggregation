bd8dc9a48b67078e990a92b51b6fabf61e7a5723
Sebastian Olsson <sebastian.olsson@tink.se>
2019-04-01 13:49:55 +0200

    test(be-kbc): Removed redundant @Ignore

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/KbcAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/KbcAgentTest.java
@@ -11,7 +11,6 @@ import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
-@Ignore
 public class KbcAgentTest {
     private enum Arg {
         LOAD_BEFORE,
