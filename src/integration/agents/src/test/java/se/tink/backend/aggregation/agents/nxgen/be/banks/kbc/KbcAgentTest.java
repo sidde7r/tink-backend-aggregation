e9f9f00ffe1c1a1508cf4844d10b1e5f29661901
Sebastian Olsson <sebastian.olsson@tink.se>
2019-04-01 13:49:43 +0200

    test(be-kbc): ArgumentHelper -> ArgumentManager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/KbcAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/kbc/KbcAgentTest.java
@@ -1,42 +1,47 @@
 package se.tink.backend.aggregation.agents.nxgen.be.banks.kbc;
 
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import se.tink.backend.agents.rpc.Field;
-import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
-import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Random;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
 @Ignore
 public class KbcAgentTest {
+    private enum Arg {
+        LOAD_BEFORE,
+        SAVE_AFTER,
+        USERNAME, // 17 first digits of card number
+    }
 
     // NB  m4ri needs to be installed
     // See ../tools/libkbc_wbaes_src/README
-    private final ArgumentHelper helper = new ArgumentHelper("tink.username");
-    private final AgentIntegrationTest.Builder builder =
-            new AgentIntegrationTest.Builder("be", "be-kbc-cardreader")
-                    .loadCredentialsBefore(true)
-                    .saveCredentialsAfter(true);
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+    private AgentIntegrationTest.Builder builder;
 
     @Before
     public void before() {
-        helper.before();
+        manager.before();
+
+        builder =
+                new AgentIntegrationTest.Builder("be", "be-kbc-cardreader")
+                        .loadCredentialsBefore(Boolean.parseBoolean(manager.get(Arg.LOAD_BEFORE)))
+                        .saveCredentialsAfter(Boolean.parseBoolean(manager.get(Arg.SAVE_AFTER)));
     }
 
     @AfterClass
     public static void afterClass() {
-        se.tink.backend.aggregation.agents.framework.ArgumentHelper.afterClass();
+        ArgumentManager.afterClass();
     }
 
     @Test
     public void testRefresh() throws Exception {
-        builder.addCredentialField(Field.Key.USERNAME, helper.get("tink.username"))
+        builder.addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
                 .setUserLocale(getRandomSupportedLanguage())
                 .build()
                 .testRefresh();
