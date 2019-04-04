4482ac019bde9ecac384041a12374bbf31feca73
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2019-04-04 16:49:36 +0200

    refactor(IngBE): Un-nest lambda

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/ing/fetcher/IngAccountsFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/ing/fetcher/IngAccountsFetcher.java
@@ -3,8 +3,10 @@ package se.tink.backend.aggregation.agents.nxgen.be.openbanking.ing.fetcher;
 import java.util.Collection;
 import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.be.openbanking.ing.IngApiClient;
+import se.tink.backend.aggregation.agents.nxgen.be.openbanking.ing.fetcher.entities.AccountEntity;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.libraries.amount.Amount;
 
 public class IngAccountsFetcher implements AccountFetcher<TransactionalAccount> {
 
@@ -19,10 +21,14 @@ public class IngAccountsFetcher implements AccountFetcher<TransactionalAccount> 
     @Override
     public Collection<TransactionalAccount> fetchAccounts() {
         return client.fetchAccounts().getAccounts(currency).stream()
-                .map(
-                        a ->
-                                a.toTinkAccount(
-                                        client.fetchBalances(a).getBalanceAmount(a.getCurrency())))
+                .map(this::enrichAccountWithBalance)
                 .collect(Collectors.toList());
+    }
+
+    private TransactionalAccount enrichAccountWithBalance(AccountEntity account) {
+        final Amount balance =
+                client.fetchBalances(account).getBalanceAmount(account.getCurrency());
+
+        return account.toTinkAccount(balance);
     }
 }
