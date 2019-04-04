8c9d7d7abcef1199fb5c2501eb15f2496cdc3571
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2019-04-04 17:13:16 +0200

    refactor(IngBE): Simplify account balance parsing logic

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/ing/fetcher/IngAccountsFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/ing/fetcher/IngAccountsFetcher.java
@@ -1,9 +1,12 @@
 package se.tink.backend.aggregation.agents.nxgen.be.openbanking.ing.fetcher;
 
+import static com.google.common.base.Predicates.or;
+
 import java.util.Collection;
 import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.be.openbanking.ing.IngApiClient;
 import se.tink.backend.aggregation.agents.nxgen.be.openbanking.ing.fetcher.entities.AccountEntity;
+import se.tink.backend.aggregation.agents.nxgen.be.openbanking.ing.fetcher.entities.BalancesEntity;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 import se.tink.libraries.amount.Amount;
@@ -27,7 +30,16 @@ public class IngAccountsFetcher implements AccountFetcher<TransactionalAccount> 
 
     private TransactionalAccount enrichAccountWithBalance(AccountEntity account) {
         final Amount balance =
-                client.fetchBalances(account).getBalanceAmount(account.getCurrency());
+                client.fetchBalances(account).getBalances().stream()
+                        .filter(b -> b.getCurrency().equalsIgnoreCase(currency))
+                        .filter(
+                                or(
+                                        BalancesEntity::isExpected,
+                                        BalancesEntity::isInterimBooked,
+                                        BalancesEntity::isClosingBooked))
+                        .map(BalancesEntity::getAmount)
+                        .findFirst()
+                        .orElse(new Amount(currency, 0));
 
         return account.toTinkAccount(balance);
     }
