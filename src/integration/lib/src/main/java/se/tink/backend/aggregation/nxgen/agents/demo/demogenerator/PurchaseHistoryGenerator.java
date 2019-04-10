5c444dfdbbd53290156304fc060d2f46baa52175
Mathew Clegg <mathew.clegg@live.se>
2019-04-10 10:35:58 +0200

    feat: Added recurring costs and salary

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/agents/demo/demogenerator/PurchaseHistoryGenerator.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/agents/demo/demogenerator/PurchaseHistoryGenerator.java
@@ -87,6 +87,11 @@ public class PurchaseHistoryGenerator {
             transactions.addAll(generateOneDayOfTransactions(dateCursor, currency));
         }
 
+        transactions.addAll(addMontlyRecurringCost(from, to, "Salary", 3500));
+        transactions.addAll(addMontlyRecurringCost(from, to, "Netflix", -9.99));
+        transactions.addAll(addMontlyRecurringCost(from, to, "Spotify", -9.99));
+        transactions.addAll(addMontlyRecurringCost(from, to, "Gym Membership", -45.99));
+
         return PaginatorResponseImpl.create(transactions, false);
     }
 
@@ -114,5 +119,26 @@ public class PurchaseHistoryGenerator {
                         .collect(toList());
 
         return PaginatorResponseImpl.create(transactions, false);
+    }
+
+    private List<Transaction> addMontlyRecurringCost(
+            Date from, Date to, String name, double amount) {
+        String currency = "EUR";
+        int numberOfMonths = (int) DateUtils.getNumberOfMonthsBetween(from, to);
+        List<Transaction> transactions =
+                IntStream.range(0, numberOfMonths)
+                        .mapToObj(
+                                i ->
+                                        Transaction.builder()
+                                                .setAmount(new Amount(currency, amount))
+                                                .setPending(false)
+                                                .setDescription(name)
+                                                .setDate(
+                                                        DateUtils.addMonths(
+                                                                DateUtils.getToday(), -i))
+                                                .build())
+                        .collect(toList());
+
+        return transactions;
     }
 }
