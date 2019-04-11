7376e009278a725f38034b50631d3b2d9e317046
Sebastian Olsson <sebastian.olsson@tink.se>
2019-04-11 17:48:57 +0200

    style(nxgen/demo): google-java-format formatting

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/demofakebank/fetcher/transactionalaccount/rpc/DemoFakeBankAccountsResponseTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/demofakebank/fetcher/transactionalaccount/rpc/DemoFakeBankAccountsResponseTest.java
@@ -1,14 +1,14 @@
 package se.tink.backend.aggregation.agents.nxgen.demo.banks.demofakebank.fetcher.transactionalaccount.rpc;
 
-import com.google.common.collect.ImmutableList;
-import java.util.List;
-import org.junit.Test;
-import se.tink.backend.aggregation.agents.nxgen.demo.banks.demofakebank.fetcher.transactionalaccount.entities.FakeAccount;
-
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.nxgen.demo.banks.demofakebank.fetcher.transactionalaccount.entities.FakeAccount;
 
 public class DemoFakeBankAccountsResponseTest {
 
