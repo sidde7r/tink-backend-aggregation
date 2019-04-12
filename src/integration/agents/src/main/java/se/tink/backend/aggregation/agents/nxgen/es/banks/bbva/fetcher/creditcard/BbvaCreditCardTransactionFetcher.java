773fb7534ad838085651bcfab4ca74bd7a55827b
Annica <annica.alstermark@tink.se>
2019-04-12 14:49:08 +0200

    fix(BBVA): Handle empty response when fetching cc trx

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/fetcher/creditcard/BbvaCreditCardTransactionFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/fetcher/creditcard/BbvaCreditCardTransactionFetcher.java
@@ -1,9 +1,12 @@
 package se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.fetcher.creditcard;
 
+import org.apache.http.HttpStatus;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.BbvaApiClient;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginator;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginatorResponse;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginatorResponseImpl;
 import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
+import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
 
 public class BbvaCreditCardTransactionFetcher
         implements TransactionKeyPaginator<CreditCardAccount, String> {
@@ -16,6 +19,15 @@ public class BbvaCreditCardTransactionFetcher
     @Override
     public TransactionKeyPaginatorResponse<String> getTransactionsFor(
             CreditCardAccount account, String key) {
-        return apiClient.fetchCreditCardTransactions(account, key);
+
+        try {
+            return apiClient.fetchCreditCardTransactions(account, key);
+        } catch (HttpResponseException e) {
+            if (e.getResponse().getStatus() == HttpStatus.SC_NO_CONTENT) {
+                return TransactionKeyPaginatorResponseImpl.createEmpty();
+            }
+
+            throw e;
+        }
     }
 }
