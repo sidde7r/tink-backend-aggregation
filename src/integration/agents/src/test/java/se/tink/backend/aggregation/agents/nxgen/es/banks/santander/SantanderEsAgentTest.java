e857237c283760cfe44b3c36356700bbeccbbd18
Mattias Hird <mattias.hird@tink.se>
2019-04-17 10:35:57 +0200

    refactor(santanderES): Update agent test to use argument manager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/santander/SantanderEsAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/santander/SantanderEsAgentTest.java
@@ -1,22 +1,43 @@
 package se.tink.backend.aggregation.agents.nxgen.es.banks.santander;
 
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.agents.rpc.Field.Key;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
 @Ignore
 public class SantanderEsAgentTest {
+    private enum Arg {
+        USERNAME,
+        PASSWORD
+    }
+
+    private final ArgumentManager<Arg> helper = new ArgumentManager<>(Arg.values());
     private final String USERNAME = "";
     private final String PASSWORD = "";
 
+    private final AgentIntegrationTest.Builder builder =
+            new AgentIntegrationTest.Builder("es", "es-bancosantander-password")
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false);
+
+    @Before
+    public void before() {
+        helper.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
     @Test
     public void testLogin() throws Exception {
-        new AgentIntegrationTest.Builder("es", "es-bancosantander-password")
-                .addCredentialField(Field.Key.USERNAME, USERNAME)
-                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
-                .loadCredentialsBefore(false)
-                .saveCredentialsAfter(true)
+        builder.addCredentialField(Key.USERNAME, helper.get(Arg.USERNAME))
+                .addCredentialField(Key.PASSWORD, helper.get(Arg.PASSWORD))
                 .build()
                 .testRefresh();
     }
