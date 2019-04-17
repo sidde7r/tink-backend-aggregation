125678e707531bed94c31b362e32591fc4527f0b
Melih Yalcin <melih.yalcin@tink.se>
2019-04-17 17:20:49 +0200

    feat(cronjob): change the url for aggregation

--- jobs/cron/connectivity/cronjob.py
+++ jobs/cron/connectivity/cronjob.py
@@ -5,16 +5,32 @@ import requests
 
 response = None
 
-try:
+if (len(sys.argv) < 3):
+    print("Not enough argument: {}".format(sys.argv))
+    sys.exit(1)
+
+cluster_id = sys.argv[1]
+environment = sys.argv[2]
+
+request_url = None;
+verify = None;
+
+if (cluster_id == "local" and environment == "development"):
     request_url = 'https://192.168.99.100:31011/monitoring/connectivity'
-    response = requests.get(request_url, verify=False)
+    verify = "False"
+else:
+    request_url = "https://aggregation2.{}.{}.tink.se".format(environment, cluster_id)
+    verify = "True"
+
+try:
+    response = requests.get(request_url, verify)
 except requests.exceptions.ConnectionError as error:
-    print("Failed to connect to Aggregation Service: " + format(error))
+    print("Failed to connect to Aggregation Service: {}".format(error))
     sys.exit(1)
 
 if(response.status_code is 200):
     print(response.text)
     sys.exit(0)
 else:
-    print("Some other error happened : " + str(response.status_code))
+    print("Some other error happened : {}".format(response.status_code))
     sys.exit(1)
