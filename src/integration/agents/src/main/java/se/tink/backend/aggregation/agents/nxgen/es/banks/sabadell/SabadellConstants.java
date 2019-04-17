19a0252e4c6ca8b5ddb079c1b0e17f71dd0f8f57
Mauritz Zachrisson <mauritz.zachrisson@tink.se>
2019-04-17 15:30:03 +0200

    fix(sabadell): Update JSON schema version

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/sabadell/SabadellConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/sabadell/SabadellConstants.java
@@ -45,7 +45,7 @@ public final class SabadellConstants {
     }
 
     public static final class Headers {
-        public static final String SABADELL_ACCEPT = "application/vnd.idk.bsmobil-v1852+json";
+        public static final String SABADELL_ACCEPT = "application/vnd.idk.bsmobil-v1921+json";
     }
 
     public enum QueryParamPairs {
