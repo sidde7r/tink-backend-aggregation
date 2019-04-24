28b661670d34367c62ddb60fd75b5abaf26885e5
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2019-04-24 15:41:14 +0200

    fea(ukobv31): Make Rbs related banks use aisv3.1

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/ulster/UlsterAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/ulster/UlsterAgent.java
@@ -7,11 +7,13 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.uko
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v11.UkOpenBankingV11Pis;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v20.UkOpenBankingV20Agent;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v20.UkOpenBankingV20Ais;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UkOpenBankingV31Agent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UkOpenBankingV31Ais;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
 import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
-public class UlsterAgent extends UkOpenBankingV20Agent {
+public class UlsterAgent extends UkOpenBankingV31Agent {
     public UlsterAgent(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
         super(request, context, signatureKeyPair);
@@ -19,7 +21,7 @@ public class UlsterAgent extends UkOpenBankingV20Agent {
 
     @Override
     protected UkOpenBankingAis makeAis() {
-        return new UkOpenBankingV20Ais();
+        return new UkOpenBankingV31Ais();
     }
 
     @Override
