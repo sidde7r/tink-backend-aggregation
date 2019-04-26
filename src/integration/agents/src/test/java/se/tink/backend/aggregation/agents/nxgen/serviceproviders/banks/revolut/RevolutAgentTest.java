0035fcad24ab2c52bd247a3d7e761335da912600
Mauritz Zachrisson <mauritz.zachrisson@tink.se>
2019-04-26 15:10:51 +0200

    feat(revolut): Identity data for Revolut service provider

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/revolut/RevolutAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/revolut/RevolutAgentTest.java
@@ -1,24 +1,41 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.revolut;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
-@Ignore
 public class RevolutAgentTest {
 
-    private final String USERNAME =
-            ""; // phone number +{country code}{phone number}, eg. +46701234567
-    private final String PASSWORD = ""; // four digits, eg. 1234
+    private enum Arg {
+        USERNAME, // phone number +{country code}{phone number}, eg. +46701234567
+        PASSWORD // four digits, eg. 1234
+    }
+
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+
+    @Before
+    public void setup() {
+        manager.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
 
     @Test
     public void testLoginRefresh() throws Exception {
         new AgentIntegrationTest.Builder("uk", "uk-revolut-password")
-                .addCredentialField(Field.Key.USERNAME, USERNAME)
-                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
+                .addCredentialField(Field.Key.PASSWORD, manager.get(Arg.PASSWORD))
                 .loadCredentialsBefore(true)
                 .saveCredentialsAfter(true)
+                .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
                 .build()
                 .testRefresh();
     }
