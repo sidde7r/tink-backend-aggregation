bfc3c57f1d624cfb49db0ea0177287eaab4c213c
Mauritz Zachrisson <mauritz.zachrisson@tink.se>
2019-04-26 17:34:21 +0200

    feat(amex): Upgrade Amex ES :es: to v62

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/creditcards/amex/AmericanExpressESAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/creditcards/amex/AmericanExpressESAgent.java
@@ -1,18 +1,15 @@
 package se.tink.backend.aggregation.agents.nxgen.es.creditcards.amex;
 
 import se.tink.backend.aggregation.agents.AgentContext;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v45.AmericanExpressAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.AmericanExpressV62Agent;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
-import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
-public class AmericanExpressESAgent extends AmericanExpressAgent {
+public class AmericanExpressESAgent extends AmericanExpressV62Agent {
 
     public AmericanExpressESAgent(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
         super(request, context, signatureKeyPair, new AmericanExpressESConfiguration());
     }
 
-    @Override
-    protected void configureHttpClient(TinkHttpClient client) {}
 }
--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/creditcards/amex/AmericanExpressESAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/creditcards/amex/AmericanExpressESAgentTest.java
@@ -1,20 +1,41 @@
 package se.tink.backend.aggregation.agents.nxgen.es.creditcards.amex;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
-@Ignore
 public class AmericanExpressESAgentTest {
+
+    private enum Arg {
+        USERNAME,
+        PASSWORD
+    }
+
+    private ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+
+    @Before
+    public void setUp() throws Exception {
+        manager.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
 
     private AgentIntegrationTest.Builder builder() {
         return new AgentIntegrationTest.Builder(
                         AmericanExpressESConstants.MARKET, AmericanExpressESConstants.PROVIDER_NAME)
-                .addCredentialField(Field.Key.USERNAME, "")
-                .addCredentialField(Field.Key.PASSWORD, "")
+                .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
+                .addCredentialField(Field.Key.PASSWORD, manager.get(Arg.PASSWORD))
                 .loadCredentialsBefore(false)
-                .saveCredentialsAfter(false);
+                .saveCredentialsAfter(false)
+                .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                .addRefreshableItems(RefreshableItem.IDENTITY_DATA);
     }
 
     @Test
