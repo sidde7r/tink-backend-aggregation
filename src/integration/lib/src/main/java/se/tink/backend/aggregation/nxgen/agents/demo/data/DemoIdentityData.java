dd4101ad2b4a884ea8422888e3f990be25a5b9f0
Melih Yalcin <melih.yalcin@tink.se>
2019-04-29 11:10:48 +0200

    feat(identity): add the identity fetcher to test banks

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/agents/demo/data/DemoIdentityData.java
@@ -0,0 +1,10 @@
+package se.tink.backend.aggregation.nxgen.agents.demo.data;
+
+import java.time.LocalDate;
+import java.util.List;
+import se.tink.libraries.identitydata.NameElement;
+
+public interface DemoIdentityData {
+    List<NameElement> getNameElements();
+    LocalDate getDateOfBirth();
+}
