ce54c6ca420299c9de71930b56dcfbffb47bdb56
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2019-04-29 12:11:59 +0200

    fea(v31): Create new v31 agents

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/rbs/RbsV31Agent.java
@@ -0,0 +1,36 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.rbs;
+
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingAis;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingPis;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v11.UkOpenBankingV11Pis;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v20.UkOpenBankingV20Ais;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UkOpenBankingV31Agent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UkOpenBankingV31Ais;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class RbsV31Agent extends UkOpenBankingV31Agent {
+    public RbsV31Agent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected UkOpenBankingAis makeAis() {
+        return new UkOpenBankingV31Ais();
+    }
+
+    @Override
+    protected Optional<UkOpenBankingPis> makePis() {
+        return Optional.of(new UkOpenBankingV11Pis());
+    }
+
+    @Override
+    protected void configureAisHttpClient(TinkHttpClient httpClient) {}
+
+    @Override
+    protected void configurePisHttpClient(TinkHttpClient httpClient) {}
+}
