fa2cb1d6c619399e538e53645d680e11e2320e37
Annica <annica.alstermark@tink.se>
2019-04-30 14:25:26 +0200

    chore(BBVA): Update constants to latest version of app

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/BbvaConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/BbvaConstants.java
@@ -111,8 +111,8 @@ public final class BbvaConstants {
         BBVA_USER_AGENT(
                 "BBVA-User-Agent",
                 "%s;iPhone;Apple;iPhone9,3;750x1334;iOS;10.1.1;WOODY;6.14.1;xhdpi"),
-        ORIGIN("Origin", "https://movil.bbva.es"),
-        REFERER("Referer", "https://movil.bbva.es/versions/woody/6.14.5/index.html");
+        ORIGIN("Origin", "https://beta.movil.bbva.es"),
+        REFERER("Referer", "https://beta.movil.bbva.es/versions/woody/6.18.3/index.html");
 
         private final String key;
         private final String value;
