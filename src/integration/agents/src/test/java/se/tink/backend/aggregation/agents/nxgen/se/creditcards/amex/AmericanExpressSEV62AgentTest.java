04817347687685c0abe353402becc37ca1b15dcb
Sebastian Olsson <sebastian.olsson@tink.se>
2019-05-02 15:14:17 +0200

    test(amex): Introduced ArgumentManager in Amex SE test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/amex/AmericanExpressSEV62AgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/amex/AmericanExpressSEV62AgentTest.java
@@ -1,28 +1,39 @@
 package se.tink.backend.aggregation.agents.nxgen.se.creditcards.amex;
 
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
-@Ignore
-public class AmericanExpressSEV62AgentTest {
+public final class AmericanExpressSEV62AgentTest {
+    private enum Arg {
+        USERNAME,
+        PASSWORD,
+        PROVIDER, // "americanexpress" or "saseurobonusamericanexpress"
+    }
 
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    private final ArgumentManager<Arg> helper = new ArgumentManager<>(Arg.values());
 
     private AgentIntegrationTest.Builder builder;
 
     @Before
     public void setup() {
+        helper.before();
+
         builder =
-                new AgentIntegrationTest.Builder("se", "americanexpress")
-                        .addCredentialField(Field.Key.USERNAME, USERNAME)
-                        .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                new AgentIntegrationTest.Builder("se", helper.get(Arg.PROVIDER))
+                        .addCredentialField(Field.Key.USERNAME, helper.get(Arg.USERNAME))
+                        .addCredentialField(Field.Key.PASSWORD, helper.get(Arg.PASSWORD))
                         .loadCredentialsBefore(false)
                         .saveCredentialsAfter(false)
                         .doLogout(true);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
     }
 
     @Test
