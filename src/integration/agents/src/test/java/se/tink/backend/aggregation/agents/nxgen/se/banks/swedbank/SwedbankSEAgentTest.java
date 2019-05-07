df3940834570c1492a80159269efdfb54fd81111
Annica <annica.alstermark@tink.se>
2019-05-07 12:06:29 +0200

    test(Swedbank): Use arg manager and add refreshable items

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/SwedbankSEAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/SwedbankSEAgentTest.java
@@ -1,19 +1,42 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank;
 
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
 @Ignore
 public class SwedbankSEAgentTest {
+
+    private enum Arg {
+        USERNAME // 12 digit SSN
+    }
+
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+
+    @Before
+    public void setUp() throws Exception {
+        manager.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
     @Test
     public void testRegisterAndRefresh() throws Exception {
         AgentIntegrationTest.Builder builder =
                 new AgentIntegrationTest.Builder("se", "swedbank-bankid")
-                        .addCredentialField(Field.Key.USERNAME, "yyyyMMddnnnn")
+                        .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
                         .loadCredentialsBefore(false)
-                        .saveCredentialsAfter(true);
+                        .saveCredentialsAfter(true)
+                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                        .addRefreshableItems(RefreshableItem.IDENTITY_DATA);
 
         builder.build().testRefresh();
     }
