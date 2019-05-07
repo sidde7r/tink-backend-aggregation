14c5135fad653818aee082394a69839b2d1fccc0
Alexander Jamm <alexander.jamm@tink.se>
2019-05-07 10:14:56 +0200

    fix(SDC DK): Defaulting to SMS authenticator for SDC in DK

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/sdcdk/SdcDkConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/sdcdk/SdcDkConstants.java
@@ -1,6 +1,5 @@
 package se.tink.backend.aggregation.agents.nxgen.dk.banks.sdcdk;
 
-import com.google.common.collect.ImmutableList;
 import se.tink.backend.aggregation.agents.utils.log.LogTag;
 
 public class SdcDkConstants {
@@ -14,11 +13,5 @@ public class SdcDkConstants {
     public static class Fetcher {
         public static final LogTag LOAN_LOGGING = LogTag.from("#loan_logging_sdc_dk");
         public static final LogTag INVESTMENTS_LOGGING = LogTag.from("#investment_logging_sdc_dk");
-    }
-
-    public static class Authentication {
-        // SDC banks supporting SMS pinning of devices
-        public static final ImmutableList<String> BANKS_WITH_SMS_AUTHENTICATION =
-                ImmutableList.of("0400", "9090", "9335");
     }
 }
