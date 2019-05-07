51307617fb212fb5fe3533ee29775460538cc4dc
Annica <annica.alstermark@tink.se>
2019-05-07 14:29:54 +0200

    feat(Icabanken): Add identity data fetching

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/icabanken/fetcher/identitydata/IcaBankenIdentityDataFetcher.java
@@ -0,0 +1,45 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.icabanken.fetcher.identitydata;
+
+import java.util.List;
+import org.apache.commons.collections4.CollectionUtils;
+import se.tink.backend.aggregation.agents.FetchIdentityDataResponse;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.icabanken.IcaBankenApiClient;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.icabanken.fetcher.accounts.entities.AccountEntity;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.icabanken.fetcher.accounts.entities.AccountOwnerEntity;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.identitydata.IdentityDataFetcher;
+import se.tink.libraries.identitydata.IdentityData;
+import se.tink.libraries.identitydata.countries.SeIdentityData;
+
+public class IcaBankenIdentityDataFetcher implements IdentityDataFetcher {
+
+    private final IcaBankenApiClient apiClient;
+    private final String ssnFromCredential;
+
+    public IcaBankenIdentityDataFetcher(IcaBankenApiClient apiClient, String ssnFromCredential) {
+
+        this.apiClient = apiClient;
+        this.ssnFromCredential = ssnFromCredential;
+    }
+
+    @Override
+    public IdentityData fetchIdentityData() {
+        List<AccountEntity> ownAccounts = apiClient.fetchAccounts().getOwnAccounts();
+
+        // Use SSN from credential if user has no own accounts
+        if (CollectionUtils.isEmpty(ownAccounts)) {
+            return SeIdentityData.of("", ssnFromCredential);
+        }
+
+        // Icabanken splits accounts types on "own", "joint", and "minor". This should make it safe
+        // to use the account owner data from one of the own account entities for identity data.
+        AccountEntity ownAccountEntity = ownAccounts.get(0);
+        AccountOwnerEntity accountOwner = ownAccountEntity.getAccountOwner();
+
+        return SeIdentityData.of(
+                accountOwner.getFullName(), accountOwner.getNationalIdWithCentury());
+    }
+
+    public FetchIdentityDataResponse getIdentityDataResponse() {
+        return new FetchIdentityDataResponse(fetchIdentityData());
+    }
+}
