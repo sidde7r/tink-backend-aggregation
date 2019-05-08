fc3195d4b245f6e1bef600c8c97231c88bb8053a
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2019-05-08 16:37:58 +0200

    ref(FailuresEntity): Use GenericTypeMapper

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/nordeabase/entities/FailuresEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/nordeabase/entities/FailuresEntity.java
@@ -1,64 +1,58 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.nordeabase.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.common.collect.ImmutableMap;
+import java.util.Optional;
 import java.util.function.BiFunction;
-import se.tink.backend.aggregation.agents.exceptions.payment.*;
+import org.assertj.core.util.Strings;
+import se.tink.backend.aggregation.agents.exceptions.payment.CreditorValidationException;
+import se.tink.backend.aggregation.agents.exceptions.payment.DebtorValidationException;
+import se.tink.backend.aggregation.agents.exceptions.payment.PaymentException;
+import se.tink.backend.aggregation.agents.exceptions.payment.PaymentValidationException;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.nxgen.core.account.GenericTypeMapper;
 
 @JsonObject
 public class FailuresEntity {
+    @JsonIgnore
+    private static final GenericTypeMapper<BiFunction<String, Throwable, PaymentException>, String>
+            errorCodeToPaymentExceptionMapper =
+                    GenericTypeMapper
+                            .<BiFunction<String, Throwable, PaymentException>, String>
+                                    genericBuilder()
+                            .put(
+                                    (description, cause) ->
+                                            new PaymentValidationException(description, cause),
+                                    "error.validation",
+                                    "error.apikey.missing",
+                                    "error.token",
+                                    "error.token.invalid",
+                                    "error.token.expired",
+                                    "error.token.expired",
+                                    "error.resource.denied")
+                            .build();
+
     private String code;
     private String description;
     private String path;
     private String type;
 
     @JsonIgnore
-    private static final ImmutableMap<String, BiFunction<String, Throwable, PaymentException>>
-            errorCodeToPaymentException =
-                    ImmutableMap.<String, BiFunction<String, Throwable, PaymentException>>builder()
-                            .put(
-                                    "error.validation",
-                                    (description, cause) ->
-                                            new PaymentValidationException(description, cause))
-                            .put(
-                                    "error.apikey.missing",
-                                    (description, cause) ->
-                                            new PaymentAuthenticationException(description, cause))
-                            .put(
-                                    "error.token",
-                                    (description, cause) ->
-                                            new PaymentAuthenticationException(description, cause))
-                            .put(
-                                    "error.token.invalid",
-                                    (description, cause) ->
-                                            new PaymentAuthenticationException(description, cause))
-                            .put(
-                                    "error.token.expired",
-                                    (description, cause) ->
-                                            new PaymentAuthenticationException(description, cause))
-                            .put(
-                                    "error.resource.denied",
-                                    (description, cause) ->
-                                            new PaymentAuthorizationException(description, cause))
-                            .build();
-
-    @JsonIgnore
     public PaymentException buildRelevantException(Throwable cause) {
         if (code == "error.validation") {
-            if (path != null) {
+            if (!Strings.isNullOrEmpty(path)) {
                 if (path.startsWith("creditor")) {
                     return new CreditorValidationException(description, path, cause);
                 } else if (path.startsWith("debtor")) {
                     return new DebtorValidationException(description, path, cause);
-                } else {
-                    return new PaymentValidationException(description, path, cause);
                 }
+                return new PaymentValidationException(description, path, cause);
             }
-        } else if (errorCodeToPaymentException.get(code) == null) {
-            return new PaymentException(this.toString(), cause);
         }
-        return errorCodeToPaymentException.get(code).apply(description, cause);
+
+        Optional<BiFunction<String, Throwable, PaymentException>> error =
+                errorCodeToPaymentExceptionMapper.translate(code);
+        return error.map(e -> e.apply(description, cause))
+                .orElseGet(() -> new PaymentException(this.toString(), cause));
     }
 
     @JsonIgnore
