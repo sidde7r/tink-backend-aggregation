c2b3318efed58e42bc601168512103cdecd6947b
Ivan Furdek <i.furdek@vegaitsourcing.rs>
2019-05-09 11:00:31 +0200

    fix(Sibs) - Comment out ignore on a test...

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/santander/SantanderAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/santander/SantanderAgentTest.java
@@ -1,10 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.pt.openbanking.santander;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
-//@Ignore
+@Ignore
 public class SantanderAgentTest {
 
     private AgentIntegrationTest.Builder builder;
