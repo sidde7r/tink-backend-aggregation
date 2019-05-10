bb9b168b69f3aadb2cdc2bce7c0b79c3c5b338e9
ndjuricic <n.djuricic@vegaitsourcing.rs>
2019-05-10 09:17:31 +0200

    refactor(SBAB) - Removed unused imports

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/sbab/SBABAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/sbab/SBABAgentTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
+@Ignore
 public class SBABAgentTest {
 
     private final String TEST_USERNAME = "SOMEUSER";
@@ -14,18 +15,18 @@ public class SBABAgentTest {
     private AgentIntegrationTest.Builder builder;
 
     @Before
-    public void setup(){
-        builder = new AgentIntegrationTest.Builder("se", "se-sbab-oauth2")
-                .addCredentialField(Field.Key.USERNAME, TEST_USERNAME)
-                .addCredentialField(Field.Key.PASSWORD, TEST_PASSWORD)
-                .loadCredentialsBefore(false)
-                .saveCredentialsAfter(false)
-                .expectLoggedIn(false);
-
+    public void setup() {
+        builder =
+                new AgentIntegrationTest.Builder("se", "se-sbab-oauth2")
+                        .addCredentialField(Field.Key.USERNAME, TEST_USERNAME)
+                        .addCredentialField(Field.Key.PASSWORD, TEST_PASSWORD)
+                        .loadCredentialsBefore(false)
+                        .saveCredentialsAfter(false)
+                        .expectLoggedIn(false);
     }
 
     @Test
-    public void testRefresh() throws Exception{
+    public void testRefresh() throws Exception {
         builder.build().testRefresh();
     }
 }
