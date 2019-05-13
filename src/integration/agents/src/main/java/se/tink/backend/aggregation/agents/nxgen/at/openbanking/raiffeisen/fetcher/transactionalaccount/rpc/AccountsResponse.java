f64c28c36984ac59e6ee3847cde67667434f4d2b
Ivan Furdek <i.furdek@vegaitsourcing.rs>
2019-05-13 16:39:41 +0200

    fix(Raiffeisen) - Add null safety check

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/raiffeisen/fetcher/transactionalaccount/rpc/AccountsResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/raiffeisen/fetcher/transactionalaccount/rpc/AccountsResponse.java
@@ -1,6 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.at.openbanking.raiffeisen.fetcher.transactionalaccount.rpc;
 
 import java.util.List;
+import java.util.Optional;
+import org.assertj.core.util.Lists;
 import se.tink.backend.aggregation.agents.nxgen.at.openbanking.raiffeisen.fetcher.transactionalaccount.entity.account.AccountEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
@@ -10,6 +12,6 @@ public class AccountsResponse {
     private List<AccountEntity> accounts;
 
     public List<AccountEntity> getAccounts() {
-        return accounts;
+        return Optional.ofNullable(accounts).orElse(Lists.emptyList());
     }
 }
