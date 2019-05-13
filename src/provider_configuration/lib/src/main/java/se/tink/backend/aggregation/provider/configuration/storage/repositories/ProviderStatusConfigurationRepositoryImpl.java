0406c221f7842e2e48048d6db2bbf92b63ab5d9f
Gustav Westling <gustav.westling@tink.se>
2019-05-13 13:10:09 +0200

    chore: remove provider-configuration from this repository
    It has been moved to the tink-backend repository. https://github.com/tink-ab/tink-backend/pull/12163

--- src/provider_configuration/lib/src/main/java/se/tink/backend/aggregation/provider/configuration/storage/repositories/ProviderStatusConfigurationRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package se.tink.backend.aggregation.provider.configuration.storage.repositories;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-public class ProviderStatusConfigurationRepositoryImpl
-        implements ProviderStatusConfigurationRepositoryCustom {
-    @PersistenceContext private EntityManager em;
-}
