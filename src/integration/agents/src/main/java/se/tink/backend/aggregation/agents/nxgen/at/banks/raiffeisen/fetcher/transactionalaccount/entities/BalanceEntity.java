c12dbc5702d7d703cab8b909c2c2733a9b2fb2a9
Sebastian Olsson <sebastian.olsson@tink.se>
2019-05-14 09:55:21 +0200

    fix(at-raiffeisen): Amounts were set to NaN

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/banks/raiffeisen/fetcher/transactionalaccount/entities/BalanceEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/banks/raiffeisen/fetcher/transactionalaccount/entities/BalanceEntity.java
@@ -1,16 +1,19 @@
 package se.tink.backend.aggregation.agents.nxgen.at.banks.raiffeisen.fetcher.transactionalaccount.entities;
 
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.libraries.amount.Amount;
 
 @JsonObject
 public class BalanceEntity {
     private String currency;
-    private Double value;
+    private Double amount;
 
     public BalanceEntity() {}
 
-    public Amount getAmount() {
-        return new Amount(currency, value);
+    public String getCurrency() {
+        return currency;
+    }
+
+    public Double getAmount() {
+        return amount;
     }
 }
