b5840b5465c43e481ccee7ce4a3b6e7b65c5a715
Lars Almgren <lars.almgren@tink.se>
2019-05-14 08:35:53 +0200

    feat(demo agent): add psd2 redirect demo agent
    This one does not literally redirect, just uses third party app.

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/psd2/redirect/authenticator/RedirectAutoAuthenticator.java
@@ -0,0 +1,13 @@
+package se.tink.backend.aggregation.agents.nxgen.demo.banks.psd2.redirect.authenticator;
+
+import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
+import se.tink.backend.aggregation.agents.exceptions.BankServiceException;
+import se.tink.backend.aggregation.agents.exceptions.SessionException;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticator;
+
+public class RedirectAutoAuthenticator implements AutoAuthenticator {
+    @Override
+    public void autoAuthenticate() throws SessionException, BankServiceException, AuthorizationException {
+
+    }
+}
