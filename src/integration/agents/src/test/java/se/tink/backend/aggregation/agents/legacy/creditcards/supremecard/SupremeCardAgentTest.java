b460d308f4dbdf0c2e6d7f7a1d3eff5e58c17f02
Mauritz Zachrisson <mauritz.zachrisson@tink.se>
2019-05-14 13:12:06 +0200

    feat(supreme): Identity Data for Supreme Card :female_detective:

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/legacy/creditcards/supremecard/SupremeCardAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/legacy/creditcards/supremecard/SupremeCardAgentTest.java
@@ -1,22 +1,41 @@
 package se.tink.backend.aggregation.agents.creditcards.supremecard;
 
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
-import se.tink.backend.agents.rpc.CredentialsTypes;
-import se.tink.backend.aggregation.agents.AbstractAgentTest;
-import se.tink.libraries.social.security.TestSSN;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
-public class SupremeCardAgentTest extends AbstractAgentTest<SupremeCardAgent> {
-    public SupremeCardAgentTest() {
-        super(SupremeCardAgent.class);
+public class SupremeCardAgentTest {
+
+    private enum Arg {
+        USERNAME
+    }
+
+    private ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+    private AgentIntegrationTest.Builder builder;
+
+    @Before
+    public void setup() {
+        manager.before();
+        builder =
+                new AgentIntegrationTest.Builder("se", "supremecard-bankid")
+                        .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
+                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                        .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
+                        .loadCredentialsBefore(true)
+                        .saveCredentialsAfter(false);
     }
 
     @Test
-    public void testUser1AuthenticationError() throws Exception {
-        testAgentAuthenticationError(TestSSN.FH, "testtest");
+    public void testRefresh() throws Exception {
+        builder.build().testRefresh();
     }
 
-    @Test
-    public void testStartSession() throws Exception {
-        testAgent(TestSSN.AL, null, CredentialsTypes.MOBILE_BANKID);
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
     }
 }
