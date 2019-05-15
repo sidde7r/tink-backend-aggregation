c1b9c78d104ded42b2b7391d87a0883df9d84e56
Sebastian Olsson <sebastian.olsson@tink.se>
2019-05-15 20:08:47 +0200

    feat(lysa): Upgraded ArgumentHelper -> ArgumentManager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/legacy/brokers/lysa/LysaAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/legacy/brokers/lysa/LysaAgentTest.java
@@ -5,14 +5,20 @@ import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
-import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
 public class LysaAgentTest {
+
+    private enum Arg {
+        USERNAME,
+    }
+
     private final AgentIntegrationTest.Builder builder =
             new AgentIntegrationTest.Builder("se", "lysa")
                     .loadCredentialsBefore(false)
                     .saveCredentialsAfter(true);
-    private final ArgumentHelper helper = new ArgumentHelper("tink.username");
+
+    private final ArgumentManager<Arg> helper = new ArgumentManager<>(Arg.values());
 
     @Before
     public void before() {
@@ -21,12 +27,12 @@ public class LysaAgentTest {
 
     @AfterClass
     public static void afterClass() {
-        ArgumentHelper.afterClass();
+        ArgumentManager.afterClass();
     }
 
     @Test
     public void testRefresh() throws Exception {
-        builder.addCredentialField(Field.Key.USERNAME, helper.get("tink.username"))
+        builder.addCredentialField(Field.Key.USERNAME, helper.get(Arg.USERNAME))
                 .build()
                 .testRefresh();
     }
