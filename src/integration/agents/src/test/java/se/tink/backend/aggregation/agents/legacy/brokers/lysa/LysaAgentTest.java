4fbfeb347ced77dd866bd6b725d979b2001cb497
Sebastian Olsson <sebastian.olsson@tink.se>
2019-05-15 20:08:28 +0200

    feat(lysa): Removed redundant Ignore

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/legacy/brokers/lysa/LysaAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/legacy/brokers/lysa/LysaAgentTest.java
@@ -2,13 +2,11 @@ package se.tink.backend.aggregation.agents.brokers.lysa;
 
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
 
-@Ignore
 public class LysaAgentTest {
     private final AgentIntegrationTest.Builder builder =
             new AgentIntegrationTest.Builder("se", "lysa")
