8f229b8efa21620503b43ef4a55b12d71668ebb7
Sebastian Olsson <sebastian.olsson@tink.se>
2019-05-15 20:05:46 +0200

    feat(seb): Upgraded ArgumentHelper -> ArgumentManager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/legacy/banks/seb/SebApiAgentIntegrationTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/legacy/banks/seb/SebApiAgentIntegrationTest.java
@@ -5,15 +5,20 @@ import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
-import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
 public class SebApiAgentIntegrationTest {
+
+    private enum Arg {
+        USERNAME,
+    }
+
     private final AgentIntegrationTest.Builder builder =
             new AgentIntegrationTest.Builder("se", "seb-bankid")
                     .expectLoggedIn(false)
                     .loadCredentialsBefore(false)
                     .saveCredentialsAfter(true);
-    private final ArgumentHelper helper = new ArgumentHelper("tink.username");
+    private final ArgumentManager<Arg> helper = new ArgumentManager<>(Arg.values());
 
     @Before
     public void before() {
@@ -22,12 +27,12 @@ public class SebApiAgentIntegrationTest {
 
     @AfterClass
     public static void afterClass() {
-        ArgumentHelper.afterClass();
+        ArgumentManager.afterClass();
     }
 
     @Test
     public void testRefresh() throws Exception {
-        builder.addCredentialField(Field.Key.USERNAME, helper.get("tink.username"))
+        builder.addCredentialField(Field.Key.USERNAME, helper.get(Arg.USERNAME))
                 .build()
                 .testRefresh();
     }
