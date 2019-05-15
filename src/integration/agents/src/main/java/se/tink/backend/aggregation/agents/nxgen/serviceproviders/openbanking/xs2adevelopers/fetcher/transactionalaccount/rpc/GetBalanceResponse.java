d56d3bdf340f4aeb76944e7e9f8143cb4676b3df
Vojin Pesalj <woyin95@gmail.com>
2019-05-15 14:39:09 +0200

    fix(Xs2aDevelopers) Added null check in getBalances method

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/xs2adevelopers/fetcher/transactionalaccount/rpc/GetBalanceResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/xs2adevelopers/fetcher/transactionalaccount/rpc/GetBalanceResponse.java
@@ -1,6 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.fetcher.transactionalaccount.rpc;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.fetcher.transactionalaccount.entities.BalanceEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
@@ -9,6 +11,6 @@ public class GetBalanceResponse {
     private List<BalanceEntity> balances;
 
     public List<BalanceEntity> getBalances() {
-        return balances;
+        return Optional.ofNullable(balances).orElse(Collections.emptyList());
     }
 }
