f7a6de9fb287a0be5ee2a7fbb5bcb1e8496d5b7c
Mauritz Zachrisson <mauritz.zachrisson@tink.se>
2019-05-15 16:47:20 +0200

    feat(volvofinans): Identity Data for Volvofinans Bank

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/volvofinans/VolvoFinansAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/volvofinans/VolvoFinansAgentTest.java
@@ -1,22 +1,40 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.volvofinans;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
-@Ignore
 public class VolvoFinansAgentTest {
 
-    private final String USERNAME = "";
+    private enum Arg {
+        USERNAME
+    }
+
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+
+    @Before
+    public void setUp() throws Exception {
+        manager.before();
+    }
 
     @Test
     public void testLogin() throws Exception {
         new AgentIntegrationTest.Builder("se", "volvofinans-bankid")
-                .addCredentialField(Field.Key.USERNAME, USERNAME)
+                .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
+                .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false)
                 .build()
                 .testRefresh();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
     }
 }
