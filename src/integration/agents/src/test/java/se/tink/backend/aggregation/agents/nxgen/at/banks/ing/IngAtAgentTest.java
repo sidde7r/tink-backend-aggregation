de1bc02da277d9c92a3b8533a55f903c59603f27
Sebastian Olsson <sebastian.olsson@tink.se>
2019-05-15 19:37:32 +0200

    feat(at-ingdiba): Upgraded ArgumentHelper -> ArgumentManager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/at/banks/ing/IngAtAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/at/banks/ing/IngAtAgentTest.java
@@ -5,10 +5,15 @@ import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
-import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
 public class IngAtAgentTest {
-    private final ArgumentHelper helper = new ArgumentHelper("tink.username", "tink.password");
+    private enum Arg {
+        USERNAME,
+        PASSWORD,
+    }
+
+    private final ArgumentManager<Arg> helper = new ArgumentManager<>(Arg.values());
 
     @Before
     public void before() {
@@ -17,14 +22,14 @@ public class IngAtAgentTest {
 
     @AfterClass
     public static void afterClass() {
-        ArgumentHelper.afterClass();
+        ArgumentManager.afterClass();
     }
 
     @Test
     public void testLoginAndRefresh() throws Exception {
         new AgentIntegrationTest.Builder("at", "at-ingdiba-password")
-                .addCredentialField(Field.Key.USERNAME, helper.get("tink.username"))
-                .addCredentialField(Field.Key.PASSWORD, helper.get("tink.password"))
+                .addCredentialField(Field.Key.USERNAME, helper.get(Arg.USERNAME))
+                .addCredentialField(Field.Key.PASSWORD, helper.get(Arg.PASSWORD))
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false)
                 .doLogout(true)
