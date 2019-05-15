ad99c730c5f0ba641a9b9c2fdf0c9d923ff097eb
Sebastian Olsson <sebastian.olsson@tink.se>
2019-05-15 20:12:37 +0200

    feat(mx-bbva): Upgraded ArgumentHelper -> ArgumentManager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/mx/BbvaMxAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/mx/BbvaMxAgentTest.java
@@ -5,13 +5,20 @@ import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
-import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 import se.tink.backend.aggregation.agents.nxgen.mx.banks.bbva.BbvaMxConstants;
 
 public class BbvaMxAgentTest {
 
-    private final ArgumentHelper helper =
-            new ArgumentHelper("tink.username", "tink.password", "tink.market", "tink.provider");
+    private enum Arg {
+        PHONE_NUMBER,
+        CARD_NUMBER,
+        PASSWORD,
+        MARKET,
+        PROVIDER,
+    }
+
+    private final ArgumentManager<Arg> helper = new ArgumentManager<>(Arg.values());
 
     @Before
     public void before() {
@@ -20,15 +27,15 @@ public class BbvaMxAgentTest {
 
     @AfterClass
     public static void afterClass() {
-        ArgumentHelper.afterClass();
+        ArgumentManager.afterClass();
     }
 
     @Test
     public void testLogin() throws Exception {
         new AgentIntegrationTest.Builder("mx", "mx-bbva-password")
-                .addCredentialField(Field.Key.USERNAME, helper.get("phoneNumber"))
-                .addCredentialField(Field.Key.PASSWORD, helper.get("password"))
-                .addCredentialField(BbvaMxConstants.FIELDS.CARD_NUMBER, helper.get("cardNumber"))
+                .addCredentialField(Field.Key.USERNAME, helper.get(Arg.PHONE_NUMBER))
+                .addCredentialField(Field.Key.PASSWORD, helper.get(Arg.PASSWORD))
+                .addCredentialField(BbvaMxConstants.FIELDS.CARD_NUMBER, helper.get(Arg.CARD_NUMBER))
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false)
                 .build()
