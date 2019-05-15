bf8bf34b02f768efa4e6bf9cc7e0821dfce92626
Mattias Hird <mattias.hird@tink.se>
2019-05-15 10:11:40 +0200

    fix(alandsbanken): Use argument manager in tests

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/alandsbanken/bankid/AlandsBankenBankidSeAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/alandsbanken/bankid/AlandsBankenBankidSeAgentTest.java
@@ -1,28 +1,42 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.alandsbanken.bankid;
 
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
 @Ignore
 public class AlandsBankenBankidSeAgentTest {
-    private static final String USERNAME = "";
+    private enum Arg {
+        USERNAME,
+        PASSWORD
+    }
 
-    private AgentIntegrationTest.Builder builder;
+    private final ArgumentManager<Arg> helper = new ArgumentManager<>(Arg.values());
+
+    private final AgentIntegrationTest.Builder builder =
+            new AgentIntegrationTest.Builder("se", "se-alandsbanken-bankid")
+                    .loadCredentialsBefore(false)
+                    .saveCredentialsAfter(false);
 
     @Before
     public void setup() {
-        builder =
-                new AgentIntegrationTest.Builder("se", "se-alandsbanken-bankid")
-                        .addCredentialField(Field.Key.USERNAME, USERNAME)
-                        .loadCredentialsBefore(false)
-                        .saveCredentialsAfter(false);
+        helper.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
     }
 
     @Test
     public void testRefresh() throws Exception {
-        builder.build().testRefresh();
+        builder.addCredentialField(Field.Key.USERNAME, helper.get(Arg.USERNAME))
+                .addCredentialField(Field.Key.PASSWORD, helper.get(Arg.PASSWORD))
+                .build()
+                .testRefresh();
     }
 }
