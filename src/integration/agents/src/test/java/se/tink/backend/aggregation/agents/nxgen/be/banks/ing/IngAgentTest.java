836be61700a72aa9cc292ceaf64d323ac8170466
Sebastian Olsson <sebastian.olsson@tink.se>
2019-05-15 19:59:16 +0200

    feat(be-ing): Upgraded ArgumentHelper -> ArgumentManager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/ing/IngAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/ing/IngAgentTest.java
@@ -5,11 +5,16 @@ import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
-import se.tink.backend.aggregation.agents.framework.ArgumentHelper;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
 public class IngAgentTest {
 
-    private final ArgumentHelper helper = new ArgumentHelper("tink.username", "tink.cardId");
+    private enum Arg {
+        USERNAME,
+        CARD_ID,
+    }
+
+    private final ArgumentManager<Arg> helper = new ArgumentManager<>(Arg.values());
 
     @Before
     public void before() {
@@ -18,14 +23,14 @@ public class IngAgentTest {
 
     @AfterClass
     public static void afterClass() {
-        ArgumentHelper.afterClass();
+        ArgumentManager.afterClass();
     }
 
     @Test
     public void testRefresh() throws Exception {
         new AgentIntegrationTest.Builder("be", "be-ing-cardreader")
-                .addCredentialField(Field.Key.USERNAME, helper.get("tink.username"))
-                .addCredentialField("cardId", helper.get("tink.cardId"))
+                .addCredentialField(Field.Key.USERNAME, helper.get(Arg.USERNAME))
+                .addCredentialField("cardId", helper.get(Arg.CARD_ID))
                 .loadCredentialsBefore(true)
                 .saveCredentialsAfter(true)
                 .build()
