7445e5fef7c8a7947b65c6db6b14bbcf23aca385
Vojin Pesalj <woyin95@gmail.com>
2019-05-16 14:11:32 +0200

    fix(Xs2aDevelopers) Used ArgumentManager for iban

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/axa/AxaAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/axa/AxaAgentTest.java
@@ -1,21 +1,30 @@
 package se.tink.backend.aggregation.agents.nxgen.be.openbanking.axa;
 
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.Xs2aDevelopersConstants.CredentialKeys;
 
 @Ignore
 public class AxaAgentTest {
-    public final String TEST_IBAN = "BE87548648412194";
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
     private AgentIntegrationTest.Builder builder;
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
 
     @Before
     public void setup() {
+        manager.before();
+
         builder =
                 new AgentIntegrationTest.Builder("be", "be-axa-oauth2")
-                        .addCredentialField(CredentialKeys.IBAN, TEST_IBAN)
+                        .addCredentialField(CredentialKeys.IBAN, manager.get(Arg.IBAN))
                         .expectLoggedIn(false)
                         .loadCredentialsBefore(false)
                         .saveCredentialsAfter(false);
@@ -24,5 +33,9 @@ public class AxaAgentTest {
     @Test
     public void testRefresh() throws Exception {
         builder.build().testRefresh();
+    }
+
+    private enum Arg {
+        IBAN,
     }
 }
--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/crelan/CrelanAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/crelan/CrelanAgentTest.java
@@ -1,21 +1,31 @@
 package se.tink.backend.aggregation.agents.nxgen.be.openbanking.crelan;
 
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.Xs2aDevelopersConstants.CredentialKeys;
 
 @Ignore
 public class CrelanAgentTest {
-    public final String TEST_IBAN = "BE87548648412194";
+
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
     private AgentIntegrationTest.Builder builder;
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
 
     @Before
     public void setup() {
+        manager.before();
+
         builder =
                 new AgentIntegrationTest.Builder("be", "be-crelan-oauth2")
-                        .addCredentialField(CredentialKeys.IBAN, TEST_IBAN)
+                        .addCredentialField(CredentialKeys.IBAN, manager.get(Arg.IBAN))
                         .expectLoggedIn(false)
                         .loadCredentialsBefore(false)
                         .saveCredentialsAfter(false);
@@ -24,5 +34,9 @@ public class CrelanAgentTest {
     @Test
     public void testRefresh() throws Exception {
         builder.build().testRefresh();
+    }
+
+    private enum Arg {
+        IBAN,
     }
 }
