dc98be512843c3f75af6ff778f979916a0bb992b
Vojin Pesalj <woyin95@gmail.com>
2019-05-16 11:43:56 +0200

    fix(Fintechblocks): Fixed agent class

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/hu/openbanking/granitbank/GranitBankAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/hu/openbanking/granitbank/GranitBankAgent.java
@@ -1,21 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.hu.openbanking.granitbank;
 
-import java.util.Optional;
 import se.tink.backend.aggregation.agents.AgentContext;
-import se.tink.backend.aggregation.agents.nxgen.hu.openbanking.granitbank.authenticator.GranitBankAuthenticator;
-import se.tink.backend.aggregation.agents.nxgen.hu.openbanking.granitbank.configuration.GranitBankConfiguration;
-import se.tink.backend.aggregation.agents.nxgen.hu.openbanking.granitbank.fetcher.transactionalaccount.GranitBankTransactionalAccountFetcher;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.fintechblocks.FintechblocksAgent;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.fintechblocks.FintechblocksConstants.ErrorMessages;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
-import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
-import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
-import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.ThirdPartyAppAuthenticationController;
-import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2AuthenticationController;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcherController;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionPagePaginationController;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
-import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
 public final class GranitBankAgent extends FintechblocksAgent {
@@ -23,58 +10,9 @@ public final class GranitBankAgent extends FintechblocksAgent {
     public GranitBankAgent(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
         super(request, context, signatureKeyPair);
-
-        apiClient = new GranitBankApiClient(client, persistentStorage);
     }
 
-    @Override
-    protected GranitBankConfiguration getClientConfiguration() {
-        return configuration
-                .getIntegrations()
-                .getClientConfiguration(
-                        GranitBankConstants.INTEGRATION_NAME,
-                        clientName,
-                        GranitBankConfiguration.class)
-                .orElseThrow(() -> new IllegalStateException(ErrorMessages.MISSING_CONFIGURATION));
-    }
-
-    @Override
-    protected Authenticator constructAuthenticator() {
-        final OAuth2AuthenticationController controller =
-                new OAuth2AuthenticationController(
-                        persistentStorage,
-                        supplementalInformationHelper,
-                        new GranitBankAuthenticator(
-                                (GranitBankApiClient) apiClient,
-                                persistentStorage,
-                                getClientConfiguration()));
-
-        return new AutoAuthenticationController(
-                request,
-                systemUpdater,
-                new ThirdPartyAppAuthenticationController<>(
-                        controller, supplementalInformationHelper),
-                controller);
-    }
-
-    @Override
-    protected Optional<TransactionalAccountRefreshController>
-            constructTransactionalAccountRefreshController() {
-        final GranitBankTransactionalAccountFetcher accountFetcher =
-                new GranitBankTransactionalAccountFetcher((GranitBankApiClient) apiClient);
-
-        return Optional.of(
-                new TransactionalAccountRefreshController(
-                        metricRefreshController,
-                        updateController,
-                        accountFetcher,
-                        new TransactionFetcherController<>(
-                                transactionPaginationHelper,
-                                new TransactionPagePaginationController<>(accountFetcher, 1))));
-    }
-
-    @Override
-    protected SessionHandler constructSessionHandler() {
-        return SessionHandler.alwaysFail();
+    protected String getIntegrationName() {
+        return GranitBankConstants.INTEGRATION_NAME;
     }
 }
