018fb25d25ec51c0363b72445aa33b5beec69a54
Vojin Pesalj <woyin95@gmail.com>
2019-05-16 11:46:31 +0200

    refactor(Fintechblocks): Removed unused classes

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/hu/openbanking/sberbank/configuration/SberBankConfiguration.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.hu.openbanking.sberbank.configuration;
-
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.fintechblocks.configuration.FintechblocksConfiguration;
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class SberBankConfiguration extends FintechblocksConfiguration {}
