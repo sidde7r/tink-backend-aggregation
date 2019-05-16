454c3aa3fdc3441849f7ab4ee26245b02e40cabe
Lars Almgren <lars.almgren@tink.se>
2019-05-16 11:16:15 +0200

    feat(agents): add unregulated third party app demo agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/psd2/unregulated/ThirdPartyAuthenticationDemoAgent.java
@@ -0,0 +1,203 @@
+package se.tink.backend.aggregation.agents.nxgen.demo.banks.psd2.unregulated;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import se.tink.backend.agents.rpc.Provider;
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.exceptions.SessionException;
+import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
+import se.tink.backend.aggregation.agents.nxgen.demo.banks.psd2.unregulated.authenticator.ThirdPartyAppAuthenticator;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.backend.aggregation.nxgen.agents.demo.NextGenerationDemoAgent;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoCreditCardAccount;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoIdentityData;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoInvestmentAccount;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoLoanAccount;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoSavingsAccount;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoTransactionAccount;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.ThirdPartyAppAuthenticationController;
+import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
+import se.tink.backend.aggregation.nxgen.controllers.transfer.TransferController;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+import se.tink.libraries.identitydata.NameElement;
+
+public class ThirdPartyAuthenticationDemoAgent extends NextGenerationDemoAgent {
+    private static String username;
+    private final ThirdPartyAppAuthenticator.FailCause failCause;
+    private final boolean successfulAuthentication;
+
+    public ThirdPartyAuthenticationDemoAgent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+
+        this.username = request.getCredentials().getField("username");
+        Provider provider = request.getProvider();
+
+        String name = provider.getName();
+        this.successfulAuthentication = !name.contains("failing");
+
+        String[] splitProviderName = name.split("-");
+        String lastPart = splitProviderName[splitProviderName.length - 1];
+
+        switch (lastPart) {
+        case "timeout":
+            failCause = ThirdPartyAppAuthenticator.FailCause.TIMEOUT;
+            break;
+        case "inprogress":
+            failCause = ThirdPartyAppAuthenticator.FailCause.IN_PROGRESS;
+            break;
+        case "cancelled":
+            failCause = ThirdPartyAppAuthenticator.FailCause.CANCELLED;
+            break;
+        default:
+            this.failCause = null;
+        }
+    }
+
+    @Override
+    protected Authenticator constructAuthenticator() {
+        ThirdPartyAppAuthenticator redirectThirdPartyAppAuthenticator =
+                new ThirdPartyAppAuthenticator(username, successfulAuthentication, failCause);
+
+        return new ThirdPartyAppAuthenticationController<>(
+                redirectThirdPartyAppAuthenticator, supplementalInformationHelper);
+    }
+
+    @Override
+    protected SessionHandler constructSessionHandler() {
+        return new SessionHandler() {
+            @Override
+            public void logout() {
+                // nop.
+            }
+
+            @Override
+            public void keepAlive() throws SessionException {
+                throw SessionError.SESSION_EXPIRED.exception();
+            }
+        };
+    }
+
+    @Override
+    protected Optional<TransferController> constructTransferController() {
+        return Optional.empty();
+    }
+
+    @Override
+    public DemoInvestmentAccount getInvestmentAccounts() {
+        return new DemoInvestmentAccount() {
+            @Override
+            public String getAccountId() {
+                return "9999-444444444444";
+            }
+
+            @Override
+            public String getName() {
+                return "Investment";
+            }
+
+            @Override
+            public double getAccountBalance() {
+                return 123456;
+            }
+        };
+    }
+
+    @Override
+    public DemoSavingsAccount getDemoSavingsAccounts() {
+        return new DemoSavingsAccount() {
+            @Override
+            public String getAccountId() {
+                return "9999-222222222222";
+            }
+
+            @Override
+            public String getAccountName() {
+                return "Savings Account";
+            }
+
+            @Override
+            public double getAccountBalance() {
+                return 385245.33;
+            }
+        };
+    }
+
+    @Override
+    public DemoLoanAccount getDemoLoanAccounts() {
+        return new DemoLoanAccount() {
+            @Override
+            public String getMortgageId() {
+                return "9999-333333333333";
+            }
+
+            @Override
+            public String getBlancoId() {
+                return "9999-333334444444";
+            }
+
+            @Override
+            public String getMortgageLoanName() {
+                return "Bolån";
+            }
+
+            @Override
+            public String getBlancoLoanName() {
+                return "Santander";
+            }
+
+            @Override
+            public double getMortgageInterestName() {
+                return 0.19;
+            }
+
+            @Override
+            public double getBlancoInterestName() {
+                return 1.45;
+            }
+
+            @Override
+            public double getMortgageBalance() {
+                return -2300000D;
+            }
+
+            @Override
+            public double getBlancoBalance() {
+                return -50000D;
+            }
+        };
+    }
+
+    @Override
+    public DemoTransactionAccount getTransactionalAccountAccounts() {
+        return null;
+    }
+
+    @Override
+    public List<DemoCreditCardAccount> getCreditCardAccounts() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public DemoIdentityData getIdentityDataResponse() {
+        return new DemoIdentityData() {
+            @Override
+            public List<NameElement> getNameElements() {
+                return new ArrayList<>(
+                        Arrays.asList(
+                                new NameElement(NameElement.Type.FIRST_NAME, "Jane"),
+                                new NameElement(NameElement.Type.SURNAME, "Doe")));
+            }
+
+            @Override
+            public LocalDate getDateOfBirth() {
+                return LocalDate.of(1970, 1, 1);
+            }
+        };
+    }
+}
