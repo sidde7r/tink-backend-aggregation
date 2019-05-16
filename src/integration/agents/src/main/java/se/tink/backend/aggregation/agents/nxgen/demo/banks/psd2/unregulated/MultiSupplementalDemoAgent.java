74e8ff5ce8aea438c987f892ea0f0cb9290b5931
Lars Almgren <lars.almgren@tink.se>
2019-05-16 11:09:58 +0200

    feat(agents): add unregulated multisupplemental demo agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/psd2/unregulated/MultiSupplementalDemoAgent.java
@@ -0,0 +1,177 @@
+package se.tink.backend.aggregation.agents.nxgen.demo.banks.psd2.unregulated;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.exceptions.SessionException;
+import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
+import se.tink.backend.aggregation.agents.nxgen.demo.banks.psd2.unregulated.authenticator.MultiSupplementalAutoAuthenticator;
+import se.tink.backend.aggregation.agents.nxgen.demo.banks.psd2.unregulated.authenticator.MultiSupplementalManualAuthenticator;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.backend.aggregation.nxgen.agents.demo.NextGenerationDemoAgent;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoCreditCardAccount;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoIdentityData;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoInvestmentAccount;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoLoanAccount;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoSavingsAccount;
+import se.tink.backend.aggregation.nxgen.agents.demo.data.DemoTransactionAccount;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
+import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
+import se.tink.backend.aggregation.nxgen.controllers.transfer.TransferController;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+import se.tink.libraries.identitydata.NameElement;
+
+public class MultiSupplementalDemoAgent extends NextGenerationDemoAgent {
+
+    public MultiSupplementalDemoAgent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected Authenticator constructAuthenticator() {
+        return new AutoAuthenticationController(
+                request,
+                systemUpdater,
+                new MultiSupplementalManualAuthenticator(supplementalInformationHelper, catalog),
+                new MultiSupplementalAutoAuthenticator());
+    }
+
+    @Override
+    protected SessionHandler constructSessionHandler() {
+        return new SessionHandler() {
+            @Override
+            public void logout() {
+                // nop.
+            }
+
+            @Override
+            public void keepAlive() throws SessionException {
+                throw SessionError.SESSION_EXPIRED.exception();
+            }
+        };
+    }
+
+    @Override
+    protected Optional<TransferController> constructTransferController() {
+        return Optional.empty();
+    }
+
+    @Override
+    public DemoInvestmentAccount getInvestmentAccounts() {
+        return new DemoInvestmentAccount() {
+            @Override
+            public String getAccountId() {
+                return "8888-444444444444";
+            }
+
+            @Override
+            public String getName() {
+                return "Small Investment";
+            }
+
+            @Override
+            public double getAccountBalance() {
+                return 456;
+            }
+        };
+    }
+
+    @Override
+    public DemoSavingsAccount getDemoSavingsAccounts() {
+        return new DemoSavingsAccount() {
+            @Override
+            public String getAccountId() {
+                return "8888-222222222222";
+            }
+
+            @Override
+            public String getAccountName() {
+                return "Savings Account";
+            }
+
+            @Override
+            public double getAccountBalance() {
+                return 245.33;
+            }
+        };
+    }
+
+    @Override
+    public DemoLoanAccount getDemoLoanAccounts() {
+        return new DemoLoanAccount() {
+            @Override
+            public String getMortgageId() {
+                return "8888-333333333333";
+            }
+
+            @Override
+            public String getBlancoId() {
+                return "8888-333334444444";
+            }
+
+            @Override
+            public String getMortgageLoanName() {
+                return "Loan";
+            }
+
+            @Override
+            public String getBlancoLoanName() {
+                return "Tesco Invest";
+            }
+
+            @Override
+            public double getMortgageInterestName() {
+                return 0.53;
+            }
+
+            @Override
+            public double getBlancoInterestName() {
+                return 1.73;
+            }
+
+            @Override
+            public double getMortgageBalance() {
+                return -2300D;
+            }
+
+            @Override
+            public double getBlancoBalance() {
+                return -5D;
+            }
+        };
+    }
+
+    @Override
+    public DemoTransactionAccount getTransactionalAccountAccounts() {
+        return null;
+    }
+
+    @Override
+    public List<DemoCreditCardAccount> getCreditCardAccounts() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public DemoIdentityData getIdentityDataResponse() {
+        return new DemoIdentityData() {
+            @Override
+            public List<NameElement> getNameElements() {
+                return new ArrayList<>(
+                        Arrays.asList(
+                                new NameElement(NameElement.Type.FIRST_NAME, "Jane"),
+                                new NameElement(NameElement.Type.SURNAME, "Doe")));
+            }
+
+            @Override
+            public LocalDate getDateOfBirth() {
+                return LocalDate.of(1970, 1, 1);
+            }
+        };
+    };
+}
