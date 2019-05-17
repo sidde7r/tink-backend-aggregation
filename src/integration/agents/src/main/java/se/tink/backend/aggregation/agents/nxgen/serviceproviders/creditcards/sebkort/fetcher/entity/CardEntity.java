853f9462e28cc6977decf38974e5530bea86eb9c
Annica <annica.alstermark@tink.se>
2019-05-17 08:58:55 +0200

    fix(SEBKort): Change logic for setting balance and available credit
    Set available credit and balance to 0 for secondary card holders.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/sebkort/fetcher/entity/CardEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/sebkort/fetcher/entity/CardEntity.java
@@ -1,6 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.sebkort.fetcher.entity;
 
 import java.util.Map;
+import java.util.Objects;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.sebkort.SebKortConstants;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
@@ -54,38 +55,52 @@ public class CardEntity {
             Map<String, CardAccountEntity> accountsHashMap, CardContractEntity contract) {
         final CardAccountEntity account = accountsHashMap.get(contract.getCardAccountId());
 
-        CreditCardAccount.Builder builder =
-                CreditCardAccount.builder(getMaskedCardNumber())
-                        .setAccountNumber(getMaskedCardNumber())
-                        .setName(contract.getProductName())
-                        .setHolderName(new HolderName(getNameOnCard()))
-                        .setBankIdentifier(contract.getId())
-                        .putInTemporaryStorage(
-                                SebKortConstants.StorageKey.CARD_ACCOUNT_ID,
-                                contract.getCardAccountId())
-                        .putInTemporaryStorage(
-                                SebKortConstants.StorageKey.IS_ACCOUNT_OWNER, contract.isOwned());
+        return CreditCardAccount.builder(getMaskedCardNumber())
+                .setAccountNumber(getMaskedCardNumber())
+                .setName(contract.getProductName())
+                .setHolderName(new HolderName(getNameOnCard()))
+                .setBalance(getBalanceForOwner(account, contract))
+                .setAvailableCredit(getAvailableCreditForOwnerIfPresent(account, contract))
+                .setBankIdentifier(contract.getId())
+                .putInTemporaryStorage(
+                        SebKortConstants.StorageKey.CARD_ACCOUNT_ID, contract.getCardAccountId())
+                .putInTemporaryStorage(
+                        SebKortConstants.StorageKey.IS_ACCOUNT_OWNER, contract.isOwned())
+                .build();
+    }
 
-        if (account != null) {
-            return (CreditCardAccount)
-                    builder.setBalance(
-                                    new Amount(
-                                                    account.getCurrencyCode(),
-                                                    account.getCurrentBalance())
-                                            .negate())
-                            .setAvailableCredit(
-                                    new Amount(
-                                            account.getCurrencyCode(),
-                                            account.getDisposableAmount()))
-                            .build();
-        } else {
-            return (CreditCardAccount)
-                    builder.setBalance(
-                                    new Amount(
-                                                    contract.getCurrencyCode(),
-                                                    contract.getNonBilledAmount())
-                                            .negate())
-                            .build();
+    private Amount getAvailableCreditForOwnerIfPresent(
+            CardAccountEntity account, CardContractEntity cardContract) {
+        if (Objects.isNull(account)) {
+            return null;
         }
+
+        if (!cardContract.isOwned()) {
+            if (!cardContract.isOwned()) {
+                return new Amount(cardContract.getCurrencyCode(), 0d);
+            }
+        }
+
+        return new Amount(account.getCurrencyCode(), account.getDisposableAmount());
+    }
+
+    private Amount getBalanceForOwner(CardAccountEntity account, CardContractEntity cardContract) {
+
+        // Some SEBKort providers do not supply card accounts. In this case we have to use the
+        // amount from the card contract. This amount is not the whole truth, it's the amount
+        // generated from transactions on that specific card during current period.
+        if (Objects.isNull(account)) {
+            return new Amount(cardContract.getCurrencyCode(), cardContract.getNonBilledAmount())
+                    .negate();
+        }
+
+        // The card account balance is global for the whole account. Only set it for the account
+        // owner, and set 0 for the secondary cards. Then we don't summarize to more debt than the
+        // user actually has.
+        if (!cardContract.isOwned()) {
+            return new Amount(cardContract.getCurrencyCode(), 0d);
+        }
+
+        return new Amount(account.getCurrencyCode(), account.getCurrentBalance()).negate();
     }
 }
