8ade4f23d42c9bc908871c51f28b38624e2ef897
Jesús A. Álvarez <jesus.alvarez@tink.se>
2019-05-20 15:13:18 +0200

    refactor(Ålandsbanken FI): Use ArgumentManager in agent test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/alandsbanken/AlandsBankenFIAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/alandsbanken/AlandsBankenFIAgentTest.java
@@ -1,24 +1,36 @@
 package se.tink.backend.aggregation.agents.nxgen.fi.banks.alandsbanken;
 
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
-@Ignore
 public class AlandsBankenFIAgentTest {
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    private enum Arg {
+        USERNAME,
+        PASSWORD
+    }
 
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
     private AgentIntegrationTest.Builder builder;
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
 
     @Before
     public void setup() {
+        manager.before();
         builder =
                 new AgentIntegrationTest.Builder("fi", "fi-alandsbanken-codecard")
-                        .addCredentialField(Field.Key.USERNAME, USERNAME)
-                        .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                        .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
+                        .addCredentialField(Field.Key.PASSWORD, manager.get(Arg.PASSWORD))
+                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                        .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
                         .loadCredentialsBefore(true)
                         .saveCredentialsAfter(true);
     }
