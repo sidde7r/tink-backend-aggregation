f841c9ca1f3bb4a22a68a470f86649341f5e6104
Jesús A. Álvarez <jesus.alvarez@tink.se>
2019-05-22 14:54:39 +0200

    refactor(Saastopankki): use ArgumentManager in test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/saastopankki/SpAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/saastopankki/SpAgentTest.java
@@ -1,19 +1,37 @@
 package se.tink.backend.aggregation.agents.nxgen.fi.banks.saastopankki;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
-@Ignore
 public class SpAgentTest {
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    private enum Arg {
+        USERNAME,
+        PASSWORD
+    }
+
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
+    @Before
+    public void setup() {
+        manager.before();
+    }
 
     private AgentIntegrationTest.Builder builder() {
         return new AgentIntegrationTest.Builder("fi", "fi-saastopankki-codecard")
-                .addCredentialField(Field.Key.USERNAME, USERNAME)
-                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
+                .addCredentialField(Field.Key.PASSWORD, manager.get(Arg.PASSWORD))
+                .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false);
     }
