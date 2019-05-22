60889720059745f2e1896e0112046669504d5e32
Jesús A. Álvarez <jesus.alvarez@tink.se>
2019-05-22 15:21:34 +0200

    test(POP Pankki): add agent integration test

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/poppankki/PopPankkiAgentTest.java
@@ -0,0 +1,43 @@
+package se.tink.backend.aggregation.agents.nxgen.fi.banks.poppankki;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.libraries.credentials.service.RefreshableItem;
+
+public class PopPankkiAgentTest {
+    private enum Arg {
+        USERNAME,
+        PASSWORD
+    }
+
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
+    @Before
+    public void setup() {
+        manager.before();
+    }
+
+    private AgentIntegrationTest.Builder builder() {
+        return new AgentIntegrationTest.Builder("fi", "fi-poppankki-codecard")
+                .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
+                .addCredentialField(Field.Key.PASSWORD, manager.get(Arg.PASSWORD))
+                .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false);
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder().build().testRefresh();
+    }
+}
