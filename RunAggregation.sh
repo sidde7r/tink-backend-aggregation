8736abca45b6442334b455d2f0b03bf295af46e2
Mathew Clegg <mathew.clegg@live.se>
2019-05-23 15:30:35 +0200

    feat: set defaults for image & pullpolicy
    fixes

---
+++ RunAggregation.sh
@@ -0,0 +1,24 @@
+env=$1
+
+if [ $# -eq 0 ]
+  then
+    echo "#############################################################################################################"
+    echo "If you are running with a private Docker registry, please run ./RunAggregation.sh <yourPrivateRegistry>:5000"
+    echo "#############################################################################################################"
+else
+    endpoint=$env
+fi
+
+bazel run //src/aggregation/service:aggregation-image
+
+if [ $# -ne 0 ]
+  then
+    echo "#############################################################################################################"
+    echo "Remember to change your image in .charts/tink-backend-aggregation/values/local-development to  $env/aggregation/service:aggregation-image"
+    echo "#############################################################################################################"
+    docker tag bazel/src/aggregation/service:aggregation-image $env/aggregation/service:aggregation-image
+    docker push $env/aggregation/service:aggregation-image
+fi
+
+kubectl delete deployment -n aggregation aggregation
+../tink-infrastructure/kubernetes-generator/kubernetes-generator.sh --chart tink-backend-aggregation --cluster local --environment development | kubectl apply -f-
