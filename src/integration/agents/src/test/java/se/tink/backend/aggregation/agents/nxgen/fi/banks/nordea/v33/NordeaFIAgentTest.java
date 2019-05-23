3d94e3076bd3410c7b10a8b301c8aa239f5994a1
Jesús A. Álvarez <jesus.alvarez@tink.se>
2019-05-23 15:46:13 +0200

    refactor(Nordea FI): use ArgumentManager in agent test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/nordea/v33/NordeaFIAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/nordea/v33/NordeaFIAgentTest.java
@@ -1,22 +1,32 @@
 package se.tink.backend.aggregation.agents.nxgen.fi.banks.nordea.v33;
 
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
-@Ignore
 public class NordeaFIAgentTest {
-    private static final String USERNAME = "";
+    private enum Arg {
+        USERNAME
+    }
 
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
     private AgentIntegrationTest.Builder builder;
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
 
     @Before
     public void setup() {
+        manager.before();
         builder =
                 new AgentIntegrationTest.Builder("fi", "fi-nordea-thirdpartyapp")
-                        .addCredentialField(Field.Key.USERNAME, USERNAME)
+                        .addCredentialField(Field.Key.USERNAME, manager.get(Arg.USERNAME))
                         .loadCredentialsBefore(false)
                         .saveCredentialsAfter(false)
                         .doLogout(true);
