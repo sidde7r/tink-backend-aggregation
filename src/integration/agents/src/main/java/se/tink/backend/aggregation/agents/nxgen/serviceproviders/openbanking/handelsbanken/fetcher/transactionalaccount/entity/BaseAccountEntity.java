2e2e099da7f41b5b31b4e371f0b78d86e4730519
Milan Deket <m.deket@vegaitsourcing.rs>
2019-05-24 12:09:36 +0200

    refactor(Handelsbanken): Changed creation of TransactionalAccount.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/handelsbanken/fetcher/transactionalaccount/entity/BaseAccountEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/handelsbanken/fetcher/transactionalaccount/entity/BaseAccountEntity.java
@@ -2,8 +2,11 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ha
 
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseConstants.StorageKeys;
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.backend.aggregation.nxgen.core.account.transactional.CheckingAccount;
+import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.balance.BalanceModule;
+import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.id.IdModule;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.SavingsAccount;
+import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccountType;
 import se.tink.libraries.account.AccountIdentifier;
 import se.tink.libraries.amount.Amount;
 
@@ -29,23 +32,31 @@ public class BaseAccountEntity {
         return accountType;
     }
 
-    public CheckingAccount createCheckingAccount(BalanceEntity balance) {
-        return CheckingAccount.builder()
-                .setUniqueIdentifier(getAccountNumber())
-                .setAccountNumber(getAccountNumber())
-                .setBalance(
-                        new Amount(
-                                balance.getAmount().getCurrency(),
-                                balance.getAmount().getContent()))
-                .setAlias(name)
-                .addAccountIdentifier(AccountIdentifier.create(AccountIdentifier.Type.IBAN, iban))
+    public TransactionalAccount createCheckingAccount(BalanceEntity balance) {
+        return TransactionalAccount.nxBuilder()
+                .withType(TransactionalAccountType.CHECKING)
+                .withId(
+                        IdModule.builder()
+                                .withUniqueIdentifier(getAccountNumber())
+                                .withAccountNumber(getAccountNumber())
+                                .withAccountName(name)
+                                .addIdentifier(
+                                        AccountIdentifier.create(AccountIdentifier.Type.IBAN, iban))
+                                .build())
+                .withBalance(BalanceModule.of(getAmount(balance)))
                 .addHolderName(ownerName)
+                .setApiIdentifier(accountId)
+                .setBankIdentifier(getAccountNumber())
                 .putInTemporaryStorage(StorageKeys.ACCOUNT_ID, accountId)
                 .build();
     }
 
     private String getAccountNumber() {
         return iban.substring(9);
+    }
+
+    private Amount getAmount(BalanceEntity balance) {
+        return new Amount(balance.getAmount().getCurrency(), balance.getAmount().getContent());
     }
 
     public SavingsAccount createSavingsAccount(BalanceEntity balance) {
