b3830bc32343d100aa4842723dc739333d0cf2b2
Mauritz Zachrisson <mauritz.zachrisson@tink.se>
2019-05-24 13:33:06 +0200

    feat(samlink): Enable API v2 flag
    API v2 seemingly has very minor changes, the logic and entities we use still work.
    New headers force the API version.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/samlink/SamlinkConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/samlink/SamlinkConfiguration.java
@@ -5,13 +5,28 @@ import se.tink.backend.aggregation.nxgen.http.URL;
 public class SamlinkConfiguration {
 
     private final String baseUrl;
+    private String clientApp;
+    private boolean v2 = false;
+
+    public SamlinkConfiguration(String baseUrl, String clientApp) {
+        this.baseUrl = baseUrl;
+        this.clientApp = clientApp;
+        v2 = true;
+    }
 
     public SamlinkConfiguration(String baseUrl) {
-
         this.baseUrl = baseUrl;
     }
 
     public URL build(String path) {
         return new URL(baseUrl + path);
+    }
+
+    public String getClientApp() {
+        return clientApp;
+    }
+
+    public boolean isV2() {
+        return v2;
     }
 }
