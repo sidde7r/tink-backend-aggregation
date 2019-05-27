9441025e7839302a9bf4e45f552876e1aa337af3
Jesús A. Álvarez <jesus.alvarez@tink.se>
2019-05-27 11:12:43 +0200

    style(Swedbank): bazel format

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/utils/QrCodeParser.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/utils/QrCodeParser.java
@@ -8,7 +8,6 @@ import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
-
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
