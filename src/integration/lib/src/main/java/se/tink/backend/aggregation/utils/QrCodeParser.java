233b74b60f151a2ee95fdb25c800b7398dab54bf
Jesús A. Álvarez <jesus.alvarez@tink.se>
2019-05-27 09:39:32 +0200

    test(Swedbank): add test for QR code failure

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/utils/QrCodeParser.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/utils/QrCodeParser.java
@@ -1,12 +1,14 @@
 package se.tink.backend.aggregation.utils;
 
 import com.google.api.client.util.Base64;
+import com.google.common.base.Strings;
 import com.google.zxing.BinaryBitmap;
 import com.google.zxing.LuminanceSource;
 import com.google.zxing.MultiFormatReader;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
+
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -24,6 +26,9 @@ public class QrCodeParser {
      * @throws IOException if the image could not be decoded
      */
     public static String decodeQRCode(final String qrCodeImage) throws IOException {
+        if (Strings.isNullOrEmpty(qrCodeImage)) {
+            throw new IOException("Cannot decode empty data.");
+        }
         BufferedImage bufferedImage;
         byte[] decoded = Base64.decodeBase64(qrCodeImage);
         bufferedImage = ImageIO.read(new ByteArrayInputStream(decoded));
@@ -55,7 +60,7 @@ public class QrCodeParser {
             if (matcher.find()) {
                 return matcher.group(1);
             } else {
-                throw new NoSuchElementException("Could not find autostart token.");
+                throw new NoSuchElementException("Invalid format of QR code. Check on bank side.");
             }
         } catch (IOException e) {
             throw new NoSuchElementException("Could not decode QR code.");
