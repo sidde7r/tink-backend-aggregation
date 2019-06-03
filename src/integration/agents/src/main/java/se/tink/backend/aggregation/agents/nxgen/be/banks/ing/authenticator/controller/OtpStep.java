726d0cfb7eb44020509ec509c9785684871f7268
Sebastian Olsson <sebastian.olsson@tink.se>
2019-06-03 15:29:41 +0200

    log(be-ing): Added logging to OtpStep

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/ing/authenticator/controller/OtpStep.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/ing/authenticator/controller/OtpStep.java
@@ -2,11 +2,15 @@ package se.tink.backend.aggregation.agents.nxgen.be.banks.ing.authenticator.cont
 
 import java.util.Collections;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.AuthenticationResponse;
 import se.tink.backend.aggregation.nxgen.controllers.utils.SupplementalInformationFormer;
 
 public final class OtpStep {
+
+    private static Logger logger = LoggerFactory.getLogger(OtpStep.class);
 
     private final SupplementalInformationFormer supplementalInformationFormer;
 
@@ -15,6 +19,8 @@ public final class OtpStep {
     }
 
     public AuthenticationResponse respond() {
+        logger.info("ING OtpStep");
+
         List<Field> otpInput =
                 Collections.singletonList(
                         supplementalInformationFormer.getField(Field.Key.OTP_INPUT));
