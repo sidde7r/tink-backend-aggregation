8e6f700cba5bee89e4a8c5f838f1c55f17d1c6a4
Hantao Wang <hantao.wang@tink.se>
2019-06-04 11:35:04 +0200

    feat(test-password-agent): Cancel on payment signing for saving account

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/password/executor/transfer/PasswordDemoPaymentExecutor.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/password/executor/transfer/PasswordDemoPaymentExecutor.java
@@ -1,11 +1,24 @@
 package se.tink.backend.aggregation.agents.nxgen.demo.banks.password.executor.transfer;
 
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.TransferExecutionException;
 import se.tink.backend.aggregation.nxgen.controllers.transfer.PaymentExecutor;
+import se.tink.libraries.signableoperation.enums.SignableOperationStatuses;
 import se.tink.libraries.transfer.rpc.Transfer;
 
 public class PasswordDemoPaymentExecutor implements PaymentExecutor {
     public PasswordDemoPaymentExecutor() {}
 
     @Override
-    public void executePayment(Transfer transfer) {}
+    public void executePayment(Transfer transfer) {
+        Optional<String> sourceAccountName = transfer.getSource().getName();
+
+        if (sourceAccountName.isPresent() && sourceAccountName.get().contains("Savings Account")) {
+            // Mock the user cancel for transfers from the saving account
+            throw TransferExecutionException.builder(SignableOperationStatuses.CANCELLED)
+                    .setEndUserMessage("Cancel on payment signing (test)")
+                    .setMessage("Cancel on payment signing (test)")
+                    .build();
+        }
+    }
 }
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/password/executor/transfer/PasswordDemoTransferExecutor.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/password/executor/transfer/PasswordDemoTransferExecutor.java
@@ -2,8 +2,10 @@ package se.tink.backend.aggregation.agents.nxgen.demo.banks.password.executor.tr
 
 import java.util.Optional;
 import se.tink.backend.agents.rpc.Credentials;
+import se.tink.backend.aggregation.agents.TransferExecutionException;
 import se.tink.backend.aggregation.agents.contexts.SupplementalRequester;
 import se.tink.backend.aggregation.nxgen.controllers.transfer.BankTransferExecutor;
+import se.tink.libraries.signableoperation.enums.SignableOperationStatuses;
 import se.tink.libraries.transfer.rpc.Transfer;
 
 public class PasswordDemoTransferExecutor implements BankTransferExecutor {
@@ -18,6 +20,16 @@ public class PasswordDemoTransferExecutor implements BankTransferExecutor {
 
     @Override
     public Optional<String> executeTransfer(Transfer transfer) {
+        Optional<String> sourceAccountName = transfer.getSource().getName();
+
+        if (sourceAccountName.isPresent() && sourceAccountName.get().contains("Savings Account")) {
+            // Mock the user cancel for transfers from the saving account
+            throw TransferExecutionException.builder(SignableOperationStatuses.CANCELLED)
+                    .setEndUserMessage("Cancel on payment signing (test)")
+                    .setMessage("Cancel on payment signing (test)")
+                    .build();
+        }
+
         return Optional.empty();
     }
 }
