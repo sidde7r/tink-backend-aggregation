9034c3bac6e72249be09f80321c51eb60876edf3
Seckin Savasci <seckin.savasci@tink.se>
2019-06-07 11:42:41 +0200

    refactor(ConfigurationValidator): Separate validation concerns

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/configuration/ConfigurationValidator.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/configuration/ConfigurationValidator.java
@@ -9,6 +9,11 @@ import se.tink.backend.aggregation.storage.database.models.AggregatorConfigurati
 import se.tink.backend.aggregation.storage.database.models.ClientConfiguration;
 import se.tink.backend.aggregation.storage.database.models.ClusterConfiguration;
 
+/**
+ * Validates configurations required for a sane service state.
+ *
+ * @see ConfigurationValidator#validate() throws exceptions if any of the validations fail.
+ */
 public final class ConfigurationValidator {
     private final Map<String, ClientConfiguration> clientConfigurations;
     private final Map<String, ClusterConfiguration> clusterConfigurations;
@@ -32,26 +37,40 @@ public final class ConfigurationValidator {
     }
 
     private void validate() {
-        clientConfigurations.forEach(
-                (clientApiKey, clientConfiguration) -> {
-                    Preconditions.checkNotNull(
-                            clusterConfigurations.get(clientConfiguration.getClusterId()),
-                            "Client Api Key [%s] is missing cluster configuration",
-                            clientApiKey);
+        clientConfigurations.forEach(this::validateClientConfiguration);
+    }
 
-                    Preconditions.checkNotNull(
-                            aggregatorConfigurations.get(clientConfiguration.getAggregatorId()),
-                            "Client Api Key [%s] is missing aggregator configuration",
-                            clientApiKey);
+    private void validateClientConfiguration(
+            String clientApiKey, ClientConfiguration clientConfiguration) {
+        validateClusterConfiguration(clientApiKey, clientConfiguration);
+        validateAggregatorConfiguration(clientApiKey, clientConfiguration);
+        validateCryptoConfiguration(clientApiKey, clientConfiguration);
+    }
 
-                    Preconditions.checkState(
-                            cryptoConfigurationDao
-                                    .getCryptoWrapperOfClientName(
-                                            clientConfiguration.getClientName())
-                                    .getClientName()
-                                    .isPresent(),
-                            "Client Api Key [%s] is missing crypto configuration",
-                            clientApiKey);
-                });
+    private void validateCryptoConfiguration(
+            String clientApiKey, ClientConfiguration clientConfiguration) {
+        Preconditions.checkState(
+                cryptoConfigurationDao
+                        .getCryptoWrapperOfClientName(clientConfiguration.getClientName())
+                        .getClientName()
+                        .isPresent(),
+                "Client Api Key [%s] is missing crypto configuration",
+                clientApiKey);
+    }
+
+    private void validateAggregatorConfiguration(
+            String clientApiKey, ClientConfiguration clientConfiguration) {
+        Preconditions.checkNotNull(
+                aggregatorConfigurations.get(clientConfiguration.getAggregatorId()),
+                "Client Api Key [%s] is missing aggregator configuration",
+                clientApiKey);
+    }
+
+    private void validateClusterConfiguration(
+            String clientApiKey, ClientConfiguration clientConfiguration) {
+        Preconditions.checkNotNull(
+                clusterConfigurations.get(clientConfiguration.getClusterId()),
+                "Client Api Key [%s] is missing cluster configuration",
+                clientApiKey);
     }
 }
