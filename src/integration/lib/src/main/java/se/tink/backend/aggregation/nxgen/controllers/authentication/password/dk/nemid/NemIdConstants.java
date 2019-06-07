5132a64f7dbb15b170de362d232c57e53a01e2f2
berkgedik <berk.gedik@tink.se>
2019-06-07 08:53:56 +0200

    feat(NemidAuthenticationController): Collect more logs to investigate error

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/password/dk/nemid/NemIdConstants.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/password/dk/nemid/NemIdConstants.java
@@ -9,6 +9,7 @@ public class NemIdConstants {
 
     public static final LogTag NEM_ID_AUTH_ERROR_TAG = LogTag.from("nem-id-auth-error");
     public static final LogTag NEM_ID_AUTH_SUCCESS_TAG = LogTag.from("nem-id-auth-success");
+    public static final LogTag NEM_ID_AUTH_IFRAME_TAG = LogTag.from("nem-id-iframe");
 
     // This html is taken from Nordea DK iOS and android. It has been modified to only include
     // necessary parts.
@@ -26,6 +27,12 @@ public class NemIdConstants {
                     + "                     document.getElementById(\"tink_nemIdToken\").innerHTML = message.content;\n"
                     + "                    e.preventDefault();\n"
                     + "                }\n"
+                    + "                if (!document.getElementById(\"debug_div\")) {\n"
+                    + "                    document.body.innerHTML += '<div id=\\\"debug_div\\\"/>';\n"
+                    + "                }\n"
+                    + "                var newElement = document.createElement('div');\n"
+                    + "                newElement.innerHTML = '<div>' + JSON.stringify(message) + '</div>';\n"
+                    + "                document.getElementById(\"debug_div\").appendChild(newElement);\n"
                     + "            });\n"
                     + "        </script>\n"
                     + "    </body>\n"
