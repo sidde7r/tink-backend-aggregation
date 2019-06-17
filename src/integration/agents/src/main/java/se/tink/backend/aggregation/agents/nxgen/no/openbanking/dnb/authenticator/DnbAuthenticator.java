658a18352c1e322a3d055695afb0b4ad5a781022
Milan Deket <m.deket@vegaitsourcing.rs>
2019-06-17 12:53:10 +0200

    refactor(Dnb): Removed OAuthAuthenticator.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/dnb/authenticator/DnbAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/dnb/authenticator/DnbAuthenticator.java
@@ -1,12 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.no.openbanking.dnb.authenticator;
 
-import se.tink.backend.aggregation.agents.exceptions.BankServiceException;
 import se.tink.backend.aggregation.agents.nxgen.no.openbanking.dnb.DnbApiClient;
-import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2Authenticator;
-import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
 import se.tink.backend.aggregation.nxgen.http.URL;
 
-public class DnbAuthenticator implements OAuth2Authenticator {
+public class DnbAuthenticator {
 
     private final DnbApiClient apiClient;
 
@@ -17,17 +14,4 @@ public class DnbAuthenticator implements OAuth2Authenticator {
     public URL buildAuthorizeUrl(final String state) {
         return apiClient.getAuthorizeUrl(state);
     }
-
-    @Override
-    public OAuth2Token exchangeAuthorizationCode(final String code) throws BankServiceException {
-        return null;
-    }
-
-    @Override
-    public OAuth2Token refreshAccessToken(String refreshToken) throws BankServiceException {
-        return null;
-    }
-
-    @Override
-    public void useAccessToken(OAuth2Token accessToken) {}
 }
