8b9125d8aa178f93ec4ad4f02f8b7921b10a6cf4
berkgedik <berk.gedik@tink.se>
2019-06-20 13:14:51 +0200

    fix(DK/Nordea): Fixed error message for error code MAS0019

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/NordeaDkConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/NordeaDkConstants.java
@@ -580,6 +580,10 @@ public class NordeaDkConstants {
         public static final String LOGIN_TYPE_NEMID = "MNEMID-LOGON";
     }
 
+    public static class ErrorCode {
+        public static final String NO_ACCESS_TO_MOBILBANK = "MAS0019";
+    }
+
     public static class Tag {
         public static final LogTag LOANS_POSITIVE_ERROR =
                 LogTag.from("NORDEA_DK_LOANS_POSITIVE_ERROR");
