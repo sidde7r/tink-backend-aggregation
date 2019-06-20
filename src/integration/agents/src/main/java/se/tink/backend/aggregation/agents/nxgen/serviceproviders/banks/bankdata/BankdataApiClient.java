325961a4fa7d385aa988f564e07e693b167f6c80
Alexander Jamm <alexander.jamm@tink.se>
2019-06-20 11:37:48 +0200

    fix(Bankdata): Added login error handling

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/bankdata/BankdataApiClient.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/bankdata/BankdataApiClient.java
@@ -4,9 +4,14 @@ import java.util.List;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import se.tink.backend.agents.rpc.Provider;
+import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
+import se.tink.backend.aggregation.agents.exceptions.LoginException;
+import se.tink.backend.aggregation.agents.exceptions.errors.AuthorizationError;
+import se.tink.backend.aggregation.agents.exceptions.errors.LoginError;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.bankdata.authenticator.rpc.LoginRequest;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.bankdata.authenticator.rpc.LoginResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.bankdata.authenticator.rpc.TimeTokenResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.bankdata.error.LoginErrorResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.bankdata.fetcher.entities.BankdataDepositEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.bankdata.fetcher.entities.BankdataPoolAccountEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.bankdata.fetcher.rpc.AssetDetailsRequest;
@@ -21,6 +26,7 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.bankdata.
 import se.tink.backend.aggregation.nxgen.http.RequestBuilder;
 import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.http.URL;
+import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
 
 public class BankdataApiClient {
 
@@ -37,8 +43,41 @@ public class BankdataApiClient {
         return getRequest(BankdataConstants.Url.LOGIN_TIME_TOKEN).get(TimeTokenResponse.class);
     }
 
-    public LoginResponse pinLogin(LoginRequest request) {
-        return postRequest(BankdataConstants.Url.LOGIN).post(LoginResponse.class, request);
+    public LoginResponse pinLogin(LoginRequest request)
+            throws AuthorizationException, LoginException {
+        try {
+            return postRequest(BankdataConstants.Url.LOGIN).post(LoginResponse.class, request);
+        } catch (HttpResponseException e) {
+            LoginErrorResponse error = e.getResponse().getBody(LoginErrorResponse.class);
+
+            handleLoginErrors(error);
+
+            throw new IllegalStateException(
+                    String.format(
+                            "Unhandled login error: errorCode: %s, errorMessage: %s, getNumberOfLoginAttemptsLeft: %s",
+                            error.getErrorCode(),
+                            error.getErrorMessage(),
+                            error.getNumberOfLoginAttemptsLeft()));
+        }
+    }
+
+    private void handleLoginErrors(LoginErrorResponse error)
+            throws AuthorizationException, LoginException {
+
+        if (error.isBlocked()) {
+            throw AuthorizationError.ACCOUNT_BLOCKED.exception();
+        } else if (error.getErrorCode() == BankdataConstants.ErrorCodes.INCORRECT_CREDENTIALS) {
+
+            if (error.isLastChance()) {
+                throw LoginError.INCORRECT_CREDENTIALS_LAST_ATTEMPT.exception();
+            }
+
+            throw LoginError.INCORRECT_CREDENTIALS.exception();
+
+        } else if (error.getErrorCode()
+                == BankdataConstants.ErrorCodes.ACCOUNT_NOT_ACTIVATED_IN_ONLINE_BANK) {
+            throw AuthorizationError.NO_VALID_PROFILE.exception();
+        }
     }
 
     public GetAccountsResponse getAccounts() {
