8a2c125eacb4e08bd12a8b9d2c96d65da1270c25
Jesper Bränn <jesper.brann@tink.se>
2019-06-20 14:22:37 +0200

    feat(version): fix version in docker container
    Similar to the fix made here
    https://github.com/tink-ab/tink-backend/pull/12190

--- .buildkite/build-and-push-docker.sh
+++ .buildkite/build-and-push-docker.sh
@@ -8,5 +8,8 @@ export PATH="/usr/local/bin/google-cloud-sdk/bin:$PATH"
 echo "$GOOGLE_CLOUD_ACCOUNT_JSON" | base64 --decode | gcloud auth activate-service-account --key-file=-
 yes | gcloud auth configure-docker || true
 
+echo "--- Generate versions file"
+.buildkite/generate-version-file.sh
+
 echo "--- Build and push image to gcr.io"
 ./bazel-wrapper run --workspace_status_command $(pwd)/stamp.sh --disk_cache=/cache --repository_cache=/cache/external $TARGET
