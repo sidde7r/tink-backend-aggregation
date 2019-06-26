25f363210cc48d3bc6747991970c43f3b77013b8
Krzysztof Korolczuk <krzysztof.korolczuk@tink.se>
2019-06-26 16:09:01 +0200

    fix(sibs): Wrong integration name for caixa

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/caixacrl/CaixaCrlConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/caixacrl/CaixaCrlConstants.java
@@ -2,5 +2,5 @@ package se.tink.backend.aggregation.agents.nxgen.pt.openbanking.caixacrl;
 
 public class CaixaCrlConstants {
 
-    public static final String INTEGRATION_NAME = "caixa-crl";
+    public static final String INTEGRATION_NAME = "caixacrl-pt";
 }
