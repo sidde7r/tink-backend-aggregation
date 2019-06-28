ba3f2b7a61ea4088dd88e72e9a9bb8a6a25c6043
tzietek <tymon.zietek@tink.se>
2019-06-28 11:41:30 +0200

    feat(CreditAgricoleAgent): adds new classes to procceed with getting additional data afther OAuth1

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/creditagricole/postauthentication/CreditAgricolePostAuthentication.java
@@ -0,0 +1,8 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.creditagricole.postauthentication;
+
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.creditagricole.CreditAgricoleApiClient;
+
+public class CreditAgricolePostAuthentication {
+
+    public void getAdditionalParamsPostAuthentication(CreditAgricoleApiClient apiClient) {}
+}
