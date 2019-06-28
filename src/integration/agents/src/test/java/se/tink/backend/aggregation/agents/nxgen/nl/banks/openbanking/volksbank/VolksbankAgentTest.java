87b7a371cdb06744238c0a09d58487ec4999415f
Sebastian Olsson <sebastian.olsson@tink.se>
2019-06-28 12:09:37 +0200

    test(nl-sns): Introduced ArgumentManager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/VolksbankAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/VolksbankAgentTest.java
@@ -1,17 +1,34 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
-@Ignore
-public class VolksbankAgentTest {
+public final class VolksbankAgentTest {
+    private enum Arg {
+        LOAD_BEFORE,
+        SAVE_AFTER,
+    }
+
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
+
+    @Before
+    public void before() {
+        manager.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
 
     @Test
     public void testSandbox() throws Exception {
         new AgentIntegrationTest.Builder("nl", "nl-snsbank-sandbox-oauth2")
-                .loadCredentialsBefore(false)
-                .saveCredentialsAfter(false)
+                .loadCredentialsBefore(Boolean.parseBoolean(manager.get(Arg.LOAD_BEFORE)))
+                .saveCredentialsAfter(Boolean.parseBoolean(manager.get(Arg.SAVE_AFTER)))
                 .expectLoggedIn(false)
                 .build()
                 .testRefresh();
