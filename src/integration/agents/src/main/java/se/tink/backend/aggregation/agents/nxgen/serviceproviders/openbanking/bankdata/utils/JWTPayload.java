b35ae86318edfb0fcaff6a5132a5b7e40295f814
Louise Zetterlund <louise.zetterlund@tink.se>
2019-06-28 13:09:26 +0200

    fix(Bankdata): Few changes after review

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/bankdata/utils/JWTPayload.java
+++
@@ -1,3 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bankdata.utils;
-
-public interface JWTPayload {}
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/bankdata/utils/JWTUtils.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bankdata.utils;
-
-public final class JWTUtils {
-
-    private JWTUtils() {
-        throw new AssertionError("Suppress default constructor for noninstantiability");
-    }
-}
