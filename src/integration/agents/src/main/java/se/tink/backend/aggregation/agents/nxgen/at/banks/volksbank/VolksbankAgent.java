c5899517ce166a5cbee10a2aa1bd44a70951ac99
Sebastian Olsson <sebastian.olsson@tink.se>
2019-07-01 11:23:03 +0200

    refactor(nl-sns): Removed redundant method

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/banks/volksbank/VolksbankAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/banks/volksbank/VolksbankAgent.java
@@ -18,7 +18,6 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.Transac
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginationController;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
-import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
 public final class VolksbankAgent extends NextGenerationAgent
@@ -31,15 +30,11 @@ public final class VolksbankAgent extends NextGenerationAgent
     public VolksbankAgent(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
         super(request, context, signatureKeyPair);
-        configureHttpClient(client);
+        client.setUserAgent(VolksbankConstants.USER_AGENT);
         this.apiClient = VolksbankApiClient.create(persistentStorage, sessionStorage, client);
 
         this.transactionalAccountRefreshController =
                 constructTransactionalAccountRefreshController();
-    }
-
-    protected void configureHttpClient(TinkHttpClient client) {
-        client.setUserAgent(VolksbankConstants.USER_AGENT);
     }
 
     @Override
