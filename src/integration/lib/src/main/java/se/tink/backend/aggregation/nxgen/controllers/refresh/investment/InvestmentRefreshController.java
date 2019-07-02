c73bf7073102e4b0b626d099184749ce12fc5d61
Ahmed Hussein <ahmed.hussein@tink.se>
2019-07-02 16:38:38 +0200

    feat(SubsequentGenerationAgent): Fetch Investment transactions

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/investment/InvestmentRefreshController.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/investment/InvestmentRefreshController.java
@@ -1,19 +1,30 @@
 package se.tink.backend.aggregation.nxgen.controllers.refresh.investment;
 
 import com.google.common.base.Preconditions;
-import java.util.*;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import se.tink.backend.agents.rpc.Account;
 import se.tink.backend.aggregation.agents.models.AccountFeatures;
+import se.tink.backend.aggregation.agents.models.Transaction;
 import se.tink.backend.aggregation.nxgen.controllers.metrics.MetricRefreshAction;
 import se.tink.backend.aggregation.nxgen.controllers.metrics.MetricRefreshController;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountRefresher;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.TransactionRefresher;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.UpdateController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcher;
 import se.tink.backend.aggregation.nxgen.core.account.investment.InvestmentAccount;
+import se.tink.backend.aggregation.nxgen.core.transaction.AggregationTransaction;
 import se.tink.libraries.metrics.MetricId;
 import se.tink.libraries.pair.Pair;
 
-public final class InvestmentRefreshController implements AccountRefresher {
+public final class InvestmentRefreshController implements AccountRefresher, TransactionRefresher {
     private static final MetricId.MetricLabels METRIC_ACCOUNT_TYPE =
             new MetricId.MetricLabels().add(AccountRefresher.METRIC_ACCOUNT_TYPE, "investment");
 
@@ -22,6 +33,7 @@ public final class InvestmentRefreshController implements AccountRefresher {
     private final AccountFetcher<InvestmentAccount> investmentFetcher;
 
     private Collection<InvestmentAccount> investments;
+    private TransactionFetcher<InvestmentAccount> transactionFetcher;
 
     public InvestmentRefreshController(
             MetricRefreshController metricRefreshController,
@@ -30,6 +42,15 @@ public final class InvestmentRefreshController implements AccountRefresher {
         this.metricRefreshController = Preconditions.checkNotNull(metricRefreshController);
         this.updateController = Preconditions.checkNotNull(updateController);
         this.investmentFetcher = Preconditions.checkNotNull(investmentFetcher);
+    }
+
+    public InvestmentRefreshController(
+            MetricRefreshController metricRefreshController,
+            UpdateController updateController,
+            AccountFetcher<InvestmentAccount> investmentFetcher,
+            TransactionFetcher<InvestmentAccount> transactionFetcher) {
+        this(metricRefreshController, updateController, investmentFetcher);
+        this.transactionFetcher = Preconditions.checkNotNull(transactionFetcher);
     }
 
     @Override
@@ -44,7 +65,7 @@ public final class InvestmentRefreshController implements AccountRefresher {
 
             Map<Account, AccountFeatures> systemAccounts = new HashMap<>();
 
-            for (InvestmentAccount account : getInvestments()) {
+            for (InvestmentAccount account : getInvestmentAccounts()) {
                 Pair<Account, AccountFeatures> systemAccount =
                         updateController.updateAccount(account);
                 if (systemAccount != null) {
@@ -63,7 +84,42 @@ public final class InvestmentRefreshController implements AccountRefresher {
         }
     }
 
-    private Collection<InvestmentAccount> getInvestments() {
+    @Override
+    public Map<Account, List<Transaction>> fetchTransactions() {
+        MetricRefreshAction action =
+                metricRefreshController.buildAction(
+                        TransactionRefresher.METRIC_ID.label(METRIC_ACCOUNT_TYPE),
+                        TransactionRefresher.METRIC_COUNTER_BUCKETS);
+
+        try {
+            action.start();
+
+            Map<Account, List<Transaction>> transactionsMap =
+                    getInvestmentAccounts().stream()
+                            .map(this::fetchTransactionsFor)
+                            .filter(Objects::nonNull)
+                            .peek((pair -> action.count(pair.second.size())))
+                            .collect(Collectors.toMap(pair -> pair.first, pair -> pair.second));
+
+            action.completed();
+            return transactionsMap;
+        } catch (RuntimeException e) {
+            action.failed();
+            throw e;
+        } finally {
+            action.stop();
+        }
+    }
+
+    private Pair<Account, List<Transaction>> fetchTransactionsFor(InvestmentAccount account) {
+        List<AggregationTransaction> transactions =
+                Optional.ofNullable(transactionFetcher)
+                        .map(fetcher -> fetcher.fetchTransactionsFor(account))
+                        .orElseGet(Collections::emptyList);
+        return updateController.updateTransactions(account, transactions);
+    }
+
+    private Collection<InvestmentAccount> getInvestmentAccounts() {
         if (investments == null) {
             investments =
                     Optional.ofNullable(investmentFetcher.fetchAccounts())
