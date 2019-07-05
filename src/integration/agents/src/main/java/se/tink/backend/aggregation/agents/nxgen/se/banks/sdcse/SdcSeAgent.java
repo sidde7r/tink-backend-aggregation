a085b84720de6602e5cbfacfaa4c7435e3dd7ee1
Mattias Hird <mattias.hird@tink.se>
2019-07-05 14:46:49 +0200

    refactor(sdc): Use new bankid auth interface
    Make the sdc agents use the new interface for bankid authentication

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/sdcse/SdcSeAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/sdcse/SdcSeAgent.java
@@ -39,7 +39,8 @@ public class SdcSeAgent extends SdcAgent implements RefreshIdentityDataExecutor 
         return new BankIdAuthenticationController<>(
                 supplementalRequester,
                 new SdcBankIdAuthenticator(bankClient, sdcSessionStorage, credentials),
-                true);
+                true,
+                persistentStorage);
     }
 
     @Override
