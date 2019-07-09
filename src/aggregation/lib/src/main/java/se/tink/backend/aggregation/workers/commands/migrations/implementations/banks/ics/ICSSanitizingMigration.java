e81f3d10d70ccab74fffbd5be824362c88a93cfa
Alexander Jamm <alexander.jamm@tink.se>
2019-07-09 14:04:03 +0200

    fix(ICS): migration for ICS accounts

---
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/migrations/implementations/banks/ics/ICSSanitizingMigration.java
@@ -0,0 +1,37 @@
+package se.tink.backend.aggregation.workers.commands.migrations.implementations.banks.ics;
+
+import se.tink.backend.agents.rpc.Account;
+import se.tink.backend.agents.rpc.Provider;
+import se.tink.backend.aggregation.workers.commands.migrations.ClusterSafeAgentVersionMigration;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class ICSSanitizingMigration extends ClusterSafeAgentVersionMigration {
+    @Override
+    public boolean isOldAgent(Provider provider) {
+        return true;
+    }
+
+    @Override
+    public boolean isNewAgent(Provider provider) {
+        return true;
+    }
+
+    @Override
+    public String getNewAgentClassName(Provider oldProvider) {
+        return oldProvider.getClassName();
+    }
+
+    @Override
+    public boolean isDataMigrated(CredentialsRequest request) {
+        return request.getAccounts().stream().noneMatch(this::shouldMigrate);
+    }
+
+    @Override
+    public void migrateData(CredentialsRequest request) {
+        request.getAccounts().forEach(account -> account.setId(account.getAccountNumber()));
+    }
+
+    private boolean shouldMigrate(Account account) {
+        return !account.getUserId().equals(account.getAccountNumber());
+    }
+}
