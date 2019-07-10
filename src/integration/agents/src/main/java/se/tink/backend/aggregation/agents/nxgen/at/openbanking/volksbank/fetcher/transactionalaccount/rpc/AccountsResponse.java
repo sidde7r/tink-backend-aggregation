83129820a4f230bfb9e51906ab1ce4c67bb52034
Ivan Furdek <i.furdek@vegaitsourcing.rs>
2019-07-10 16:30:08 +0200

    fix(Volksbank): Use Collections instead of Lists

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/volksbank/fetcher/transactionalaccount/rpc/AccountsResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/volksbank/fetcher/transactionalaccount/rpc/AccountsResponse.java
@@ -1,8 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.at.openbanking.volksbank.fetcher.transactionalaccount.rpc;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import org.assertj.core.util.Lists;
 import se.tink.backend.aggregation.agents.nxgen.at.openbanking.volksbank.fetcher.transactionalaccount.entity.account.AccountEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
@@ -12,6 +12,6 @@ public class AccountsResponse {
     public List<AccountEntity> accounts;
 
     public List<AccountEntity> getAccounts() {
-        return Optional.ofNullable(accounts).orElse(Lists.emptyList());
+        return Optional.ofNullable(accounts).orElse(Collections.emptyList());
     }
 }
