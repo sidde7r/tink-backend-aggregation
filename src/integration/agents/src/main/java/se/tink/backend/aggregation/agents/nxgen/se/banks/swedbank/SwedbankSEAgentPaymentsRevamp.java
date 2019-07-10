37430a22e52fe3b31ddd9db4f4af054e794af268
Jonas Renold <jonas.renold@tink.se>
2019-07-10 12:08:57 +0200

    ref(loan/refresh): use refresh executor in agent SwedbankSEAgentPaymentsRevamp
    The refactoring process:
    - Move controller construction to agent constructor
    - Remove constructLoanRefreshController
    - Add methods defined in RefreshLoanAccountsExecutor

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/SwedbankSEAgentPaymentsRevamp.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/SwedbankSEAgentPaymentsRevamp.java
@@ -1,14 +1,18 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank;
 
-import java.util.Optional;
 import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.FetchLoanAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
+import se.tink.backend.aggregation.agents.RefreshLoanAccountsExecutor;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.loan.SwedbankSELoanFetcher;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.swedbank.SwedbankAbstractAgentPaymentsRevamp;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.loan.LoanRefreshController;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
-public class SwedbankSEAgentPaymentsRevamp extends SwedbankAbstractAgentPaymentsRevamp {
+public class SwedbankSEAgentPaymentsRevamp extends SwedbankAbstractAgentPaymentsRevamp
+        implements RefreshLoanAccountsExecutor {
+    private final LoanRefreshController loanRefreshController;
 
     public SwedbankSEAgentPaymentsRevamp(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
@@ -18,14 +22,21 @@ public class SwedbankSEAgentPaymentsRevamp extends SwedbankAbstractAgentPayments
                 signatureKeyPair,
                 new SwedbankSEConfiguration(request.getProvider().getPayload()),
                 new SwedbankSEApiClientProvider());
-    }
 
-    @Override
-    protected Optional<LoanRefreshController> constructLoanRefreshController() {
-        return Optional.of(
+        this.loanRefreshController =
                 new LoanRefreshController(
                         metricRefreshController,
                         updateController,
-                        new SwedbankSELoanFetcher((SwedbankSEApiClient) apiClient)));
+                        new SwedbankSELoanFetcher((SwedbankSEApiClient) apiClient));
+    }
+
+    @Override
+    public FetchLoanAccountsResponse fetchLoanAccounts() {
+        return loanRefreshController.fetchLoanAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchLoanTransactions() {
+        return loanRefreshController.fetchLoanTransactions();
     }
 }
