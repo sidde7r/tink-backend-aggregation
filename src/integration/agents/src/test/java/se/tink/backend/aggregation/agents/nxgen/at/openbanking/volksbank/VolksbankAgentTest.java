22c2dc1a58665463dbd5984ff9725cb04432a7a4
Ivan Furdek <i.furdek@vegaitsourcing.rs>
2019-07-10 16:17:01 +0200

    fix(Volksbank): Use argument manager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/volksbank/VolksbankAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/volksbank/VolksbankAgentTest.java
@@ -1,28 +1,42 @@
 package se.tink.backend.aggregation.agents.nxgen.at.openbanking.volksbank;
 
+import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field.Key;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
-@Ignore
 public class VolksbankAgentTest {
 
-    private final String TEST_PSU_ID = "100002";
-    private final String TEST_PSU_ID_TYPE = "disposer-nr";
+    // TEST_PSU_ID => "100002"
+    // TEST_PSU_ID_TYPE => "disposer-nr"
+
+    private enum Arg {
+        PSU_ID,
+        PSU_ID_TYPE
+    }
+
+    private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
 
     private AgentIntegrationTest.Builder builder;
 
     @Before
     public void setup() {
+        manager.before();
+
         builder =
                 new AgentIntegrationTest.Builder("at", "at-volksbank-oauth2")
-                        .addCredentialField(Key.LOGIN_INPUT, TEST_PSU_ID)
-                        .addCredentialField(Key.LOGIN_DESCRIPTION, TEST_PSU_ID_TYPE)
+                        .addCredentialField(Key.LOGIN_INPUT, manager.get(Arg.PSU_ID))
+                        .addCredentialField(Key.LOGIN_DESCRIPTION, manager.get(Arg.PSU_ID_TYPE))
                         .loadCredentialsBefore(false)
                         .saveCredentialsAfter(false)
                         .expectLoggedIn(false);
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
     }
 
     @Test
