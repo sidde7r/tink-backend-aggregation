1e75c50a6496da44ee6d4a88effb13d592064a01
Mattias Hird <mattias.hird@tink.se>
2019-07-10 10:52:05 +0200

    docs(bankid auth): Add docs regarding the oauth methods

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/bankid/BankIdAuthenticator.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/bankid/BankIdAuthenticator.java
@@ -13,9 +13,25 @@ public interface BankIdAuthenticator<T> {
 
     BankIdStatus collect(T reference) throws AuthenticationException, AuthorizationException;
 
+    /**
+     * For bankid authenticators built for PSD2 agents this should return the OAuth2Token retrieved
+     * in its authentication calls. If it is an authenticator that does not use OAuth2, this should
+     * return Optional.empty().
+     *
+     * <p>Basically, if it is a PSD2 agent with bankid, implement this method to return the
+     * OAuth2Token if it is an RE agent, return Optional.empty()
+     *
+     * @return an Optional containing the OAuth2Token or Optional.empty() if there is no token.
+     */
     Optional<String> getAutostartToken();
 
     Optional<OAuth2Token> getAcessToken();
 
+    /**
+     * Attempts to use the refresh token supplied to return a new OAuth2Token to extend validity.
+     *
+     * @param refreshToken
+     * @return An Optional containing the OAuth2Token or Optional.empty()
+     */
     Optional<OAuth2Token> refreshAccessToken(final String refreshToken);
 }
