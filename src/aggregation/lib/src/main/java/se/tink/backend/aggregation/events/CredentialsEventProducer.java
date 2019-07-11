63960e12a43c0860a7fdab4d30943dd62ed29897
Nisha Bhagtani <nisha.bhagtani@tink.se>
2019-07-11 14:10:43 +0200

    feat(kpi-events): Added cred event producer to aggr

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/CredentialsEventProducer.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/CredentialsEventProducer.java
@@ -1,11 +1,50 @@
 package se.tink.backend.aggregation.events;
 
+import com.google.common.base.Strings;
+import com.google.protobuf.Any;
+import java.time.Instant;
+import javax.annotation.Nullable;
+import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.tink.backend.agents.rpc.Credentials;
+import se.tink.eventproducerservice.events.grpc.CredentialsRefreshCommandChainStartedProto.CredentialsRefreshCommandChainStarted;
+import se.tink.libraries.event_producer_service_client.grpc.EventProducerServiceClient;
+import se.tink.libraries.serialization.proto.utils.ProtobufTypeUtil;
+//import se.tink.backend.agents.rpc.Credentials;
+//import se.tink.libraries.event_producer_service_client.grpc.EventProducerServiceClient;
 
 public class CredentialsEventProducer {
 
-  private static final Logger log =
-      LoggerFactory.getLogger(CredentialsEventProducer.class);
+    private static final Logger logger = LoggerFactory.getLogger(CredentialsEventProducer.class);
+    private final EventProducerServiceClient eventProducerServiceClient;
 
+
+    @Inject
+    public CredentialsEventProducer(
+            @Nullable EventProducerServiceClient eventProducerServiceClient) {
+        this.eventProducerServiceClient = eventProducerServiceClient;
+    }
+
+    public void sendCredentialsRefreshCommandChainStarted(Credentials credentials, String appId) {
+        CredentialsRefreshCommandChainStarted data =
+                CredentialsRefreshCommandChainStarted.newBuilder()
+                        .setTimestamp(ProtobufTypeUtil.toProtobufTimestamp(Instant.now()))
+                        .setAppId(Strings.nullToEmpty(appId))
+                        .setUserId(credentials.getUserId())
+                        .setCredentialsId(credentials.getId())
+                        .setProviderName(credentials.getProviderName())
+                        .build();
+
+        sendEvent(Any.pack(data));
+    }
+
+    private void sendEvent(Any data) {
+        if (eventProducerServiceClient == null) {
+            logger.info(
+                    "No EventProducerService service is configured. Skipping posting of event.");
+            return;
+        }
+        eventProducerServiceClient.postEventAsync(data);
+    }
 }
