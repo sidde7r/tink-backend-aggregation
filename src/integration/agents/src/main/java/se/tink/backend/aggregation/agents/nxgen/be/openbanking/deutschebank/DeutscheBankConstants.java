97b03f9692c9c4fda0bcd6eac5e0a9f757893b24
Luka Klacar <l.klacar@vegaitsourcing.rs>
2019-07-12 14:05:22 +0200

    fix(DeutscheBank): Removed HeaderKeys

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/deutschebank/DeutscheBankConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/deutschebank/DeutscheBankConstants.java
@@ -48,8 +48,6 @@ public final class DeutscheBankConstants {
         public static final String DELTA_LIST = "true";
     }
 
-    public static class HeaderKeys {}
-
     public static class FormKeys {}
 
     public static class FormValues {}
