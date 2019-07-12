d240b6cbf9eaa17a1432ab63e1ac096619a62cba
Gustaf Svanerud <gustaf.svanerud@tink.se>
2019-07-12 14:54:58 +0200

    chore(nordea): delete nordea openbanking agent
    This agent now lives in `tink-backend-integration-thirdparties`.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/openbanking/NordeaDKConstants.java
+++
@@ -1,5 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.openbanking;
-
-public class NordeaDKConstants {
-    public static final String COUNTRY = "DK";
-}
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/nordea/openbanking/transactionalaccount/NordeaFIAccountParser.java
+++
@@ -1,5 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.fi.banks.nordea.openbanking.transactionalaccount;
-
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.nordeaopenbanking.v3.transactionalaccount.NordeaAccountParser;
-
-public class NordeaFIAccountParser extends NordeaAccountParser {}
