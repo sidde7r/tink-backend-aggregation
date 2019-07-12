f0bdfaf5590082f0fae50bf29d7cbd71d532798b
Gustaf Svanerud <gustaf.svanerud@tink.se>
2019-07-12 16:01:55 +0200

    chore(sibs): delete sibs agent
    This agent now lives in `tink-backend-aggregation`.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/caixa/CaixaConstants.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.caixa;
-
-public class CaixaConstants {
-
-    public static class Market {
-        public static final String INTEGRATION_NAME = "caixa-fr";
-    }
-}
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/bancomontepio/BancoMonteptioConstants.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.pt.openbanking.bancomontepio;
-
-public class BancoMonteptioConstants {
-
-    public static class Market {
-        public static final String INTEGRATION_NAME = "bancomontepio";
-    }
-}
