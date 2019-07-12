42b43f11e5f9085e14c05ea761cd978e70421e4b
Vojin Pesalj <woyin95@gmail.com>
2019-07-12 14:57:41 +0200

    fix(Xs2aDevelopers): Removed @Ignore anotations

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/axa/AxaAgentPaymentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/axa/AxaAgentPaymentTest.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Random;
 import org.iban4j.CountryCode;
 import org.iban4j.Iban;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager;
@@ -20,7 +19,6 @@ import se.tink.libraries.payment.rpc.Creditor;
 import se.tink.libraries.payment.rpc.Debtor;
 import se.tink.libraries.payment.rpc.Payment;
 
-@Ignore
 public class AxaAgentPaymentTest {
     private final ArgumentManager<Arg> manager = new ArgumentManager<>(Arg.values());
     private AgentIntegrationTest.Builder builder;
