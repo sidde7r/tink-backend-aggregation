fdd24c0751b3c8f5b70a827ccedbf220e37cfb10
Nisha Bhagtani <nisha.bhagtani@tink.se>
2019-07-12 13:09:58 +0200

    test(kpi-events): modifying unit test to execute command

--- src/aggregation/lib/src/test/java/se/tink/backend/aggregation/events/CredentialsEventProducerTest.java
+++ src/aggregation/lib/src/test/java/se/tink/backend/aggregation/events/CredentialsEventProducerTest.java
@@ -1,71 +1,67 @@
 package se.tink.backend.aggregation.events;
 
 import com.google.protobuf.Any;
-import com.google.protobuf.InvalidProtocolBufferException;
+import org.assertj.core.api.Assertions;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import se.tink.backend.agents.rpc.Credentials;
+import se.tink.backend.aggregation.workers.AgentWorkerCommandResult;
 import se.tink.eventproducerservice.events.grpc.CredentialsRefreshCommandChainStartedProto.CredentialsRefreshCommandChainStarted;
 import se.tink.libraries.event_producer_service_client.grpc.EventProducerServiceClient;
 import se.tink.libraries.uuid.UUIDUtils;
 
 public class CredentialsEventProducerTest {
 
-  private Credentials validCredentials;
-  private CredentialsEventProducer credentialsEventProducer;
-  private EventProducerServiceClient eventProducerServiceClient;
-  private String appId;
+    private Credentials validCredentials;
+    private EventProducerServiceClient eventProducerServiceClient;
+    private String appId;
 
-  @Before
-  public void setup() {
-    this.eventProducerServiceClient = Mockito.mock(EventProducerServiceClient.class);
-    this.credentialsEventProducer = new CredentialsEventProducer(eventProducerServiceClient);
-    this.validCredentials = buildValidCredentials();
-    this.appId = UUIDUtils.generateUUID();
-  }
+    @Before
+    public void setup() {
+        this.eventProducerServiceClient = Mockito.mock(EventProducerServiceClient.class);
+        this.validCredentials = buildValidCredentials();
+        this.appId = UUIDUtils.generateUUID();
+    }
 
-  @Test
-  public void
-  testSendCredentialsRefreshCommandChainStarted_WhenClientIsNotAvailable_EventShouldBeDropped() {
-    credentialsEventProducer = new CredentialsEventProducer(null);
-    credentialsEventProducer.sendCredentialsRefreshCommandChainStarted(validCredentials, appId);
-  }
+    @Test
+    public void
+            testCredentialsRefreshCommandChainStarted_WhenClientIsAvailable_EventShouldBePosted()
+                    throws Exception {
 
-  @Test
-  public void
-  testCredentialsRefreshCommandChainStarted_WhenClientIsAvailable_EventShouldBePosted()
-      throws InvalidProtocolBufferException {
+        ArgumentCaptor<Any> capture = ArgumentCaptor.forClass(Any.class);
 
-    ArgumentCaptor<Any> capture = ArgumentCaptor.forClass(Any.class);
+        CredentialsEventProducer credentialsEventProducer =
+                new CredentialsEventProducer(eventProducerServiceClient, validCredentials, appId);
 
-    credentialsEventProducer.sendCredentialsRefreshCommandChainStarted(validCredentials, appId);
-    Mockito.verify(eventProducerServiceClient, Mockito.times(1))
-        .postEventAsync(capture.capture());
+        Assertions.assertThat(credentialsEventProducer.execute())
+                .isEqualTo(AgentWorkerCommandResult.CONTINUE);
 
-    CredentialsRefreshCommandChainStarted output =
-        capture.getValue().unpack(CredentialsRefreshCommandChainStarted.class);
+        Mockito.verify(eventProducerServiceClient, Mockito.times(1))
+                .postEventAsync(capture.capture());
 
-    CredentialsRefreshCommandChainStarted data =
-        CredentialsRefreshCommandChainStarted.newBuilder()
-            .setTimestamp(output.getTimestamp())
-            .setAppId(appId)
-            .setUserId(validCredentials.getUserId())
-            .setCredentialsId(validCredentials.getId())
-            .setProviderName(validCredentials.getProviderName())
-            .build();
+        CredentialsRefreshCommandChainStarted output =
+                capture.getValue().unpack(CredentialsRefreshCommandChainStarted.class);
 
-    Assert.assertEquals(data, output);
-  }
+        CredentialsRefreshCommandChainStarted data =
+                CredentialsRefreshCommandChainStarted.newBuilder()
+                        .setTimestamp(output.getTimestamp())
+                        .setAppId(appId)
+                        .setUserId(validCredentials.getUserId())
+                        .setCredentialsId(validCredentials.getId())
+                        .setProviderName(validCredentials.getProviderName())
+                        .build();
 
-  private Credentials buildValidCredentials() {
-    Credentials credentials = new Credentials();
-    credentials.setId(UUIDUtils.generateUUID());
-    credentials.setUserId(UUIDUtils.generateUUID());
-    credentials.setProviderName(UUIDUtils.generateUUID());
-    return credentials;
-  }
+        Assert.assertEquals(data, output);
+    }
 
+    private Credentials buildValidCredentials() {
+        Credentials credentials = new Credentials();
+        credentials.setId(UUIDUtils.generateUUID());
+        credentials.setUserId(UUIDUtils.generateUUID());
+        credentials.setProviderName(UUIDUtils.generateUUID());
+        return credentials;
+    }
 }
