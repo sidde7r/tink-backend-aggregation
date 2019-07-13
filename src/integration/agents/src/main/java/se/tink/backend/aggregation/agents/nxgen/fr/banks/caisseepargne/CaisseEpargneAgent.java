9417ad391edf2ccbf2305004dbe49052690bcb50
Jonas Renold <jonas.renold@tink.se>
2019-07-13 17:55:15 +0200

    refactor(transactionalacc/refresh): use refresh executor in agent CaisseEpargneAgent
    The refactoring process:
    - Make controller construction private and invoke it from agent constructor
    - Add methods defined in RefreshCheckingAccountsExecutor and RefreshSavingsAccountsExecutor
    
    Note that TransactionalAccountRefreshController wraps both checking and savings accounts.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/banks/caisseepargne/CaisseEpargneAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/banks/caisseepargne/CaisseEpargneAgent.java
@@ -1,7 +1,10 @@
 package se.tink.backend.aggregation.agents.nxgen.fr.banks.caisseepargne;
 
-import java.util.Optional;
 import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
+import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
+import se.tink.backend.aggregation.agents.RefreshSavingsAccountsExecutor;
 import se.tink.backend.aggregation.agents.nxgen.fr.banks.caisseepargne.authenticator.CaisseEpargneAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.fr.banks.caisseepargne.fetcher.transactionalaccount.CaisseEpargneTransactionalAccountFetcher;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
@@ -15,9 +18,11 @@ import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 import se.tink.libraries.uuid.UUIDUtils;
 
-public class CaisseEpargneAgent extends NextGenerationAgent {
+public class CaisseEpargneAgent extends NextGenerationAgent
+        implements RefreshCheckingAccountsExecutor, RefreshSavingsAccountsExecutor {
 
     private final CaisseEpargneApiClient apiClient;
+    private final TransactionalAccountRefreshController transactionalAccountRefreshController;
 
     public CaisseEpargneAgent(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
@@ -31,6 +36,8 @@ public class CaisseEpargneAgent extends NextGenerationAgent {
             deviceId = UUIDUtils.generateUUID();
             persistentStorage.put(CaisseEpargneConstants.StorageKey.DEVICE_ID, deviceId);
         }
+
+        transactionalAccountRefreshController = constructTransactionalAccountRefreshController();
     }
 
     @Override
@@ -40,18 +47,35 @@ public class CaisseEpargneAgent extends NextGenerationAgent {
     }
 
     @Override
-    protected Optional<TransactionalAccountRefreshController>
-            constructTransactionalAccountRefreshController() {
+    public FetchAccountsResponse fetchCheckingAccounts() {
+        return transactionalAccountRefreshController.fetchCheckingAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchCheckingTransactions() {
+        return transactionalAccountRefreshController.fetchCheckingTransactions();
+    }
+
+    @Override
+    public FetchAccountsResponse fetchSavingsAccounts() {
+        return transactionalAccountRefreshController.fetchSavingsAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchSavingsTransactions() {
+        return transactionalAccountRefreshController.fetchSavingsTransactions();
+    }
+
+    private TransactionalAccountRefreshController constructTransactionalAccountRefreshController() {
         CaisseEpargneTransactionalAccountFetcher fetcher =
                 new CaisseEpargneTransactionalAccountFetcher(apiClient);
-        return Optional.of(
-                new TransactionalAccountRefreshController(
-                        metricRefreshController,
-                        updateController,
-                        fetcher,
-                        new TransactionFetcherController<>(
-                                transactionPaginationHelper,
-                                new TransactionKeyPaginationController<>(fetcher))));
+        return new TransactionalAccountRefreshController(
+                metricRefreshController,
+                updateController,
+                fetcher,
+                new TransactionFetcherController<>(
+                        transactionPaginationHelper,
+                        new TransactionKeyPaginationController<>(fetcher)));
     }
 
     @Override
