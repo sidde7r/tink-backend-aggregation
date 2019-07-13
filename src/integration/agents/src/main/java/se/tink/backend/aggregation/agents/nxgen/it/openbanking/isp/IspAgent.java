554bb7c22cf35f2b4e453e8986217cc3d29e0069
Jonas Renold <jonas.renold@tink.se>
2019-07-13 17:55:20 +0200

    refactor(transactionalacc/refresh): use refresh executor in agent IspAgent
    The refactoring process:
    - Make controller construction private and invoke it from agent constructor
    - Add methods defined in RefreshCheckingAccountsExecutor and RefreshSavingsAccountsExecutor
    
    Note that TransactionalAccountRefreshController wraps both checking and savings accounts.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/isp/IspAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/isp/IspAgent.java
@@ -1,7 +1,10 @@
 package se.tink.backend.aggregation.agents.nxgen.it.openbanking.isp;
 
-import java.util.Optional;
 import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
+import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
+import se.tink.backend.aggregation.agents.RefreshSavingsAccountsExecutor;
 import se.tink.backend.aggregation.agents.nxgen.it.openbanking.isp.fetcher.transactionalaccount.IspTransactionalAccountFetcher;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeAgent;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
@@ -10,11 +13,15 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.paginat
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
-public class IspAgent extends CbiGlobeAgent {
+public class IspAgent extends CbiGlobeAgent
+        implements RefreshCheckingAccountsExecutor, RefreshSavingsAccountsExecutor {
+    private final TransactionalAccountRefreshController transactionalAccountRefreshController;
 
     public IspAgent(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
         super(request, context, signatureKeyPair);
+
+        transactionalAccountRefreshController = constructTransactionalAccountRefreshController();
     }
 
     @Override
@@ -23,18 +30,35 @@ public class IspAgent extends CbiGlobeAgent {
     }
 
     @Override
-    protected Optional<TransactionalAccountRefreshController>
-            constructTransactionalAccountRefreshController() {
+    public FetchAccountsResponse fetchCheckingAccounts() {
+        return transactionalAccountRefreshController.fetchCheckingAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchCheckingTransactions() {
+        return transactionalAccountRefreshController.fetchCheckingTransactions();
+    }
+
+    @Override
+    public FetchAccountsResponse fetchSavingsAccounts() {
+        return transactionalAccountRefreshController.fetchSavingsAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchSavingsTransactions() {
+        return transactionalAccountRefreshController.fetchSavingsTransactions();
+    }
+
+    private TransactionalAccountRefreshController constructTransactionalAccountRefreshController() {
         final IspTransactionalAccountFetcher accountFetcher =
                 new IspTransactionalAccountFetcher(apiClient, persistentStorage, controller);
 
-        return Optional.of(
-                new TransactionalAccountRefreshController(
-                        metricRefreshController,
-                        updateController,
-                        accountFetcher,
-                        new TransactionFetcherController<>(
-                                transactionPaginationHelper,
-                                new TransactionDatePaginationController<>(accountFetcher))));
+        return new TransactionalAccountRefreshController(
+                metricRefreshController,
+                updateController,
+                accountFetcher,
+                new TransactionFetcherController<>(
+                        transactionPaginationHelper,
+                        new TransactionDatePaginationController<>(accountFetcher)));
     }
 }
