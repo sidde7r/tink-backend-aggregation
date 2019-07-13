3fc3de18c37c7ddb51d2ee263ddcc20ec319032a
Jonas Renold <jonas.renold@tink.se>
2019-07-13 11:42:53 +0200

    refactor(transactional/refresh): use refresh executor in agent CreditAgricoleAgent
    The refactoring process:
    - Make controller construction private and invoke it from agent constructor
    - Add methods defined in RefreshCheckingAccountsExecutor and RefreshSavingsAccountsExecutor

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/creditagricole/CreditAgricoleAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/creditagricole/CreditAgricoleAgent.java
@@ -1,7 +1,10 @@
 package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.creditagricole;
 
-import java.util.Optional;
 import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
+import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
+import se.tink.backend.aggregation.agents.RefreshSavingsAccountsExecutor;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.creditagricole.CreditAgricoleConstants.ErrorMessages;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.creditagricole.authenticator.CreditAgricoleAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.creditagricole.configuration.CreditAgricoleConfiguration;
@@ -14,10 +17,12 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccoun
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
-public final class CreditAgricoleAgent extends NextGenerationAgent {
+public final class CreditAgricoleAgent extends NextGenerationAgent
+        implements RefreshCheckingAccountsExecutor, RefreshSavingsAccountsExecutor {
 
     private final String clientName;
     private final CreditAgricoleApiClient apiClient;
+    private final TransactionalAccountRefreshController transactionalAccountRefreshController;
 
     public CreditAgricoleAgent(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
@@ -25,6 +30,8 @@ public final class CreditAgricoleAgent extends NextGenerationAgent {
 
         apiClient = new CreditAgricoleApiClient(client, persistentStorage);
         clientName = request.getProvider().getPayload();
+
+        transactionalAccountRefreshController = getTransactionalAccountRefreshController();
     }
 
     @Override
@@ -51,14 +58,31 @@ public final class CreditAgricoleAgent extends NextGenerationAgent {
     }
 
     @Override
-    protected Optional<TransactionalAccountRefreshController>
-            constructTransactionalAccountRefreshController() {
+    public FetchAccountsResponse fetchCheckingAccounts() {
+        return transactionalAccountRefreshController.fetchCheckingAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchCheckingTransactions() {
+        return transactionalAccountRefreshController.fetchCheckingTransactions();
+    }
+
+    @Override
+    public FetchAccountsResponse fetchSavingsAccounts() {
+        return transactionalAccountRefreshController.fetchSavingsAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchSavingsTransactions() {
+        return transactionalAccountRefreshController.fetchSavingsTransactions();
+    }
+
+    private TransactionalAccountRefreshController getTransactionalAccountRefreshController() {
         final CreditAgricoleTransactionalAccountFetcher accountFetcher =
                 new CreditAgricoleTransactionalAccountFetcher(apiClient);
 
-        return Optional.of(
-                new TransactionalAccountRefreshController(
-                        metricRefreshController, updateController, accountFetcher, accountFetcher));
+        return new TransactionalAccountRefreshController(
+                metricRefreshController, updateController, accountFetcher, accountFetcher);
     }
 
     @Override
