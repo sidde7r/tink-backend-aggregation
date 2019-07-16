f17c6f309a5016aa08f8db660faaa9132d6d83cd
Nazri Abdullah <nazri.abdullah@tink.se>
2019-07-16 10:57:54 +0200

    refactor(nl-rabobank): rename variable

--- src/commands/psd2-register/src/main/java/se/tink/backend/aggregation/register/nl/rabobank/entities/PayloadEntity.java
+++ src/commands/psd2-register/src/main/java/se/tink/backend/aggregation/register/nl/rabobank/entities/PayloadEntity.java
@@ -1,17 +1,20 @@
 package se.tink.backend.aggregation.register.nl.rabobank.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
 public final class PayloadEntity {
-    private String ptc_email;
+    @JsonProperty("ptc_email")
+    private String email;
+
     private String organization;
     private Integer exp;
 
     public static PayloadEntity create(
             final int exp, final String email, final String organization) {
         final PayloadEntity entity = new PayloadEntity();
-        entity.ptc_email = email;
+        entity.email = email;
         entity.exp = exp;
         entity.organization = organization;
         return entity;
