fb0a01d35809a062dc934fdf13accbc21e3520e0
Sebastian Olsson <sebastian.olsson@tink.se>
2019-07-16 07:58:46 +0200

    feat(at-bawagpsk): Enabling debugOutput from development.yml

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/banks/easybank/bawagpsk/BawagPskAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/banks/easybank/bawagpsk/BawagPskAgent.java
@@ -17,6 +17,7 @@ import se.tink.backend.aggregation.agents.nxgen.at.banks.easybank.bawagpsk.fetch
 import se.tink.backend.aggregation.agents.nxgen.at.banks.easybank.bawagpsk.fetcher.loan.BawagPskLoanFetcher;
 import se.tink.backend.aggregation.agents.nxgen.at.banks.easybank.bawagpsk.fetcher.transactional.BawagPskTransactionalAccountFetcher;
 import se.tink.backend.aggregation.agents.nxgen.at.banks.easybank.bawagpsk.session.BawagPskSessionHandler;
+import se.tink.backend.aggregation.configuration.AgentsServiceConfiguration;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
 import se.tink.backend.aggregation.nxgen.agents.NextGenerationAgent;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
@@ -72,6 +73,12 @@ public class BawagPskAgent extends NextGenerationAgent
         creditCardRefreshController = constructCreditCardRefreshController();
 
         transactionalAccountRefreshController = constructTransactionalAccountRefreshController();
+    }
+
+    @Override
+    public void setConfiguration(final AgentsServiceConfiguration configuration) {
+        super.setConfiguration(configuration);
+        client.setDebugOutput(configuration.getTestConfiguration().isDebugOutputEnabled());
     }
 
     protected void configureHttpClient(TinkHttpClient client) {
