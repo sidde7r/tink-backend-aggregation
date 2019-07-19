eed4eca2543059c1a25f88465c73ddc52e01b9b9
Richard Stephens <richard.stephens@tink.se>
2019-07-19 14:58:14 +0200

    feat(eidas): improvements to dev cert generation

--- tools/convert-dev-cert.sh
+++ tools/convert-dev-cert.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+set -e
 
-openssl pkcs12 -export -clcerts -in $1 -inkey $2 -out eidas_client.p12 -passout pass:changeme
+ED=$HOME/.eidas
+CLIENTKEY="$ED/client.key"
+
+if ! test -f "$1"; then
+    echo "Certificate not found"
+    exit 1
+fi
+
+if test -f "$CLIENTKEY"; then
+    openssl pkcs12 -export -clcerts -in "$1" -inkey "$CLIENTKEY" -out "$HOME/eidas_client.p12" -passout pass:changeme
+    echo "Your client certificate has been saved to $HOME/eidas_client.p12"
+else
+    echo "client key not found"
+fi
--- tools/generate-dev-cert.sh
+++ tools/generate-dev-cert.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-read -p "Enter your username [$USER]: " name
+ED=$HOME/.eidas
+CLIENTKEY="$ED/client.key"
+mkdir -p "$ED"
+
+if test -f "$CLIENTKEY"; then
+    read -p "$CLIENTKEY already exists, would you like to overwrite? " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        rm "$CLIENTKEY"
+    else
+        echo "cancelled"
+        exit 1
+    fi
+fi
+
+read -rp "Enter your username [$USER]: " name
 name=${name:-$USER}
-echo $name
 
-read -p "Enter your email [$USER@tink.se]: " email
-email=${email:-$USER@tink.se}
-echo $email
+read -rp "Enter your email [$name@tink.se]: " email
+email=${email:-$name@tink.se}
 
-openssl req -new -keyout $name.key -out $name.csr\
+echo "Generating certificate with subject /CN=$name/emailAddress=$email"
+openssl req -new -keyout "$CLIENTKEY" -out "$HOME/$name.csr"\
   -nodes -subj "/CN=$name/emailAddress=$email" -newkey rsa:4096
 
+echo "Your CSR has been saved at $HOME/$name.csr"
 
