72f819ebb76952d7621a44c18da9591df55d779c
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2019-07-22 11:53:33 +0200

    refactor(lib/uuid): Simplify method expressions

--- src/libraries/uuid/src/main/java/se/tink/libraries/uuid/UUIDUtils.java
+++ src/libraries/uuid/src/main/java/se/tink/libraries/uuid/UUIDUtils.java
@@ -13,20 +13,6 @@ public class UUIDUtils {
                     "[a-fA-F0-9]{8}-?[a-fA-F0-9]{4}-?4[a-fA-F0-9]{3}-?[89aAbB][a-fA-F0-9]{3}-?[a-fA-F0-9]{12}");
     private static final Pattern HEXADECIMAL_PATTERN = Pattern.compile("[a-f0-9]{32}");
 
-    public static boolean isValidTinkUUID(String uuid) {
-        if (uuid == null) {
-            return false;
-        }
-        return HEXADECIMAL_PATTERN.matcher(uuid).matches();
-    }
-
-    public static boolean isValidUUIDv4(String uuid) {
-        if (uuid == null) {
-            return false;
-        }
-        return UUID_V4_PATTERN.matcher(uuid).matches();
-    }
-
     /**
      * Convert a Tink UUID {@code String} object (07da44eeae8d448b8b67bdf5e0488832) to a Java {@code
      * UUID} object.
@@ -77,6 +63,7 @@ public class UUIDUtils {
     /** Returns val represented by the specified number of hex digits. */
     private static String digits(long val, int digits) {
         long hi = 1L << (digits * 4);
+
         return Long.toHexString(hi | (val & (hi - 1))).substring(1);
     }
 
@@ -93,10 +80,23 @@ public class UUIDUtils {
      * (24e6b8cb-8e79-46cd-bbec-d354c4111aa5).
      */
     public static UUID fromString(final String value) {
-        if (Strings.isNullOrEmpty(value)) {
-            return null;
-        }
+        return Strings.isNullOrEmpty(value) ? null : UUIDUtils.fromTinkUUID(value.replace("-", ""));
+    }
 
-        return UUIDUtils.fromTinkUUID(value.replace("-", ""));
+    public static boolean isValidTinkUUID(String uuid) {
+        return !Strings.isNullOrEmpty(uuid) && HEXADECIMAL_PATTERN.matcher(uuid).matches();
+    }
+
+    public static boolean isValidUUIDv4(String uuid) {
+        return !Strings.isNullOrEmpty(uuid) && UUID_V4_PATTERN.matcher(uuid).matches();
+    }
+
+    /**
+     * Return true when the specified uuid in a valid UUID V4 format or a valid Tink UUID format.
+     */
+    public static boolean isValidUUID(String uuid) {
+        return !Strings.isNullOrEmpty(uuid) && isValidTinkUUID(uuid.replace("-", ""));
+    }
+
     }
 }
