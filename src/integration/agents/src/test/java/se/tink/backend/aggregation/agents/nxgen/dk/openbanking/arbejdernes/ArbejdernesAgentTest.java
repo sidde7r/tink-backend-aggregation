8bdae2bc4725bfe41bf8d5535a3db8b625715496
John Helbrink <john.helbrink@tink.se>
2019-07-22 16:28:52 +0200

    feat(bec): added sparnordbank

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/arbejdernes/ArbejdernesAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/arbejdernes/ArbejdernesAgentTest.java
@@ -1,10 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.arbejdernes;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
-
+@Ignore
 public class ArbejdernesAgentTest {
     private AgentIntegrationTest.Builder builder;
 
