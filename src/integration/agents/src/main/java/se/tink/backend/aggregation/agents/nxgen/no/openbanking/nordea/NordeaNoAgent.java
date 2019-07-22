276ce5fcfad2109a9112498a981364ae0759905b
ndjuricic <n.djuricic@vegaitsourcing.rs>
2019-07-22 10:10:26 +0200

    refactor(Nordea): Missing imports

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/nordea/NordeaNoAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/nordea/NordeaNoAgent.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.no.openbanking.nordea;
 
+import java.util.Optional;
 import se.tink.backend.aggregation.agents.AgentContext;
 import se.tink.backend.aggregation.agents.FetchAccountsResponse;
 import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
