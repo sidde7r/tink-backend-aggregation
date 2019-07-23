1b8419f14ce3026285e400e1620fbcfbe2c6f98b
EmilieTexier <emilie.texier@tink.se>
2019-07-23 15:52:22 +0200

    Feat(creditmutuel-FR):PSD2 Production AIS Agent

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/creditmutuel/CreditMutuelAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/creditmutuel/CreditMutuelAgentTest.java
@@ -1,10 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.creditmutuel;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
-// @Ignore
+@Ignore
 public class CreditMutuelAgentTest {
 
     private AgentIntegrationTest.Builder builder;
