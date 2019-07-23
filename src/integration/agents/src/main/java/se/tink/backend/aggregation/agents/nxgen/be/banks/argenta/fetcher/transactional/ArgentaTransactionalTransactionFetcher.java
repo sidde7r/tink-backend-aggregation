a76527c4a8d1da108310d240369b053f54fd441b
Nazri Abdullah <nazri.abdullah@tink.se>
2019-07-23 15:04:46 +0200

    fix(be-argenta): transactional account fetching bug

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/argenta/fetcher/transactional/ArgentaTransactionalTransactionFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/argenta/fetcher/transactional/ArgentaTransactionalTransactionFetcher.java
@@ -1,6 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.be.banks.argenta.fetcher.transactional;
 
 import se.tink.backend.aggregation.agents.nxgen.be.banks.argenta.ArgentaApiClient;
+import se.tink.backend.aggregation.agents.nxgen.be.banks.argenta.ArgentaPersistentStorage;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionPagePaginator;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
@@ -8,16 +9,17 @@ import se.tink.backend.aggregation.nxgen.core.account.transactional.Transactiona
 public class ArgentaTransactionalTransactionFetcher
         implements TransactionPagePaginator<TransactionalAccount> {
     private ArgentaApiClient apiClient;
-    private final String deviceId;
+    private final ArgentaPersistentStorage persistentStorage;
 
-    public ArgentaTransactionalTransactionFetcher(ArgentaApiClient apiClient, String deviceId) {
+    public ArgentaTransactionalTransactionFetcher(
+            ArgentaApiClient apiClient, ArgentaPersistentStorage persistentStorage) {
         this.apiClient = apiClient;
-        this.deviceId = deviceId;
+        this.persistentStorage = persistentStorage;
     }
 
     @Override
     public PaginatorResponse getTransactionsFor(TransactionalAccount account, int page) {
         String accountId = account.getAccountNumber();
-        return apiClient.fetchTransactions(accountId, page, deviceId);
+        return apiClient.fetchTransactions(accountId, page, persistentStorage.getDeviceId());
     }
 }
