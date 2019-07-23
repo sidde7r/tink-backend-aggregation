7739aeb18e874b2344c92448e352ba0d3c2e5731
John Helbrink <john.helbrink@tink.se>
2019-07-23 13:44:04 +0200

    feat(bec): Added Vest Jysk Bank

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/vestjyskbank/VestJyskBankAgent.java
@@ -0,0 +1,22 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.vestjyskbank;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.dk.openbanking.vestjyskbank.VestJyskBankConstants.Urls;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bec.BecAgent;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class VestJyskBankAgent extends BecAgent {
+
+
+    public VestJyskBankAgent(CredentialsRequest request,
+        AgentContext context,
+        SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected String getBaseUrl() {
+        return Urls.BASE_URL;
+    }
+}
