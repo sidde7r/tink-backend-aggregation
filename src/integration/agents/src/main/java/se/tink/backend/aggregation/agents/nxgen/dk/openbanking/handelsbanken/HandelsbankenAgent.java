07d95032cf7bcc56fcc1116800b743093fc41820
John Helbrink <john.helbrink@tink.se>
2019-07-23 15:52:12 +0200

    feat(bec): Added Handelsbanken

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/handelsbanken/HandelsbankenAgent.java
@@ -0,0 +1,21 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.handelsbanken;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.dk.openbanking.handelsbanken.HandelsbankenConstants.Urls;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bec.BecAgent;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class HandelsbankenAgent extends BecAgent {
+
+    public HandelsbankenAgent(CredentialsRequest request,
+        AgentContext context,
+        SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected String getBaseUrl() {
+        return Urls.BASE_URL;
+    }
+}
