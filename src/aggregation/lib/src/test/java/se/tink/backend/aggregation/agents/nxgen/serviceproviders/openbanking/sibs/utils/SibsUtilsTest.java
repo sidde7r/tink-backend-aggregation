bfc444846b934a3e2b45c6a7da4df1b6025046e9
Krzysztof Korolczuk <krzysztof.korolczuk@tink.se>
2019-07-23 12:00:08 +0200

    test(sibs): add test for pagination calculation

--- src/aggregation/lib/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/utils/SibsUtilsTest.java
+++ src/aggregation/lib/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/utils/SibsUtilsTest.java
@@ -2,6 +2,11 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.si
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsConstants;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.transactionalaccount.Consent;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SibsUtilsTest {
 
@@ -12,6 +17,9 @@ public class SibsUtilsTest {
                     + "date: dummyDate";
 
     private static final String EXPECTED_DIGEST = "LWFzxWAWcmfw7V0AsX2Tx4pNg3s0WCVJ7w5bO4zwJhs=";
+
+    private static final DateTimeFormatter PAGINATION_DATE_FORMATTER =
+            DateTimeFormatter.ofPattern(SibsConstants.Formats.PAGINATION_DATE_FORMAT);
 
     @Test
     public void shouldReturnSigningString() {
@@ -27,5 +35,25 @@ public class SibsUtilsTest {
         String signature = SibsUtils.getDigest("dummyBody");
 
         Assertions.assertThat(signature).isEqualTo(EXPECTED_DIGEST);
+    }
+
+    @Test
+    public void shouldReturnTransactionsFromBeginningPaginationDate() {
+        Consent consent = new Consent("dummyId", LocalDateTime.now().toString());
+
+        String paginationDate = SibsUtils.getPaginationDate(consent);
+
+        Assertions.assertThat(paginationDate).isEqualTo("1970-01-01");
+    }
+
+    @Test
+    public void shouldReturnLast90DaysTransactionsPaginationDate() {
+        Consent consent = new Consent("dummyId", LocalDateTime.now().minusMinutes(30L).toString());
+
+        String paginationDate = SibsUtils.getPaginationDate(consent);
+
+        String expectedPaginationDate = PAGINATION_DATE_FORMATTER.format(LocalDateTime.now().minusDays(89));
+
+        Assertions.assertThat(paginationDate).isEqualTo(expectedPaginationDate);
     }
 }
