f30fc8cf06d6028c098b788ed16b7ea96f0c15c8
John Helbrink <john.helbrink@tink.se>
2019-07-23 16:44:32 +0200

    feat(bec): Added Andelskassen faelleskassen

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/andelskassen/AndelskassenConstants.java
@@ -0,0 +1,9 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.andelskassen;
+
+public final class AndelskassenConstants {
+
+    public static class Urls {
+        public static final String BASE_URL = "https://psd2api41.prod.bec.dk/eidas/1.0/v1";
+    }
+
+}
