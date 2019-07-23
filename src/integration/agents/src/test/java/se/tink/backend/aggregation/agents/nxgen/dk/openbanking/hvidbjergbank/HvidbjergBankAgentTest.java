55ff4c125a43691c43c116aa5c5f30968f0b1e16
John Helbrink <john.helbrink@tink.se>
2019-07-23 14:07:20 +0200

    feat(bec): Added Hvidbjerg Bank

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/hvidbjergbank/HvidbjergBankAgentTest.java
@@ -0,0 +1,24 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.hvidbjergbank;
+
+import org.junit.Before;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+
+
+public class HvidbjergBankAgentTest {
+    private AgentIntegrationTest.Builder builder;
+
+    @Before
+    public void setup() {
+        builder =
+            new AgentIntegrationTest.Builder("dk", "dk-hvidbjergbank-oauth2")
+                .expectLoggedIn(false)
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false);
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder.build().testRefresh();
+    }
+}
