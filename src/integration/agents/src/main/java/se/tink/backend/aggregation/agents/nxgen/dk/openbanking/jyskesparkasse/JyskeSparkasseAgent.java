638ca628364e60195a3e841c651f6d42f3c1c518
John Helbrink <john.helbrink@tink.se>
2019-07-23 13:06:23 +0200

    feat(bec): Added Den Jyske Sparkasse

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/jyskesparkasse/JyskeSparkasseAgent.java
@@ -0,0 +1,21 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.jyskesparkasse;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.at.openbanking.volksbank.VolksbankConstants.Urls;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bec.BecAgent;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class JyskeSparkasseAgent extends BecAgent {
+
+    public JyskeSparkasseAgent(CredentialsRequest request,
+        AgentContext context,
+        SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected String getBaseUrl() {
+        return Urls.BASE_URL;
+    }
+}
