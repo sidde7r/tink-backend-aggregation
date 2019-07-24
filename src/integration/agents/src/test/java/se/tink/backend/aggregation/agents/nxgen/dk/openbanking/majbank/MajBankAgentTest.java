98fca51816da3ab11c30d44f4dc9d8e4aaf230cf
John Helbrink <john.helbrink@tink.se>
2019-07-24 16:49:10 +0200

    feat(bec): Added Maj Bank

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/majbank/MajBankAgentTest.java
@@ -0,0 +1,26 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.majbank;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+
+@Ignore
+public class MajBankAgentTest {
+
+    private AgentIntegrationTest.Builder builder;
+
+    @Before
+    public void setup() {
+        builder =
+            new AgentIntegrationTest.Builder("dk", "dk-majbank-oauth2")
+                .expectLoggedIn(false)
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false);
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder.build().testRefresh();
+    }
+}
