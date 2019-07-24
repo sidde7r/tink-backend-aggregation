14ff3ae39518c968a71a244e29db9bb5164945e4
berkgedik <berk.gedik@tink.se>
2019-07-24 13:03:00 +0200

    revert(DK/Danske): Removed loggings for debugging

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/danskebank/authenticator/password/DanskeBankPasswordAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/danskebank/authenticator/password/DanskeBankPasswordAuthenticator.java
@@ -13,7 +13,6 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskeban
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.authenticator.DanskeBankAbstractAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.authenticator.rpc.FinalizeAuthenticationRequest;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.authenticator.rpc.FinalizeAuthenticationResponse;
-import se.tink.backend.aggregation.log.AggregationLogger;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.password.PasswordAuthenticator;
 import se.tink.backend.aggregation.nxgen.http.HttpResponse;
 import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
@@ -21,8 +20,6 @@ import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
 public class DanskeBankPasswordAuthenticator extends DanskeBankAbstractAuthenticator
         implements PasswordAuthenticator {
 
-    private static final AggregationLogger log =
-            new AggregationLogger(DanskeBankPasswordAuthenticator.class);
     private final DanskeBankApiClient apiClient;
     private final String deviceId;
     private final DanskeBankConfiguration configuration;
@@ -74,8 +71,6 @@ public class DanskeBankPasswordAuthenticator extends DanskeBankAbstractAuthentic
             // Finalize authentication
             finalizeAuthentication();
         } catch (HttpResponseException hre) {
-            log.errorExtraLong(
-                    "Error in authenticate", DanskeBankConstants.loginPasswordAuthError, hre);
             DanskeBankPasswordErrorHandler.throwError(hre);
         } finally {
             if (driver != null) {
