231ce23a4bedbd69e7ad19cf92b26b834fe0031d
John Helbrink <john.helbrink@tink.se>
2019-07-24 16:00:14 +0200

    feat(bec): Added Frorup andelskasse

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/frorupandelskasse/FrorupAndelskasseAgentTest.java
@@ -0,0 +1,25 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.frorupandelskasse;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+
+@Ignore
+public class FrorupAndelskasseAgentTest {
+    private AgentIntegrationTest.Builder builder;
+
+    @Before
+    public void setup() {
+        builder =
+            new AgentIntegrationTest.Builder("dk", "dk-frorupandelskasse-oauth2")
+                .expectLoggedIn(false)
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false);
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder.build().testRefresh();
+    }
+}
