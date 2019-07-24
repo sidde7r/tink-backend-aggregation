d648b210f7e9fea0afd8778239db1511a53ce253
John Helbrink <john.helbrink@tink.se>
2019-07-24 16:14:21 +0200

    feat(bec): Added Merkur Andelskasse

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/merkurandelskasse/MerkurAndelskasseAgent.java
@@ -0,0 +1,21 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.merkurandelskasse;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.dk.openbanking.merkurandelskasse.MerkurAndelskasseConstants.Urls;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bec.BecAgent;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class MerkurAndelskasseAgent extends BecAgent {
+
+    public MerkurAndelskasseAgent(CredentialsRequest request,
+        AgentContext context,
+        SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected String getBaseUrl() {
+        return Urls.BASE_URL;
+    }
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/merkurandelskasse/MerkurAndelskasseConstants.java
@@ -0,0 +1,8 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.merkurandelskasse;
+
+public final class MerkurAndelskasseConstants {
+    public static class Urls {
+        public static final String BASE_URL = "https://psd2api87.prod.bec.dk/eidas/1.0/v1";
+    }
+
+}
