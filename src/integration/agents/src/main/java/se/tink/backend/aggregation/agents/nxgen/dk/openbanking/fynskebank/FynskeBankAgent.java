6dedf09b466ce272d566bf006ae4d79245e6c5e0
John Helbrink <john.helbrink@tink.se>
2019-07-24 16:41:52 +0200

    feat(bec): Added Fynske Bank

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/fynskebank/FynskeBankAgent.java
@@ -0,0 +1,22 @@
+package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.fynskebank;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.dk.openbanking.fynskebank.FynskeBankConstants.Urls;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bec.BecAgent;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class FynskeBankAgent extends BecAgent {
+
+    public FynskeBankAgent(CredentialsRequest request,
+        AgentContext context,
+        SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected String getBaseUrl() {
+        return Urls.BASE_URL;
+    }
+
+}
