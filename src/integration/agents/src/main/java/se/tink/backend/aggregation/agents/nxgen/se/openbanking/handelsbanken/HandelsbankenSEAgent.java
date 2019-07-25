d59893c89493665049eccfaf9236a66a8ae30d0f
Olivia Stigeborn <olivia.stigeborn@tink.se>
2019-07-25 15:13:51 +0200

    feat(Handelsbanken SE): SE specific

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/handelsbanken/HandelsbankenSEAgent.java
@@ -0,0 +1,51 @@
+package se.tink.backend.aggregation.agents.nxgen.se.openbanking.handelsbanken;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAccountConverter;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseConstants;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.authenticator.HandelsbankenBankidAuthenticator;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.bankid.BankIdAuthenticationController;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public final class HandelsbankenSEAgent extends HandelsbankenBaseAgent {
+
+    private final HandelsbankenAccountConverter accountConverter;
+
+    public HandelsbankenSEAgent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+        this.accountConverter = new HandelsbankenAccountConverter();
+
+        transactionalAccountRefreshController = getTransactionalAccountRefreshController();
+    }
+
+    @Override
+    protected HandelsbankenBaseAccountConverter getAccountConverter() {
+        return accountConverter;
+    }
+
+    @Override
+    protected Date setMaxPeriodTransactions() {
+        Date date = new Date();
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.MONTH, - HandelsbankenSEConstants.MAX_FETCH_PERIOD_MONTHS);
+        sessionStorage.put(HandelsbankenBaseConstants.StorageKeys.MAX_FETCH_PERIOD_MONTHS, date);
+
+        return date;
+    }
+
+    @Override
+    protected Authenticator constructAuthenticator() {
+
+        return new BankIdAuthenticationController<>(
+                supplementalRequester,
+                new HandelsbankenBankidAuthenticator(apiClient, sessionStorage),
+                persistentStorage);
+    }
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/handelsbanken/HandelsbankenSEConstants.java
@@ -0,0 +1,6 @@
+package se.tink.backend.aggregation.agents.nxgen.se.openbanking.handelsbanken;
+
+public class HandelsbankenSEConstants {
+
+    public static final int MAX_FETCH_PERIOD_MONTHS = 15;
+}
