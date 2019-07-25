0dfe02ba83424bda842791526d5f13c921a36f9d
Stefanus Louca <stefanus.louca@tink.se>
2019-07-25 17:16:03 +0200

    feat(cirkle k se): branded cards seb

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/sebcard/circlek/CircleKAgent.java
@@ -0,0 +1,19 @@
+package se.tink.backend.aggregation.agents.nxgen.se.openbanking.sebcard.circlek;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbrandedcards.SebBrandedCardsAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbrandedcards.SebBrandedCardsConstants;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class CircleKAgent extends SebBrandedCardsAgent {
+
+    public CircleKAgent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(
+                request,
+                context,
+                signatureKeyPair,
+                SebBrandedCardsConstants.BrandedCardsSweden.CIRCLE_K);
+    }
+}
