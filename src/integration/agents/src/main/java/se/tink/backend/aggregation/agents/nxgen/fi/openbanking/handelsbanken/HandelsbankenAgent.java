14c7763afe11704fe9f46987c200dc7975ea9fdb
Olivia Stigeborn <olivia.stigeborn@tink.se>
2019-07-25 15:13:03 +0200

    feat(Handelsbanken NL/FI/GB): Unimplemented markets

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/handelsbanken/HandelsbankenAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/handelsbanken/HandelsbankenAgent.java
@@ -3,8 +3,13 @@ package se.tink.backend.aggregation.agents.nxgen.fi.openbanking.handelsbanken;
 import se.tink.backend.aggregation.agents.AgentContext;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAccountConverter;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseConstants;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.libraries.credentials.service.CredentialsRequest;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public final class HandelsbankenAgent extends HandelsbankenBaseAgent {
     private final HandelsbankenAccountConverter accountConverter;
@@ -13,6 +18,23 @@ public final class HandelsbankenAgent extends HandelsbankenBaseAgent {
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
         super(request, context, signatureKeyPair);
         accountConverter = new HandelsbankenAccountConverter();
+
+        transactionalAccountRefreshController = getTransactionalAccountRefreshController();
+    }
+
+    @Override
+    protected Authenticator constructAuthenticator() {
+        throw new IllegalStateException("Authenticator missing");
+    }
+
+    @Override
+    protected Date setMaxPeriodTransactions() {
+        Date date = new Date();
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.MONTH, - HandelsbankenFIConstants.MAX_FETCH_PERIOD_MONTHS);
+        sessionStorage.put(HandelsbankenBaseConstants.StorageKeys.MAX_FETCH_PERIOD_MONTHS, date);
+
+        return date;
     }
 
     @Override
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/gb/openbanking/handelsbanken/HandelsbankenGBConstants.java
@@ -0,0 +1,6 @@
+package se.tink.backend.aggregation.agents.nxgen.gb.openbanking.handelsbanken;
+
+public class HandelsbankenGBConstants {
+
+    public static final int MAX_FETCH_PERIOD_MONTHS = 13;
+}
