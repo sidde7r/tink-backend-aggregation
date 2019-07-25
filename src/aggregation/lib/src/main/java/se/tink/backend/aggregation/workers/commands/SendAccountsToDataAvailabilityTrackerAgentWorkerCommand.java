196ddecb582abe8dd4ab0acc0ab840ab6d5460b0
Gustaf Svanerud <gustaf.svanerud@tink.se>
2019-07-25 14:48:23 +0200

    feat(agent-dat): limit tracking to a percent of market

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/SendAccountsToDataAvailabilityTrackerAgentWorkerCommand.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/SendAccountsToDataAvailabilityTrackerAgentWorkerCommand.java
@@ -52,7 +52,8 @@ public class SendAccountsToDataAvailabilityTrackerAgentWorkerCommand extends Age
         boolean forceMockClient = !TEST_MARKET.equalsIgnoreCase(request.getProvider().getMarket());
 
         this.agentDataAvailabilityTrackerClient =
-                AgentDataAvailabilityTrackerClientFactory.getClient(configuration, forceMockClient);
+                AgentDataAvailabilityTrackerClientFactory.getInstance()
+                        .getClient(configuration, forceMockClient);
     }
 
     @Override
--- src/integration/agent_data_availability_tracker/client/src/main/java/se/tink/backend/integration/agent_data_availability_tracker/client/AgentDataAvailabilityTrackerClientFactory.java
+++ src/integration/agent_data_availability_tracker/client/src/main/java/se/tink/backend/integration/agent_data_availability_tracker/client/AgentDataAvailabilityTrackerClientFactory.java
@@ -1,6 +1,7 @@
 package se.tink.backend.integration.agent_data_availability_tracker.client;
 
 import com.google.common.base.Strings;
+import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +9,25 @@ public class AgentDataAvailabilityTrackerClientFactory {
     private static final Logger log =
             LoggerFactory.getLogger(AgentDataAvailabilityTrackerClientFactory.class);
 
-    public static AgentDataAvailabilityTrackerClient getClient(
+    private static final float TRACKING_FRACTION = 0.05f; // 5% of requests
+    private static AgentDataAvailabilityTrackerClientFactory instance;
+
+    private final Random random;
+
+    private AgentDataAvailabilityTrackerClientFactory() {
+        random = new Random();
+    }
+
+    public static AgentDataAvailabilityTrackerClientFactory getInstance() {
+
+        if (instance == null) {
+            instance = new AgentDataAvailabilityTrackerClientFactory();
+        }
+
+        return instance;
+    }
+
+    public AgentDataAvailabilityTrackerClient getClient(
             final AgentDataAvailabilityTrackerConfiguration configuration,
             final boolean forceMockClient) {
         if (configuration != null && !Strings.isNullOrEmpty(configuration.getHost())) {
@@ -16,7 +35,7 @@ public class AgentDataAvailabilityTrackerClientFactory {
             /*
              *  Temporary limitation to prevent client running on all providers.
              */
-            if (!forceMockClient) {
+            if (!forceMockClient && shouldTrackData()) {
                 try {
                     return new AgentDataAvailabilityTrackerClientImpl(
                             configuration.getHost(), configuration.getPort());
@@ -29,5 +48,9 @@ public class AgentDataAvailabilityTrackerClientFactory {
 
         // Default to noop mock client
         return new AgentDataAvailabilityTrackerMockClientImpl();
+    }
+
+    private boolean shouldTrackData() {
+        return random.nextFloat() < TRACKING_FRACTION;
     }
 }
