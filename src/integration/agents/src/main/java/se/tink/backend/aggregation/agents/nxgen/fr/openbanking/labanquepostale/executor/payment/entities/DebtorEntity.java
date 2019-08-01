9c65b55230b1f3b5464bacee924136974c558676
jovanmanojlovic42 <j.manojlovic@vegaitsourcing.rs>
2019-08-01 14:35:38 +0200

    fix(LaBanquePostal): Added JsonCreator

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/labanquepostale/executor/payment/entities/DebtorEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/labanquepostale/executor/payment/entities/DebtorEntity.java
@@ -1,5 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.labanquepostale.executor.payment.entities;
 
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonIgnore;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.controllers.payment.PaymentRequest;
 
@@ -9,42 +11,20 @@ public class DebtorEntity {
     private PostalAddressEntity postalAddress;
     private IdEntity privateId;
 
+    @JsonIgnore
     public static DebtorEntity of(PaymentRequest paymentRequest) {
-        return new DebtorEntity.Builder()
-                .withName(paymentRequest.getPayment().getCreditor().getName())
-                .build();
+        return new DebtorEntity(paymentRequest.getPayment().getCreditor().getName());
     }
 
-    private DebtorEntity(Builder builder) {
-        this.name = builder.name;
-        this.postalAddress = builder.postalAddress;
-        this.privateId = builder.privateId;
+    @JsonCreator
+    public DebtorEntity(String name, PostalAddressEntity postalAddress, IdEntity privateId) {
+        this.name = name;
+        this.postalAddress = postalAddress;
+        this.privateId = privateId;
     }
 
-    public DebtorEntity() {}
-
-    public static class Builder {
-        private String name;
-        private PostalAddressEntity postalAddress;
-        private IdEntity privateId;
-
-        public Builder withPostalAddress(PostalAddressEntity postalAddress) {
-            this.postalAddress = postalAddress;
-            return this;
-        }
-
-        public Builder withName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Builder withPrivateId(IdEntity privateId) {
-            this.privateId = privateId;
-            return this;
-        }
-
-        public DebtorEntity build() {
-            return new DebtorEntity(this);
-        }
+    @JsonIgnore
+    public DebtorEntity(String name) {
+        this.name = name;
     }
 }
