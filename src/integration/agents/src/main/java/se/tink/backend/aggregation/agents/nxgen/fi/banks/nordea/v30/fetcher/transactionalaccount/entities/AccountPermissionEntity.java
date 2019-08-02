932a5a2dcf61908c1b7c3e174fec8a2d12edf1bd
Mauritz Zachrisson <mauritz.zachrisson@tink.se>
2019-08-02 16:17:28 +0200

    refactor(agents): Remove unused, dead code agents

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/nordea/v30/fetcher/transactionalaccount/entities/AccountPermissionEntity.java
+++
@@ -1,6 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.fi.banks.nordea.v30.fetcher.transactionalaccount.entities;
-
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class AccountPermissionEntity {}
