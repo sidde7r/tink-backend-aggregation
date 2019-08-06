b66c6ca58c5b1b23eaad93fe6e687a6f5c946d6e
tzietek <tymon.zietek@tink.se>
2019-08-06 11:21:18 +0200

    style(CbiGlobe): bazel formatting

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/authenticator/rpc/ConsentStatus.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/authenticator/rpc/ConsentStatus.java
@@ -1,18 +1,17 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.authenticator.rpc;
 
 public enum ConsentStatus {
-  received,
-  rejected,
-  valid,
-  revokedByPsu,
-  expired,
-  terminatedByTpp,
-  replaced,
-  invalidated,
-  pendingExpired;
+    received,
+    rejected,
+    valid,
+    revokedByPsu,
+    expired,
+    terminatedByTpp,
+    replaced,
+    invalidated,
+    pendingExpired;
 
-  public boolean isAcceptedStatus() {
-    return this == received || this == valid || this == revokedByPsu || this == replaced;
-  }
-
+    public boolean isAcceptedStatus() {
+        return this == received || this == valid || this == revokedByPsu || this == replaced;
+    }
 }
