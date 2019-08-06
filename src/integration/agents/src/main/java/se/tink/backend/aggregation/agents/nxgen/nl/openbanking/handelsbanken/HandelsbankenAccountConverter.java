9881e163ff1dc6bb398e69e046d10e88274e4056
Mauritz Zachrisson <mauritz.zachrisson@tink.se>
2019-08-06 13:59:38 +0200

    fix(handelsbanken): Simplify account parsing, add product name fallback

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/handelsbanken/HandelsbankenAccountConverter.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/handelsbanken/HandelsbankenAccountConverter.java
@@ -1,24 +1,25 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.openbanking.handelsbanken;
 
 import java.util.Optional;
-import se.tink.backend.agents.rpc.AccountTypes;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAccountConverter;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.fetcher.transactionalaccount.entity.AccountsItemEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.fetcher.transactionalaccount.entity.BalancesItemEntity;
 import se.tink.backend.aggregation.nxgen.core.account.TypeMapper;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccountType;
 
 public class HandelsbankenAccountConverter implements HandelsbankenBaseAccountConverter {
 
-    private final TypeMapper<AccountTypes> accountTypes =
-            TypeMapper.<AccountTypes>builder().put(AccountTypes.CHECKING, "Betaalrekening").build();
+    private final TypeMapper<TransactionalAccountType> accountTypes =
+            TypeMapper.<TransactionalAccountType>builder()
+                    .put(TransactionalAccountType.CHECKING, "Betaalrekening")
+                    .build();
 
     @Override
     public Optional<TransactionalAccount> toTinkAccount(
             AccountsItemEntity accountEntity, BalancesItemEntity balance) {
         return accountTypes
                 .translate(accountEntity.getAccountType())
-                .filter(AccountTypes.CHECKING::equals)
-                .map(account -> accountEntity.createCheckingAccount(balance));
+                .map(type -> accountEntity.toTinkAccount(type, balance));
     }
 }
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/handelsbanken/HandelsbankenAccountConverter.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/handelsbanken/HandelsbankenAccountConverter.java
@@ -1,19 +1,19 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.handelsbanken;
 
 import java.util.Optional;
-import se.tink.backend.agents.rpc.AccountTypes;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAccountConverter;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.fetcher.transactionalaccount.entity.AccountsItemEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.fetcher.transactionalaccount.entity.BalancesItemEntity;
 import se.tink.backend.aggregation.nxgen.core.account.TypeMapper;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccountType;
 
 public class HandelsbankenAccountConverter implements HandelsbankenBaseAccountConverter {
 
-    private final TypeMapper<AccountTypes> accountTypes =
-            TypeMapper.<AccountTypes>builder()
+    private final TypeMapper<TransactionalAccountType> accountTypes =
+            TypeMapper.<TransactionalAccountType>builder()
                     .put(
-                            AccountTypes.CHECKING,
+                            TransactionalAccountType.CHECKING,
                             "Allkonto Ung",
                             "Allkonto",
                             "Checkkonto",
@@ -25,7 +25,6 @@ public class HandelsbankenAccountConverter implements HandelsbankenBaseAccountCo
             AccountsItemEntity accountEntity, BalancesItemEntity balance) {
         return accountTypes
                 .translate(accountEntity.getAccountType())
-                .filter(AccountTypes.CHECKING::equals)
-                .map(account -> accountEntity.createCheckingAccount(balance));
+                .map(type -> accountEntity.toTinkAccount(type, balance));
     }
 }
