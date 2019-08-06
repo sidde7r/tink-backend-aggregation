54fa329bcc5b20ef41b1ffa4158adeb843c7aa64
tzietek <tymon.zietek@tink.se>
2019-08-06 10:54:18 +0200

    feat(CbiGlobe): saves accounts in persistent storage

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/isp/fetcher/transactionalaccount/IspTransactionalAccountFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/isp/fetcher/transactionalaccount/IspTransactionalAccountFetcher.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeConstants.QueryValues;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeConstants.StorageKeys;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.authenticator.CbiGlobeAuthenticationController;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.fetcher.transactionalaccount.rpc.GetAccountsResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
@@ -14,6 +15,7 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.paginat
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginator;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
+import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class IspTransactionalAccountFetcher
         implements AccountFetcher<TransactionalAccount>,
@@ -42,11 +44,23 @@ public class IspTransactionalAccountFetcher
 
     @Override
     public Collection<TransactionalAccount> fetchAccounts() {
-        GetAccountsResponse getAccountsResponse = apiClient.getAccounts();
-        controller.openThirdPartyApp(getAccountsResponse);
+        GetAccountsResponse getAccountsResponse =
+                SerializationUtils.deserializeFromString(
+                        persistentStorage.get(StorageKeys.ACCOUNTS), GetAccountsResponse.class);
+
+        if (getAccountsResponse == null) {
+            getAccountsResponse = getAccounts();
+            persistentStorage.put(StorageKeys.ACCOUNTS, getAccountsResponse);
+        }
 
         return getAccountsResponse.getAccounts().stream()
                 .map(acc -> acc.toTinkAccount(apiClient.getBalances(acc.getResourceId())))
                 .collect(Collectors.toList());
+    }
+
+    private GetAccountsResponse getAccounts() {
+        GetAccountsResponse getAccountsResponse = apiClient.getAccounts();
+        controller.openThirdPartyApp(getAccountsResponse);
+        return getAccountsResponse;
     }
 }
