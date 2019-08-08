cec2d8be628773b4b00728d36675275d495d9e5a
Luka Klacar <l.klacar@vegaitsourcing.rs>
2019-08-08 13:53:51 +0200

    fix(CMCIC): Removed builder

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cmcic/authenticator/entity/ClientCredentialsTokenRequest.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cmcic/authenticator/entity/ClientCredentialsTokenRequest.java
@@ -5,41 +5,9 @@ import se.tink.backend.aggregation.nxgen.http.AbstractForm;
 
 public class ClientCredentialsTokenRequest extends AbstractForm {
 
-    private ClientCredentialsTokenRequest(String clientId, String grantType, String scope) {
+    public ClientCredentialsTokenRequest(String clientId, String grantType, String scope) {
         put(FormKeys.CLIENT_ID, clientId);
         put(FormKeys.GRANT_TYPE, grantType);
         put(FormKeys.SCOPE, scope);
-    }
-
-    public static ClientCredentialsTokenRequestBuilder builder() {
-        return new ClientCredentialsTokenRequestBuilder();
-    }
-
-    public static class ClientCredentialsTokenRequestBuilder {
-
-        private String clientId;
-        private String grantType;
-        private String scope;
-
-        ClientCredentialsTokenRequestBuilder() {}
-
-        public ClientCredentialsTokenRequestBuilder clientId(String clientId) {
-            this.clientId = clientId;
-            return this;
-        }
-
-        public ClientCredentialsTokenRequestBuilder grantType(String grantType) {
-            this.grantType = grantType;
-            return this;
-        }
-
-        public ClientCredentialsTokenRequestBuilder scope(String scope) {
-            this.scope = scope;
-            return this;
-        }
-
-        public ClientCredentialsTokenRequest build() {
-            return new ClientCredentialsTokenRequest(clientId, grantType, scope);
-        }
     }
 }
