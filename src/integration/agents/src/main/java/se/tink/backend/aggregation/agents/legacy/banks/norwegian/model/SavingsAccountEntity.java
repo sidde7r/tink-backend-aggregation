1facbdb5581db5b1de61211074099859ac47ff5d
Annica <annica.alstermark@tink.se>
2019-08-09 15:30:37 +0200

    fix(Norwegian): Parse json response for savings account
    Also removed the test and utils for savings account parsing as it's no longer HTML. I left the account construction as is, since changing that would require a much bigger refactoring.

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/norwegian/model/SavingsAccountEntity.java
@@ -0,0 +1,34 @@
+package se.tink.backend.aggregation.agents.banks.norwegian.model;
+
+import net.minidev.json.annotate.JsonIgnore;
+import se.tink.backend.agents.rpc.Account;
+import se.tink.backend.agents.rpc.AccountTypes;
+import se.tink.backend.aggregation.annotations.JsonObject;
+
+@JsonObject
+public class SavingsAccountEntity {
+    private String accountNumber;
+    private String ocrNumber;
+    private double amountAvailable;
+    private double balance;
+    private double interestAccumulated;
+    private String payToAccountNumber;
+    private String nickName;
+    private int region;
+    private String danishOcrGiroNummer;
+    private boolean showNickName;
+    private boolean isPreferredPaymentAccountNumber;
+
+    @JsonIgnore
+    public Account toTinkAccount() {
+
+        Account account = new Account();
+        account.setType(AccountTypes.SAVINGS);
+        account.setBankId("NORWEGIAN_SAVINGS_ACCOUNT");
+        account.setAccountNumber(accountNumber);
+        account.setBalance(balance);
+        account.setName("Norwegian Sparkonto");
+
+        return account;
+    }
+}
