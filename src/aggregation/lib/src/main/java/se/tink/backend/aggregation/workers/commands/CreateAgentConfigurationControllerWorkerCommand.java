7b6d04ea4d3d674cce3816031b6604a0212ac501
Agusti Tomas <agusti.tomas@tink.se>
2019-08-09 10:08:33 +0200

    chore(eidas secrets): Remove unused variable

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/CreateAgentConfigurationControllerWorkerCommand.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/CreateAgentConfigurationControllerWorkerCommand.java
@@ -17,18 +17,12 @@ public class CreateAgentConfigurationControllerWorkerCommand extends AgentWorker
     private final AgentsServiceConfiguration agentsServiceConfiguration;
     private final CredentialsRequest credentialsRequest;
     private final AgentWorkerCommandContext agentWorkerCommandContext;
-    private final boolean tppSecretsServiceEnabled;
 
     public CreateAgentConfigurationControllerWorkerCommand(
             AgentWorkerCommandContext agentWorkerCommandContext) {
         this.agentsServiceConfiguration = agentWorkerCommandContext.getAgentsServiceConfiguration();
         this.credentialsRequest = agentWorkerCommandContext.getRequest();
         this.agentWorkerCommandContext = agentWorkerCommandContext;
-        this.tppSecretsServiceEnabled =
-                agentWorkerCommandContext
-                        .getAgentsServiceConfiguration()
-                        .getTppSecretsServiceConfiguration()
-                        .isEnabled();
     }
 
     @Override
