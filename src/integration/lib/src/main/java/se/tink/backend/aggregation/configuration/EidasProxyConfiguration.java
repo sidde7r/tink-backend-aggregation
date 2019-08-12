4f40e1ca75c2e11a808744d2a4a2007277d2cd8a
Richard Stephens <richard.stephens@tink.se>
2019-08-12 17:59:21 +0200

    refactor(eidas): pull eidas configuration methods out of tinkhttpclient

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/configuration/EidasProxyConfiguration.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/configuration/EidasProxyConfiguration.java
@@ -1,6 +1,7 @@
 package se.tink.backend.aggregation.configuration;
 
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.configuration.eidas.InternalEidasProxyConfiguration;
 
 @JsonObject
 public class EidasProxyConfiguration {
@@ -37,5 +38,10 @@ public class EidasProxyConfiguration {
 
     public boolean isLocalEidasDev() {
         return localEidasDev;
+    }
+
+    public InternalEidasProxyConfiguration toInternalConfig() {
+        return new InternalEidasProxyConfiguration(
+                host, caPath, tlsCrtPath, tlsKeyPath, localEidasDev);
     }
 }
