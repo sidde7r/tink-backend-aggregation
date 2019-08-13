8dc756b1673ba2067742c4da0a6d5d5c5a9fa061
Alexander Jamm <alexander.jamm@tink.se>
2019-08-13 13:04:09 +0200

    fix(HvidbjergBank): use BecAgent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/hvidbjergbank/HvidbjergBankConstants.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.hvidbjergbank;
-
-public final class HvidbjergBankConstants {
-
-    public static class Urls {
-        public static final String BASE_URL = "https://psd2api25.prod.bec.dk/eidas/1.0/v1";
-    }
-}
