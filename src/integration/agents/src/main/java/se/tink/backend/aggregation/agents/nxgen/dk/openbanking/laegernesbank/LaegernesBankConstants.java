ba04073a53337df4a50a69273694754ce9644f2c
Alexander Jamm <alexander.jamm@tink.se>
2019-08-13 13:07:25 +0200

    fix(LaegernesBank): use BecAgent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/laegernesbank/LaegernesBankConstants.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.laegernesbank;
-
-public final class LaegernesBankConstants {
-    public static class Urls {
-        public static final String BASE_URL = "https://psd2api30.prod.bec.dk/eidas/1.0/v1";
-    }
-}
