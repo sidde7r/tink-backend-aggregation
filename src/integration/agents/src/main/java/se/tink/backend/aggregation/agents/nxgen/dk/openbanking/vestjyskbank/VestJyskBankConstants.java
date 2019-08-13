a7165744e64864105d497fa036d4aa188ff362c2
Alexander Jamm <alexander.jamm@tink.se>
2019-08-13 13:22:08 +0200

    fix(VestJyskBank): use BecAgent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/vestjyskbank/VestJyskBankConstants.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.vestjyskbank;
-
-public final class VestJyskBankConstants {
-
-    public static class Urls {
-        public static final String BASE_URL = "https://psd2api19.prod.bec.dk/eidas/1.0/v1";
-    }
-}
