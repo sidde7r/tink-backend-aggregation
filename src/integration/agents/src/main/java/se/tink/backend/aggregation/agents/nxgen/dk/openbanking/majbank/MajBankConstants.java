111c2c4257d3a46399a071cbc5d5ceaf9709931b
Alexander Jamm <alexander.jamm@tink.se>
2019-08-13 13:09:51 +0200

    fix(MajBank): use BecAgent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/majbank/MajBankConstants.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.majbank;
-
-public final class MajBankConstants {
-    public static class Urls {
-        public static final String BASE_URL = "https://psd2api831.prod.bec.dk/eidas/1.0/v1";
-    }
-}
