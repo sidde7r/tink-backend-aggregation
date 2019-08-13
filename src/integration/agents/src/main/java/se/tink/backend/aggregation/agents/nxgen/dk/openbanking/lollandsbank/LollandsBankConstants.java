049b8855f15fa77e6323afeffe0655cf349b2066
Alexander Jamm <alexander.jamm@tink.se>
2019-08-13 13:08:45 +0200

    fix(LollandsBank): use BecAgent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/openbanking/lollandsbank/LollandsBankConstants.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.dk.openbanking.lollandsbank;
-
-public final class LollandsBankConstants {
-    public static class Urls {
-        public static final String BASE_URL = "https://psd2api95.prod.bec.dk/eidas/1.0/v1";
-    }
-}
