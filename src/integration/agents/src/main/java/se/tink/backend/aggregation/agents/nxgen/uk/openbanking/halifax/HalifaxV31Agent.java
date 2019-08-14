3d65f304581282fa02bba903b9db47aee0ed1506
Girish Sharma <girish.sharma@tink.se>
2019-08-14 13:52:45 +0200

    feat(UK/Halifax_PROD): Halifax app2app support

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/halifax/HalifaxV31Agent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/halifax/HalifaxV31Agent.java
@@ -23,17 +23,19 @@ public class HalifaxV31Agent extends UkOpenBankingBaseAgent {
 
     private final UkOpenBankingAisConfig aisConfig;
     private final UkOpenBankingPisConfig pisConfig;
+    private final URL appToAppAuthUrl;
 
     public HalifaxV31Agent(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
         super(request, context, signatureKeyPair, new URL(V31.WELL_KNOWN_URL));
         aisConfig = new UkOpenBankingV31AisConfiguration(V31.AIS_API_URL, V31.AIS_AUTH_URL);
         pisConfig = new UkOpenBankingV31PisConfiguration(V31.PIS_API_URL, V31.PIS_AUTH_URL);
+        appToAppAuthUrl = new URL(V31.APP_TO_APP_AUTH_URL);
     }
 
     @Override
     protected Authenticator constructAuthenticator() {
-        return super.constructAuthenticator(aisConfig);
+        return super.constructAuthenticator(aisConfig, appToAppAuthUrl);
     }
 
     @Override
@@ -58,7 +60,8 @@ public class HalifaxV31Agent extends UkOpenBankingBaseAgent {
                         new URL(Urls.V31.WELL_KNOWN_URL),
                         supplementalInformationHelper,
                         credentials,
-                        strongAuthenticationState);
+                        strongAuthenticationState,
+                        appToAppAuthUrl);
         return Optional.of(new PaymentController(paymentExecutor, paymentExecutor));
     }
 }
