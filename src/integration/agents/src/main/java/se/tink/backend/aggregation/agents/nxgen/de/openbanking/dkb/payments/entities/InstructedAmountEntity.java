be1c8817b2ba626720de319e55b31792f4b1261d
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2019-08-15 11:51:07 +0200

    fix: Remove rogue Minidev dependencies

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/dkb/payments/entities/InstructedAmountEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/dkb/payments/entities/InstructedAmountEntity.java
@@ -1,6 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.de.openbanking.dkb.payments.entities;
 
-import net.minidev.json.annotate.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.controllers.payment.PaymentRequest;
 import se.tink.libraries.amount.Amount;
