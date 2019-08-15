8bc94146174b61571ce4565285b6291ae1361bdc
Emil Tullstedt <emil.tullstedt@tink.se>
2019-08-15 10:47:29 +0200

    feat(SIBS): adding cofidis agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/cofidis/CofidisAgent.java
@@ -0,0 +1,19 @@
+package se.tink.backend.aggregation.agents.nxgen.pt.openbanking.cofidis;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsBaseAgent;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class CofidisAgent extends SibsBaseAgent {
+
+    public CofidisAgent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected String getIntegrationName() {
+        return CofidisConstants.INTEGRATION_NAME;
+    }
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/cofidis/CofidisConstants.java
@@ -0,0 +1,6 @@
+package se.tink.backend.aggregation.agents.nxgen.pt.openbanking.cofidis;
+
+public class CofidisConstants {
+
+    public static final String INTEGRATION_NAME = "cofidis";
+}
