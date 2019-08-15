383730c5da692189e05ab3fbc618dfeb9a3377f1
Girish Sharma <girish.sharma@tink.se>
2019-08-15 16:21:15 +0200

    feat(UK/FirstTrust_PROD): Adding FirstTrust Bank agent

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/FirstTrustAgentTest.java
@@ -0,0 +1,18 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+
+@Ignore
+public class FirstTrustAgentTest {
+    @Test
+    public void test() throws Exception {
+        new AgentIntegrationTest.Builder("uk", "uk-firsttrust-oauth2")
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(true)
+                .expectLoggedIn(false)
+                .build()
+                .testRefresh();
+    }
+}
