d6b477d1223df42d27217a4919c34d6ed3d7d8b6
Sebastian Mandrean <sebastian.mandrean@gmail.com>
2019-08-15 17:43:44 +0200

    refactor(Sparebank): Break out maybeGetRedirectUri

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/authenticator/SparebankAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/authenticator/SparebankAuthenticator.java
@@ -19,18 +19,16 @@ public class SparebankAuthenticator {
     public URL buildAuthorizeUrl(String state) {
         return Try.of(() -> apiClient.getScaRedirect(state))
                 .map(ScaResponse::getRedirectUri)
-                .recoverWith(
-                        HttpResponseException.class,
-                        e ->
-                                e.getResponse().getBody(String.class).contains("scaRedirect")
-                                        ? Try.success(
-                                                e.getResponse()
-                                                        .getBody(ScaResponse.class)
-                                                        .getRedirectUri())
-                                        : Try.failure(e))
+                .recoverWith(HttpResponseException.class, this::maybeGetRedirectUri)
                 .filter(not(Strings::isNullOrEmpty))
                 .map(URL::new)
                 .getOrElseThrow(() -> new IllegalStateException("SCA redirect missing"));
+    }
+
+    private Try<String> maybeGetRedirectUri(HttpResponseException e) {
+        return e.getResponse().getBody(String.class).contains("scaRedirect")
+                ? Try.success(e.getResponse().getBody(ScaResponse.class).getRedirectUri())
+                : Try.failure(e);
     }
 
     public void setUpPsuAndSession(String psuId, String tppSessionId) {
