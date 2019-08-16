508f871b9d4576564da70a9fae60b5a7945c133f
Gustav Westling <gustav.westling@tink.se>
2019-08-16 13:02:42 +0200

    feat(aws): add source of aws_java_sdk_core

---
+++ third_party/aws_java_sdk_core/src/main/java/com/amazonaws/http/apache/utils/ApacheUtils.java
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2016. Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.amazonaws.http.apache.utils;
+
+import com.amazonaws.Request;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.http.HttpResponse;
+import com.amazonaws.http.settings.HttpClientSettings;
+import com.amazonaws.util.FakeIOException;
+import tink.org.apache.http.Header;
+import tink.org.apache.http.HttpEntity;
+import tink.org.apache.http.HttpHost;
+import tink.org.apache.http.HttpStatus;
+import tink.org.apache.http.auth.AuthScope;
+import tink.org.apache.http.auth.Credentials;
+import tink.org.apache.http.auth.NTCredentials;
+import tink.org.apache.http.client.AuthCache;
+import tink.org.apache.http.client.CredentialsProvider;
+import tink.org.apache.http.client.methods.HttpRequestBase;
+import tink.org.apache.http.client.protocol.HttpClientContext;
+import tink.org.apache.http.entity.BufferedHttpEntity;
+import tink.org.apache.http.entity.StringEntity;
+import tink.org.apache.http.impl.auth.BasicScheme;
+import tink.org.apache.http.impl.client.BasicAuthCache;
+import tink.org.apache.http.impl.client.BasicCredentialsProvider;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Map;
+import tink.org.apache.http.protocol.HttpContext;
+
+public class ApacheUtils {
+
+    /**
+     * Checks if the request was successful or not based on the status code.
+     *
+     * @param response HTTP response
+     * @return True if the request was successful (i.e. has a 2xx status code), false otherwise.
+     */
+    public static boolean isRequestSuccessful(tink.org.apache.http.HttpResponse response) {
+        int status = response.getStatusLine().getStatusCode();
+        return status / 100 == HttpStatus.SC_OK / 100;
+    }
+
+    /**
+     * Creates and initializes an HttpResponse object suitable to be passed to an HTTP response
+     * handler object.
+     *
+     * @param request Marshalled request object.
+     * @param method  The HTTP method that was invoked to get the response.
+     * @param context The HTTP context associated with the request and response.
+     * @return The new, initialized HttpResponse object ready to be passed to an HTTP response
+     * handler object.
+     * @throws IOException If there were any problems getting any response information from the
+     *                     HttpClient method object.
+     */
+    public static HttpResponse createResponse(Request<?> request,
+                                        HttpRequestBase method,
+                                        tink.org.apache.http.HttpResponse apacheHttpResponse,
+                                        HttpContext context) throws IOException {
+        HttpResponse httpResponse = new HttpResponse(request, method, context);
+
+        if (apacheHttpResponse.getEntity() != null) {
+            httpResponse.setContent(apacheHttpResponse.getEntity().getContent());
+        }
+
+        httpResponse.setStatusCode(apacheHttpResponse.getStatusLine().getStatusCode());
+        httpResponse.setStatusText(apacheHttpResponse.getStatusLine().getReasonPhrase());
+        for (Header header : apacheHttpResponse.getAllHeaders()) {
+            httpResponse.addHeader(header.getName(), header.getValue());
+        }
+
+        return httpResponse;
+    }
+
+    /**
+     * Utility function for creating a new StringEntity and wrapping any errors
+     * as a SdkClientException.
+     *
+     * @param s The string contents of the returned HTTP entity.
+     * @return A new StringEntity with the specified contents.
+     */
+    public static HttpEntity newStringEntity(String s) {
+        try {
+            return new StringEntity(s);
+        } catch (UnsupportedEncodingException e) {
+            throw new SdkClientException("Unable to create HTTP entity: " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Utility function for creating a new BufferedEntity and wrapping any errors
+     * as a SdkClientException.
+     *
+     * @param entity The HTTP entity to wrap with a buffered HTTP entity.
+     * @return A new BufferedHttpEntity wrapping the specified entity.
+     * @throws FakeIOException only for test simulation
+     */
+    public static HttpEntity newBufferedHttpEntity(HttpEntity entity) throws
+            FakeIOException {
+        try {
+            return new BufferedHttpEntity(entity);
+        } catch (FakeIOException e) {
+            // Only for test simulation.
+            throw e;
+        } catch (IOException e) {
+            throw new SdkClientException("Unable to create HTTP entity: " + e.getMessage(), e);
+        }
+    }
+
+    /**
+     * Returns a new HttpClientContext used for request execution.
+     */
+    public static HttpClientContext newClientContext(HttpClientSettings settings,
+                                                     Map<String, ? extends Object>
+                                                             attributes) {
+        final HttpClientContext clientContext = new HttpClientContext();
+
+        if (attributes != null && !attributes.isEmpty()) {
+            for (Map.Entry<String, ?> entry : attributes.entrySet()) {
+                clientContext.setAttribute(entry.getKey(), entry.getValue());
+            }
+        }
+
+        addPreemptiveAuthenticationProxy(clientContext, settings);
+
+        clientContext.setAttribute(HttpContextUtils.DISABLE_SOCKET_PROXY_PROPERTY, settings.disableSocketProxy());
+        return clientContext;
+
+    }
+
+    /**
+     * Returns a new Credentials Provider for use with proxy authentication.
+     */
+    public static CredentialsProvider newProxyCredentialsProvider
+    (HttpClientSettings settings) {
+        final CredentialsProvider provider = new BasicCredentialsProvider();
+        provider.setCredentials(newAuthScope(settings), newNTCredentials(settings));
+        return provider;
+    }
+
+    /**
+     * Returns a new instance of NTCredentials used for proxy authentication.
+     */
+    private static Credentials newNTCredentials(HttpClientSettings settings) {
+        return new NTCredentials(settings.getProxyUsername(),
+                settings.getProxyPassword(),
+                settings.getProxyWorkstation(),
+                settings.getProxyDomain());
+    }
+
+    /**
+     * Returns a new instance of AuthScope used for proxy authentication.
+     */
+    private static AuthScope newAuthScope(HttpClientSettings settings) {
+        return new AuthScope(settings.getProxyHost(), settings.getProxyPort());
+    }
+
+    private static void addPreemptiveAuthenticationProxy(HttpClientContext clientContext,
+                                                         HttpClientSettings settings) {
+
+        if (settings.isPreemptiveBasicProxyAuth()) {
+            HttpHost targetHost = new HttpHost(settings.getProxyHost(), settings
+                    .getProxyPort());
+            final CredentialsProvider credsProvider = newProxyCredentialsProvider(settings);
+            // Create AuthCache instance
+            AuthCache authCache = new BasicAuthCache();
+            // Generate BASIC scheme object and add it to the local auth cache
+            BasicScheme basicAuth = new BasicScheme();
+            authCache.put(targetHost, basicAuth);
+
+            clientContext.setCredentialsProvider(credsProvider);
+            clientContext.setAuthCache(authCache);
+        }
+    }
+}
---
+++ third_party/aws_java_sdk_core/src/test/java/com/amazonaws/auth/InstanceProfileCredentialsProviderIntegrationTest.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright 2010-2018 Amazon.com, Inc. or its affiliates. All Rights
+ * Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is
+ * distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either
+ * express or implied. See the License for the specific language
+ * governing
+ * permissions and limitations under the License.
+ */
+package com.amazonaws.auth;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
+
+import com.amazonaws.SDKGlobalConfiguration;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.util.LogCaptor;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.amazonaws.AmazonClientException;
+
+/**
+ * Unit tests for the InstanceProfileCredentialsProvider.
+ */
+public class InstanceProfileCredentialsProviderIntegrationTest extends LogCaptor.LogCaptorTestBase {
+
+    private EC2MetadataServiceMock mockServer;
+
+    /** Starts up the mock EC2 Instance Metadata Service. */
+    @Before
+    public void setUp() throws Exception {
+        mockServer = new EC2MetadataServiceMock();
+        mockServer.start();
+    }
+
+    /** Shuts down the mock EC2 Instance Metadata Service. */
+    @After
+    public void tearDown() throws Exception {
+        mockServer.stop();
+        Thread.sleep(1000);
+    }
+
+    @Test
+    public void getInstance_ReturnsSameInstance() {
+        assertEquals(InstanceProfileCredentialsProvider.getInstance(),
+                     InstanceProfileCredentialsProvider.getInstance());
+    }
+
+    /** Tests that we correctly handle the metadata service returning credentials. */
+    @Test
+    public void testSessionCredentials() throws Exception {
+        mockServer.setResponseFileName("sessionResponse");
+        mockServer.setAvailableSecurityCredentials("aws-dr-tools-test");
+
+        InstanceProfileCredentialsProvider credentialsProvider = new InstanceProfileCredentialsProvider();
+        AWSSessionCredentials credentials = (AWSSessionCredentials)credentialsProvider.getCredentials();
+
+        assertEquals("ACCESS_KEY_ID",     credentials.getAWSAccessKeyId());
+        assertEquals("SECRET_ACCESS_KEY", credentials.getAWSSecretKey());
+        assertEquals("TOKEN_TOKEN_TOKEN", credentials.getSessionToken());
+    }
+
+    /**
+     * Tests that we correctly handle the metadata service returning credentials
+     * when multiple instance profiles are available.
+     */
+    @Test
+    public void testSessionCredentials_MultipleInstanceProfiles() throws Exception {
+        mockServer.setResponseFileName("sessionResponse");
+        mockServer.setAvailableSecurityCredentials("test-credentials");
+
+        InstanceProfileCredentialsProvider credentialsProvider = new InstanceProfileCredentialsProvider();
+        AWSSessionCredentials credentials = (AWSSessionCredentials)credentialsProvider.getCredentials();
+
+        assertEquals("ACCESS_KEY_ID",     credentials.getAWSAccessKeyId());
+        assertEquals("SECRET_ACCESS_KEY", credentials.getAWSSecretKey());
+        assertEquals("TOKEN_TOKEN_TOKEN", credentials.getSessionToken());
+    }
+
+    /**
+     * Tests that we correctly handle when no instance profiles are available
+     * through the metadata service.
+     */
+    @Test
+    public void testNoInstanceProfiles() throws Exception {
+        mockServer.setResponseFileName("sessionResponse");
+        mockServer.setAvailableSecurityCredentials("");
+
+        InstanceProfileCredentialsProvider credentialsProvider = new InstanceProfileCredentialsProvider();
+
+        try {
+            credentialsProvider.getCredentials();
+            fail("Expected an AmazonClientException, but wasn't thrown");
+        } catch (AmazonClientException ace) {
+            assertNotNull(ace.getMessage());
+        }
+    }
+
+    @Test
+    public void getCredentialsDisabled_shouldGetCredentialsAfterEnabled() throws Exception {
+        InstanceProfileCredentialsProvider credentialsProvider = null;
+        try {
+            System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+            credentialsProvider = new InstanceProfileCredentialsProvider();
+            credentialsProvider.getCredentials();
+            fail("exception not thrown when ec2Metadata disabled");
+        } catch (AmazonClientException ex) {
+            //expected
+        } finally {
+            System.clearProperty("com.amazonaws.sdk.disableEc2Metadata");
+        }
+        mockServer.setResponseFileName("sessionResponse");
+        mockServer.setAvailableSecurityCredentials("test-credentials");
+        assertNotNull(credentialsProvider.getCredentials());
+    }
+
+    /**
+     * Tests that we correctly handle when the metadata service credentials have
+     * expired.
+     */
+    @Test
+    public void testSessionCredentials_Expired() throws Exception {
+        mockServer.setResponseFileName("sessionResponseExpired");
+        mockServer.setAvailableSecurityCredentials("test-credentials");
+
+        InstanceProfileCredentialsProvider credentialsProvider = new InstanceProfileCredentialsProvider();
+        try {
+            credentialsProvider.getCredentials();
+            fail("Expected an AmazonClientException, but wasn't thrown");
+        } catch (AmazonClientException ace) {
+            assertNotNull(ace.getMessage());
+        }
+    }
+
+    /**
+     * Tests by initiating a refresh thread in parallel which refreshes the
+     * credentials. Next call to credentials provider will result in refreshing
+     * and getting new credentials.
+     */
+    @Test
+    public void testMultipleThreadsLoadingAndRefreshingCredentials()
+            throws Exception {
+        mockServer.setResponseFileName("sessionResponse");
+        mockServer.setAvailableSecurityCredentials("test-credentials");
+
+        InstanceProfileCredentialsProvider credentialsProvider = new InstanceProfileCredentialsProvider();
+        AWSSessionCredentials credentials = (AWSSessionCredentials) credentialsProvider
+                .getCredentials();
+
+        assertNotNull(credentials);
+
+        new RefreshThread(credentialsProvider).join();
+
+        AWSSessionCredentials newCredentials = (AWSSessionCredentials) credentialsProvider
+                .getCredentials();
+
+        assertNotNull(newCredentials);
+
+        assertNotSame(credentials, newCredentials);
+    }
+
+    @Test(expected = AmazonClientException.class)
+    public void canBeConfiguredToOnlyRefreshCredentialsAfterFirstCallToGetCredentials() throws InterruptedException {
+        mockServer.setResponseFileName("sessionResponseExpired");
+        mockServer.setAvailableSecurityCredentials("test-credentials");
+
+        InstanceProfileCredentialsProvider credentialsProvider = InstanceProfileCredentialsProvider.createAsyncRefreshingProvider(false);
+        Thread.sleep(1000);
+
+        //Hacky assert but we know that this mockServer will create an exception that will be logged, if there's no log entry
+        //then there's no exception, which means that getCredentials didn't get called on the fetcher
+        assertThat(loggedEvents(), is(empty()));
+
+        credentialsProvider.getCredentials();
+    }
+
+    private class RefreshThread extends Thread{
+
+        private InstanceProfileCredentialsProvider provider;
+
+        public RefreshThread(InstanceProfileCredentialsProvider provider){
+            this.provider = provider;
+            this.start();
+        }
+
+        @Override
+        public void run() {
+            this.provider.refresh();
+        }
+    }
+
+}
