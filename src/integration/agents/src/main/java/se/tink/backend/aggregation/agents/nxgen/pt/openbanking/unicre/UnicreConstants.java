48e1383978689ad2803912ef22fb18342fbbbb33
julkamaje <julia.majewska@tink.se>
2019-08-19 12:10:50 +0200

    feat(SIBS): adding unicre agent, changes after PR

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/unicre/UnicreConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/unicre/UnicreConstants.java
@@ -2,5 +2,5 @@ package se.tink.backend.aggregation.agents.nxgen.pt.openbanking.unicre;
 
 public class UnicreConstants {
 
-    public static final String INTEGRATION_NAME = "unicre";
+    public static final String INTEGRATION_NAME = "unicre-pt";
 }
