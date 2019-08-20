8928f72c810f98c7d64eed6c0cbda021a3d756c5
Sebastian Olsson <sebastian.olsson@tink.se>
2019-08-20 23:27:38 +0200

    refactor(controllers/auth): Removed unreachable code

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/thirdpartyapp/RedirectStep.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/thirdpartyapp/RedirectStep.java
@@ -2,19 +2,15 @@ package se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor
 
 import java.util.Collections;
 import java.util.Map;
-import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
-import se.tink.backend.aggregation.agents.exceptions.ThirdPartyAppException;
 import se.tink.backend.aggregation.agents.exceptions.errors.LoginError;
-import se.tink.backend.aggregation.agents.exceptions.errors.ThirdPartyAppError;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.AuthenticationRequest;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.AuthenticationResponse;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.AuthenticationStep;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2AuthenticationProgressiveController;
 import se.tink.backend.aggregation.nxgen.controllers.utils.SupplementalInformationHelper;
-import se.tink.libraries.i18n.LocalizableKey;
 
 final class RedirectStep<T> implements AuthenticationStep {
 
@@ -45,12 +41,5 @@ final class RedirectStep<T> implements AuthenticationStep {
         authenticator.collect(callbackData);
 
         return new AuthenticationResponse(Collections.emptyList());
-    }
-
-    private ThirdPartyAppException decorateException(
-            ThirdPartyAppStatus status, ThirdPartyAppError error) {
-        Optional<LocalizableKey> authenticatorMessage =
-                authenticator.getUserErrorMessageFor(status);
-        return error.exception(authenticatorMessage.orElse(error.userMessage()));
     }
 }
