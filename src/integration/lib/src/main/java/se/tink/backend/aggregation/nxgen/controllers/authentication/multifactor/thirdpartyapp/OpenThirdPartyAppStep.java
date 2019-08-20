31ab5dd59db48b5f8517f14a2b07f25b667149e6
Sebastian Olsson <sebastian.olsson@tink.se>
2019-08-21 00:43:08 +0200

    refactor(controllers/auth): Coupled first auth step to OAuth2 impl

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/thirdpartyapp/OpenThirdPartyAppStep.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/thirdpartyapp/OpenThirdPartyAppStep.java
@@ -4,13 +4,14 @@ import com.google.common.base.Preconditions;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.AuthenticationRequest;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.AuthenticationResponse;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.AuthenticationStep;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2AuthenticationProgressiveController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.payloads.ThirdPartyAppAuthenticationPayload;
 
 final class OpenThirdPartyAppStep<T> implements AuthenticationStep {
 
-    private final ThirdPartyAppProgressiveAuthenticator<T> authenticator;
+    private final OAuth2AuthenticationProgressiveController authenticator;
 
-    OpenThirdPartyAppStep(final ThirdPartyAppProgressiveAuthenticator<T> authenticator) {
+    OpenThirdPartyAppStep(final OAuth2AuthenticationProgressiveController authenticator) {
         this.authenticator = authenticator;
     }
 
