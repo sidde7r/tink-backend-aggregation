6ae43d263a8711ee77d174a14b3a87150926f34d
Mauritz Zachrisson <mauritz.zachrisson@tink.se>
2019-08-21 16:47:33 +0200

    refactor(aws-sdk): Fix Bazel "package name missing" warning

--- third_party/aws_java_sdk_core/src/main/java/com/amazonaws/log/JulLogFactory.java
+++ third_party/aws_java_sdk_core/src/main/java/com/amazonaws/log/JulLogFactory.java
@@ -1,3 +1,5 @@
+package com.amazonaws.log;
+
 /*
  * Copyright 2015-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
@@ -11,7 +13,7 @@
  * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
- */ package com.amazonaws.log;
+ */
 
 import java.util.logging.Logger;
 
