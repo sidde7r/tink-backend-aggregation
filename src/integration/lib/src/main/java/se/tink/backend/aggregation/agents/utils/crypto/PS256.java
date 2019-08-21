71e8ca86988d662d04692d2dffea9284ad42fbca
Girish Sharma <girish.sharma@tink.se>
2019-08-21 14:29:56 +0200

    refactor(UK_PROD): support for byte[], string as input

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/crypto/PS256.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/crypto/PS256.java
@@ -13,10 +13,23 @@ import se.tink.libraries.serialization.utils.SerializationUtils;
 public class PS256 {
     private PS256() {}
 
+    public static String sign(JWSHeader header, byte[] payloadObject, RSAPrivateKey signingKey) {
+        Payload payload = new Payload(payloadObject);
+        return sign(header, signingKey, payload);
+    }
+
+    public static String sign(JWSHeader header, String payloadObject, RSAPrivateKey signingKey) {
+        Payload payload = new Payload(payloadObject);
+        return sign(header, signingKey, payload);
+    }
+
     public static String sign(
             JWSHeader header, JSONObject payloadObject, RSAPrivateKey signingKey) {
-
         Payload payload = new Payload(SerializationUtils.serializeToString(payloadObject));
+        return sign(header, signingKey, payload);
+    }
+
+    private static String sign(JWSHeader header, RSAPrivateKey signingKey, Payload payload) {
         JWSObject signed = new JWSObject(header, payload);
         JWSSigner signer = new RSASSASigner(signingKey);
         try {
