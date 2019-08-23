a89293e215061249220dfa8f48b154eb80484d16
Mattias Hird <mattias.hird@tink.se>
2019-08-23 14:02:16 +0200

    fix(swedbank): remove ignore on test as it uses argman

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/SwedbankSEAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/SwedbankSEAgentTest.java
@@ -2,14 +2,12 @@ package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank;
 
 import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 import se.tink.libraries.credentials.service.RefreshableItem;
 
-@Ignore
 public class SwedbankSEAgentTest {
 
     private enum Arg {
