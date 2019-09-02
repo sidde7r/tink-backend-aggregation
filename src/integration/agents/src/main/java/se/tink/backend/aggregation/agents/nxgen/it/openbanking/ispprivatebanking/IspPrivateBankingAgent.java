c318351145bf4135704798a159a057cf96816177
tzietek <tymon.zietek@tink.se>
2019-09-02 17:51:40 +0200

    feat(cbi): adds isp private banking agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/ispprivatebanking/IspPrivateBankingAgent.java
@@ -0,0 +1,23 @@
+package se.tink.backend.aggregation.agents.nxgen.it.openbanking.ispprivatebanking;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
+import se.tink.backend.aggregation.agents.RefreshSavingsAccountsExecutor;
+import se.tink.backend.aggregation.agents.nxgen.it.openbanking.isp.IspAgent;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class IspPrivateBankingAgent extends IspAgent
+        implements RefreshCheckingAccountsExecutor, RefreshSavingsAccountsExecutor {
+
+    public IspPrivateBankingAgent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected String getIntegrationName() {
+        return IspPrivateBankingConstants.INTEGRATION_NAME;
+    }
+
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/ispprivatebanking/IspPrivateBankingConstants.java
@@ -0,0 +1,9 @@
+package se.tink.backend.aggregation.agents.nxgen.it.openbanking.ispprivatebanking;
+
+public final class IspPrivateBankingConstants {
+    public static final String INTEGRATION_NAME = "ispprivatebanking";
+
+    private IspPrivateBankingConstants() {
+        throw new AssertionError();
+    }
+}
