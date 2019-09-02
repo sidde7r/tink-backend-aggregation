d8ff7022a2e4e0384290cc589cbcaee363fb35d8
tzietek <tymon.zietek@tink.se>
2019-09-02 17:53:56 +0200

    feat(cbi): adds findomestic agent implementation

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/findomestic/FindomesticConstants.java
@@ -0,0 +1,9 @@
+package se.tink.backend.aggregation.agents.nxgen.it.openbanking.findomestic;
+
+public final class FindomesticConstants {
+    public static final String INTEGRATION_NAME = "findomestic";
+
+    private FindomesticConstants() {
+        throw new AssertionError();
+    }
+}
