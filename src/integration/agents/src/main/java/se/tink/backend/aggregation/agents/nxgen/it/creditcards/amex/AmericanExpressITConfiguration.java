5202c14209548ec17f1c02ffe3a9782821616d34
Mateusz Pedzich <mateusz.pedzich@tink.se>
2019-09-04 14:44:50 +0200

    feat(IT/amex): adding amex IT re agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/creditcards/amex/AmericanExpressITConfiguration.java
@@ -0,0 +1,53 @@
+package se.tink.backend.aggregation.agents.nxgen.it.creditcards.amex;
+
+import java.util.Date;
+import se.tink.backend.aggregation.agents.nxgen.it.creditcards.amex.AmericanExpressITConstants.BodyValue;
+import se.tink.backend.aggregation.agents.nxgen.it.creditcards.amex.AmericanExpressITConstants.HeaderValues;
+import se.tink.backend.aggregation.agents.nxgen.it.creditcards.amex.fetcher.rpc.TimelineITRequest;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.AmericanExpressV62Configuration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.AmericanExpressV62Constants;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.fetcher.entities.CardEntity;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.fetcher.rpc.TimelineRequest;
+import se.tink.libraries.amount.Amount;
+import se.tink.libraries.date.ThreadSafeDateFormat;
+
+public class AmericanExpressITConfiguration implements AmericanExpressV62Configuration {
+
+    @Override
+    public String getLocale() {
+        return BodyValue.LOCALE;
+    }
+
+    @Override
+    public String getAppId() {
+        return HeaderValues.APP_ID;
+    }
+
+    @Override
+    public String getUserAgent() {
+        return HeaderValues.USER_AGENT;
+    }
+
+    @Override
+    public String getBankId(CardEntity cardEntity) {
+        return cardEntity.getCardNumberDisplay();
+    }
+
+    @Override
+    public Amount toAmount(Double amount) {
+        return Amount.inEUR(amount);
+    }
+
+    @Override
+    public TimelineRequest createTimelineRequest(Integer cardIndex) {
+        TimelineITRequest request = new TimelineITRequest();
+        request.setTimeZone(AmericanExpressV62Constants.RequestValue.TIME_ZONE);
+        request.setTimeZoneOffset(AmericanExpressV62Constants.RequestValue.TIME_ZONE_OFFSET);
+        request.setSortedIndex(cardIndex);
+        request.setLocalTime(new ThreadSafeDateFormat("MM-dd-YYYY'T'HH:mm:ss").format(new Date()));
+        request.setPendingChargeEnabled(true);
+        request.setCmlEnabled(true);
+        request.setTimestamp(Long.toString(System.currentTimeMillis()));
+        return request;
+    }
+}
