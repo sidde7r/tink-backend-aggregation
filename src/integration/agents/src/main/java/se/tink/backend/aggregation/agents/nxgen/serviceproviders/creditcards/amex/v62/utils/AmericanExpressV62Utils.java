1753a0ea8d45b2be2650241c9e64bc38c7b3a44d
Adriana Rychlinska <[adriana.rychlinska@tink.se]>
2019-09-05 11:41:58 +0200

    style(Amex): Fix formatting

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/amex/v62/utils/AmericanExpressV62Utils.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/amex/v62/utils/AmericanExpressV62Utils.java
@@ -1,10 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.utils;
 
-import java.math.BigDecimal;
-import se.tink.libraries.strings.StringUtils;
 import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.AmericanExpressV62Constants.DASH;
 import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.AmericanExpressV62Constants.NOT_APPLICABLE;
 import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.AmericanExpressV62Constants.NUMBER_REGEX;
+
+import java.math.BigDecimal;
+import se.tink.libraries.strings.StringUtils;
 
 public class AmericanExpressV62Utils {
     public static boolean isValidAmount(final String value) {
