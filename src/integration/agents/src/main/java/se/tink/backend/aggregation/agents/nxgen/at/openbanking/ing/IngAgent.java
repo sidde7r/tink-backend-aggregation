6f551b7fcad457a50bfd230e9191cb50c2bd24d7
tzietek <tymon.zietek@tink.se>
2019-09-10 13:56:18 +0200

    feat(ing): adds austrian ing impl

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/ing/IngAgent.java
@@ -0,0 +1,21 @@
+package se.tink.backend.aggregation.agents.nxgen.at.openbanking.ing;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ingbase.IngBaseAgent;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class IngAgent extends IngBaseAgent {
+
+    public static final String INTEGRATION_NAME = "ing";
+
+    public IngAgent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected String getIntegrationName() {
+        return INTEGRATION_NAME;
+    }
+}
