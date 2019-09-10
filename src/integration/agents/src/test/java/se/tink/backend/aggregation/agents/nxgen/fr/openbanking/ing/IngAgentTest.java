11104f7f4e486fc8e2c8a80c8e97a60628205e50
tzietek <tymon.zietek@tink.se>
2019-09-10 13:57:48 +0200

    feat(ing): adds french ing impl

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/ing/IngAgentTest.java
@@ -0,0 +1,26 @@
+package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.ing;
+
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+
+@Ignore
+public class IngAgentTest {
+
+    private AgentIntegrationTest.Builder builder;
+
+    @Before
+    public void setup() {
+        builder =
+                new AgentIntegrationTest.Builder("fr", "fr-ing-ob")
+                        .loadCredentialsBefore(false)
+                        .saveCredentialsAfter(false)
+                        .expectLoggedIn(false);
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder.build().testRefresh();
+    }
+}
