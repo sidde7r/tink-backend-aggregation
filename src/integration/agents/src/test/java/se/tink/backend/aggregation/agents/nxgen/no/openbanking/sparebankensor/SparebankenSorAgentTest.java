302e425fb498342de90f1bc2a23f862500931700
gyslainzola <gyslain.zola@tink.se>
2019-09-11 11:27:25 +0200

    fix(NO/SparebankenSor): Removed unecessary ignore

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/sparebankensor/SparebankenSorAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/sparebankensor/SparebankenSorAgentTest.java
@@ -1,11 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.no.openbanking.sparebankensor;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
-@Ignore
 public class SparebankenSorAgentTest {
 
     private AgentIntegrationTest.Builder builder;
