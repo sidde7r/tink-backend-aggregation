68bd0b5625ce6973c707738ba64489692e1f69e0
Vibhu Srivastava <vibhu.srivastava@tink.com>
2019-09-11 13:47:15 +0200

    fix(de-deutschebank): update consentBaseResponse

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/deutschebank/authenticator/rpc/ConsentStatusResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/deutschebank/authenticator/rpc/ConsentStatusResponse.java
@@ -1,21 +1,12 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.deutschebank.authenticator.rpc;
 
-<<<<<<< HEAD
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-=======
->>>>>>> 6d66ab014d6ce0c10e31a52e2682c7d21def5624
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
 public class ConsentStatusResponse {
-<<<<<<< HEAD
     @JsonProperty private String consentStatus;
-=======
-
-    @JsonProperty
-    private String consentStatus;
->>>>>>> 6d66ab014d6ce0c10e31a52e2682c7d21def5624
 
     @JsonIgnore
     public String getConsentStatus() {
