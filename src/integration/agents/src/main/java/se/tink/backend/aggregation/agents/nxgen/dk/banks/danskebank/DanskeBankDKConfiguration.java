1ccd11d8e6eef2a22f104c8ad3ce04e2628cd16e
Schahin Rajab <schahin.rajab@tink.se>
2019-09-12 11:37:01 +0200

    fix(danskebank): update headers

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/danskebank/DanskeBankDKConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/danskebank/DanskeBankDKConfiguration.java
@@ -18,8 +18,8 @@ public class DanskeBankDKConfiguration implements DanskeBankConfiguration {
     private static final String MARKET_CODE = "DK";
     private static final String CLIENT_ID = "5ec4b8ad-a93d-43e1-831c-8e78ee6e661a";
     private static final String CLIENT_SECRET =
-            "EsSh56X7pitypbM77NTTLjYPEB22dpxELyPDt9mY26NFGWL9xt";
-    private static final String APP_VERSION_HEADER = "MobileBank ios DK 248035554";
+            "38EhwxUTUYq1KTTbMvfEahKQY3TtXrlifHjwveFBUaqwIgwZ2t";
+    private static final String APP_VERSION_HEADER = "MobileBank ios DK 285229706";
     private static final String DEVICE_SERIAL_NO_KEY = "x-device-serial-no";
     private static final String STEP_UP_TOKEN_KEY = "x-stepup-token";
 
