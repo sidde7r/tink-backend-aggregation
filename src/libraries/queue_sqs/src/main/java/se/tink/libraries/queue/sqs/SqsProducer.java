03381f2565ac7823c86d89e3b162d072320f29df
Jonas Renold <jonas.renold@tink.se>
2019-09-12 13:20:13 +0200

    feat(SqsQueue): add requeue() on Producer
    Re-implement the old logic of requeue on the QueueProducer. Manual revert of: a74276bb026969441d144795e47bd49734369ec4

--- src/libraries/queue_sqs/src/main/java/se/tink/libraries/queue/sqs/SqsProducer.java
+++ src/libraries/queue_sqs/src/main/java/se/tink/libraries/queue/sqs/SqsProducer.java
@@ -1,14 +1,18 @@
 package se.tink.libraries.queue.sqs;
 
 import com.amazonaws.services.sqs.model.SendMessageRequest;
+import com.google.common.base.Preconditions;
 import com.google.inject.Inject;
 import java.io.IOException;
+import java.time.temporal.ValueRange;
+import java.util.concurrent.ThreadLocalRandom;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.tink.libraries.queue.QueueProducer;
 
 public class SqsProducer implements QueueProducer {
 
+    private static final ValueRange DELAY_MAX_RANGE = ValueRange.of(0, 900);
     private SqsQueue sqsQueue;
     private EncodingHandler encodingHandler;
     private Logger logger = LoggerFactory.getLogger(SqsProducer.class);
@@ -37,7 +41,29 @@ public class SqsProducer implements QueueProducer {
     }
 
     @Override
+    public void requeue(String encodedMessageBody) {
+        SendMessageRequest sendMessageStandardQueue =
+                new SendMessageRequest()
+                        .withQueueUrl(sqsQueue.getUrl())
+                        .withMessageBody(encodedMessageBody)
+                        // With delay seconds can max hide a message for 15 min.
+                        .withDelaySeconds(randomTimeoutSeconds(0, 900));
+        sqsQueue.getSqs().sendMessage(sendMessageStandardQueue);
+    }
+
+    @Override
     public boolean isAvailable() {
         return sqsQueue.isAvailable();
+    }
+
+    private int randomTimeoutSeconds(int min, int max) {
+        Preconditions.checkArgument(
+                min < max, "The minimum value cannot be same or equal to the maximum");
+        Preconditions.checkArgument(
+                DELAY_MAX_RANGE.isValidValue(min), "The minimum value is not in the allowed range");
+        Preconditions.checkArgument(
+                DELAY_MAX_RANGE.isValidValue(max), "The maximum value is not in the allowed range");
+
+        return ThreadLocalRandom.current().nextInt(min, max);
     }
 }
