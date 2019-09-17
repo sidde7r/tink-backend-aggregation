b77de04e9b9eab7a14b97daf4bc9165863602022
Annica <annica.alstermark@tink.se>
2019-09-17 13:20:56 +0200

    refactor(SBAB/legacy): Use stream and lambdas

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/sbab/executor/rpc/SignProcessResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/sbab/executor/rpc/SignProcessResponse.java
@@ -1,8 +1,9 @@
 package se.tink.backend.aggregation.agents.banks.sbab.executor.rpc;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.common.base.Strings;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import se.tink.backend.aggregation.agents.TransferExecutionException;
 import se.tink.backend.aggregation.agents.TransferExecutionException.EndUserMessage;
 import se.tink.backend.aggregation.agents.banks.sbab.executor.entities.SignatureEntity;
@@ -18,23 +19,15 @@ public class SignProcessResponse {
 
     @JsonIgnore
     public String getBankIdRefOrThrowIfNotPresent(Catalog catalog) {
-        if (signatureIsPresent()) {
-            SignatureEntity signature = signatures.get(0);
-            String bankIdRef = signature.getId();
-
-            if (!Strings.isNullOrEmpty(bankIdRef)) {
-                return bankIdRef;
-            }
-        }
-
-        throw TransferExecutionException.builder(SignableOperationStatuses.FAILED)
-                .setMessage(catalog.getString(EndUserMessage.BANKID_FAILED))
-                .setEndUserMessage(catalog.getString(EndUserMessage.BANKID_FAILED))
-                .build();
-    }
-
-    @JsonIgnore
-    private boolean signatureIsPresent() {
-        return signatures != null && !signatures.isEmpty();
+        return Optional.ofNullable(signatures).orElseGet(Collections::emptyList).stream()
+                .map(SignatureEntity::getId)
+                .findFirst()
+                .orElseThrow(
+                        () ->
+                                TransferExecutionException.builder(SignableOperationStatuses.FAILED)
+                                        .setMessage(catalog.getString(EndUserMessage.BANKID_FAILED))
+                                        .setEndUserMessage(
+                                                catalog.getString(EndUserMessage.BANKID_FAILED))
+                                        .build());
     }
 }
