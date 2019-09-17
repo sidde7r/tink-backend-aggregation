65ebfa1a6581597e7b014ae858a1dad1c1f57789
Annica <annica.alstermark@tink.se>
2019-09-17 10:41:42 +0200

    chore(SBAB/legacy): Remove unused exception

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/sbab/exception/UnsupportedSignTypeException.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.banks.sbab.exception;
-
-public class UnsupportedSignTypeException extends RuntimeException {
-    public UnsupportedSignTypeException(SignType type) {
-        super("Unsupported sign type: " + type);
-    }
-}
