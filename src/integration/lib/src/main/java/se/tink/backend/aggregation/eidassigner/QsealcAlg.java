dcbd452760a4628d2e302f73e1414fcfec130349
Nazri Abdullah <nazri.abdullah@tink.se>
2019-09-18 10:26:19 +0200

    feat(eidas-proxy-signer): adds PSS type signer in QsealCSigner

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/eidassigner/QsealcAlg.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/eidassigner/QsealcAlg.java
@@ -2,7 +2,8 @@ package se.tink.backend.aggregation.eidassigner;
 
 public enum QsealcAlg {
     EIDAS_RSA_SHA256("/sign-rsa-sha256/"),
-    EIDAS_PSS_SHA256("/sign-pss-sha256/");
+    EIDAS_PSS_SHA256("/sign-pss-sha256/"),
+    EIDAS_JWS_PS256("/sign-jws-ps256/");
 
     private final String signingType;
 
