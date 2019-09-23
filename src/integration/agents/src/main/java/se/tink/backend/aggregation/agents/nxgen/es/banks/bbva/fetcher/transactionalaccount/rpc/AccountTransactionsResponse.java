dbb564dc180fc3298950b8e9b86370d536119c99
Shankey Jain <shankey.jain@tink.se>
2019-09-23 11:42:50 +0200

    fix(bbva): Handling null-pointer while calling canFetchMore

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/fetcher/transactionalaccount/rpc/AccountTransactionsResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/fetcher/transactionalaccount/rpc/AccountTransactionsResponse.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.vavr.CheckedPredicate;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.BbvaConstants.Fetchers;
@@ -26,6 +27,7 @@ public class AccountTransactionsResponse implements TransactionKeyPaginatorRespo
     }
 
     @Override
+    @JsonIgnore
     public Collection<Transaction> getTinkTransactions() {
         return accountTransactions.stream()
                 .map(AccountTransactionEntity::toTransaction)
@@ -33,8 +35,13 @@ public class AccountTransactionsResponse implements TransactionKeyPaginatorRespo
     }
 
     @Override
+    @JsonIgnore
     public Optional<Boolean> canFetchMore() {
-        return Optional.of(getPagination().getPage() < getPagination().getNumPages());
+        if (!Objects.isNull(pagination)) {
+            return Optional.of(getPagination().getPage() < getPagination().getNumPages());
+        } else {
+            return Optional.of(false);
+        }
     }
 
     public PaginationEntity getPagination() {
@@ -54,6 +61,7 @@ public class AccountTransactionsResponse implements TransactionKeyPaginatorRespo
     }
 
     @Override
+    @JsonIgnore
     public String nextKey() {
         if (pagination == null) {
             return null;
