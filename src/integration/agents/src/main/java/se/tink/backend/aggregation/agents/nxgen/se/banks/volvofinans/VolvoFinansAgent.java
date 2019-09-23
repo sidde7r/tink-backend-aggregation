e01d564ac450d77c16c21b545885436706d181cc
Sebastian Olsson <sebastian.olsson@tink.se>
2019-09-23 22:44:20 +0200

    feat(volvofinans): Throwing NO_BANK_SERVICE on 503s
    This is to handle temporary downtime, which has been observed to consist
    of HTTP 503 responses with the following body:
    
    {"status":"STANGD","meddelande":"Tjänsten är tyvärr för närvarande stängt på grund av underhåll. Vänligen försök senare."}

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/volvofinans/VolvoFinansAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/volvofinans/VolvoFinansAgent.java
@@ -24,6 +24,7 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.Transac
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginationController;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
+import se.tink.backend.aggregation.nxgen.http.filter.ServiceUnavailableBankServiceErrorFilter;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 import se.tink.libraries.identitydata.countries.SeIdentityData;
 
@@ -40,6 +41,8 @@ public class VolvoFinansAgent extends NextGenerationAgent
     public VolvoFinansAgent(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
         super(request, context, signatureKeyPair);
+
+        client.addFilter(new ServiceUnavailableBankServiceErrorFilter());
         apiClient = new VolvoFinansApiClient(client, sessionStorage);
 
         creditCardRefreshController = constructCreditCardRefreshController();
