bcd5eb11700bbc6200751f3150807fdf675b1335
Jesús A. Álvarez <jesus.alvarez@tink.se>
2019-09-25 16:40:36 +0200

    test(es-ing): use ArgumentManager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/ing/v195/IngAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/ing/v195/IngAgentTest.java
@@ -1,17 +1,37 @@
 package se.tink.backend.aggregation.agents.nxgen.es.banks.ing.v195;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 
-@Ignore
 public class IngAgentTest {
+    private enum Arg {
+        USERNAME,
+        PASSWORD, // six digits
+        DATE_OF_BIRTH // ddMMyyyy
+    }
+
+    private final ArgumentManager<Arg> args = new ArgumentManager<>(Arg.values());
+
+    @Before
+    public void before() {
+        args.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
     private AgentIntegrationTest.Builder builder() {
         return new AgentIntegrationTest.Builder(IngConstants.MARKET, IngConstants.PROVIDER_NAME)
-                .addCredentialField(Field.Key.USERNAME, "")
-                .addCredentialField(IngConstants.DATE_OF_BIRTH, "") // ddMMyyyy
-                .addCredentialField(Field.Key.PASSWORD, "") // six digits
+                .setFinancialInstitutionId("es-ing")
+                .addCredentialField(Field.Key.USERNAME, args.get(Arg.USERNAME))
+                .addCredentialField(IngConstants.DATE_OF_BIRTH, args.get(Arg.DATE_OF_BIRTH))
+                .addCredentialField(Field.Key.PASSWORD, args.get(Arg.PASSWORD))
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false);
     }
