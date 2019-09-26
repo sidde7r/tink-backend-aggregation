1ed1db4c571cb980cb181268a40c044e685acdf8
Mattias Hird <mattias.hird@tink.se>
2019-09-26 16:14:21 +0200

    refactor(avanza): Fix loan fetcher to fetch accounts once only.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/brokers/avanza/fetcher/loan/AvanzaLoanFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/brokers/avanza/fetcher/loan/AvanzaLoanFetcher.java
@@ -2,20 +2,17 @@ package se.tink.backend.aggregation.agents.nxgen.se.brokers.avanza.fetcher.loan;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.nxgen.se.brokers.avanza.AvanzaApiClient;
 import se.tink.backend.aggregation.agents.nxgen.se.brokers.avanza.AvanzaAuthSessionStorage;
-import se.tink.backend.aggregation.agents.nxgen.se.brokers.avanza.fetcher.investment.entities.SessionAccountPair;
+import se.tink.backend.aggregation.agents.nxgen.se.brokers.avanza.AvanzaConstants.StorageKeys;
 import se.tink.backend.aggregation.agents.nxgen.se.brokers.avanza.fetcher.transactionalaccount.entities.AccountEntity;
-import se.tink.backend.aggregation.agents.nxgen.se.brokers.avanza.fetcher.transactionalaccount.rpc.AccountDetailsResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
+import se.tink.backend.aggregation.nxgen.core.account.entity.HolderName;
 import se.tink.backend.aggregation.nxgen.core.account.loan.LoanAccount;
-import se.tink.backend.aggregation.nxgen.exceptions.NotImplementedException;
 import se.tink.backend.aggregation.nxgen.storage.TemporaryStorage;
 import se.tink.libraries.serialization.utils.SerializationUtils;
 
@@ -37,40 +34,27 @@ public class AvanzaLoanFetcher implements AccountFetcher<LoanAccount> {
 
     @Override
     public Collection<LoanAccount> fetchAccounts() {
-        final List<SessionAccountPair> sessionAccountPairs =
-                authSessionStorage.keySet().stream()
-                        .flatMap(getSessionAccountPairs())
-                        .collect(Collectors.toList());
-        sessionAccountPairs.forEach(this::getAccounts);
+        final HolderName holderName = new HolderName(temporaryStorage.get(StorageKeys.HOLDER_NAME));
+
+        authSessionStorage.keySet().stream().flatMap(getAccounts(holderName)).close();
         return Collections.emptyList();
     }
 
-    private Function<String, Stream<? extends SessionAccountPair>> getSessionAccountPairs() {
+    private Function<String, Stream<?>> getAccounts(HolderName holderName) {
         return authSession ->
                 apiClient.fetchAccounts(authSession).getAccounts().stream()
                         .filter(AccountEntity::isLoanAccount)
+                        .peek(
+                                acc ->
+                                        LOGGER.info(
+                                                "Avanza Loan Account: {}",
+                                                SerializationUtils.serializeToString(acc)))
                         .map(AccountEntity::getAccountId)
-                        .map(accountId -> new SessionAccountPair(authSession, accountId));
-    }
-
-    private Function<String, Stream<? extends LoanAccount>> getAccounts(
-            SessionAccountPair sessionAccount) {
-
-        final String authSession = sessionAccount.getAuthSession();
-        List<AccountEntity> accounts =
-                apiClient.fetchAccounts(authSession).getAccounts().stream()
-                        .filter(AccountEntity::isLoanAccount)
-                        .collect(Collectors.toList());
-        LOGGER.info("Avanza Loan Accounts: {}", SerializationUtils.serializeToString(accounts));
-        List<AccountDetailsResponse> accountDetails =
-                accounts.stream()
-                        .map(AccountEntity::getAccountId)
-                        .map(accountId -> apiClient.fetchAccountDetails(accountId, authSession))
-                        .collect(Collectors.toList());
-        LOGGER.info(
-                "Avanza Loan Account details: {}",
-                SerializationUtils.serializeToString(accountDetails));
-        throw new NotImplementedException(
-                "Implementation not complete, logging response entities only for now until we can implement!");
+                        .map(accId -> apiClient.fetchAccountDetails(accId, authSession))
+                        .peek(
+                                details ->
+                                        LOGGER.info(
+                                                "Avanza Loan Account details: {}",
+                                                SerializationUtils.serializeToString(details)));
     }
 }
