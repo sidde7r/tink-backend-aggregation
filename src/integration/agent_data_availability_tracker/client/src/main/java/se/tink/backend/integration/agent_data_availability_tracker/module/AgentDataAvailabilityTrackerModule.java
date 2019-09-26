b9f860ae0e45d4a4c5a4b2bae9c5359cbac34afe
Gustaf Svanerud <gustaf.svanerud@tink.se>
2019-09-26 16:16:08 +0200

    fix(agent-dat): run mock client if not configured
    If the service is not configured it will not bind a mock client. This
    is to prevent errors when running locally without any server configured.
    
    This commit also extracts the tracker to its own module instead of
    weaving it into the aggregation module.

---
+++ src/integration/agent_data_availability_tracker/client/src/main/java/se/tink/backend/integration/agent_data_availability_tracker/module/AgentDataAvailabilityTrackerModule.java
@@ -0,0 +1,37 @@
+package se.tink.backend.integration.agent_data_availability_tracker.module;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+import java.util.Objects;
+import se.tink.backend.integration.agent_data_availability_tracker.client.AgentDataAvailabilityTrackerClient;
+import se.tink.backend.integration.agent_data_availability_tracker.client.AgentDataAvailabilityTrackerClientImpl;
+import se.tink.backend.integration.agent_data_availability_tracker.client.AgentDataAvailabilityTrackerClientMockImpl;
+import se.tink.backend.integration.agent_data_availability_tracker.client.AgentDataAvailabilityTrackerConfiguration;
+
+public class AgentDataAvailabilityTrackerModule extends AbstractModule {
+
+    private final AgentDataAvailabilityTrackerConfiguration configuration;
+
+    public AgentDataAvailabilityTrackerModule(
+            AgentDataAvailabilityTrackerConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Override
+    protected void configure() {
+
+        bind(AgentDataAvailabilityTrackerConfiguration.class).toInstance(configuration);
+
+        if (Objects.nonNull(configuration) && configuration.isValid()) {
+
+            bind(AgentDataAvailabilityTrackerClient.class)
+                    .to(AgentDataAvailabilityTrackerClientImpl.class)
+                    .in(Scopes.SINGLETON);
+        } else {
+
+            bind(AgentDataAvailabilityTrackerClient.class)
+                    .to(AgentDataAvailabilityTrackerClientMockImpl.class)
+                    .in(Scopes.SINGLETON);
+        }
+    }
+}
