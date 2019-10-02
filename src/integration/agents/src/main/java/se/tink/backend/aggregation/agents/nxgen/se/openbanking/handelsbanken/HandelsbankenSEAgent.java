19e9e072fc8ae8e5aeb97b10e7fed2c69d2cf27d
Annica <annica.alstermark@tink.se>
2019-10-02 16:50:13 +0200

    fix(SHB): Actually use autoauth

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/handelsbanken/HandelsbankenSEAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/handelsbanken/HandelsbankenSEAgent.java
@@ -9,8 +9,10 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.han
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAgent;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.authenticator.HandelsbankenBankidAuthenticator;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.authenticator.rpc.SessionResponse;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.bankid.BankIdAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.payment.PaymentController;
 import se.tink.libraries.credentials.service.CredentialsRequest;
@@ -45,11 +47,18 @@ public final class HandelsbankenSEAgent extends HandelsbankenBaseAgent {
     @Override
     protected Authenticator constructAuthenticator() {
 
-        return new BankIdAuthenticationController<>(
-                supplementalRequester,
-                new HandelsbankenBankidAuthenticator(apiClient, sessionStorage),
-                persistentStorage,
-                credentials);
+        BankIdAuthenticationController bankIdAuthenticationController =
+                new BankIdAuthenticationController<>(
+                        supplementalRequester,
+                        new HandelsbankenBankidAuthenticator(apiClient, sessionStorage),
+                        persistentStorage,
+                        credentials);
+
+        return new AutoAuthenticationController(
+                request,
+                systemUpdater,
+                bankIdAuthenticationController,
+                bankIdAuthenticationController);
     }
 
     @Override
