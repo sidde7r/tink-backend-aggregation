ecfba0e0eae135b020311cc1660b1e7ffdb9696f
Sebastian Olsson <sebastian.olsson@tink.se>
2019-10-05 23:32:02 +0200

    test(aggregation/lib): Corrected test (Bawag now has its own pkg)

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bankia/fetcher/investment/entities/ValueAccountIdentifierEntityTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bankia/fetcher/investment/entities/ValueAccountIdentifierEntityTest.java
@@ -3,7 +3,6 @@ package se.tink.backend.aggregation.agents.nxgen.es.banks.bankia.fetcher.investm
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import se.tink.backend.aggregation.agents.nxgen.es.banks.bankia.fetcher.investment.entities.ValueAccountIdentifierEntity;
 
 public class ValueAccountIdentifierEntityTest {
 
