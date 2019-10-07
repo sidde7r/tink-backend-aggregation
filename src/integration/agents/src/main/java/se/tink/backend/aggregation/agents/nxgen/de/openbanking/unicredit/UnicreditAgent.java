a94d1365cbe39a7d69128c296b1c649fc2887dd5
Trixxa <niclas.albinsson@gmail.com>
2019-10-07 15:49:48 +0200

    fix(unicredit): updated the flow according to changes in the bank api

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/unicredit/UnicreditAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/unicredit/UnicreditAgent.java
@@ -2,11 +2,9 @@ package se.tink.backend.aggregation.agents.nxgen.de.openbanking.unicredit;
 
 import se.tink.backend.aggregation.agents.AgentContext;
 import se.tink.backend.aggregation.agents.nxgen.de.openbanking.unicredit.UnicreditConstants.Market;
-import se.tink.backend.aggregation.agents.nxgen.de.openbanking.unicredit.authenticator.UnicreditAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.unicredit.UnicreditBaseAgent;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.unicredit.UnicreditBaseApiClient;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
-import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
 public class UnicreditAgent extends UnicreditBaseAgent {
@@ -24,10 +22,5 @@ public class UnicreditAgent extends UnicreditBaseAgent {
     @Override
     protected UnicreditBaseApiClient getApiClient(boolean manualRequest) {
         return new UnicreditApiClient(client, persistentStorage, credentials, manualRequest);
-    }
-
-    @Override
-    protected Authenticator constructAuthenticator() {
-        return new UnicreditAuthenticator((UnicreditApiClient) apiClient);
     }
 }
--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/unicredit/UnicreditAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/unicredit/UnicreditAgentTest.java
@@ -3,15 +3,10 @@ package se.tink.backend.aggregation.agents.nxgen.de.openbanking.unicredit;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-import se.tink.backend.agents.rpc.Field.Key;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
 @Ignore
 public class UnicreditAgentTest {
-
-    private static final String TEST_USERNAME = "bgdemo";
-    private static final String TEST_PASSWORD = "bgpassword";
-    private static final String TEST_OTP = "123456";
 
     private AgentIntegrationTest.Builder builder;
 
@@ -19,9 +14,6 @@ public class UnicreditAgentTest {
     public void setup() {
         builder =
                 new AgentIntegrationTest.Builder("de", "de-unicredit-ob")
-                        .addCredentialField(Key.USERNAME, TEST_USERNAME)
-                        .addCredentialField(Key.PASSWORD, TEST_PASSWORD)
-                        .addCredentialField(Key.OTP_INPUT, TEST_OTP)
                         .loadCredentialsBefore(false)
                         .saveCredentialsAfter(false)
                         .expectLoggedIn(false);
