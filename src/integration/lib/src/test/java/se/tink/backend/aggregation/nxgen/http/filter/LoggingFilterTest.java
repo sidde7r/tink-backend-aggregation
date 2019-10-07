5a1fa9de93de7cf33905e145de226cbedfe74205
Sebastian Olsson <sebastian.olsson@tink.se>
2019-10-07 22:20:56 +0200

    test(nxgen/http): Added test to cover said bug

---
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/nxgen/http/filter/LoggingFilterTest.java
@@ -0,0 +1,35 @@
+package se.tink.backend.aggregation.nxgen.http.filter;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Rule;
+import org.junit.Test;
+import se.tink.backend.aggregation.nxgen.http.NextGenTinkHttpClient;
+import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
+
+public class LoggingFilterTest {
+
+    @Rule public WireMockRule rule = new WireMockRule(8888);
+
+    @Test
+    public void whenSendingRequestWithFormBody_oneOfTheLogOutputLinesExactlyMatchesTheBody() {
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+
+        TinkHttpClient client = NextGenTinkHttpClient.builder().setPrintStream(printStream).build();
+
+        client.setDebugOutput(true);
+
+        client.request("http://127.0.0.1:8888/__admin").body("hoy").post();
+
+        final List<String> lines = Arrays.asList(outputStream.toString().split("\\r?\\n"));
+
+        // At the printing of the request body, there should exist one line with the exact text
+        // "hoy" (without the quotes)
+        assert lines.contains("hoy");
+    }
+}
