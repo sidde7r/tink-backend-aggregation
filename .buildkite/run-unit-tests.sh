6ee5b1adacf146a2c6545c72d4233a0d465687dc
Sebastian Olsson <sebastian.olsson@tink.se>
2019-10-09 12:56:11 +0200

    build(agents): Ensuring agent tests can compile in Buildkite pipeline

--- .buildkite/run-unit-tests.sh
+++ .buildkite/run-unit-tests.sh
@@ -3,6 +3,15 @@
 # Explicitly not setting `-e` here to be able to contain errors from bazel-wrapper.
 set -x
 
+# Ensure that tests tagged "manual" can be built
+./bazel-wrapper build --workspace_status_command $(pwd)/stamp.sh --disk_cache=/cache/v4-disk --repository_cache=/cache/v4-repo --deleted_packages=deb,docker --curses=yes --color=yes -- src/integration/agents:agents_test
+
+BUILD_OUTCOME=$?
+
+if [ $BUILD_OUTCOME -ne 0 ]; then
+    exit $BUILD_OUTCOME
+fi
+
 ./bazel-wrapper test --workspace_status_command $(pwd)/stamp.sh --disk_cache=/cache/v4-disk --repository_cache=/cache/v4-repo --deleted_packages=deb,docker --curses=yes --color=yes -- ...:all
 OUTCOME=$?
 .buildkite/upload-test-files.sh
