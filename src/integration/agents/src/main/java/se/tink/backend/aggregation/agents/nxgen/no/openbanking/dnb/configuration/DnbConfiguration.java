d348e06378574ea8288da199df861b9486a67edb
berkgedik <berk.gedik@tink.se>
2019-10-11 20:59:15 +0200

    feat(NO/DNB): Added annotations for config class

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/dnb/configuration/DnbConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/dnb/configuration/DnbConfiguration.java
@@ -1,15 +1,17 @@
 package se.tink.backend.aggregation.agents.nxgen.no.openbanking.dnb.configuration;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
 import se.tink.backend.aggregation.agents.nxgen.no.openbanking.dnb.DnbConstants.ErrorMessages;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.configuration.ClientConfiguration;
 
 @JsonObject
 public class DnbConfiguration implements ClientConfiguration {
 
-    private String redirectUrl;
+    @JsonProperty @Secret private String redirectUrl;
 
     public String getRedirectUrl() {
         Preconditions.checkNotNull(
