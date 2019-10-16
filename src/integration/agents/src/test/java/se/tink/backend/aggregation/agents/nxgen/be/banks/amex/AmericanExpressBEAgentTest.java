7a5a0819fba47838140f0c626a281a42e04d26e0
Nazri Abdullah <nazri.abdullah@tink.se>
2019-10-16 13:30:42 +0200

    test(be-amex): added a test class

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/amex/AmericanExpressBEAgentTest.java
@@ -0,0 +1,25 @@
+package se.tink.backend.aggregation.agents.nxgen.be.banks.amex;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+
+@Ignore
+public class AmericanExpressBEAgentTest {
+
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+
+    private final AgentIntegrationTest.Builder builder =
+            new AgentIntegrationTest.Builder("be", "be-americanexpress-password")
+                    .addCredentialField(Field.Key.USERNAME, USERNAME)
+                    .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                    .loadCredentialsBefore(false)
+                    .saveCredentialsAfter(false);
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder.build().testRefresh();
+    }
+}
