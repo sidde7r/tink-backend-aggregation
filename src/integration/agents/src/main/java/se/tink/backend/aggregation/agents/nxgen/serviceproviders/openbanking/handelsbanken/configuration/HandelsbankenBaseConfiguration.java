348fd5306c5d1d0545269eb2f85ee22f558d40ee
Annica <annica.alstermark@tink.se>
2019-10-16 13:21:23 +0200

    fix(SHB): Make clientId a sensitive secret

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/handelsbanken/configuration/HandelsbankenBaseConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/handelsbanken/configuration/HandelsbankenBaseConfiguration.java
@@ -3,12 +3,13 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ha
 import com.fasterxml.jackson.annotation.JsonProperty;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
+import se.tink.backend.aggregation.annotations.SensitiveSecret;
 import se.tink.backend.aggregation.configuration.ClientConfiguration;
 
 @JsonObject
 public class HandelsbankenBaseConfiguration implements ClientConfiguration {
 
-    @JsonProperty @Secret private String clientId;
+    @JsonProperty @SensitiveSecret private String clientId;
     @JsonProperty @Secret private String redirectUrl;
 
     public String getClientId() {
