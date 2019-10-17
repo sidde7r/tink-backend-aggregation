620c0d0b8d5ca8d7f94e84abce9f804c2ecaa5ad
Yunyi Huang <yunyi.huang@tink.se>
2019-10-17 16:50:02 +0200

    chore(eidas): Remove deprecated eidas files

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/eidas/Signer.java
+++
@@ -1,6 +0,0 @@
-package se.tink.backend.aggregation.eidas;
-
-/** Abstraction for any class that can create a signature from a signing string. */
-public interface Signer {
-    byte[] getSignature(byte[] signingBytes);
-}
