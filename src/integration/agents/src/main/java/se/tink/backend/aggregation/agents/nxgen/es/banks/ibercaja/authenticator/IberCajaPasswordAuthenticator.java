e3c32d9e1bf06848117f80478d4f1995bc5fb65d
Ahmed Hussein <ahmed.hussein@tink.se>
2019-10-17 16:14:24 +0200

    log(Ibercaja): remove unwanted log

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/ibercaja/authenticator/IberCajaPasswordAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/ibercaja/authenticator/IberCajaPasswordAuthenticator.java
@@ -10,13 +10,9 @@ import se.tink.backend.aggregation.agents.nxgen.es.banks.ibercaja.authenticator.
 import se.tink.backend.aggregation.agents.nxgen.es.banks.ibercaja.authenticator.rpc.LoginResponse;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.ibercaja.authenticator.rpc.SessionRequest;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.ibercaja.authenticator.rpc.SessionResponse;
-import se.tink.backend.aggregation.log.AggregationLogger;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.password.PasswordAuthenticator;
-import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class IberCajaPasswordAuthenticator implements PasswordAuthenticator {
-    private static final AggregationLogger LOGGER =
-            new AggregationLogger(IberCajaPasswordAuthenticator.class);
 
     private final IberCajaApiClient bankClient;
     private final IberCajaSessionStorage iberCajaSessionStorage;
@@ -44,14 +40,7 @@ public class IberCajaPasswordAuthenticator implements PasswordAuthenticator {
         iberCajaSessionStorage.saveTicket(sessionResponse.getTicket());
         iberCajaSessionStorage.saveFullName(sessionResponse.getName());
 
-        if (Strings.isNullOrEmpty(sessionResponse.getNif())) {
-            // Logging whole session response in case NIF value isn't present. Expecting to find
-            // a field called NIE or something else in that case. But we'll have to log the whole
-            // session response to find out.
-            LOGGER.infoExtraLong(
-                SerializationUtils.serializeToString(sessionResponse),
-                IberCajaConstants.Log.NIF_NOT_PRESENT);
-        } else {
+        if (!Strings.isNullOrEmpty(sessionResponse.getNif())) {
             iberCajaSessionStorage.saveDocumentNumber(sessionResponse.getNif());
         }
         // store NICI in the session storage to be masked in the logs
