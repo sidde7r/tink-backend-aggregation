2d872686a2da534aaf376962cf640a13a7d51066
Shankey Jain <shankey.jain@tink.se>
2019-10-17 12:49:16 +0200

    feat(countryProxies): Adding possibility to use country specific proxies

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/configuration/PasswordBasedProxyConfiguration.java
@@ -0,0 +1,22 @@
+package se.tink.backend.aggregation.configuration;
+
+import se.tink.backend.aggregation.annotations.JsonObject;
+
+@JsonObject
+public final class PasswordBasedProxyConfiguration {
+    private String host;
+    private String username;
+    private String password;
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
