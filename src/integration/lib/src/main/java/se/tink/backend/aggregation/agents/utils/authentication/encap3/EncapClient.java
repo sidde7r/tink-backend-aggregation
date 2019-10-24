80316d074ec237740161daa2ea35de3528228d53
Schahin Rajab <schahin.rajab@tink.se>
2019-10-24 17:20:41 +0200

    fix(encap3): set correct endpoint

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/authentication/encap3/EncapClient.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/authentication/encap3/EncapClient.java
@@ -116,7 +116,8 @@ public class EncapClient {
         String activateDeviceBody =
                 soapUtils.buildActivationCreateRequest(
                         username, activationSessionId, samlObjectB64);
-        soapResponse = messageUtils.encryptSoapAndSend(Urls.SOAP_ACTIVATION, activateDeviceBody);
+        soapResponse =
+                messageUtils.encryptSoapAndSend(Urls.SOAP_AUTHENTICATION, activateDeviceBody);
 
         return createDeviceRegistrationResponse(soapResponse);
     }
@@ -160,7 +161,7 @@ public class EncapClient {
         String authenticateDeviceBody =
                 soapUtils.buildAuthSessionReadRequest(username, samlObjectB64);
         String soapResponse =
-                messageUtils.encryptSoapAndSend(Urls.SOAP_ACTIVATION, authenticateDeviceBody);
+                messageUtils.encryptSoapAndSend(Urls.SOAP_AUTHENTICATION, authenticateDeviceBody);
 
         return createDeviceAuthenticationResponse(soapResponse);
     }
