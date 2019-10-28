7f6827a28b22ebde8ae18b37daa2dd59c823260a
Sasa Jankovic <s.jankovic@vegaitsourcing.rs>
2019-10-28 09:39:12 +0100

    fix(Skadia): Fix the import of the URL class

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/skandia/authenticator/SkandiaAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/skandia/authenticator/SkandiaAuthenticator.java
@@ -9,7 +9,7 @@ import se.tink.backend.aggregation.agents.nxgen.se.openbanking.skandia.SkandiaCo
 import se.tink.backend.aggregation.agents.nxgen.se.openbanking.skandia.configuration.SkandiaConfiguration;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2Authenticator;
 import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
-import se.tink.backend.aggregation.nxgen.http.URL;
+import se.tink.backend.aggregation.nxgen.http.url.URL;
 import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
 
 public class SkandiaAuthenticator implements OAuth2Authenticator {
