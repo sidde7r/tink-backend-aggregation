a125a6614afa3731a8a04c316b7acb720bab0259
Sebastian Olsson <sebastian.olsson@tink.se>
2019-10-28 17:12:39 +0100

    revert(nl-ics): Migrating secrets from k8 to SS
    This reverts commit 266d2cf1daacedcec5d37dfe106a9fd5630a369b.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/creditcards/ICS/ICSAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/creditcards/ICS/ICSAgent.java
@@ -4,10 +4,12 @@ import se.tink.backend.aggregation.agents.AgentContext;
 import se.tink.backend.aggregation.agents.FetchAccountsResponse;
 import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
 import se.tink.backend.aggregation.agents.RefreshCreditCardAccountsExecutor;
+import se.tink.backend.aggregation.agents.nxgen.nl.creditcards.ICS.ICSConstants.ErrorMessages;
 import se.tink.backend.aggregation.agents.nxgen.nl.creditcards.ICS.authenticator.ICSOAuthAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.nl.creditcards.ICS.configuration.ICSConfiguration;
 import se.tink.backend.aggregation.agents.nxgen.nl.creditcards.ICS.fetchers.credit.ICSAccountFetcher;
 import se.tink.backend.aggregation.agents.nxgen.nl.creditcards.ICS.fetchers.credit.ICSCreditCardFetcher;
+import se.tink.backend.aggregation.agents.utils.encoding.EncodingUtils;
 import se.tink.backend.aggregation.configuration.AgentsServiceConfiguration;
 import se.tink.backend.aggregation.nxgen.agents.NextGenerationAgent;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
@@ -37,9 +39,20 @@ public class ICSAgent extends NextGenerationAgent implements RefreshCreditCardAc
         redirectUri = request.getProvider().getPayload().split(" ")[1];
 
         final ICSConfiguration icsConfiguration =
-                getAgentConfigurationController().getAgentConfiguration(ICSConfiguration.class);
+                agentsServiceConfiguration
+                        .getIntegrations()
+                        .getClientConfiguration(
+                                ICSConstants.INTEGRATION_NAME, clientName, ICSConfiguration.class)
+                        .orElseThrow(
+                                () ->
+                                        new IllegalStateException(
+                                                ErrorMessages.MISSING_CONFIGURATION));
 
-        client.setEidasProxy(agentsServiceConfiguration.getEidasProxy());
+        client.setSslClientCertificate(
+                EncodingUtils.decodeBase64String(icsConfiguration.getClientSSLCertificate()), "");
+        client.trustRootCaCertificate(
+                EncodingUtils.decodeBase64String(icsConfiguration.getRootCACertificate()),
+                icsConfiguration.getRootCAPassword());
 
         apiClient =
                 new ICSApiClient(
