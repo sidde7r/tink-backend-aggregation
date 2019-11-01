ca55256b76cf58ccbbd2be059823be1279cb3995
Ahmed Hussein <ahmed.hussein@tink.se>
2019-11-01 16:02:35 +0100

    revert(NordeaPartner): add mandatory redirect uri param
    This reverts commit fef047cde5235d7c8bae4ed8ce51c461f8df7481.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/nordea/partner/configuration/NordeaPartnerConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/nordea/partner/configuration/NordeaPartnerConfiguration.java
@@ -10,7 +10,6 @@ import se.tink.backend.aggregation.nxgen.http.url.URL;
 
 @JsonObject
 public class NordeaPartnerConfiguration implements ClientConfiguration {
-    @Secret private String redirectUrl;
     @Secret private String partnerId;
     @Secret private String baseUrl;
     @Secret private String nordeaSigningPublicKey;
