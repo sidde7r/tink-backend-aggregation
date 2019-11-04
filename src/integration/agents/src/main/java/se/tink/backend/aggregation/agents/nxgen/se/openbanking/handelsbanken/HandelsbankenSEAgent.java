2bcb1e5fa2faab49c157dde695cbc6dd3b191de5
Jesús A. Álvarez <jesus.alvarez@tink.se>
2019-11-04 16:50:13 +0100

    feat(shb/pis): add generic transfer destinations

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/handelsbanken/HandelsbankenSEAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/handelsbanken/HandelsbankenSEAgent.java
@@ -1,9 +1,18 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.handelsbanken;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import se.tink.backend.agents.rpc.Account;
 import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.FetchTransferDestinationsResponse;
+import se.tink.backend.aggregation.agents.RefreshTransferDestinationExecutor;
+import se.tink.backend.aggregation.agents.general.TransferDestinationPatternBuilder;
+import se.tink.backend.aggregation.agents.general.models.GeneralAccountEntityImpl;
+import se.tink.backend.aggregation.agents.models.TransferDestinationPattern;
 import se.tink.backend.aggregation.agents.nxgen.se.openbanking.handelsbanken.executor.payment.HandelsbankenSEPaymentExecutor;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAccountConverter;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAgent;
@@ -15,9 +24,12 @@ import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticato
 import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.bankid.BankIdAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.payment.PaymentController;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.account.identifiers.SwedishSHBInternalIdentifier;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
-public final class HandelsbankenSEAgent extends HandelsbankenBaseAgent {
+public final class HandelsbankenSEAgent extends HandelsbankenBaseAgent
+        implements RefreshTransferDestinationExecutor {
 
     private final HandelsbankenAccountConverter accountConverter;
 
@@ -71,5 +83,29 @@ public final class HandelsbankenSEAgent extends HandelsbankenBaseAgent {
                 new HandelsbankenSEPaymentExecutor(
                         apiClient, credentials, supplementalRequester, persistentStorage);
         return Optional.of(new PaymentController(paymentExecutor, paymentExecutor));
+    }
+
+    @Override
+    public FetchTransferDestinationsResponse fetchTransferDestinations(List<Account> accounts) {
+        return new FetchTransferDestinationsResponse(
+                new TransferDestinationPatternBuilder()
+                        .setTinkAccounts(accounts)
+                        .setSourceAccounts(
+                                accounts.stream()
+                                        .map(GeneralAccountEntityImpl::createFromCoreAccount)
+                                        .filter(Optional::isPresent)
+                                        .map(Optional::get)
+                                        .collect(Collectors.toList()))
+                        .setDestinationAccounts(new ArrayList<>())
+                        .addMultiMatchPattern(
+                                AccountIdentifier.Type.SE, TransferDestinationPattern.ALL)
+                        .addMultiMatchPattern(
+                                AccountIdentifier.Type.SE_BG, TransferDestinationPattern.ALL)
+                        .addMultiMatchPattern(
+                                AccountIdentifier.Type.SE_PG, TransferDestinationPattern.ALL)
+                        .matchDestinationAccountsOn(
+                                AccountIdentifier.Type.SE_SHB_INTERNAL,
+                                SwedishSHBInternalIdentifier.class)
+                        .build());
     }
 }
