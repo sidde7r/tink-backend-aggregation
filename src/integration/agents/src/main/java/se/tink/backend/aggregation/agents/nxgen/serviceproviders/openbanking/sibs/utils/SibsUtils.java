a8cc837305c9ed898efbd4144c0dd0e90b4e9504
Krzysztof Korolczuk <krzysztof.korolczuk@tink.se>
2019-11-06 11:29:15 +0100

    refac(sibs): move util method to utils, add test, field visibility chg

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/utils/SibsUtils.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/utils/SibsUtils.java
@@ -5,9 +5,12 @@ import com.github.rholder.retry.RetryerBuilder;
 import com.github.rholder.retry.StopStrategies;
 import com.github.rholder.retry.WaitStrategies;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import org.apache.commons.lang3.StringUtils;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.authenticator.entity.ConsentStatus;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.executor.payment.entities.dictionary.SibsTransactionStatus;
 import se.tink.backend.aggregation.agents.utils.crypto.Hash;
@@ -15,6 +18,8 @@ import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public final class SibsUtils {
 
+    private static final DateTimeFormatter CONSENT_BODY_DATE_FORMATTER =
+            DateTimeFormatter.ofPattern(SibsConstants.Formats.CONSENT_BODY_DATE_FORMAT);
     private static final String DASH = "-";
 
     private SibsUtils() {}
@@ -23,6 +28,12 @@ public final class SibsUtils {
         String stringBody = SerializationUtils.serializeToString(body);
         byte[] bytes = stringBody.getBytes(StandardCharsets.UTF_8);
         return Hash.sha256Base64(bytes);
+    }
+
+    public static String get90DaysValidConsentStringDate() {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime days90Later = now.plusDays(90);
+        return CONSENT_BODY_DATE_FORMATTER.format(days90Later);
     }
 
     public static String getRequestId() {
