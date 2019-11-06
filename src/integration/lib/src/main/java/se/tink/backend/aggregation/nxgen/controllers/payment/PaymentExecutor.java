330c3250cc2453d44af01293be367ada19a776e9
Annica Alstermark <annica.alstermark@tink.se>
2019-11-06 10:46:35 +0100

    chore(GPI): Handle bankId exception in payment controller
    This is so that we don't have to have this exact parsing in every agent that uses bankID for signing payments. We'll probably need to rethink this logic when we have more signing cases implemented, for thirdparty apps etc. Settling for this now so we don't over engineer for a use case we don't fully understand.

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/payment/PaymentExecutor.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/payment/PaymentExecutor.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.nxgen.controllers.payment;
 
+import se.tink.backend.aggregation.agents.exceptions.BankIdException;
 import se.tink.backend.aggregation.agents.exceptions.payment.PaymentException;
 
 public interface PaymentExecutor {
@@ -7,7 +8,7 @@ public interface PaymentExecutor {
     PaymentResponse create(PaymentRequest paymentRequest) throws PaymentException;
 
     PaymentMultiStepResponse sign(PaymentMultiStepRequest paymentMultiStepRequest)
-            throws PaymentException;
+            throws PaymentException, BankIdException;
 
     CreateBeneficiaryMultiStepResponse createBeneficiary(
             CreateBeneficiaryMultiStepRequest createBeneficiaryMultiStepRequest);
