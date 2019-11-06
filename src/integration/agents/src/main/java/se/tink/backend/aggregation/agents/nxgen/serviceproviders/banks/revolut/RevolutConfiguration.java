089dc35e5e5214ec91f01938380ca2143fcfe400
Agusti Tomas <agusti.tomas@tink.se>
2019-11-06 15:36:55 +0100

    fix(revolut): Fixed ClientConfiguration package

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/revolut/RevolutConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/revolut/RevolutConfiguration.java
@@ -2,7 +2,7 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.revolut;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import se.tink.backend.aggregation.annotations.Secret;
-import se.tink.backend.aggregation.configuration.ClientConfiguration;
+import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
 
 public class RevolutConfiguration implements ClientConfiguration {
 
