a41b45c3942b18234abee6ce18065ccdbeec511a
berkgedik <berk.gedik@tink.se>
2019-11-06 11:02:34 +0100

    build(UK/Lloyds): Isolated Bazel package

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/lloyds/LloydsAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/lloyds/LloydsAgentTest.java
@@ -1,4 +1,4 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking;
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.lloyds;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
