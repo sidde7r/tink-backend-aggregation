58bea9d34fb9391213db7528fefff91d8e70e1d1
Yunyi Huang <yunyi.huang@tink.se>
2019-11-07 23:16:50 +0100

    feat(se-nordea): Get scopes from secrets service

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/nordeabase/configuration/NordeaBaseConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/nordeabase/configuration/NordeaBaseConfiguration.java
@@ -1,7 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.nordeabase.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.nordeabase.AgentType;
+import se.tink.backend.aggregation.annotations.AgentConfigParam;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.annotations.SensitiveSecret;
@@ -11,7 +13,8 @@ import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
 public class NordeaBaseConfiguration implements ClientConfiguration {
     @JsonProperty @Secret private String clientId;
     @JsonProperty @SensitiveSecret private String clientSecret;
-    @JsonProperty @Secret private String redirectUrl;
+    @JsonProperty @AgentConfigParam private String redirectUrl;
+    @JsonProperty @AgentConfigParam private List<String> scopes;
     @JsonProperty private AgentType agentType = AgentType.PERSONAL;
 
     public String getClientId() {
@@ -28,5 +31,9 @@ public class NordeaBaseConfiguration implements ClientConfiguration {
 
     public AgentType getAgentType() {
         return agentType;
+    }
+
+    public List<String> getScopes() {
+        return scopes;
     }
 }
