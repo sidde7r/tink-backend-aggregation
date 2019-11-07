a19f6959a6b1697ce259f436858303c498721861
berkgedik <berk.gedik@tink.se>
2019-11-07 09:54:01 +0100

    build(UK/Natwest): Isolated Bazel package

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/natwest/NatWestAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/natwest/NatWestAgentTest.java
@@ -1,4 +1,4 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking;
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.natwest;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
