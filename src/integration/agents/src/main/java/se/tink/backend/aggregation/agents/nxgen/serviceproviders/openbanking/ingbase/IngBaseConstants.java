773ed42c3ab6f4656bd09624b9894aa85f656287
Nazri Abdullah <nazri.abdullah@tink.se>
2019-11-08 16:03:08 +0100

    fix(nl-ing-ob): fixed unhandled 500 error from bank

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ingbase/IngBaseConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ingbase/IngBaseConstants.java
@@ -102,6 +102,7 @@ public final class IngBaseConstants {
         public static final String MISSING_CLIENT_ID = "Cannot find client id!";
         public static final int FORBIDDEN = 403;
         public static final int NOT_FOUND = 404;
+        public static final int INTERNAL_SERVER_ERROR = 500;
     }
 
     public static class Retry {
