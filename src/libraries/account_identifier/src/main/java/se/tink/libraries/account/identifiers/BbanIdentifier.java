ebae8b195f69462c7debd179eafc0294e078d53e
Gustaf Svanerud <gustaf.svanerud@tink.se>
2019-11-12 12:05:59 +0100

    fix(bban): use accountNumber as identifier
    Previously an always null field `identifier` was used. This was
    unnoticed since the class was not used by agents directly before.

--- src/libraries/account_identifier/src/main/java/se/tink/libraries/account/identifiers/BbanIdentifier.java
+++ src/libraries/account_identifier/src/main/java/se/tink/libraries/account/identifiers/BbanIdentifier.java
@@ -5,19 +5,14 @@ import se.tink.libraries.account.AccountIdentifier;
 public class BbanIdentifier extends AccountIdentifier {
 
     private String accountNumber;
-    private String identifier;
 
     public BbanIdentifier(String accountNumber) {
         this.accountNumber = accountNumber;
     }
 
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
     @Override
     public String getIdentifier() {
-        return identifier;
+        return accountNumber;
     }
 
     @Override
