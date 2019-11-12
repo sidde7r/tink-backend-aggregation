1036c8652c21e6d831f4219635ed31fdbb0e97af
Gustaf Svanerud <gustaf.svanerud@tink.se>
2019-11-12 12:12:17 +0100

    feat(UkobClientConfigAdapter): new client config interface
    New client configuration interface that uses the new
    signer and tls config interfaces.
    
    This configuration is compatible with both eIdas signing/tls and
    signing/tls configured from secret service values.

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/base/configuration/UkOpenBankingClientConfigurationAdapter.java
@@ -0,0 +1,22 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.configuration;
+
+import se.tink.backend.aggregation.configuration.ClientConfiguration;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.ProviderConfiguration;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.SoftwareStatementAssertion;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.jwt.JwtSigner;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.tls.TlsConfigurationAdapter;
+
+public interface UkOpenBankingClientConfigurationAdapter extends ClientConfiguration {
+
+    byte[] getRootCAData();
+
+    String getRootCAPassword();
+
+    ProviderConfiguration getProviderConfiguration();
+
+    SoftwareStatementAssertion getSoftwareStatementAssertion();
+
+    TlsConfigurationAdapter getTlsConfigurationAdapter();
+
+    JwtSigner getSigner();
+}
