2db38ad861c3cff83604e756bdd958cab5cc8a49
Sebastian Brzozowski <sebastian.brzozowski@tink.se>
2019-11-20 08:52:40 +0100

    BPI RE agent init commit

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/AuthenticationRequest.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/AuthenticationRequest.java
@@ -12,7 +12,7 @@ public final class AuthenticationRequest implements Credentialsable {
     private ImmutableMap<String, String> userInputs;
     private ImmutableMap<String, String> callbackData;
 
-    AuthenticationRequest(final Credentials credentials) {
+    public AuthenticationRequest(final Credentials credentials) {
         this.credentials = credentials;
     }
 
