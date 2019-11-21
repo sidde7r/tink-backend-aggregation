8eacd4e75eb31c8bb63028a12dc4828d7266e6d6
Sebastian Olsson <sebastian.olsson@tink.se>
2019-11-21 13:23:57 +0100

    test(phantomjs): Corrected test by catching expected exception

--- src/aggregation/lib/src/test/java/se/tink/backend/aggregation/phantomjs/PhantomJsTest.java
+++ src/aggregation/lib/src/test/java/se/tink/backend/aggregation/phantomjs/PhantomJsTest.java
@@ -3,25 +3,27 @@ package se.tink.backend.aggregation.phantomjs;
 import java.util.Map;
 import org.junit.Test;
 import org.openqa.selenium.Capabilities;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 
 public class PhantomJsTest {
 
     @Test
-    public void whenAttemptingToLoadFirefoxDriver_raisePermissibleException() {
-        final WebDriver driver =
-                new PhantomJSDriver(
-                        new Capabilities() {
-                            @Override
-                            public Map<String, Object> asMap() {
-                                return null;
-                            }
+    public void whenAttemptingToLoadPhantomJs_raiseCannotFindExecutableInPathException() {
+        try {
+            new PhantomJSDriver(
+                    new Capabilities() {
+                        @Override
+                        public Map<String, Object> asMap() {
+                            return null;
+                        }
 
-                            @Override
-                            public Object getCapability(String s) {
-                                return null;
-                            }
-                        });
+                        @Override
+                        public Object getCapability(String s) {
+                            return null;
+                        }
+                    });
+        } catch (IllegalStateException e) {
+            assert e.getMessage().contains("The path to the driver executable must");
+        }
     }
 }
