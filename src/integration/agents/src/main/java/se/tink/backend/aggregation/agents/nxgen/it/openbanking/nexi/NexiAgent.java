f6979490be048fabe8c8ee91b3287c697f8ef57b
tymon zietek <tymon.zietek@tink.se>
2019-11-25 14:52:06 +0100

    feat(nexi): adds nexi agent impl

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/nexi/NexiAgent.java
@@ -0,0 +1,43 @@
+package se.tink.backend.aggregation.agents.nxgen.it.openbanking.nexi;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeApiClient;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.fetcher.transactionalaccount.CbiGlobeTransactionalAccountFetcher;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcherController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionPagePaginationController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public class NexiAgent extends CbiGlobeAgent {
+
+    public NexiAgent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected String getIntegrationName() {
+        return NexiConstants.INTEGRATION_NAME;
+    }
+
+    @Override
+    protected CbiGlobeApiClient getApiClient(boolean requestManual) {
+        return new NexiApiClient(client, persistentStorage, requestManual, temporaryStorage);
+    }
+
+    @Override
+    protected TransactionalAccountRefreshController getTransactionalAccountRefreshController() {
+        final CbiGlobeTransactionalAccountFetcher accountFetcher =
+                CbiGlobeTransactionalAccountFetcher.createFromBooked(apiClient, persistentStorage);
+
+        return new TransactionalAccountRefreshController(
+                metricRefreshController,
+                updateController,
+                accountFetcher,
+                new TransactionFetcherController<>(
+                        transactionPaginationHelper,
+                        new TransactionPagePaginationController<>(accountFetcher, 1)));
+    }
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/nexi/NexiApiClient.java
@@ -0,0 +1,18 @@
+package se.tink.backend.aggregation.agents.nxgen.it.openbanking.nexi;
+
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeApiClient;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.configuration.InstrumentType;
+import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
+import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
+import se.tink.backend.aggregation.nxgen.storage.TemporaryStorage;
+
+public class NexiApiClient extends CbiGlobeApiClient {
+
+    public NexiApiClient(
+            TinkHttpClient client,
+            PersistentStorage persistentStorage,
+            boolean requestManual,
+            TemporaryStorage temporaryStorage) {
+        super(client, persistentStorage, requestManual, temporaryStorage, InstrumentType.CARDS_ACCOUNTS);
+    }
+}
