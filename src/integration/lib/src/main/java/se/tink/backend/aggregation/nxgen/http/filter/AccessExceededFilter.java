34decf3b6a3b43a7e11e8ebfa9706ab7f511aa5e
tymon zietek <tymon.zietek@tink.se>
2019-11-25 16:53:21 +0100

    refactor(filters): make AccessEsceededFilter public

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/filter/AccessExceededFilter.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/filter/AccessExceededFilter.java
@@ -1,14 +1,13 @@
-package se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.rabobank;
+package se.tink.backend.aggregation.nxgen.http.filter;
 
 import se.tink.backend.aggregation.agents.exceptions.errors.BankServiceError;
-import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.rabobank.RabobankConstants.ErrorMessages;
 import se.tink.backend.aggregation.nxgen.http.HttpResponse;
 import se.tink.backend.aggregation.nxgen.http.exceptions.HttpClientException;
 import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
-import se.tink.backend.aggregation.nxgen.http.filter.Filter;
 import se.tink.backend.aggregation.nxgen.http.request.HttpRequest;
 
-final class AccessExceededFilter extends Filter {
+public final class AccessExceededFilter extends Filter {
+    private static final CharSequence ACCESS_EXCEEDED = "access_exceeded";
 
     @Override
     public HttpResponse handle(final HttpRequest httpRequest)
@@ -18,7 +17,7 @@ final class AccessExceededFilter extends Filter {
         // HTTP 429: Too Many Requests
         if (response.getStatus() == 429) {
             String body = response.getBody(String.class).toLowerCase();
-            if (body.contains(ErrorMessages.ACCESS_EXCEEDED)) {
+            if (body.contains(ACCESS_EXCEEDED)) {
                 throw BankServiceError.ACCESS_EXCEEDED.exception();
             }
         }
