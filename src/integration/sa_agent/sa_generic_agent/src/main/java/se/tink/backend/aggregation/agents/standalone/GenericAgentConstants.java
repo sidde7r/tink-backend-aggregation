cc165a6c1b6da8aa8ae3e5dd34f25a0b0e9d3967
Lukasz Malek <lukasz.malek@tink.se>
2019-11-26 13:39:57 +0100

    feat(sa_agent): login

---
+++ src/integration/sa_agent/sa_generic_agent/src/main/java/se/tink/backend/aggregation/agents/standalone/GenericAgentConstants.java
@@ -0,0 +1,9 @@
+package se.tink.backend.aggregation.agents.standalone;
+
+public final class GenericAgentConstants {
+
+    public static class Grpc {
+        public static final String host = "localhost";
+        public static final int port = 8085;
+    }
+}
--- src/integration/sa_agent/sa_generic_agent/src/main/java/se/tink/sa/agent/facade/command/StandaloneAgentCommand.java
+++
@@ -1,6 +0,0 @@
-package se.tink.sa.agent.facade.command;
-
-public interface StandaloneAgentCommand<Q, S> {
-
-    S execute(Q request);
-}
