6a44b21e15ea5f26f39895a679ce8b6a85e00ae9
Gustaf Svanerud <gustaf.svanerud@tink.se>
2019-11-26 13:42:02 +0100

    fix(danskebank): handle interrupted authentication
    If an ongoing authentication attempt is interrupted by another attempt
    for the same user a previously unhandled status would be returned to
    the nemid polling.
    
    This commit introduces. The exception structure was copied from BankId.

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/thirdpartyapp/nemid/exception/NemIdException.java
@@ -0,0 +1,30 @@
+package se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.nemid.exception;
+
+import se.tink.backend.aggregation.agents.exceptions.MultiFactorAuthenticationException;
+import se.tink.backend.aggregation.agents.exceptions.errors.AgentBaseError;
+import se.tink.backend.aggregation.agents.exceptions.errors.AgentError;
+import se.tink.libraries.i18n.LocalizableKey;
+
+public class NemIdException extends MultiFactorAuthenticationException {
+
+    public NemIdException(AgentError error) {
+        super(error);
+    }
+
+    public NemIdException(AgentError error, Throwable cause) {
+        super(error, cause);
+    }
+
+    public NemIdException(AgentError error, LocalizableKey userMessage) {
+        super(error, userMessage);
+    }
+
+    public NemIdException(AgentError error, LocalizableKey userMessage, Throwable cause) {
+        super(error, userMessage, cause);
+    }
+
+    @Override
+    public AgentBaseError getError() {
+        return null;
+    }
+}
