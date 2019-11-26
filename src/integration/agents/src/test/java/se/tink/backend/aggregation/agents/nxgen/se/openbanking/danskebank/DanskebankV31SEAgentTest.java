5a2a39953b5f82fa606567fdaa02c8c6d5e1344d
Gustaf Svanerud <gustaf.svanerud@tink.se>
2019-11-26 16:33:35 +0100

    feat(danskebank): SE agent

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/danskebank/DanskebankV31SEAgentTest.java
@@ -0,0 +1,21 @@
+package se.tink.backend.aggregation.agents.nxgen.se.openbanking.danskebank;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+
+@Ignore
+public class DanskebankV31SEAgentTest {
+
+    @Test
+    public void testRefresh() throws Exception {
+        new AgentIntegrationTest.Builder("se", "se-danskebank-ob")
+                .setAppId("tink")
+                .setFinancialInstitutionId("danskebank")
+                .loadCredentialsBefore(true)
+                .saveCredentialsAfter(true)
+                .expectLoggedIn(false)
+                .build()
+                .testRefresh();
+    }
+}
