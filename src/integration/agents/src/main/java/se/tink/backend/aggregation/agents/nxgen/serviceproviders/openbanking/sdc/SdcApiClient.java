53cf015e439b2af14fcc70667bdaceb8f9afd0cd
Berk Gedik <berk.gedik@tink.se>
2019-11-28 00:13:48 +0100

    refactor(SDC): Moved the agent to serviceproviders folder

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sdc/SdcApiClient.java
@@ -0,0 +1,192 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc;
+
+import java.util.Date;
+import java.util.Optional;
+import javax.ws.rs.core.MediaType;
+import se.tink.backend.agents.rpc.Credentials;
+import se.tink.backend.agents.rpc.Field.Key;
+import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.BerlinGroupConstants;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.utils.BerlinGroupUtils;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.SdcConstants.ErrorMessages;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.SdcConstants.FormValues;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.SdcConstants.HeaderKeys;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.SdcConstants.PathParameters;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.SdcConstants.QueryKeys;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.SdcConstants.QueryValues;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.SdcConstants.StorageKeys;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.SdcConstants.Urls;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.authenticator.rpc.RefreshAccessTokenRequest;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.authenticator.rpc.TokenRequest;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.authenticator.rpc.TokenResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.configuration.SdcConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.fetcher.transactionalaccount.rpc.AccountsResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.fetcher.transactionalaccount.rpc.BalancesResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.fetcher.transactionalaccount.rpc.TransactionsResponse;
+import se.tink.backend.aggregation.configuration.EidasProxyConfiguration;
+import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
+import se.tink.backend.aggregation.nxgen.http.RequestBuilder;
+import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
+import se.tink.backend.aggregation.nxgen.http.url.URL;
+import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
+import se.tink.libraries.date.ThreadSafeDateFormat;
+
+public final class SdcApiClient {
+
+    private final TinkHttpClient client;
+    private final PersistentStorage persistentStorage;
+    private final Credentials credentials;
+    private SdcConfiguration configuration;
+
+    public SdcApiClient(
+            TinkHttpClient client, PersistentStorage persistentStorage, Credentials credentials) {
+        this.client = client;
+        this.persistentStorage = persistentStorage;
+        this.credentials = credentials;
+    }
+
+    private SdcConfiguration getConfiguration() {
+        return Optional.ofNullable(configuration)
+                .orElseThrow(() -> new IllegalStateException(ErrorMessages.MISSING_CONFIGURATION));
+    }
+
+    protected void setConfiguration(
+            SdcConfiguration configuration, EidasProxyConfiguration eidasProxyConfiguration) {
+        this.configuration = configuration;
+        this.client.setEidasProxy(eidasProxyConfiguration);
+    }
+
+    private RequestBuilder createRequest(URL url) {
+        return client.request(url)
+                .accept(MediaType.APPLICATION_JSON)
+                .type(MediaType.APPLICATION_JSON);
+    }
+
+    private RequestBuilder createRequestInSession(URL url) {
+        final OAuth2Token authToken = getTokenFromStorage();
+
+        return createRequest(url).addBearerToken(authToken);
+    }
+
+    private OAuth2Token getTokenFromStorage() {
+        return persistentStorage
+                .get(StorageKeys.OAUTH_TOKEN, OAuth2Token.class)
+                .orElseThrow(
+                        () -> new IllegalStateException(SessionError.SESSION_EXPIRED.exception()));
+    }
+
+    public URL buildAuthorizeUrl(String state) {
+        return createRequest(SdcConstants.Urls.AUTHORIZATION)
+                .queryParam(QueryKeys.SCOPE, SdcConstants.HeaderValues.SCOPE_AIS)
+                .queryParam(QueryKeys.RESPONSE_TYPE, QueryValues.CODE)
+                .queryParam(QueryKeys.REDIRECT_URI, getConfiguration().getRedirectUrl())
+                .queryParam(QueryKeys.CLIENT_ID, getConfiguration().getClientId())
+                .queryParam(QueryKeys.STATE, state)
+                .getUrl();
+    }
+
+    public OAuth2Token exchangeAuthorizationCode(String code) {
+
+        TokenRequest tokenRequest =
+                new TokenRequest(
+                        SdcConstants.FormValues.AUTHORIZATION_CODE,
+                        code,
+                        getConfiguration().getRedirectUrl(),
+                        getConfiguration().getClientId(),
+                        getConfiguration().getClientSecret(),
+                        SdcConstants.FormValues.SCOPE_AIS,
+                        credentials.getField(Key.LOGIN_INPUT),
+                        "");
+
+        return createRequest(Urls.TOKEN)
+                .header(HeaderKeys.X_REQUEST_ID, BerlinGroupUtils.getRequestId())
+                .post(TokenResponse.class, tokenRequest)
+                .toTinkToken();
+    }
+
+    public OAuth2Token refreshAccessToken(String refreshToken) {
+
+        RefreshAccessTokenRequest tokenRequest =
+                new RefreshAccessTokenRequest(
+                        FormValues.REFRESH_TOKEN,
+                        refreshToken,
+                        getConfiguration().getRedirectUrl(),
+                        getConfiguration().getClientId(),
+                        getConfiguration().getClientSecret(),
+                        SdcConstants.FormValues.SCOPE_AIS);
+
+        return createRequest(Urls.TOKEN)
+                .header(HeaderKeys.X_REQUEST_ID, BerlinGroupUtils.getRequestId())
+                .post(TokenResponse.class, tokenRequest)
+                .toTinkToken();
+    }
+
+    public AccountsResponse fetchAccounts() {
+        return createRequestInSession(Urls.ACCOUNTS)
+                .header(
+                        BerlinGroupConstants.HeaderKeys.X_REQUEST_ID,
+                        BerlinGroupUtils.getRequestId())
+                .header(BerlinGroupConstants.HeaderKeys.CONSENT_ID, BerlinGroupUtils.getRequestId())
+                .header(
+                        SdcConstants.HeaderKeys.OCP_APIM_SUBSCRIPTION_KEY,
+                        getConfiguration().getOcpApimSubscriptionKey())
+                .queryParam(BerlinGroupConstants.QueryKeys.WITH_BALANCE, String.valueOf(true))
+                .get(AccountsResponse.class);
+    }
+
+    public BalancesResponse fetchAccountBalances(String accountId) {
+        return createRequestInSession(Urls.BALANCES.parameter(PathParameters.ACCOUNT_ID, accountId))
+                .header(
+                        BerlinGroupConstants.HeaderKeys.X_REQUEST_ID,
+                        BerlinGroupUtils.getRequestId())
+                .header(BerlinGroupConstants.HeaderKeys.CONSENT_ID, BerlinGroupUtils.getRequestId())
+                .header(
+                        SdcConstants.HeaderKeys.OCP_APIM_SUBSCRIPTION_KEY,
+                        getConfiguration().getOcpApimSubscriptionKey())
+                .get(BalancesResponse.class);
+    }
+
+    public TransactionsResponse getTransactionsFor(
+            TransactionalAccount account, Date fromDate, Date toDate) {
+
+        /*
+           Currently, for a given date range, if there are more than 200 transactions, the bank
+           just returns the first 200 and ignores the rest and does not give any indicator to us
+           to make us realise the issue. Bank will make a fix for that in 2020.
+           For this reason we temporarily narrowed down the date range (from 3 months to 1 month
+           see SdcAgent class)
+
+           It is not a perfect solution as one might still have more than 200 transactions for a
+           given date period. For this reason there are two things that we can do if we ever get
+           200 transactions as a response of an API call:
+
+           1) Crash the agent so we will be sure that we will not show any corrupted data (data with
+           missing transactions) to the user
+
+           2) Divide the date range into two, recursively call getTransactionsFor for these two
+           smaller date ranges, merge the results and return
+
+           We need to decide one of these approaches and implement it here
+        */
+
+        return createRequestInSession(
+                        Urls.TRANSACTIONS.parameter(
+                                PathParameters.ACCOUNT_ID, account.getApiIdentifier()))
+                .header(
+                        BerlinGroupConstants.HeaderKeys.X_REQUEST_ID,
+                        BerlinGroupUtils.getRequestId())
+                .header(BerlinGroupConstants.HeaderKeys.CONSENT_ID, BerlinGroupUtils.getRequestId())
+                .header(
+                        SdcConstants.HeaderKeys.OCP_APIM_SUBSCRIPTION_KEY,
+                        getConfiguration().getOcpApimSubscriptionKey())
+                .queryParam(
+                        BerlinGroupConstants.QueryKeys.BOOKING_STATUS,
+                        SdcConstants.QueryValues.BOOKED)
+                .queryParam(
+                        SdcConstants.QueryKeys.DATE_FROM,
+                        ThreadSafeDateFormat.FORMATTER_DAILY.format(fromDate))
+                .queryParam(QueryKeys.DATE_TO, ThreadSafeDateFormat.FORMATTER_DAILY.format(toDate))
+                .get(TransactionsResponse.class);
+    }
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sdc/SdcConstants.java
@@ -0,0 +1,96 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc;
+
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2Constants.PersistentStorageKeys;
+import se.tink.backend.aggregation.nxgen.http.url.URL;
+
+public final class SdcConstants {
+
+    private SdcConstants() {
+        throw new AssertionError();
+    }
+
+    public static class ErrorMessages {
+        public static final String INVALID_CONFIGURATION =
+                "Invalid Configuration: %s cannot be empty or null";
+        public static final String MISSING_CONFIGURATION = "Client Configuration missing.";
+        public static final String MISSING_TOKEN = "Cannot find token.";
+    }
+
+    public static class Urls {
+
+        public static final String BASE_AUTH_URL = "https://auth.sdc.dk";
+        public static final String BASE_API_URL = "https://api-proxy.sdc.dk/api/psd2";
+
+        public static final URL AUTHORIZATION = new URL(BASE_AUTH_URL + Endpoints.AUTHORIZATION);
+        public static final URL TOKEN = new URL(BASE_AUTH_URL + Endpoints.TOKEN);
+        public static final URL ACCOUNTS = new URL(BASE_API_URL + Endpoints.ACCOUNTS);
+        public static final URL BALANCES = new URL(BASE_API_URL + Endpoints.BALANCES);
+        public static final URL TRANSACTIONS = new URL(BASE_API_URL + Endpoints.TRANSACTIONS);
+    }
+
+    private static class Endpoints {
+        public static final String AUTHORIZATION = "/Account/Login";
+        public static final String TOKEN = "/Token";
+        public static final String ACCOUNTS = "/v1/accounts";
+        public static final String BALANCES = "/v1/accounts/{account-id}/balances";
+        public static final String TRANSACTIONS = "/v1/accounts/{account-id}/transactions";
+    }
+
+    public static class PathParameters {
+        public static final String ACCOUNT_ID = "account-id";
+    }
+
+    public static class StorageKeys {
+        public static final String OAUTH_TOKEN = PersistentStorageKeys.OAUTH_2_TOKEN;
+    }
+
+    public static class QueryKeys {
+        public static final String SCOPE = "scope";
+        public static final String RESPONSE_TYPE = "response_type";
+        public static final String REDIRECT_URI = "redirect_uri";
+        public static final String CLIENT_ID = "client_id";
+        public static final String STATE = "state";
+        public static final String DATE_FROM = "dateFrom";
+        public static final String DATE_TO = "dateTo";
+    }
+
+    public static class QueryValues {
+        public static final String CODE = "code";
+        public static final String BOOKED = "booked";
+    }
+
+    public static class HeaderKeys {
+        public static final String X_REQUEST_ID = "x_request_id";
+        public static final String OCP_APIM_SUBSCRIPTION_KEY = "Ocp-Apim-Subscription-Key";
+    }
+
+    public static class FormKeys {
+        public static final String GRANT_TYPE = "grant_type";
+        public static final String CODE = "code";
+        public static final String REDIRECT_URI = "redirect_uri";
+        public static final String CLIENT_ID = "client_id";
+        public static final String CLIENT_SECRET = "client_secret";
+        public static final String SCOPE = "scope";
+        public static final String SSN = "ssn";
+        public static final String IPIDFR = "ipidfr";
+    }
+
+    public static class FormValues {
+        public static final String REFRESH_TOKEN = "refresh_token";
+        public static final String AUTHORIZATION_CODE = "authorization_code";
+        public static final String SCOPE_AIS = "psd2.aisp";
+    }
+
+    public static class HeaderValues {
+        public static final String SCOPE_AIS = "psd2.aisp";
+    }
+
+    public static class Account {
+        public static final String AVAILABLE = "available";
+        public static final String CLOSING_BOOKED = "closingBooked";
+    }
+
+    public static class Transactions {
+        public static final int MAX_TRANSACTIONS_PER_RESPONSE = 400;
+    }
+}
