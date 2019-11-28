ed8f1ac8cb3c980257525d6eb4830add64e678b1
Ahmed Hussein <ahmed.hussein@tink.se>
2019-11-28 16:09:25 +0100

    fix(lacaxia): add time out filter

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/lacaixa/LaCaixaAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/lacaixa/LaCaixaAgent.java
@@ -32,6 +32,8 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.Transac
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionPagePaginationController;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
+import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
+import se.tink.backend.aggregation.nxgen.http.filter.TimeoutFilter;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
 public class LaCaixaAgent extends NextGenerationAgent
@@ -51,6 +53,7 @@ public class LaCaixaAgent extends NextGenerationAgent
     public LaCaixaAgent(
             CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
         super(request, context, signatureKeyPair);
+        configureHttpClient(client);
         apiClient = new LaCaixaApiClient(client);
 
         LaCaixaInvestmentFetcher investmentFetcher = new LaCaixaInvestmentFetcher(apiClient);
@@ -67,6 +70,10 @@ public class LaCaixaAgent extends NextGenerationAgent
         creditCardRefreshController = constructCreditCardRefreshController();
 
         transactionalAccountRefreshController = constructTransactionalAccountRefreshController();
+    }
+
+    private void configureHttpClient(TinkHttpClient client) {
+        client.addFilter(new TimeoutFilter());
     }
 
     @Override
