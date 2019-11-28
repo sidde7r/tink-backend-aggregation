eaf825ad1f26916b8fc7a2295c22aa52f762f8e0
Lukasz Malek <lukasz.malek@tink.se>
2019-11-28 10:21:25 +0100

    feat(sa_agent):fix account/model.proto

--- src/integration/sa_agent/sa_generic_agent/src/main/java/se/tink/backend/aggregation/agents/standalone/grpc/CheckingService.java
+++ src/integration/sa_agent/sa_generic_agent/src/main/java/se/tink/backend/aggregation/agents/standalone/grpc/CheckingService.java
@@ -1,15 +1,24 @@
 package se.tink.backend.aggregation.agents.standalone.grpc;
 
 import io.grpc.ManagedChannel;
+import java.util.Calendar;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import se.tink.backend.agents.rpc.Account;
+import se.tink.backend.agents.rpc.AccountTypes;
 import se.tink.backend.aggregation.agents.FetchAccountsResponse;
 import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.account.enums.AccountExclusion;
+import se.tink.libraries.amount.ExactCurrencyAmount;
+import se.tink.sa.services.fetch.account.AccountIdentifierType;
 import se.tink.sa.services.fetch.account.FetchAccountsRequest;
 import se.tink.sa.services.fetch.account.FetchAccountsServiceGrpc;
+import se.tink.sa.services.fetch.account.TransactionalAccountType;
 
 public class CheckingService {
 
@@ -46,7 +55,75 @@ public class CheckingService {
 
     private Account mapAccount(
             final se.tink.sa.services.fetch.account.TransactionalAccount transactionalAccount) {
-        // TODO
-        return null;
+        final Account account = new Account();
+        account.setAccountNumber(transactionalAccount.getAccountNumber());
+        account.setAccountExclusion(
+                mapAccountExclusion(transactionalAccount.getAccountExclusion()));
+        account.setAvailableCredit(transactionalAccount.getAvailableCredit());
+        account.setExactAvailableCredit(
+                mapExactCurrencyAmount(transactionalAccount.getExactAvailableCredit()));
+        account.setBalance(transactionalAccount.getBalance());
+        account.setExactBalance(mapExactCurrencyAmount(transactionalAccount.getExactBalance()));
+        account.setCurrencyCode(transactionalAccount.getCurrencyCode());
+        account.setBankId(transactionalAccount.getBankId());
+        account.setCertainDate(mapFromGoogleDate(transactionalAccount.getCertainDate()));
+        account.setCredentialsId(transactionalAccount.getCredentialsId());
+        account.setExcluded(transactionalAccount.getExcluded());
+        account.setFavored(transactionalAccount.getFavored());
+        account.setId(transactionalAccount.getId());
+        account.setName(transactionalAccount.getName());
+        account.setOwnership(transactionalAccount.getOwnership());
+        account.setPayload(transactionalAccount.getPayload());
+        account.setType(mapAccountTypes(transactionalAccount.getType()));
+        account.setUserId(transactionalAccount.getUserId());
+        account.setUserModifiedExcluded(transactionalAccount.getUserModifiedExcluded());
+        account.setUserModifiedName(transactionalAccount.getUserModifiedName());
+        account.setUserModifiedType(transactionalAccount.getUserModifiedType());
+        account.setIdentifiers(mapAccountIdentifiers(transactionalAccount.getIdentifiersList()));
+
+        return account;
+    }
+
+    private AccountExclusion mapAccountExclusion(
+            final se.tink.sa.services.fetch.account.AccountExclusion accountExclusion) {
+        return AccountExclusion.values()[accountExclusion.getNumber()];
+    }
+
+    private ExactCurrencyAmount mapExactCurrencyAmount(
+            final se.tink.sa.services.fetch.account.ExactCurrencyAmount exactCurrencyAmount) {
+        return ExactCurrencyAmount.of(
+                exactCurrencyAmount.getValue(), exactCurrencyAmount.getCurrencyCode());
+    }
+
+    private Date mapFromGoogleDate(final com.google.type.Date date) {
+        final Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, date.getYear());
+        calendar.set(Calendar.MONTH, date.getMonth());
+        calendar.set(Calendar.DAY_OF_MONTH, date.getDay());
+        return calendar.getTime();
+    }
+
+    private AccountTypes mapAccountTypes(final TransactionalAccountType type) {
+        return AccountTypes.values()[type.getNumber()];
+    }
+
+    private Collection<AccountIdentifier> mapAccountIdentifiers(
+            final List<se.tink.sa.services.fetch.account.AccountIdentifier> accountIdentifiers) {
+        return Optional.ofNullable(accountIdentifiers).orElse(Collections.emptyList()).stream()
+                .map(this::mapAccountIdentifier)
+                .collect(Collectors.toList());
+    }
+
+    private AccountIdentifier mapAccountIdentifier(
+            final se.tink.sa.services.fetch.account.AccountIdentifier accountIdentifier) {
+        return AccountIdentifier.create(
+                mapAccountIdentifierType(accountIdentifier.getType()),
+                accountIdentifier.getId(),
+                accountIdentifier.getName());
+    }
+
+    private AccountIdentifier.Type mapAccountIdentifierType(
+            final AccountIdentifierType accountIdentifierType) {
+        return AccountIdentifier.Type.values()[accountIdentifierType.getNumber()];
     }
 }
