fabb5b6f58d7e3f226ebc775de110611e73bef6d
Jesús A. Álvarez <jesus.alvarez@tink.se>
2019-12-03 10:44:31 +0100

    feat(bankinter): parse investment links from global position response

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bankinter/rpc/GlobalPositionResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bankinter/rpc/GlobalPositionResponse.java
@@ -2,11 +2,15 @@ package se.tink.backend.aggregation.agents.nxgen.es.banks.bankinter.rpc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import se.tink.backend.aggregation.nxgen.http.HttpResponse;
 
 public class GlobalPositionResponse extends HtmlResponse {
+    private static final Pattern INVESTMENT_LINK_PATTERN =
+            Pattern.compile("'(/fondos/secure/fondo_inversion\\.xhtml?[^']+)'");
 
     public GlobalPositionResponse(HttpResponse response) {
         super(response);
@@ -31,5 +35,30 @@ public class GlobalPositionResponse extends HtmlResponse {
             accountIds.add(new Integer(accountId - 1));
         }
         return accountIds;
+    }
+
+    private NodeList getInvestmentNodes() {
+        return evaluateXPath(
+                "//li[@data-element='AhorroInversion>BoxFondoInversion']", NodeList.class);
+    }
+
+    public long getNumberOfInvestments() {
+        return getInvestmentNodes().getLength();
+    }
+
+    public List<String> getInvestmentLinks() {
+        ArrayList<String> links = new ArrayList<>();
+        final NodeList nodes = getInvestmentNodes();
+        for (int i = 0; i < nodes.getLength(); i += 1) {
+            // extract link from attached javascript
+            final Node node = nodes.item(i);
+            final String script =
+                    evaluateXPath(node, "script/comment()", Node.class).getTextContent();
+            final Matcher matcher = INVESTMENT_LINK_PATTERN.matcher(script);
+            if (matcher.find()) {
+                links.add(matcher.group(1));
+            }
+        }
+        return links;
     }
 }
--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bankinter/rpc/GlobalPositionResponseTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bankinter/rpc/GlobalPositionResponseTest.java
@@ -13,5 +13,13 @@ public class GlobalPositionResponseTest {
 
         assertEquals(1, response.getNumberOfAccounts());
         assertEquals(0, response.getAccountIds().get(0).intValue());
+
+        assertEquals(2, response.getNumberOfInvestments());
+        assertEquals(
+                "/fondos/secure/fondo_inversion.xhtml?INDEX_CTA=1&COD_FONDO=0097",
+                response.getInvestmentLinks().get(0));
+        assertEquals(
+                "/fondos/secure/fondo_inversion.xhtml?INDEX_CTA=2&COD_FONDO=0073",
+                response.getInvestmentLinks().get(1));
     }
 }
