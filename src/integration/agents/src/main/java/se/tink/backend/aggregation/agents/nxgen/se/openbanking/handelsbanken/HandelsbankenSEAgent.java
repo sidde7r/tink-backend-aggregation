f75640357fd8263e216b2ebb26b4c13328bd134b
Ahmed Hussein <ahmed.hussein@tink.se>
2019-12-04 16:32:08 +0100

    fix(SHB): transfer destination fetching

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/handelsbanken/HandelsbankenSEAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/handelsbanken/HandelsbankenSEAgent.java
@@ -1,31 +1,26 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.handelsbanken;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import se.tink.backend.agents.rpc.Account;
 import se.tink.backend.aggregation.agents.AgentContext;
 import se.tink.backend.aggregation.agents.FetchTransferDestinationsResponse;
 import se.tink.backend.aggregation.agents.RefreshTransferDestinationExecutor;
-import se.tink.backend.aggregation.agents.general.TransferDestinationPatternBuilder;
-import se.tink.backend.aggregation.agents.general.models.GeneralAccountEntityImpl;
-import se.tink.backend.aggregation.agents.models.TransferDestinationPattern;
 import se.tink.backend.aggregation.agents.nxgen.se.openbanking.handelsbanken.executor.payment.HandelsbankenSEPaymentExecutor;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAccountConverter;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseAgent;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.HandelsbankenBaseConstants.Market;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.authenticator.HandelsbankenBankIdAuthenticator;
+import se.tink.backend.aggregation.agents.utils.transfer.InferredTransferDestinations;
 import se.tink.backend.aggregation.configuration.SignatureKeyPair;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.bankid.BankIdAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.payment.PaymentController;
 import se.tink.libraries.account.AccountIdentifier;
-import se.tink.libraries.account.identifiers.SwedishSHBInternalIdentifier;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
 public final class HandelsbankenSEAgent extends HandelsbankenBaseAgent
@@ -87,25 +82,10 @@ public final class HandelsbankenSEAgent extends HandelsbankenBaseAgent
 
     @Override
     public FetchTransferDestinationsResponse fetchTransferDestinations(List<Account> accounts) {
-        return new FetchTransferDestinationsResponse(
-                new TransferDestinationPatternBuilder()
-                        .setTinkAccounts(accounts)
-                        .setSourceAccounts(
-                                accounts.stream()
-                                        .map(GeneralAccountEntityImpl::createFromCoreAccount)
-                                        .filter(Optional::isPresent)
-                                        .map(Optional::get)
-                                        .collect(Collectors.toList()))
-                        .setDestinationAccounts(new ArrayList<>())
-                        .addMultiMatchPattern(
-                                AccountIdentifier.Type.SE, TransferDestinationPattern.ALL)
-                        .addMultiMatchPattern(
-                                AccountIdentifier.Type.SE_BG, TransferDestinationPattern.ALL)
-                        .addMultiMatchPattern(
-                                AccountIdentifier.Type.SE_PG, TransferDestinationPattern.ALL)
-                        .matchDestinationAccountsOn(
-                                AccountIdentifier.Type.SE_SHB_INTERNAL,
-                                SwedishSHBInternalIdentifier.class)
-                        .build());
+        return InferredTransferDestinations.forPaymentAccounts(
+                accounts,
+                AccountIdentifier.Type.SE,
+                AccountIdentifier.Type.SE_PG,
+                AccountIdentifier.Type.SE_BG);
     }
 }
