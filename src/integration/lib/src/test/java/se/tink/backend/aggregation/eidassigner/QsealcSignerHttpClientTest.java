0fbb42d1eb8e18ef6a24dc185602a720d719fdc5
Hao Zhao <hao.zhao@tink.se>
2019-12-04 12:59:01 +0100

    feat(aggregation): commit missed files

--- src/integration/lib/src/test/java/se/tink/backend/aggregation/eidassigner/QsealcSignerHttpClientTest.java
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/eidassigner/QsealcSignerHttpClientTest.java
@@ -1,24 +1,21 @@
-package se.tink.backend.aggregation.QsealcSigner;
+package se.tink.backend.aggregation.eidassigner;
 
-import com.google.common.io.Files;
-import org.eclipse.jetty.http.HttpVersion;
-import org.eclipse.jetty.server.HttpConfiguration;
-import org.eclipse.jetty.server.HttpConnectionFactory;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.SecureRequestCustomizer;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.SslConnectionFactory;
 import org.eclipse.jetty.server.handler.AbstractHandler;
-import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 import org.yaml.snakeyaml.representer.Representer;
-import se.tink.backend.aggregation.agents.utils.crypto.parser.Pem;
 import se.tink.backend.aggregation.configuration.eidas.InternalEidasProxyConfiguration;
+import se.tink.libraries.simple_http_server.SimpleHTTPServer;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -27,69 +24,43 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.SecureRandom;
-import java.security.cert.Certificate;
-import java.security.cert.CertificateException;
-import java.util.Base64;
+import java.security.Security;
+import java.util.concurrent.CountDownLatch;
 
 public class QsealcSignerHttpClientTest {
+
+    private static InternalEidasProxyConfiguration configuration;
+    static SimpleHTTPServer proxyServer;
 
     @BeforeClass
     public static void startupProxyWithTestConfiguration() throws Exception {
 
-        InternalEidasProxyConfiguration configuration =
-                InternalEidasProxyConfiguration.getConfiguration(
-                        "src/tink-integration-eidas-proxy/etc/test-configuration.yaml",
-                        InternalEidasProxyConfiguration.class);
+        Security.addProvider(new BouncyCastleProvider());
 
-        Server server = new Server(new InetSocketAddress(12345));
-        ContextHandler ctx = new ContextHandler();
-        ctx.setContextPath("/");
-        ctx.setHandler(new ProxyServerHandler());
-        server.setHandler(ctx);
-        final HttpConfiguration httpConfiguration = new HttpConfiguration();
-        httpConfiguration.setSecureScheme("https");
-        httpConfiguration.setSecurePort(12443);
+        configuration =
+                getConfiguration("data/test/qsealc/test-configuration.yaml", EidasProxy.class)
+                        .toInternalConfig();
+        proxyServer = new SimpleHTTPServer(12345);
+        proxyServer.addContext("/", new ProxyServerHandler());
+        proxyServer.addContext("/test", new ProxyServerHandler());
+        proxyServer.start();
+    }
 
-
-        final SslContextFactory sslContextFactory = new SslContextFactory();
-        String keyStorePassword = generateRandomKeystorePassword();
-        KeyStore trustStore = getRootCaTrustStore();
-        KeyStore keyStore = getClientCertKeystore();
-        sslContextFactory.setKeyStore(
-                keyStore);
-        sslContextFactory.setKeyStorePassword(keyStorePassword);
-        sslContextFactory.setNeedClientAuth(true);
-        sslContextFactory.setTrustStore(trustStore);
-        sslContextFactory.setEndpointIdentificationAlgorithm(null);
-
-        final HttpConfiguration httpsConfiguration = new HttpConfiguration(httpConfiguration);
-        httpsConfiguration.addCustomizer(new SecureRequestCustomizer(false));
-        final ServerConnector httpsConnector =
-                new ServerConnector(
-                        server,
-                        new SslConnectionFactory(
-                                sslContextFactory, HttpVersion.HTTP_1_1.asString()),
-                        new HttpConnectionFactory(httpsConfiguration));
-        httpsConnector.setPort(12443);
-        server.addConnector(httpsConnector);
-        server.start();
-
-
+    @AfterClass
+    public static void stopProxy() throws Exception {
+        proxyServer.stop(new CountDownLatch(1));
     }
 
     @Test
-    public void qsealcSignerHttpClientTest() {
-
+    public void qsealcSignerHttpClientTest() throws Exception {
+        HttpClient httpClient = QsealcSignerHttpClient.getHttpClient(configuration);
+        HttpPost post = new HttpPost("http://127.0.0.1:12345/test/");
+        HttpResponse response = httpClient.execute(post);
+        Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+        Assert.assertEquals("development", configuration.getEnvironment());
     }
 
-    public static <T> T getConfiguration(String fileName, Class<T> cls) throws FileNotFoundException {
+    static <T> T getConfiguration(String fileName, Class<T> cls) throws FileNotFoundException {
         FileInputStream configFileStream = new FileInputStream(new File(fileName));
 
         Representer representer = new Representer();
@@ -99,38 +70,80 @@ public class QsealcSignerHttpClientTest {
         return cls.cast(yaml.load(configFileStream));
     }
 
-    private static KeyStore getRootCaTrustStore() throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException {
-        final Certificate certificate;
-        certificate = Pem.parseCertificate(Files.toByteArray(new File("")));
-        KeyStore keyStore = KeyStore.getInstance("JKS");
-        keyStore.load(null, "changeme".toCharArray());
-        keyStore.setCertificateEntry("changeme", certificate);
-        return keyStore;
-    }
-
-    private static KeyStore getClientCertKeystore() throws KeyStoreException, CertificateException, NoSuchAlgorithmException, IOException, NoSuchProviderException {
-            Certificate certificate = Pem.parseCertificate(Files.toByteArray(new File("")));
-            PrivateKey privateKey = Pem.parsePrivateKey(Files.toByteArray(new File("")));
-            KeyStore keyStore = KeyStore.getInstance("PKCS12", "BC");
-            keyStore.load(null, "password".toCharArray());
-            Certificate[] certificateChain = new Certificate[1];
-            certificateChain[0] = certificate;
-            keyStore.setKeyEntry("clientcert", privateKey, null, certificateChain);
-            return keyStore;
-    }
-
-    private static String generateRandomKeystorePassword() {
-        SecureRandom random = new SecureRandom();
-        byte[] bytes = new byte[20];
-        random.nextBytes(bytes);
-        return Base64.getEncoder().encodeToString(bytes);
-    }
-
     static class ProxyServerHandler extends AbstractHandler {
-
         @Override
-        public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+        public void handle(
+                String s,
+                Request request,
+                HttpServletRequest httpServletRequest,
+                HttpServletResponse httpServletResponse)
+                throws IOException, ServletException {
+            httpServletResponse.setStatus(200);
+            request.setHandled(true);
+        }
+    }
 
+    public static class EidasProxy {
+        @JsonProperty private String host;
+        @JsonProperty private String caPath;
+        @JsonProperty private String tlsCrtPath;
+        @JsonProperty private String tlsKeyPath;
+        @JsonProperty private String environment;
+        @JsonProperty private boolean localEidasDev;
+
+        public EidasProxy() {}
+
+        public InternalEidasProxyConfiguration toInternalConfig() {
+            return new InternalEidasProxyConfiguration(
+                    host, caPath, tlsCrtPath, tlsKeyPath, environment, localEidasDev);
+        }
+
+        public String getHost() {
+            return host;
+        }
+
+        public String getCaPath() {
+            return caPath;
+        }
+
+        public String getTlsCrtPath() {
+            return tlsCrtPath;
+        }
+
+        public String getTlsKeyPath() {
+            return tlsKeyPath;
+        }
+
+        public String getEnvironment() {
+            return environment;
+        }
+
+        public boolean isLocalEidasDev() {
+            return localEidasDev;
+        }
+
+        public void setHost(String host) {
+            this.host = host;
+        }
+
+        public void setCaPath(String caPath) {
+            this.caPath = caPath;
+        }
+
+        public void setTlsCrtPath(String tlsCrtPath) {
+            this.tlsCrtPath = tlsCrtPath;
+        }
+
+        public void setTlsKeyPath(String tlsKeyPath) {
+            this.tlsKeyPath = tlsKeyPath;
+        }
+
+        public void setEnvironment(String environment) {
+            this.environment = environment;
+        }
+
+        public void setLocalEidasDev(boolean localEidasDev) {
+            this.localEidasDev = localEidasDev;
         }
     }
 }
