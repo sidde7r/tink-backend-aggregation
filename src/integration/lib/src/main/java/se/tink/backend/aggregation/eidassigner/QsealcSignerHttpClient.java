6efb51c058e7b8731f9ee031eb00eb13033e94f6
Hao Zhao <hao.zhao@tink.se>
2019-12-05 13:32:10 +0100

    fix(aggregation): config qsealc signer httpclient

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/eidassigner/QsealcSignerHttpClient.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/eidassigner/QsealcSignerHttpClient.java
@@ -1,15 +1,22 @@
 package se.tink.backend.aggregation.eidassigner;
 
-import java.security.KeyStore;
-import java.util.concurrent.TimeUnit;
-import javax.net.ssl.SSLContext;
+import org.apache.http.HttpHost;
 import org.apache.http.client.HttpClient;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.ConnectionKeepAliveStrategy;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
 import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.SSLContextBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import se.tink.backend.aggregation.configuration.eidas.InternalEidasProxyConfiguration;
 import se.tink.backend.aggregation.nxgen.http.truststrategy.TrustRootCaStrategy;
+
+import javax.net.ssl.SSLContext;
+import java.security.KeyStore;
 
 public class QsealcSignerHttpClient {
     private static HttpClient httpClient;
@@ -27,13 +34,25 @@ public class QsealcSignerHttpClient {
                                 .loadKeyMaterial(keyStore, "changeme".toCharArray())
                                 .build();
 
+                SSLConnectionSocketFactory sslsf =
+                        new SSLConnectionSocketFactory(sslContext, new AllowAllHostnameVerifier());
+
+                PoolingHttpClientConnectionManager connectionManager =
+                        new PoolingHttpClientConnectionManager(
+                                RegistryBuilder.<ConnectionSocketFactory>create()
+                                        .register(
+                                                "http",
+                                                PlainConnectionSocketFactory.getSocketFactory())
+                                        .register("https", sslsf)
+                                        .build());
+                connectionManager.setDefaultMaxPerRoute(4);
+                connectionManager.setMaxPerRoute(new HttpRoute(new HttpHost(conf.getHost())), 10);
+
+                ConnectionKeepAliveStrategy ttl = (r, c) -> 60 * 1000;
                 httpClient =
                         HttpClients.custom()
-                                .setHostnameVerifier(new AllowAllHostnameVerifier())
-                                .setSslcontext(sslContext)
-                                .setConnectionManager(
-                                        new PoolingHttpClientConnectionManager(
-                                                60, TimeUnit.SECONDS))
+                                .setConnectionManager(connectionManager)
+                                .setKeepAliveStrategy(ttl)
                                 .build();
             } catch (Exception e) {
                 throw new IllegalStateException(e);
