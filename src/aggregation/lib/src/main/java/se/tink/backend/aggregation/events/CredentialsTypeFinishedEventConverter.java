97cfc7620df46bd5ac885c627706222a3a01a87c
Lars Almgren <lars.almgren@tink.se>
2019-12-05 10:27:10 +0100

    fix(Events): use correct import and format converters

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/CredentialsTypeFinishedEventConverter.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/CredentialsTypeFinishedEventConverter.java
@@ -1,20 +1,32 @@
 package se.tink.backend.aggregation.events;
 
 import com.google.common.collect.ImmutableMap;
-import se.tink.backend.nasa.boot.rpc.CredentialsType;
 import se.tink.eventproducerservice.events.grpc.CredentialsRefreshCommandChainFinishedProto.CredentialsRefreshCommandChainFinished.CredentialsTypes;
 
 public class CredentialsTypeFinishedEventConverter {
-    private static final ImmutableMap<CredentialsType, CredentialsTypes> CREDENTIALS_TYPE_FINISHED_EVENT_MAP =
-            ImmutableMap.<CredentialsType, CredentialsTypes>builder()
-                    .put(CredentialsType.FRAUD, CredentialsTypes.CREDENTIAL_TYPES_FRAUD)
-                    .put(CredentialsType.KEYFOB, CredentialsTypes.CREDENTIAL_TYPES_KEYFOB)
-                    .put(CredentialsType.MOBILE_BANKID, CredentialsTypes.CREDENTIAL_TYPES_MOBILE_BANKID)
-                    .put(CredentialsType.PASSWORD, CredentialsTypes.CREDENTIAL_TYPES_PASSWORD)
-                    .put(CredentialsType.THIRD_PARTY_APP, CredentialsTypes.CREDENTIAL_TYPES_THIRD_PARTY_APP)
-                    .build();
+    private static final ImmutableMap<se.tink.backend.agents.rpc.CredentialsTypes, CredentialsTypes>
+            CREDENTIALS_TYPE_FINISHED_EVENT_MAP =
+                    ImmutableMap
+                            .<se.tink.backend.agents.rpc.CredentialsTypes, CredentialsTypes>
+                                    builder()
+                            .put(
+                                    se.tink.backend.agents.rpc.CredentialsTypes.FRAUD,
+                                    CredentialsTypes.CREDENTIAL_TYPES_FRAUD)
+                            .put(
+                                    se.tink.backend.agents.rpc.CredentialsTypes.KEYFOB,
+                                    CredentialsTypes.CREDENTIAL_TYPES_KEYFOB)
+                            .put(
+                                    se.tink.backend.agents.rpc.CredentialsTypes.MOBILE_BANKID,
+                                    CredentialsTypes.CREDENTIAL_TYPES_MOBILE_BANKID)
+                            .put(
+                                    se.tink.backend.agents.rpc.CredentialsTypes.PASSWORD,
+                                    CredentialsTypes.CREDENTIAL_TYPES_PASSWORD)
+                            .put(
+                                    se.tink.backend.agents.rpc.CredentialsTypes.THIRD_PARTY_APP,
+                                    CredentialsTypes.CREDENTIAL_TYPES_THIRD_PARTY_APP)
+                            .build();
 
-    public static CredentialsTypes convert(CredentialsType type) {
+    public static CredentialsTypes convert(se.tink.backend.agents.rpc.CredentialsTypes type) {
         return CREDENTIALS_TYPE_FINISHED_EVENT_MAP.getOrDefault(
                 type, CredentialsTypes.CREDENTIAL_TYPES_UNKNOWN);
     }
--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/RefreshableItemsConverter.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/RefreshableItemsConverter.java
@@ -7,25 +7,51 @@ import se.tink.libraries.credentials.service.RefreshableItem;
 public class RefreshableItemsConverter {
     private static final ImmutableMap<RefreshableItem, RefreshableItems> REFRESHABLE_ITEMS_MAP =
             ImmutableMap.<RefreshableItem, RefreshableItems>builder()
-                    .put(RefreshableItem.CHECKING_ACCOUNTS, RefreshableItems.REFRESHABLE_ITEMS_CHECKING_ACCOUNTS)
-                    .put(RefreshableItem.CHECKING_TRANSACTIONS, RefreshableItems.REFRESHABLE_ITEMS_CHECKING_TRANSACTIONS)
-                    .put(RefreshableItem.CREDITCARD_ACCOUNTS, RefreshableItems.REFRESHABLE_ITEMS_CREDITCARD_ACCOUNTS)
-                    .put(RefreshableItem.CREDITCARD_TRANSACTIONS, RefreshableItems.REFRESHABLE_ITEMS_CREDITCARD_TRANSACTIONS)
-                    .put(RefreshableItem.IDENTITY_DATA, RefreshableItems.REFRESHABLE_ITEMS_IDENTITY_DATA)
-                    .put(RefreshableItem.INVESTMENT_ACCOUNTS, RefreshableItems.REFRESHABLE_ITEMS_INVESTMENT_ACCOUNTS)
-                    .put(RefreshableItem.INVESTMENT_TRANSACTIONS, RefreshableItems.REFRESHABLE_ITEMS_INVESTMENT_TRANSACTIONS)
-                    .put(RefreshableItem.LOAN_ACCOUNTS, RefreshableItems.REFRESHABLE_ITEMS_LOAN_ACCOUNTS)
-                    .put(RefreshableItem.LOAN_TRANSACTIONS, RefreshableItems.REFRESHABLE_ITEMS_LOAN_TRANSACTIONS)
-                    .put(RefreshableItem.SAVING_ACCOUNTS, RefreshableItems.REFRESHABLE_ITEMS_SAVING_ACCOUNTS)
-                    .put(RefreshableItem.SAVING_TRANSACTIONS, RefreshableItems.REFRESHABLE_ITEMS_SAVING_TRANSACTIONS)
+                    .put(
+                            RefreshableItem.CHECKING_ACCOUNTS,
+                            RefreshableItems.REFRESHABLE_ITEMS_CHECKING_ACCOUNTS)
+                    .put(
+                            RefreshableItem.CHECKING_TRANSACTIONS,
+                            RefreshableItems.REFRESHABLE_ITEMS_CHECKING_TRANSACTIONS)
+                    .put(
+                            RefreshableItem.CREDITCARD_ACCOUNTS,
+                            RefreshableItems.REFRESHABLE_ITEMS_CREDITCARD_ACCOUNTS)
+                    .put(
+                            RefreshableItem.CREDITCARD_TRANSACTIONS,
+                            RefreshableItems.REFRESHABLE_ITEMS_CREDITCARD_TRANSACTIONS)
+                    .put(
+                            RefreshableItem.IDENTITY_DATA,
+                            RefreshableItems.REFRESHABLE_ITEMS_IDENTITY_DATA)
+                    .put(
+                            RefreshableItem.INVESTMENT_ACCOUNTS,
+                            RefreshableItems.REFRESHABLE_ITEMS_INVESTMENT_ACCOUNTS)
+                    .put(
+                            RefreshableItem.INVESTMENT_TRANSACTIONS,
+                            RefreshableItems.REFRESHABLE_ITEMS_INVESTMENT_TRANSACTIONS)
+                    .put(
+                            RefreshableItem.LOAN_ACCOUNTS,
+                            RefreshableItems.REFRESHABLE_ITEMS_LOAN_ACCOUNTS)
+                    .put(
+                            RefreshableItem.LOAN_TRANSACTIONS,
+                            RefreshableItems.REFRESHABLE_ITEMS_LOAN_TRANSACTIONS)
+                    .put(
+                            RefreshableItem.SAVING_ACCOUNTS,
+                            RefreshableItems.REFRESHABLE_ITEMS_SAVING_ACCOUNTS)
+                    .put(
+                            RefreshableItem.SAVING_TRANSACTIONS,
+                            RefreshableItems.REFRESHABLE_ITEMS_SAVING_TRANSACTIONS)
                     .put(RefreshableItem.ACCOUNTS, RefreshableItems.REFRESHABLE_ITEMS_ACCOUNTS)
                     .put(RefreshableItem.EINVOICES, RefreshableItems.REFRESHABLE_ITEMS_EINVOICES)
-                    .put(RefreshableItem.TRANSFER_DESTINATIONS, RefreshableItems.REFRESHABLE_ITEMS_TRANSFER_DESTINATIONS)
-                    .put(RefreshableItem.TRANSACTIONAL_ACCOUNTS_AND_TRANSACTIONS, RefreshableItems.REFRESHABLE_ITEMS_TRANSACTIONAL_ACCOUNTS_AND_TRANSACTIONS)
+                    .put(
+                            RefreshableItem.TRANSFER_DESTINATIONS,
+                            RefreshableItems.REFRESHABLE_ITEMS_TRANSFER_DESTINATIONS)
+                    .put(
+                            RefreshableItem.TRANSACTIONAL_ACCOUNTS_AND_TRANSACTIONS,
+                            RefreshableItems
+                                    .REFRESHABLE_ITEMS_TRANSACTIONAL_ACCOUNTS_AND_TRANSACTIONS)
                     .build();
 
     public static RefreshableItems convert(RefreshableItem item) {
-        return REFRESHABLE_ITEMS_MAP.getOrDefault(
-                item, RefreshableItems.REFRESHABLE_ITEMS_UNKNOWN);
+        return REFRESHABLE_ITEMS_MAP.getOrDefault(item, RefreshableItems.REFRESHABLE_ITEMS_UNKNOWN);
     }
 }
