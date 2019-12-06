f363c771f0a8ae2b05dae2ddf302e5b5612835d7
Lukasz Malek <lukasz.malek@tink.se>
2019-12-06 13:37:28 +0100

    feat(sa_agent): wip - fix authentication

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/step/ThirdPartyAppAuthenticationStep.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/step/ThirdPartyAppAuthenticationStep.java
@@ -33,7 +33,9 @@ public class ThirdPartyAppAuthenticationStep implements AuthenticationStep {
     @Override
     public Optional<SupplementInformationRequester> execute(AuthenticationRequest request)
             throws AuthenticationException, AuthorizationException {
-        if (request.getCallbackData().isEmpty()) {
+        if (request == null
+                || request.getCallbackData() == null
+                || request.getCallbackData().isEmpty()) {
             return Optional.of(
                     new SupplementInformationRequester.Builder()
                             .withThirdPartyAppAuthenticationPayload(payload)
