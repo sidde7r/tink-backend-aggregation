cc59e0ce50c6a0d2745e1d7521008669438017b6
Lukasz Malek <lukasz.malek@tink.se>
2019-12-09 08:05:18 +0100

    feat(sa_agent): small fixes

--- src/integration/sa_agent/sa_generic_agent/src/test/java/se/tink/sa/agent/GenericAgentTest.java
+++ src/integration/sa_agent/sa_generic_agent/src/test/java/se/tink/sa/agent/GenericAgentTest.java
@@ -1,10 +1,11 @@
 package se.tink.sa.agent;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
-// @Ignore
+@Ignore
 public class GenericAgentTest {
 
     private AgentIntegrationTest.Builder builder;
