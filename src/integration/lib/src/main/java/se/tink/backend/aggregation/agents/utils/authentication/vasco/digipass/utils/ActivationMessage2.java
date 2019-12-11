ce41310fe558817277edb21cb55b8f6c9a665c97
Jonas Renold <jonas.renold@tink.se>
2019-12-11 14:30:57 +0100

    feat(digipass): activationMessage2 utils
    Collection of code to work with the activationMessage2.

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/authentication/vasco/digipass/utils/ActivationMessage2.java
@@ -0,0 +1,32 @@
+package se.tink.backend.aggregation.agents.utils.authentication.vasco.digipass.utils;
+
+import com.google.common.primitives.Bytes;
+import java.util.Arrays;
+import se.tink.backend.aggregation.agents.utils.crypto.AES;
+
+// The message in the protocol is called `activationMessage2`, i.e. this is not
+// the second iteration of a `ActivationMessage` utils class.
+public class ActivationMessage2 {
+    public static byte[] decrypt(byte[] key, byte[] activationMessage2) {
+        // 39 bytes of data
+        byte[] data = Arrays.copyOfRange(activationMessage2, 15, 15 + 39);
+
+        // 8 bytes from activationMessage2 followed by 0 zeroes
+        byte[] ctr = Bytes.concat(Arrays.copyOfRange(activationMessage2, 7, 7 + 8), new byte[8]);
+        return AES.decryptCtr(key, ctr, data);
+    }
+
+    public static int extractDeviceCount(byte[] decryptedActivationMessage2) {
+        return (int) decryptedActivationMessage2[5];
+    }
+
+    public static byte[] decryptKey1(byte[] key, byte[] decryptedActivationMessage2) {
+        byte[] data = Arrays.copyOfRange(decryptedActivationMessage2, 6, 6 + 16);
+        return AES.decryptEcbNoPadding(key, data);
+    }
+
+    public static byte[] decryptKey2(byte[] key, byte[] decryptedActivationMessage2) {
+        byte[] data = Arrays.copyOfRange(decryptedActivationMessage2, 6 + 16 + 1, 6 + 16 + 1 + 16);
+        return AES.decryptEcbNoPadding(key, data);
+    }
+}
