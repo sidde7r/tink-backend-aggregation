4bcd34f46b6efdf2048e50efce01e416659630e7
Gustaf Svanerud <gustaf.svanerud@tink.se>
2019-12-11 15:27:39 +0100

    feat(AutoAuthenticator): allow AuthenticationException
    Widen `SessionException` to `AuthenticationException`.
    
    Many bank endpoints that we use for automatic authentication can throw
    incorrect credential errors. There is no real reason why
    AutoAuthenticator should not be able to throw these as well.

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/automatic/AutoAuthenticator.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/automatic/AutoAuthenticator.java
@@ -2,8 +2,10 @@ package se.tink.backend.aggregation.nxgen.controllers.authentication.automatic;
 
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
 import se.tink.backend.aggregation.agents.exceptions.BankServiceException;
+import se.tink.backend.aggregation.agents.exceptions.LoginException;
 import se.tink.backend.aggregation.agents.exceptions.SessionException;
 
 public interface AutoAuthenticator {
-    void autoAuthenticate() throws SessionException, BankServiceException, AuthorizationException;
+    void autoAuthenticate()
+            throws SessionException, LoginException, BankServiceException, AuthorizationException;
 }
