032cb2b5cffc829dc0873300e4c847e04fafdb60
Jonas Renold <jonas.renold@tink.se>
2019-12-11 13:32:15 +0100

    feat(digipass): dynamicVector utils
    Collection of code to work with the dynamic vector (also known as activationMessage).

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/authentication/vasco/digipass/utils/DynamicVector.java
@@ -0,0 +1,22 @@
+package se.tink.backend.aggregation.agents.utils.authentication.vasco.digipass.utils;
+
+import java.util.Arrays;
+import se.tink.backend.aggregation.agents.utils.crypto.AES;
+
+// The message in the protocol is called `activationMessage`, however the code references to this as
+// DynamicVector.
+public class DynamicVector {
+    public static String calculateLogonId(byte[] dynamicVector) {
+        long output = 0;
+        for (int shift = 0, off = 4; shift != -24; shift -= 8) {
+            long v = dynamicVector[off++] & 0xff;
+            output += (v << (shift + 16));
+        }
+        return String.format("%07d", output & 0xffffffffL);
+    }
+
+    public static byte[] decryptDynamicVectorKey(byte[] key, byte[] dynamicVector) {
+        byte[] data = Arrays.copyOfRange(dynamicVector, 32, 32 + 16);
+        return AES.decryptEcbNoPadding(key, data);
+    }
+}
---
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/utils/authentication/vasco/digipass/utils/DynamicVectorTest.java
@@ -0,0 +1,30 @@
+package se.tink.backend.aggregation.agents.utils.authentication.vasco.digipass.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.utils.encoding.EncodingUtils;
+
+public class DynamicVectorTest {
+    private static final String DYNAMIC_VECTOR_AS_HEX =
+            "00005FDF47AA6B0000000000000000004D01F2BD471260C4201A7126BADF62726786D8FB49E2D82D6CB051E572088035641ACFB6DA59";
+
+    @Test
+    public void testCalculateLogonId() {
+        byte[] dynamicVector = EncodingUtils.decodeHexString(DYNAMIC_VECTOR_AS_HEX);
+
+        String logonId = DynamicVector.calculateLogonId(dynamicVector);
+        Assert.assertEquals("LogonId mismatch.", "4696683", logonId);
+    }
+
+    @Test
+    public void testDecryptDynamicVectorKey() {
+        byte[] key = EncodingUtils.decodeHexString("f3ff3eaf401cf61bfd1a531619956d5d");
+        byte[] dynamicVector = EncodingUtils.decodeHexString(DYNAMIC_VECTOR_AS_HEX);
+
+        byte[] dynamicVectorKey = DynamicVector.decryptDynamicVectorKey(key, dynamicVector);
+        Assert.assertEquals(
+                "DynamicVectorKey mismatch.",
+                "e61bf5f0afc4e89536b0fb07bf3b788c",
+                EncodingUtils.encodeHexAsString(dynamicVectorKey));
+    }
+}
