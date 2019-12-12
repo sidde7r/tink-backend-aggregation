315446cf39a4b8c292eed527d7fbae5e83374b20
Nazri Abdullah <nazri.abdullah@tink.se>
2019-12-12 20:56:58 +0100

    refactor(at/raiffeisen): changed entity from library

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/raiffeisen/executor/payment/entities/HrefEntity.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.at.openbanking.raiffeisen.executor.payment.entities;
-
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class HrefEntity {
-    private String href;
-}
