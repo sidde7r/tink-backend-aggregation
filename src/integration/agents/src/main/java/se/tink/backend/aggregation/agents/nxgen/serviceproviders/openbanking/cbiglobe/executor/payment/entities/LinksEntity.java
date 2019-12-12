93e8625d092eab011c1c4a1343a71941a25e9545
Nazri Abdullah <nazri.abdullah@tink.se>
2019-12-12 21:26:27 +0100

    refactor(serviceprovider/cbiglobe): updated entity from library

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/executor/payment/entities/LinksEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/executor/payment/entities/LinksEntity.java
@@ -1,14 +1,15 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.executor.payment.entities;
 
+import se.tink.backend.aggregation.agents.Href;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
 public class LinksEntity {
 
-    private HrefEntity self;
-    private HrefEntity updatePsuAuthenticationRedirect;
+    private Href self;
+    private Href updatePsuAuthenticationRedirect;
 
-    public HrefEntity getUpdatePsuAuthenticationRedirect() {
+    public Href getUpdatePsuAuthenticationRedirect() {
         return updatePsuAuthenticationRedirect;
     }
 }
