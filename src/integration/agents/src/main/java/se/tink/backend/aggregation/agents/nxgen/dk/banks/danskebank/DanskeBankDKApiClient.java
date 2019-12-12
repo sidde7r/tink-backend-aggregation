2af889ab0dc77d35e2503c4918129967bb40d4ea
Alexander Jamm <alexander.jamm@tink.se>
2019-12-12 16:22:57 +0100

    fix(DanskebankDK): throw exception when clientSecret changed

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/danskebank/DanskeBankDKApiClient.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/danskebank/DanskeBankDKApiClient.java
@@ -1,11 +1,29 @@
 package se.tink.backend.aggregation.agents.nxgen.dk.banks.danskebank;
 
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.DanskeBankApiClient;
+import se.tink.backend.aggregation.nxgen.http.HttpResponse;
 import se.tink.backend.aggregation.nxgen.http.TinkHttpClient;
+import se.tink.backend.aggregation.nxgen.http.exceptions.HttpResponseException;
 
 public class DanskeBankDKApiClient extends DanskeBankApiClient {
     protected DanskeBankDKApiClient(
             TinkHttpClient client, DanskeBankDKConfiguration configuration) {
         super(client, configuration);
+    }
+
+    @Override
+    public HttpResponse collectDynamicLogonJavascript(String securitySystem, String brand) {
+        try {
+            return super.collectDynamicLogonJavascript(securitySystem, brand);
+        } catch (HttpResponseException e) {
+            HttpResponse response = e.getResponse();
+
+            if (response.getStatus() == 412
+                    && response.getBody(String.class).equalsIgnoreCase("Unauthorized client")) {
+                throw new IllegalStateException(
+                        "Check if DanskebankDK has rotated their clientSecret", e);
+            }
+            throw e;
+        }
     }
 }
