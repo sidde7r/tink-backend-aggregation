7b6d0f1c2bf8dc1eb4dae1968fbc55601430dd8d
Mattias Hird <mattias.hird@tink.se>
2019-12-12 13:55:56 +0100

    chore(avanza): Call before on arg manager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/brokers/avanza/AvanzaAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/brokers/avanza/AvanzaAgentTest.java
@@ -6,31 +6,10 @@ import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager;
-import se.tink.backend.aggregation.agents.framework.ArgumentManager.ArgumentManagerEnum;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager.UsernameArgumentEnum;
 import se.tink.libraries.credentials.service.RefreshableItem;
 
 public class AvanzaAgentTest {
-
-    private enum Arg implements ArgumentManagerEnum {
-        USERNAME; // 12 digit SSN
-
-        private final boolean optional;
-
-        Arg(boolean optional) {
-            this.optional = optional;
-        }
-
-        Arg() {
-            this.optional = false;
-        }
-
-        @Override
-        public boolean isOptional() {
-            return optional;
-        }
-    }
-
     private final ArgumentManager<UsernameArgumentEnum> manager =
             new ArgumentManager<>(UsernameArgumentEnum.values());
 
