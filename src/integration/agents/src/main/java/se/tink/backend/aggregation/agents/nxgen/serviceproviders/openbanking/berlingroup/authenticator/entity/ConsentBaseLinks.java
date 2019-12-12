5589296b541fb7879cb21daf99d6cf1c23c0c733
Nazri Abdullah <nazri.abdullah@tink.se>
2019-12-12 20:42:38 +0100

    refactor(berligroup): updated reference from common library

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/berlingroup/authenticator/entity/ConsentBaseLinks.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/berlingroup/authenticator/entity/ConsentBaseLinks.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.authenticator.entity;
 
+import se.tink.backend.aggregation.agents.Href;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
