9de53ba238b9f490103435d451229ca8f3a63c51
Nazri Abdullah <nazri.abdullah@tink.se>
2019-12-12 21:16:59 +0100

    refactor(se/icabanken): updated entity from library

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/icabanken/executor/payment/entities/HrefEntity.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.se.openbanking.icabanken.executor.payment.entities;
-
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class HrefEntity {
-    private String href;
-}
