a202b645b77440e29c6172d49a1317fc5edfba08
Nazri Abdullah <nazri.abdullah@tink.se>
2019-12-13 19:00:23 +0100

    feat(lib/integration): added custom date formatting used in psd2 agents

---
+++ src/libraries/date/src/main/java/se/tink/libraries/date/DateFormat.java
@@ -0,0 +1,57 @@
+package se.tink.libraries.date;
+
+import com.google.common.base.Strings;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public final class DateFormat {
+
+    public static final String UTC = "UTC";
+    public static final String YEAR_MONTH_DAY = "yyyy-MM-dd";
+
+    private DateFormat() {
+        throw new AssertionError();
+    }
+
+    public static String formatDate(
+            final Date date, final String dateFormat, final String timeZone) {
+        final SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+
+        if (!Strings.isNullOrEmpty(timeZone)) {
+            sdf.setTimeZone(TimeZone.getTimeZone(timeZone));
+        }
+
+        return sdf.format(date);
+    }
+
+    public static String formatDate(final Date date, final String timestampFormat) {
+        final SimpleDateFormat sdf = new SimpleDateFormat(timestampFormat);
+        return sdf.format(date);
+    }
+
+    public static String formatDateTime(
+            final Date date, final String timestampFormat, final String timezone) {
+        final SimpleDateFormat sdf = new SimpleDateFormat(timestampFormat);
+        if (!Strings.isNullOrEmpty(timezone)) {
+            sdf.setTimeZone(TimeZone.getTimeZone(timezone));
+        }
+        return sdf.format(date);
+    }
+
+    public static String getFormattedCurrentDate(final String format, final String timeZone) {
+        return formatDate(Calendar.getInstance().getTime(), format, timeZone);
+    }
+
+    public static Date convertToDateViaInstant(LocalDate dateToConvert) {
+        return java.util.Date.from(
+                dateToConvert.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static LocalDate convertToLocalDateViaInstant(Date dateToConvert) {
+        return dateToConvert.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    }
+}
