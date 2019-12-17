fd34d2c7a485f36461e8ebc813f84fc37c1438aa
Vibhu Srivastava <vibhu.srivastava@tink.com>
2019-12-17 11:22:53 +0100

    refactor(de-fidor): Add HrefEntity

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/fidor/authenticator/entities/ConsentBaseLinksEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/fidor/authenticator/entities/ConsentBaseLinksEntity.java
@@ -2,7 +2,6 @@ package se.tink.backend.aggregation.agents.nxgen.de.openbanking.fidor.authentica
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.deutschebank.authenticator.entities.HrefEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
