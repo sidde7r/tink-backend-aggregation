fda4ba8315beef0a6943eb6581e20b16fa1c5c06
Jan Ossowski <jan.ossowski@tink.se>
2019-12-18 17:21:44 +0100

    chore(crypto): add MGF1 and TOTP generation

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/crypto/TOTP.java
@@ -0,0 +1,36 @@
+package se.tink.backend.aggregation.agents.utils.crypto;
+
+import se.tink.backend.aggregation.agents.utils.encoding.EncodingUtils;
+
+public class TOTP {
+
+    private static final int[] DIGITS_POWER
+            // 0  1   2    3     4      5       6        7         8
+            = {1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000};
+
+    public static String generateTotpIgnoreLast4bitsHmacSha1(
+            String hexKey, long epochS, int returnDigits) {
+        StringBuilder result = null;
+        byte[] hash;
+        epochS <<= 4;
+        byte[] keyBytes = EncodingUtils.decodeHexString(hexKey);
+        StringBuilder timeKey = new StringBuilder(Long.toHexString(epochS));
+        timeKey = new StringBuilder(timeKey.substring(0, timeKey.length() - 2));
+        while (timeKey.length() < 16) timeKey.insert(0, "0");
+        byte[] msg = EncodingUtils.decodeHexString(timeKey.toString());
+        hash = Hash.hmacSha1(keyBytes, msg);
+        int offset = hash[hash.length - 1] & 0xf;
+
+        int binary =
+                ((hash[offset] & 0x7f) << 24)
+                        | ((hash[offset + 1] & 0xff) << 16)
+                        | ((hash[offset + 2] & 0xff) << 8)
+                        | (hash[offset + 3] & 0xff);
+        int otp = binary % DIGITS_POWER[returnDigits];
+        result = new StringBuilder(Integer.toString(otp));
+        while (result.length() < returnDigits) {
+            result.insert(0, "0");
+        }
+        return result.toString();
+    }
+}
---
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/utils/crypto/MGF1Test.java
@@ -0,0 +1,20 @@
+package se.tink.backend.aggregation.agents.utils.crypto;
+
+import org.junit.Assert;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.utils.encoding.EncodingUtils;
+
+public class MGF1Test {
+
+    private static final String MASK = "c8efc25c440ab641132265749b539ebdbbda76c1";
+    private static final String SEED = "13579";
+
+    private static final String EXPECTED = "AtBBM27yvFkmhMJh7gC0I77zZtE=";
+
+    @Test
+    public void testSha1Mask() {
+        byte[] maskedSeed = MGF1.generateMaskSHA1(SEED.getBytes(), MASK.length() / 2);
+
+        Assert.assertArrayEquals(EncodingUtils.decodeBase64String(EXPECTED), maskedSeed);
+    }
+}
