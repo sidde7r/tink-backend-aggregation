06858121b82c26f91e248c9d535d3929152a6190
Nazri Abdullah <nazri.abdullah@tink.se>
2019-12-19 22:40:45 +0100

    refactor(at/bawag): replaced with self-contained entity class

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/bawag/authenticator/entity/ConsentBaseLinks.java
@@ -0,0 +1,25 @@
+package se.tink.backend.aggregation.agents.nxgen.at.openbanking.bawag.authenticator.entity;
+
+import se.tink.backend.aggregation.agents.Href;
+import se.tink.backend.aggregation.annotations.JsonObject;
+
+@JsonObject
+public class ConsentBaseLinks {
+    private Href scaRedirect;
+    private Href status;
+    private Href scaStatus;
+
+    public ConsentBaseLinks() {}
+
+    public Href getScaRedirect() {
+        return scaRedirect;
+    }
+
+    public Href getStatus() {
+        return status;
+    }
+
+    public Href getScaStatus() {
+        return scaStatus;
+    }
+}
