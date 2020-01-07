61bfa9ec9542cef02049a748ad25d4221c98b475
Sebastian Olsson <sebastian.olsson@tink.se>
2020-01-07 20:31:43 +0100

    build(filters): Moved filter to subdirectory

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/creditcards/ICS/ICSAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/creditcards/ICS/ICSAgent.java
@@ -19,9 +19,9 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.Transac
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginationController;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
 import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
-import se.tink.backend.aggregation.nxgen.http.filter.AccessExceededFilter;
 import se.tink.backend.aggregation.nxgen.http.filter.BankServiceInternalErrorFilter;
 import se.tink.backend.aggregation.nxgen.http.filter.RateLimitRetryFilter;
+import se.tink.backend.aggregation.nxgen.http.filter.filters.AccessExceededFilter;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
 public class ICSAgent extends NextGenerationAgent implements RefreshCreditCardAccountsExecutor {
