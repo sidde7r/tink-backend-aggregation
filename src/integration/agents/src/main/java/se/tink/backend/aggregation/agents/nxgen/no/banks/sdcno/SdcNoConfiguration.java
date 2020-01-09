1ef8d732f6482280924b3f6230f7827e68613b52
Piotr Miecielica <piotr.miecielica@tink.se>
2020-01-09 16:06:41 +0100

    refactor(no/sdcno): Add tiny improvements

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/sdcno/SdcNoConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/sdcno/SdcNoConfiguration.java
@@ -1,6 +1,10 @@
 package se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno;
 
+import static se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.SdcNoConstants.Market.EIKA_BANKS;
+
 import se.tink.backend.agents.rpc.Provider;
+import se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.SdcNoConstants.Logging;
+import se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.SdcNoConstants.Market;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcConfiguration;
 import se.tink.backend.aggregation.agents.utils.log.LogTag;
 
@@ -8,10 +12,10 @@ public class SdcNoConfiguration extends SdcConfiguration {
 
     public SdcNoConfiguration(Provider provider) {
         super(provider);
-        if (SdcNoConstants.Market.EIKA_BANKS.contains(provider.getPayload())) {
-            baseUrl = SdcNoConstants.Market.EIKA_BASE_URL + bankCode + "/";
+        if (EIKA_BANKS.contains(provider.getPayload())) {
+            baseUrl = Market.EIKA_BASE_URL + bankCode + "/";
         } else {
-            baseUrl = SdcNoConstants.Market.BASE_URL + bankCode + "/";
+            baseUrl = Market.BASE_URL + bankCode + "/";
         }
     }
 
@@ -22,16 +26,16 @@ public class SdcNoConfiguration extends SdcConfiguration {
 
     @Override
     public String getPhoneCountryCode() {
-        return SdcNoConstants.Market.PHONE_COUNTRY_CODE;
+        return Market.PHONE_COUNTRY_CODE;
     }
 
     @Override
     public LogTag getLoanLogTag() {
-        return SdcNoConstants.Fetcher.LOAN_LOGGING;
+        return Logging.LOAN_TAG;
     }
 
     @Override
     public LogTag getInvestmentsLogTag() {
-        return SdcNoConstants.Fetcher.INVESTMENTS_LOGGING;
+        return Logging.INVESTMENTS_TAG;
     }
 }
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/sdcno/SdcNoConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/sdcno/SdcNoConstants.java
@@ -5,16 +5,15 @@ import se.tink.backend.aggregation.agents.utils.log.LogTag;
 
 public class SdcNoConstants {
     public static class Market {
-        public static final String NORWAY = "NO";
         public static final String BASE_URL = "https://prod.smartno.sdc.dk/restapi/";
-        public static final String PHONE_COUNTRY_CODE = "+47";
         public static final String EIKA_BASE_URL = "https://prod-smarteika.portalbank.no/restapi/";
+        public static final String PHONE_COUNTRY_CODE = "+47";
         public static final ImmutableList<String> EIKA_BANKS = ImmutableList.of("2230");
     }
 
-    public static class Fetcher {
-        public static final LogTag LOAN_LOGGING = LogTag.from("#loan_logging_sdc_no");
-        public static final LogTag INVESTMENTS_LOGGING = LogTag.from("#investment_logging_sdc_no");
+    public static class Logging {
+        public static final LogTag LOAN_TAG = LogTag.from("#loan_logging_sdc_no");
+        public static final LogTag INVESTMENTS_TAG = LogTag.from("#investment_logging_sdc_no");
     }
 
     public static class Authentication {
