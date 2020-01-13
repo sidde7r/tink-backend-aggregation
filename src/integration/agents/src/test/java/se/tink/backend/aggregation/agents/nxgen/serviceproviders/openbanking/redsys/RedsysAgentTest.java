769147365cac0c2274b5897807062df61d2ba884
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-01-13 11:11:33 +0100

    test(redsys): use provider name from argument

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/redsys/RedsysAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/redsys/RedsysAgentTest.java
@@ -1,20 +1,39 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.redsys;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager.ArgumentManagerEnum;
 import se.tink.backend.aggregation.agents.framework.DualAgentIntegrationTest;
 
-@Ignore
 public class RedsysAgentTest {
+    private final ArgumentManager<ProviderNameEnum> manager =
+            new ArgumentManager<>(ProviderNameEnum.values());
 
-    private AgentIntegrationTest.Builder builder;
+    public enum ProviderNameEnum implements ArgumentManagerEnum {
+        PROVIDER_NAME;
 
-    private String PROVIDER_NAME = "es-redsys-bbva-ob";
+        @Override
+        public boolean isOptional() {
+            return false;
+        }
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
+    @Before
+    public void setup() {
+        manager.before();
+    }
 
     @Test
     public void testRefresh() throws Exception {
-        createAgentTest(PROVIDER_NAME).testRefresh();
+        createAgentTest(manager.get(ProviderNameEnum.PROVIDER_NAME)).testRefresh();
     }
 
     private static AgentIntegrationTest createAgentTest(String providerName) {
