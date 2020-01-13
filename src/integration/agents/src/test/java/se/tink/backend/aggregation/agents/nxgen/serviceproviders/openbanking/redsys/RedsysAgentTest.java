f5b1396f7c97909f7aa9985bdaa17b4022aa0fd1
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-01-13 11:05:47 +0100

    test(redsys): add class method to run dual agent test with another test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/redsys/RedsysAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/redsys/RedsysAgentTest.java
@@ -1,9 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.redsys;
 
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.DualAgentIntegrationTest;
 
 @Ignore
 public class RedsysAgentTest {
@@ -12,19 +12,24 @@ public class RedsysAgentTest {
 
     private String PROVIDER_NAME = "es-redsys-bbva-ob";
 
-    @Before
-    public void setup() {
-        builder =
-                new AgentIntegrationTest.Builder("es", PROVIDER_NAME)
-                        .setFinancialInstitutionId("redsys")
-                        .setAppId("tink")
-                        .expectLoggedIn(false)
-                        .loadCredentialsBefore(false)
-                        .saveCredentialsAfter(false);
-    }
-
     @Test
     public void testRefresh() throws Exception {
-        builder.build().testRefresh();
+        createAgentTest(PROVIDER_NAME).testRefresh();
+    }
+
+    private static AgentIntegrationTest createAgentTest(String providerName) {
+        return new AgentIntegrationTest.Builder("es", providerName)
+                .setFinancialInstitutionId("redsys")
+                .setAppId("tink")
+                .expectLoggedIn(false)
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(true)
+                .build();
+    }
+
+    public static void runDualAgentTest(String providerName, AgentIntegrationTest reAgentTest)
+            throws Exception {
+        final AgentIntegrationTest obAgentTest = RedsysAgentTest.createAgentTest(providerName);
+        DualAgentIntegrationTest.of(reAgentTest, obAgentTest).testAndCompare();
     }
 }
