ccf0c964b52522a94eac9b81a9a78fc254b434d8
Ahmed Hussein <ahmed.hussein@tink.se>
2020-01-13 15:45:46 +0100

    ref(NordeaPartner): extract account mapping to market agent class

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/nordea/partner/NordeaPartnerSeAccountMapper.java
@@ -0,0 +1,39 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.nordea.partner;
+
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.NordeaPartnerConstants;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.fetcher.mapper.NordeaPartnerAccountMapper;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.fetcher.transactional.entity.AccountEntity;
+import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.balance.BalanceModule;
+import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.id.IdModule;
+import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.account.AccountIdentifier.Type;
+import se.tink.libraries.amount.ExactCurrencyAmount;
+
+public class NordeaPartnerSeAccountMapper implements NordeaPartnerAccountMapper {
+
+    @Override
+    public Optional<TransactionalAccount> toTinkTransactionalAccount(AccountEntity account) {
+        return TransactionalAccount.nxBuilder()
+                .withTypeAndFlagsFrom(
+                        NordeaPartnerConstants.TRANSACTIONAL_ACCOUNT_TYPE_MAPPER,
+                        account.getCategory())
+                .withBalance(
+                        BalanceModule.of(
+                                ExactCurrencyAmount.of(
+                                        account.getAvailableBalance(), account.getCurrency())))
+                .withId(
+                        IdModule.builder()
+                                .withUniqueIdentifier(account.getIban())
+                                .withAccountNumber(account.getDisplayAccountNumber())
+                                .withAccountName(account.getNickname())
+                                .addIdentifier(
+                                        AccountIdentifier.create(Type.IBAN, account.getIban()))
+                                .setProductName(account.getProductName())
+                                .build())
+                .addHolderName(account.getHolderName())
+                .setApiIdentifier(account.getAccountId())
+                .build();
+    }
+}
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/nordea/partner/fetcher/transactional/rpc/AccountListResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/nordea/partner/fetcher/transactional/rpc/AccountListResponse.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.fetcher.mapper.NordeaPartnerAccountMapper;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.fetcher.transactional.entity.AccountEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
@@ -13,10 +14,11 @@ import se.tink.backend.aggregation.nxgen.core.account.transactional.Transactiona
 public class AccountListResponse {
     private List<AccountEntity> result;
 
-    public Collection<TransactionalAccount> toTinkTransactionalAccounts() {
+    public Collection<TransactionalAccount> toTinkTransactionalAccounts(
+            NordeaPartnerAccountMapper accountMapper) {
         return Optional.ofNullable(result).orElse(Collections.emptyList()).stream()
                 .filter(AccountEntity::hasIban)
-                .map(AccountEntity::toTinkTransactionalAccount)
+                .map(accountMapper::toTinkTransactionalAccount)
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .collect(Collectors.toList());
