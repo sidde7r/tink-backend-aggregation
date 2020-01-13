376f1c98e5829ab7861946fa9e8feae52165b4e4
Ahmed Hussein <ahmed.hussein@tink.se>
2020-01-13 17:21:08 +0100

    feat(Fi/NordeaPartner): add nordea custom agent FI

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/nordea/partner/NordeaPartnerFiAgent.java
@@ -0,0 +1,15 @@
+package se.tink.backend.aggregation.agents.nxgen.fi.banks.nordea.partner;
+
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.NordeaPartnerAgent;
+import se.tink.backend.aggregation.configuration.AgentsServiceConfiguration;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public final class NordeaPartnerFiAgent extends NordeaPartnerAgent {
+    public NordeaPartnerFiAgent(
+            CredentialsRequest request,
+            AgentContext context,
+            AgentsServiceConfiguration agentsServiceConfiguration) {
+        super(request, context, agentsServiceConfiguration);
+    }
+}
