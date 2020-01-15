27171a24b281c073ba6b2f30a5b31b6ee67ea35b
Nazri Abdullah <nazri.abdullah@tink.se>
2020-01-15 17:46:47 +0100

    feat(nl/rabobank): added constants for timer in range

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/rabobank/RabobankConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/rabobank/RabobankConstants.java
@@ -85,7 +85,8 @@ public class RabobankConstants {
     }
 
     public static class HttpClient {
-        public static final int MAX_RETRIES = 4;
-        public static final int RETRY_SLEEP_MILLISECONDS = 2000;
+        public static final int MAX_RETRIES = 5;
+        public static final int RETRY_SLEEP_MILLISECONDS_MINIMUM = 2000;
+        public static final int RETRY_SLEEP_MILLISECONDS_MAXIMUM = 10000;
     }
 }
