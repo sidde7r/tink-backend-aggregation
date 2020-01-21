be6189b9c257991c0728d3b6333dbf99b543f87b
Mateusz Pedzich <mateusz.pedzich@tink.se>
2020-01-21 20:36:43 +0100

    fix(BE/amex): upgrade to v62 amex

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/amex/AmericanExpressBEAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/banks/amex/AmericanExpressBEAgentTest.java
@@ -1,25 +1,47 @@
 package se.tink.backend.aggregation.agents.nxgen.be.banks.amex;
 
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager.UsernamePasswordArgumentEnum;
+import se.tink.backend.aggregation.agents.nxgen.be.creditcards.amex.AmericanExpressBEConstants;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
 @Ignore
 public class AmericanExpressBEAgentTest {
 
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    private ArgumentManager<UsernamePasswordArgumentEnum> manager =
+            new ArgumentManager<>(UsernamePasswordArgumentEnum.values());
 
-    private final AgentIntegrationTest.Builder builder =
-            new AgentIntegrationTest.Builder("be", "be-americanexpress-password")
-                    .addCredentialField(Field.Key.USERNAME, USERNAME)
-                    .addCredentialField(Field.Key.PASSWORD, PASSWORD)
-                    .loadCredentialsBefore(false)
-                    .saveCredentialsAfter(false);
+    @Before
+    public void setUp() throws Exception {
+        manager.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
+    private AgentIntegrationTest.Builder builder() {
+        return new AgentIntegrationTest.Builder(
+                        AmericanExpressBEConstants.MARKET, AmericanExpressBEConstants.PROVIDER_NAME)
+                .addCredentialField(
+                        Field.Key.USERNAME, manager.get(UsernamePasswordArgumentEnum.USERNAME))
+                .addCredentialField(
+                        Field.Key.PASSWORD, manager.get(UsernamePasswordArgumentEnum.PASSWORD))
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false)
+                .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                .addRefreshableItems(RefreshableItem.IDENTITY_DATA);
+    }
 
     @Test
     public void testRefresh() throws Exception {
-        builder.build().testRefresh();
+        builder().build().testRefresh();
     }
 }
