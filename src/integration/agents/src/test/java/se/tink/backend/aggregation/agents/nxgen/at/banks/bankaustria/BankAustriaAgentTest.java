1cd0f4fe5599e6d72295aa355a9544bb291175c6
Nazri Abdullah <nazri.abdullah@tink.se>
2020-01-26 20:35:16 +0100

    test(at/bankaustria): implemented argument manager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/at/banks/bankaustria/BankAustriaAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/at/banks/bankaustria/BankAustriaAgentTest.java
@@ -1,25 +1,42 @@
 package se.tink.backend.aggregation.agents.nxgen.at.banks.bankaustria;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager.UsernamePasswordArgumentEnum;
 
-@Ignore
 public class BankAustriaAgentTest {
-    private static final String USERNAME = "";
-    private static final String PIN = "";
 
-    private final AgentIntegrationTest.Builder builder =
-            new AgentIntegrationTest.Builder("at", "at-bankaustria-password")
-                    .addCredentialField(Field.Key.USERNAME, USERNAME)
-                    .addCredentialField(Field.Key.PASSWORD, PIN)
-                    .doLogout(true)
-                    .loadCredentialsBefore(false)
-                    .saveCredentialsAfter(false);
+    private final ArgumentManager<UsernamePasswordArgumentEnum> helper =
+            new ArgumentManager<>(UsernamePasswordArgumentEnum.values());
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
+    @Before
+    public void setup() {
+        helper.before();
+    }
+
+    private AgentIntegrationTest createAgentTest() {
+        return new AgentIntegrationTest.Builder("at", "at-bankaustria-password")
+                .addCredentialField(
+                        Field.Key.USERNAME, helper.get(UsernamePasswordArgumentEnum.USERNAME))
+                .addCredentialField(
+                        Field.Key.PASSWORD, helper.get(UsernamePasswordArgumentEnum.PASSWORD))
+                .doLogout(true)
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false)
+                .build();
+    }
 
     @Test
     public void testRefresh() throws Exception {
-        builder.build().testRefresh();
+        createAgentTest().testRefresh();
     }
 }
