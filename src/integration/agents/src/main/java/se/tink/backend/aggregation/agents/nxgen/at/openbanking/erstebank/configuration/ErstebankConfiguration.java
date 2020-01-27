a7a3173967484441230847366d2471d608ceba1f
Mateusz Pedzich <mateusz.pedzich@tink.se>
2020-01-27 14:49:22 +0100

    refactor(Berlin Group): changed berlin group config to interface

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/erstebank/configuration/ErstebankConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/erstebank/configuration/ErstebankConfiguration.java
@@ -3,14 +3,76 @@ package se.tink.backend.aggregation.agents.nxgen.at.openbanking.erstebank.config
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.common.base.Preconditions;
 import com.google.common.base.Strings;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Objects;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.BerlinGroupConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.BerlinGroupConstants.ErrorMessages;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.configuration.BerlinGroupConfiguration;
+import se.tink.backend.aggregation.annotations.AgentConfigParam;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.annotations.Secret;
+import se.tink.backend.aggregation.annotations.SensitiveSecret;
 
 @JsonObject
-public class ErstebankConfiguration extends BerlinGroupConfiguration {
+public class ErstebankConfiguration implements BerlinGroupConfiguration {
 
     @JsonProperty private String apiKey;
+    @JsonProperty @Secret private String oauthBaseUrl;
+    @JsonProperty @Secret private String baseUrl;
+    @JsonProperty @Secret private String clientId;
+    @JsonProperty @SensitiveSecret private String clientSecret;
+    @JsonProperty @AgentConfigParam private String redirectUrl;
+    @JsonProperty @Secret private String psuIpAddress;
+
+    @Override
+    public String getClientId() {
+        Preconditions.checkNotNull(
+                Strings.emptyToNull(clientId),
+                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client ID"));
+
+        return clientId;
+    }
+
+    @Override
+    public String getClientSecret() {
+        Preconditions.checkNotNull(
+                Strings.emptyToNull(clientSecret),
+                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client Secret"));
+
+        return clientSecret;
+    }
+
+    @Override
+    public String getRedirectUrl() {
+        Preconditions.checkNotNull(
+                Strings.emptyToNull(redirectUrl),
+                String.format(ErrorMessages.INVALID_CONFIGURATION, "Redirect URL"));
+
+        return redirectUrl;
+    }
+
+    @Override
+    public String getBaseUrl() {
+        Preconditions.checkNotNull(
+                Strings.emptyToNull(baseUrl),
+                String.format(ErrorMessages.INVALID_CONFIGURATION, "Base URL"));
+
+        return baseUrl;
+    }
+
+    @Override
+    public String getPsuIpAddress() {
+        if (Objects.nonNull(psuIpAddress)) {
+            return psuIpAddress;
+        }
+
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return BerlinGroupConstants.DEFAULT_IP;
+        }
+    }
 
     public String getApiKey() {
         Preconditions.checkNotNull(
