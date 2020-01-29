cfadb8d9d42aa06c6405f777cdc960cfaa1147ce
Krzysztof Korolczuk <krzysztof.korolczuk@tink.se>
2020-01-29 11:41:32 +0100

    refactor(sibs): annotate configuration in SIBS

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/configuration/SibsConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/configuration/SibsConfiguration.java
@@ -1,89 +1,51 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.configuration;
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsConstants;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsConstants.ErrorMessages;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.annotations.Secret;
+import se.tink.backend.aggregation.annotations.SensitiveSecret;
 import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
 
 @JsonObject
 public class SibsConfiguration implements ClientConfiguration {
 
-    private String baseUrl;
-    private String certificateId;
-    private String clientId;
-    private String clientSecret;
-    private String redirectUrl;
-    private String clientSigningCertificate;
-    private String clientSigningCertificateSerialNumber;
-    private String aspspCode;
+    @Secret private String baseUrl;
+    @Secret private String certificateId;
+    @Secret private String clientId;
+    @SensitiveSecret private String clientSecret;
+    @Secret private String redirectUrl;
+    @Secret private String clientSigningCertificate;
+    @Secret private String clientSigningCertificateSerialNumber;
+    @Secret private String aspspCode;
 
     public String getBaseUrl() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(baseUrl),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Base URL"));
-
         return baseUrl;
     }
 
     public String getCertificateId() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(certificateId),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Certificate ID"));
-
         return certificateId;
     }
 
     public String getClientId() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(clientId),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client ID"));
-
         return clientId;
     }
 
     public String getClientSecret() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(clientSecret),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client Secret"));
-
         return clientSecret;
     }
 
     public String getRedirectUrl() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(redirectUrl),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Redirect URL"));
-
         return redirectUrl;
     }
 
     public String getClientSigningCertificate() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(clientSigningCertificate),
-                String.format(
-                        SibsConstants.ErrorMessages.INVALID_CONFIGURATION,
-                        "Client Signing Certificate"));
-
         return clientSigningCertificate;
     }
 
     public String getClientSigningCertificateSerialNumber() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(clientSigningCertificateSerialNumber),
-                String.format(
-                        SibsConstants.ErrorMessages.INVALID_CONFIGURATION,
-                        "Client Signing Certificate Serial Number"));
-
         return clientSigningCertificateSerialNumber;
     }
 
     public String getAspspCode() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(aspspCode),
-                String.format(SibsConstants.ErrorMessages.INVALID_CONFIGURATION, "Aspsp Code"));
-
         return aspspCode;
     }
 }
