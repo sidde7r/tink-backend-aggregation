34712ed1b337a5eb4fcff6d948aa4d5f54f62ae6
Mateusz Pedzich <mateusz.pedzich@tink.se>
2020-01-31 17:18:41 +0100

    refactor(FR/Credit Agricole): added fetching transactions from 90 days

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/creditagricole/transactionalaccount/CreditAgricoleBaseTransactionalAccountFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/creditagricole/transactionalaccount/CreditAgricoleBaseTransactionalAccountFetcher.java
@@ -2,6 +2,7 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cr
 
 import java.util.Collection;
 import java.util.Date;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.creditagricole.CreditAgricoleBaseConstants.StorageKeys;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.creditagricole.transactionalaccount.apiclient.CreditAgricoleBaseApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.creditagricole.transactionalaccount.rpc.GetAccountsResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
@@ -9,15 +10,20 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.paginat
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponseImpl;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginator;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
+import se.tink.libraries.date.DateUtils;
 
 public class CreditAgricoleBaseTransactionalAccountFetcher
         implements AccountFetcher<TransactionalAccount>,
                 TransactionDatePaginator<TransactionalAccount> {
 
     private final CreditAgricoleBaseApiClient apiClient;
+    private final PersistentStorage persistentStorage;
 
-    public CreditAgricoleBaseTransactionalAccountFetcher(CreditAgricoleBaseApiClient apiClient) {
+    public CreditAgricoleBaseTransactionalAccountFetcher(
+            CreditAgricoleBaseApiClient apiClient, PersistentStorage persistentStorage) {
         this.apiClient = apiClient;
+        this.persistentStorage = persistentStorage;
     }
 
     @Override
@@ -36,9 +42,31 @@ public class CreditAgricoleBaseTransactionalAccountFetcher
     public PaginatorResponse getTransactionsFor(
             TransactionalAccount account, Date fromDate, Date toDate) {
 
+        return isInitialFetch()
+                ? getAllTransactions(account, fromDate, toDate)
+                : get90DaysTransactions(account, toDate);
+    }
+
+    private PaginatorResponse getAllTransactions(
+            TransactionalAccount account, Date fromDate, Date toDate) {
         return PaginatorResponseImpl.create(
                 apiClient
                         .getTransactions(account.getApiIdentifier(), fromDate, toDate)
                         .getTinkTransactions());
+    }
+
+    private PaginatorResponse get90DaysTransactions(TransactionalAccount account, Date toDate) {
+        return PaginatorResponseImpl.create(
+                apiClient
+                        .getTransactions(
+                                account.getApiIdentifier(), DateUtils.addDays(toDate, -90), toDate)
+                        .getTinkTransactions(),
+                false);
+    }
+
+    private boolean isInitialFetch() {
+        return persistentStorage
+                .get(StorageKeys.IS_INITIAL_FETCH, Boolean.class)
+                .orElse(Boolean.FALSE);
     }
 }
