00e85271ed6a3017d01ff03e6d5915817ac06c47
Nazri Abdullah <nazri.abdullah@tink.se>
2020-02-02 12:44:14 +0100

    refactor(dk/nordea): implement arg manager in test agent

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/NordeaDkAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/NordeaDkAgentTest.java
@@ -1,26 +1,38 @@
 package se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager.UsernamePasswordArgumentEnum;
 
-@Ignore
 public class NordeaDkAgentTest {
-    private static final String USERNAME = "";
-    private static final String PASSWORD = "";
+    private final ArgumentManager<UsernamePasswordArgumentEnum> manager =
+            new ArgumentManager<>(UsernamePasswordArgumentEnum.values());
 
-    private AgentIntegrationTest.Builder builder() {
-        return new AgentIntegrationTest.Builder("dk", "dk-nordea-nemid")
-                .addCredentialField(Field.Key.USERNAME, USERNAME)
-                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
-                .expectLoggedIn(false)
-                .loadCredentialsBefore(false)
-                .saveCredentialsAfter(false);
+    @Before
+    public void before() {
+        manager.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
     }
 
     @Test
-    public void testRefresh() throws Exception {
-        builder().build().testRefresh();
+    public void testLoginAndRefresh() throws Exception {
+        new AgentIntegrationTest.Builder("dk", "dk-nordea-nemid")
+                .addCredentialField(
+                        Field.Key.USERNAME, manager.get(UsernamePasswordArgumentEnum.USERNAME))
+                .addCredentialField(
+                        Field.Key.PASSWORD, manager.get(UsernamePasswordArgumentEnum.PASSWORD))
+                .expectLoggedIn(false)
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false)
+                .build()
+                .testRefresh();
     }
 }
