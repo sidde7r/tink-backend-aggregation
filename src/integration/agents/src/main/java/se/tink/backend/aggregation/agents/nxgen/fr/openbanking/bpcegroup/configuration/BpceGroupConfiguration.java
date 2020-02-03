59bb4bc4d599e5e87210e83ca841c7cb8f3d141b
Kamil Woźniak <kamil.wozniak@tink.se>
2020-02-03 12:01:52 +0100

    fix(fr/bpce): Corrected redirectUrl annotation

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/bpcegroup/configuration/BpceGroupConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/bpcegroup/configuration/BpceGroupConfiguration.java
@@ -1,6 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bpcegroup.configuration;
 
 import lombok.Getter;
+import se.tink.backend.aggregation.annotations.AgentConfigParam;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
@@ -10,7 +11,7 @@ import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
 public class BpceGroupConfiguration implements ClientConfiguration {
 
     @Secret private String clientId;
-    @Secret private String redirectUrl;
+    @AgentConfigParam private String redirectUrl;
     @Secret private String serverUrl;
     @Secret private String keyId;
 }
