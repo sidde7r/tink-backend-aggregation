00fa5542154c79e977089c0746988df78466a1ec
Ahmed Hussein <ahmed.hussein@tink.se>
2020-02-04 11:13:15 +0100

    feat(NxBuilders): add availableBalance+creditLimit to balance module

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/nxbuilders/modules/balance/BalanceModule.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/nxbuilders/modules/balance/BalanceModule.java
@@ -13,11 +13,15 @@ public final class BalanceModule {
     private final ExactCurrencyAmount exactBalance;
     private final Double interestRate;
     private final ExactCurrencyAmount exactAvailableCredit;
+    private ExactCurrencyAmount exactAvailableBalance;
+    private ExactCurrencyAmount exactCreditLimit;
 
     private BalanceModule(Builder builder) {
         this.interestRate = builder.interestRate;
         this.exactAvailableCredit = builder.exactAvailableCredit;
         this.exactBalance = builder.exactBalance;
+        this.exactAvailableBalance = builder.exactAvailableBalance;
+        this.exactCreditLimit = builder.exactCreditLimit;
     }
 
     public static BalanceStep<BalanceBuilderStep> builder() {
@@ -55,11 +59,21 @@ public final class BalanceModule {
         return Optional.ofNullable(this.exactAvailableCredit);
     }
 
+    public ExactCurrencyAmount getExactAvailableBalance() {
+        return exactAvailableBalance;
+    }
+
+    public ExactCurrencyAmount getExactCreditLimit() {
+        return exactCreditLimit;
+    }
+
     private static class Builder implements BalanceStep<BalanceBuilderStep>, BalanceBuilderStep {
 
         private Double interestRate;
         private ExactCurrencyAmount exactAvailableCredit;
         private ExactCurrencyAmount exactBalance;
+        private ExactCurrencyAmount exactAvailableBalance;
+        private ExactCurrencyAmount exactCreditLimit;
 
         @Override
         public BalanceBuilderStep setInterestRate(double interestRate) {
@@ -98,6 +112,23 @@ public final class BalanceModule {
         public BalanceBuilderStep withBalance(@Nonnull ExactCurrencyAmount balance) {
             Preconditions.checkNotNull(balance, "Balance must not be null.");
             this.exactBalance = balance;
+            return this;
+        }
+
+        @Override
+        public BalanceBuilderStep setAvailableBalance(
+                @Nonnull ExactCurrencyAmount availableBalance) {
+            Preconditions.checkNotNull(availableBalance, "Available Balance must not be null.");
+            this.exactAvailableBalance = availableBalance;
+            return this;
+        }
+
+        @Override
+        public BalanceBuilderStep setCreditLimit(@Nonnull ExactCurrencyAmount creditLimit) {
+            Preconditions.checkNotNull(creditLimit, "Credit Limit must not be null.");
+            Preconditions.checkArgument(
+                    creditLimit.getDoubleValue() >= 0, "Credit Limit must not be negative.");
+            this.exactCreditLimit = creditLimit;
             return this;
         }
 
