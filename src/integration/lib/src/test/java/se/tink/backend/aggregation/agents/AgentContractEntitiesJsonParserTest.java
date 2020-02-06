26f841b13b24eaceaa3b4b3c3353e90adf6c84e6
Berk Gedik <berk.gedik@tink.se>
2020-02-06 13:01:53 +0100

    test(blackbox): test for JSON contract parser

---
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/AgentContractEntitiesJsonParserTest.java
@@ -0,0 +1,28 @@
+package se.tink.backend.aggregation.agents;
+
+import org.junit.Assert;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonParser;
+import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
+import se.tink.backend.aggregation.agents.framework.assertions.utils.ResourceFileReader;
+
+public class AgentContractEntitiesJsonParserTest {
+
+    ResourceFileReader reader = new ResourceFileReader();
+
+    @Test
+    public void testContractJsonParsing() {
+        String jsonContent =
+                reader.read(
+                        String.format(
+                                "%s/%s",
+                                "src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/resources",
+                                "contract_test.json"));
+
+        AgentContractEntitiesJsonParser parser = new AgentContractEntitiesJsonParser();
+        AgentContractEntity entity = parser.parse(jsonContent);
+
+        Assert.assertEquals(1, entity.getAccounts().size());
+        Assert.assertEquals(4, entity.getTransactions().size());
+    }
+}
