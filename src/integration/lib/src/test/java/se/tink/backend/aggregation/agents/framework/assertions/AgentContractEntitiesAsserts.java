034a02aba97e7c9048666478ad59b07cc293be06
Berk Gedik <berk.gedik@tink.se>
2020-02-06 12:51:11 +0100

    refactor(blackbox): Updated contract assertion logic

--- src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/framework/assertions/AgentContractEntitiesAsserts.java
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/framework/assertions/AgentContractEntitiesAsserts.java
@@ -1,17 +1,13 @@
 package se.tink.backend.aggregation.agents.framework.assertions;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
+import org.junit.Assert;
 import se.tink.backend.agents.rpc.Account;
 import se.tink.backend.agents.rpc.AccountTypes;
-import se.tink.backend.aggregation.agents.framework.assertions.entities.AccountTestEntity;
-import se.tink.backend.aggregation.agents.framework.assertions.entities.TransactionTestEntity;
 import se.tink.backend.aggregation.agents.models.Transaction;
 import se.tink.backend.aggregation.agents.models.TransactionTypes;
 import se.tink.libraries.account.enums.AccountExclusion;
@@ -19,32 +15,78 @@ import se.tink.libraries.amount.ExactCurrencyAmount;
 
 public class AgentContractEntitiesAsserts {
 
+    private static <T> boolean areEqualIgnoringOrder(
+            List<T> list1, List<T> list2, EqualityChecker comparator) {
+
+        if (list1.size() != list2.size()) {
+            return false;
+        }
+
+        List<T> copy1 = new ArrayList<>(list1);
+        List<T> copy2 = new ArrayList<>(list2);
+
+        for (T obj1 : copy1) {
+            boolean found = false;
+            for (T obj2 : copy2) {
+                if (comparator.isEqual(obj1, obj2)) {
+                    found = true;
+                    copy2.remove(obj2);
+                    break;
+                }
+            }
+            if (!found) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public static void compareAccounts(List<Account> expected, List<Account> given) {
-        List<AccountTestEntity> expectedAccounts =
-                expected.stream().map(a -> new AccountTestEntity(a)).collect(Collectors.toList());
-
-        List<AccountTestEntity> givenAccounts =
-                given.stream().map(a -> new AccountTestEntity(a)).collect(Collectors.toList());
-
-        Collections.sort(expectedAccounts);
-        Collections.sort(givenAccounts);
-
-        assertThat(expectedAccounts).isEqualTo(givenAccounts);
+        Assert.assertTrue(
+                areEqualIgnoringOrder(
+                        expected,
+                        given,
+                        new EqualityChecker<Account>() {
+                            @Override
+                            public boolean isEqual(Account account1, Account account2) {
+                                return Account.deepEquals(account1, account2);
+                            }
+                        }));
     }
 
     public static void compareTransactions(List<Transaction> expected, List<Transaction> given) {
-        List<TransactionTestEntity> expectedTransactions =
-                expected.stream()
-                        .map(a -> new TransactionTestEntity(a))
-                        .collect(Collectors.toList());
-
-        List<TransactionTestEntity> givenTransactions =
-                given.stream().map(a -> new TransactionTestEntity(a)).collect(Collectors.toList());
-
-        Collections.sort(expectedTransactions);
-        Collections.sort(givenTransactions);
-
-        assertThat(expectedTransactions).isEqualTo(givenTransactions);
+        Assert.assertTrue(
+                areEqualIgnoringOrder(
+                        expected,
+                        given,
+                        new EqualityChecker<Transaction>() {
+                            @Override
+                            public boolean isEqual(
+                                    Transaction transaction1, Transaction transaction2) {
+                                return Double.compare(
+                                                        transaction1.getAmount(),
+                                                        transaction2.getAmount())
+                                                == 0
+                                        && Double.compare(
+                                                        transaction1.getOriginalAmount(),
+                                                        transaction2.getOriginalAmount())
+                                                == 0
+                                        && transaction1.isPending() == transaction2.isPending()
+                                        && transaction1.isUpcoming() == transaction2.isUpcoming()
+                                        && Objects.equals(
+                                                transaction1.getCredentialsId(),
+                                                transaction2.getCredentialsId())
+                                        && Objects.equals(
+                                                transaction1.getDate(), transaction2.getDate())
+                                        && Objects.equals(
+                                                transaction1.getDescription(),
+                                                transaction2.getDescription())
+                                        && transaction1.getType() == transaction2.getType()
+                                        && Objects.equals(
+                                                transaction1.getUserId(), transaction2.getUserId());
+                            }
+                        }));
     }
 
     public static Account createAccount(
