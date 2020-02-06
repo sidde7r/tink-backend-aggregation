cc61cfae9fa2f87de933c198d3e66b43097529e5
Berk Gedik <berk.gedik@tink.se>
2020-02-06 12:43:20 +0100

    feat(blackbox): Added proper deserializer for contract file

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/creditcards/amex/v62/AmexV62UkMockServerAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/creditcards/amex/v62/AmexV62UkMockServerAgentTest.java
@@ -5,7 +5,6 @@ import static se.tink.backend.aggregation.agents.framework.assertions.AgentContr
 import static se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesAsserts.createAccount;
 import static se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesAsserts.createTransaction;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.Test;
@@ -14,15 +13,12 @@ import se.tink.backend.agents.rpc.AccountTypes;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.agents.framework.NewAgentTestContext;
-import se.tink.backend.aggregation.agents.framework.assertions.AgentContractFileParser;
-import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
 import se.tink.backend.aggregation.agents.framework.utils.wiremock.WiremockS3LogRequestResponseParser;
 import se.tink.backend.aggregation.agents.framework.wiremock.AgentIntegrationMockServerTest;
 import se.tink.backend.aggregation.agents.models.Transaction;
 import se.tink.backend.aggregation.agents.models.TransactionTypes;
-import se.tink.libraries.amount.ExactCurrencyAmount;
 
-public final class AmericanExpressV62UKMockServerAgentTest extends AgentIntegrationMockServerTest {
+public final class AmexV62UkMockServerAgentTest extends AgentIntegrationMockServerTest {
 
     private String USERNAME = "testUser";
     private String PASSWORD = "testPassword";
@@ -95,57 +91,6 @@ public final class AmericanExpressV62UKMockServerAgentTest extends AgentIntegrat
         List<Transaction> expectedTransactions =
                 Arrays.asList(
                         new Transaction[] {transaction1, transaction2, transaction3, transaction4});
-
-        // When
-        NewAgentTestContext context =
-                new AgentIntegrationTest.Builder("uk", "uk-americanexpress-password")
-                        .addCredentialField(Field.Key.USERNAME, USERNAME)
-                        .addCredentialField(Field.Key.PASSWORD, PASSWORD)
-                        .loadCredentialsBefore(false)
-                        .saveCredentialsAfter(false)
-                        .useWireMockServerHost("localhost:" + getWireMockPort())
-                        .build()
-                        .testRefresh();
-
-        List<Transaction> givenTransactions = context.getTransactions();
-        List<Account> givenAccounts = context.getUpdatedAccounts();
-
-        // Then
-        compareAccounts(expectedAccounts, givenAccounts);
-        compareTransactions(expectedTransactions, givenTransactions);
-    }
-
-    @Test
-    public void testRefreshWithContractFile() throws Exception {
-
-        // Given
-        prepareMockServer(
-                new WiremockS3LogRequestResponseParser(
-                        String.format(
-                                "%s/%s",
-                                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/creditcards/amex/v62",
-                                "resources/mock.txt"),
-                        "https://global.americanexpress.com"));
-
-        AgentContractEntity expected =
-                AgentContractFileParser.parseAgentContractFile(
-                        String.format(
-                                "%s/%s",
-                                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/creditcards/amex/v62",
-                                "resources/contract.json"));
-
-        List<Account> expectedAccounts = expected.getAccounts();
-
-        /**
-         * TODO: I could not find a way to read "exactAvailableCredit" and "exactBalance" fields
-         * (they are ExactCurrencyAmount objects) from JSON so I'm setting them up manually for now
-         */
-        expectedAccounts
-                .get(0)
-                .setExactAvailableCredit(new ExactCurrencyAmount(new BigDecimal(0), "GBP"));
-        expectedAccounts.get(0).setExactBalance(new ExactCurrencyAmount(new BigDecimal(0), "GBP"));
-
-        List<Transaction> expectedTransactions = expected.getTransactions();
 
         // When
         NewAgentTestContext context =
---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/creditcards/amex/v62/AmexV62UkMockServerWithContractFileAgentTest.java
@@ -0,0 +1,64 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.creditcards.amex.v62;
+
+import static se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesAsserts.compareAccounts;
+import static se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesAsserts.compareTransactions;
+
+import java.util.List;
+import org.junit.Test;
+import se.tink.backend.agents.rpc.Account;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.NewAgentTestContext;
+import se.tink.backend.aggregation.agents.framework.assertions.AgentContractFileParser;
+import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
+import se.tink.backend.aggregation.agents.framework.utils.wiremock.WiremockS3LogRequestResponseParser;
+import se.tink.backend.aggregation.agents.framework.wiremock.AgentIntegrationMockServerTest;
+import se.tink.backend.aggregation.agents.models.Transaction;
+
+public final class AmexV62UkMockServerWithContractFileAgentTest
+        extends AgentIntegrationMockServerTest {
+
+    private String USERNAME = "testUser";
+    private String PASSWORD = "testPassword";
+
+    @Test
+    public void testRefreshWithJSONContractFile() throws Exception {
+
+        // Given
+        prepareMockServer(
+                new WiremockS3LogRequestResponseParser(
+                        String.format(
+                                "%s/%s",
+                                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/creditcards/amex/v62",
+                                "resources/mock.txt"),
+                        "https://global.americanexpress.com"));
+
+        AgentContractEntity expected =
+                AgentContractFileParser.parseAgentContractFile(
+                        String.format(
+                                "%s/%s",
+                                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/creditcards/amex/v62",
+                                "resources/contract.json"));
+
+        List<Account> expectedAccounts = expected.getAccounts();
+        List<Transaction> expectedTransactions = expected.getTransactions();
+
+        // When
+        NewAgentTestContext context =
+                new AgentIntegrationTest.Builder("uk", "uk-americanexpress-password")
+                        .addCredentialField(Field.Key.USERNAME, USERNAME)
+                        .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                        .loadCredentialsBefore(false)
+                        .saveCredentialsAfter(false)
+                        .useWireMockServerHost("localhost:" + getWireMockPort())
+                        .build()
+                        .testRefresh();
+
+        List<Transaction> givenTransactions = context.getTransactions();
+        List<Account> givenAccounts = context.getUpdatedAccounts();
+
+        // Then
+        compareAccounts(expectedAccounts, givenAccounts);
+        compareTransactions(expectedTransactions, givenTransactions);
+    }
+}
