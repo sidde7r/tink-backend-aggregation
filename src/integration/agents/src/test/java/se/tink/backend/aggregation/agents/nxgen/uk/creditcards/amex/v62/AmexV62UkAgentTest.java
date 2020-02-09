ab5c417a9eab6b067d9bfd39b1942c9c8bb2fa3e
Nazri Abdullah <nazri.abdullah@tink.se>
2020-02-09 12:55:35 +0100

    test(uk/amex): implements argument manager

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/creditcards/amex/v62/AmexV62UkAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/creditcards/amex/v62/AmexV62UkAgentTest.java
@@ -1,21 +1,35 @@
 package se.tink.backend.aggregation.agents.nxgen.uk.creditcards.amex.v62;
 
-import org.junit.Ignore;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager.UsernamePasswordArgumentEnum;
 
-@Ignore
 public class AmexV62UkAgentTest {
 
-    private final String USERNAME = "";
-    private final String PASSWORD = "";
+    private final ArgumentManager<UsernamePasswordArgumentEnum> manager =
+            new ArgumentManager<>(UsernamePasswordArgumentEnum.values());
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
+    @Before
+    public void setup() {
+        manager.before();
+    }
 
     @Test
     public void testLogin() throws Exception {
         new AgentIntegrationTest.Builder("uk", "uk-americanexpress-password")
-                .addCredentialField(Field.Key.USERNAME, USERNAME)
-                .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+                .addCredentialField(
+                        Field.Key.USERNAME, manager.get(UsernamePasswordArgumentEnum.USERNAME))
+                .addCredentialField(
+                        Field.Key.PASSWORD, manager.get(UsernamePasswordArgumentEnum.PASSWORD))
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false)
                 .build()
