f4ea8e7f0f0245cad3d0dd64587628bfc1fe77ed
Ashfaq Haider <ashfaq.haider@tink.se>
2020-02-10 16:40:06 +0100

    feat(HSBC Agent): implement PIS for HSBC

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/hsbc/HSBCAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/hsbc/HSBCAgentTest.java
@@ -1,11 +1,23 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.hsbc;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.utils.random.RandomUtils;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.amount.ExactCurrencyAmount;
+import se.tink.libraries.payment.rpc.Creditor;
+import se.tink.libraries.payment.rpc.Payment;
+import se.tink.libraries.payment.rpc.Reference;
 
 @Ignore
 public class HSBCAgentTest {
+    private final String SOURCE_IDENTIFIER = "";
+    private final String DESTINATION_IDENTIFIER = "";
+
     @Test
     public void test() throws Exception {
         new AgentIntegrationTest.Builder("uk", "uk-hsbc-oauth2")
@@ -16,5 +28,39 @@ public class HSBCAgentTest {
                 .setAppId("tink")
                 .build()
                 .testRefresh();
+    }
+
+    @Test
+    public void testPayments() throws Exception {
+        AgentIntegrationTest.Builder builder =
+                new AgentIntegrationTest.Builder("uk", "uk-hsbc-oauth2")
+                        .expectLoggedIn(false)
+                        .loadCredentialsBefore(false)
+                        .saveCredentialsAfter(false)
+                        .setFinancialInstitutionId("hsbc")
+                        .setAppId("tink");
+        builder.build().testGenericPaymentUKOB(createMockedDomesticPayment());
+    }
+
+    private List<Payment> createMockedDomesticPayment() {
+        ArrayList<Payment> payments = new ArrayList<>();
+        ExactCurrencyAmount amount = ExactCurrencyAmount.of("1.00", "GBP");
+        LocalDate executionDate = LocalDate.now();
+        String currency = "GBP";
+        payments.add(
+                new Payment.Builder()
+                        .withCreditor(
+                                new Creditor(
+                                        AccountIdentifier.create(
+                                                AccountIdentifier.Type.SORT_CODE,
+                                                DESTINATION_IDENTIFIER),
+                                        "anyname"))
+                        .withExactCurrencyAmount(amount)
+                        .withExecutionDate(executionDate)
+                        .withCurrency(currency)
+                        .withReference(new Reference("TRANSFER", "Test Tink"))
+                        .withUniqueId(RandomUtils.generateRandomHexEncoded(15))
+                        .build());
+        return payments;
     }
 }
