54e0c438f87ef4bd26123563ff138ffa71d3ee82
Annica Alstermark <annica.alstermark@tink.se>
2020-02-11 11:36:33 +0100

    feat(First Direct): Implement PIS

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/firstdirect/FirstDirectConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/firstdirect/FirstDirectConstants.java
@@ -7,9 +7,12 @@ final class FirstDirectConstants {
         class V31 {
             public static final String AIS_API_URL =
                     "https://private.api.firstdirect.com/open-banking/v3.1/aisp";
-            public static final String PIS_API_URL = "";
+            public static final String PIS_API_URL =
+                    "https://private.api.firstdirect.com/open-banking/v3.1/pisp";
             public static final String WELL_KNOWN_URL =
                     "https://developer.hsbc.com/firstdirect/.well-known/openid-configuration";
+            public static final String APP_TO_APP_AUTH_URL =
+                    "https://openbanking.firstdirect.com/open-banking/personal/v1.1/authorize";
         }
     }
 }
