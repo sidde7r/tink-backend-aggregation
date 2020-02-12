983b43bc62a1c640b1d8ea268ad0110afbe3dc6a
Annica Alstermark <annica.alstermark@tink.se>
2020-02-12 13:52:20 +0100

    feat(UK/Santander): Implement PIS

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/santander/SantanderConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/santander/SantanderConstants.java
@@ -7,7 +7,8 @@ final class SantanderConstants {
         class V31 {
             public static final String AIS_API_URL =
                     "https://openbanking-ma.santander.co.uk/sanuk/external/open-banking/v3.1/aisp";
-            public static final String PIS_API_URL = "";
+            public static final String PIS_API_URL =
+                    "https://openbanking-ma.santander.co.uk/sanuk/external/open-banking/v3.1/pisp";
             public static final String WELL_KNOWN_URL =
                     "https://openbanking.santander.co.uk/sanuk/external/open-banking/openid-connect-provider/v1/.well-known/openid-configuration";
         }
