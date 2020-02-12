fbd863e7ee48351dc6e907b8239b632411cfc770
Berk Gedik <berk.gedik@tink.se>
2020-02-12 11:37:05 +0100

    feat(Events): Implementation for DataTrackerEventProducer

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/DataTrackerEventProducer.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/DataTrackerEventProducer.java
@@ -1,7 +1,11 @@
 package se.tink.backend.aggregation.events;
 
+import com.google.protobuf.Any;
+import java.time.Instant;
 import javax.inject.Inject;
+import se.tink.eventproducerservice.events.grpc.DataTrackerEventProto.DataTrackerEvent;
 import se.tink.libraries.event_producer_service_client.grpc.EventProducerServiceClient;
+import se.tink.libraries.serialization.proto.utils.ProtobufTypeUtil;
 
 public class DataTrackerEventProducer {
 
@@ -10,5 +14,28 @@ public class DataTrackerEventProducer {
     @Inject
     public DataTrackerEventProducer(EventProducerServiceClient eventProducerServiceClient) {
         this.eventProducerServiceClient = eventProducerServiceClient;
+    }
+
+    public void sendDataTrackerEvent(
+            String providerName,
+            String correlationId,
+            String fieldName,
+            boolean hasValue,
+            String appId,
+            String clusterId,
+            String userId) {
+        DataTrackerEvent event =
+                DataTrackerEvent.newBuilder()
+                        .setTimestamp(ProtobufTypeUtil.toProtobufTimestamp(Instant.now()))
+                        .setProviderName(providerName)
+                        .setCorrelationId(correlationId)
+                        .setFieldName(fieldName)
+                        .setHasValue(hasValue)
+                        .setAppId(appId)
+                        .setClusterId(clusterId)
+                        .setUserId(userId)
+                        .build();
+
+        eventProducerServiceClient.postEventAsync(Any.pack(event));
     }
 }
