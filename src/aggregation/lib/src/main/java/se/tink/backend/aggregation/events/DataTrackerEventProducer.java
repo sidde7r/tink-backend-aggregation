e402a19e83c7aa1111dd92ed0fb22132fa71ab76
Berk Gedik <berk.gedik@tink.se>
2020-02-12 13:32:54 +0100

    feat(Events): Added error handling and logging for DataTrackerEventProducer

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/DataTrackerEventProducer.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/DataTrackerEventProducer.java
@@ -3,6 +3,8 @@ package se.tink.backend.aggregation.events;
 import com.google.protobuf.Any;
 import java.time.Instant;
 import javax.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.eventproducerservice.events.grpc.DataTrackerEventProto.DataTrackerEvent;
 import se.tink.libraries.event_producer_service_client.grpc.EventProducerServiceClient;
 import se.tink.libraries.serialization.proto.utils.ProtobufTypeUtil;
@@ -10,6 +12,7 @@ import se.tink.libraries.serialization.proto.utils.ProtobufTypeUtil;
 public class DataTrackerEventProducer {
 
     private final EventProducerServiceClient eventProducerServiceClient;
+    private static final Logger log = LoggerFactory.getLogger(DataTrackerEventProducer.class);
 
     @Inject
     public DataTrackerEventProducer(EventProducerServiceClient eventProducerServiceClient) {
@@ -24,18 +27,22 @@ public class DataTrackerEventProducer {
             String appId,
             String clusterId,
             String userId) {
-        DataTrackerEvent event =
-                DataTrackerEvent.newBuilder()
-                        .setTimestamp(ProtobufTypeUtil.toProtobufTimestamp(Instant.now()))
-                        .setProviderName(providerName)
-                        .setCorrelationId(correlationId)
-                        .setFieldName(fieldName)
-                        .setHasValue(hasValue)
-                        .setAppId(appId)
-                        .setClusterId(clusterId)
-                        .setUserId(userId)
-                        .build();
+        try {
+            DataTrackerEvent event =
+                    DataTrackerEvent.newBuilder()
+                            .setTimestamp(ProtobufTypeUtil.toProtobufTimestamp(Instant.now()))
+                            .setProviderName(providerName)
+                            .setCorrelationId(correlationId)
+                            .setFieldName(fieldName)
+                            .setHasValue(hasValue)
+                            .setAppId(appId)
+                            .setClusterId(clusterId)
+                            .setUserId(userId)
+                            .build();
 
-        eventProducerServiceClient.postEventAsync(Any.pack(event));
+            eventProducerServiceClient.postEventAsync(Any.pack(event));
+        } catch (Exception e) {
+            log.error("Could not push Data Tracker event", e);
+        }
     }
 }
