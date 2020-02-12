c690b69de4ec18b83ab8b46060b69e97ae0441cd
Annica Alstermark <annica.alstermark@tink.se>
2020-02-12 13:53:08 +0100

    test(UK/Santander): Add payment test to agent test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/santander/SantanderAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/santander/SantanderAgentTest.java
@@ -1,11 +1,26 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.santander;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.utils.random.RandomUtils;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.account.AccountIdentifier.Type;
+import se.tink.libraries.amount.ExactCurrencyAmount;
+import se.tink.libraries.payment.rpc.Creditor;
+import se.tink.libraries.payment.rpc.Debtor;
+import se.tink.libraries.payment.rpc.Payment;
+import se.tink.libraries.payment.rpc.Reference;
 
 @Ignore
 public class SantanderAgentTest {
+
+    private final String SOURCE_IDENTIFIER = "";
+    private final String DESTINATION_IDENTIFIER = "";
+
     @Test
     public void test() throws Exception {
         new AgentIntegrationTest.Builder("uk", "uk-santander-oauth2")
@@ -16,5 +31,47 @@ public class SantanderAgentTest {
                 .setAppId("tink")
                 .build()
                 .testRefresh();
+    }
+
+    @Test
+    public void testPayments() throws Exception {
+        AgentIntegrationTest.Builder builder =
+                new AgentIntegrationTest.Builder("uk", "uk-santander-oauth2")
+                        .expectLoggedIn(false)
+                        .loadCredentialsBefore(false)
+                        .saveCredentialsAfter(false)
+                        .setFinancialInstitutionId("santander")
+                        .setAppId("tink");
+
+        builder.build().testGenericPaymentUKOB(createMockedDomesticPayment());
+    }
+
+    private List<Payment> createMockedDomesticPayment() {
+
+        List<Payment> payments = new ArrayList<>();
+        ExactCurrencyAmount amount = ExactCurrencyAmount.of("1.00", "GBP");
+        LocalDate executionDate = LocalDate.now();
+        String currency = "GBP";
+
+        payments.add(
+                new Payment.Builder()
+                        .withCreditor(
+                                new Creditor(
+                                        AccountIdentifier.create(
+                                                AccountIdentifier.Type.SORT_CODE,
+                                                DESTINATION_IDENTIFIER),
+                                        "Unknown person"))
+                        .withDebtor(
+                                new Debtor(
+                                        AccountIdentifier.create(
+                                                Type.SORT_CODE, SOURCE_IDENTIFIER)))
+                        .withExactCurrencyAmount(amount)
+                        .withExecutionDate(executionDate)
+                        .withCurrency(currency)
+                        .withReference(new Reference("TRANSFER", "test Tink"))
+                        .withUniqueId(RandomUtils.generateRandomHexEncoded(15))
+                        .build());
+
+        return payments;
     }
 }
