52e4e2650ff64cdb26b40fd603d93d022bc5527c
Nazri Abdullah <nazri.abdullah@tink.se>
2020-02-15 11:01:00 +0100

    fix(nl/rabobank): trying to fix issue invalid token used in request

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/rabobank/authenticator/RabobankAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/rabobank/authenticator/RabobankAuthenticator.java
@@ -102,7 +102,9 @@ public class RabobankAuthenticator implements OAuth2Authenticator {
     }
 
     @Override
-    public void useAccessToken(final OAuth2Token accessToken) {}
+    public void useAccessToken(final OAuth2Token accessToken) {
+        persistentStorage.put(StorageKey.OAUTH_TOKEN, accessToken);
+    }
 
     private String getConsentId(String scope) {
         String consentId = null;
