5a3e848630878d94de2fb0bd00b6e8a931cad079
Nazri Abdullah <nazri.abdullah@tink.se>
2020-02-17 21:46:38 +0100

    refactor(se/handelsbanken): replaced Instrument  with InstrumentModule

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/fetcher/investment/entities/SecurityHolding.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/fetcher/investment/entities/SecurityHolding.java
@@ -1,25 +1,25 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.fetcher.investment.entities;
 
 import java.util.Optional;
-import se.tink.backend.aggregation.agents.models.Instrument;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.HandelsbankenSEApiClient;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.fetcher.investment.rpc.HandelsbankenSEFundAccountHoldingDetail;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.HandelsbankenConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.rpc.BaseResponse;
+import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.instrument.InstrumentModule;
 import se.tink.backend.aggregation.nxgen.http.url.URL;
 
 public class SecurityHolding extends BaseResponse {
 
-    public Optional<Instrument> toInstrument(HandelsbankenSEApiClient client) {
+    public Optional<InstrumentModule> toInstrumentModule(HandelsbankenSEApiClient client) {
 
         Optional<URL> fundDetailsLink =
                 searchLink(HandelsbankenConstants.URLS.Links.FUND_HOLDING_DETAILS);
         if (fundDetailsLink.isPresent()) {
             return client.fundHoldingDetail(this)
-                    .flatMap(HandelsbankenSEFundAccountHoldingDetail::toInstrument);
+                    .flatMap(HandelsbankenSEFundAccountHoldingDetail::toInstrumentModule);
         }
 
-        return client.securityHolding(this).flatMap(SecurityHoldingContainer::toInstrument);
+        return client.securityHolding(this).flatMap(SecurityHoldingContainer::toInstrumentModule);
     }
 
     public Optional<URL> toSecurityHolding() {
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/fetcher/investment/entities/SecurityHoldingList.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/fetcher/investment/entities/SecurityHoldingList.java
@@ -3,18 +3,18 @@ package se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.fetcher.
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
-import se.tink.backend.aggregation.agents.models.Instrument;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.HandelsbankenSEApiClient;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.instrument.InstrumentModule;
 
 @JsonObject
 public class SecurityHoldingList {
     private List<SecurityHolding> holdingList;
 
-    public Stream<Optional<Instrument>> toInstruments(HandelsbankenSEApiClient client) {
+    public Stream<Optional<InstrumentModule>> toInstrumentModules(HandelsbankenSEApiClient client) {
         if (holdingList == null) {
             return Stream.empty();
         }
-        return holdingList.stream().map(holdingList -> holdingList.toInstrument(client));
+        return holdingList.stream().map(holdingList -> holdingList.toInstrumentModule(client));
     }
 }
