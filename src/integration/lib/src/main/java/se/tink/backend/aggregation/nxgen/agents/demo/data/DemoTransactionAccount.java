dd74118f48ebbdfd521648aae8786879c5b34806
Ahmed Hussein <ahmed.hussein@tink.se>
2020-02-18 15:49:08 +0100

    ref(Demo): add default credit & available balance methods to accounts
    DemoTransactionAccount and DemoSavingsAccount

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/agents/demo/data/DemoTransactionAccount.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/agents/demo/data/DemoTransactionAccount.java
@@ -1,6 +1,7 @@
 package se.tink.backend.aggregation.nxgen.agents.demo.data;
 
 import java.util.List;
+import java.util.Optional;
 import se.tink.libraries.account.AccountIdentifier;
 
 public interface DemoTransactionAccount {
@@ -11,4 +12,12 @@ public interface DemoTransactionAccount {
     double getBalance();
 
     List<AccountIdentifier> getIdentifiers();
+
+    default Optional<Double> getAvailableBalance() {
+        return Optional.empty();
+    }
+
+    default Optional<Double> getCreditLimit() {
+        return Optional.empty();
+    }
 }
