f9aa0050f3231cbbd500bec26ce30ed42dfd2771
Sebastian Olsson <sebastian.olsson@tink.se>
2020-02-18 09:12:50 +0100

    test(data-tracker-event-producer): Added unit test

---
+++ src/aggregation/lib/src/test/java/se/tink/backend/aggregation/events/DataTrackerEventProducerTest.java
@@ -0,0 +1,97 @@
+package se.tink.backend.aggregation.events;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.protobuf.Any;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Descriptors.FieldDescriptor;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.internal.util.collections.Sets;
+import se.tink.backend.eventproducerservice.grpc.EventAck;
+import se.tink.backend.eventproducerservice.grpc.PostEventRequest;
+import se.tink.libraries.event_producer_service_client.grpc.EventProducerServiceClient;
+
+public final class DataTrackerEventProducerTest {
+
+    private static class FakeEventProducerServiceClient implements EventProducerServiceClient {
+
+        private Any postedData;
+
+        @Override
+        public ListenableFuture<EventAck> postEventAsync(Any data) {
+            postedData = data;
+            return null;
+        }
+
+        @Override
+        public EventAck postEvent(PostEventRequest request) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public ListenableFuture<EventAck> postEventAsync(PostEventRequest request) {
+            throw new UnsupportedOperationException();
+        }
+
+        Any getPostedData() {
+            return Optional.ofNullable(postedData).orElseThrow(IllegalStateException::new);
+        }
+    }
+
+    @Test
+    public void sendDataTrackerEventCallsPostEventAsync() {
+        // given
+        final FakeEventProducerServiceClient producerClient = new FakeEventProducerServiceClient();
+
+        final DataTrackerEventProducer producer = new DataTrackerEventProducer(producerClient);
+
+        final String providerName = "hoy";
+        final String correlationId = "hoy";
+        final String fieldName = "hoy";
+        final boolean hasValue = true;
+        final String appId = "hoy";
+        final String clusterId = "hoy";
+        final String userId = "hoy";
+
+        // when
+        producer.sendDataTrackerEvent(
+                providerName, correlationId, fieldName, hasValue, appId, clusterId, userId);
+
+        final Map<FieldDescriptor, Object> fields = producerClient.getPostedData().getAllFields();
+        final Set<String> keys =
+                fields.keySet().stream().map(FieldDescriptor::getName).collect(Collectors.toSet());
+        final Collection<Object> values = fields.values();
+
+        // then
+        Assert.assertEquals(2, fields.size());
+
+        final String typeUrlValue =
+                (String)
+                        fields.entrySet().stream()
+                                .filter(e -> Objects.equals(e.getKey().getName(), "type_url"))
+                                .findFirst()
+                                .get()
+                                .getValue();
+        final byte[] valueValue =
+                ((ByteString)
+                                fields.entrySet().stream()
+                                        .filter(e -> Objects.equals(e.getKey().getName(), "value"))
+                                        .findFirst()
+                                        .get()
+                                        .getValue())
+                        .toByteArray();
+
+        assertThat(keys, is(Sets.newSet("type_url", "value")));
+        Assert.assertEquals("type.googleapis.com/proto.DataTrackerEvent", typeUrlValue);
+        Assert.assertTrue(new String(valueValue).contains("hoy"));
+    }
+}
