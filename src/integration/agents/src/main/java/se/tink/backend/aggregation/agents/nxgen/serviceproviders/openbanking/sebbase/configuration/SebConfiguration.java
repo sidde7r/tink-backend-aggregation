6fae3a5fcb11ab16daab82f9d549c28deb0945bb
Nazri Abdullah <nazri.abdullah@tink.se>
2020-02-20 15:14:42 +0100

    feat(se/seb-ob): updated configuration class with null check

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbase/configuration/SebConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbase/configuration/SebConfiguration.java
@@ -1,6 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.SebCommonConstants.ErrorMessages;
 import se.tink.backend.aggregation.annotations.AgentConfigParam;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
@@ -9,21 +12,32 @@ import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
 
 @JsonObject
 public class SebConfiguration implements ClientConfiguration {
+
     @JsonProperty @Secret private String clientId;
-
     @JsonProperty @SensitiveSecret private String clientSecret;
-
     @JsonProperty @AgentConfigParam private String redirectUrl;
 
     public String getClientId() {
+        Preconditions.checkNotNull(
+                Strings.emptyToNull(clientId),
+                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client ID"));
+
         return clientId;
     }
 
     public String getClientSecret() {
+        Preconditions.checkNotNull(
+                Strings.emptyToNull(clientSecret),
+                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client Secret"));
+
         return clientSecret;
     }
 
     public String getRedirectUrl() {
+        Preconditions.checkNotNull(
+                Strings.emptyToNull(redirectUrl),
+                String.format(ErrorMessages.INVALID_CONFIGURATION, "Redirect Url"));
+
         return redirectUrl;
     }
 }
