e4f3ef57e6393a53136a735a445daabef167b761
Nazri Abdullah <nazri.abdullah@tink.se>
2020-02-20 13:37:19 +0100

    feat(se/seb-ob): updated parameters for token request

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbase/authenticator/rpc/TokenRequest.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbase/authenticator/rpc/TokenRequest.java
@@ -1,39 +1,39 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.authenticator.rpc;
 
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.SebCommonConstants;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.SebCommonConstants.QueryKeys;
 import se.tink.backend.aggregation.nxgen.http.form.Form;
 
 public class TokenRequest {
     private final String clientId;
     private final String clientSecret;
     private final String redirectUri;
-    private final String code;
     private final String grantType;
+    private final String code;
     private final String scope;
 
     public TokenRequest(
             String clientId,
             String clientSecret,
             String redirectUri,
-            String code,
             String grantType,
+            String code,
             String scope) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.redirectUri = redirectUri;
-        this.code = code;
         this.grantType = grantType;
+        this.code = code;
         this.scope = scope;
     }
 
     public String toData() {
         return Form.builder()
-                .put(SebCommonConstants.QueryKeys.CLIENT_ID, clientId)
-                .put(SebCommonConstants.QueryKeys.CLIENT_SECRET, clientSecret)
-                .put(SebCommonConstants.QueryKeys.REDIRECT_URI, redirectUri)
-                .put(SebCommonConstants.QueryKeys.CODE, code)
-                .put(SebCommonConstants.QueryKeys.GRANT_TYPE, grantType)
-                .put(SebCommonConstants.QueryKeys.SCOPE, scope)
+                .put(QueryKeys.CLIENT_ID, clientId)
+                .put(QueryKeys.CLIENT_SECRET, clientSecret)
+                .put(QueryKeys.REDIRECT_URI, redirectUri)
+                .put(QueryKeys.GRANT_TYPE, grantType)
+                .put(QueryKeys.CODE, code)
+                .put(QueryKeys.SCOPE, scope)
                 .build()
                 .serialize();
     }
