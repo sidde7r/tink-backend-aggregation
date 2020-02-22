83f7a6c2e20e932ced502ff963089a1590692b84
Sebastian Olsson <sebastian.olsson@tink.se>
2020-02-22 19:52:38 +0100

    refactor(agent_tracker/client): Moved responsibilities out of class

---
+++ src/integration/agent_data_availability_tracker/client/src/test/java/se/tink/backend/integration/agent_data_availability_tracker/client/grpc/PlaintextChannelProvider.java
@@ -0,0 +1,24 @@
+package se.tink.backend.integration.agent_data_availability_tracker.client.grpc;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import io.grpc.ManagedChannel;
+import io.grpc.netty.NettyChannelBuilder;
+import java.net.InetSocketAddress;
+
+public final class PlaintextChannelProvider implements Provider<ManagedChannel> {
+
+    private final InetSocketAddress socket;
+
+    @Inject
+    private PlaintextChannelProvider(final InetSocketAddress socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public ManagedChannel get() {
+        return NettyChannelBuilder.forAddress(socket.getHostName(), socket.getPort())
+                .usePlaintext()
+                .build();
+    }
+}
