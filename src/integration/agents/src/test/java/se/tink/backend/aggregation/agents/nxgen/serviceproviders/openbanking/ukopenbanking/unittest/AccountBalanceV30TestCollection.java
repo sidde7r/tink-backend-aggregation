063191c9c1e1d28622b63e79b9ba95e34da82423
Szymon Mysiak <szymon.mysiak@tink.se>
2020-02-25 11:45:41 +0100

    refactor(ukob): remove unusued v30 spec classess

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/unittest/AccountBalanceV30TestCollection.java
+++
@@ -1,5 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.unittest;
-
-import java.util.ArrayList;
-
-public class AccountBalanceV30TestCollection extends ArrayList<AccountBalanceV30TestEntity> {}
