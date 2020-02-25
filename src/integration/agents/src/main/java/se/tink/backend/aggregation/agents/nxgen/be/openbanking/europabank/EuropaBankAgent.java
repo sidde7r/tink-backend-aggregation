519ec541fdd9d1c55985c0570ad8b0662ed9a42d
Krzysztof Korolczuk <krzysztof.korolczuk@tink.se>
2020-02-25 16:25:08 +0100

    feat(BE): IFD 815 missing agents for EquensWordline service provider

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/europabank/EuropaBankAgent.java
@@ -0,0 +1,41 @@
+package se.tink.backend.aggregation.agents.nxgen.be.openbanking.europabank;
+
+import java.time.temporal.ChronoUnit;
+import se.tink.backend.aggregation.agents.AgentContext;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.Xs2aDevelopersTransactionalAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.fetcher.transactionalaccount.Xs2aDevelopersTransactionalAccountFetcher;
+import se.tink.backend.aggregation.configuration.SignatureKeyPair;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcherController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginationController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+
+public final class EuropaBankAgent extends Xs2aDevelopersTransactionalAgent {
+
+    private static final String INTEGRATION_NAME = "europabank";
+
+    public EuropaBankAgent(
+            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
+        super(request, context, signatureKeyPair);
+    }
+
+    @Override
+    protected String getIntegrationName() {
+        return INTEGRATION_NAME;
+    }
+
+    @Override
+    protected TransactionalAccountRefreshController getTransactionalAccountRefreshController() {
+        final Xs2aDevelopersTransactionalAccountFetcher accountFetcher =
+                new Xs2aDevelopersTransactionalAccountFetcher(apiClient);
+
+        return new TransactionalAccountRefreshController(
+                metricRefreshController,
+                updateController,
+                accountFetcher,
+                new TransactionFetcherController<>(
+                        transactionPaginationHelper,
+                        new TransactionDatePaginationController<>(
+                                accountFetcher, 4, 15, ChronoUnit.DAYS)));
+    }
+}
---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/nagelmackers/NagelmackersAgentTest.java
@@ -0,0 +1,40 @@
+package se.tink.backend.aggregation.agents.nxgen.be.openbanking.nagelmackers;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager.IbanArgumentEnum;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.Xs2aDevelopersConstants.CredentialKeys;
+
+public class NagelmackersAgentTest {
+
+    private final ArgumentManager<IbanArgumentEnum> manager =
+            new ArgumentManager<>(IbanArgumentEnum.values());
+    private AgentIntegrationTest.Builder builder;
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
+    @Before
+    public void setup() {
+        manager.before();
+
+        builder =
+                new AgentIntegrationTest.Builder("be", "be-nagelmackers-ob")
+                        .addCredentialField(CredentialKeys.IBAN, manager.get(IbanArgumentEnum.IBAN))
+                        .expectLoggedIn(false)
+                        .loadCredentialsBefore(false)
+                        .saveCredentialsAfter(false)
+                        .setFinancialInstitutionId("nagelmackers")
+                        .setAppId("tink");
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder.build().testRefresh();
+    }
+}
