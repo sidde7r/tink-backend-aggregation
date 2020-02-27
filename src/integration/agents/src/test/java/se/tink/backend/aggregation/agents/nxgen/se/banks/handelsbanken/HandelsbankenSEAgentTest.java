42e7c1bc691b0b9941973222c9c61cdd218eae84
Ankur madan <ankur.madan@tink.se>
2020-02-27 12:58:18 +0100

    fix(Agg): Add NPE fix & Payment test case

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/HandelsbankenSEAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/HandelsbankenSEAgentTest.java
@@ -7,13 +7,7 @@ import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager.SsnArgumentEnum;
-import se.tink.libraries.account.AccountIdentifier;
-import se.tink.libraries.amount.Amount;
 import se.tink.libraries.credentials.service.RefreshableItem;
-import se.tink.libraries.transfer.enums.TransferType;
-import se.tink.libraries.transfer.rpc.Transfer;
-
-import java.util.Calendar;
 
 public class HandelsbankenSEAgentTest {
     private AgentIntegrationTest.Builder builder =
@@ -44,27 +38,5 @@ public class HandelsbankenSEAgentTest {
                 .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
                 .build()
                 .testRefresh();
-    }
-
-    @Test
-    public void testPayment() throws Exception {
-        Transfer transfer = new Transfer();
-        transfer.setSource(
-                AccountIdentifier.create(AccountIdentifier.Type.SE,"" ));
-        transfer.setDestination(AccountIdentifier.create(AccountIdentifier.Type.SE_BG, "900-8004"));
-        transfer.setAmount(Amount.inSEK(1d));
-        transfer.setType(TransferType.PAYMENT);
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.DAY_OF_MONTH, 1);
-        transfer.setDueDate(c.getTime());
-        transfer.setDestinationMessage("32456789");
-
-        new AgentIntegrationTest.Builder("se", "handelsbanken-bankid")
-                .addCredentialField(Field.Key.USERNAME, manager.get(SsnArgumentEnum.SSN))
-                .loadCredentialsBefore(false)
-                .saveCredentialsAfter(false)
-                .doLogout(true)
-                .build()
-                .testBankTransfer(transfer);
     }
 }
---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/executor/HandelsbankenPaymentFlowTest.java
@@ -0,0 +1,83 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.executor;
+
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.amount.Amount;
+import se.tink.libraries.date.CountryDateHelper;
+import se.tink.libraries.transfer.enums.TransferType;
+import se.tink.libraries.transfer.rpc.Transfer;
+
+public class HandelsbankenPaymentFlowTest {
+
+    private enum Arg implements ArgumentManager.ArgumentManagerEnum {
+        SSN,
+        SRC_ACCOUNT,
+        DEST_BG_ACCOUNT; // Sweden Redcross BG number (900-8004)
+
+        @Override
+        public boolean isOptional() {
+            return false;
+        }
+    }
+
+    private final ArgumentManager<HandelsbankenPaymentFlowTest.Arg> manager =
+            new ArgumentManager<>(HandelsbankenPaymentFlowTest.Arg.values());
+
+    @Before
+    public void setUp() throws Exception {
+        manager.before();
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        ArgumentManager.afterClass();
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        Transfer transfer = createTransfer();
+
+        new AgentIntegrationTest.Builder("se", "handelsbanken-bankid")
+                .addCredentialField(
+                        Field.Key.USERNAME, manager.get(HandelsbankenPaymentFlowTest.Arg.SSN))
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(false)
+                .doLogout(true)
+                .build()
+                .testBankTransfer(transfer);
+    }
+
+    private Transfer createTransfer() {
+        Locale locale = new Locale("sv", "SE");
+        TimeZone timezone = TimeZone.getTimeZone("CET");
+        CountryDateHelper dateHelper = new CountryDateHelper(locale, timezone);
+
+        Transfer transfer = new Transfer();
+        transfer.setSource(
+                AccountIdentifier.create(
+                        AccountIdentifier.Type.SE,
+                        manager.get(HandelsbankenPaymentFlowTest.Arg.SRC_ACCOUNT)));
+
+        transfer.setDestination(
+                AccountIdentifier.create(
+                        AccountIdentifier.Type.SE_BG,
+                        manager.get(HandelsbankenPaymentFlowTest.Arg.DEST_BG_ACCOUNT)));
+        transfer.setAmount(Amount.inSEK(1d));
+        transfer.setType(TransferType.PAYMENT);
+        Calendar c = Calendar.getInstance();
+        c.add(Calendar.DAY_OF_MONTH, 1);
+        transfer.setDueDate(c.getTime());
+        transfer.setDestinationMessage("0123456789");
+
+        return transfer;
+    }
+}
