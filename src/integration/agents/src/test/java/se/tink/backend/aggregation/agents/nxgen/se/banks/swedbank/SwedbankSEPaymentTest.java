c2202e9187748282a5b4f57efb744192c3922c24
Annica Alstermark <annica.alstermark@tink.se>
2020-02-28 13:35:58 +0100

    test(Swedbank): Move payment test existing payment test class

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/SwedbankSEPaymentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/SwedbankSEPaymentTest.java
@@ -1,6 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank;
 
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +19,13 @@ import se.tink.libraries.transfer.rpc.Transfer;
 
 /** Careful with this test! Actual transfers made! */
 public class SwedbankSEPaymentTest {
+
+    final AgentIntegrationTest.Builder builder =
+            new AgentIntegrationTest.Builder("SE", "swedbank-bankid")
+                    .loadCredentialsBefore(false)
+                    .saveCredentialsAfter(false)
+                    .expectLoggedIn(false);
+
     private final ArgumentManager<SsnArgumentEnum> ssnManager =
             new ArgumentManager<>(SsnArgumentEnum.values());
     private final ArgumentManager<ToAccountFromAccountArgumentEnum> toFromManager =
@@ -35,15 +44,6 @@ public class SwedbankSEPaymentTest {
 
     @Test
     public void testTransfer() throws Exception {
-        final AgentIntegrationTest.Builder builder =
-                new AgentIntegrationTest.Builder("SE", "swedbank-bankid")
-                        .addCredentialField(Field.Key.USERNAME, ssnManager.get(SsnArgumentEnum.SSN))
-                        .loadCredentialsBefore(false)
-                        .saveCredentialsAfter(false);
-        builder.build().testBankTransfer(createTransfer());
-    }
-
-    private Transfer createTransfer() {
         Transfer transfer = new Transfer();
         transfer.setSource(
                 AccountIdentifier.create(
@@ -54,11 +54,37 @@ public class SwedbankSEPaymentTest {
                         toFromManager.get(ToAccountFromAccountArgumentEnum.TO_ACCOUNT)));
         transfer.setAmount(Amount.inSEK(1d));
         transfer.setType(TransferType.BANK_TRANSFER);
-        Calendar c = Calendar.getInstance();
-        c.add(Calendar.DAY_OF_MONTH, 1);
-        transfer.setDueDate(c.getTime());
+        transfer.setDueDate(getDueDate());
         transfer.setDestinationMessage("This is a test transfer");
 
-        return transfer;
+        builder.addCredentialField(Field.Key.USERNAME, ssnManager.get(SsnArgumentEnum.SSN))
+                .build()
+                .testBankTransfer(transfer);
+    }
+
+    @Test
+    public void testBgPayment() throws Exception {
+        Transfer transfer = new Transfer();
+        transfer.setSource(
+                AccountIdentifier.create(
+                        AccountIdentifier.Type.SE,
+                        toFromManager.get(ToAccountFromAccountArgumentEnum.FROM_ACCOUNT)));
+        transfer.setDestination(
+                AccountIdentifier.create(
+                        Type.SE_BG,
+                        toFromManager.get(ToAccountFromAccountArgumentEnum.TO_ACCOUNT)));
+        transfer.setAmount(Amount.inSEK(1d));
+        transfer.setType(TransferType.PAYMENT);
+        transfer.setDueDate(getDueDate());
+        transfer.setDestinationMessage("Reference");
+
+        builder.addCredentialField(Field.Key.USERNAME, ssnManager.get(SsnArgumentEnum.SSN))
+                .build()
+                .testBankTransfer(transfer);
+    }
+
+    private Date getDueDate() {
+        return Date.from(
+                LocalDate.now().atStartOfDay().plusDays(5).atZone(ZoneId.of("CET")).toInstant());
     }
 }
