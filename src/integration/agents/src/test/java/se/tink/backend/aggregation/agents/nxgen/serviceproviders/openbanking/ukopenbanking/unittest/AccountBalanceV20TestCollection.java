6ecf6861696125e30e2c8afba9b9b030590cd350
Szymon Mysiak <szymon.mysiak@tink.se>
2020-03-03 15:50:47 +0100

    refactor(ukob): remove v20 spec and bank of ireland

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/unittest/AccountBalanceV20TestCollection.java
+++
@@ -1,5 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.unittest;
-
-import java.util.ArrayList;
-
-public class AccountBalanceV20TestCollection extends ArrayList<AccountBalanceV20TestEntity> {}
