6d04dddbc9d1b15cfaa2f61afa0ff2967105e71f
Nazri Abdullah <nazri.abdullah@tink.se>
2020-03-05 17:05:23 +0100

    refactor(se/skandia-ob): removed unused entity classes

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/skandia/fetcher/transactionalaccount/entities/TransactionsLinkEntity.java
+++
@@ -1,9 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.se.openbanking.skandia.fetcher.transactionalaccount.entities;
-
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class TransactionsLinkEntity {
-
-    private String href;
-}
