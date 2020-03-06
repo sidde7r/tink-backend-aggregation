56ef64d2467c63ba93fdac5b1812e17162fdca9c
Sebastian Olsson <sebastian.olsson@tink.se>
2020-03-06 10:49:18 +0100

    ci(unit-tests): Split up long line

--- .buildkite/run-unit-tests.sh
+++ .buildkite/run-unit-tests.sh
@@ -12,7 +12,15 @@ if [ $BUILD_OUTCOME -ne 0 ]; then
     exit $BUILD_OUTCOME
 fi
 
-./bazel-wrapper test --workspace_status_command $(pwd)/stamp.sh --disk_cache=/cache/v4-disk --repository_cache=/cache/v4-repo --deleted_packages=deb,docker --curses=yes --color=yes -- ...:all
+./bazel-wrapper test \
+    --workspace_status_command $(pwd)/stamp.sh \
+    --disk_cache=/cache/v4-disk \
+    --repository_cache=/cache/v4-repo \
+    --deleted_packages=deb,docker \
+    --curses=yes \
+    --color=yes \
+    -- \
+    ...:all
 OUTCOME=$?
 .buildkite/upload-test-files.sh
 exit $OUTCOME
