a60fcbaa2e4b869a950af97a30c2af4952634ec9
Nazri Abdullah <nazri.abdullah@tink.se>
2020-03-10 16:47:10 +0100

    fix(nl/volksbank): added consent status check and handler

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/authenticator/ConsentFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/authenticator/ConsentFetcher.java
@@ -1,5 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.authenticator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceError;
 import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.VolksbankApiClient;
 import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.VolksbankConstants.Storage;
 import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.authenticator.rpc.ConsentResponse;
@@ -7,6 +10,8 @@ import se.tink.backend.aggregation.nxgen.http.url.URL;
 import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
 
 public final class ConsentFetcher {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsentFetcher.class);
 
     private final VolksbankApiClient client;
     private final PersistentStorage persistentStorage;
@@ -27,14 +32,26 @@ public final class ConsentFetcher {
     /** @return A consent ID, either by asking the bank for it or by getting it from storage. */
     public String fetchConsent() {
         final String consentId;
+
         if (persistentStorage.containsKey(Storage.CONSENT)) {
             consentId = persistentStorage.get(Storage.CONSENT);
         } else {
             final ConsentResponse consent = client.consentRequest(redirectUrl, clientId);
             consentId = consent.getConsentId();
         }
+        isConsentValid(consentId);
         persistentStorage.put(Storage.CONSENT, consentId);
+
         return consentId;
+    }
+
+    private void isConsentValid(String consentId) {
+        final String consentStatus =
+                client.consentStatusRequest(clientId, consentId).getConsentStatus();
+        if (!consentStatus.equalsIgnoreCase("valid")) {
+            log.info("Consent status: {}", consentStatus);
+            throw BankServiceError.CONSENT_EXPIRED.exception();
+        }
     }
 
     public String getClientId() {
