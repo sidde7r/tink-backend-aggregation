076da5e9b9773720b3e0f99e7dd0c594ed283209
Krzysztof Korolczuk <krzysztof.korolczuk@tink.se>
2020-03-11 09:25:23 +0100

    fix(ing): meaningful message when certificate is invalid and tests

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ingbase/utils/IngBaseUtils.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ingbase/utils/IngBaseUtils.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ingbase.utils;
 
+import com.google.common.base.Strings;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.Base64;
@@ -9,11 +10,20 @@ public final class IngBaseUtils {
 
     private IngBaseUtils() {}
 
-    public static String getCertificateSerial(String encodedCertificate)
-            throws CertificateException {
-        final X509Certificate certificate =
-                (X509Certificate)
-                        Pem.parseCertificate(Base64.getDecoder().decode(encodedCertificate));
-        return String.format("SN=%x", certificate.getSerialNumber());
+    public static String getCertificateSerial(String encodedCertificate) {
+        if (Strings.isNullOrEmpty(encodedCertificate)) {
+            throw new IllegalStateException(
+                    "Provided certificate is null or empty, make sure secrets are uploaded.");
+        }
+
+        try {
+            final X509Certificate certificate =
+                    (X509Certificate)
+                            Pem.parseCertificate(Base64.getDecoder().decode(encodedCertificate));
+            return String.format("SN=%x", certificate.getSerialNumber());
+        } catch (CertificateException | IllegalArgumentException e) {
+            throw new IllegalStateException(
+                    "Provided certificate is null or empty, make sure secrets are uploaded.");
+        }
     }
 }
