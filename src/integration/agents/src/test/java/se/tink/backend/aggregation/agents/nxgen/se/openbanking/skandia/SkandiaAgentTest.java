899616a2d0bdf1c87d26b6c84385a5b78cad63c9
Nazri Abdullah <nazri.abdullah@tink.se>
2020-03-12 16:58:58 +0100

    chore(se/skandiabanken): renamed provider name/ updated test agent

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/skandia/SkandiaAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/skandia/SkandiaAgentTest.java
@@ -1,11 +1,10 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.skandia;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
-@Ignore
+// @Ignore
 public class SkandiaAgentTest {
 
     private AgentIntegrationTest.Builder builder;
@@ -13,9 +12,11 @@ public class SkandiaAgentTest {
     @Before
     public void setup() {
         builder =
-                new AgentIntegrationTest.Builder("se", "se-skandia-ob")
+                new AgentIntegrationTest.Builder("se", "se-skandiabanken-ob")
                         .expectLoggedIn(false)
-                        .setFinancialInstitutionId("skandia")
+                        .setFinancialInstitutionId("skandiabanken")
+                        .setClusterId("oxford-staging")
+                        .setAppId("tink")
                         .loadCredentialsBefore(false)
                         .saveCredentialsAfter(false);
     }
