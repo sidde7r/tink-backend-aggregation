6c4ada6c73b4eaa7c0c1e81acbd48a77acc8c57e
Berk Gedik <berk.gedik@tink.se>
2020-03-26 09:37:22 +0100

    refactor(ContractAssertor): Moved necessary functions

--- src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/framework/assertions/AgentContractEntitiesAsserts.java
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/framework/assertions/AgentContractEntitiesAsserts.java
@@ -1,17 +1,11 @@
 package se.tink.backend.aggregation.agents.framework.assertions;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 import org.junit.Assert;
 import se.tink.backend.agents.rpc.Account;
-import se.tink.backend.agents.rpc.AccountTypes;
 import se.tink.backend.aggregation.agents.models.Transaction;
-import se.tink.backend.aggregation.agents.models.TransactionTypes;
-import se.tink.libraries.account.enums.AccountExclusion;
-import se.tink.libraries.amount.ExactCurrencyAmount;
 
 public class AgentContractEntitiesAsserts {
 
@@ -87,60 +81,5 @@ public class AgentContractEntitiesAsserts {
                                                 transaction1.getUserId(), transaction2.getUserId());
                             }
                         }));
-    }
-
-    public static Account createAccount(
-            String accountNumber,
-            double availableCredit,
-            double balance,
-            String currencyCode,
-            String bankId,
-            String name,
-            AccountTypes accountType,
-            String holderName) {
-
-        Account account = new Account();
-        account.setAccountNumber(accountNumber);
-        account.setAccountExclusion(AccountExclusion.NONE);
-        account.setExactAvailableCredit(
-                new ExactCurrencyAmount(new BigDecimal(availableCredit), currencyCode));
-        account.setExactBalance(new ExactCurrencyAmount(new BigDecimal(balance), currencyCode));
-        account.setCurrencyCode(currencyCode);
-        account.setBankId(bankId);
-        account.setExcluded(false);
-        account.setFavored(false);
-        account.setName(name);
-        account.setOwnership(1.0);
-        account.setType(accountType);
-        account.setUserModifiedExcluded(false);
-        account.setUserModifiedName(false);
-        account.setUserModifiedType(false);
-        account.setIdentifiers(new ArrayList<>());
-        account.setClosed(false);
-        account.setHolderName(holderName);
-        account.setFlags(new ArrayList<>());
-        account.setAvailableCredit(availableCredit);
-        account.setBalance(balance);
-        return account;
-    }
-
-    public static Transaction createTransaction(
-            String accountId,
-            double amount,
-            long date,
-            String description,
-            boolean pending,
-            boolean upcoming,
-            TransactionTypes type) {
-
-        Transaction transaction = new Transaction();
-        transaction.setAccountId(accountId);
-        transaction.setAmount(amount);
-        transaction.setDate(new Date(date));
-        transaction.setDescription(description);
-        transaction.setPending(pending);
-        transaction.setType(type);
-        transaction.setUpcoming(upcoming);
-        return transaction;
     }
 }
