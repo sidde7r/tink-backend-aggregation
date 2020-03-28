5375bdef273d3185f7f61fb9eb99a554bc48ed6a
Piotr Miecielica <piotr.miecielica@tink.se>
2020-03-28 23:43:27 +0100

    refactor(de/fints): applying code review comments

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/FinTsAccountInformation.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/FinTsAccountInformation.java
@@ -2,7 +2,6 @@ package se.tink.backend.aggregation.agents.nxgen.de.banks.fints;
 
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.experimental.Accessors;
 import se.tink.backend.aggregation.agents.nxgen.de.banks.fints.protocol.parts.response.HISAL;
 import se.tink.backend.aggregation.agents.nxgen.de.banks.fints.protocol.parts.response.HISPA;
@@ -12,8 +11,8 @@ import se.tink.backend.aggregation.agents.nxgen.de.banks.fints.protocol.parts.re
 @Accessors(chain = true)
 public class FinTsAccountInformation {
     @Getter private HIUPD basicInfo;
-    @Getter @Setter private HISPA.Detail sepaDetails;
-    @Getter @Setter private HISAL balance;
+    private HISPA.Detail sepaDetails;
+    private HISAL balance;
 
     public FinTsAccountInformation(HIUPD basicInfo) {
         this.basicInfo = basicInfo;
