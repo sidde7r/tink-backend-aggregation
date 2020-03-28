f1091c321da0f46852c3a043d3cb9622bcfd9a19
Piotr Miecielica <piotr.miecielica@tink.se>
2020-03-28 23:10:17 +0100

    refactor(de/fints): use Pattern in decoder

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/protocol/base64/FinTsBase64.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/protocol/base64/FinTsBase64.java
@@ -2,14 +2,16 @@ package se.tink.backend.aggregation.agents.nxgen.de.banks.fints.protocol.base64;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.regex.Pattern;
 import se.tink.backend.aggregation.agents.nxgen.de.banks.fints.protocol.parts.request.FinTsRequest;
 
 public class FinTsBase64 {
+    private static final Pattern REPLACE_PATTERN = Pattern.compile("\\R");
 
     public static String decodeResponseFromBase64(String b64EncodedResponse) {
+        String responseToDecode = REPLACE_PATTERN.matcher(b64EncodedResponse).replaceAll("");
         return new String(
-                Base64.getDecoder().decode(b64EncodedResponse.replaceAll("\\R", "")),
-                StandardCharsets.ISO_8859_1);
+                Base64.getDecoder().decode(responseToDecode), StandardCharsets.ISO_8859_1);
     }
 
     public static String encodeRequestToBase64(FinTsRequest request) {
