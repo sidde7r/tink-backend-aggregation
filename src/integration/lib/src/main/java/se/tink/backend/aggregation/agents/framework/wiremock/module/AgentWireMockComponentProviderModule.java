5ab6678814b3a4c72c784aa42d90c727210b8afe
Gustaf Svanerud <gustaf.svanerud@tink.se>
2020-03-30 10:20:36 +0200

    refactor(wiremock): move wiremock configurations
    Move WireMock configurations from test to main in order to make it
    available when running the service in decoupled mode.

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/framework/wiremock/module/AgentWireMockComponentProviderModule.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/framework/wiremock/module/AgentWireMockComponentProviderModule.java
@@ -1,4 +1,4 @@
-package se.tink.backend.aggregation.agents.framework.module;
+package se.tink.backend.aggregation.agents.framework.wiremock.module;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
