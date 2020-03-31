a1930e8d69ea10608afc13430f82ddc9165f241b
Schahin Rajab <schahin.rajab@tink.se>
2020-03-31 11:22:12 +0200

    feat(se/swedbank): add swedbank business agent profile

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/serviceprovider/rpc/BankEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/serviceprovider/rpc/BankEntity.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.rpc;
 
+import java.util.List;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
@@ -8,6 +9,7 @@ public class BankEntity {
     private String url;
     private String bankId;
     private PrivateProfileEntity privateProfile;
+    private List<PrivateProfileEntity> corporateProfiles;
 
     public String getName() {
         return name;
@@ -22,6 +24,9 @@ public class BankEntity {
     }
 
     public PrivateProfileEntity getPrivateProfile() {
-        return privateProfile;
+        if (corporateProfiles.isEmpty()) {
+            return privateProfile;
+        }
+        return corporateProfiles.get(0);
     }
 }
