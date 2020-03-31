c26035e4e479f462d3f46364462f3143b60388ba
Gustaf Svanerud <gustaf.svanerud@tink.se>
2020-03-31 15:10:39 +0200

    refactor(ukob/barclays): inject JwtSigner
    UkOpenBankingBaseAgent now has a module that sets up dependencies
    requiered for injecting JwtSigner with instance of LocalKeySigner.
    
    UkOpenBankingBaseAgent also has a new constructor taking the JwtSigner
    directly.
    
    In order to not moving all agents to injecting JwtSigner in this commit
    some duplicate code is tolerated, but this should be handled in
    subsequent commits.

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/barclays/mock/BarclaysAgentWireMockTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/barclays/mock/BarclaysAgentWireMockTest.java
@@ -43,6 +43,7 @@ public class BarclaysAgentWireMockTest {
 
         final WireMockConfiguration configuration =
                 WireMockConfiguration.builder("localhost:" + server.getHttpsPort())
+                        .setAgentModule(new BarclaysWireMockTestModule())
                         .setConfigurationPath(
                                 "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/barclays/mock/resources/configuration.yml")
                         .setCallbackData(
@@ -93,6 +94,7 @@ public class BarclaysAgentWireMockTest {
 
         final WireMockConfiguration configuration =
                 WireMockConfiguration.builder("localhost:" + server.getHttpsPort())
+                        .setAgentModule(new BarclaysWireMockTestModule())
                         .setConfigurationPath(
                                 "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/barclays/mock/resources/configuration.yml")
                         .setCallbackData(
