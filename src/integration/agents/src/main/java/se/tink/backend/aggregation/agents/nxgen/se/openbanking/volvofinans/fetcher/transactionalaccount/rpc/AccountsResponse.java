40cfd762ea4748bc3e9a39413c32ac103c8683de
Nazri Abdullah <nazri.abdullah@tink.se>
2020-04-01 15:44:15 +0200

    refactor(volvofinans-ob): changed to credit card parsing

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/volvofinans/fetcher/transactionalaccount/rpc/AccountsResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/volvofinans/fetcher/transactionalaccount/rpc/AccountsResponse.java
@@ -2,19 +2,16 @@ package se.tink.backend.aggregation.agents.nxgen.se.openbanking.volvofinans.fetc
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Optional;
 import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.se.openbanking.volvofinans.fetcher.transactionalaccount.entities.AccountEntity;
-import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
 
 public class AccountsResponse extends ArrayList<AccountEntity> {
 
-    public Collection<TransactionalAccount> toTinkAccounts() {
+    public Collection<CreditCardAccount> toCreditCardAccounts() {
         return this.stream()
                 .filter(AccountEntity::isEnabled)
-                .map(AccountEntity::toTinkAccount)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
+                .map(AccountEntity::toCreditCardAccount)
                 .collect(Collectors.toList());
     }
 }
