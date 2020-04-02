143191bddaa0710cb55264babbf389ead93219d5
Karl Johan Krantz <karl-johan.krantz@tink.se>
2020-04-02 09:11:25 +0200

    refactor(credential-request-types): remove unused request-types

--- src/aggregation/aggregationcontroller_api/src/main/java/se/tink/backend/aggregation/aggregationcontroller/v1/rpc/CredentialsRequestType.java
+++ src/aggregation/aggregationcontroller_api/src/main/java/se/tink/backend/aggregation/aggregationcontroller/v1/rpc/CredentialsRequestType.java
@@ -4,11 +4,8 @@ public enum CredentialsRequestType {
     REFRESH_INFORMATION,
     TRANSFER,
     KEEP_ALIVE,
-    PRODUCT_CREATE,
     DELETE,
     CREATE,
-    PRODUCT_INFORMATION,
-    PRODUCT_REFRESH,
     UPDATE,
     REENCRYPT,
     MIGRATE
