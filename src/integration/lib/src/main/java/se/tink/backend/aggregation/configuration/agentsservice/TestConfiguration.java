e556c3d71e16cf3c6bf5eed82b71a76af9e89021
Gustaf Svanerud <gustaf.svanerud@tink.se>
2020-04-02 13:58:21 +0200

    refactor(testconfig): remove unused methods

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/configuration/agentsservice/TestConfiguration.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/configuration/agentsservice/TestConfiguration.java
@@ -1,41 +1,14 @@
 package se.tink.backend.aggregation.configuration.agentsservice;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
 public final class TestConfiguration {
 
-    @JsonProperty private boolean censorSensitiveHeaders = true;
     @JsonProperty private boolean debugOutput;
-
-    @JsonProperty private boolean mockServer = false;
-    @JsonProperty private String mockURL;
-
-    @JsonIgnore private int mockServerPort;
 
     public boolean isDebugOutputEnabled() {
         return debugOutput;
-    }
-
-    public boolean isCensorSensitiveHeadersEnabled() {
-        return censorSensitiveHeaders;
-    }
-
-    public boolean isMockServer() {
-        return mockServer;
-    }
-
-    public String getMockURL() {
-        return mockURL;
-    }
-
-    public void setMockServerPort(int port) {
-        this.mockServerPort = port;
-    }
-
-    public int getMockServerPort() {
-        return mockServerPort;
     }
 }
