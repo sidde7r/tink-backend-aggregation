47e1f3feeeab40b07913f1ab50653dfcb7d7aa29
Jacek Garbulinski <jacek.garbulinski@tink.se>
2020-04-04 17:24:46 +0200

    feat(sdc-ss): add configuration

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/sdcno/config/SdcConfigurationException.java
@@ -0,0 +1,7 @@
+package se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.config;
+
+public class SdcConfigurationException extends RuntimeException {
+    SdcConfigurationException(final String message) {
+        super(message);
+    }
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/sdcno/config/SdcNoConfiguration.java
@@ -0,0 +1,81 @@
+package se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.config;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import se.tink.backend.agents.rpc.Provider;
+import se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.SdcPayload;
+import se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.authenticator.bankmappers.AuthenticationType;
+import se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.authenticator.bankmappers.ProviderMapping;
+import se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.config.SdcNoConstants.EikaBankPortal;
+import se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.config.SdcNoConstants.ErrorMessages;
+import se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.config.SdcNoConstants.NettBankPortal;
+import se.tink.backend.aggregation.agents.nxgen.no.banks.sdcno.config.SdcNoConstants.PortalBank;
+import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
+import se.tink.libraries.payloadparser.PayloadParser;
+
+public class SdcNoConfiguration implements ClientConfiguration {
+    private final String bankCode;
+    private final AuthenticationType authenticationType;
+    private Map<AuthenticationType, String> baseUrlMap = new HashMap<>();
+    private Map<AuthenticationType, String> loginUrlMap = new HashMap<>();
+
+    public SdcNoConfiguration(Provider provider) {
+        this.bankCode = getBankCode(provider.getPayload());
+        this.authenticationType = ProviderMapping.getAuthenticationTypeByBankCode(bankCode);
+        generateBaseUrlMap();
+        generateLoginUrlMap();
+    }
+
+    private void generateBaseUrlMap() {
+        baseUrlMap.put(AuthenticationType.NETTBANK, NettBankPortal.BASE_URL);
+        baseUrlMap.put(AuthenticationType.PORTAL, PortalBank.BASE_URL);
+        baseUrlMap.put(AuthenticationType.EIKA, EikaBankPortal.BASE_URL);
+    }
+
+    private void generateLoginUrlMap() {
+        loginUrlMap.put(
+                AuthenticationType.NETTBANK, NettBankPortal.LOGIN_MATCHER.replaceAll(bankCode));
+        loginUrlMap.put(AuthenticationType.PORTAL, PortalBank.LOGIN_MATCHER.replaceAll(bankCode));
+        loginUrlMap.put(AuthenticationType.EIKA, EikaBankPortal.LOGIN_URL);
+    }
+
+    private String getBankCode(String payload) {
+        Preconditions.checkNotNull(
+                Strings.emptyToNull(payload),
+                String.format(ErrorMessages.INVALID_CONFIGURATION, "Bank Code"));
+        SdcPayload sdcPayload = PayloadParser.parse(payload, SdcPayload.class);
+        return sdcPayload.getBankcode();
+    }
+
+    public String getLoginUrl() {
+        return Optional.ofNullable(loginUrlMap.get(authenticationType))
+                .orElseThrow(
+                        () ->
+                                new SdcConfigurationException(
+                                        authenticationType + " login url not found."));
+    }
+
+    public String getBasePageUrl() {
+        return Optional.ofNullable(baseUrlMap.get(authenticationType))
+                .map(s -> s + bankCode + "/")
+                .orElseThrow(
+                        () ->
+                                new SdcConfigurationException(
+                                        authenticationType + " base page url not found."));
+    }
+
+    public String getBaseApiUrl() {
+        return Optional.ofNullable(baseUrlMap.get(authenticationType))
+                .orElseThrow(
+                        () ->
+                                new SdcConfigurationException(
+                                        authenticationType + " base api url not found."));
+    }
+
+    public AuthenticationType getAuthenticationType() {
+        return authenticationType;
+    }
+}
