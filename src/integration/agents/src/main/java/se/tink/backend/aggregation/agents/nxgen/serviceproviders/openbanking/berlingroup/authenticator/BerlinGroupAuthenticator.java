d3fbe798ed3f93c444e505d4a4c44a0c28f32aca
Kamil Woźniak <kamil.wozniak@tink.se>
2020-04-06 14:12:09 +0200

    log(be/kbc): Logging token hash
    Fixed formatting
    
    Changes afre review

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/berlingroup/authenticator/BerlinGroupAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/berlingroup/authenticator/BerlinGroupAuthenticator.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.authenticator;
 
+import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceException;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.BerlinGroupApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.BerlinGroupConstants.StorageKeys;
@@ -7,7 +8,9 @@ import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.
 import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
 import se.tink.backend.aggregation.nxgen.http.url.URL;
 
+@Slf4j
 public abstract class BerlinGroupAuthenticator implements OAuth2Authenticator {
+
     protected final BerlinGroupApiClient apiClient;
 
     public BerlinGroupAuthenticator(final BerlinGroupApiClient apiClient) {
@@ -20,8 +23,16 @@ public abstract class BerlinGroupAuthenticator implements OAuth2Authenticator {
 
     @Override
     public OAuth2Token refreshAccessToken(final String refreshToken) throws BankServiceException {
+        log.info("Token for refresh hash: {}}", refreshToken.hashCode());
+
         final OAuth2Token token = apiClient.refreshToken(refreshToken);
         apiClient.setTokenToSession(token, StorageKeys.OAUTH_TOKEN);
+
+        token.getRefreshToken()
+                .ifPresent(
+                        refreshedToken ->
+                                log.info("Refreshed token hash: {}", refreshedToken.hashCode()));
+
         return token;
     }
 
