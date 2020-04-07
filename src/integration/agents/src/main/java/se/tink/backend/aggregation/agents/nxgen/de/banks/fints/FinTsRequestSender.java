83427a9a3b8954c9c4d10c5b4bff21531f9334f7
Jan Sameryt <jan.sameryt@tink.com>
2020-04-07 11:44:54 +0200

    fix(FinTS): Remove unnecessary logging

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/FinTsRequestSender.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/FinTsRequestSender.java
@@ -1,6 +1,5 @@
 package se.tink.backend.aggregation.agents.nxgen.de.banks.fints;
 
-import java.util.regex.Pattern;
 import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.aggregation.agents.nxgen.de.banks.fints.protocol.base64.FinTsBase64;
 import se.tink.backend.aggregation.agents.nxgen.de.banks.fints.protocol.parts.request.FinTsRequest;
@@ -10,7 +9,6 @@ import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
 @Slf4j
 class FinTsRequestSender {
 
-    private static final Pattern REPLACE_PATTERN = Pattern.compile("\u0000");
     private final TinkHttpClient httpClient;
     private final String endpoint;
 
@@ -20,11 +18,8 @@ class FinTsRequestSender {
     }
 
     public FinTsResponse sendRequest(FinTsRequest request) {
-        log.info("Request: {}", request.toFinTsFormat());
         String b64Response = send(FinTsBase64.encodeRequestToBase64(request));
-
         String plainResponse = FinTsBase64.decodeResponseFromBase64(b64Response);
-        log.info("Response: {}", REPLACE_PATTERN.matcher(plainResponse).replaceAll("0"));
         return new FinTsResponse(plainResponse);
     }
 
