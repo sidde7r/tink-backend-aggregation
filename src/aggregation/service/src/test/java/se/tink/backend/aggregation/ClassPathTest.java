3573b6e6870d51b2b99da8df152c2584f4da1142
Sebastian Olsson <sebastian.olsson@tink.se>
2020-04-14 17:27:17 +0200

    test(aggregation/service): Added classpath test for Hibernate class

--- src/aggregation/service/src/test/java/se/tink/backend/aggregation/ClassPathTest.java
+++ src/aggregation/service/src/test/java/se/tink/backend/aggregation/ClassPathTest.java
@@ -24,4 +24,31 @@ public final class ClassPathTest {
 
         assert guavaJars.size() <= 1 : String.format("Conflicting dependencies: %s", guavaJars);
     }
+
+    /*
+    Guards against the following exception on aggregation service startup:
+    <pre>
+    Exception in thread "main" com.google.inject.CreationException: Unable to create injector, see the following errors:
+    1) No implementation for se.tink.backend.aggregation.storage.database.repositories.AggregatorConfigurationsRepository was bound.
+    [...]
+    at se.tink.libraries.repository.config.repository.AggregationRepositoryConfiguration$$EnhancerBySpringCGLIB$$a9e6da6e.entityManagerFactory(<generated>)
+    at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
+    at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)
+    at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)
+    at java.lang.reflect.Method.invoke(Method.java:498)
+    at org.springframework.beans.factory.support.SimpleInstantiationStrategy.instantiate(SimpleInstantiationStrategy.java:162)
+    ... 33 more
+    Caused by: java.lang.ClassNotFoundException: org.hibernate.ejb.HibernateEntityManagerFactory
+    at java.net.URLClassLoader.findClass(URLClassLoader.java:382)
+    at java.lang.ClassLoader.loadClass(ClassLoader.java:418)
+    at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:352)
+    at java.lang.ClassLoader.loadClass(ClassLoader.java:351)
+    at org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter.<init>(HibernateJpaVendorAdapter.java:89)
+    ... 48 more
+    </pre>
+     */
+    @Test
+    public void hibernateEntityManagerFactoryShouldBePresent() throws ClassNotFoundException {
+        Class.forName("org.hibernate.ejb.HibernateEntityManagerFactory");
+    }
 }
