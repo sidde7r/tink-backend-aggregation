06d132aac502b40b6e72fa703b4d5b3659745bd0
Schahin Rajab <schahin.rajab@tink.se>
2020-04-15 15:26:54 +0200

    feat(nxgen/nordea): implement transactional accounts fetching

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/nordea/NordeaSEAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/nordea/NordeaSEAgent.java
@@ -6,23 +6,30 @@ import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
 import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
 import se.tink.backend.aggregation.agents.RefreshSavingsAccountsExecutor;
 import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.authenticator.NordeaSEBankIdAuthenticator;
+import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.fetcher.transactionalaccount.NordeaTransactionFetcher;
+import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.fetcher.transactionalaccount.NordeaTransactionalAccountFetcher;
 import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.rpc.filter.NordeaSEFilter;
 import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.session.NordeaSESessionHandler;
 import se.tink.backend.aggregation.nxgen.agents.NextGenerationAgent;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.bankid.BankIdAuthenticationController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcherController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginationController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
 
 public class NordeaSEAgent extends NextGenerationAgent
         implements RefreshCheckingAccountsExecutor, RefreshSavingsAccountsExecutor {
     private final NordeaSEApiClient apiClient;
+    private final TransactionalAccountRefreshController transactionalAccountRefreshController;
 
     @Inject
     public NordeaSEAgent(AgentComponentProvider componentProvider) {
         super(componentProvider);
         client.addFilter(new NordeaSEFilter());
         apiClient = new NordeaSEApiClient(client, sessionStorage);
+        transactionalAccountRefreshController = constructTransactionalAccountRefreshController();
     }
 
     @Override
@@ -36,26 +43,37 @@ public class NordeaSEAgent extends NextGenerationAgent
 
     @Override
     public FetchAccountsResponse fetchCheckingAccounts() {
-        return null;
+        return transactionalAccountRefreshController.fetchCheckingAccounts();
     }
 
     @Override
     public FetchTransactionsResponse fetchCheckingTransactions() {
-        return null;
+        return transactionalAccountRefreshController.fetchCheckingTransactions();
     }
 
     @Override
     public FetchAccountsResponse fetchSavingsAccounts() {
-        return null;
+        return transactionalAccountRefreshController.fetchSavingsAccounts();
     }
 
     @Override
     public FetchTransactionsResponse fetchSavingsTransactions() {
-        return null;
+        return transactionalAccountRefreshController.fetchSavingsTransactions();
     }
 
     @Override
     protected SessionHandler constructSessionHandler() {
         return new NordeaSESessionHandler(sessionStorage);
+    }
+
+    private TransactionalAccountRefreshController constructTransactionalAccountRefreshController() {
+        final NordeaTransactionFetcher transactionFetcher = new NordeaTransactionFetcher(apiClient);
+        return new TransactionalAccountRefreshController(
+                metricRefreshController,
+                updateController,
+                new NordeaTransactionalAccountFetcher(apiClient),
+                new TransactionFetcherController<>(
+                        transactionPaginationHelper,
+                        new TransactionKeyPaginationController<>(transactionFetcher)));
     }
 }
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/nordea/NordeaSEApiClient.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/nordea/NordeaSEApiClient.java
@@ -1,7 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.se.business.nordea;
 
+import java.util.Locale;
 import javax.ws.rs.core.MediaType;
 import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.NordeaSEConstants.Headers;
+import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.NordeaSEConstants.QueryKeys;
 import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.NordeaSEConstants.StorageKeys;
 import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.NordeaSEConstants.Urls;
 import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.authenticator.rpc.FetchTokenRequest;
@@ -9,6 +11,9 @@ import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.authenticator
 import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.authenticator.rpc.InitBankIdRequest;
 import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.authenticator.rpc.InitBankIdResponse;
 import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.authenticator.rpc.ResultBankIdResponse;
+import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.fetcher.transactionalaccount.rpc.FetchAccountDetailsResponse;
+import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.fetcher.transactionalaccount.rpc.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.nxgen.se.business.nordea.fetcher.transactionalaccount.rpc.FetchTransactionsResponse;
 import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.storage.SessionStorage;
 
@@ -44,5 +49,34 @@ public class NordeaSEApiClient {
                 .type(MediaType.APPLICATION_JSON_TYPE)
                 .accept(MediaType.WILDCARD_TYPE)
                 .post(FetchTokenResponse.class, fetchTokenRequest);
+    }
+
+    public FetchAccountsResponse fetchAccount() {
+        return client.request(Urls.FETCH_ACCOUNT)
+                .headers(NordeaSEConstants.NORDEA_CUSTOM_HEADERS)
+                .header(Headers.SECURITY_TOKEN, sessionStorage.get(StorageKeys.SECURITY_TOKEN))
+                .accept(MediaType.WILDCARD_TYPE)
+                .acceptLanguage(Locale.US)
+                .get(FetchAccountsResponse.class);
+    }
+
+    public FetchAccountDetailsResponse fetchAccountDetails(String accountId) {
+        return client.request(Urls.FETCH_ACCOUNT_DETAILS + accountId)
+                .headers(NordeaSEConstants.NORDEA_CUSTOM_HEADERS)
+                .header(Headers.SECURITY_TOKEN, sessionStorage.get(StorageKeys.SECURITY_TOKEN))
+                .accept(MediaType.WILDCARD_TYPE)
+                .acceptLanguage(Locale.US)
+                .get(FetchAccountDetailsResponse.class);
+    }
+
+    public FetchTransactionsResponse fetchTransactions(String accountId, String continueKey) {
+        return client.request(Urls.FETCH_TRANSACTIONS)
+                .queryParam(QueryKeys.ACCOUNT_ID, accountId)
+                .queryParam(QueryKeys.CONTINUE_KEY, continueKey)
+                .headers(NordeaSEConstants.NORDEA_CUSTOM_HEADERS)
+                .header(Headers.SECURITY_TOKEN, sessionStorage.get(StorageKeys.SECURITY_TOKEN))
+                .accept(MediaType.WILDCARD_TYPE)
+                .acceptLanguage(Locale.US)
+                .get(FetchTransactionsResponse.class);
     }
 }
