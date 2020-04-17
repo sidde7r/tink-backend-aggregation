4ff977e553a0fcb5f72472f4f60d138cffd7ca08
Sofia Johnsen <sofia.johnsen@tink.se>
2020-04-17 15:49:07 +0200

    test(lansforsakringar/legacy): Extract common builder

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/legacy/banks/lansforsakringar/LansforsakringarAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/legacy/banks/lansforsakringar/LansforsakringarAgentTest.java
@@ -7,6 +7,7 @@ import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager.SsnArgumentEnum;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager.ToAccountFromAccountArgumentEnum;
 import se.tink.libraries.account.AccountIdentifier;
 import se.tink.libraries.amount.Amount;
 import se.tink.libraries.credentials.service.RefreshableItem;
@@ -16,8 +17,14 @@ import se.tink.libraries.transfer.rpc.Transfer;
 public class LansforsakringarAgentTest {
     private final ArgumentManager<SsnArgumentEnum> ssnManager =
             new ArgumentManager<>(SsnArgumentEnum.values());
-    private final ArgumentManager<ArgumentManager.ToAccountFromAccountArgumentEnum> accountManager =
-            new ArgumentManager<>(ArgumentManager.ToAccountFromAccountArgumentEnum.values());
+    private final ArgumentManager<ToAccountFromAccountArgumentEnum> accountManager =
+            new ArgumentManager<>(ToAccountFromAccountArgumentEnum.values());
+
+    private AgentIntegrationTest.Builder builder =
+            new AgentIntegrationTest.Builder("se", "lansforsakringar-bankid")
+                    .addCredentialField(Field.Key.USERNAME, ssnManager.get(SsnArgumentEnum.SSN))
+                    .loadCredentialsBefore(false)
+                    .saveCredentialsAfter(false);
 
     @Before
     public void setUp() throws Exception {
@@ -32,11 +39,7 @@ public class LansforsakringarAgentTest {
 
     @Test
     public void testRefresh() throws Exception {
-        new AgentIntegrationTest.Builder("se", "lansforsakringar-bankid")
-                .addCredentialField(Field.Key.USERNAME, ssnManager.get(SsnArgumentEnum.SSN))
-                .loadCredentialsBefore(false)
-                .saveCredentialsAfter(false)
-                .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+        builder.addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
                 .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
                 .build()
                 .testRefresh();
@@ -49,22 +52,17 @@ public class LansforsakringarAgentTest {
                 AccountIdentifier.create(
                         AccountIdentifier.Type.SE,
                         accountManager.get(
-                                ArgumentManager.ToAccountFromAccountArgumentEnum.FROM_ACCOUNT)));
+                                ToAccountFromAccountArgumentEnum.FROM_ACCOUNT)));
         transfer.setDestination(
                 AccountIdentifier.create(
                         AccountIdentifier.Type.SE_BG,
                         accountManager.get(
-                                ArgumentManager.ToAccountFromAccountArgumentEnum.TO_ACCOUNT)));
+                                ToAccountFromAccountArgumentEnum.TO_ACCOUNT)));
         transfer.setAmount(Amount.inSEK(2d));
         transfer.setType(TransferType.PAYMENT);
         transfer.setDueDate(null);
         transfer.setDestinationMessage("Destination message");
 
-        new AgentIntegrationTest.Builder("se", "lansforsakringar-bankid")
-                .addCredentialField(Field.Key.USERNAME, ssnManager.get(SsnArgumentEnum.SSN))
-                .loadCredentialsBefore(false)
-                .saveCredentialsAfter(false)
-                .build()
-                .testBankTransfer(transfer);
+        builder.build().testBankTransfer(transfer);
     }
 }
