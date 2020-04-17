8d66dcf4e1fdcc49d3608a674f3494521b13181f
Ahmed Hussein <ahmed.hussein@tink.se>
2020-04-17 14:58:51 +0200

    fix(localisation): remove unneeded params form generate pot script
    remove ununeeded params when locating srcs to extract translation keys from

--- tools/translation/potfilegenerator.php
+++ tools/translation/potfilegenerator.php
@@ -5,7 +5,7 @@ class PotFileGenerator {
     public function generateBackendPotFile($root) {
         chdir($root);
         echo "Generating '" . $this->backendPotFilename . "'..." . PHP_EOL;
-        exec ('find src/ -name \'*.java\' ! -path \'*Lookup*\' ! -name SeedMerchantsCommand.java ! -name \'*Test.java\' -exec xgettext --from-code="UTF-8" -kgetString -kgetPluralString:1,2 -kLocalizableKey -kLocalizableParametrizedKey -kLocalizablePluralKey:1,2 --join-existing -o po/' . $this->backendPotFilename . ' {} +');
+        exec ('find src/ -name \'*.java\' ! -name \'*Test.java\' -exec xgettext --from-code="UTF-8" -kgetString -kgetPluralString:1,2 -kLocalizableKey -kLocalizableParametrizedKey -kLocalizablePluralKey:1,2 --join-existing -o po/' . $this->backendPotFilename . ' {} +');
         echo "Generated '" . $this->backendPotFilename . "'" . PHP_EOL . PHP_EOL;
         return $this->backendPotFilename;
     }
