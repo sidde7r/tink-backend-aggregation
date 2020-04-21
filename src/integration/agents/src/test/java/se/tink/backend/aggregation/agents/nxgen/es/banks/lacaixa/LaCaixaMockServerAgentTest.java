f5e13e4ca0565c47b144bfc5be17a308fe80e71f
Gustaf Svanerud <gustaf.svanerud@tink.se>
2020-04-21 14:03:52 +0200

    refactor(caixa): use new wiremock test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/lacaixa/LaCaixaMockServerAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/lacaixa/LaCaixaMockServerAgentTest.java
@@ -1,24 +1,12 @@
 package se.tink.backend.aggregation.agents.nxgen.es.banks.lacaixa;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import org.junit.Assert;
 import org.junit.Test;
-import se.tink.backend.agents.rpc.Account;
-import se.tink.backend.agents.rpc.Field;
-import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
-import se.tink.backend.aggregation.agents.framework.NewAgentTestContext;
-import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesAsserts;
+import se.tink.backend.agents.rpc.Field.Key;
 import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonFileParser;
 import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
-import se.tink.backend.aggregation.agents.framework.wiremock.WireMockTestServer;
-import se.tink.backend.aggregation.agents.framework.wiremock.configuration.WireMockConfiguration;
-import se.tink.backend.aggregation.agents.framework.wiremock.utils.AapFileParser;
-import se.tink.backend.aggregation.agents.framework.wiremock.utils.ResourceFileReader;
-import se.tink.backend.aggregation.agents.models.Transaction;
+import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockrefresh.AgentWireMockRefreshTest;
 import se.tink.libraries.credentials.service.RefreshableItem;
-import se.tink.libraries.identitydata.IdentityData;
+import se.tink.libraries.enums.MarketCode;
 
 public class LaCaixaMockServerAgentTest {
 
@@ -29,57 +17,28 @@ public class LaCaixaMockServerAgentTest {
     public void testRefresh() throws Exception {
 
         // Given
-        WireMockTestServer server = new WireMockTestServer();
-        server.prepareMockServer(
-                new AapFileParser(
-                        new ResourceFileReader()
-                                .read(
-                                        "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/lacaixa/resources/caixa-refresh-traffic.aap")));
+        final String wireMockServerFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/lacaixa/resources/caixa-refresh-traffic.aap";
+        final String wireMockContractFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/lacaixa/resources/agent-contract.json";
 
-        final WireMockConfiguration configuration =
-                WireMockConfiguration.builder("localhost:" + server.getHttpsPort()).build();
+        final AgentContractEntity expected =
+                new AgentContractEntitiesJsonFileParser()
+                        .parseContractOnBasisOfFile(wireMockContractFilePath);
 
-        AgentContractEntitiesJsonFileParser contractParser =
-                new AgentContractEntitiesJsonFileParser();
-        AgentContractEntity expected =
-                contractParser.parseContractOnBasisOfFile(
-                        "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/lacaixa/resources/agent-contract.json");
-
-        List<Map<String, Object>> expectedAccounts = expected.getAccounts();
-        List<Map<String, Object>> expectedTransactions = expected.getTransactions();
-        Map<String, Object> expectedIdentityData =
-                expected.getIdentityData().orElseGet(Collections::emptyMap);
-
-        // When
-        NewAgentTestContext context =
-                new AgentIntegrationTest.Builder("es", "es-lacaixa-password")
-                        .addCredentialField(Field.Key.USERNAME, USERNAME)
-                        .addCredentialField(Field.Key.PASSWORD, PASSWORD)
+        final AgentWireMockRefreshTest agentWireMockRefreshTest =
+                AgentWireMockRefreshTest.builder(
+                                MarketCode.ES, "es-lacaixa-password", wireMockServerFilePath)
+                        .addCredentialField(Key.USERNAME.getFieldKey(), USERNAME)
+                        .addCredentialField(Key.PASSWORD.getFieldKey(), PASSWORD)
                         .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
                         .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
-                        .loadCredentialsBefore(false)
-                        .saveCredentialsAfter(false)
-                        .expectLoggedIn(false)
-                        .setWireMockConfiguration(configuration)
-                        .build()
-                        .testRefresh();
+                        .build();
 
-        List<Transaction> givenTransactions = context.getTransactions();
-        List<Account> givenAccounts = context.getUpdatedAccounts();
-        IdentityData givenIdentityData =
-                context.getIdentityData()
-                        .orElseThrow(() -> new AssertionError("Expected identity data. Was null."));
+        // When
+        agentWireMockRefreshTest.executeRefresh();
 
         // Then
-        Assert.assertTrue(
-                AgentContractEntitiesAsserts.areListsMatchingVerbose(
-                        expectedAccounts, givenAccounts));
-        Assert.assertTrue(
-                AgentContractEntitiesAsserts.areListsMatchingVerbose(
-                        expectedTransactions, givenTransactions));
-        Assert.assertTrue(
-                AgentContractEntitiesAsserts.areListsMatchingVerbose(
-                        Collections.singletonList(expectedIdentityData),
-                        Collections.singletonList(givenIdentityData)));
+        agentWireMockRefreshTest.assertExpectedData(expected);
     }
 }
