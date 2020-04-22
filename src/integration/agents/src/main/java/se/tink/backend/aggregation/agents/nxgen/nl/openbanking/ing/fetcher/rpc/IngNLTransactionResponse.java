02bfc540dc66836555c3d36f0a3c7a313ce9bc55
Nazri Abdullah <nazri.abdullah@tink.se>
2020-04-22 09:34:47 +0200

    feat(nl-ing-ob): added nl based transaction entity class

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/fetcher/rpc/IngNLTransactionResponse.java
@@ -0,0 +1,7 @@
+package se.tink.backend.aggregation.agents.nxgen.nl.openbanking.ing.fetcher.rpc;
+
+import se.tink.backend.aggregation.agents.nxgen.nl.openbanking.ing.fetcher.entities.IngNLTransactionsEntity;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ingbase.fetcher.rpc.BaseFetchTransactionsResponse;
+
+public class IngNLTransactionResponse
+        extends BaseFetchTransactionsResponse<IngNLTransactionsEntity> {}
