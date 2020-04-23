2ed6c7ace1785f85390fefe644aa7c131c81f042
Gustaf Svanerud <gustaf.svanerud@tink.se>
2020-04-23 10:50:01 +0200

    refactor(ukob): define base module for JwtSigner
    Implements module for binding JwtSigner which can be used by the agents
    to provide a LocalKeyJwtSigner.

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/base/module/UkOpenBankingLocalKeySignerModule.java
@@ -0,0 +1,20 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Scopes;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.configuration.UkOpenBankingConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module.provider.LocalKeySignerProvider;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module.provider.UkOpenBankingConfigurationProvider;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.jwt.signer.iface.JwtSigner;
+
+public final class UkOpenBankingLocalKeySignerModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+
+        bind(UkOpenBankingConfiguration.class)
+                .toProvider(UkOpenBankingConfigurationProvider.class)
+                .in(Scopes.SINGLETON);
+        bind(JwtSigner.class).toProvider(LocalKeySignerProvider.class).in(Scopes.SINGLETON);
+    }
+}
