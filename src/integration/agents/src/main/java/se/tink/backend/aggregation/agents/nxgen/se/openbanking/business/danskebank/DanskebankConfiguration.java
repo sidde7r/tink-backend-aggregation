8d747c846fa4f57c9931340c77b890681c1cda82
Niclas Albinsson <niclas.albinsson@tink.se>
2020-04-23 11:19:43 +0200

    chore(danskebank/business): add new agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/business/danskebank/DanskebankConfiguration.java
@@ -0,0 +1,54 @@
+package se.tink.backend.aggregation.agents.nxgen.se.openbanking.business.danskebank;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.configuration.UkOpenBankingClientConfigurationAdapter;
+import se.tink.backend.aggregation.annotations.AgentConfigParam;
+import se.tink.backend.aggregation.annotations.Secret;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.ClientInfo;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.ProviderConfiguration;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.configuration.SoftwareStatementAssertion;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.jwt.signer.iface.JwtSigner;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.tls.TlsConfigurationOverride;
+
+public class DanskebankConfiguration implements UkOpenBankingClientConfigurationAdapter {
+
+    @JsonProperty @Secret private String organizationId;
+    @JsonProperty @Secret private String clientId;
+    @JsonProperty @Secret private String softwareStatementAssertion;
+    @JsonProperty @AgentConfigParam private String redirectUrl;
+    @JsonProperty @Secret private String softwareId;
+
+    @Override
+    public byte[] getRootCAData() {
+        return new byte[0];
+    }
+
+    @Override
+    public String getRootCAPassword() {
+        return null;
+    }
+
+    @Override
+    public ProviderConfiguration getProviderConfiguration() {
+        Preconditions.checkState(!Strings.isNullOrEmpty(clientId), "ClientId is null or empty.");
+        return new ProviderConfiguration(organizationId, new ClientInfo(clientId, ""));
+    }
+
+    @Override
+    public SoftwareStatementAssertion getSoftwareStatementAssertion() {
+        return new SoftwareStatementAssertion(softwareStatementAssertion, softwareId, redirectUrl);
+    }
+
+    @Override
+    public Optional<TlsConfigurationOverride> getTlsConfigurationOverride() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<JwtSigner> getSignerOverride() {
+        return Optional.empty();
+    }
+}
