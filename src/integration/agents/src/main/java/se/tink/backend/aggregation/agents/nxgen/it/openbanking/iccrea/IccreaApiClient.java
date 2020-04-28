255acdf5a816d881e52fffe8adfe6d747ea8fb0e
Jakub Barejka <jakub.barejka@tink.se>
2020-04-28 14:22:21 +0200

    feat(Iccrea): Iccrea OB

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/iccrea/IccreaApiClient.java
@@ -0,0 +1,29 @@
+package se.tink.backend.aggregation.agents.nxgen.it.openbanking.iccrea;
+
+import se.tink.backend.aggregation.agents.nxgen.it.openbanking.iccrea.authenticator.rpc.ConsentScaResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeApiClient;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.authenticator.entities.ConsentType;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.authenticator.rpc.ConsentRequest;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.configuration.InstrumentType;
+import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
+import se.tink.backend.aggregation.nxgen.http.filter.filterable.request.RequestBuilder;
+import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
+import se.tink.backend.aggregation.nxgen.storage.TemporaryStorage;
+
+public class IccreaApiClient extends CbiGlobeApiClient {
+
+    IccreaApiClient(
+            TinkHttpClient client,
+            PersistentStorage persistentStorage,
+            boolean requestManual,
+            TemporaryStorage temporaryStorage) {
+        super(client, persistentStorage, requestManual, temporaryStorage, InstrumentType.ACCOUNTS);
+    }
+
+    @Override
+    public ConsentScaResponse createConsent(
+            String state, ConsentType consentType, ConsentRequest consentRequest) {
+        RequestBuilder rb = createConsentRequest(state, consentType);
+        return rb.post(ConsentScaResponse.class, consentRequest);
+    }
+}
