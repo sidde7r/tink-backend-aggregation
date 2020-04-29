3f43b536d10461bbd7c37232a98cb961b048fc12
Schahin Rajab <schahin.rajab@tink.se>
2020-04-29 11:49:36 +0200

    chore(swedbank/ob): remove ignore annotation

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/swedbank/SwedbankAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/swedbank/SwedbankAgentTest.java
@@ -1,11 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.swedbank;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
-@Ignore
 public class SwedbankAgentTest {
 
     private AgentIntegrationTest.Builder builder;
