fa9009a0f92987848c82d72726142f9530779b4d
Nazri Abdullah <nazri.abdullah@tink.se>
2020-04-30 12:17:14 +0200

    feat(se-entercard-re): updated account nxbuilder

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/entercard/fetcher/rpc/Account.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/creditcards/entercard/fetcher/rpc/Account.java
@@ -1,12 +1,16 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.entercard.fetcher.rpc;
 
 import java.util.List;
+import java.util.Objects;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.entercard.EnterCardConstants;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
-import se.tink.backend.aggregation.nxgen.core.account.entity.HolderName;
+import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.creditcard.CreditCardModule;
+import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.id.IdModule;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.account.AccountIdentifier.Type;
 import se.tink.libraries.amount.ExactCurrencyAmount;
 
-@SuppressWarnings("unused")
 @JsonObject
 public class Account {
 
@@ -16,7 +20,7 @@ public class Account {
     private double openToBuy;
     private boolean displayEFaktura;
     private boolean enableChangeDueDate;
-    private List<Card> cards = null;
+    private List<Card> cards;
     private Object eFakturaMsg;
     private Object autoPaymentMsg;
     private String kID;
@@ -24,13 +28,39 @@ public class Account {
     private String kid;
 
     public CreditCardAccount toCreditCardAccount(User user, String accountId) {
-        return CreditCardAccount.builder(accountId)
-                .setHolderName(new HolderName(user.name))
-                .setBankIdentifier(accountId)
-                .setExactAvailableCredit(ExactCurrencyAmount.inSEK(openToBuy))
-                .setAccountNumber(accountId)
-                .setExactBalance(ExactCurrencyAmount.inSEK(usedCredit).negate())
-                .setName(productName)
+
+        return CreditCardAccount.nxBuilder()
+                .withCardDetails(
+                        CreditCardModule.builder()
+                                .withCardNumber(getPrimaryCard().getMaskedNr())
+                                .withBalance(
+                                        ExactCurrencyAmount.of(
+                                                usedCredit, EnterCardConstants.CURRENCY))
+                                .withAvailableCredit(
+                                        ExactCurrencyAmount.of(
+                                                openToBuy, EnterCardConstants.CURRENCY))
+                                .withCardAlias(getPrimaryCard().getCardHolderName())
+                                .build())
+                .withInferredAccountFlags()
+                .withId(
+                        IdModule.builder()
+                                .withUniqueIdentifier(accountId)
+                                .withAccountNumber(getPrimaryCard().getMaskedNr())
+                                .withAccountName(getPrimaryCard().getCardHolderName())
+                                .addIdentifier(
+                                        AccountIdentifier.create(
+                                                Type.PAYMENT_CARD_NUMBER, getKid()))
+                                .setProductName(productName)
+                                .build())
+                .setApiIdentifier(accountId)
                 .build();
+    }
+
+    private Card getPrimaryCard() {
+        return cards.stream().filter(Objects::nonNull).filter(Card::isPrimary).findFirst().get();
+    }
+
+    public String getKid() {
+        return kid.substring(0, kid.length() - 1);
     }
 }
