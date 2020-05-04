0becd610e514a54adf1930f370ff409098a54c06
Sebastian Olsson <sebastian.olsson@tink.se>
2020-05-04 18:44:14 +0200

    build(se/danskebank): Put manual tests in manual/ subdirectory

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/danskebank/manual/DanskeBankSEAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/danskebank/manual/DanskeBankSEAgentTest.java
@@ -1,4 +1,4 @@
-package se.tink.backend.aggregation.agents.nxgen.se.banks.danskebank;
+package se.tink.backend.aggregation.agents.nxgen.se.banks.danskebank.manual;
 
 import org.junit.AfterClass;
 import org.junit.Before;
