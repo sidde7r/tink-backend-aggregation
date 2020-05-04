9349825b914a6a454132dd311346726b05a54367
Sebastian Olsson <sebastian.olsson@tink.se>
2020-05-04 19:15:14 +0200

    build(se/sdcse): Put manual tests in manual/ subdirectory

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/sdcse/manual/SdcSeAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/sdcse/manual/SdcSeAgentTest.java
@@ -1,4 +1,4 @@
-package se.tink.backend.aggregation.agents.nxgen.se.banks.sdcse;
+package se.tink.backend.aggregation.agents.nxgen.se.banks.sdcse.manual;
 
 import org.junit.AfterClass;
 import org.junit.Before;
