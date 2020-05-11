5ef10ba5dff1dac17cb89ff2b2f01c51e3eaf11d
Ashfaq Haider <ashfaq.haider@tink.se>
2020-05-11 16:27:08 +0200

    log(PAY1-414): logging for SBAB Transfer status

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/sbab/executor/entities/TransferEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/sbab/executor/entities/TransferEntity.java
@@ -1,6 +1,8 @@
 package se.tink.backend.aggregation.agents.banks.sbab.executor.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.banks.sbab.SBABConstants;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
@@ -20,12 +22,34 @@ public class TransferEntity {
     private boolean recurringTransfer;
     private String fromAccountName;
     private String toAccountName;
+    private static final Logger log = LoggerFactory.getLogger(TransferEntity.class);
 
     @JsonIgnore
     public boolean hasSuccessStatus() {
-        return SBABConstants.TransferStatus.PENDING.equalsIgnoreCase(transferStatus)
-                || SBABConstants.TransferStatus.COMPLETE.equalsIgnoreCase(transferStatus)
-                || (futureTransfer
-                        && SBABConstants.TransferStatus.UNKNOWN.equalsIgnoreCase(transferStatus));
+        logUnknownStatus();
+        return isStatusPending() || isStatusComplete() || isSuccessfulFutureTransfer();
+    }
+
+    @JsonIgnore
+    private void logUnknownStatus() {
+        if (!(isStatusComplete() || (isSuccessfulFutureTransfer()))) {
+            log.info("Transfer Status: {}, isFutureTransfer: {}", transferStatus, futureTransfer);
+        }
+    }
+
+    @JsonIgnore
+    private boolean isStatusComplete() {
+        return SBABConstants.TransferStatus.COMPLETE.equalsIgnoreCase(transferStatus);
+    }
+
+    @JsonIgnore
+    private boolean isStatusPending() {
+        return SBABConstants.TransferStatus.PENDING.equalsIgnoreCase(transferStatus);
+    }
+
+    @JsonIgnore
+    private boolean isSuccessfulFutureTransfer() {
+        return futureTransfer
+                && SBABConstants.TransferStatus.UNKNOWN.equalsIgnoreCase(transferStatus);
     }
 }
