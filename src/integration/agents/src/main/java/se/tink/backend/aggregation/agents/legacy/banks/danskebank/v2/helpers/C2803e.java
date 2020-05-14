55ace9cde9d6e3f5789f7270ea2814c08982ebd1
Jacek Garbulinski <jacek.garbulinski@tink.se>
2020-05-14 18:06:48 +0200

    fix: remove potential runtime issue related to varargs

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/danskebank/v2/helpers/C2803e.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/danskebank/v2/helpers/C2803e.java
@@ -8,7 +8,7 @@ public class C2803e {
     private final byte[] f10328b;
     private final byte[] f10329c;
 
-    public C2803e(C2808a c2808a, byte[] bArr, byte[] bArr2) {
+    C2803e(C2808a c2808a, byte[] bArr, byte[] bArr2) {
         this.f10327a = c2808a;
         this.f10328b = new byte[bArr.length];
         System.arraycopy(bArr, 0, this.f10328b, 0, bArr.length);
@@ -16,15 +16,15 @@ public class C2803e {
         System.arraycopy(bArr2, 0, this.f10329c, 0, bArr2.length);
     }
 
-    public String m12068a() {
+    String m12068a() {
         String a = C2809b.m12106a(this.f10327a.m12101b());
         String a2 = C2809b.m12106a(this.f10328b);
         String a3 = C2809b.m12106a(this.f10329c);
-        return String.format("%1s.%2s.%3s", new Object[] {a, a2, a3});
+        return String.format("%1s.%2s.%3s", a, a2, a3);
     }
 
     public Map<String, String> m12069b() {
-        Map<String, String> hashMap = new HashMap<String, String>();
+        Map<String, String> hashMap = new HashMap<>();
         hashMap.put("Authorization", m12068a());
         return hashMap;
     }
