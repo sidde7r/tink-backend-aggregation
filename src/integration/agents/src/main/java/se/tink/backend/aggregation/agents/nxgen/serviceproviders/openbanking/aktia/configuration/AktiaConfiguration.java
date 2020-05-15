c93ef4e154a057d2d0d728b6a8c3e7819624a61f
Kamil Woźniak <kamil.wozniak@tink.se>
2020-05-15 14:28:01 +0200

    fix(fi/aktia): Added temp hack to fix secret upload issue

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/aktia/configuration/AktiaConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/aktia/configuration/AktiaConfiguration.java
@@ -1,6 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.aktia.configuration;
 
 import lombok.Data;
+import se.tink.backend.aggregation.annotations.AgentConfigParam;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.annotations.SensitiveSecret;
@@ -15,4 +16,7 @@ public class AktiaConfiguration implements ClientConfiguration {
     @Secret private String authServerUrl;
 
     @SensitiveSecret private String basicAuthHeaderValue;
+
+    /* TODO: Added temporary. Remove when secrets uploading is fixed. */
+    @AgentConfigParam private String redirectUrl;
 }
