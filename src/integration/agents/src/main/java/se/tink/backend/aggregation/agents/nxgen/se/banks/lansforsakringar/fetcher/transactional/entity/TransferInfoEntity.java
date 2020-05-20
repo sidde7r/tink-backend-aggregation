9b37ca05f3e716aff0bf8ae34ffeeadf1e0c154d
Sofia Johnsen <sofia.johnsen@tink.se>
2020-05-20 12:09:13 +0200

    refactor(Lansforsakringar/nxgen): Remove dependency on legacy agent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/lansforsakringar/fetcher/transactional/entity/TransferInfoEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/lansforsakringar/fetcher/transactional/entity/TransferInfoEntity.java
@@ -1,16 +1,61 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.lansforsakringar.fetcher.transactional.entity;
 
-import se.tink.backend.aggregation.annotations.JsonObject;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonObject
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TransferInfoEntity {
     private String toText;
     private String fromText;
-    private boolean transferToAccountInternal;
     private String toAccountBankName;
-    private boolean recurring;
-    private String recurringFrequencyText;
     private String finalDate;
-    // `thirdPartyProviderName` is null - cannot define it!
-    // `thirdPartyProviderPaymentReference` is null - cannot define it!
+    private boolean transferToAccountInternal;
+    private boolean recurring;
+
+    public String getToText() {
+        return toText;
+    }
+
+    public void setToText(String toText) {
+        this.toText = toText;
+    }
+
+    public String getFromText() {
+        return fromText;
+    }
+
+    public void setFromText(String fromText) {
+        this.fromText = fromText;
+    }
+
+    public String getToAccountBankName() {
+        return toAccountBankName;
+    }
+
+    public void setToAccountBankName(String toAccountBankName) {
+        this.toAccountBankName = toAccountBankName;
+    }
+
+    public String getFinalDate() {
+        return finalDate;
+    }
+
+    public void setFinalDate(String finalDate) {
+        this.finalDate = finalDate;
+    }
+
+    public boolean isTransferToAccountInternal() {
+        return transferToAccountInternal;
+    }
+
+    public void setTransferToAccountInternal(boolean transferToAccountInternal) {
+        this.transferToAccountInternal = transferToAccountInternal;
+    }
+
+    public boolean isRecurring() {
+        return recurring;
+    }
+
+    public void setRecurring(boolean recurring) {
+        this.recurring = recurring;
+    }
 }
