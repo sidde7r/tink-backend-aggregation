8bbeccb5454c075e68b64465594d65c9197ec03e
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-05-22 16:31:22 +0200

    fix(nordea): use customer_id as SSN when person_id is missing
    also double check that it's a valid personnummer

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/nordea/v30/fetcher/identitydata/rpc/FetchIdentityDataResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/nordea/v30/fetcher/identitydata/rpc/FetchIdentityDataResponse.java
@@ -1,13 +1,22 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.nordea.v30.fetcher.identitydata.rpc;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Strings;
 import java.time.LocalDate;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.libraries.giro.validation.LuhnCheck;
 import se.tink.libraries.identitydata.IdentityData;
 import se.tink.libraries.identitydata.countries.SeIdentityData;
 
 @JsonObject
 public class FetchIdentityDataResponse {
+    private static final Logger log = LoggerFactory.getLogger(FetchIdentityDataResponse.class);
+    private static final String SSN_PATTERN =
+            "^(19|20)\\d{2}(0[1-9]|1[0-2])([0-2]\\d|3[0-1])\\d{4}$";
+
     // e.g. 199201234567
     @JsonProperty("customer_id")
     private String customerId;
@@ -40,13 +49,27 @@ public class FetchIdentityDataResponse {
     private boolean usResident;
 
     public IdentityData toTinkIdentityData() {
-        if (personId == null) {
-            return IdentityData.builder()
-                    .addFirstNameElement(firstName)
-                    .addSurnameElement(lastName)
-                    .setDateOfBirth(LocalDate.parse(birthDate))
-                    .build();
+        // Both customerId and personId are SSN, but personId is missing sometimes.
+        if (!Strings.isNullOrEmpty(personId)
+                && !Strings.isNullOrEmpty(customerId)
+                && !personId.equals(customerId)) {
+            log.warn("person_id doesn't match customer_id");
         }
-        return SeIdentityData.of(firstName, lastName, personId);
+
+        final String ssn = Optional.ofNullable(personId).orElse(customerId);
+        if (isValidSsn(ssn)) {
+            return SeIdentityData.of(firstName, lastName, ssn);
+        }
+
+        return IdentityData.builder()
+                .addFirstNameElement(firstName)
+                .addSurnameElement(lastName)
+                .setDateOfBirth(LocalDate.parse(birthDate))
+                .build();
+    }
+
+    private boolean isValidSsn(String ssn) {
+        return ssn.matches(SSN_PATTERN)
+                && LuhnCheck.isLastCharCorrectLuhnMod10Check(ssn.substring(2));
     }
 }
--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/nordea/v30/NordeaIdentityDataTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/nordea/v30/NordeaIdentityDataTest.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.nordea.v30;
 
+import java.time.format.DateTimeParseException;
 import org.junit.Assert;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.nordea.v30.fetcher.identitydata.rpc.FetchIdentityDataResponse;
@@ -33,7 +34,30 @@ public class NordeaIdentityDataTest {
         Assert.assertEquals(surname, "LASTNAME");
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
+    public void testIdentityDataParsingWithMissingPersonId() {
+        final FetchIdentityDataResponse response =
+                SerializationUtils.deserializeFromString(
+                        responseWithMissingPersonId, FetchIdentityDataResponse.class);
+        IdentityData id = response.toTinkIdentityData();
+        Assert.assertEquals(id.getNameElements().stream().count(), 2);
+        final String firstName =
+                id.getNameElements().stream()
+                        .filter(e -> e.getType() == Type.FIRST_NAME)
+                        .map(NameElement::getValue)
+                        .findFirst()
+                        .orElse("");
+        Assert.assertEquals(firstName, "FIRSTNAME");
+        final String surname =
+                id.getNameElements().stream()
+                        .filter(e -> e.getType() == Type.SURNAME)
+                        .map(NameElement::getValue)
+                        .findFirst()
+                        .orElse("");
+        Assert.assertEquals(surname, "LASTNAME");
+    }
+
+    @Test(expected = DateTimeParseException.class)
     public void testInvalidIdentityParsing() {
         final FetchIdentityDataResponse response =
                 SerializationUtils.deserializeFromString(
@@ -44,10 +68,24 @@ public class NordeaIdentityDataTest {
     // identity response object as it was seen 2020-01-29
     private final String responseWithValidIdentityData =
             "{\n"
-                    + "\t\"customer_id\": \"199201234567\",\n"
+                    + "\t\"customer_id\": \"199201234565\",\n"
                     + "\t\"segment\": \"household\",\n"
                     + "\t\"loyalty_group\": \"\",\n"
-                    + "\t\"person_id\": \"199201234567\",\n"
+                    + "\t\"person_id\": \"199201234565\",\n"
+                    + "\t\"birth_date\": \"1992-01-23\",\n"
+                    + "\t\"first_name\": \"FIRSTNAME\",\n"
+                    + "\t\"last_name\": \"LASTNAME\",\n"
+                    + "\t\"phone_number\": \"46712345678\",\n"
+                    + "\t\"employee\": false,\n"
+                    + "\t\"us_resident\": false\n"
+                    + "}";
+
+    // as seen on 2020-05-22, sometimes person_id is missing, but customer_id is still the SSN
+    private final String responseWithMissingPersonId =
+            "{\n"
+                    + "\t\"customer_id\": \"199201234565\",\n"
+                    + "\t\"segment\": \"household\",\n"
+                    + "\t\"loyalty_group\": \"\",\n"
                     + "\t\"birth_date\": \"1992-01-23\",\n"
                     + "\t\"first_name\": \"FIRSTNAME\",\n"
                     + "\t\"last_name\": \"LASTNAME\",\n"
