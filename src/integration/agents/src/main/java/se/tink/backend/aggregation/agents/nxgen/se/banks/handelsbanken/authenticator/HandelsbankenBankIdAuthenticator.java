1a98390f822310bc3faae39ff1106245185775c6
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-05-22 11:36:42 +0200

    fix(handelsbanken): ensure logged in SSN matches credentials

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/authenticator/HandelsbankenBankIdAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/authenticator/HandelsbankenBankIdAuthenticator.java
@@ -1,13 +1,16 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.authenticator;
 
 import com.google.common.base.Strings;
+import java.util.List;
 import java.util.Optional;
 import se.tink.backend.agents.rpc.Credentials;
 import se.tink.backend.aggregation.agents.bankid.status.BankIdStatus;
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
 import se.tink.backend.aggregation.agents.exceptions.BankIdException;
+import se.tink.backend.aggregation.agents.exceptions.LoginException;
 import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceException;
+import se.tink.backend.aggregation.agents.exceptions.errors.LoginError;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.HandelsbankenSEApiClient;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.HandelsbankenSEConstants;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.HandelsbankenSEConstants.BankIdAuthentication;
@@ -16,8 +19,10 @@ import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.authentic
 import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.authenticator.rpc.bankid.InitBankIdRequest;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.authenticator.rpc.bankid.InitBankIdResponse;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.authenticator.validators.BankidAuthenticationValidator;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.fetcher.identity.HandelsbankenSEIdentityFetcher;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.HandelsbankenPersistentStorage;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.HandelsbankenSessionStorage;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.authenticator.entities.Mandate;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.authenticator.rpc.ApplicationEntryPointResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.authenticator.rpc.auto.AuthorizeResponse;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.bankid.BankIdAuthenticator;
@@ -31,6 +36,7 @@ public class HandelsbankenBankIdAuthenticator implements BankIdAuthenticator<Ini
     private int pollCount;
     private String autoStartToken;
     private String lastWaitingResult;
+    private String givenSsn;
 
     public HandelsbankenBankIdAuthenticator(
             HandelsbankenSEApiClient client,
@@ -46,6 +52,7 @@ public class HandelsbankenBankIdAuthenticator implements BankIdAuthenticator<Ini
     @Override
     public InitBankIdResponse init(String ssn) throws BankIdException, AuthorizationException {
         pollCount = 0;
+        this.givenSsn = ssn;
         return refreshAutostartToken();
     }
 
@@ -66,6 +73,11 @@ public class HandelsbankenBankIdAuthenticator implements BankIdAuthenticator<Ini
         switch (bankIdStatus) {
             case DONE:
                 AuthorizeResponse authorize = client.authorize(authenticate);
+
+                // If SSN is given, check that it matches the logged in user
+                if (!Strings.isNullOrEmpty(this.givenSsn)) {
+                    checkIdentity(this.givenSsn, authorize.getMandates());
+                }
 
                 new BankidAuthenticationValidator(credentials, authorize).validate();
 
@@ -115,5 +127,13 @@ public class HandelsbankenBankIdAuthenticator implements BankIdAuthenticator<Ini
     @Override
     public Optional<OAuth2Token> refreshAccessToken(String refreshToken) {
         return Optional.empty();
+    }
+
+    private void checkIdentity(String ssn, List<Mandate> mandates) throws LoginException {
+        final String identitySsn =
+                HandelsbankenSEIdentityFetcher.mandatesToIdentity(mandates).get().getSsn();
+        if (!identitySsn.equalsIgnoreCase(ssn)) {
+            throw LoginError.INCORRECT_CREDENTIALS.exception();
+        }
     }
 }
