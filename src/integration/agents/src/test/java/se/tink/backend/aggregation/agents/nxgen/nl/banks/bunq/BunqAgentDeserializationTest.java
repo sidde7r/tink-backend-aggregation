79a508c823fa4cd6fc87b9802c3ae5cee5841b46
Farrokh Bolandi <farrokh.bolandi@tink.se>
2020-05-22 14:58:09 +0200

    test(nl/bunq): added de-serialization test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/bunq/BunqAgentDeserializationTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/bunq/BunqAgentDeserializationTest.java
@@ -1,5 +1,22 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.banks.bunq;
 
+import static se.tink.backend.aggregation.agents.nxgen.nl.banks.bunq.BunqAgentTestData.TRANSACTION_01;
+
+import org.junit.Assert;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.bunq.fetchers.transactional.rpc.TransactionWrapper;
+import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
+import se.tink.libraries.serialization.utils.SerializationUtils;
+
 public class BunqAgentDeserializationTest {
 
+    @Test
+    public void testTransactionDescription() {
+        TransactionWrapper transactionEntity =
+                SerializationUtils.deserializeFromString(TRANSACTION_01, TransactionWrapper.class);
+        Transaction transaction = transactionEntity.toTinkTransaction();
+        Assert.assertEquals(
+                transaction.getDescription().trim(),
+                "Topup account NL44BUNQ2025348894Minderhoud\n" + "MJD MINDERHOUD");
+    }
 }
