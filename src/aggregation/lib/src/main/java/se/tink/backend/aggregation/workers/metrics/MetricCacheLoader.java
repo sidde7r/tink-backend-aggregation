ec0728e3ec7e0cd9f59bb7217770aa738209e101
Seckin Savasci <seckin.savasci@tink.se>
2020-05-24 12:01:06 +0200

    fix: remove unused variable

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/metrics/MetricCacheLoader.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/metrics/MetricCacheLoader.java
@@ -1,15 +1,12 @@
 package se.tink.backend.aggregation.workers.metrics;
 
 import java.util.List;
-import se.tink.backend.aggregation.log.AggregationLogger;
 import se.tink.libraries.metrics.core.MetricId;
 import se.tink.libraries.metrics.registry.MetricRegistry;
 import se.tink.libraries.metrics.types.timers.Timer;
 
 // TODO: This class doesn't do anything useful, please remove.
 public class MetricCacheLoader {
-    private static final AggregationLogger log = new AggregationLogger(MetricCacheLoader.class);
-
     private MetricRegistry metricRegistry;
 
     public MetricCacheLoader(MetricRegistry registry) {
