979a15c190a05b5dbfef007c529d31405cd7353f
Sai He <sai.he@tink.se>
2020-05-25 20:23:05 +0200

    fix(DebtorAccount): To fix iban issue and add test

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/sebopenbanking/utils/SebUtils.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/sebopenbanking/utils/SebUtils.java
@@ -1,13 +1,32 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.sebopenbanking.utils;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import se.tink.backend.aggregation.agents.nxgen.se.openbanking.sebopenbanking.executor.payment.entities.PaymentProduct;
 import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.creditcard.CreditCardModule;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.id.IdModule;
 import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.account.identifiers.IbanIdentifier;
 import se.tink.libraries.amount.ExactCurrencyAmount;
 
 public class SebUtils {
+
+    private SebUtils() {}
+
+    private static final List<String> IBAN_PRODUCT_SCOPES =
+            Arrays.asList(
+                    PaymentProduct.SWEDISH_DOMESTIC_PRIVATE_CREDIT_TRANSFERS.getValue(),
+                    PaymentProduct.SEPA_CREDIT_TRANSFER.getValue());
+
+    public static boolean isValidAccountForProduct(String paymentProduct, String accountNumber) {
+        if (IBAN_PRODUCT_SCOPES.stream().anyMatch(paymentProduct::equalsIgnoreCase)) {
+            IbanIdentifier ibanIdentifier = new IbanIdentifier(accountNumber);
+            return ibanIdentifier.isValid();
+        }
+        return true;
+    }
 
     public static ExactCurrencyAmount getZeroBalance(String currencyCode) {
         return ExactCurrencyAmount.of(BigDecimal.ZERO, currencyCode);
---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/sebopenbanking/SebUtilsTest.java
@@ -0,0 +1,49 @@
+package se.tink.backend.aggregation.agents.nxgen.se.openbanking.sebopenbanking;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.exceptions.payment.CreditorValidationException;
+import se.tink.backend.aggregation.agents.exceptions.payment.DebtorValidationException;
+import se.tink.backend.aggregation.agents.nxgen.se.openbanking.sebopenbanking.executor.payment.entities.CreditorAccountEntity;
+import se.tink.backend.aggregation.agents.nxgen.se.openbanking.sebopenbanking.executor.payment.entities.DebtorAccountEntity;
+import se.tink.backend.aggregation.agents.nxgen.se.openbanking.sebopenbanking.utils.SebUtils;
+
+public class SebUtilsTest {
+
+    private static final String IBAN_REQUIRED_PRODUCT = "sepa-credit-transfers";
+    private static final String IBAN_ACCOUNT_EXAMPLE_SEB = "SE4550000000058398257466";
+    private static final String NON_IBAN_ACCOUNT_NUMBER = "56240273996";
+
+    @Test
+    public void testFalseWhenAccountNumberNotIban() {
+        assertFalse(
+                SebUtils.isValidAccountForProduct(IBAN_REQUIRED_PRODUCT, NON_IBAN_ACCOUNT_NUMBER));
+    }
+
+    @Test
+    public void testTrueWhenAccountNumberIban() {
+        assertTrue(
+                SebUtils.isValidAccountForProduct(IBAN_REQUIRED_PRODUCT, IBAN_ACCOUNT_EXAMPLE_SEB));
+    }
+
+    @Test(expected = CreditorValidationException.class)
+    public void testCreditorValidationException() throws CreditorValidationException {
+        CreditorAccountEntity.create(NON_IBAN_ACCOUNT_NUMBER, IBAN_REQUIRED_PRODUCT);
+    }
+
+    @Test(expected = DebtorValidationException.class)
+    public void testDebtorValidationException() throws DebtorValidationException {
+        DebtorAccountEntity.of(NON_IBAN_ACCOUNT_NUMBER);
+    }
+
+    @Test
+    public void testValidAccountEntity()
+            throws CreditorValidationException, DebtorValidationException {
+        DebtorAccountEntity.of(IBAN_ACCOUNT_EXAMPLE_SEB);
+        CreditorAccountEntity.create(IBAN_ACCOUNT_EXAMPLE_SEB, IBAN_REQUIRED_PRODUCT);
+        // No Exception
+        assertTrue(true);
+    }
+}
