d0d61be0fa31bb3f173702115f53a63c569e43cd
Berk Gedik <berk.gedik@tink.se>
2020-05-26 10:20:33 +0200

    refactor(PackageLoader): Prevent code duplication
    Now module loaders are extending an abstract class which contains the main logic for module detection

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/module/loader/AgentDependencyModuleLoaderForDecoupled.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/module/loader/AgentDependencyModuleLoaderForDecoupled.java
@@ -1,40 +1,10 @@
 package se.tink.backend.aggregation.agents.module.loader;
 
-import com.google.common.collect.ImmutableSet;
-import com.google.inject.Module;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModules;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForDecoupledMode;
 
-public final class AgentDependencyModuleLoaderForDecoupled implements AgentDependencyModuleLoader {
+public final class AgentDependencyModuleLoaderForDecoupled extends AgentDependencyModuleLoader {
 
-    private static final ModuleDetector moduleDetector = new ModuleDetector();
-
-    /**
-     * Looks for <code>@AgentDependencyModules</code> on agent and tries to instantiate any guice
-     * modules defined there.
-     *
-     * @param agentClass Class of agent to load modules for.
-     * @return Set of all modules specified in <code>@AgentDependencyModules</code> annotation on
-     *     the agent.
-     * @throws NoSuchMethodException if one or more modules does not have default constructor.
-     */
-    @Override
-    public ImmutableSet<Module> getModulesFromAnnotation(final Class<?> agentClass)
-            throws NoSuchMethodException {
-        List<Class<? extends Module>> modules = new ArrayList<>();
-        if (agentClass.isAnnotationPresent(AgentDependencyModules.class)) {
-            final AgentDependencyModules moduleAnnotation =
-                    agentClass.getAnnotation(AgentDependencyModules.class);
-            modules.addAll(Arrays.asList(moduleAnnotation.modules()));
-        }
-        if (agentClass.isAnnotationPresent(AgentDependencyModulesForDecoupledMode.class)) {
-            final AgentDependencyModulesForDecoupledMode moduleAnnotation =
-                    agentClass.getAnnotation(AgentDependencyModulesForDecoupledMode.class);
-            modules.addAll(Arrays.asList(moduleAnnotation.modules()));
-        }
-        return moduleDetector.getModulesFromAnnotation(modules);
+    public AgentDependencyModuleLoaderForDecoupled() {
+        super(AgentDependencyModulesForDecoupledMode.class);
     }
 }
--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/module/loader/AgentDependencyModuleLoaderForProduction.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/module/loader/AgentDependencyModuleLoaderForProduction.java
@@ -1,40 +1,10 @@
 package se.tink.backend.aggregation.agents.module.loader;
 
-import com.google.common.collect.ImmutableSet;
-import com.google.inject.Module;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModules;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForProductionMode;
 
-public final class AgentDependencyModuleLoaderForProduction implements AgentDependencyModuleLoader {
+public final class AgentDependencyModuleLoaderForProduction extends AgentDependencyModuleLoader {
 
-    private static final ModuleDetector moduleDetector = new ModuleDetector();
-
-    /**
-     * Looks for <code>@AgentDependencyModulesForProduction</code> on agent and tries to instantiate
-     * any guice modules defined there.
-     *
-     * @param agentClass Class of agent to load modules for.
-     * @return Set of all modules specified in <code>@AgentDependencyModulesForProduction</code>
-     *     annotation on the agent.
-     * @throws NoSuchMethodException if one or more modules does not have default constructor.
-     */
-    @Override
-    public ImmutableSet<Module> getModulesFromAnnotation(final Class<?> agentClass)
-            throws NoSuchMethodException {
-        List<Class<? extends Module>> modules = new ArrayList<>();
-        if (agentClass.isAnnotationPresent(AgentDependencyModules.class)) {
-            final AgentDependencyModules moduleAnnotation =
-                    agentClass.getAnnotation(AgentDependencyModules.class);
-            modules.addAll(Arrays.asList(moduleAnnotation.modules()));
-        }
-        if (agentClass.isAnnotationPresent(AgentDependencyModulesForProductionMode.class)) {
-            final AgentDependencyModulesForProductionMode moduleAnnotation =
-                    agentClass.getAnnotation(AgentDependencyModulesForProductionMode.class);
-            modules.addAll(Arrays.asList(moduleAnnotation.modules()));
-        }
-        return moduleDetector.getModulesFromAnnotation(modules);
+    public AgentDependencyModuleLoaderForProduction() {
+        super(AgentDependencyModulesForProductionMode.class);
     }
 }
