057b678226840ba2d9da37993bb4d849d60e7d1b
Jacek Garbulinski <jacek.garbulinski@tink.se>
2020-05-26 16:58:23 +0200

    fix(danske-fi): new version of native app use new client secret

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/danskebank/DanskeBankFIConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/danskebank/DanskeBankFIConfiguration.java
@@ -21,7 +21,7 @@ public class DanskeBankFIConfiguration implements DanskeBankConfiguration {
     private static final String STEP_UP_TOKEN_KEY = "x-stepup-token";
     private static final String CLIENT_ID = "5ec4b8ad-a93d-43e1-831c-8e78ee6e661a";
     private static final String CLIENT_SECRET =
-            "38EhwxUTUYq1KTTbMvfEahKQY3TtXrlifHjwveFBUaqwIgwZ2t";
+            "OZoNeXiyEwSrGGDUOuWumpmzJWPat2tSSOdjct1cGq3maTMyUB";
     private static final String APP_VERSION_HEADER = "MobileBank ios FI 285229709";
 
     @Override
