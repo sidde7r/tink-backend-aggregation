2cd622077608de76ae7c72ce9205f4dca0457036
Vishwanath Sharma <vishwanath.sharma@tink.se>
2020-05-26 09:13:53 +0200

    fix(payments): BPM Agent refactor

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/bpm/manual/BpmAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/bpm/manual/BpmAgentTest.java
@@ -1,4 +1,4 @@
-package se.tink.backend.aggregation.agents.nxgen.it.openbanking.bpm;
+package se.tink.backend.aggregation.agents.nxgen.it.openbanking.bpm.manual;
 
 import org.junit.Before;
 import org.junit.Ignore;
