ab225bcae8d6e8ba8918646837360eb0145ffee0
Mattias Hird <mattias.hird@tink.se>
2020-05-27 15:26:35 +0200

    feat(beneficiary): Add and modify beneficiary requests/responses
    So we are able to make these requests as separate actions.

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/payment/CreateBeneficiaryMultiStepResponse.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/payment/CreateBeneficiaryMultiStepResponse.java
@@ -1,14 +1,36 @@
 package se.tink.backend.aggregation.nxgen.controllers.payment;
 
+import java.util.ArrayList;
 import java.util.List;
 import se.tink.backend.agents.rpc.Field;
-import se.tink.libraries.payment.rpc.Payment;
+import se.tink.backend.aggregation.nxgen.storage.Storage;
+import se.tink.libraries.payment.rpc.AddBeneficiary;
 
-public class CreateBeneficiaryMultiStepResponse {
+public class CreateBeneficiaryMultiStepResponse extends AddBeneficiaryResponse {
     private String step;
     private List<Field> fields;
 
-    public CreateBeneficiaryMultiStepResponse(Payment payment, String step, List<Field> fields) {
+    public CreateBeneficiaryMultiStepResponse(
+            AddBeneficiary beneficiary, Storage storage, String step, List<Field> fields) {
+        super(beneficiary, storage);
+        this.step = step;
+        this.fields = fields;
+    }
+
+    public CreateBeneficiaryMultiStepResponse(
+            CreateBeneficiaryMultiStepRequest createBeneficiaryMultiStepRequest,
+            String step,
+            ArrayList<Field> fields) {
+        super(
+                createBeneficiaryMultiStepRequest.getBeneficiary(),
+                createBeneficiaryMultiStepRequest.getStorage());
+        this.step = step;
+        this.fields = fields;
+    }
+
+    public CreateBeneficiaryMultiStepResponse(
+            AddBeneficiaryResponse addBeneficiaryResponse, String step, ArrayList<Field> fields) {
+        super(addBeneficiaryResponse.getBeneficiary(), addBeneficiaryResponse.getStorage());
         this.step = step;
         this.fields = fields;
     }
