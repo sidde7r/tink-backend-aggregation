7ce690cf92c80039c8930a69d1541e838c5a1086
Berk Gedik <berk.gedik@tink.se>
2020-05-28 00:05:02 +0200

    refactor(AgentTest): minor change

--- src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/agentfactory/utils/TestConfigurationReaderUtil.java
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/agentfactory/utils/TestConfigurationReaderUtil.java
@@ -2,7 +2,6 @@ package se.tink.backend.aggregation.agents.agentfactory.utils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import lombok.Getter;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
@@ -14,14 +13,12 @@ public class TestConfigurationReaderUtil {
     private final AgentFactoryTestConfiguration agentFactoryTestConfiguration;
 
     public TestConfigurationReaderUtil(String configurationPath) {
-        FileInputStream configFileStream = null;
-        try {
-            configFileStream = new FileInputStream(new File(configurationPath));
+        try (FileInputStream configFileStream = new FileInputStream(new File(configurationPath))) {
             Yaml yaml = new Yaml(new Constructor(AgentFactoryTestConfiguration.class));
             yaml.setBeanAccess(BeanAccess.FIELD);
             this.agentFactoryTestConfiguration =
                     yaml.loadAs(configFileStream, AgentFactoryTestConfiguration.class);
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
