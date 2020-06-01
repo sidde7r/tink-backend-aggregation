482a5e23323263cc07a64dfe7ec9942006a45172
Schahin Rajab <schahin.rajab@tink.se>
2020-06-01 16:03:59 +0200

    fix(se/amex): upgrade TimeLine request body

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/amex/v62/AmericanExpressV62SEConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/amex/v62/AmericanExpressV62SEConfiguration.java
@@ -1,12 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.se.creditcards.amex.v62;
 
-import java.util.Date;
 import se.tink.backend.aggregation.agents.nxgen.se.creditcards.amex.v62.fetcher.rpc.TimelineSERequest;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.AmericanExpressV62Configuration;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.AmericanExpressV62Constants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.fetcher.entities.CardEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.amex.v62.fetcher.rpc.TimelineRequest;
-import se.tink.libraries.date.ThreadSafeDateFormat;
 
 public class AmericanExpressV62SEConfiguration implements AmericanExpressV62Configuration {
 
@@ -28,13 +25,13 @@ public class AmericanExpressV62SEConfiguration implements AmericanExpressV62Conf
     @Override
     public TimelineRequest createTimelineRequest(Integer cardIndex) {
         TimelineSERequest request = new TimelineSERequest();
-        request.setTimeZone(AmericanExpressV62Constants.RequestValue.TIME_ZONE);
-        request.setTimeZoneOffset(AmericanExpressV62Constants.RequestValue.TIME_ZONE_OFFSET);
-        request.setSortedIndex(cardIndex);
-        request.setLocalTime(new ThreadSafeDateFormat("MM-dd-YYYY'T'HH:mm:ss").format(new Date()));
-        request.setPendingChargeEnabled(true);
         request.setCmlEnabled(true);
-        request.setTimestamp(Long.toString(System.currentTimeMillis()));
+        request.setGoodsSvcOfferEnabled(false);
+        request.setPayWithPointsEnabled(false);
+        request.setPayYourWayEnabled(false);
+        request.setPendingChargeEnabled(true);
+        request.setPushEnabled(true);
+        request.setSortedIndex(cardIndex);
         return request;
     }
 
