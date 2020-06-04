a62d9887fa7ea3aa8f15cecf004468d4e4b88222
Yunyi Huang <yunyi.huang@tink.com>
2020-06-04 16:35:00 +0200

    refactor(NO): Use getAgentCommonConfiguration

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/dnb/configuration/DnbConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/dnb/configuration/DnbConfiguration.java
@@ -1,23 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.no.openbanking.dnb.configuration;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
-import se.tink.backend.aggregation.agents.nxgen.no.openbanking.dnb.DnbConstants.ErrorMessages;
-import se.tink.backend.aggregation.annotations.AgentConfigParam;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
 
 @JsonObject
-public class DnbConfiguration implements ClientConfiguration {
-
-    @JsonProperty @AgentConfigParam private String redirectUrl;
-
-    public String getRedirectUrl() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(redirectUrl),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Redirect URL"));
-
-        return redirectUrl;
-    }
-}
+public class DnbConfiguration implements ClientConfiguration {}
