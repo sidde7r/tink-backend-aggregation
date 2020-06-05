b8602d2fd5496b2b152c67456a576e83d2537503
Vishwanath Sharma <vishwanath.sharma@tink.se>
2020-06-05 16:46:20 +0200

    fix(CBIGlobe): optimised code

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/CbiGlobeConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/CbiGlobeConstants.java
@@ -105,7 +105,7 @@ public final class CbiGlobeConstants {
     }
 
     public static class HeaderValues {
-        public static final String DEFAULT_PSU_IP_ADDRESS = "0.0.0.0";
+        public static final String DEFAULT_PSU_IP_ADDRESS = "127.0.0.1";
         public static final String UPDATE_PSU_DATA = "updatePsuData";
         public static final String CODE = "code";
     }
