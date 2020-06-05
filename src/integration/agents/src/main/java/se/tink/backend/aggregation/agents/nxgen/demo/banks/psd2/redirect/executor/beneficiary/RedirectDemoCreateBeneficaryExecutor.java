51384086541a8c39025e1adc9fe78c64cd35d683
Mattias Hird <mattias.hird@tink.se>
2020-06-05 13:12:49 +0200

    chore(demoagent): Add logging for beneficiary creation

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/psd2/redirect/executor/beneficiary/RedirectDemoCreateBeneficaryExecutor.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/psd2/redirect/executor/beneficiary/RedirectDemoCreateBeneficaryExecutor.java
@@ -1,6 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.demo.banks.psd2.redirect.executor.beneficiary;
 
 import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.agents.rpc.Credentials;
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
@@ -19,8 +22,10 @@ import se.tink.backend.aggregation.nxgen.controllers.payment.CreateBeneficiaryRe
 import se.tink.backend.aggregation.nxgen.controllers.signing.SigningStepConstants;
 import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
 import se.tink.libraries.payment.enums.CreateBeneficiaryStatus;
+import se.tink.libraries.payment.rpc.Beneficiary;
 
 public class RedirectDemoCreateBeneficaryExecutor implements CreateBeneficiaryExecutor {
+    private Logger log = LoggerFactory.getLogger(RedirectDemoCreateBeneficaryExecutor.class);
 
     private final ThirdPartyAppAuthenticationController thirdPartyAppAuthenticationController;
     private final Credentials credentials;
@@ -40,10 +45,12 @@ public class RedirectDemoCreateBeneficaryExecutor implements CreateBeneficiaryEx
         // AIS auth token.
         PersistentStorage dummyStorage = new PersistentStorage();
 
+        log.info("Creating beneficiary step");
         createBeneficiaryResponse =
                 new CreateBeneficiaryResponse(
                         createBeneficiaryRequest.getBeneficiary(), dummyStorage);
         createBeneficiaryResponse.getBeneficiary().setStatus(CreateBeneficiaryStatus.CREATED);
+        log.info("Done with Creating beneficiary step");
         return this.createBeneficiaryResponse;
     }
 
@@ -51,12 +58,16 @@ public class RedirectDemoCreateBeneficaryExecutor implements CreateBeneficiaryEx
     public CreateBeneficiaryMultiStepResponse sign(
             CreateBeneficiaryMultiStepRequest createBeneficiaryMultiStepRequest)
             throws BeneficiaryException, AuthenticationException {
+        log.info("Signing beneficiary step");
         switch (createBeneficiaryMultiStepRequest.getStep()) {
             case SigningStepConstants.STEP_INIT:
+                log.info("Signing init");
                 return init(createBeneficiaryMultiStepRequest);
             case RedirectAuthenticationDemoAgentConstants.Step.AUTHORIZE:
+                log.info("Signing authorize");
                 return authorized(createBeneficiaryMultiStepRequest);
             case RedirectAuthenticationDemoAgentConstants.Step.CREATE_BENEFICIARY:
+                log.info("Signing create");
                 return createBeneficiary(createBeneficiaryMultiStepRequest);
             default:
                 throw new IllegalStateException(
@@ -67,6 +78,7 @@ public class RedirectDemoCreateBeneficaryExecutor implements CreateBeneficiaryEx
 
     private CreateBeneficiaryMultiStepResponse createBeneficiary(
             CreateBeneficiaryMultiStepRequest createBeneficiaryMultiStepRequest) {
+        log.info("Creating beneficiary");
         String providerName = credentials.getProviderName();
         // This block handles PIS only business use case as source-account will be null in request
         RedirectDemoAgentUtils.throwIfFailStateProvider(providerName);
@@ -76,18 +88,46 @@ public class RedirectDemoCreateBeneficaryExecutor implements CreateBeneficiaryEx
                         createBeneficiaryMultiStepRequest,
                         AuthenticationStepConstants.STEP_FINALIZE,
                         new ArrayList<>());
+        Beneficiary beneficiary =
+                createBeneficiaryMultiStepResponse.getBeneficiary().getBeneficiary();
         createBeneficiaryMultiStepResponse
                 .getBeneficiary()
                 .setStatus(CreateBeneficiaryStatus.ADDED);
+        log.info(
+                "Done with adding beneficiary, name: {}, type: {}, account number: {}, owner account number: {}",
+                beneficiary.getName(),
+                beneficiary.getAccountNumberType(),
+                StringUtils.overlay(
+                        beneficiary.getAccountNumber(),
+                        StringUtils.repeat(
+                                '*',
+                                beneficiary.getAccountNumber().length()
+                                        - (beneficiary.getAccountNumber().length() > 4 ? 4 : 0)),
+                        0,
+                        beneficiary.getAccountNumber().length()
+                                - (beneficiary.getAccountNumber().length() > 4 ? 4 : 0)),
+                StringUtils.overlay(
+                        createBeneficiaryMultiStepResponse.getBeneficiary().getOwnerAccountNumber(),
+                        StringUtils.repeat(
+                                '*',
+                                createBeneficiaryMultiStepResponse
+                                                .getBeneficiary()
+                                                .getOwnerAccountNumber()
+                                                .length()
+                                        - -(beneficiary.getAccountNumber().length() > 4 ? 4 : 0)),
+                        0,
+                        beneficiary.getAccountNumber().length()
+                                - (beneficiary.getAccountNumber().length() > 4 ? 4 : 0)));
         return createBeneficiaryMultiStepResponse;
     }
 
     private CreateBeneficiaryMultiStepResponse init(
             CreateBeneficiaryMultiStepRequest createBeneficiaryMultiStepRequest)
             throws BeneficiaryAuthorizationException {
-
+        log.info("Init step");
         switch (createBeneficiaryMultiStepRequest.getBeneficiary().getStatus()) {
             case CREATED:
+                log.info("Init step created");
                 return new CreateBeneficiaryMultiStepResponse(
                         createBeneficiaryMultiStepRequest, Step.AUTHORIZE, new ArrayList<>());
             case REJECTED:
@@ -105,10 +145,12 @@ public class RedirectDemoCreateBeneficaryExecutor implements CreateBeneficiaryEx
     private CreateBeneficiaryMultiStepResponse authorized(
             CreateBeneficiaryMultiStepRequest createBeneficiaryMultiStepRequest) {
         try {
+            log.info("Authorize step");
             thirdPartyAppAuthenticationController.authenticate(credentials);
             this.createBeneficiaryResponse
                     .getBeneficiary()
                     .setStatus(CreateBeneficiaryStatus.SIGNED);
+            log.info("Done authorizing");
             return new CreateBeneficiaryMultiStepResponse(
                     createBeneficiaryMultiStepRequest, Step.CREATE_BENEFICIARY, new ArrayList<>());
         } catch (AuthenticationException | AuthorizationException e) {
