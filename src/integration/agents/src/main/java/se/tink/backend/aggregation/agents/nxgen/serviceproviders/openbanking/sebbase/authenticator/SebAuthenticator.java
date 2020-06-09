a28c546ff92c32b48fa9e0741d572213925bfc8e
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-06-09 15:06:37 +0200

    fix(seb-ob): handle invalid_grant error in api client

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbase/authenticator/SebAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbase/authenticator/SebAuthenticator.java
@@ -1,21 +1,17 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.authenticator;
 
-import org.apache.commons.httpclient.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.exceptions.SessionException;
 import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceException;
-import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.SebBaseApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.SebCommonConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.SebCommonConstants.Urls;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.authenticator.rpc.ErrorResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.authenticator.rpc.RefreshRequest;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.authenticator.rpc.TokenRequest;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.configuration.SebConfiguration;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2Authenticator;
 import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
-import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
 import se.tink.backend.aggregation.nxgen.http.url.URL;
 
 public class SebAuthenticator implements OAuth2Authenticator {
@@ -61,26 +57,13 @@ public class SebAuthenticator implements OAuth2Authenticator {
     @Override
     public OAuth2Token refreshAccessToken(String refreshToken)
             throws SessionException, BankServiceException {
-
-        RefreshRequest request =
+        final RefreshRequest request =
                 new RefreshRequest(
                         refreshToken,
                         configuration.getClientId(),
                         configuration.getClientSecret(),
                         SebCommonConstants.QueryValues.REFRESH_TOKEN_GRANT);
-
-        try {
-            return client.refreshToken(Urls.OAUTH2_TOKEN, request);
-        } catch (HttpResponseException e) {
-            if (e.getResponse().getStatus() == HttpStatus.SC_BAD_REQUEST) {
-                ErrorResponse error = e.getResponse().getBody(ErrorResponse.class);
-                if (error.isInvalidGrant()) {
-                    log.warn("Invalid refresh token.");
-                    throw SessionError.SESSION_EXPIRED.exception();
-                }
-            }
-            throw e;
-        }
+        return client.refreshToken(Urls.OAUTH2_TOKEN, request);
     }
 
     @Override
