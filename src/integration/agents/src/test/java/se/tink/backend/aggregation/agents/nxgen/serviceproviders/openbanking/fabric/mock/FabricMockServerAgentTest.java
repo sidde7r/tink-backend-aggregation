44864d34638aaab912120d8f59c0a335df08db51
Sai He <sai.he@tink.se>
2020-06-17 15:16:32 +0200

    feat(Wiremock): To add Fabric Wiremock Payment test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/fabric/mock/FabricMockServerAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/fabric/mock/FabricMockServerAgentTest.java
@@ -5,9 +5,16 @@ import static se.tink.libraries.enums.MarketCode.IT;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonFileParser;
 import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
+import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockpayment.AgentWireMockPaymentTest;
 import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockrefresh.AgentWireMockRefreshTest;
 import se.tink.backend.aggregation.configuration.AgentsServiceConfigurationReader;
 import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.amount.ExactCurrencyAmount;
+import se.tink.libraries.payment.rpc.Creditor;
+import se.tink.libraries.payment.rpc.Debtor;
+import se.tink.libraries.payment.rpc.Payment;
+import se.tink.libraries.payment.rpc.Reference;
 
 public class FabricMockServerAgentTest {
     private static final String CONFIGURATION_PATH =
@@ -41,5 +48,40 @@ public class FabricMockServerAgentTest {
 
         // then
         agentWireMockRefreshTest.assertExpectedData(expected);
+    }
+
+    @Test
+    public void testPayment() throws Exception {
+        final String wireMockFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/fabric/mock/resources/fabric-pis.aap";
+        final AgentsServiceConfiguration configuration =
+                AgentsServiceConfigurationReader.read(CONFIGURATION_PATH);
+
+        final AgentWireMockPaymentTest agentWireMockPaymentTest =
+                AgentWireMockPaymentTest.builder(IT, "it-bancasella-ob", wireMockFilePath)
+                        .withConfigurationFile(configuration)
+                        .addPayment(createMockedDomesticPayment())
+                        .withHttpDebugTrace()
+                        .buildWithoutLogin();
+    }
+
+    private Payment createMockedDomesticPayment() {
+        String currency = "EUR";
+        ExactCurrencyAmount amount = ExactCurrencyAmount.of("1.00", currency);
+
+        return new Payment.Builder()
+                .withCreditor(
+                        new Creditor(
+                                AccountIdentifier.create(
+                                        AccountIdentifier.Type.IBAN, "IT55R0503401693000000002042"),
+                                "Translation"))
+                .withDebtor(
+                        new Debtor(
+                                AccountIdentifier.create(
+                                        AccountIdentifier.Type.IBAN, "IT74J032682230005296715315")))
+                .withExactCurrencyAmount(amount)
+                .withCurrency(currency)
+                .withReference(new Reference("TRANSFER", "WTF"))
+                .build();
     }
 }
