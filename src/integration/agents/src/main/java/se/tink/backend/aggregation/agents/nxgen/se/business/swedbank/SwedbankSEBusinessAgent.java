3777bc201fcec45862d3fda9c21612dd653aa72a
Niclas Albinsson <niclas.albinsson@tink.se>
2020-06-17 13:48:56 +0200

    chore(swedbank/business): split to its own agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/swedbank/SwedbankSEBusinessAgent.java
@@ -0,0 +1,11 @@
+package se.tink.backend.aggregation.agents.nxgen.se.business.swedbank;
+
+import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.SwedbankSEAgent;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
+
+public class SwedbankSEBusinessAgent extends SwedbankSEAgent {
+
+    public SwedbankSEBusinessAgent(AgentComponentProvider componentProvider) {
+        super(componentProvider);
+    }
+}
