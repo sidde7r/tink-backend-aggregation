bae16444d501e6827f1a4d6f949c15364c34eb41
Nazri Abdullah <nazri.abdullah@tink.se>
2020-06-22 14:57:39 +0200

    feat(bbva-re): updated authentication with otp

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/authenticator/BbvaAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/authenticator/BbvaAuthenticator.java
@@ -1,43 +1,64 @@
 package se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.authenticator;
 
-import io.vavr.API;
-import io.vavr.Predicates;
-import io.vavr.control.Try;
 import org.apache.http.HttpStatus;
+import se.tink.backend.agents.rpc.Credentials;
+import se.tink.backend.agents.rpc.CredentialsTypes;
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
 import se.tink.backend.aggregation.agents.exceptions.errors.LoginError;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.BbvaApiClient;
-import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.BbvaPredicates;
+import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.BbvaConstants.AuthenticationStates;
+import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.BbvaConstants.CredentialKeys;
+import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.authenticator.rpc.AuthorizeRequest;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.authenticator.rpc.LoginRequest;
+import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.authenticator.rpc.LoginResponse;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.utils.BbvaUtils;
-import se.tink.backend.aggregation.nxgen.controllers.authentication.password.PasswordAuthenticator;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.MultiFactorAuthenticator;
+import se.tink.backend.aggregation.nxgen.controllers.utils.SupplementalInformationHelper;
 import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
 import se.tink.backend.aggregation.nxgen.storage.SessionStorage;
 
-public class BbvaAuthenticator implements PasswordAuthenticator {
+public class BbvaAuthenticator implements MultiFactorAuthenticator {
     private final SessionStorage sessionStorage;
     private BbvaApiClient apiClient;
+    private SupplementalInformationHelper supplementalInformationHelper;
 
-    public BbvaAuthenticator(BbvaApiClient apiClient, SessionStorage sessionStorage) {
+    public BbvaAuthenticator(
+            BbvaApiClient apiClient,
+            SessionStorage sessionStorage,
+            SupplementalInformationHelper supplementalInformationHelper) {
         this.apiClient = apiClient;
         this.sessionStorage = sessionStorage;
+        this.supplementalInformationHelper = supplementalInformationHelper;
     }
 
     @Override
-    public void authenticate(String username, String password)
+    public void authenticate(Credentials credentials)
             throws AuthenticationException, AuthorizationException {
+        String username = credentials.getField(CredentialKeys.USERNAME);
+        String password = credentials.getField(CredentialKeys.PASSWORD);
         final LoginRequest loginRequest =
                 new LoginRequest(BbvaUtils.formatUsername(username), password);
-        Try.of(() -> apiClient.login(loginRequest))
-                .filterTry(
-                        BbvaPredicates.IS_LOGIN_SUCCESS,
-                        () -> new IllegalStateException("Could not authenticate"))
-                .mapFailure(
-                        API.Case(
-                                API.$(Predicates.instanceOf(HttpResponseException.class)),
-                                this::mapHttpErrors))
-                .get();
+        try {
+            LoginResponse loginResponse = apiClient.login(loginRequest);
+            if (loginResponse
+                    .getAuthenticationState()
+                    .equalsIgnoreCase(AuthenticationStates.GO_ON)) {
+                String otpCode = supplementalInformationHelper.waitForOtpInput();
+                final AuthorizeRequest authorizeRequest =
+                        new AuthorizeRequest(
+                                username, otpCode, loginResponse.getMultistepProcessId());
+                LoginResponse authorizeResponse = apiClient.authorize(authorizeRequest);
+            }
+
+        } catch (HttpResponseException ex) {
+            mapHttpErrors(ex);
+        }
+    }
+
+    @Override
+    public CredentialsTypes getType() {
+        return CredentialsTypes.PASSWORD;
     }
 
     private Exception mapHttpErrors(HttpResponseException e) {
