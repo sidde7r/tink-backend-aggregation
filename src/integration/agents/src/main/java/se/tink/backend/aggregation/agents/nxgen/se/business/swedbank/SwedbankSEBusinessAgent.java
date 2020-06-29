3af1d8ff7b1b8f1e7089c64aad7e784bd4150b52
Sebastian Olsson <sebastian.olsson@tink.se>
2020-06-29 15:16:33 +0200

    build(se-swedbank): Added missing annotation

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/swedbank/SwedbankSEBusinessAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/swedbank/SwedbankSEBusinessAgent.java
@@ -1,10 +1,12 @@
 package se.tink.backend.aggregation.agents.nxgen.se.business.swedbank;
 
+import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.SwedbankSEAgent;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 
 public class SwedbankSEBusinessAgent extends SwedbankSEAgent {
 
+    @Inject
     public SwedbankSEBusinessAgent(AgentComponentProvider componentProvider) {
         super(componentProvider);
     }
