f3b94c44485e9c307665057c112f6270be9a5279
Szymon Mysiak <szymon.mysiak@tink.se>
2020-07-02 11:20:19 +0200

    fix(uk/nationwide): delegate custom credit card balance mapper to default

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/nationwide/NationwideCreditCardBalanceMapper.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/nationwide/NationwideCreditCardBalanceMapper.java
@@ -1,31 +1,27 @@
 package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.nationwide;
 
-import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.api.UkOpenBankingApiDefinitions.AccountBalanceType.CLOSING_AVAILABLE;
-
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.entities.AccountBalanceEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.entities.CreditLineEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.mapper.creditcards.CreditCardBalanceMapper;
-import se.tink.backend.aggregation.nxgen.exceptions.NotImplementedException;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.mapper.creditcards.DefaultCreditCardBalanceMapper;
 import se.tink.libraries.amount.ExactCurrencyAmount;
 
+@Slf4j
+@RequiredArgsConstructor
 class NationwideCreditCardBalanceMapper implements CreditCardBalanceMapper {
+
+    private final DefaultCreditCardBalanceMapper defaultCreditCardBalanceMapper;
 
     @Override
     public ExactCurrencyAmount getAccountBalance(Collection<AccountBalanceEntity> balances) {
-        return balances.stream()
-                .filter(b -> CLOSING_AVAILABLE.equals(b.getType()))
-                .map(AccountBalanceEntity::getAmount)
-                .findAny()
-                .orElseThrow(
-                        () ->
-                                new NoSuchElementException(
-                                        "Unable to get available credit - no CLOSING_AVAILABLE available balance found."));
+        return defaultCreditCardBalanceMapper.getAccountBalance(balances);
     }
 
     @Override
@@ -39,11 +35,11 @@ class NationwideCreditCardBalanceMapper implements CreditCardBalanceMapper {
                         .collect(Collectors.toList());
 
         if (availableCreditLines.isEmpty()) {
-            // for now API does not return any credit line, so its impossible to calculate available
-            // credit
+            log.debug(
+                    "Calculating available credit impossible. API did not return credit lines. Setting to 0.");
             return ExactCurrencyAmount.of(BigDecimal.ZERO, "GBP");
         } else {
-            throw new NotImplementedException("Calculation of available credit not implemented.");
+            return defaultCreditCardBalanceMapper.getAvailableCredit(balances);
         }
     }
 }
