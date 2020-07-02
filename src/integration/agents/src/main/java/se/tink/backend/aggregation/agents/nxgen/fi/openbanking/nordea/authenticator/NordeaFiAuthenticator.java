32d087d2e4178484c7f24b236ecef4e1d28ea1ab
Stefanus Louca <stefanus.louca@tink.se>
2020-07-02 08:23:51 +0200

    fix(FI/Nordea/OB): fix IllegalStateException
    When the user cancels the authentication process Nordea will redirect the user back to us without providing a 'code'. This will cause IllegalStateException with the message: callbackData did not contain 'code'.
    
    Instead, they provide httpMessage=error.session.cancelled.
    
    I will now catch it and throw ThirdParty.Cancel error

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/nordea/authenticator/NordeaFiAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/nordea/authenticator/NordeaFiAuthenticator.java
@@ -1,7 +1,13 @@
 package se.tink.backend.aggregation.agents.nxgen.fi.openbanking.nordea.authenticator;
 
+import com.google.common.base.Strings;
+import java.util.Map;
+import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
+import se.tink.backend.aggregation.agents.exceptions.errors.ThirdPartyAppError;
 import se.tink.backend.aggregation.agents.nxgen.fi.openbanking.nordea.NordeaFiApiClient;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.nordeabase.NordeaBaseConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.nordeabase.authenticator.NordeaBaseAuthenticator;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.constants.OAuth2Constants;
 import se.tink.backend.aggregation.nxgen.http.url.URL;
 
 public class NordeaFiAuthenticator extends NordeaBaseAuthenticator {
@@ -15,5 +21,26 @@ public class NordeaFiAuthenticator extends NordeaBaseAuthenticator {
     @Override
     public URL buildAuthorizeUrl(String state) {
         return apiClient.getAuthorizeUrl(state, COUNTRY);
+    }
+
+    @Override
+    public void handleSpecificCallbackDataError(Map<String, String> callbackData)
+            throws AuthenticationException {
+
+        final String code = callbackData.getOrDefault(OAuth2Constants.CallbackParams.CODE, null);
+        final String httpMessage =
+                callbackData.getOrDefault(NordeaBaseConstants.CallbackParams.HTTP_MESSAGE, null);
+
+        if (Strings.isNullOrEmpty(code)) {
+            if (Strings.isNullOrEmpty(httpMessage)) {
+                throw new IllegalStateException(
+                        "callbackData did not contain 'code' or 'httpMessage'");
+            }
+            if (httpMessage.equalsIgnoreCase(NordeaBaseConstants.ErrorCodes.SESSION_CANCELLED)) {
+                throw ThirdPartyAppError.CANCELLED.exception();
+            }
+            throw new IllegalStateException(
+                    String.format("Unknown callbackData for 'httpMessage': %s", httpMessage));
+        }
     }
 }
