e5c5380573573f708cdfd03a83012c545d5d92fd
Piotr Miecielica <piotr.miecielica@tink.se>
2020-07-07 18:09:54 +0200

    refactor(psd2): minor improvements

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/compliance/regulatory_restrictions/RegulatoryRestrictions.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/compliance/regulatory_restrictions/RegulatoryRestrictions.java
@@ -2,6 +2,7 @@ package se.tink.backend.aggregation.compliance.regulatory_restrictions;
 
 import com.google.inject.Inject;
 import java.util.Optional;
+import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.tink.backend.agents.rpc.Account;
@@ -14,7 +15,8 @@ public class RegulatoryRestrictions {
     private static final Logger logger = LoggerFactory.getLogger(RegulatoryRestrictions.class);
 
     @Inject
-    public RegulatoryRestrictions(RegulatoryRestrictionsMetrics regulatoryRestrictionsMetrics) {
+    public RegulatoryRestrictions(
+            @Nullable RegulatoryRestrictionsMetrics regulatoryRestrictionsMetrics) {
         this.regulatoryRestrictionsMetrics = regulatoryRestrictionsMetrics;
     }
 
@@ -23,42 +25,48 @@ public class RegulatoryRestrictions {
             Account account,
             Optional<Psd2PaymentAccountClassificationResult> paymentAccountClassification) {
         try {
-            boolean isRestricted = isRestricted(request, account, paymentAccountClassification);
+            boolean isRestricted = shouldBeRestricted(request, paymentAccountClassification);
             regulatoryRestrictionsMetrics.regulatoryRestrictions(
                     request.getProvider(), account, isRestricted);
             return isRestricted;
-        } catch (
-                RuntimeException
-                        e) { // this is just temporary just to make regulatory feature not fail
+        } catch (RuntimeException e) {
+            // this try-catch is just temporary to make regulatory feature safe and not break
             // other processes
             logger.warn("[RegulatoryRestrictions] Unexpected exception occurred: ", e);
             return false;
         }
     }
 
-    private boolean isRestricted(
+    private boolean shouldBeRestricted(
             CredentialsRequest request,
-            Account account,
             Optional<Psd2PaymentAccountClassificationResult> paymentAccountClassification) {
         return request.getDataFetchingRestrictions().stream()
-                .anyMatch(
-                        restriction -> {
-                            if (restriction
-                                    == DataFetchingRestrictions
-                                            .RESTRICT_FETCHING_PSD2_PAYMENT_ACCOUNTS) {
-                                return paymentAccountClassification.isPresent()
-                                        && paymentAccountClassification.get()
+                .anyMatch(restriction -> isRestricted(paymentAccountClassification, restriction));
+    }
+
+    private boolean isRestricted(
+            Optional<Psd2PaymentAccountClassificationResult> paymentAccountClassification,
+            DataFetchingRestrictions restriction) {
+        switch (restriction) {
+            case RESTRICT_FETCHING_PSD2_PAYMENT_ACCOUNTS:
+                return paymentAccountClassification
+                        .map(
+                                classification ->
+                                        classification
                                                 == Psd2PaymentAccountClassificationResult
-                                                        .PAYMENT_ACCOUNT;
-                            } else if (restriction
-                                    == DataFetchingRestrictions
-                                            .RESTRICT_FETCHING_PSD2_UNDETERMINED_PAYMENT_ACCOUNTS) {
-                                return !paymentAccountClassification.isPresent()
-                                        || paymentAccountClassification.get()
+                                                        .PAYMENT_ACCOUNT)
+                        .orElse(false);
+            case RESTRICT_FETCHING_PSD2_UNDETERMINED_PAYMENT_ACCOUNTS:
+                return paymentAccountClassification
+                        .map(
+                                classification ->
+                                        classification
                                                 == Psd2PaymentAccountClassificationResult
-                                                        .UNDETERMINED_PAYMENT_ACCOUNT;
-                            }
-                            return false;
-                        });
+                                                        .UNDETERMINED_PAYMENT_ACCOUNT)
+                        .orElse(true);
+            default:
+                logger.warn("Unrecognized restriction: " + restriction);
+                return false;
+        }
     }
 }
--- src/integration/lib/src/main/java/se/tink/backend/aggregation/compliance/regulatory_restrictions/RegulatoryRestrictionsMetrics.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/compliance/regulatory_restrictions/RegulatoryRestrictionsMetrics.java
@@ -1,17 +1,19 @@
 package se.tink.backend.aggregation.compliance.regulatory_restrictions;
 
 import com.google.inject.Inject;
+import javax.annotation.Nullable;
 import se.tink.backend.agents.rpc.Account;
 import se.tink.backend.agents.rpc.Provider;
 import se.tink.libraries.metrics.core.MetricId;
 import se.tink.libraries.metrics.registry.MetricRegistry;
 
 public class RegulatoryRestrictionsMetrics {
-    private static final MetricId regulatoryRestriction = MetricId.newId("regulatory_restriction");
+    private static final MetricId regulatoryRestriction =
+            MetricId.newId("aggregation_regulatory_restriction");
     private final MetricRegistry metricRegistry;
 
     @Inject
-    RegulatoryRestrictionsMetrics(MetricRegistry metricRegistry) {
+    RegulatoryRestrictionsMetrics(@Nullable MetricRegistry metricRegistry) {
         this.metricRegistry = metricRegistry;
     }
 
@@ -19,8 +21,9 @@ public class RegulatoryRestrictionsMetrics {
         metricRegistry
                 .meter(
                         regulatoryRestriction
-                                .label("account_type", account.getType().name())
                                 .label("provider", provider.toString())
+                                .label("market", provider.getMarket())
+                                .label("account_type", account.getType().name())
                                 .label("isRestricted", isRestricted))
                 .inc();
     }
