d4f799498b55ca06fef6304598bfa3ffb95ca495
Stefanus Louca <stefanus.louca@tink.se>
2020-07-07 13:50:40 +0200

    fix(Nordea FI/OB): add logging to see callback parameters
    We are expecting 'httpMessage' in the callback from Nordea when the user cancel the authentication. But we are not getting it, resulting in IllegalStateException. I added logging to see what parameters are getting through

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/nordea/authenticator/NordeaFiAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/nordea/authenticator/NordeaFiAuthenticator.java
@@ -2,6 +2,8 @@ package se.tink.backend.aggregation.agents.nxgen.fi.openbanking.nordea.authentic
 
 import com.google.common.base.Strings;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.errors.ThirdPartyAppError;
 import se.tink.backend.aggregation.agents.nxgen.fi.openbanking.nordea.NordeaFiApiClient;
@@ -11,6 +13,7 @@ import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.
 import se.tink.backend.aggregation.nxgen.http.url.URL;
 
 public class NordeaFiAuthenticator extends NordeaBaseAuthenticator {
+    private static final Logger logger = LoggerFactory.getLogger(NordeaFiAuthenticator.class);
 
     private static final String COUNTRY = "FI";
 
@@ -33,6 +36,12 @@ public class NordeaFiAuthenticator extends NordeaBaseAuthenticator {
 
         if (Strings.isNullOrEmpty(code)) {
             if (Strings.isNullOrEmpty(httpMessage)) {
+
+                logger.info(
+                        String.format(
+                                "callbackData keys: %s, values: %s",
+                                callbackData.keySet(), callbackData.values()));
+
                 throw new IllegalStateException(
                         "callbackData did not contain 'code' or 'httpMessage'");
             }
