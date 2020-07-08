8d7d871df32e6e08b1f1fbc8fcd326b7f00e41ff
Sebastian Olsson <sebastian.olsson@tink.se>
2020-07-08 09:03:13 +0200

    revert(se-handelsbanken-ob): Re-enabling BG refreshes
    This reverts commit d9e892fa88fc0984f87fea94c91da7cadfbcaa27, reversing
    changes made to 8b184c36627c465b9579ea482b9d918ec65f30d3.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/handelsbanken/fetcher/transactionalaccount/rpc/TransactionResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/handelsbanken/fetcher/transactionalaccount/rpc/TransactionResponse.java
@@ -1,9 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.fetcher.transactionalaccount.rpc;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.handelsbanken.fetcher.transactionalaccount.entity.TransactionsItemEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
@@ -20,11 +20,9 @@ public class TransactionResponse implements PaginatorResponse {
 
     @Override
     public Collection<? extends Transaction> getTinkTransactions() {
-        // Temporary hack to return empty list (incident IC-124)
-        return Collections.emptyList();
-        /*return transactions.stream()
-        .map(TransactionsItemEntity::toTinkTransaction)
-        .collect(Collectors.toList());*/
+        return transactions.stream()
+                .map(TransactionsItemEntity::toTinkTransaction)
+                .collect(Collectors.toList());
     }
 
     @Override
