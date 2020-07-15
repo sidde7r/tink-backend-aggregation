3c77220311bf1e498b1694e9608f4c6357a8c8ed
Annica Alstermark <annica.alstermark@tink.se>
2020-07-15 13:54:36 +0200

    feature(Swedbank/token): Support challenge exchange for login

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/serviceprovider/authenticator/rpc/InitSecurityTokenChallengeResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/serviceprovider/authenticator/rpc/InitSecurityTokenChallengeResponse.java
@@ -1,15 +1,29 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.authenticator.rpc;
 
+import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.rpc.LinkEntity;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.rpc.LinksEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
 public class InitSecurityTokenChallengeResponse {
     private String challenge;
+    private LinkEntity imageChallenge;
     private LinksEntity links;
     private boolean useOneTimePassword;
 
+    public String getChallenge() {
+        return challenge;
+    }
+
+    public LinkEntity getImageChallenge() {
+        return imageChallenge;
+    }
+
     public LinksEntity getLinks() {
         return links;
+    }
+
+    public boolean isUseOneTimePassword() {
+        return useOneTimePassword;
     }
 }
