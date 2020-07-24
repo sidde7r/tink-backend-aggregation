ef5b8b37087b1af4772c57fe56a6cd1d2218ddc1
Piotr Miecielica <piotr.miecielica@tink.se>
2020-07-24 12:59:07 +0200

    feat(psd2): add new command SendPsd2PaymentClassificationToUpdateServiceAgentWorkerCommand

---
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/SendPsd2PaymentClassificationToUpdateServiceAgentWorkerCommand.java
@@ -0,0 +1,137 @@
+package se.tink.backend.aggregation.workers.commands;
+
+import com.google.common.collect.Lists;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import java.util.List;
+import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.tink.backend.agents.rpc.Account;
+import se.tink.backend.aggregation.aggregationcontroller.ControllerWrapper;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.CorePsd2Classification;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.CoreRegulatoryClassification;
+import se.tink.backend.aggregation.aggregationcontroller.v1.rpc.UpsertRegulatoryClassificationRequest;
+import se.tink.backend.aggregation.compliance.account_classification.psd2_payment_account.Psd2PaymentAccountClassifier;
+import se.tink.backend.aggregation.compliance.account_classification.psd2_payment_account.result.Psd2PaymentAccountClassificationResult;
+import se.tink.backend.aggregation.workers.commands.metrics.MetricsCommand;
+import se.tink.backend.aggregation.workers.context.AgentWorkerCommandContext;
+import se.tink.backend.aggregation.workers.metrics.AgentWorkerCommandMetricState;
+import se.tink.backend.aggregation.workers.metrics.MetricAction;
+import se.tink.backend.aggregation.workers.operation.AgentWorkerCommand;
+import se.tink.backend.aggregation.workers.operation.AgentWorkerCommandResult;
+import se.tink.backend.aggregation.workers.operation.type.AgentWorkerOperationMetricType;
+import se.tink.libraries.metrics.core.MetricId;
+
+public class SendPsd2PaymentClassificationToUpdateServiceAgentWorkerCommand
+        extends AgentWorkerCommand implements MetricsCommand {
+    private static final Logger log =
+            LoggerFactory.getLogger(
+                    SendPsd2PaymentClassificationToUpdateServiceAgentWorkerCommand.class);
+
+    private final AgentWorkerCommandContext context;
+    private final AgentWorkerCommandMetricState metrics;
+    private final Psd2PaymentAccountClassifier psd2PaymentAccountClassifier;
+    private final ControllerWrapper controllerWrapper;
+
+    public SendPsd2PaymentClassificationToUpdateServiceAgentWorkerCommand(
+            AgentWorkerCommandContext context,
+            AgentWorkerCommandMetricState metrics,
+            Psd2PaymentAccountClassifier psd2PaymentAccountClassifier,
+            ControllerWrapper controllerWrapper) {
+        this.context = context;
+        this.metrics = metrics.init(this);
+        this.psd2PaymentAccountClassifier = psd2PaymentAccountClassifier;
+        this.controllerWrapper = controllerWrapper;
+    }
+
+    @Override
+    public String getMetricName() {
+        return "agent_refresh";
+    }
+
+    @Override
+    public AgentWorkerCommandResult execute() throws Exception {
+        metrics.start(AgentWorkerOperationMetricType.EXECUTE_COMMAND);
+        MetricAction action = null;
+        try {
+            log.info("Sending PSD2 Payment Account classification to UpdateService");
+            action =
+                    metrics.buildAction(
+                            new MetricId.MetricLabels()
+                                    .add("action", "send_psd2_account_classification"));
+
+            context.getAccountDataCache()
+                    .getProcessedAccounts()
+                    .forEach(this::sendPsd2AccountClassificationToUpdateService);
+
+            action.completed();
+        } catch (RuntimeException e) {
+            // don't fail refresh if account holder information is not updated
+            log.warn("Couldn't send PSD2 Payment Account Classification to UpdateService", e);
+            if (action != null) {
+                action.failed();
+            }
+        } finally {
+            metrics.stop();
+        }
+
+        return AgentWorkerCommandResult.CONTINUE;
+    }
+
+    private void sendPsd2AccountClassificationToUpdateService(Account processedAccount) {
+        String tinkAccountId = processedAccount.getId();
+        Optional<Psd2PaymentAccountClassificationResult> classification =
+                psd2PaymentAccountClassifier.classify(
+                        context.getRequest().getProvider(), processedAccount);
+        if (!classification.isPresent()) {
+            log.debug(
+                    String.format(
+                            "tinkAccountId: %s has no PSD2 Payment Account Classification",
+                            tinkAccountId));
+            return;
+        }
+        UpsertRegulatoryClassificationRequest request =
+                prepareRequest(tinkAccountId, classification.get());
+
+        try {
+            controllerWrapper.upsertRegulatoryClassification(request);
+        } catch (UniformInterfaceException e) {
+            log.error(
+                    "Request for PSD2 Payment Account Classification update failed, response: "
+                            + (e.getResponse().hasEntity()
+                                    ? e.getResponse().getEntity(String.class)
+                                    : ""));
+            throw e;
+        }
+    }
+
+    private UpsertRegulatoryClassificationRequest prepareRequest(
+            String tinkAccountId, Psd2PaymentAccountClassificationResult classification) {
+        UpsertRegulatoryClassificationRequest request = new UpsertRegulatoryClassificationRequest();
+        request.setAccountId(tinkAccountId);
+        request.setAppId(context.getAppId());
+        request.setUserId(context.getRequest().getUser().getId());
+        CoreRegulatoryClassification coreRegulatoryClassification =
+                new CoreRegulatoryClassification();
+        coreRegulatoryClassification.setPsd2(CorePsd2Classification.of(classification));
+        return request;
+    }
+
+    @Override
+    public void postProcess() throws Exception {
+        // Deliberately left empty.
+    }
+
+    @Override
+    public List<MetricId.MetricLabels> getCommandTimerName(AgentWorkerOperationMetricType type) {
+        MetricId.MetricLabels typeName =
+                new MetricId.MetricLabels()
+                        .add(
+                                "class",
+                                SendPsd2PaymentClassificationToUpdateServiceAgentWorkerCommand.class
+                                        .getSimpleName())
+                        .add("command", type.getMetricName());
+
+        return Lists.newArrayList(typeName);
+    }
+}
