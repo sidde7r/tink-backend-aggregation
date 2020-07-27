fce9ad23c70106cf5494339158ca6f8dc3020c6a
Seckin Savasci <seckin.savasci@tink.se>
2020-07-27 15:45:40 +0200

    feat(PAY1-683): Construct remittance information with unstructured only
    This commit enables clients to construct and send unstructured
    remittance information.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/v31/pis/entity/domestic/RemittanceInformation.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/v31/pis/entity/domestic/RemittanceInformation.java
@@ -1,14 +1,22 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.pis.entity.domestic;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.libraries.payment.rpc.Reference;
 
+/**
+ * https://openbanking.atlassian.net/wiki/spaces/DZ/pages/1077806532/Domestic+Payment+Message+Formats+-+v3.1.2#DomesticPaymentMessageFormats-v3.1.2-ISO20022
+ */
 @JsonObject
 @JsonNaming(PropertyNamingStrategy.UpperCamelCaseStrategy.class)
 public class RemittanceInformation {
+    @JsonInclude(Include.NON_NULL)
     private String unstructured;
+
+    @JsonInclude(Include.NON_NULL)
     private String reference;
 
     // Used in serialization unit tests
@@ -17,6 +25,13 @@ public class RemittanceInformation {
     public RemittanceInformation(String unstructured, Reference reference) {
         this.unstructured = unstructured;
         this.reference = reference.getValue();
+    }
+
+    public static RemittanceInformation ofUnstructured(String unstructured) {
+        RemittanceInformation remittanceInformation = new RemittanceInformation();
+        remittanceInformation.unstructured = unstructured;
+
+        return remittanceInformation;
     }
 
     public String getReference() {
---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/v31/pis/entity/domestic/RemittanceInformationTest.java
@@ -0,0 +1,26 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.pis.entity.domestic;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RemittanceInformationTest {
+    @Test
+    public void testShouldSerializeUnstructuredFieldOnly() throws IOException {
+        // Arrange
+        ObjectMapper objectMapper = new ObjectMapper();
+        String unstructedRemittanceInformation = "TEST_MESSAGE";
+        RemittanceInformation remittanceInformation =
+                RemittanceInformation.ofUnstructured(unstructedRemittanceInformation);
+
+        // Act
+        String serializedRemittanceInformation =
+                objectMapper.writeValueAsString(remittanceInformation);
+
+        // Assert
+        Assert.assertTrue(
+                serializedRemittanceInformation.contains(unstructedRemittanceInformation));
+        Assert.assertFalse(serializedRemittanceInformation.contains("reference"));
+    }
+}
