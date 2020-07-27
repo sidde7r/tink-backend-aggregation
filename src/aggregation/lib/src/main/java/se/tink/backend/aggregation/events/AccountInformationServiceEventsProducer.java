a7d7bca7f429b2c31e65b308a1a9c7d8b64ac591
Piotr Miecielica <piotr.miecielica@tink.se>
2020-07-27 22:31:45 +0200

    feat(psd2): extend and clean events

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/AccountInformationServiceEventsProducer.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/events/AccountInformationServiceEventsProducer.java
@@ -1,12 +1,9 @@
 package se.tink.backend.aggregation.events;
 
 import com.google.protobuf.Any;
-import java.time.Instant;
-import java.util.Optional;
-import javax.inject.Inject;
-import javax.inject.Named;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.tink.backend.agents.rpc.Provider;
 import se.tink.backend.aggregation.compliance.account_capabilities.AccountCapabilities;
 import se.tink.backend.aggregation.source_info.AccountSourceInfo;
 import se.tink.eventproducerservice.events.grpc.AccountHoldersRefreshedEventProto;
@@ -14,6 +11,11 @@ import se.tink.eventproducerservice.events.grpc.AccountSourceInfoEventProto;
 import se.tink.eventproducerservice.events.grpc.Psd2PaymentAccountClassificationEventProto;
 import se.tink.libraries.event_producer_service_client.grpc.EventProducerServiceClient;
 import se.tink.libraries.serialization.proto.utils.ProtobufTypeUtil;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.time.Instant;
+import java.util.Optional;
 
 public class AccountInformationServiceEventsProducer {
     private final EventProducerServiceClient eventProducerServiceClient;
@@ -59,7 +61,11 @@ public class AccountInformationServiceEventsProducer {
 
             eventProducerServiceClient.postEventFireAndForget(Any.pack(event));
         } catch (RuntimeException e) {
-            log.warn("Could not produce event: AccountHoldersRefreshedEvent", e);
+            log.warn(
+                    "Could not produce event: AccountHoldersRefreshedEvent for (userId, correlationId): ({}, {})",
+                    userId,
+                    correlationId,
+                    e);
         }
     }
 
@@ -67,8 +73,7 @@ public class AccountInformationServiceEventsProducer {
             String clusterId,
             String appId,
             String userId,
-            String providerName,
-            String marketCode,
+            Provider provider,
             String correlationId,
             String credentialsId,
             String accountId,
@@ -79,41 +84,67 @@ public class AccountInformationServiceEventsProducer {
             return;
         }
         try {
-            Psd2PaymentAccountClassificationEventProto.Psd2PaymentAccountClassificationEvent event =
-                    Psd2PaymentAccountClassificationEventProto.Psd2PaymentAccountClassificationEvent
-                            .newBuilder()
-                            .setAccountCapabilities(
-                                    Psd2PaymentAccountClassificationEventProto.AccountCapabilities
-                                            .newBuilder()
-                                            .setCanExecuteExternalTransfer(
-                                                    accountCapabilities
-                                                            .getCanExecuteExternalTransfer()
-                                                            .name())
-                                            .setCanReceiveExternalTransfer(
-                                                    accountCapabilities
-                                                            .getCanReceiveExternalTransfer()
-                                                            .name())
-                                            .setCanPlaceFunds(
-                                                    accountCapabilities.getCanPlaceFunds().name())
-                                            .setCanWithdrawCash(
-                                                    accountCapabilities.getCanWithdrawCash().name())
-                                            .build())
-                            .setAccountId(accountId)
-                            .setAccountType(accountType)
-                            .setAppId(appId)
-                            .setUserId(userId)
-                            .setClusterId(clusterId)
-                            .setClassification(classificationResult)
-                            .setCorrelationId(correlationId)
-                            .setCredentialsId(credentialsId)
-                            .setProviderName(providerName)
-                            .setMarketCode(marketCode)
-                            .setTimestamp(ProtobufTypeUtil.toProtobufTimestamp(Instant.now()))
-                            .build();
-            eventProducerServiceClient.postEventFireAndForget(Any.pack(event));
+            Psd2PaymentAccountClassificationEventProto.Psd2PaymentAccountClassificationEvent.Builder
+                    builder =
+                            Psd2PaymentAccountClassificationEventProto
+                                    .Psd2PaymentAccountClassificationEvent.newBuilder()
+                                    .setAccountCapabilities(
+                                            Psd2PaymentAccountClassificationEventProto
+                                                    .AccountCapabilities.newBuilder()
+                                                    .setCanExecuteExternalTransfer(
+                                                            accountCapabilities
+                                                                    .getCanExecuteExternalTransfer()
+                                                                    .name())
+                                                    .setCanReceiveExternalTransfer(
+                                                            accountCapabilities
+                                                                    .getCanReceiveExternalTransfer()
+                                                                    .name())
+                                                    .setCanPlaceFunds(
+                                                            accountCapabilities
+                                                                    .getCanPlaceFunds()
+                                                                    .name())
+                                                    .setCanWithdrawCash(
+                                                            accountCapabilities
+                                                                    .getCanWithdrawCash()
+                                                                    .name())
+                                                    .build())
+                                    .setAccountId(accountId)
+                                    .setAccountType(accountType)
+                                    .setAppId(appId)
+                                    .setUserId(userId)
+                                    .setClusterId(clusterId)
+                                    .setClassification(classificationResult)
+                                    .setCorrelationId(correlationId)
+                                    .setCredentialsId(credentialsId)
+                                    .setProviderName(provider.getName())
+                                    .setMarketCode(provider.getMarket())
+                                    .setProviderAccessType(
+                                            Optional.ofNullable(provider.getAccessType())
+                                                    .map(Enum::name)
+                                                    .orElse(null))
+                                    .setProviderAuthenticationUserType(
+                                            Optional.ofNullable(
+                                                            provider.getAuthenticationUserType())
+                                                    .map(Enum::name)
+                                                    .orElse(null))
+                                    .setTimestamp(
+                                            ProtobufTypeUtil.toProtobufTimestamp(Instant.now()));
+            Optional.ofNullable(provider.getAccessType())
+                    .ifPresent(accessType -> builder.setProviderAccessType(accessType.name()));
+            Optional.ofNullable(provider.getAuthenticationUserType())
+                    .ifPresent(
+                            authUserType ->
+                                    builder.setProviderAuthenticationUserType(authUserType.name()));
+
+            eventProducerServiceClient.postEventFireAndForget(Any.pack(builder.build()));
 
         } catch (RuntimeException e) {
-            log.warn("Could not produce event: Psd2PaymentAccountClassificationEvent", e);
+            log.warn(
+                    "Could not produce event: Psd2PaymentAccountClassificationEvent for (userId, credentialsId, correlationId): ({}, {}, {})",
+                    userId,
+                    credentialsId,
+                    correlationId,
+                    e);
         }
     }
 
@@ -121,8 +152,7 @@ public class AccountInformationServiceEventsProducer {
             String clusterId,
             String appId,
             String userId,
-            String providerName,
-            String marketCode,
+            Provider provider,
             String correlationId,
             String credentialsId,
             String accountId,
@@ -139,8 +169,8 @@ public class AccountInformationServiceEventsProducer {
                             .setClusterId(clusterId)
                             .setCorrelationId(correlationId)
                             .setCredentialsId(credentialsId)
-                            .setProviderName(providerName)
-                            .setMarketCode(marketCode)
+                            .setProviderName(provider.getName())
+                            .setMarketCode(provider.getMarket())
                             .setTimestamp(ProtobufTypeUtil.toProtobufTimestamp(Instant.now()));
             Optional.ofNullable(sourceInfo)
                     .ifPresent(
@@ -155,7 +185,12 @@ public class AccountInformationServiceEventsProducer {
             eventProducerServiceClient.postEventFireAndForget(Any.pack(builder.build()));
 
         } catch (RuntimeException e) {
-            log.warn("Could not produce event: Psd2PaymentAccountClassificationEvent", e);
+            log.warn(
+                    "Could not produce event: AccountSourceInfoEvent for (userId, credentialsId, correlationId): ({}, {}, {})",
+                    userId,
+                    credentialsId,
+                    correlationId,
+                    e);
         }
     }
 }
