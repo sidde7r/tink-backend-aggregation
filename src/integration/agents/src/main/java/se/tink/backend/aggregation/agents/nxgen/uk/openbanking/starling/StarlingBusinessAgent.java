d8395ead528cad56930b54ed7066baf116a71e23
Krzysztof Korolczuk <krzysztof.korolczuk@tink.se>
2020-07-28 16:42:06 +0200

    feat(uk/monzo/starling/business): add agent, test, bump backend version

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/starling/StarlingBusinessAgent.java
@@ -0,0 +1,84 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling;
+
+import com.google.inject.Inject;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
+import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
+import se.tink.backend.aggregation.agents.RefreshSavingsAccountsExecutor;
+import se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.configuration.StarlingConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.featcher.transactional.StarlingTransactionFetcher;
+import se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.featcher.transactional.StarlingTransactionalAccountFetcher;
+import se.tink.backend.aggregation.configuration.agents.AgentConfiguration;
+import se.tink.backend.aggregation.nxgen.agents.SubsequentProgressiveGenerationAgent;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.oauth.progressive.OAuth2Authenticator;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcherController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginationController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
+import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
+
+public final class StarlingBusinessAgent extends SubsequentProgressiveGenerationAgent
+        implements RefreshCheckingAccountsExecutor, RefreshSavingsAccountsExecutor {
+    private final StarlingApiClient apiClient;
+    private final TransactionalAccountRefreshController transactionalAccountRefreshController;
+    private final OAuth2Authenticator authenticator;
+
+    @Inject
+    public StarlingBusinessAgent(AgentComponentProvider componentProvider) {
+        super(componentProvider);
+        final AgentConfiguration<StarlingConfiguration> agentConfiguration =
+                getAgentConfigurationController()
+                        .getAgentConfiguration(StarlingConfiguration.class);
+        final StarlingConfiguration starlingConfiguration =
+                agentConfiguration.getProviderSpecificConfiguration();
+        authenticator =
+                new StarlingOAut2Authenticator(
+                        persistentStorage,
+                        client,
+                        starlingConfiguration.getAisConfiguration(),
+                        agentConfiguration.getRedirectUrl());
+        apiClient = new StarlingApiClient(client, authenticator);
+        transactionalAccountRefreshController = constructTransactionalAccountRefreshController();
+    }
+
+    @Override
+    public FetchAccountsResponse fetchCheckingAccounts() {
+        return transactionalAccountRefreshController.fetchCheckingAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchCheckingTransactions() {
+        return transactionalAccountRefreshController.fetchCheckingTransactions();
+    }
+
+    @Override
+    public FetchAccountsResponse fetchSavingsAccounts() {
+        return transactionalAccountRefreshController.fetchSavingsAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchSavingsTransactions() {
+        return transactionalAccountRefreshController.fetchSavingsTransactions();
+    }
+
+    private TransactionalAccountRefreshController constructTransactionalAccountRefreshController() {
+        return new TransactionalAccountRefreshController(
+                metricRefreshController,
+                updateController,
+                new StarlingTransactionalAccountFetcher(apiClient),
+                new TransactionFetcherController<>(
+                        transactionPaginationHelper,
+                        new TransactionDatePaginationController<>(
+                                new StarlingTransactionFetcher(apiClient))));
+    }
+
+    @Override
+    protected SessionHandler constructSessionHandler() {
+        return SessionHandler.alwaysFail();
+    }
+
+    @Override
+    public OAuth2Authenticator getAuthenticator() {
+        return authenticator;
+    }
+}
