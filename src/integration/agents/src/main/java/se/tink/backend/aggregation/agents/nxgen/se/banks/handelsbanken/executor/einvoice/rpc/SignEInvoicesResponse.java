a2e3bb52f19efe00ca50cd0e9d7c8fbab5b329f9
Shankey Jain <shankey.jain@tink.se>
2020-07-30 10:50:48 +0200

    refactor(SHB/RE): Removing EInvoice rpcs

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/executor/einvoice/rpc/SignEInvoicesResponse.java
+++
@@ -1,6 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.executor.einvoice.rpc;
-
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class SignEInvoicesResponse {}
