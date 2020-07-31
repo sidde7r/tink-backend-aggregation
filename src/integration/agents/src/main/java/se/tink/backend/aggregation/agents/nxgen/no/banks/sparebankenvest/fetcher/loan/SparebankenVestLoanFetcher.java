07c688c9a9dadf59a7c824196b823233fbcafce0
Seckin Savasci <seckin.savasci@tink.se>
2020-07-31 10:15:48 +0200

    refactor(SparebankenVestLoanFetcher): Use slf4j logger

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/sparebankenvest/fetcher/loan/SparebankenVestLoanFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/sparebankenvest/fetcher/loan/SparebankenVestLoanFetcher.java
@@ -1,19 +1,21 @@
 package se.tink.backend.aggregation.agents.nxgen.no.banks.sparebankenvest.fetcher.loan;
 
+import java.lang.invoke.MethodHandles;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.nxgen.no.banks.sparebankenvest.SparebankenVestApiClient;
 import se.tink.backend.aggregation.agents.nxgen.no.banks.sparebankenvest.SparebankenVestConstants;
 import se.tink.backend.aggregation.agents.nxgen.no.banks.sparebankenvest.fetcher.loan.entities.LoanEntity;
 import se.tink.backend.aggregation.agents.nxgen.no.banks.sparebankenvest.fetcher.loan.rpc.FetchLoansResponse;
-import se.tink.backend.aggregation.log.AggregationLogger;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.core.account.loan.LoanAccount;
 
 public class SparebankenVestLoanFetcher implements AccountFetcher<LoanAccount> {
-    private static final AggregationLogger logger =
-            new AggregationLogger(SparebankenVestLoanFetcher.class);
+    private static final Logger logger =
+            LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private final SparebankenVestApiClient apiClient;
 
@@ -46,7 +48,7 @@ public class SparebankenVestLoanFetcher implements AccountFetcher<LoanAccount> {
                         .collect(Collectors.toList());
             }
         } catch (Exception e) {
-            logger.warnExtraLong(e.getMessage(), SparebankenVestConstants.LogTags.LOANS, e);
+            logger.warn("tag={}", SparebankenVestConstants.LogTags.LOANS, e);
         }
 
         return Collections.emptyList();
@@ -56,8 +58,8 @@ public class SparebankenVestLoanFetcher implements AccountFetcher<LoanAccount> {
         try {
             apiClient.fetchCurrencyLoanDetails(loanEntity);
         } catch (Exception e) {
-            logger.infoExtraLong(
-                    "DETAILS: Failed to fetch details: " + e.getMessage(),
+            logger.info(
+                    "tag={} DETAILS: Failed to fetch details",
                     SparebankenVestConstants.LogTags.LOANS,
                     e);
         }
