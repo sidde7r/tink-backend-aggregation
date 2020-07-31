43714bbbe36140fb7d4eb745f6f396b7f0aec122
Seckin Savasci <seckin.savasci@tink.se>
2020-07-31 15:52:14 +0200

    refactor(SparebankenSorCreditCardAccountFetcher): Use slf4j logger

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/sparebankensor/fetcher/creditcard/SparebankenSorCreditCardAccountFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/sparebankensor/fetcher/creditcard/SparebankenSorCreditCardAccountFetcher.java
@@ -1,16 +1,18 @@
 package se.tink.backend.aggregation.agents.nxgen.no.banks.sparebankensor.fetcher.creditcard;
 
+import java.lang.invoke.MethodHandles;
 import java.util.Collection;
 import java.util.Collections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.nxgen.no.banks.sparebankensor.SparebankenSorApiClient;
 import se.tink.backend.aggregation.agents.nxgen.no.banks.sparebankensor.SparebankenSorConstants;
-import se.tink.backend.aggregation.log.AggregationLogger;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
 
 public class SparebankenSorCreditCardAccountFetcher implements AccountFetcher<CreditCardAccount> {
-    private static final AggregationLogger logger =
-            new AggregationLogger(SparebankenSorCreditCardAccountFetcher.class);
+    private static final Logger logger =
+            LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private final SparebankenSorApiClient apiClient;
 
@@ -23,8 +25,8 @@ public class SparebankenSorCreditCardAccountFetcher implements AccountFetcher<Cr
         try {
             apiClient.fetchCreditCards();
         } catch (Exception e) {
-            logger.infoExtraLong(
-                    "Failed to retrieve credit cards",
+            logger.info(
+                    "tag={} Failed to retrieve credit cards",
                     SparebankenSorConstants.LogTags.CREDIT_CARD_LOG_TAG,
                     e);
         }
