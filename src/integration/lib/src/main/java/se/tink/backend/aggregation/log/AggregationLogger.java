537f5bedc280f08f39e97cacb98d1a31cfdd1695
Seckin Savasci <seckin.savasci@tink.se>
2020-08-02 00:12:41 +0200

    refactor(AggregationLogger): Retire `infoExtraLong` method
    This is the last custom method to remove.
    Removing this method also make it possible to remove a few more
    methods and tests.

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/log/AggregationLogger.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/log/AggregationLogger.java
@@ -1,17 +1,7 @@
 package se.tink.backend.aggregation.log;
 
-import com.google.api.client.util.Charsets;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.collect.ImmutableList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-import java.util.function.Consumer;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import se.tink.backend.aggregation.agents.utils.log.LogTag;
 
 /**
  * Extension of AggregationLogger to provide custom formatter for Credentials
@@ -26,8 +16,6 @@ import se.tink.backend.aggregation.agents.utils.log.LogTag;
 @Deprecated
 public class AggregationLogger {
     protected Logger log;
-
-    @VisibleForTesting static final int EXTRA_LONG_LIMIT = 1550;
 
     public AggregationLogger(Class clazz) {
         this.log = LoggerFactory.getLogger(clazz);
@@ -59,54 +47,5 @@ public class AggregationLogger {
 
     public void warn(String message, Throwable e) {
         log.warn(message, e);
-    }
-
-    public void infoExtraLong(String message, LogTag logTag) {
-        logExtraLong(message, logTag, this::info);
-    }
-
-    @VisibleForTesting
-    static void logExtraLong(String message, LogTag logTag, Consumer<String> logger) {
-        // Hack to handle max characters 2048 in logging message
-        UUID uuid = UUID.randomUUID();
-        String messageHeader =
-                String.format("%s - %s - counter: COUNTER - message: ", logTag, uuid);
-        List<String> loggableStrings = toLoggableStrings(messageHeader, message);
-        int totalSize = loggableStrings.size();
-        for (int i = 0; i < totalSize; i++) {
-            logger.accept(
-                    messageHeader.replaceAll("COUNTER", (i + 1) + "/" + totalSize)
-                            + loggableStrings.get(i));
-        }
-    }
-
-    private static List<String> toLoggableStrings(String messageHeader, String message) {
-        if (StringUtils.isBlank(message)) {
-            return Collections.singletonList("Logging empty message.");
-        }
-        ImmutableList.Builder<String> loggableStrings = ImmutableList.builder();
-        int messageLength = EXTRA_LONG_LIMIT - messageHeader.getBytes(Charsets.UTF_8).length;
-        char[] chars = message.toCharArray();
-        int substringLength = 0;
-        int substringStart = 0;
-        for (char aChar : chars) {
-            if (aChar > 255) {
-                substringLength++;
-            }
-
-            if (substringLength >= messageLength) {
-                loggableStrings.add(subString(chars, substringLength, substringStart));
-                substringStart += substringLength;
-                substringLength = 0;
-            }
-
-            substringLength++;
-        }
-        loggableStrings.add(subString(chars, substringLength, substringStart));
-        return loggableStrings.build();
-    }
-
-    private static String subString(char[] chars, int length, int start) {
-        return new String(Arrays.copyOfRange(chars, start, start + length));
     }
 }
