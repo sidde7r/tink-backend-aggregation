72bc3fb45677efebe9d46c6de12ab896ec0b6dfe
Seckin Savasci <seckin.savasci@tink.se>
2020-08-01 11:23:12 +0200

    refactor(LoanDetailsResponse): Use slf4j logger

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/spankki/v2/fetcher/loan/rpc/LoanDetailsResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/spankki/v2/fetcher/loan/rpc/LoanDetailsResponse.java
@@ -2,27 +2,31 @@ package se.tink.backend.aggregation.agents.nxgen.fi.banks.spankki.v2.fetcher.loa
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.lang.invoke.MethodHandles;
 import java.math.BigDecimal;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.spankki.v2.SpankkiConstants;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.spankki.v2.fetcher.loan.entities.LoansEntity;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.spankki.v2.rpc.SpankkiResponse;
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.backend.aggregation.log.AggregationLogger;
 import se.tink.libraries.serialization.utils.SerializationUtils;
 
 @JsonObject
 public class LoanDetailsResponse extends SpankkiResponse {
+    @JsonIgnore
+    private static final Logger logger =
+            LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     @JsonProperty private BigDecimal totalDebtValue;
     @JsonProperty private List<LoansEntity> loans;
 
     @JsonIgnore
-    private static final AggregationLogger logger =
-            new AggregationLogger(LoanDetailsResponse.class);
-
-    @JsonIgnore
     public void logLoans() {
-        logger.infoExtraLong(
-                SerializationUtils.serializeToString(this), SpankkiConstants.LogTags.LOAN_DETAILS);
+        logger.info(
+                "tag={} {}",
+                SpankkiConstants.LogTags.LOAN_DETAILS,
+                SerializationUtils.serializeToString(this));
     }
 }
