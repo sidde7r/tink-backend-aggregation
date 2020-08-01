dc0e90f148c1ee6c7937d17008f7a666508783a0
Seckin Savasci <seckin.savasci@tink.se>
2020-08-01 21:16:24 +0200

    refactor(LoansResponse): Use slf4j logger

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/spankki/v2/fetcher/loan/rpc/LoansResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/spankki/v2/fetcher/loan/rpc/LoansResponse.java
@@ -2,20 +2,23 @@ package se.tink.backend.aggregation.agents.nxgen.fi.banks.spankki.v2.fetcher.loa
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.lang.invoke.MethodHandles;
 import java.math.BigDecimal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.spankki.v2.SpankkiConstants;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.spankki.v2.rpc.SpankkiResponse;
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.backend.aggregation.log.AggregationLogger;
 import se.tink.libraries.serialization.utils.SerializationUtils;
 
 @JsonObject
 public class LoansResponse extends SpankkiResponse {
+    @JsonIgnore
+    private static final Logger logger =
+            LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     @JsonProperty private Boolean hasLoans;
     @JsonProperty private BigDecimal totalBalance;
-
-    @JsonIgnore
-    private static final AggregationLogger logger = new AggregationLogger(LoansResponse.class);
 
     @JsonIgnore
     public boolean hasLoans() {
@@ -24,7 +27,9 @@ public class LoansResponse extends SpankkiResponse {
 
     @JsonIgnore
     public void logLoans() {
-        logger.infoExtraLong(
-                SerializationUtils.serializeToString(this), SpankkiConstants.LogTags.LOAN);
+        logger.info(
+                "tag={} {}",
+                SpankkiConstants.LogTags.LOAN,
+                SerializationUtils.serializeToString(this));
     }
 }
