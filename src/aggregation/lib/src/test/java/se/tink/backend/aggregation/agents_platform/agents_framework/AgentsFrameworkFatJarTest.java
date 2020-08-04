de530725d4c9645ac2893dad49b32e1b2fba4de7
Sebastian Olsson <sebastian.olsson@tink.se>
2020-08-04 08:42:18 +0200

    feat(agents-platform): Added targets for framework and agents-framework
    This will allow the framework and agents-framework targets in
    tink-backend/agents-platform/ to be used as fat jars with all
    third-party dependencies shaded, so that they can be used without
    polluting the aggregaiton service with a bunch of libraries with
    conflicting versions.

---
+++ src/aggregation/lib/src/test/java/se/tink/backend/aggregation/agents_platform/agents_framework/AgentsFrameworkFatJarTest.java
@@ -0,0 +1,17 @@
+package se.tink.backend.aggregation.agents_platform.agents_framework;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public final class AgentsFrameworkFatJarTest {
+
+    @Test
+    public void thirdPartyLibraryClassInFatJarIsRelocated() {
+        Assertions.assertThatCode(
+                        () -> {
+                            Class.forName(
+                                    "agents_platform_agents_framework.com.fasterxml.jackson.core.io.CharTypes");
+                        })
+                .doesNotThrowAnyException();
+    }
+}
