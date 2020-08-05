9702cd3d737aebcf50e31d308cad2fbaddb274aa
Nazri Abdullah <nazri.abdullah@tink.se>
2020-08-05 15:51:11 +0200

    feat(be/kbc): replaced sessionStorage with persistenceStorage

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/kbc/KbcAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/kbc/KbcAgent.java
@@ -31,7 +31,6 @@ public final class KbcAgent extends BerlinGroupAgent<KbcApiClient, KbcConfigurat
     protected KbcApiClient createApiClient() {
         return new KbcApiClient(
                 client,
-                sessionStorage,
                 getConfiguration().getProviderSpecificConfiguration(),
                 getConfiguration().getRedirectUrl(),
                 credentials,
