ae8009cce15e88a3b0031f8fae7000e965921c91
Jacek Garbulinski <jacek.garbulinski@tink.se>
2020-08-06 08:38:12 +0200

    task(fabric): add real time balance, apply cr suggestions

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/fabric/fetcher/transactionalaccount/entities/FabricBalanceTransform.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/fabric/fetcher/transactionalaccount/entities/FabricBalanceTransform.java
@@ -1,29 +1,19 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.fabric.fetcher.transactionalaccount.entities;
 
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.balance.BalanceModule;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.balance.builder.BalanceBuilderStep;
 import se.tink.libraries.amount.ExactCurrencyAmount;
 
 public class FabricBalanceTransform {
-    private static final Map<String, Integer> bookedBalancePriority = new HashMap<>();
 
-    private static final Map<String, Integer> availableBalancePriority = new HashMap<>();
-
-    static {
-        bookedBalancePriority.put("closingBooked", 1);
-        bookedBalancePriority.put("openingBooked", 2);
-        bookedBalancePriority.put("interimAvailable", 3);
-        bookedBalancePriority.put("expected", 4);
-
-        availableBalancePriority.put("interimAvailable", 0);
-        availableBalancePriority.put("expected", 1);
-        availableBalancePriority.put("forwardAvailable", 2);
-    }
+    private static final List<String> bookedBalancePriority =
+            Arrays.asList("closingBooked", "openingBooked", "interimAvailable", "expected");
+    private static final List<String> availableBalancePriority =
+            Arrays.asList("interimAvailable", "expected", "forwardAvailable");
 
     private FabricBalanceTransform() {}
 
@@ -45,12 +35,13 @@ public class FabricBalanceTransform {
     }
 
     private static Optional<ExactCurrencyAmount> getBalanceUsingPriorityMap(
-            final List<BalanceEntity> balances, final Map<String, Integer> balancePriorityMap) {
+            final List<BalanceEntity> balances, final List<String> balancePriority) {
+
         return balances.stream()
-                .filter(balance -> balancePriorityMap.containsKey(balance.getBalanceType()))
+                .filter(balance -> balancePriority.contains(balance.getBalanceType()))
                 .min(
                         Comparator.comparing(
-                                balance -> balancePriorityMap.get(balance.getBalanceType())))
+                                balance -> balancePriority.indexOf(balance.getBalanceType())))
                 .map(balance -> balance.getBalanceAmount().toAmount());
     }
 }
