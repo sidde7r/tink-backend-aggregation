3e3a6dcd1ab21718dbf5e57e8d361ae79f0a5414
Daniel Obrębski <daniel.obrebski@tink.se>
2020-08-11 12:25:44 +0200

    fix(nordea-dk): add support for account with credit option
    ITE-1215

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/AccountFetcherTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/AccountFetcherTest.java
@@ -2,6 +2,7 @@ package se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.NordeaTestData.ACCOUNT_1_API_ID;
+import static se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.NordeaTestData.ACCOUNT_WITH_CREDIT_API_ID;
 
 import java.math.BigDecimal;
 import java.util.Collection;
@@ -27,18 +28,60 @@ public class AccountFetcherTest {
         // when
         Collection<TransactionalAccount> fetchedAccounts = fetcher.fetchAccounts();
         // then
-        assertThat(fetchedAccounts).hasSize(3);
+        assertThat(fetchedAccounts).hasSize(4);
         // and
-        Optional<TransactionalAccount> account1 =
+        TransactionalAccount regularAccount =
                 fetchedAccounts.stream()
                         .filter(account -> ACCOUNT_1_API_ID.equals(account.getApiIdentifier()))
-                        .findAny();
-        assertThat(account1.isPresent()).isTrue();
-        assertThat(account1.get().getExactBalance().getCurrencyCode()).isEqualTo("DKK");
-        assertThat(account1.get().getExactBalance().getExactValue())
+                        .findAny()
+                        .orElse(null);
+        assertRegularAccountValid(regularAccount);
+
+        TransactionalAccount creditAccount =
+                fetchedAccounts.stream()
+                        .filter(
+                                account ->
+                                        ACCOUNT_WITH_CREDIT_API_ID.equals(
+                                                account.getApiIdentifier()))
+                        .findAny()
+                        .orElse(null);
+        assertCreditAccountValid(creditAccount);
+    }
+
+    private void assertRegularAccountValid(TransactionalAccount account) {
+        assertThat(account).isNotNull();
+        assertThat(account.getExactBalance().getCurrencyCode()).isEqualTo("DKK");
+        assertThat(account.getExactBalance().getExactValue())
                 .isEqualByComparingTo(new BigDecimal("91844.66"));
-        assertThat(account1.get().getAccountNumber()).isEqualTo("DK4520007418529630");
-        assertThat(account1.get().getIdModule().getUniqueId()).isEqualTo("7418529630");
+        assertThat(account.getExactAvailableBalance().getCurrencyCode()).isEqualTo("DKK");
+        assertThat(account.getExactAvailableBalance().getExactValue())
+                .isEqualByComparingTo(new BigDecimal("91844.66"));
+        assertThat(account.getExactCreditLimit().getCurrencyCode()).isEqualTo("DKK");
+        assertThat(account.getExactCreditLimit().getExactValue())
+                .isEqualByComparingTo(new BigDecimal("0.00"));
+        assertThat(account.getExactAvailableCredit().getCurrencyCode()).isEqualTo("DKK");
+        assertThat(account.getExactAvailableCredit().getExactValue())
+                .isEqualByComparingTo(new BigDecimal("0.00"));
+        assertThat(account.getAccountNumber()).isEqualTo("DK4520007418529630");
+        assertThat(account.getIdModule().getUniqueId()).isEqualTo("7418529630");
+    }
+
+    private void assertCreditAccountValid(TransactionalAccount creditAccount) {
+        assertThat(creditAccount).isNotNull();
+        assertThat(creditAccount.getExactBalance().getCurrencyCode()).isEqualTo("DKK");
+        assertThat(creditAccount.getExactBalance().getExactValue())
+                .isEqualByComparingTo(new BigDecimal("-10349.23"));
+        assertThat(creditAccount.getExactAvailableCredit().getCurrencyCode()).isEqualTo("DKK");
+        assertThat(creditAccount.getExactAvailableCredit().getExactValue())
+                .isEqualByComparingTo(new BigDecimal("150.77"));
+        assertThat(creditAccount.getExactCreditLimit().getCurrencyCode()).isEqualTo("DKK");
+        assertThat(creditAccount.getExactCreditLimit().getExactValue())
+                .isEqualByComparingTo(new BigDecimal("10500.00"));
+        assertThat(creditAccount.getExactAvailableBalance().getCurrencyCode()).isEqualTo("DKK");
+        assertThat(creditAccount.getExactAvailableBalance().getExactValue())
+                .isEqualByComparingTo(new BigDecimal("0.00"));
+        assertThat(creditAccount.getAccountNumber()).isEqualTo("DK9320002551697498");
+        assertThat(creditAccount.getIdModule().getUniqueId()).isEqualTo("2551697498");
     }
 
     @Test
--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/NordeaDkTestUtils.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/NordeaDkTestUtils.java
@@ -5,74 +5,68 @@ import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
+import java.io.File;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.creditcard.rpc.CreditCardDetailsResponse;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.creditcard.rpc.CreditCardTransactionsResponse;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.creditcard.rpc.CreditCardsResponse;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.investment.rpc.CustodyAccountsResponse;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.transactionalaccount.rpc.AccountsResponse;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.transactionalaccount.rpc.TransactionsResponse;
-import se.tink.backend.aggregation.nxgen.storage.SessionStorage;
+import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class NordeaDkTestUtils {
 
-    private static ObjectMapper mapper = new ObjectMapper();
-
     static NordeaDkApiClient mockApiClient() {
         NordeaDkApiClient client = mock(NordeaDkApiClient.class);
-        try {
-            when(client.getAccounts())
-                    .thenReturn(
-                            mapper.readValue(
-                                    NordeaTestData.FETCH_ACCOUNTS_RESULT, AccountsResponse.class));
-            when(client.getAccountTransactions(
-                            contains(NordeaTestData.ACCOUNT_1_API_ID),
-                            contains("UNGOPS"),
-                            isNull()))
-                    .thenReturn(
-                            mapper.readValue(
-                                    NordeaTestData.FETCH_ACCOUNT_1_TRANSACTIONS_RESULT,
-                                    TransactionsResponse.class));
-            when(client.getAccountTransactions(
-                            contains(NordeaTestData.ACCOUNT_1_API_ID),
-                            contains("UNGOPS"),
-                            contains(NordeaTestData.TRANSACTIONS_CONTINUATION_KEY)))
-                    .thenReturn(
-                            mapper.readValue(
-                                    NordeaTestData.FETCH_ACCOUNT_1_TRANSACTIONS_CONTINUATION,
-                                    TransactionsResponse.class));
-            when(client.fetchCreditCards())
-                    .thenReturn(
-                            mapper.readValue(
-                                    NordeaTestData.FETCH_CREDIT_CARDS, CreditCardsResponse.class));
-            when(client.fetchCreditCardDetails(contains(NordeaTestData.CREDIT_CARD_ID)))
-                    .thenReturn(
-                            mapper.readValue(
-                                    NordeaTestData.FETCH_CREDIT_CARD_DETAILS,
-                                    CreditCardDetailsResponse.class));
-            when(client.fetchCreditCardTransactions(NordeaTestData.CREDIT_CARD_ID, 1))
-                    .thenReturn(
-                            mapper.readValue(
-                                    NordeaTestData.FETCH_CREDIT_CARD_TRANSACTIONS_PAGE1,
-                                    CreditCardTransactionsResponse.class));
-            when(client.fetchCreditCardTransactions(NordeaTestData.CREDIT_CARD_ID, 2))
-                    .thenReturn(
-                            mapper.readValue(
-                                    NordeaTestData.FETCH_CREDIT_CARD_TRANSACTIONS_PAGE2,
-                                    CreditCardTransactionsResponse.class));
-            when(client.fetchInvestments())
-                    .thenReturn(
-                            mapper.readValue(
-                                    NordeaTestData.FETCH_INVESTMENT_ACCOUNTS,
-                                    CustodyAccountsResponse.class));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        return client;
-    }
 
-    static SessionStorage mockSessionStorage() {
-        return mock(SessionStorage.class);
+        when(client.getAccounts())
+                .thenReturn(
+                        SerializationUtils.deserializeFromString(
+                                new File(NordeaTestData.FETCH_ACCOUNTS_FILE_PATH),
+                                AccountsResponse.class));
+        when(client.getAccountTransactions(
+                        contains(NordeaTestData.ACCOUNT_1_API_ID), contains("UNGOPS"), isNull()))
+                .thenReturn(
+                        SerializationUtils.deserializeFromString(
+                                new File(NordeaTestData.FETCH_ACCOUNT_TRANSACTIONS_FILE_PATH),
+                                TransactionsResponse.class));
+        when(client.getAccountTransactions(
+                        contains(NordeaTestData.ACCOUNT_1_API_ID),
+                        contains("UNGOPS"),
+                        contains(NordeaTestData.TRANSACTIONS_CONTINUATION_KEY)))
+                .thenReturn(
+                        SerializationUtils.deserializeFromString(
+                                new File(
+                                        NordeaTestData
+                                                .FETCH_ACCOUNT_TRANSACTIONS_CONTINUATION_FILE_PATH),
+                                TransactionsResponse.class));
+        when(client.fetchCreditCards())
+                .thenReturn(
+                        SerializationUtils.deserializeFromString(
+                                new File(NordeaTestData.FETCH_CREDIT_CARDS_FILE_PATH),
+                                CreditCardsResponse.class));
+        when(client.fetchCreditCardDetails(contains(NordeaTestData.CREDIT_CARD_ID)))
+                .thenReturn(
+                        SerializationUtils.deserializeFromString(
+                                new File(NordeaTestData.FETCH_CREDIT_CARDS_DETAILS_FILE_PATH),
+                                CreditCardDetailsResponse.class));
+        when(client.fetchCreditCardTransactions(NordeaTestData.CREDIT_CARD_ID, 1))
+                .thenReturn(
+                        SerializationUtils.deserializeFromString(
+                                new File(NordeaTestData.FETCH_CREDIT_CARD_TRANSACTIONS_FILE_PATH),
+                                CreditCardTransactionsResponse.class));
+        when(client.fetchCreditCardTransactions(NordeaTestData.CREDIT_CARD_ID, 2))
+                .thenReturn(
+                        SerializationUtils.deserializeFromString(
+                                new File(
+                                        NordeaTestData
+                                                .FETCH_CREDIT_TRANSACTIONS_CONTINUATION_FILE_PATH),
+                                CreditCardTransactionsResponse.class));
+        when(client.fetchInvestments())
+                .thenReturn(
+                        SerializationUtils.deserializeFromString(
+                                new File(NordeaTestData.FETCH_INVESTMENT_ACCOUNTS_FILE_PATH),
+                                CustodyAccountsResponse.class));
+        return client;
     }
 }
