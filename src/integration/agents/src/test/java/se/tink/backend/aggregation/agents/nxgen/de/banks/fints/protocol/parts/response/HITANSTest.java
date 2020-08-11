01d2bf76db2df58eca4558cc64ed10842396f58a
tymon zietek <tymon.zietek@tink.se>
2020-08-11 16:33:37 +0200

    feat(hitans): extends support range for HITANS segment

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/protocol/parts/response/HITANSTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/de/banks/fints/protocol/parts/response/HITANSTest.java
@@ -9,145 +9,9 @@ import se.tink.backend.aggregation.agents.nxgen.de.banks.fints.protocol.parser.R
 public class HITANSTest {
 
     @Test
-    public void shouldParseAuthMethods() {
+    public void shouldParseAuthMethodsV6() {
         // given
-        String[][] arr = {
-            {"HITANS", "14", "6", "4"},
-            {"1"},
-            {"1"},
-            {"0"},
-            {
-                "N",
-                "N",
-                "0",
-                "910",
-                "2",
-                "HHD1.3.2OPT",
-                "HHDOPT1",
-                "1.3.2",
-                "chipTAN optisch HHD1.3.2",
-                "6",
-                "1",
-                "Challenge",
-                "999",
-                "N",
-                "1",
-                "N",
-                "0",
-                "2",
-                "N",
-                "J",
-                "00",
-                "2",
-                "N",
-                "9",
-                "911",
-                "2",
-                "HHD1.3.2",
-                "HHD",
-                "1.3.2",
-                "chipTAN manuell HHD1.3.2",
-                "6",
-                "1",
-                "Challenge",
-                "999",
-                "N",
-                "1",
-                "N",
-                "0",
-                "2",
-                "N",
-                "J",
-                "00",
-                "2",
-                "N",
-                "9",
-                "912",
-                "2",
-                "HHD1.4OPT",
-                "HHDOPT1",
-                "1.4",
-                "chipTAN optisch HHD1.4",
-                "6",
-                "1",
-                "Challenge",
-                "999",
-                "N",
-                "1",
-                "N",
-                "0",
-                "2",
-                "N",
-                "J",
-                "00",
-                "2",
-                "N",
-                "9",
-                "913",
-                "2",
-                "HHD1.4",
-                "HHD",
-                "1.4",
-                "chipTAN manuell HHD1.4",
-                "6",
-                "1",
-                "Challenge",
-                "999",
-                "N",
-                "1",
-                "N",
-                "0",
-                "2",
-                "N",
-                "J",
-                "00",
-                "2",
-                "N",
-                "9",
-                "920",
-                "2",
-                "BestSign",
-                "BestSign",
-                "",
-                "BestSign",
-                "6",
-                "2",
-                "BestSign",
-                "999",
-                "N",
-                "1",
-                "N",
-                "0",
-                "2",
-                "N",
-                "J",
-                "00",
-                "2",
-                "N",
-                "9",
-                "930",
-                "2",
-                "mobileTAN",
-                "mobileTAN",
-                "",
-                "mobileTAN",
-                "6",
-                "2",
-                "mobileTAN",
-                "999",
-                "N",
-                "1",
-                "N",
-                "0",
-                "2",
-                "N",
-                "J",
-                "00",
-                "2",
-                "N",
-                "9"
-            }
-        };
+        String[][] arr = HITANSTestResponse.genHITANSResponseV6();
         RawSegment rawSegment = RawSegmentComposer.compose(arr);
 
         // when
@@ -155,7 +19,6 @@ public class HITANSTest {
 
         // then
         assertThat(segment).isNotNull();
-        assertThat(segment.getSupportedVersions()).hasSize(1).contains(6);
         assertThat(segment.getAllowedScaMethods())
                 .hasSize(6)
                 .containsEntry("930", "mobileTAN")
@@ -164,5 +27,90 @@ public class HITANSTest {
                 .containsEntry("911", "chipTAN manuell HHD1.3.2")
                 .containsEntry("912", "chipTAN optisch HHD1.4")
                 .containsEntry("913", "chipTAN manuell HHD1.4");
+    }
+
+    @Test
+    public void shouldParseAuthMethodsV1() {
+        // given
+        String[][] arr = HITANSTestResponse.genHITANSResponseV1();
+        RawSegment rawSegment = RawSegmentComposer.compose(arr);
+
+        // when
+        HITANS segment = new HITANS(rawSegment);
+
+        // then
+        assertThat(segment).isNotNull();
+        assertThat(segment.getAllowedScaMethods())
+                .hasSize(2)
+                .containsEntry("900", "iTAN")
+                .containsEntry("930", "mobileTAN");
+    }
+
+    @Test
+    public void shouldParseAuthMethodsV2() {
+        // given
+        String[][] arr = HITANSTestResponse.genHITANSResponseV2();
+        RawSegment rawSegment = RawSegmentComposer.compose(arr);
+
+        // when
+        HITANS segment = new HITANS(rawSegment);
+
+        // then
+        assertThat(segment).isNotNull();
+        assertThat(segment.getAllowedScaMethods())
+                .hasSize(2)
+                .containsEntry("900", "iTAN")
+                .containsEntry("930", "mobileTAN");
+    }
+
+    @Test
+    public void shouldParseAuthMethodsV3() {
+        // given
+        String[][] arr = HITANSTestResponse.genHITANSResponseV3();
+        RawSegment rawSegment = RawSegmentComposer.compose(arr);
+
+        // when
+        HITANS segment = new HITANS(rawSegment);
+
+        // then
+        assertThat(segment).isNotNull();
+        assertThat(segment.getAllowedScaMethods())
+                .hasSize(2)
+                .containsEntry("900", "iTAN")
+                .containsEntry("930", "mobileTAN");
+    }
+
+    @Test
+    public void shouldParseAuthMethodsV4() {
+        // given
+        String[][] arr = HITANSTestResponse.genHITANSResponseV4();
+        RawSegment rawSegment = RawSegmentComposer.compose(arr);
+
+        // when
+        HITANS segment = new HITANS(rawSegment);
+
+        // then
+        assertThat(segment).isNotNull();
+        assertThat(segment.getAllowedScaMethods())
+                .hasSize(2)
+                .containsEntry("900", "iTAN")
+                .containsEntry("930", "mobileTAN");
+    }
+
+    @Test
+    public void shouldParseAuthMethodsV5() {
+        // given
+        String[][] arr = HITANSTestResponse.genHITANSResponseV5();
+        RawSegment rawSegment = RawSegmentComposer.compose(arr);
+
+        // when
+        HITANS segment = new HITANS(rawSegment);
+
+        // then
+        assertThat(segment).isNotNull();
+        assertThat(segment.getAllowedScaMethods())
+                .hasSize(2)
+                .containsEntry("900", "iTAN")
+                .containsEntry("930", "mobileTAN");
     }
 }
