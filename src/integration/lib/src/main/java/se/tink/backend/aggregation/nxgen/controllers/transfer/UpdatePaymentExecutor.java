320b07ce8889883e7a761eaed8ac2ff85aa64360
Shankey Jain <shankey.jain@tink.se>
2020-08-13 17:32:23 +0200

    refactor(EInvoice): Removing UpdatePaymentExecutor

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/transfer/UpdatePaymentExecutor.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.nxgen.controllers.transfer;
-
-import se.tink.backend.aggregation.agents.exceptions.transfer.TransferExecutionException;
-import se.tink.libraries.transfer.rpc.Transfer;
-
-public interface UpdatePaymentExecutor {
-    void updatePayment(Transfer transfer) throws TransferExecutionException;
-}
