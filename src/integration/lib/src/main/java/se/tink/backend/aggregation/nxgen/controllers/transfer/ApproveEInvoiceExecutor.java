5362e6e1e03135457bda870732631c499a1d3221
Shankey Jain <shankey.jain@tink.se>
2020-08-13 17:32:03 +0200

    refactor(EInvoice): Removing ApproveEInvoiceExecutor

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/transfer/ApproveEInvoiceExecutor.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.nxgen.controllers.transfer;
-
-import se.tink.backend.aggregation.agents.exceptions.transfer.TransferExecutionException;
-import se.tink.libraries.transfer.rpc.Transfer;
-
-public interface ApproveEInvoiceExecutor {
-    void approveEInvoice(Transfer transfer) throws TransferExecutionException;
-}
