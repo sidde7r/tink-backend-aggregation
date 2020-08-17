8c61e3319fdcc9a1ca83b0260f2eee643ad8f031
Vibhu Srivastava <vibhu.srivastava@tink.com>
2020-08-17 19:38:34 +0200

    feat(provider): add CORPORATE accountHolderType

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/AccountHolderType.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/AccountHolderType.java
@@ -2,7 +2,8 @@ package se.tink.backend.aggregation.nxgen.core.account;
 
 public enum AccountHolderType {
     PERSONAL(se.tink.backend.agents.rpc.AccountHolderType.PERSONAL),
-    BUSINESS(se.tink.backend.agents.rpc.AccountHolderType.BUSINESS);
+    BUSINESS(se.tink.backend.agents.rpc.AccountHolderType.BUSINESS),
+    CORPORATE(se.tink.backend.agents.rpc.AccountHolderType.CORPORATE);
 
     private final se.tink.backend.agents.rpc.AccountHolderType systemType;
 
