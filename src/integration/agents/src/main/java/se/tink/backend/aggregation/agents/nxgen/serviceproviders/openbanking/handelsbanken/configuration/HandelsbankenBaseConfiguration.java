565995120f5a0ea75a8ddad44b2c8a8aaa7e3428
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-08-17 12:11:55 +0200

    chore(handelsbanken): don't mark client ID as sensitive secret

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/handelsbanken/configuration/HandelsbankenBaseConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/handelsbanken/configuration/HandelsbankenBaseConfiguration.java
@@ -2,14 +2,14 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ha
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.backend.aggregation.annotations.SensitiveSecret;
+import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
 import se.tink.backend.aggregation.configuration.agents.ClientIdConfiguration;
 
 @JsonObject
 public class HandelsbankenBaseConfiguration implements ClientConfiguration {
 
-    @JsonProperty @SensitiveSecret @ClientIdConfiguration private String clientId;
+    @JsonProperty @Secret @ClientIdConfiguration private String clientId;
 
     public String getClientId() {
         return clientId;
