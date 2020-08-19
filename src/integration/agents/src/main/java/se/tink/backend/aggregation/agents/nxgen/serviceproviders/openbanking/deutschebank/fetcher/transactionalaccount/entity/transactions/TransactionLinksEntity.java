f87868f5f186036250af614abae7b8f2ec570f86
Mateusz Wiski <mateusz.wiski@tink.se>
2020-08-19 11:18:47 +0200

    fix(DE, DeutscheBank): change mapping of transactions next link

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/deutschebank/fetcher/transactionalaccount/entity/transactions/TransactionLinksEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/deutschebank/fetcher/transactionalaccount/entity/transactions/TransactionLinksEntity.java
@@ -1,18 +1,19 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.deutschebank.fetcher.transactionalaccount.entity.transactions;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Optional;
+import lombok.Getter;
+import se.tink.backend.aggregation.agents.Href;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
+@Getter
 public class TransactionLinksEntity {
 
-    private String next;
+    @JsonProperty("next")
+    private Href nextLink;
 
     public boolean hasNextLink() {
-        return Optional.ofNullable(next).isPresent();
-    }
-
-    public String getNextLink() {
-        return next;
+        return Optional.ofNullable(nextLink).map(Href::getHref).isPresent();
     }
 }
