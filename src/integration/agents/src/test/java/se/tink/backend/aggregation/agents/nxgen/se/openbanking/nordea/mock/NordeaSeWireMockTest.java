26a3a3e18c3983b25756f498c386eaa00ef41e6a
Schahin Rajab <schahin.rajab@tink.se>
2020-08-20 15:31:26 +0200

    refactor(nordea): rename folder to lower case

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/nordea/mock/NordeaSeWireMockTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/nordea/mock/NordeaSeWireMockTest.java
@@ -1,34 +1,34 @@
-package se.tink.backend.aggregation.agents.nxgen.fi.openbanking.nordea.wiremock;
+package se.tink.backend.aggregation.agents.nxgen.se.openbanking.nordea.mock;
 
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonFileParser;
 import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
 import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockrefresh.AgentWireMockRefreshTest;
-import se.tink.backend.aggregation.agents.nxgen.fi.openbanking.nordea.wiremock.module.NordeaWireMockTestModule;
+import se.tink.backend.aggregation.agents.nxgen.se.openbanking.nordea.mock.module.NordeaWireMockTestModule;
 import se.tink.backend.aggregation.configuration.AgentsServiceConfigurationReader;
 import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
 import se.tink.libraries.credentials.service.RefreshableItem;
 import se.tink.libraries.enums.MarketCode;
 
-public class NordeaFiWireMockTest {
+public class NordeaSeWireMockTest {
 
     @Test
     public void testRefresh() throws Exception {
 
         // given
         final String configurationPath =
-                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/nordea/wiremock/resources/configuration.yml";
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/nordea/mock/resources/configuration.yml";
         final String wireMockServerFilePath =
-                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/nordea/wiremock/resources/wireMock.aap";
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/nordea/mock/resources/wireMock.aap";
         final String contractFilePath =
-                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/nordea/wiremock/resources/agent-contract.json";
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/nordea/mock/resources/agent-contract.json";
 
         final AgentsServiceConfiguration configuration =
                 AgentsServiceConfigurationReader.read(configurationPath);
 
         final AgentWireMockRefreshTest agentWireMockRefreshTest =
                 AgentWireMockRefreshTest.builder(
-                                MarketCode.FI, "fi-nordea-oauth2", wireMockServerFilePath)
+                                MarketCode.SE, "se-nordea-ob", wireMockServerFilePath)
                         .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
                         .withConfigurationFile(configuration)
                         .addCallbackData("code", "dummyCode")
