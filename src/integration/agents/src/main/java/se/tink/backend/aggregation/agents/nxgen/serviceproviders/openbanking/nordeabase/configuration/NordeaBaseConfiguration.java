59a45e2a99361cbc29d5c7e3b071059e59e9a08f
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-08-21 10:34:14 +0200

    refactor(nordea/ob): use lombok in configuration

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/nordeabase/configuration/NordeaBaseConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/nordeabase/configuration/NordeaBaseConfiguration.java
@@ -1,11 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.nordeabase.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Iterables;
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaDescription;
 import java.util.List;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.nordeabase.NordeaBaseConstants;
+import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.annotations.SensitiveSecret;
@@ -15,6 +13,7 @@ import se.tink.backend.aggregation.configuration.agents.ClientSecretsConfigurati
 import se.tink.backend.aggregation.configuration.agents.ScopesConfiguration;
 
 @JsonObject
+@Getter
 public class NordeaBaseConfiguration implements ClientConfiguration {
     @JsonProperty @Secret @ClientIdConfiguration private String clientId;
     @JsonProperty @SensitiveSecret @ClientSecretsConfiguration private String clientSecret;
@@ -25,22 +24,4 @@ public class NordeaBaseConfiguration implements ClientConfiguration {
     @JsonSchemaDescription(
             "These are the service scopes that PSD2 regulates: Payment initiation services (PIS) and account information services (AIS). This should not exceed your app's subscriptions on Nordea, or your PSD2 certification.")
     private List<String> scopes;
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
-    }
-
-    public List<String> getScopes() {
-        Preconditions.checkNotNull(
-                scopes,
-                String.format(NordeaBaseConstants.ErrorMessages.INVALID_CONFIGURATION, "scopes"));
-        Preconditions.checkArgument(
-                !Iterables.isEmpty(scopes),
-                String.format(NordeaBaseConstants.ErrorMessages.EMPTY_CONFIGURATION, "scopes"));
-        return scopes;
-    }
 }
