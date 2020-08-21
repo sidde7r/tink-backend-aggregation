a472169ead890546707d053e8399edc945f17e72
Niclas Albinsson <niclas.albinsson@tink.se>
2020-08-21 11:40:53 +0200

    chore(swedbank/business/re): add transaction details

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/serviceprovider/rpc/TransactionDetails.java
@@ -0,0 +1,19 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.rpc;
+
+import com.google.common.base.Strings;
+import org.apache.commons.lang.StringUtils;
+import se.tink.backend.aggregation.annotations.JsonObject;
+
+public class TransactionDetails {
+    public final String transactionReference;
+    public final String ownReference;
+
+    @JsonObject
+    public TransactionDetails(String transactionReference, String ownReference) {
+        this.transactionReference =
+                !Strings.isNullOrEmpty(transactionReference)
+                        ? transactionReference
+                        : StringUtils.EMPTY;
+        this.ownReference = !Strings.isNullOrEmpty(ownReference) ? ownReference : StringUtils.EMPTY;
+    }
+}
