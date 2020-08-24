c98b3112fef45c38214362221684790dc6da00a9
Daniel Obrębski <daniel.obrebski@tink.se>
2020-08-24 17:01:26 +0200

    fix(SDC-DK): Change from OTP to PIN authentication
    All banks which has low login success rate uses OTP authentication.
    In the past in such cases developers changed auth from OTP to PIN.
    There is a possibility that it can fix current errors.
    
    I chose 2 banks with low success rate Broager Sparekasse and Sparekassen Djursland
    to check if change can help.
    
    ITE-1265

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/sdcdk/SdcDkConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/sdcdk/SdcDkConstants.java
@@ -7,7 +7,9 @@ public class SdcDkConstants {
 
     public static class Authentication {
         public static final ImmutableList<String> BANKS_WITH_PIN_AUTHENTICATION =
-                ImmutableList.of("9740", "9922", "9388", "9283", "0537", "8412", "9684", "0579");
+                ImmutableList.of(
+                        "9740", "9922", "9388", "9283", "0537", "8412", "9684", "0579", "9797",
+                        "9388");
     }
 
     public static class Market {
