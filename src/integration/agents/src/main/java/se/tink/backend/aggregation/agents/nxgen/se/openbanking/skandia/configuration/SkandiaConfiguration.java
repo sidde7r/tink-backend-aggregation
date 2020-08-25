8817fbf7a244f299d9b1f8a2719a9bbfb15fcf61
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-08-25 16:42:09 +0200

    chore(skandia/tppSecrets): add example and pattern

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/skandia/configuration/SkandiaConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/skandia/configuration/SkandiaConfiguration.java
@@ -1,8 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.skandia.configuration;
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
-import se.tink.backend.aggregation.agents.nxgen.se.openbanking.skandia.SkandiaConstants.ErrorMessages;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaExamples;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaInject;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaString;
+import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.annotations.SensitiveSecret;
@@ -11,19 +12,17 @@ import se.tink.backend.aggregation.configuration.agents.ClientIdConfiguration;
 import se.tink.backend.aggregation.configuration.agents.ClientSecretsConfiguration;
 
 @JsonObject
+@Getter
 public class SkandiaConfiguration implements ClientConfiguration {
-    @Secret @ClientIdConfiguration private String clientId;
-    @SensitiveSecret @ClientSecretsConfiguration private String clientSecret;
+    @Secret
+    @ClientIdConfiguration
+    @JsonSchemaExamples("4578616d706c652c2074657374696e67")
+    @JsonSchemaInject(strings = {@JsonSchemaString(path = "pattern", value = "^[0-9a-f]{32}$")})
+    private String clientId;
 
-    public String getClientId() {
-        return Preconditions.checkNotNull(
-                Strings.emptyToNull(clientId),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client ID"));
-    }
-
-    public String getClientSecret() {
-        return Preconditions.checkNotNull(
-                Strings.emptyToNull(clientSecret),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client Secret"));
-    }
+    @SensitiveSecret
+    @ClientSecretsConfiguration
+    @JsonSchemaExamples("4578616d706c652c2074657374696e67")
+    @JsonSchemaInject(strings = {@JsonSchemaString(path = "pattern", value = "^[0-9a-f]{32}$")})
+    private String clientSecret;
 }
