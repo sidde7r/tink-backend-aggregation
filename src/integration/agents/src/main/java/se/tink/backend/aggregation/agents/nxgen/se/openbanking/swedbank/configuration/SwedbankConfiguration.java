a36df1af255f83a533d83011cf1bc3c26e4ca5bc
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-08-25 17:15:21 +0200

    chore(swedbank/tppSecrets): add example and pattern

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/swedbank/configuration/SwedbankConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/swedbank/configuration/SwedbankConfiguration.java
@@ -1,5 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.swedbank.configuration;
 
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaExamples;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaInject;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaString;
 import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
@@ -11,6 +14,15 @@ import se.tink.backend.aggregation.configuration.agents.ClientSecretsConfigurati
 @JsonObject
 @Getter
 public class SwedbankConfiguration implements ClientConfiguration {
-    @Secret @ClientIdConfiguration private String clientId;
-    @SensitiveSecret @ClientSecretsConfiguration private String clientSecret;
+    @Secret
+    @ClientIdConfiguration
+    @JsonSchemaExamples("l4578616d706c652c2074657374696e671")
+    @JsonSchemaInject(strings = {@JsonSchemaString(path = "pattern", value = "^[0-9a-z]{20,40}$")})
+    private String clientId;
+
+    @SensitiveSecret
+    @ClientSecretsConfiguration
+    @JsonSchemaExamples("4578616d706c652c2074657374696e67")
+    @JsonSchemaInject(strings = {@JsonSchemaString(path = "pattern", value = "^[0-9a-f]{32}$")})
+    private String clientSecret;
 }
