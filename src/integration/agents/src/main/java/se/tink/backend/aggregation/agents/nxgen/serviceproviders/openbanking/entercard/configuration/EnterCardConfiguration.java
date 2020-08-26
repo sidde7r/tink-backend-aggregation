eaa04c828ea0a92f27b82e9f0361ed07737366d9
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-08-26 13:56:00 +0200

    chore(entercard/tppSecrets): add example and pattern

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/entercard/configuration/EnterCardConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/entercard/configuration/EnterCardConfiguration.java
@@ -1,34 +1,26 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.entercard.configuration;
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.entercard.EnterCardConstants.ErrorMessages;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaExamples;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaInject;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaString;
+import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.annotations.SensitiveSecret;
 import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
 import se.tink.backend.aggregation.configuration.agents.ClientIdConfiguration;
 import se.tink.backend.aggregation.configuration.agents.ClientSecretsConfiguration;
+import se.tink.backend.aggregation.configuration.agents.UUIDConfiguration;
 
 @JsonObject
+@Getter
 public class EnterCardConfiguration implements ClientConfiguration {
+    @Secret @ClientIdConfiguration @UUIDConfiguration private String clientId;
 
-    @Secret @ClientIdConfiguration private String clientId;
-    @SensitiveSecret @ClientSecretsConfiguration private String clientSecret;
-
-    public String getClientId() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(clientId),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client ID"));
-
-        return clientId;
-    }
-
-    public String getClientSecret() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(clientSecret),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client Secret"));
-
-        return clientSecret;
-    }
+    @SensitiveSecret
+    @ClientSecretsConfiguration
+    @JsonSchemaExamples("WB1ZGjko-MvSIS2s1HhDOIhwss_7A6eeSjORr2IBR4L71rtoMTGo4sA9ACgEQKjM")
+    @JsonSchemaInject(
+            strings = {@JsonSchemaString(path = "pattern", value = "^[-_A-Za-z0-9]{30,60}$")})
+    private String clientSecret;
 }
