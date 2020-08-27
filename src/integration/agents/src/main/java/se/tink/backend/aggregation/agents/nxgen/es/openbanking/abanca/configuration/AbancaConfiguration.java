0265f8092b68150ef4f42e7fb56e34a06b245dac
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-08-27 09:55:38 +0200

    chore(abanca/tppSecrets): annotate configuration

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/openbanking/abanca/configuration/AbancaConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/openbanking/abanca/configuration/AbancaConfiguration.java
@@ -1,32 +1,24 @@
 package se.tink.backend.aggregation.agents.nxgen.es.openbanking.abanca.configuration;
 
-import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
-import se.tink.backend.aggregation.agents.nxgen.es.openbanking.abanca.AbancaConstants.ErrorMessages;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaDescription;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle;
+import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.annotations.SensitiveSecret;
 import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
+import se.tink.backend.aggregation.configuration.agents.ClientIdConfiguration;
+import se.tink.backend.aggregation.configuration.agents.UUIDConfiguration;
 
 @JsonObject
+@Getter
 public class AbancaConfiguration implements ClientConfiguration {
 
-    @Secret private String clientId;
-    @SensitiveSecret private String apiKey;
+    @Secret @ClientIdConfiguration @UUIDConfiguration private String clientId;
 
-    public String getApiKey() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(apiKey),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "API Key"));
-
-        return apiKey;
-    }
-
-    public String getClientId() {
-        Preconditions.checkNotNull(
-                Strings.emptyToNull(clientId),
-                String.format(ErrorMessages.INVALID_CONFIGURATION, "Client ID"));
-
-        return clientId;
-    }
+    @SensitiveSecret
+    @JsonSchemaTitle("Production API Key")
+    @JsonSchemaDescription("The production API key for your App in the Abanca developer portal.")
+    @UUIDConfiguration
+    private String apiKey;
 }
