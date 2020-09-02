da5bddb2fa11db30e527887875d0eebc8ab4505e
Schahin Rajab <schahin.rajab@tink.se>
2020-09-02 16:21:10 +0200

    fix(nxgen/sbab): remove unknown type json fields
    these fields were not boolean

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/sbab/fetcher/transactionalaccounts/entities/KycEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/sbab/fetcher/transactionalaccounts/entities/KycEntity.java
@@ -4,19 +4,9 @@ import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
 public class KycEntity {
-    private boolean error;
     private boolean kycOk;
-    private boolean kycStatusReport;
-
-    public boolean isError() {
-        return error;
-    }
 
     public boolean isKycOk() {
         return kycOk;
-    }
-
-    public boolean isKycStatusReport() {
-        return kycStatusReport;
     }
 }
