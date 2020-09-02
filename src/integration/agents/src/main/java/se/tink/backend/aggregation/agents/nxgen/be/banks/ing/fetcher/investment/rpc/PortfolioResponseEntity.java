f71df5dcb9fd9ba09c3f71664f73a26a48e4d065
Mateusz Konstanciak <mateusz.konstanciak@tink.se>
2020-09-02 15:12:57 +0200

    fix(BE/Ing): Fix NPE on investment accounts fetch IFD-1342

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/ing/fetcher/investment/rpc/PortfolioResponseEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/banks/ing/fetcher/investment/rpc/PortfolioResponseEntity.java
@@ -1,14 +1,14 @@
 package se.tink.backend.aggregation.agents.nxgen.be.banks.ing.fetcher.investment.rpc;
 
 import se.tink.backend.aggregation.agents.nxgen.be.banks.ing.entites.json.BaseMobileResponseEntity;
-import se.tink.backend.aggregation.agents.nxgen.be.banks.ing.fetcher.investment.entities.PortfolioEntity;
+import se.tink.backend.aggregation.agents.nxgen.be.banks.ing.fetcher.investment.entities.PortfoliosEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
 public class PortfolioResponseEntity extends BaseMobileResponseEntity {
-    private PortfolioEntity portfolio;
+    private PortfoliosEntity portfolios;
 
-    public PortfolioEntity getPortfolio() {
-        return portfolio;
+    public PortfoliosEntity getPortfolios() {
+        return portfolios;
     }
 }
