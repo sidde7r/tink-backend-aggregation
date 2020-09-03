fb3377835909128af7884b6123d20e5680892365
Schahin Rajab <schahin.rajab@tink.se>
2020-09-03 11:16:42 +0200

    test(nxgen/sbab): add all refreshable items

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/sbab/manual/SBABAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/sbab/manual/SBABAgentTest.java
@@ -2,11 +2,14 @@ package se.tink.backend.aggregation.agents.nxgen.se.banks.sbab.manual;
 
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
 public class SBABAgentTest {
     @Test
     public void test() throws Exception {
         new AgentIntegrationTest.Builder("se", "se-sbab-bankid")
+                .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(true)
                 .build()
