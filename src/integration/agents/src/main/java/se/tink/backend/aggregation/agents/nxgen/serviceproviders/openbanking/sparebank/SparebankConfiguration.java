ff2e12125f1849bbe6aa7971bcc24113a62919e9
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-09-03 16:52:52 +0200

    fix(tppSecrets): add JsonObject annotations to empty configurations

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/SparebankConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/SparebankConfiguration.java
@@ -1,5 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sparebank;
 
+import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
 
+@JsonObject
 public class SparebankConfiguration implements ClientConfiguration {}
