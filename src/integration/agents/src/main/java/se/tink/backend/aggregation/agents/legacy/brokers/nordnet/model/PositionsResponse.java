632a73449d8a3664f9ef90e3794766bf85167e48
Nazri Abdullah <nazri.abdullah@tink.se>
2020-09-05 01:54:06 +0200

    refactor(legacy/nordnet): purged nordnet legacy agent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/brokers/nordnet/model/PositionsResponse.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.brokers.nordnet.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.ArrayList;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class PositionsResponse extends ArrayList<PositionEntity> {}
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/brokers/nordnet/model/Response/AccountResponse.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.agents.brokers.nordnet.model.Response;
-
-import java.util.ArrayList;
-import se.tink.backend.aggregation.agents.brokers.nordnet.model.AccountEntity;
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class AccountResponse extends ArrayList<AccountEntity> {}
