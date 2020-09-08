1690602bb69a4bb9863b1a927ea978a3b9707ec7
Szymon Mysiak <szymon.mysiak@tink.se>
2020-09-08 13:39:13 +0200

    fix(uk/barclays): fix organisation id

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/barclays/BarclaysConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/barclays/BarclaysConstants.java
@@ -2,7 +2,7 @@ package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.barclays;
 
 public final class BarclaysConstants {
 
-    public static final String ORGANISATION_ID = "001580000103UBKAA2";
+    public static final String ORGANISATION_ID = "0015800000jfAW1AAM";
 
     class Urls {
 
