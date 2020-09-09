fbbd9ce77ed68d930a708a78a9e9586f5f377e4a
Nazri Abdullah <nazri.abdullah@tink.se>
2020-09-09 21:58:11 +0200

    fix(nl/triodos): updated ibans entry as list

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/triodos/TriodosAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/triodos/TriodosAgentTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager.IbanArgumentEnum;
+import se.tink.backend.aggregation.agents.nxgen.nl.openbanking.triodos.TriodosConstants.CredentialKeys;
 
 public class TriodosAgentTest {
 
@@ -18,12 +19,13 @@ public class TriodosAgentTest {
         manager.before();
         builder =
                 new AgentIntegrationTest.Builder("nl", "nl-triodos-ob")
-                        .addCredentialField("IBAN", manager.get(IbanArgumentEnum.IBAN))
+                        .addCredentialField(
+                                CredentialKeys.IBANS, manager.get(IbanArgumentEnum.IBAN))
                         .setFinancialInstitutionId("triodos")
                         .setAppId("tink")
                         .setClusterId("oxford-preprod")
-                        .loadCredentialsBefore(true)
-                        .saveCredentialsAfter(true)
+                        .loadCredentialsBefore(false)
+                        .saveCredentialsAfter(false)
                         .expectLoggedIn(false);
     }
 
