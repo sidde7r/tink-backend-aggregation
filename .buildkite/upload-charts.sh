75ef290a7e1366734974251c30982ca83dffa44d
Charles Carlfjord <charles.carlfjord@tink.se>
2020-09-09 14:29:51 +0200

    fix(ci) check that service has a chart before uploading

--- .buildkite/upload-charts.sh
+++ .buildkite/upload-charts.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 main() {
     local service_names=("$@")
     for service_name in "${service_names[@]}"; do
+        if [[ ! -f ".charts/${service_name}/Chart.yaml" ]]; then
+            continue
+        fi
         /go/bin/kubernetes-generator --mode push --version "${VERSION}" --repo . --chart "${service_name}";
     done
 }
