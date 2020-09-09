8afa46fca4ed72577701f8b3a9fc6f51f39b88f8
Artur Ciskowski <artur.ciskowski@tink.se>
2020-09-09 13:02:40 +0200

    refactor(Bpost/OB): Moving transaction fetching to Xs2aAgent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/bpost/BpostAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/bpost/BpostAgent.java
@@ -3,18 +3,11 @@ package se.tink.backend.aggregation.agents.nxgen.be.openbanking.bpost;
 import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.nxgen.be.openbanking.bpost.authenticator.BPostAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.Xs2aDevelopersTransactionalAgent;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.fetcher.transactionalaccount.Xs2aDevelopersTransactionalAccountFetcher;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.fetcher.transactionalaccount.Xs2aDevelopersTransactionalAccountTransactionDateFromFetcher;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.ThirdPartyAppAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2AuthenticationController;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcher;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionKeyWithInitDateFromFetcherController;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
-import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 
 public final class BpostAgent extends Xs2aDevelopersTransactionalAgent {
 
@@ -24,21 +17,6 @@ public final class BpostAgent extends Xs2aDevelopersTransactionalAgent {
     public BpostAgent(AgentComponentProvider componentProvider) {
         super(componentProvider, "https://api.psd2.bpostbank.be");
         bPostAuthenticator = new BPostAuthenticator(apiClient, persistentStorage, configuration);
-    }
-
-    @Override
-    protected TransactionalAccountRefreshController getTransactionalAccountRefreshController() {
-        final AccountFetcher accountFetcher =
-                new Xs2aDevelopersTransactionalAccountFetcher(apiClient, oauth2Authenticator);
-
-        final TransactionFetcher<TransactionalAccount> transactionFetcher =
-                new TransactionKeyWithInitDateFromFetcherController<>(
-                        request,
-                        new Xs2aDevelopersTransactionalAccountTransactionDateFromFetcher(
-                                apiClient, oauth2Authenticator));
-
-        return new TransactionalAccountRefreshController(
-                metricRefreshController, updateController, accountFetcher, transactionFetcher);
     }
 
     @Override
