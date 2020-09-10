81281ce7698ba2bc9fbd1d5ac2293aace387bb25
Farrokh Bolandi <farrokh.bolandi@tink.se>
2020-09-10 18:27:56 +0200

    fix(se-swedbank-ob): no transaction type in description

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/swedbank/fetcher/transactionalaccount/entity/transaction/TransactionEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/swedbank/fetcher/transactionalaccount/entity/transaction/TransactionEntity.java
@@ -1,10 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.swedbank.fetcher.transactionalaccount.entity.transaction;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.common.base.Strings;
 import java.util.Date;
 import java.util.Optional;
-import se.tink.backend.aggregation.agents.nxgen.se.openbanking.swedbank.SwedbankConstants.Format;
 import se.tink.backend.aggregation.agents.nxgen.se.openbanking.swedbank.fetcher.transactionalaccount.entity.common.TransactionAmountEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
@@ -24,15 +22,13 @@ public class TransactionEntity {
 
     @JsonIgnore
     public Transaction toTinkTransaction(boolean isPending) {
-        // Usually structured gives the type of transaction while unstructured gives more context
-        String description =
-                Strings.nullToEmpty(remittanceInformationStructured)
-                        + Format.TRANSACTION_DESCRIPTION_SEPARATOR
-                        + Strings.nullToEmpty(remittanceInformationUnstructured);
+
         return Transaction.builder()
                 .setAmount(getTransactionAmount())
                 .setDate(Optional.ofNullable(transactionDate).orElse(valueDate))
-                .setDescription(description.trim())
+                .setDescription(
+                        Optional.ofNullable(remittanceInformationUnstructured)
+                                .orElse(remittanceInformationStructured))
                 .setPending(isPending)
                 .build();
     }
