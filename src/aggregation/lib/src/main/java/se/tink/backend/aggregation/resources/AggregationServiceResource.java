3b0f604d75bb88995df8a6e23aae6453f428e4cc
Piotr Miecielica <piotr.miecielica@tink.se>
2020-09-11 15:04:03 +0200

    log(psd2): add more temp DFP logging

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/resources/AggregationServiceResource.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/resources/AggregationServiceResource.java
@@ -48,6 +48,7 @@ import se.tink.backend.aggregation.workers.worker.AgentWorker;
 import se.tink.backend.aggregation.workers.worker.AgentWorkerOperationFactory;
 import se.tink.backend.aggregation.workers.worker.AgentWorkerRefreshOperationCreatorWrapper;
 import se.tink.libraries.credentials.service.CreateCredentialsRequest;
+import se.tink.libraries.credentials.service.CredentialsRequest;
 import se.tink.libraries.credentials.service.ManualAuthenticateRequest;
 import se.tink.libraries.credentials.service.RefreshInformationRequest;
 import se.tink.libraries.credentials.service.RefreshableItem;
@@ -131,6 +132,7 @@ public class AggregationServiceResource implements AggregationService {
         if (!RefreshableItem.hasAccounts(itemsToRefresh)) {
             HttpResponseHelper.error(Response.Status.BAD_REQUEST);
         }
+        logDPF(request, "configureWhitelistInformation");
         agentWorker.execute(
                 agentWorkerCommandFactory.createOperationConfigureWhitelist(request, clientInfo));
     }
@@ -155,6 +157,7 @@ public class AggregationServiceResource implements AggregationService {
         if (!RefreshableItem.hasAccounts(itemsToRefresh)) {
             HttpResponseHelper.error(Response.Status.BAD_REQUEST);
         }
+        logDPF(request, "refreshWhitelistInformation");
         agentWorker.execute(
                 agentWorkerCommandFactory.createOperationWhitelistRefresh(request, clientInfo));
     }
@@ -163,6 +166,7 @@ public class AggregationServiceResource implements AggregationService {
     public void refreshInformation(final RefreshInformationRequest request, ClientInfo clientInfo)
             throws Exception {
         if (request.isManual()) {
+            logDPF(request, "refreshInformation");
             agentWorker.execute(
                     agentWorkerCommandFactory.createOperationRefresh(request, clientInfo));
         } else {
@@ -173,6 +177,18 @@ public class AggregationServiceResource implements AggregationService {
                         AgentWorkerRefreshOperationCreatorWrapper.of(
                                 agentWorkerCommandFactory, request, clientInfo));
             }
+        }
+    }
+
+    private void logDPF(CredentialsRequest request, String method) {
+        try {
+            logger.info(
+                    "{} for credentialsId: {}, restrictions: {}",
+                    method,
+                    request.getCredentials().getId(),
+                    request.getDataFetchingRestrictions());
+        } catch (RuntimeException e) {
+            logger.warn("Couldn't log: ", e);
         }
     }
 
