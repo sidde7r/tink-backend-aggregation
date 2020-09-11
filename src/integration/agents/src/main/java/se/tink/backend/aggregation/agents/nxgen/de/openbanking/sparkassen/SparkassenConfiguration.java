f929a67584c45c0a8cbc70de70c20cd02f9cdbe2
Jan Sameryt <jan.sameryt@tink.com>
2020-09-11 13:35:49 +0200

    fix(sparkassen): ITE-1364 this time with @JsonObject

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/sparkassen/SparkassenConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/sparkassen/SparkassenConfiguration.java
@@ -1,5 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.de.openbanking.sparkassen;
 
+import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
 
+@JsonObject
 public class SparkassenConfiguration implements ClientConfiguration {}
