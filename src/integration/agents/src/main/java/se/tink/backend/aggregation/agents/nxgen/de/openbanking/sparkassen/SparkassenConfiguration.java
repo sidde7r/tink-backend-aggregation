0896a524f95c874f658ac3a2bbd822b945a70906
tymon zietek <tymon.zietek@tink.se>
2020-09-11 09:47:14 +0200

    fix(Sparkassen): adds SparkassenConfiguration class
    ITE-1364 - Json schema for secret template generationfor customers needs
     agent specific class called Configuration for template even that it's
     empty

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/sparkassen/SparkassenConfiguration.java
@@ -0,0 +1,5 @@
+package se.tink.backend.aggregation.agents.nxgen.de.openbanking.sparkassen;
+
+import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
+
+public class SparkassenConfiguration implements ClientConfiguration {}
