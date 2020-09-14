bcd8439ba4eeced3d904370f10e1bf08ed0a7b2d
Schahin Rajab <schahin.rajab@tink.se>
2020-09-14 17:53:49 +0200

    chore(legacy/sbab): delete legacy sbab agent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/sbab/rpc/AccountsResponse.java
+++
@@ -1,6 +0,0 @@
-package se.tink.backend.aggregation.agents.banks.sbab.rpc;
-
-import java.util.ArrayList;
-import se.tink.backend.aggregation.agents.banks.sbab.entities.AccountEntity;
-
-public class AccountsResponse extends ArrayList<AccountEntity> {}
