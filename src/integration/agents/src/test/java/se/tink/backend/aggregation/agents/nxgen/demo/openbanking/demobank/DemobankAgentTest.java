8c7660edc22d72572de7f52adea0635fb87ada84
Vibhu Srivastava <vibhu.srivastava@tink.com>
2020-09-18 16:24:14 +0200

    feat(demobank): add identity data in agent test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/demo/openbanking/demobank/DemobankAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/demo/openbanking/demobank/DemobankAgentTest.java
@@ -1,9 +1,12 @@
 package se.tink.backend.aggregation.agents.nxgen.demo.openbanking.demobank;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.libraries.credentials.service.RefreshableItem;
 
 public class DemobankAgentTest {
     private static final String USERNAME = "u0001";
@@ -13,11 +16,16 @@ public class DemobankAgentTest {
 
     @Before
     public void setup() {
+        Set<RefreshableItem> items = new HashSet<>();
+        items.addAll(RefreshableItem.REFRESHABLE_ITEMS_ALL);
+        items.add(RefreshableItem.IDENTITY_DATA);
+
         builder =
                 new AgentIntegrationTest.Builder("uk", "uk-demobank-password")
                         .addCredentialField(Field.Key.USERNAME, USERNAME)
                         .addCredentialField(Field.Key.PASSWORD, PASSWORD)
                         .loadCredentialsBefore(false)
+                        .setRefreshableItems(items)
                         .saveCredentialsAfter(false)
                         .expectLoggedIn(false);
     }
