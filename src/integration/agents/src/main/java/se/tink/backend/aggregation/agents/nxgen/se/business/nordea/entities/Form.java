d6c042bdcd1cdbdc4dd0f5f9fddb2efa8e62da9d
Niclas Albinsson <niclas.albinsson@tink.se>
2020-09-21 14:23:35 +0200

    fix(nordea/se/business/re): form builder

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/nordea/entities/Form.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/nordea/entities/Form.java
@@ -1,25 +1,13 @@
 package se.tink.backend.aggregation.agents.nxgen.se.business.nordea.entities;
 
-import static se.tink.backend.aggregation.nxgen.http.form.Form.builder;
-
 import java.util.Map;
-import javax.annotation.Nonnull;
-import se.tink.backend.aggregation.nxgen.http.form.Form.Builder;
+import se.tink.backend.aggregation.nxgen.http.form.AbstractForm;
 
-public class Form {
-    private final Builder formBuilder = builder();
+public class Form extends AbstractForm {
 
     public Form() {}
 
     public Form(final Map<String, String> initMap) {
         initMap.forEach(this::put);
-    }
-
-    public void put(@Nonnull String key, @Nonnull String value) {
-        formBuilder.put(key, value);
-    }
-
-    public String getBodyValue() {
-        return formBuilder.build().serialize();
     }
 }
--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/form/AbstractForm.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/form/AbstractForm.java
@@ -11,12 +11,12 @@ public abstract class AbstractForm {
     }
 
     /** Add key-value parameter. */
-    protected void put(@Nonnull String key, @Nonnull String value) {
+    public void put(@Nonnull String key, @Nonnull String value) {
         formBuilder.put(key, value);
     }
 
     /** Add parameter without a value. */
-    protected void put(@Nonnull String key) {
+    public void put(@Nonnull String key) {
         formBuilder.put(key);
     }
 }
