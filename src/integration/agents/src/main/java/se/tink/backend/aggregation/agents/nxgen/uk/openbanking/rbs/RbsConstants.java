59eb1f778adc307b7ec17dfd9b657a2e666e0b59
Bartlomiej Swieczak <bartlomiej.swieczak@tink.se>
2020-09-24 09:36:20 +0200

    feat(uk/rbs): Add new agent for RBS corporate accounts

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/rbs/RbsConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/rbs/RbsConstants.java
@@ -7,8 +7,10 @@ final class RbsConstants {
         class V31 {
             public static final String AIS_API_URL = "https://api.rbs.co.uk/open-banking/v3.1/aisp";
             public static final String PIS_API_URL = "https://api.rbs.co.uk/open-banking/v3.1/pisp";
-            public static final String WELL_KNOWN_URL =
+            public static final String PERSONAL_WELL_KNOWN_URL =
                     "https://personal.secure1.rbs.co.uk/.well-known/openid-configuration";
+            public static final String CORPORATE_WELL_KNOWN_URL =
+                    "https://corporate.secure1.rbs.co.uk/.well-known/openid-configuration";
         }
     }
 }
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/rbs/RbsV31CorporateAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/rbs/RbsV31CorporateAgent.java
@@ -1,56 +1,53 @@
 package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.rbs;
 
 import com.google.inject.Inject;
-import java.util.Optional;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForDecoupledMode;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForProductionMode;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.UkOpenBankingBaseAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.entities.AccountOwnershipType;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingAis;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingAisConfig;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingPisConfig;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module.UkOpenBankingLocalKeySignerModule;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module.UkOpenBankingLocalKeySignerModuleForDecoupledMode;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UKOpenBankingAis;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UkOpenBankingV31Ais;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UkOpenBankingV31PisConfiguration;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.mapper.creditcards.CreditCardAccountMapper;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.mapper.creditcards.RbsGroupCreditCardBalanceMapper;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.mapper.identifier.IdentifierMapper;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.pis.UKOpenbankingV31Executor;
 import se.tink.backend.aggregation.agents.nxgen.uk.openbanking.rbs.RbsConstants.Urls.V31;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date.LocalDateTimeSource;
-import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.randomness.RandomValueGenerator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.jwt.signer.iface.JwtSigner;
-import se.tink.backend.aggregation.nxgen.controllers.payment.PaymentController;
 import se.tink.libraries.mapper.PrioritizedValueExtractor;
 
 @AgentDependencyModulesForProductionMode(modules = UkOpenBankingLocalKeySignerModule.class)
 @AgentDependencyModulesForDecoupledMode(
         modules = UkOpenBankingLocalKeySignerModuleForDecoupledMode.class)
-public class RbsV31Agent extends UkOpenBankingBaseAgent {
+public class RbsV31CorporateAgent extends UkOpenBankingBaseAgent {
 
     private static final UkOpenBankingAisConfig aisConfig;
-    private final UkOpenBankingPisConfig pisConfig;
     private final LocalDateTimeSource localDateTimeSource;
-    private final RandomValueGenerator randomValueGenerator;
 
     static {
         aisConfig =
                 UKOpenBankingAis.builder()
                         .withOrganisationId("0015800000jfwB4AAI")
                         .withApiBaseURL(V31.AIS_API_URL)
-                        .withWellKnownURL(V31.PERSONAL_WELL_KNOWN_URL)
+                        .withWellKnownURL(V31.CORPORATE_WELL_KNOWN_URL)
+                        .withAllowedAccountOwnershipType(AccountOwnershipType.BUSINESS)
                         .build();
     }
 
     @Inject
-    public RbsV31Agent(AgentComponentProvider componentProvider, JwtSigner jwtSigner) {
+    public RbsV31CorporateAgent(AgentComponentProvider componentProvider, JwtSigner jwtSigner) {
         super(componentProvider, jwtSigner, aisConfig, false);
-        pisConfig = new UkOpenBankingV31PisConfiguration(V31.PIS_API_URL);
         this.localDateTimeSource = componentProvider.getLocalDateTimeSource();
-        this.randomValueGenerator = componentProvider.getRandomValueGenerator();
+    }
+
+    @Override
+    protected Authenticator constructAuthenticator() {
+        return super.constructAuthenticator(aisConfig);
     }
 
     @Override
@@ -62,26 +59,5 @@ public class RbsV31Agent extends UkOpenBankingBaseAgent {
                         new IdentifierMapper(valueExtractor));
         return new UkOpenBankingV31Ais(
                 aisConfig, persistentStorage, creditCardAccountMapper, localDateTimeSource);
-    }
-
-    @Override
-    protected Authenticator constructAuthenticator() {
-        return super.constructAuthenticator(aisConfig);
-    }
-
-    @Override
-    public Optional<PaymentController> constructPaymentController() {
-
-        UKOpenbankingV31Executor paymentExecutor =
-                new UKOpenbankingV31Executor(
-                        pisConfig,
-                        softwareStatement,
-                        providerConfiguration,
-                        apiClient,
-                        supplementalInformationHelper,
-                        credentials,
-                        strongAuthenticationState,
-                        randomValueGenerator);
-        return Optional.of(new PaymentController(paymentExecutor, paymentExecutor));
     }
 }
