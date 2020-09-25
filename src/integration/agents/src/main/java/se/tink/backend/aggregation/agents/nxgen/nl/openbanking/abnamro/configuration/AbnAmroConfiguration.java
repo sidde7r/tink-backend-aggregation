51f70d38223e28cc01f1811d21f541a3edfc27c6
Schahin Rajab <schahin.rajab@tink.se>
2020-09-25 15:34:23 +0200

    chore(abn/secrets): configure secrets to console

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/abnamro/configuration/AbnAmroConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/abnamro/configuration/AbnAmroConfiguration.java
@@ -1,29 +1,30 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.openbanking.abnamro.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaDescription;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaExamples;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaInject;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaString;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle;
+import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
-import se.tink.backend.aggregation.annotations.SensitiveSecret;
 import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
+import se.tink.backend.aggregation.configuration.agents.ClientIdConfiguration;
 
 @JsonObject
+@Getter
 public class AbnAmroConfiguration implements ClientConfiguration {
+    @JsonProperty @ClientIdConfiguration @Secret private String clientId;
 
-    @JsonProperty @Secret private String clientId;
+    @JsonProperty
+    @JsonSchemaTitle("Api Key")
+    @JsonSchemaDescription("Api Key is a public identifier for apps.")
+    @JsonSchemaExamples("vhjoYm9Bo4UciG8Xh8W0F4n05o3XpaET")
+    @JsonSchemaInject(strings = {@JsonSchemaString(path = "pattern", value = "^[A-Za-z0-9]{32}$")})
+    @Secret
+    private String apiKey;
 
-    @JsonProperty @SensitiveSecret private String apiKey;
-
+    // Used internally to differentiate between ABNAMRO and TINK
     private String certificateId;
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public String getApiKey() {
-        return apiKey;
-    }
-
-    public String getCertificateId() {
-        return certificateId;
-    }
 }
