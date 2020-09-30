7a19938c8daffe501c1d5498d11b235c70fd731f
Schahin Rajab <schahin.rajab@tink.se>
2020-09-30 16:31:06 +0200

    chore(nl/volksbank): add regex validation to secrets
    TC-2640

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/configuration/VolksbankConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/configuration/VolksbankConfiguration.java
@@ -1,6 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaExamples;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaInject;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaString;
 import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
@@ -12,6 +15,17 @@ import se.tink.backend.aggregation.configuration.agents.ClientSecretsConfigurati
 @JsonObject
 @Getter
 public class VolksbankConfiguration implements ClientConfiguration {
-    @JsonProperty @Secret @ClientIdConfiguration private String clientId;
-    @JsonProperty @SensitiveSecret @ClientSecretsConfiguration private String clientSecret;
+    @JsonProperty
+    @Secret
+    @ClientIdConfiguration
+    @JsonSchemaInject(strings = {@JsonSchemaString(path = "pattern", value = "[0-9a-f]+$")})
+    @JsonSchemaExamples("l555d5513cb123456789050b90f06a18b2")
+    private String clientId;
+
+    @JsonProperty
+    @SensitiveSecret
+    @ClientSecretsConfiguration
+    @JsonSchemaInject(strings = {@JsonSchemaString(path = "pattern", value = "^[0-9a-f]{32}$")})
+    @JsonSchemaExamples("555d5513cb123456789050b90f06a18b")
+    private String clientSecret;
 }
