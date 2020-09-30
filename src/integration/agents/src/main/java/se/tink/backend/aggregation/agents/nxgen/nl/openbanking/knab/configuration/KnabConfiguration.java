9eee6bd02619a46a751dedb274856f99776d4e8a
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-09-30 10:41:35 +0200

    fix(knab): add pattern and examples for configuration
    TC-2640

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/knab/configuration/KnabConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/knab/configuration/KnabConfiguration.java
@@ -1,6 +1,10 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.openbanking.knab.configuration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaExamples;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaInject;
+import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaString;
+import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.annotations.SensitiveSecret;
@@ -9,17 +13,21 @@ import se.tink.backend.aggregation.configuration.agents.ClientIdConfiguration;
 import se.tink.backend.aggregation.configuration.agents.ClientSecretsConfiguration;
 
 @JsonObject
+@Getter
 public class KnabConfiguration implements ClientConfiguration {
+    @JsonProperty
+    @Secret
+    @ClientIdConfiguration
+    @JsonSchemaExamples("0123456789abcdefghij")
+    @JsonSchemaInject(
+            strings = {@JsonSchemaString(path = "pattern", value = "^[-A-Za-z0-9]{10,30}$")})
+    private String clientId;
 
-    @JsonProperty @Secret @ClientIdConfiguration private String clientId;
-
-    @JsonProperty @SensitiveSecret @ClientSecretsConfiguration private String clientSecret;
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
-    }
+    @JsonProperty
+    @SensitiveSecret
+    @ClientSecretsConfiguration
+    @JsonSchemaExamples("4578616d706c652c2074657374696e67206d6f72")
+    @JsonSchemaInject(
+            strings = {@JsonSchemaString(path = "pattern", value = "^[-A-Za-z0-9]{20,60}$")})
+    private String clientSecret;
 }
