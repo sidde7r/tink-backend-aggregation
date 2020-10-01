c329eaaffc6e7db89758c420eb6fac7f1d18f4b6
Damian Baciur <damian.baciur@tink.se>
2020-10-01 16:08:19 +0200

    fix(dk-nordea-nemid): ITE-1484 fix loan details response parsing and NPE

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/nxbuilders/modules/id/builder/AccountNameStep.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/core/account/nxbuilders/modules/id/builder/AccountNameStep.java
@@ -5,7 +5,7 @@ import javax.annotation.Nonnull;
 public interface AccountNameStep<T> {
     /**
      * Sets the name of the account to the given value. Alias should be recognizable for the account
-     * holder. If the supplied value is null, the alias will be set to the account number.
+     * holder.
      *
      * @param name The name/alias of the account.
      * @return The next step of the builder.
