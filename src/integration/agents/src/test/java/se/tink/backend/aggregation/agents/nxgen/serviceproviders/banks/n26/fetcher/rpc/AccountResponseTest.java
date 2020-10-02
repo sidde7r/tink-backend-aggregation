6ce2208192de9774c728dd418c2b2ccf95ae7b43
Jacek Garbulinski <jacek.garbulinski@tink.se>
2020-10-02 14:46:48 +0200

    test(n26): use resources instead of json as string in test class
    see: #ITE-1483

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/n26/fetcher/rpc/AccountResponseTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/n26/fetcher/rpc/AccountResponseTest.java
@@ -4,32 +4,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import org.junit.Test;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.n26.fetcher.transactionalaccount.N26AccountFetcherTestData;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.CheckingAccount;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 import se.tink.libraries.account.AccountIdentifier;
 import se.tink.libraries.account.identifiers.IbanIdentifier;
 import se.tink.libraries.amount.ExactCurrencyAmount;
-import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class AccountResponseTest {
 
     @Test
     public void toTransactionalAccount() {
         // given
-        AccountResponse accountResponse =
-                SerializationUtils.deserializeFromString(
-                        "{"
-                                + "\"id\": \"sample_id\", "
-                                + "\"availableBalance\": 123.45, "
-                                + "\"usableBalance\": 234.56, "
-                                + "\"bankBalance\": 345.67, "
-                                + "\"iban\": \"DE95100110016601026293\", "
-                                + "\"bic\": \"sample bic\", "
-                                + "\"bankName\": \"sample bank name\", "
-                                + "\"seized\": true, "
-                                + "\"currency\": \"EUR\""
-                                + "}",
-                        AccountResponse.class);
+        AccountResponse accountResponse = N26AccountFetcherTestData.fetchAccountsResponse();
 
         // when
         TransactionalAccount result = accountResponse.toTransactionalAccount();
@@ -37,8 +24,8 @@ public class AccountResponseTest {
         // then
         assertThat(result).isInstanceOf(CheckingAccount.class);
         assertThat(result.getAccountNumber()).isEqualTo("DE95100110016601026293");
-        assertThat(result.getName()).isEqualTo("sample bank name");
-        assertThat(result.getExactBalance().compareTo(ExactCurrencyAmount.inEUR(123.45)))
+        assertThat(result.getName()).isEqualTo("N26 Bank");
+        assertThat(result.getExactBalance().compareTo(ExactCurrencyAmount.inEUR(100.00)))
                 .isEqualTo(0);
         // and identifiers
         List<AccountIdentifier> accountIdentifiers = result.getIdentifiers();
