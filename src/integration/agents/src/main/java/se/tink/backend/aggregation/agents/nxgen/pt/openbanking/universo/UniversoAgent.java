eea3617b0da8cff14edb17dc741c1abfd4ca8c64
Lukasz Binkiewicz <lukasz.binkiewicz@tink.se>
2020-10-05 21:00:22 +0200

    fix(PT/Universo): fixing ssl handshake error
    When trying to run agent we get ssl handshake error MINI-196

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/universo/UniversoAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/universo/UniversoAgent.java
@@ -4,49 +4,46 @@ import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capa
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.SAVINGS_ACCOUNTS;
 
 import com.google.inject.Inject;
-import java.security.cert.CertificateException;
+import lombok.SneakyThrows;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.Xs2aDevelopersApiClient;
+import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModules;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.Xs2aDevelopersTransactionalAgent;
 import se.tink.backend.aggregation.configuration.agents.AgentConfiguration;
 import se.tink.backend.aggregation.configuration.agents.utils.CertificateUtils;
+import se.tink.backend.aggregation.eidassigner.QsealcSigner;
+import se.tink.backend.aggregation.eidassigner.module.QSealcSignerModuleRSASHA256;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 
+@AgentDependencyModules(modules = QSealcSignerModuleRSASHA256.class)
 @AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS})
 public class UniversoAgent extends Xs2aDevelopersTransactionalAgent {
 
     @Inject
-    public UniversoAgent(AgentComponentProvider componentProvider) {
-        super(componentProvider, "https://api.psd2.universo.pt");
+    public UniversoAgent(AgentComponentProvider componentProvider, QsealcSigner qsealcSigner) {
+        super(componentProvider, "https://api.sandbox.psd2.universo.pt");
+        client.addFilter(
+                new UniversoSigningFilter(
+                        (UniversoProviderConfiguration) configuration, qsealcSigner));
     }
 
     @Override
+    @SneakyThrows
     protected UniversoProviderConfiguration getConfiguration(String baseUrl) {
         AgentConfiguration<UniversoConfiguration> agentConfiguration =
                 getAgentConfigurationController()
                         .getAgentConfiguration(UniversoConfiguration.class);
-        String organizationIdentifier;
-        try {
-            organizationIdentifier =
-                    CertificateUtils.getOrganizationIdentifier(agentConfiguration.getQwac());
-        } catch (CertificateException e) {
-            throw new IllegalStateException("Could not extract organization identifier!", e);
-        }
+        String organizationIdentifier =
+                CertificateUtils.getOrganizationIdentifier(agentConfiguration.getQwac());
         String redirectUrl = agentConfiguration.getRedirectUrl();
         UniversoConfiguration universoConfiguration =
                 agentConfiguration.getProviderSpecificConfiguration();
-        return new UniversoProviderConfiguration(
-                organizationIdentifier, baseUrl, redirectUrl, universoConfiguration.getApiKey());
-    }
 
-    @Override
-    protected Xs2aDevelopersApiClient constructApiClient(AgentComponentProvider componentProvider) {
-        return new UniversoApiClient(
-                componentProvider.getTinkHttpClient(),
-                persistentStorage,
-                (UniversoProviderConfiguration) configuration,
-                request.isManual(),
-                userIp,
-                componentProvider.getRandomValueGenerator());
+        return new UniversoProviderConfiguration(
+                organizationIdentifier,
+                baseUrl,
+                redirectUrl,
+                universoConfiguration.getApiKey(),
+                universoConfiguration.getKeyId(),
+                agentConfiguration.getQsealc());
     }
 }
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/universo/UniversoSigningFilter.java
@@ -0,0 +1,69 @@
+package se.tink.backend.aggregation.agents.nxgen.pt.openbanking.universo;
+
+import static se.tink.backend.aggregation.agents.nxgen.pt.openbanking.universo.UniversoConstants.HeaderKeys.DIGEST;
+import static se.tink.backend.aggregation.agents.nxgen.pt.openbanking.universo.UniversoConstants.HeaderKeys.SIGNATURE;
+import static se.tink.backend.aggregation.agents.nxgen.pt.openbanking.universo.UniversoConstants.HeaderKeys.TPP_CERTIFICATE;
+import static se.tink.backend.aggregation.agents.nxgen.pt.openbanking.universo.UniversoConstants.HeaderValues.SIGNATURE_HEADER;
+
+import java.util.Base64;
+import java.util.Optional;
+import java.util.UUID;
+import javax.ws.rs.core.MultivaluedMap;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import se.tink.backend.aggregation.agents.nxgen.pt.openbanking.universo.UniversoConstants.HeaderValues;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.Xs2aDevelopersConstants.HeaderKeys;
+import se.tink.backend.aggregation.agents.utils.crypto.hash.Hash;
+import se.tink.backend.aggregation.configuration.agents.utils.CertificateUtils;
+import se.tink.backend.aggregation.eidassigner.QsealcSigner;
+import se.tink.backend.aggregation.nxgen.http.exceptions.client.HttpClientException;
+import se.tink.backend.aggregation.nxgen.http.filter.engine.FilterOrder;
+import se.tink.backend.aggregation.nxgen.http.filter.engine.FilterPhases;
+import se.tink.backend.aggregation.nxgen.http.filter.filters.iface.Filter;
+import se.tink.backend.aggregation.nxgen.http.request.HttpRequest;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
+
+@FilterOrder(category = FilterPhases.REQUEST_HANDLE, order = Integer.MIN_VALUE)
+@RequiredArgsConstructor
+public class UniversoSigningFilter extends Filter {
+
+    private final UniversoProviderConfiguration configuration;
+    private final QsealcSigner signer;
+
+    @Override
+    public HttpResponse handle(HttpRequest httpRequest)
+            throws HttpClientException, HttpResponseException {
+        MultivaluedMap<String, Object> requestHeaders = httpRequest.getHeaders();
+        String requestId = ((UUID) requestHeaders.getFirst(HeaderKeys.X_REQUEST_ID)).toString();
+        String body = Optional.ofNullable(httpRequest.getBody().toString()).orElse("");
+        String digest = createDigest(body);
+
+        requestHeaders.add(TPP_CERTIFICATE, getFormattedCert());
+        requestHeaders.add(DIGEST, digest);
+        requestHeaders.add(UniversoConstants.HeaderKeys.API_KEY, configuration.getApiKey());
+        requestHeaders.add(SIGNATURE, generateSignature(digest, requestId));
+        return nextFilter(httpRequest);
+    }
+
+    private String createDigest(String body) {
+        return String.format(
+                HeaderValues.SHA_256, Base64.getEncoder().encodeToString(Hash.sha256(body)));
+    }
+
+    private String generateSignature(String digest, String requestId) {
+        String formattedMessage =
+                String.format(HeaderValues.QSEAL_HEADERS_SIGNATURE, requestId, digest);
+        String signature = signer.getSignatureBase64(formattedMessage.getBytes());
+        return String.format(SIGNATURE_HEADER, configuration.getKeyId(), signature);
+    }
+
+    @SneakyThrows
+    private String getFormattedCert() {
+        String certificate =
+                CertificateUtils.getDerEncodedCertFromBase64EncodedCertificate(
+                        configuration.getQseal());
+        String certificateFormat = "-----BEGIN CERTIFICATE-----\n%s\n-----END CERTIFICATE-----";
+        return String.format(certificateFormat, certificate);
+    }
+}
