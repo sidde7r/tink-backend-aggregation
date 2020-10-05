30f8d6cebf78c5a11b02b1df6b666a5426f93ab3
tymon zietek <tymon.zietek@tink.se>
2020-10-05 18:01:16 +0200

    test(bancoposta): adds saving accounts fetching tests

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/it/bancoposta/fetcher/FetcherTestData.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/it/bancoposta/fetcher/FetcherTestData.java
@@ -3,6 +3,9 @@ package se.tink.backend.aggregation.agents.nxgen.it.bancoposta.fetcher;
 import org.junit.Ignore;
 import se.tink.backend.aggregation.agents.nxgen.it.banks.bancoposta.fetcher.rpc.AccountDetailsResponse;
 import se.tink.backend.aggregation.agents.nxgen.it.banks.bancoposta.fetcher.rpc.AccountsResponse;
+import se.tink.backend.aggregation.agents.nxgen.it.banks.bancoposta.fetcher.rpc.SavingAccountDetailsResponse;
+import se.tink.backend.aggregation.agents.nxgen.it.banks.bancoposta.fetcher.rpc.SavingAccountResponse;
+import se.tink.backend.aggregation.agents.nxgen.it.banks.bancoposta.fetcher.rpc.SavingTransactionResponse;
 import se.tink.backend.aggregation.agents.nxgen.it.banks.bancoposta.fetcher.rpc.TransactionsResponse;
 import se.tink.libraries.serialization.utils.SerializationUtils;
 
@@ -270,5 +273,120 @@ public class FetcherTestData {
                         + "  }\n"
                         + "}",
                 TransactionsResponse.class);
+    }
+
+    public static SavingAccountResponse getSavingAccountsResponse() {
+        return SerializationUtils.deserializeFromString(
+                "{\n"
+                        + "    \"header\": {\n"
+                        + "        \"command-result\": \"0\",\n"
+                        + "        \"clientId\": \"f358a2ee-eb26-4726-8282-16aa65df8043\",\n"
+                        + "        \"command-result-description\": \"\",\n"
+                        + "        \"requestId\": \"\",\n"
+                        + "        \"requestid\": \"7681efa9-bbcd-40b7-8be6-730055eca35f:node-2\",\n"
+                        + "        \"command-result-details\": \"\",\n"
+                        + "        \"command-request-version\": \"v2\",\n"
+                        + "        \"command-request-service\": \"bancoposta\",\n"
+                        + "        \"command-request-command\": \"listaLibrettiH24\",\n"
+                        + "        \"command-result-reason\": \"\",\n"
+                        + "        \"status\": \"COMPLETED\"\n"
+                        + "    },\n"
+                        + "    \"body\": {\n"
+                        + "        \"statoSemaforoBfp\": \"standard\",\n"
+                        + "        \"listaLibretti\": [\n"
+                        + "            {\n"
+                        + "                \"onBoardable\": true,\n"
+                        + "                \"presenzaBfp\": false,\n"
+                        + "                \"presenzaPvr\": false,\n"
+                        + "                \"monointestato\": true,\n"
+                        + "                \"iban\": \"IT10U0760103384000050444884\",\n"
+                        + "                \"numeroRapporto\": \"000050444884\",\n"
+                        + "                \"librettoSmart\": true\n"
+                        + "            }\n"
+                        + "        ],\n"
+                        + "        \"statoSemaforoDr\": \"standard\"\n"
+                        + "    }\n"
+                        + "}",
+                SavingAccountResponse.class);
+    }
+
+    public static SavingAccountDetailsResponse getSavingAccountsDetailsResponse() {
+        return SerializationUtils.deserializeFromString(
+                "{\n"
+                        + "    \"header\": {\n"
+                        + "        \"command-result\": \"0\",\n"
+                        + "        \"clientId\": \"3786256d-d177-40c1-b0ad-5395ffde614f\",\n"
+                        + "        \"command-result-description\": \"\",\n"
+                        + "        \"requestId\": \"\",\n"
+                        + "        \"requestid\": \"f0be1897-2cb7-46b4-97f1-ba6f11537d3c:node-2\",\n"
+                        + "        \"command-result-details\": \"\",\n"
+                        + "        \"command-request-version\": \"v2\",\n"
+                        + "        \"command-request-service\": \"bancoposta\",\n"
+                        + "        \"command-request-command\": \"listaMovimentiLibrettoH24\",\n"
+                        + "        \"command-result-reason\": \"\",\n"
+                        + "        \"status\": \"COMPLETED\"\n"
+                        + "    },\n"
+                        + "    \"body\": {\n"
+                        + "        \"listaMovimentoRisparmioPostale\": [\n"
+                        + "            {\n"
+                        + "                \"segno\": \"A\",\n"
+                        + "                \"descrizione\": \"ACCREDITO GIRO CONTO ALTRO INTERMEDIARIO\",\n"
+                        + "                \"divisa\": \"EUR\",\n"
+                        + "                \"dataValuta\": \"04/02/2020\",\n"
+                        + "                \"dataContabile\": \"04/02/2020\",\n"
+                        + "                \"descrizioneEstesa\": \"ACCREDITO GIRO CONTO ALTRO INT\",\n"
+                        + "                \"importo\": 500,\n"
+                        + "                \"causale\": \"BO79\"\n"
+                        + "            }\n"
+                        + "        ],\n"
+                        + "        \"statoSemaforoBfp\": \"standard\",\n"
+                        + "        \"saldo\": {\n"
+                        + "            \"saldoDisponibile\": 500,\n"
+                        + "            \"saldoContabile\": 500\n"
+                        + "        },\n"
+                        + "        \"statoSemaforoDr\": \"standard\"\n"
+                        + "    }\n"
+                        + "}",
+                SavingAccountDetailsResponse.class);
+    }
+
+    public static SavingTransactionResponse getSavingTransactionsResponse() {
+        return SerializationUtils.deserializeFromString(
+                "{\n"
+                        + "    \"header\": {\n"
+                        + "        \"command-result\": \"0\",\n"
+                        + "        \"clientId\": \"3786256d-d177-40c1-b0ad-5395ffde614f\",\n"
+                        + "        \"command-result-description\": \"\",\n"
+                        + "        \"requestId\": \"\",\n"
+                        + "        \"requestid\": \"f0be1897-2cb7-46b4-97f1-ba6f11537d3c:node-2\",\n"
+                        + "        \"command-result-details\": \"\",\n"
+                        + "        \"command-request-version\": \"v2\",\n"
+                        + "        \"command-request-service\": \"bancoposta\",\n"
+                        + "        \"command-request-command\": \"listaMovimentiLibrettoH24\",\n"
+                        + "        \"command-result-reason\": \"\",\n"
+                        + "        \"status\": \"COMPLETED\"\n"
+                        + "    },\n"
+                        + "    \"body\": {\n"
+                        + "        \"listaMovimentoRisparmioPostale\": [\n"
+                        + "            {\n"
+                        + "                \"segno\": \"A\",\n"
+                        + "                \"descrizione\": \"ACCREDITO GIRO CONTO ALTRO INTERMEDIARIO\",\n"
+                        + "                \"divisa\": \"EUR\",\n"
+                        + "                \"dataValuta\": \"04/02/2020\",\n"
+                        + "                \"dataContabile\": \"04/02/2020\",\n"
+                        + "                \"descrizioneEstesa\": \"ACCREDITO GIRO CONTO ALTRO INT\",\n"
+                        + "                \"importo\": 500,\n"
+                        + "                \"causale\": \"BO79\"\n"
+                        + "            }\n"
+                        + "        ],\n"
+                        + "        \"statoSemaforoBfp\": \"standard\",\n"
+                        + "        \"saldo\": {\n"
+                        + "            \"saldoDisponibile\": 500,\n"
+                        + "            \"saldoContabile\": 500\n"
+                        + "        },\n"
+                        + "        \"statoSemaforoDr\": \"standard\"\n"
+                        + "    }\n"
+                        + "}",
+                SavingTransactionResponse.class);
     }
 }
