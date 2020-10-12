1a30bfe88a2769e8af180f567f64fbb8142fd46e
Annica Alstermark <annica.alstermark@tink.se>
2020-10-12 12:04:06 +0200

    fix(Volksbank): Validate consent presence

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/authenticator/ConsentFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/authenticator/ConsentFetcher.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.authenticator;
 
+import com.google.common.base.Strings;
 import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.VolksbankApiClient;
 import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.VolksbankConstants;
 import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.VolksbankConstants.Storage;
@@ -39,6 +40,14 @@ public final class ConsentFetcher {
 
     public boolean isConsentValid() {
         final String consentId = persistentStorage.get(Storage.CONSENT);
+
+        // Consent is removed if we get "unsupported_grant_type" from Volksbank. The error we throw
+        // is a bankservice error which puts the credential in temp error, this means that we try
+        // to refresh it again. This is a band aid solution, we need to the overall logic for
+        // consent errors (TC-3706).
+        if (Strings.isNullOrEmpty(consentId)) {
+            return false;
+        }
 
         final String consentStatus =
                 client.consentStatusRequest(volksbankConfiguration.getClientId(), consentId)
