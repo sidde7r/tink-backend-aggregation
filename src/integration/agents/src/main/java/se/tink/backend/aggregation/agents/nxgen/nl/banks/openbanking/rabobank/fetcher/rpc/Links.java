2cfee79abcbf9112354e566350ad98b6360c27c7
Farrokh Bolandi <farrokh.bolandi@tink.se>
2020-10-12 12:24:57 +0200

    chore(nl/rabobank): removed unused fields TC-3627: In v.4 we get href entities while in v.3 it's strings. We don't use these fields anyways

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/rabobank/fetcher/rpc/Links.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/rabobank/fetcher/rpc/Links.java
@@ -6,26 +6,8 @@ import se.tink.backend.aggregation.annotations.JsonObject;
 @JsonObject
 public class Links {
 
-    @JsonProperty("balances")
-    private String balances;
-
-    @JsonProperty("transactions")
-    private String transactions;
-
-    @JsonProperty("account")
-    private String account;
-
-    @JsonProperty("first")
-    private String first;
-
     @JsonProperty("last")
     private String last;
-
-    @JsonProperty("next")
-    private String next;
-
-    @JsonProperty("previous")
-    private String previous;
 
     public String getLast() {
         return last;
