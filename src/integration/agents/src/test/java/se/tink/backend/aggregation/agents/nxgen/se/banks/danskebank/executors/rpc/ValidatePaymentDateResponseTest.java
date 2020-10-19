84c7a376e3fba17ee0a5e5a11696097afb46808d
Shankey Jain <shankey.jain@tink.se>
2020-10-19 13:47:11 +0200

    test(Danske/RE): Unit test for different timezones

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/danskebank/executors/rpc/ValidatePaymentDateResponseTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/danskebank/executors/rpc/ValidatePaymentDateResponseTest.java
@@ -2,7 +2,9 @@ package se.tink.backend.aggregation.agents.nxgen.se.banks.danskebank.executors.r
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 import org.junit.Assert;
 import org.junit.Test;
 import se.tink.libraries.serialization.utils.SerializationUtils;
@@ -41,6 +43,23 @@ public class ValidatePaymentDateResponseTest {
         Date transferDate = new SimpleDateFormat(DATE_TIME_FORMAT).parse(DIFFERENT_TRANSFER_DATE);
 
         Assert.assertFalse(fetchPaymentResponse.isTransferDateSameAsBookingDate(transferDate));
+    }
+
+    @Test
+    public void testForDifferentDateInDifferentTimeZone() {
+        ValidatePaymentDateResponse fetchPaymentResponse =
+                SerializationUtils.deserializeFromString(
+                        DATE_RESPONSE, ValidatePaymentDateResponse.class);
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("CET"));
+        cal.set(Calendar.DAY_OF_MONTH, 28);
+        cal.set(Calendar.MONTH, 9);
+        cal.set(Calendar.YEAR, 2020);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+
+        Assert.assertTrue(fetchPaymentResponse.isTransferDateSameAsBookingDate(cal.getTime()));
     }
 
     @Test
