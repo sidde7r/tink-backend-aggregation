6960e2978f64a633522be75673786f28dcdc45d6
Jakub Barejka <jakub.barejka@tink.se>
2020-10-20 12:24:03 +0200

    ci(sonar): cache sonar scanner

--- .buildkite/run-sonar.sh
+++ .buildkite/run-sonar.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-export SONAR_SCANNER_VERSION=4.2.0.1873
-export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
-rm -rf $SONAR_SCANNER_HOME
-mkdir -p $SONAR_SCANNER_HOME
+export SONAR_SCANNER_VERSION=4.5.0.2216
+export SONAR_SCANNER_HOME=/cache/sonar-scanner-$SONAR_SCANNER_VERSION-linux
 
-curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+if [ ! -d "$SONAR_SCANNER_HOME" ]; then
+  curl -sSLo /cache/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
+  SONAR_SCANNER_SHA256SUM="ae6bc36f997604657c8fde1486eda4257c280c01c0dd59dc01a86a1af2d8dc8d"
+  echo "${SONAR_SCANNER_SHA256SUM} /cache/sonar-scanner.zip" | sha256sum -c
+  unzip /cache/sonar-scanner.zip -d /cache/
+  rm /cache/sonar-scanner.zip
+fi
 
-SONAR_SCANNER_SHA256SUM="44a5d985fc3bc10a8d4217160d2117289b7fe582acd410652b4bf59924593ce6"
-echo "${SONAR_SCANNER_SHA256SUM} $HOME/.sonar/sonar-scanner.zip" | sha256sum -c
-
-unzip $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
-rm $HOME/.sonar/sonar-scanner.zip
 export PATH=$SONAR_SCANNER_HOME/bin:$PATH
 export SONAR_SCANNER_OPTS="-server"
 
