7776d028bc6664c59640c9761f576438b689acbd
Mateusz Konstanciak <mateusz.konstanciak@tink.se>
2020-10-22 13:55:13 +0200

    fix(FI/Aktia): Add proxy to Aktia RE Agent
    Remove aggregator header
    MINI-91

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/aktia/AktiaAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/aktia/AktiaAgent.java
@@ -4,6 +4,7 @@ import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capa
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.IDENTITY_DATA;
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.SAVINGS_ACCOUNTS;
 
+import com.google.inject.Inject;
 import java.util.NoSuchElementException;
 import se.tink.backend.aggregation.agents.FetchAccountsResponse;
 import se.tink.backend.aggregation.agents.FetchIdentityDataResponse;
@@ -12,7 +13,6 @@ import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
 import se.tink.backend.aggregation.agents.RefreshIdentityDataExecutor;
 import se.tink.backend.aggregation.agents.RefreshSavingsAccountsExecutor;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
-import se.tink.backend.aggregation.agents.contexts.agent.AgentContext;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.aktia.AktiaConstants.InstanceStorage;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.aktia.authenticator.AktiaAutoAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.aktia.authenticator.AktiaEncapConfiguration;
@@ -21,8 +21,10 @@ import se.tink.backend.aggregation.agents.nxgen.fi.banks.aktia.authenticator.Akt
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.aktia.authenticator.entities.UserAccountInfo;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.aktia.fetcher.transactionalaccount.AktiaTransactionalAccountFetcher;
 import se.tink.backend.aggregation.agents.utils.authentication.encap3.EncapClient;
+import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
 import se.tink.backend.aggregation.configuration.signaturekeypair.SignatureKeyPair;
 import se.tink.backend.aggregation.nxgen.agents.NextGenerationAgent;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.keycardandsmsotp.KeyCardAndSmsOtpAuthenticationController;
@@ -30,9 +32,9 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.Transac
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginationController;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
+import se.tink.backend.aggregation.nxgen.http.MultiIpGateway;
 import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.storage.Storage;
-import se.tink.libraries.credentials.service.CredentialsRequest;
 import se.tink.libraries.identitydata.IdentityData;
 
 @AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS, IDENTITY_DATA})
@@ -46,17 +48,20 @@ public final class AktiaAgent extends NextGenerationAgent
 
     private final TransactionalAccountRefreshController transactionalAccountRefreshController;
 
+    @Inject
     public AktiaAgent(
-            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
-        super(request, context, signatureKeyPair);
-        configureHttpClient(client);
+            final AgentComponentProvider componentProvider,
+            AgentsServiceConfiguration agentsServiceConfiguration,
+            SignatureKeyPair signatureKeyPair) {
+        super(componentProvider);
+        configureHttpClient(client, agentsServiceConfiguration);
 
         this.apiClient = new AktiaApiClient(client);
         this.instanceStorage = new Storage();
 
         this.encapClient =
                 new EncapClient(
-                        context,
+                        componentProvider.getContext(),
                         request,
                         signatureKeyPair,
                         persistentStorage,
@@ -67,8 +72,13 @@ public final class AktiaAgent extends NextGenerationAgent
                 constructTransactionalAccountRefreshController();
     }
 
-    protected void configureHttpClient(TinkHttpClient client) {
+    private void configureHttpClient(
+            TinkHttpClient client, AgentsServiceConfiguration agentsServiceConfiguration) {
         client.setUserAgent(AktiaConstants.HttpHeaders.USER_AGENT);
+        client.disableAggregatorHeader();
+        final MultiIpGateway gateway =
+                new MultiIpGateway(client, credentials.getUserId(), credentials.getId());
+        gateway.setMultiIpGateway(agentsServiceConfiguration.getIntegrations());
     }
 
     @Override
