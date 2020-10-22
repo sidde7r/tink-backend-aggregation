39c5d5c671b8480c5971d79be77cae5ebd94c8d5
Jakub Badysiak <jakub.badysiak@tink.se>
2020-10-22 07:43:37 +0200

    feat(uk/danskebank/business): Added DanskeBank Business agent
    IFD-1547

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/danskebank/DanskeBankV31BusinessAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/danskebank/DanskeBankV31BusinessAgent.java
@@ -1,13 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.danskebank;
 
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CHECKING_ACCOUNTS;
-import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CREDIT_CARDS;
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.IDENTITY_DATA;
-import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.SAVINGS_ACCOUNTS;
-import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.TRANSFERS;
 
 import com.google.inject.Inject;
-import java.util.Optional;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForDecoupledMode;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForProductionMode;
@@ -15,54 +11,49 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.uko
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingAis;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingAisConfig;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingConstants.PartyEndpoint;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingPisConfig;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module.LocalKeySignerModule;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module.UkOpenBankingLocalKeySignerModuleForDecoupledMode;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module.UkOpenBankingModule;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UKOpenBankingAis;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UkOpenBankingV31Ais;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UkOpenBankingV31PisConfiguration;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.mapper.creditcards.CreditCardAccountMapper;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.mapper.creditcards.DefaultCreditCardBalanceMapper;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.pis.UKOpenbankingV31Executor;
 import se.tink.backend.aggregation.agents.nxgen.uk.openbanking.danskebank.DanskeBankConstants.Urls.V31;
 import se.tink.backend.aggregation.agents.nxgen.uk.openbanking.danskebank.authenticator.DanskeBankAuthenticator;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
-import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date.LocalDateTimeSource;
-import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.randomness.RandomValueGenerator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.jwt.signer.iface.JwtSigner;
-import se.tink.backend.aggregation.nxgen.controllers.payment.PaymentController;
 import se.tink.libraries.mapper.PrioritizedValueExtractor;
 
 @AgentDependencyModulesForProductionMode(
         modules = {UkOpenBankingModule.class, LocalKeySignerModule.class})
 @AgentDependencyModulesForDecoupledMode(
         modules = UkOpenBankingLocalKeySignerModuleForDecoupledMode.class)
-@AgentCapabilities({CHECKING_ACCOUNTS, CREDIT_CARDS, SAVINGS_ACCOUNTS, IDENTITY_DATA, TRANSFERS})
-public final class DanskeBankV31Agent extends UkOpenBankingBaseAgent {
+@AgentCapabilities({CHECKING_ACCOUNTS, IDENTITY_DATA})
+public class DanskeBankV31BusinessAgent extends UkOpenBankingBaseAgent {
 
     private static final UkOpenBankingAisConfig aisConfig;
-    private final UkOpenBankingPisConfig pisConfig;
-    private final LocalDateTimeSource localDateTimeSource;
-    private final RandomValueGenerator randomValueGenerator;
 
     static {
         aisConfig =
                 UKOpenBankingAis.builder()
                         .withOrganisationId("0015800000jf7AeAAI")
                         .withApiBaseURL(V31.AIS_API_URL)
-                        .withWellKnownURL(V31.WELL_KNOWN_URL)
+                        .withWellKnownURL(V31.BUSINESS_WELL_KNOWN_URL)
                         .withPartyEndpoints(PartyEndpoint.IDENTITY_DATA_ENDPOINT_PARTY)
                         .build();
     }
 
     @Inject
-    public DanskeBankV31Agent(AgentComponentProvider componentProvider, JwtSigner jwtSigner) {
+    public DanskeBankV31BusinessAgent(
+            AgentComponentProvider componentProvider, JwtSigner jwtSigner) {
         super(componentProvider, jwtSigner, aisConfig, false);
-        pisConfig = new UkOpenBankingV31PisConfiguration(V31.PIS_API_URL);
-        this.localDateTimeSource = componentProvider.getLocalDateTimeSource();
-        this.randomValueGenerator = componentProvider.getRandomValueGenerator();
+    }
+
+    @Override
+    protected Authenticator constructAuthenticator() {
+        DanskeBankAuthenticator authenticator = new DanskeBankAuthenticator(apiClient);
+        return createOpenIdFlowWithAuthenticator(authenticator, null);
     }
 
     @Override
@@ -78,26 +69,5 @@ public final class DanskeBankV31Agent extends UkOpenBankingBaseAgent {
                 localDateTimeSource,
                 creditCardAccountMapper,
                 UkOpenBankingV31Ais.defaultTransactionalAccountMapper());
-    }
-
-    @Override
-    protected Authenticator constructAuthenticator() {
-        DanskeBankAuthenticator authenticator = new DanskeBankAuthenticator(apiClient);
-        return createOpenIdFlowWithAuthenticator(authenticator, aisConfig.getAppToAppURL());
-    }
-
-    @Override
-    public Optional<PaymentController> constructPaymentController() {
-        UKOpenbankingV31Executor paymentExecutor =
-                new UKOpenbankingV31Executor(
-                        pisConfig,
-                        softwareStatement,
-                        providerConfiguration,
-                        apiClient,
-                        supplementalInformationHelper,
-                        credentials,
-                        strongAuthenticationState,
-                        randomValueGenerator);
-        return Optional.of(new PaymentController(paymentExecutor, paymentExecutor));
     }
 }
