7d346eb31b1941a97dcde7d11749abdef84fa7ef
Nazri Abdullah <nazri.abdullah@tink.se>
2020-10-25 10:17:35 +0800

    refactor(nl-rabobank): removed consent status check in fetchers

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/rabobank/fetcher/transactional/TransactionalAccountFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/rabobank/fetcher/transactional/TransactionalAccountFetcher.java
@@ -23,8 +23,6 @@ public class TransactionalAccountFetcher implements AccountFetcher<Transactional
     @Override
     public Collection<TransactionalAccount> fetchAccounts() {
 
-        apiClient.checkConsentStatus();
-
         return Optional.ofNullable(apiClient.fetchAccounts())
                 .map(TransactionalAccountsResponse::getAccounts).orElseGet(Collections::emptyList)
                 .stream()
