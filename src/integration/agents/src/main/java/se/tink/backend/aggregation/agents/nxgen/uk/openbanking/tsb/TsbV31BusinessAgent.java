855fc380750f27443512507f59ce14dc11e05864
Malgorzata Kosecka <malgorzata.kosecka@tink.se>
2020-10-26 16:33:46 +0100

    feat(uk/tsb): Add new business agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/tsb/TsbV31BusinessAgent.java
@@ -0,0 +1,59 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.tsb;
+
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CHECKING_ACCOUNTS;
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CREDIT_CARDS;
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.SAVINGS_ACCOUNTS;
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.TRANSFERS;
+
+import com.google.inject.Inject;
+import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
+import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForDecoupledMode;
+import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForProductionMode;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.UkOpenBankingBaseAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.entities.AccountOwnershipType;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingAis;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.interfaces.UkOpenBankingAisConfig;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module.LocalKeySignerModule;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module.UkOpenBankingLocalKeySignerModuleForDecoupledMode;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.base.module.UkOpenBankingModule;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UKOpenBankingAis;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.v31.UkOpenBankingV31Ais;
+import se.tink.backend.aggregation.agents.nxgen.uk.openbanking.tsb.TsbConstants.Urls.V31;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.openid.jwt.signer.iface.JwtSigner;
+
+@AgentDependencyModulesForProductionMode(
+        modules = {UkOpenBankingModule.class, LocalKeySignerModule.class})
+@AgentDependencyModulesForDecoupledMode(
+        modules = UkOpenBankingLocalKeySignerModuleForDecoupledMode.class)
+@AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS, CREDIT_CARDS, TRANSFERS})
+public final class TsbV31BusinessAgent extends UkOpenBankingBaseAgent {
+
+    private static final UkOpenBankingAisConfig aisConfig;
+
+    static {
+        aisConfig =
+                UKOpenBankingAis.builder()
+                        .withOrganisationId("0015800001ZEZ3hAAH")
+                        .withApiBaseURL(V31.AIS_API_URL)
+                        .withWellKnownURL(V31.WELL_KNOWN_URL)
+                        .withAllowedAccountOwnershipType(AccountOwnershipType.BUSINESS)
+                        .build();
+    }
+
+    @Inject
+    public TsbV31BusinessAgent(AgentComponentProvider componentProvider, JwtSigner jwtSigner) {
+        super(componentProvider, jwtSigner, aisConfig, false);
+    }
+
+    @Override
+    protected UkOpenBankingAis makeAis() {
+        return new UkOpenBankingV31Ais(aisConfig, persistentStorage, localDateTimeSource);
+    }
+
+    @Override
+    protected Authenticator constructAuthenticator() {
+        return super.constructAuthenticator(aisConfig);
+    }
+}
