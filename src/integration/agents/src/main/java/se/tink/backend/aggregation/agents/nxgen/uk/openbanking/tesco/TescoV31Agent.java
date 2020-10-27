5ef8447eb3b49fcef091b4f7dc058b15669e9c69
Sai He <sai.he@tink.se>
2020-10-27 09:47:55 +0100

    chore(UK/Tesco): To use constants class for ORG_ID

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/tesco/TescoV31Agent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/tesco/TescoV31Agent.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.tesco;
 
+import static se.tink.backend.aggregation.agents.nxgen.uk.openbanking.tesco.TescoConstants.Urls.V31.ORG_ID;
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CHECKING_ACCOUNTS;
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CREDIT_CARDS;
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.SAVINGS_ACCOUNTS;
@@ -45,7 +46,7 @@ public final class TescoV31Agent extends UkOpenBankingBaseAgent {
     static {
         aisConfig =
                 UKOpenBankingAis.builder()
-                        .withOrganisationId("0015800001041RXAAY")
+                        .withOrganisationId(ORG_ID)
                         .withApiBaseURL(V31.AIS_API_URL)
                         .withWellKnownURL(V31.WELL_KNOWN_URL)
                         .withPartyEndpoints(PartyEndpoint.IDENTITY_DATA_ENDPOINT_ACCOUNT_ID_PARTY)
