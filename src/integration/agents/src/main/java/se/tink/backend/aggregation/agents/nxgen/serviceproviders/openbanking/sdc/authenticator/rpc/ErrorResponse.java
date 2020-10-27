a4295cb20880fa9d9ab75d1c2b7f4f4d1e8d351b
Jan Sameryt <jan.sameryt@tink.com>
2020-10-27 14:49:30 +0100

    fix(sdcOB): remove unnecessary snakecase
    ITE-1533

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sdc/authenticator/rpc/ErrorResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sdc/authenticator/rpc/ErrorResponse.java
@@ -1,11 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sdc.authenticator.rpc;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class ErrorResponse {
     private static final String INTERNAL_ERROR = "internal_error";
 
