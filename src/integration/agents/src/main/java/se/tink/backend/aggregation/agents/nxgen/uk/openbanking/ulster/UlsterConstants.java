b6a4db3dc9a900ba4fd565eb7b89950a17760021
Jakub Badysiak <jakub.badysiak@tink.se>
2020-10-29 14:27:04 +0100

    feat(uk/ulster/corporate): Added Ulster Corporate agent
    IFD-1552

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/ulster/UlsterConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/ulster/UlsterConstants.java
@@ -2,15 +2,17 @@ package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.ulster;
 
 final class UlsterConstants {
 
-    class Urls {
+    static class Urls {
 
-        class V31 {
+        static class V31 {
             public static final String AIS_API_URL =
                     "https://api.ulsterbank.co.uk/open-banking/v3.1/aisp";
             public static final String PIS_API_URL =
                     "https://api.ulsterbank.co.uk/open-banking/v3.1/pisp";
             public static final String WELL_KNOWN_URL =
                     "https://personal.secure1.ulsterbank.co.uk/.well-known/openid-configuration";
+            public static final String CORPORATE_WELL_KNOWN_URL =
+                    "https://corporate.secure1.ulsterbank.co.uk/.well-known/openid-configuration";
             public static final String ORG_ID = "0015800000jfxrpAAA";
         }
     }
