5f091c626e296bc090773dfc0d78f4448bcc5b58
Schahin Rajab <schahin.rajab@tink.se>
2020-11-02 10:19:21 +0100

    fix(nordea): add json annotation on response class
    Co-authored-by: Annica Alstermark <annica.alstermark@tink.se>

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/nordea/v30/authenticator/rpc/VerifyPersonalCodeResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/nordea/v30/authenticator/rpc/VerifyPersonalCodeResponse.java
@@ -1,7 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.v30.authenticator.rpc;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import se.tink.backend.aggregation.annotations.JsonObject;
 
+@JsonObject
 public class VerifyPersonalCodeResponse {
     @JsonProperty("authorization_code")
     private String authorizationCode;
