2968db66eaf333962a5edc7eed60cd3c65b7a82c
Niclas Albinsson <niclas.albinsson@tink.se>
2020-11-03 16:44:14 +0100

    chore(crosskey/spankii): fix credit card transaction descriptions

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/crosskey/fetcher/entities/transaction/BankTransactionCodeEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/crosskey/fetcher/entities/transaction/BankTransactionCodeEntity.java
@@ -2,9 +2,11 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cr
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
+@Getter
 @JsonNaming(PropertyNamingStrategy.UpperCamelCaseStrategy.class)
 public class BankTransactionCodeEntity {
 
