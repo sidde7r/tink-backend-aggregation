06d45edf780e83948ee7c0d09fee95f2b1c658ef
Kamil Woźniak <kamil.wozniak@tink.se>
2020-11-04 16:39:35 +0100

    test(uk/santander): PAY2_839 Added wiremock failed test case

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/santander/integration/SantanderAgentWiremockTestFixtures.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/santander/integration/SantanderAgentWiremockTestFixtures.java
@@ -17,17 +17,26 @@ public final class SantanderAgentWiremockTestFixtures {
     static final String AUTH_CODE = "DUMMY_AUTH_CODE";
     private static final String DESTINATION_IDENTIFIER = "77788812345678";
     private static final String CURRENCY = "GBP";
-    private static final LocalDate EXECUTION_DATE = LocalDate.now();
+    private static final LocalDate EXECUTION_DATE_TODAY = LocalDate.now();
+    private static final LocalDate EXECUTION_DATE_FAR_FUTURE = LocalDate.of(2120, 11, 2);
     private static final String UNIQUE_ID = "b900555d03124056b54930e1c53c9cac";
     private static final String REMITTANCE_INFO_VALUE = "UK Demo";
     private static final String AMOUNT = "1.00";
     private static final String CREDITOR_NAME = "Dummy creditor name";
 
     static Payment createDomesticPayment() {
+        return createDomesticPayment(EXECUTION_DATE_TODAY);
+    }
+
+    static Payment createFarFutureDomesticPayment() {
+        return createDomesticPayment(EXECUTION_DATE_FAR_FUTURE);
+    }
+
+    private static Payment createDomesticPayment(LocalDate localDate) {
         return new Payment.Builder()
                 .withCreditor(createCreditor())
                 .withExactCurrencyAmount(createExactCurrencyAmount())
-                .withExecutionDate(EXECUTION_DATE)
+                .withExecutionDate(localDate)
                 .withCurrency(CURRENCY)
                 .withRemittanceInformation(createUnstructuredRemittanceInformation())
                 .withUniqueId(UNIQUE_ID)
