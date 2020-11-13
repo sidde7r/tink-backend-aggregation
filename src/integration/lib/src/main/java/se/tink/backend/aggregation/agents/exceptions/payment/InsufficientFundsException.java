9d07f211641e7501512d4e6ab0bd584a44708edd
Shankey Jain <shankey.jain@tink.se>
2020-11-13 12:19:48 +0530

    feat(InsufficientFundsException): Adding default error message

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/exceptions/payment/InsufficientFundsException.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/exceptions/payment/InsufficientFundsException.java
@@ -3,6 +3,8 @@ package se.tink.backend.aggregation.agents.exceptions.payment;
 import se.tink.libraries.signableoperation.enums.InternalStatus;
 
 public class InsufficientFundsException extends DebtorValidationException {
+    public static final String DEFAULT_MESSAGE =
+            "Could not execute payment due to insufficient funds";
 
     public InsufficientFundsException(String message, String path, Throwable cause) {
         super(message, path, cause);
