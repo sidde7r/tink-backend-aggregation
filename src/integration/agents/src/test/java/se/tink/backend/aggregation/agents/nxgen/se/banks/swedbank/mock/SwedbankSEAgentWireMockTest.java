8d9b17961153a16456be10547690fc8ecae4ae76
Ashfaq Haider <ashfaq.haider@tink.se>
2020-11-13 15:04:54 +0100

    refactor(Wiremocks): use remittanceInformation in wiremock tests

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/mock/SwedbankSEAgentWireMockTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/mock/SwedbankSEAgentWireMockTest.java
@@ -17,6 +17,7 @@ import se.tink.libraries.account.AccountIdentifier;
 import se.tink.libraries.amount.Amount;
 import se.tink.libraries.enums.MarketCode;
 import se.tink.libraries.transfer.enums.TransferType;
+import se.tink.libraries.transfer.rpc.RemittanceInformation;
 import se.tink.libraries.transfer.rpc.Transfer;
 
 public class SwedbankSEAgentWireMockTest {
@@ -125,6 +126,8 @@ public class SwedbankSEAgentWireMockTest {
 
     private Transfer createMockedDomesticTransfer() {
 
+        RemittanceInformation remittanceInformation = new RemittanceInformation();
+        remittanceInformation.setValue("642257434400156");
         Transfer transfer = new Transfer();
         transfer.setSource(AccountIdentifier.create(AccountIdentifier.Type.SE, "832791234567890"));
 
@@ -136,7 +139,7 @@ public class SwedbankSEAgentWireMockTest {
                         LocalDate.of(2020, 6, 17)
                                 .atStartOfDay(ZoneId.of("Europe/Stockholm"))
                                 .toInstant()));
-        transfer.setDestinationMessage("642257434400156");
+        transfer.setRemittanceInformation(remittanceInformation);
 
         return transfer;
     }
