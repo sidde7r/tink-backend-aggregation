aaa8d1df1fb0596e444960659f65580c8beef6ce
Jesús A. Álvarez <jesus.alvarez@tink.se>
2020-11-16 16:59:32 +0100

    refactor(swedbank/business): add business profile selector
    inherit from SwedbankAbstractAgent instead of SwedbankSEAgent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/swedbank/SwedbankSEBusinessAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/business/swedbank/SwedbankSEBusinessAgent.java
@@ -5,10 +5,18 @@ import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capa
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.SAVINGS_ACCOUNTS;
 
 import com.google.inject.Inject;
+import java.time.ZoneId;
+import java.util.Locale;
+import se.tink.backend.agents.rpc.Field.Key;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
-import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.SwedbankSEAgent;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.SwedbankSEApiClientProvider;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.SwedbankSEConstants;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.SwedbankAbstractAgent;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.configuration.SwedbankConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.executors.utilities.SwedbankDateUtils;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.fetchers.transactional.SwedbankDefaultTransactionalAccountFetcher;
 import se.tink.backend.aggregation.agents.nxgen.se.business.swedbank.fetchers.transactional.SwedbankSEBusinessTransactionalAccountFetcher;
+import se.tink.backend.aggregation.agents.nxgen.se.business.swedbank.profile.SwedbankBusinessProfileSelector;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcherController;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginationController;
@@ -16,11 +24,26 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccoun
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 
 @AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS, IDENTITY_DATA})
-public final class SwedbankSEBusinessAgent extends SwedbankSEAgent {
+public final class SwedbankSEBusinessAgent extends SwedbankAbstractAgent {
 
     @Inject
     public SwedbankSEBusinessAgent(AgentComponentProvider componentProvider) {
-        super(componentProvider);
+        super(
+                componentProvider,
+                new SwedbankConfiguration(
+                        SwedbankSEConstants.PROFILE_PARAMETERS.get(
+                                componentProvider
+                                        .getCredentialsRequest()
+                                        .getProvider()
+                                        .getPayload()),
+                        SwedbankSEConstants.HOST),
+                new SwedbankSEApiClientProvider(
+                        new SwedbankBusinessProfileSelector(
+                                componentProvider
+                                        .getCredentialsRequest()
+                                        .getCredentials()
+                                        .getField(Key.CORPORATE_ID))),
+                new SwedbankDateUtils(ZoneId.of("Europe/Stockholm"), new Locale("sv", "SE")));
     }
 
     @Override
