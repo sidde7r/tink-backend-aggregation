97e9ac414baae3bc863309dc3bd9b5ad4041551a
Seckin Savasci <seckin.savasci@tink.se>
2020-11-17 18:45:32 +0100

    refactor(LocaleValidator): Remove unused LocaleValidator

--- src/libraries/validation/src/main/java/se/tink/libraries/validation/exceptions/InvalidLocaleException.java
+++
@@ -1,7 +0,0 @@
-package se.tink.libraries.validation.exceptions;
-
-public class InvalidLocaleException extends Exception {
-    public InvalidLocaleException(String message) {
-        super(message);
-    }
-}
