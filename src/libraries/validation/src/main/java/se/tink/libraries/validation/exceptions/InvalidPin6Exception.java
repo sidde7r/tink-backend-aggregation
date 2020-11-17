fd063a246efba1be7a8741394cc6ff927a122380
Seckin Savasci <seckin.savasci@tink.se>
2020-11-17 18:42:57 +0100

    refactor(Pin6Validator): Remove unused Pin6Validator

--- src/libraries/validation/src/main/java/se/tink/libraries/validation/exceptions/InvalidPin6Exception.java
+++
@@ -1,7 +0,0 @@
-package se.tink.libraries.validation.exceptions;
-
-public class InvalidPin6Exception extends Exception {
-    public InvalidPin6Exception(String message) {
-        super(message);
-    }
-}
