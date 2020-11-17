72a961921c9c40e0273a060e04edfabd82c5b4e3
Seckin Savasci <seckin.savasci@tink.se>
2020-11-17 18:50:23 +0100

    refactor(EmailValidator): Remove unused EmailValidator

--- src/libraries/validation/src/main/java/se/tink/libraries/validation/exceptions/InvalidEmailException.java
+++
@@ -1,7 +0,0 @@
-package se.tink.libraries.validation.exceptions;
-
-public class InvalidEmailException extends Exception {
-    public InvalidEmailException(String message) {
-        super(message);
-    }
-}
