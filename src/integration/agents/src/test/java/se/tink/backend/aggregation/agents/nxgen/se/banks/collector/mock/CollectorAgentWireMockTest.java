6f293446b83b5f78e778e919506e3c00a6a1fb65
Andreas Olausson <andreas.olausson@tink.se>
2020-11-18 13:26:29 +0100

    test(nxgen/collector): add savings account and identity data to wiremock

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/collector/mock/CollectorAgentWireMockTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/collector/mock/CollectorAgentWireMockTest.java
@@ -1,12 +1,17 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.collector.mock;
 
 import org.junit.Test;
+import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonFileParser;
 import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
 import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockrefresh.AgentWireMockRefreshTest;
+import se.tink.libraries.credentials.service.RefreshableItem;
 import se.tink.libraries.enums.MarketCode;
 
 public class CollectorAgentWireMockTest {
+
+    private static final String PSU = "199901011234";
+
     @Test
     public void test() throws Exception {
         // given
@@ -18,6 +23,9 @@ public class CollectorAgentWireMockTest {
         final AgentWireMockRefreshTest agentWireMockRefreshTest =
                 AgentWireMockRefreshTest.builder(
                                 MarketCode.SE, "collector-bankid", wireMockFilePath)
+                        .addCredentialField(Field.Key.USERNAME.getFieldKey(), PSU)
+                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                        .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
                         .build();
 
         final AgentContractEntity expected =
