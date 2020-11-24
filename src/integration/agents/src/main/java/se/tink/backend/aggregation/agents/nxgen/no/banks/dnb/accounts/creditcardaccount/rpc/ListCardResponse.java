998a78f6f26346b5396c818eef459d08cd755e6b
Jan Sameryt <jan.sameryt@tink.com>
2020-11-24 09:30:35 +0100

    fix(dnbNoRE): Now checking for explicit null as well
    [ITE-1726]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/dnb/accounts/creditcardaccount/rpc/ListCardResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/banks/dnb/accounts/creditcardaccount/rpc/ListCardResponse.java
@@ -1,6 +1,5 @@
 package se.tink.backend.aggregation.agents.nxgen.no.banks.dnb.accounts.creditcardaccount.rpc;
 
-import java.util.Collections;
 import java.util.List;
 import lombok.Getter;
 import se.tink.backend.aggregation.agents.nxgen.no.banks.dnb.accounts.creditcardaccount.entities.CardEntity;
@@ -9,6 +8,6 @@ import se.tink.backend.aggregation.annotations.JsonObject;
 @JsonObject
 @Getter
 public class ListCardResponse {
-    private List<CardEntity> debetCards = Collections.emptyList();
-    private List<CardEntity> creditCards = Collections.emptyList();
+    private List<CardEntity> debetCards;
+    private List<CardEntity> creditCards;
 }
