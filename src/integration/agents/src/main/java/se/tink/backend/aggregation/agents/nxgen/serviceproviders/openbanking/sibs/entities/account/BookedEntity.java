2fa9fafbc89f6c87b0a790fb2f72f78a59e69a60
Blazej Szum <blazej.szum@tink.se>
2020-11-24 17:07:24 +0100

    fix(sibs):disable date mapping fix
     [MINI-106]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/entities/account/BookedEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/entities/account/BookedEntity.java
@@ -5,10 +5,7 @@ import java.util.Date;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsConstants.Formats;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.entities.transaction.CreditorAccount;
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.backend.aggregation.nxgen.core.transaction.DateFieldMigration;
-import se.tink.backend.aggregation.nxgen.core.transaction.FieldsMigrations;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
-import se.tink.backend.aggregation.nxgen.core.transaction.Transaction.Builder;
 
 @JsonObject
 public class BookedEntity {
@@ -24,17 +21,10 @@ public class BookedEntity {
     private String remittanceInformationUnstructured;
 
     public Transaction toTinkTransaction() {
-        Builder builder =
-                Transaction.builder()
-                        .setAmount(amount.toTinkAmount())
-                        .setDate(bookingDate)
-                        .setDescription(remittanceInformationUnstructured);
-        if (null != valueDate) {
-            builder.setFieldsMigrations(
-                    FieldsMigrations.builder()
-                            .migration(DateFieldMigration.version1(valueDate))
-                            .build());
-        }
-        return builder.build();
+        return Transaction.builder()
+                .setAmount(amount.toTinkAmount())
+                .setDate(valueDate)
+                .setDescription(remittanceInformationUnstructured)
+                .build();
     }
 }
