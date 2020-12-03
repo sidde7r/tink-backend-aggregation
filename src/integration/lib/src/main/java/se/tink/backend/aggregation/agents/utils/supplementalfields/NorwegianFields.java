030a0d54bfbf7a8b78d90be671b99438a16ed245
Mateusz Wiski <mateusz.wiski@tink.com>
2020-12-03 11:59:53 +0100

    fix(DanskeBank RE, NO): add translation for description field
    [ITE-1728]

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/supplementalfields/NorwegianFields.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/supplementalfields/NorwegianFields.java
@@ -8,14 +8,17 @@ public class NorwegianFields {
 
     public static class BankIdReferenceInfo {
         private static final String FIELD_KEY = "name";
-        private static final String DESCRIPTION = "Reference";
+        private static final LocalizableKey DESCRIPTION = new LocalizableKey("Reference");
         private static final LocalizableKey HELP_TEXT =
-                LocalizableKey.of(
+                new LocalizableKey(
                         "Continue by clicking update when you have verified the reference and signed with Mobile BankID.");
 
         public static Field build(Catalog catalog, String bankIdReference) {
             return CommonFields.Information.build(
-                    FIELD_KEY, DESCRIPTION, bankIdReference, catalog.getString(HELP_TEXT));
+                    FIELD_KEY,
+                    catalog.getString(DESCRIPTION),
+                    bankIdReference,
+                    catalog.getString(HELP_TEXT));
         }
     }
 }
