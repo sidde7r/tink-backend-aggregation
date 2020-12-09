7ded1c6c50621b959a2a60c87a2e09c9e37a562b
Daniel Obrębski <daniel.obrebski@tink.se>
2020-12-09 10:22:45 +0100

    fix(LF): fetch transactions for last 90 days for BG refreshes
    TC-3929

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/lansforsakringar/LansforsakringarConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/lansforsakringar/LansforsakringarConstants.java
@@ -11,6 +11,7 @@ import se.tink.libraries.payment.enums.PaymentType;
 public abstract class LansforsakringarConstants {
 
     public static final int MONTHS_TO_FETCH = 13;
+    public static final int DAYS_TO_FETCH_BG = 89;
     public static final int MAX_NUM_RETRIES = 3;
     public static final int RETRY_SLEEP_MILLIS_SECONDS = 2000;
 
