4497398601c568e949e8140da63fc6f00ae7dda0
Jan Sameryt <jan.sameryt@tink.com>
2020-12-10 14:36:54 +0100

    refactor(demoAgentSupp): review suggestions added
    [ITE-1793]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/customsupplemental/authenticator/CustomSupplementalAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/customsupplemental/authenticator/CustomSupplementalAuthenticator.java
@@ -14,53 +14,66 @@ import se.tink.libraries.serialization.utils.SerializationUtils;
 @AllArgsConstructor
 public class CustomSupplementalAuthenticator implements Authenticator {
 
+    private static final String FIELD_NAME = "fieldsInJson";
+
     private final SupplementalInformationController supplementalInformationController;
 
     @Override
     public void authenticate(Credentials credentials) {
-        String supplementalFieldsAsJson = credentials.getField(Field.Key.USERNAME);
+        String supplementalFieldsAsJson = credentials.getField(FIELD_NAME);
         List<List<Field>> supplementalScreens = parseInput(supplementalFieldsAsJson);
         askForInfo(supplementalScreens);
         // Everything finished, since this agent is just a shell, fail nicely
         throw LoginError.DEFAULT_MESSAGE.exception("CustomSupplemental demo finished.");
     }
 
-    private List<List<Field>> parseInput(String supplementalFieldsAsJson) {
-        // Try parsing as multiple screens
-        List<List<Field>> allFields =
-                SerializationUtils.deserializeFromString(
-                        supplementalFieldsAsJson, new TypeReference<List<List<Field>>>() {});
-
-        // If that failed, try parsing as single screen
+    private List<List<Field>> parseInput(String fieldsAsJson) {
+        List<List<Field>> allFields = tryParsingAsMultipleScreens(fieldsAsJson);
         if (allFields == null) {
-            List<Field> fields =
-                    SerializationUtils.deserializeFromString(
-                            supplementalFieldsAsJson, new TypeReference<List<Field>>() {});
-            if (fields != null) {
-                allFields = new ArrayList<>();
-                allFields.add(fields);
-            }
+            allFields = tryParsingAsSingleScreen(fieldsAsJson);
+        }
+        if (allFields == null) {
+            allFields = tryParsingAsSingleField(fieldsAsJson);
         }
 
-        // Or as a single Field
-        if (allFields == null) {
-            Field field =
-                    SerializationUtils.deserializeFromString(supplementalFieldsAsJson, Field.class);
-            if (field != null) {
-                allFields = new ArrayList<>();
-                List<Field> fields = new ArrayList<>();
-                allFields.add(fields);
-                fields.add(field);
-            }
+        if (allFields != null) {
+            return allFields;
         }
 
         // We don't support anything else, give up
-        if (allFields == null) {
-            throw LoginError.DEFAULT_MESSAGE.exception(
-                    "CustomSupplemental could not parse input as expected collection of fields");
-        } else {
-            return allFields;
+        throw LoginError.DEFAULT_MESSAGE.exception(
+                "CustomSupplemental could not parse input as expected collection of fields");
+    }
+
+    private List<List<Field>> tryParsingAsMultipleScreens(String fieldsAsJson) {
+        return SerializationUtils.deserializeFromString(
+                fieldsAsJson, new TypeReference<List<List<Field>>>() {});
+    }
+
+    private List<List<Field>> tryParsingAsSingleScreen(String fieldsAsJson) {
+        List<List<Field>> allFields = null;
+        List<Field> fields =
+                SerializationUtils.deserializeFromString(
+                        fieldsAsJson, new TypeReference<List<Field>>() {});
+
+        if (fields != null) {
+            allFields = new ArrayList<>();
+            allFields.add(fields);
         }
+        return allFields;
+    }
+
+    private List<List<Field>> tryParsingAsSingleField(String fieldsAsJson) {
+        List<List<Field>> allFields = null;
+        Field field = SerializationUtils.deserializeFromString(fieldsAsJson, Field.class);
+
+        if (field != null) {
+            allFields = new ArrayList<>();
+            List<Field> fields = new ArrayList<>();
+            allFields.add(fields);
+            fields.add(field);
+        }
+        return allFields;
     }
 
     private void askForInfo(List<List<Field>> supplementalScreens) {
