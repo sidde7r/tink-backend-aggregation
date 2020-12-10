cf6a4020bf93acb7c73756d67250326b6cc7c5b6
Niclas Albinsson <niclas.albinsson@tink.se>
2020-12-10 14:21:17 +0100

    chore(swedbank/se/ob): add account mapping

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/swedbank/SwedbankAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/swedbank/SwedbankAgent.java
@@ -1,6 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.swedbank;
 
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CHECKING_ACCOUNTS;
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.SAVINGS_ACCOUNTS;
 
 import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
@@ -11,7 +12,7 @@ import se.tink.backend.aggregation.eidassigner.module.QSealcSignerModuleRSASHA25
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 
 @AgentDependencyModules(modules = QSealcSignerModuleRSASHA256.class)
-@AgentCapabilities({CHECKING_ACCOUNTS})
+@AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS})
 public class SwedbankAgent extends SwedbankBaseAgent {
 
     @Inject
