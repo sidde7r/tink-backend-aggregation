49782a48edb4220d361a46c1ecf5da22a47ea820
Jan Sameryt <jan.sameryt@tink.com>
2020-12-11 10:28:50 +0100

    fix(testCustomSupp): adding capability to make it work
    [ITE-1793]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/customsupplemental/CustomSupplementalDemoAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/customsupplemental/CustomSupplementalDemoAgent.java
@@ -1,13 +1,23 @@
 package se.tink.backend.aggregation.agents.nxgen.demo.banks.customsupplemental;
 
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CHECKING_ACCOUNTS;
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.SAVINGS_ACCOUNTS;
+
 import com.google.inject.Inject;
+import java.util.Collections;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
+import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
+import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
 import se.tink.backend.aggregation.agents.nxgen.demo.banks.customsupplemental.authenticator.CustomSupplementalAuthenticator;
 import se.tink.backend.aggregation.nxgen.agents.NextGenerationAgent;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
 
-public class CustomSupplementalDemoAgent extends NextGenerationAgent {
+@AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS})
+public class CustomSupplementalDemoAgent extends NextGenerationAgent
+        implements RefreshCheckingAccountsExecutor {
 
     @Inject
     public CustomSupplementalDemoAgent(AgentComponentProvider componentProvider) {
@@ -22,5 +32,15 @@ public class CustomSupplementalDemoAgent extends NextGenerationAgent {
     @Override
     protected Authenticator constructAuthenticator() {
         return new CustomSupplementalAuthenticator(supplementalInformationController);
+    }
+
+    @Override
+    public FetchAccountsResponse fetchCheckingAccounts() {
+        return new FetchAccountsResponse(Collections.emptyList());
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchCheckingTransactions() {
+        return new FetchTransactionsResponse(Collections.emptyMap());
     }
 }
