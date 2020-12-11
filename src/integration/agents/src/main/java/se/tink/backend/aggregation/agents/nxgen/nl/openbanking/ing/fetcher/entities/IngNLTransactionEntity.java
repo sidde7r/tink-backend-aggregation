c8f190ba09f53befcf197bc6e380bf3fb6e10412
Matija Bruncic <matija.bruncic@tink.com>
2020-12-11 17:39:10 +0100

    feat(nl_ing_ob): move description composition to categorization service

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/fetcher/entities/IngNLTransactionEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/fetcher/entities/IngNLTransactionEntity.java
@@ -1,12 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.openbanking.ing.fetcher.entities;
 
-import se.tink.backend.aggregation.agents.nxgen.nl.openbanking.ing.utils.DutchTextCleaner;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ingbase.fetcher.entities.TransactionEntity;
 
 public class IngNLTransactionEntity extends TransactionEntity {
 
     @Override
     public String toTinkDescription() {
-        return new DutchTextCleaner().clean(getRemittanceInformationUnstructured());
+        return getRemittanceInformationUnstructured();
     }
 }
