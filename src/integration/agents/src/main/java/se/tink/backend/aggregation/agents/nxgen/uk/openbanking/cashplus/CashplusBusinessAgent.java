27f738ad9f64961f655412b6c20e97ef65657e0f
Szymon Mysiak <szymon.mysiak@tink.se>
2020-12-15 17:42:51 +0100

    feat(uk/cashplus): add business agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/cashplus/CashplusBusinessAgent.java
@@ -0,0 +1,52 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.cashplus;
+
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CHECKING_ACCOUNTS;
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CREDIT_CARDS;
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.LOANS;
+
+import com.google.inject.Inject;
+import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
+import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForDecoupledMode;
+import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForProductionMode;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.UkOpenBankingBaseAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.entities.AccountOwnershipType;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.interfaces.UkOpenBankingAis;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.interfaces.UkOpenBankingAisConfig;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.module.JwtSignerModule;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.module.UkOpenBankingLocalKeySignerModuleForDecoupledMode;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.module.UkOpenBankingModule;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.v31.UkOpenBankingAisConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.v31.UkOpenBankingV31Ais;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.jwt.signer.iface.JwtSigner;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
+
+@AgentDependencyModulesForProductionMode(
+        modules = {UkOpenBankingModule.class, JwtSignerModule.class})
+@AgentDependencyModulesForDecoupledMode(
+        modules = UkOpenBankingLocalKeySignerModuleForDecoupledMode.class)
+@AgentCapabilities({CHECKING_ACCOUNTS, CREDIT_CARDS, LOANS})
+public final class CashplusBusinessAgent extends UkOpenBankingBaseAgent {
+
+    private static final UkOpenBankingAisConfig aisConfig;
+
+    static {
+        aisConfig =
+                UkOpenBankingAisConfiguration.builder()
+                        .withAllowedAccountOwnershipType(AccountOwnershipType.BUSINESS)
+                        .withApiBaseURL("https://matls.api.ob.cashplus.com/open-banking/v3.1/aisp")
+                        .withWellKnownURL(
+                                "https://identity.ob.cashplus.com/.well-known/openid-configuration")
+                        .withOrganisationId("0015800001ZEZ3gAAH")
+                        .build();
+    }
+
+    @Inject
+    public CashplusBusinessAgent(AgentComponentProvider componentProvider, JwtSigner jwtSigner) {
+        super(componentProvider, jwtSigner, aisConfig);
+    }
+
+    @Override
+    protected UkOpenBankingAis makeAis() {
+        return new UkOpenBankingV31Ais(aisConfig, persistentStorage, localDateTimeSource);
+    }
+}
