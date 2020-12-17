2d8913b3c02f9ebd44e83b91ed95e2a465c0ec52
Jakub Badysiak <jakub.badysiak@tink.se>
2020-12-17 09:49:26 +0100

    fix(es/bbva): Throwing Temporary error instead of Authentication error
    [IFD-1784]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/authenticator/BbvaAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/bbva/authenticator/BbvaAuthenticator.java
@@ -8,6 +8,7 @@ import se.tink.backend.agents.rpc.CredentialsTypes;
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
 import se.tink.backend.aggregation.agents.exceptions.LoginException;
+import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceError;
 import se.tink.backend.aggregation.agents.exceptions.errors.LoginError;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.BbvaApiClient;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.bbva.BbvaConstants.AuthenticationStates;
@@ -74,9 +75,8 @@ public class BbvaAuthenticator implements MultiFactorAuthenticator {
     private boolean isTwoFactorAuthenticationNeeded(String authenticationState) {
         if (!request.isManual()
                 && authenticationState.equalsIgnoreCase(AuthenticationStates.GO_ON)) {
-            // TODO change it in future, throwing RuntimeException only because EVO asked for
-            // TEMPORARY_ERROR instead of AUTHENTICATION_ERROR
-            throw new RuntimeException("SCA request during auto refresh, aborting authentication");
+            throw BankServiceError.SESSION_TERMINATED.exception(
+                    "SCA request during auto refresh, aborting authentication");
         }
         return authenticationState.equalsIgnoreCase(AuthenticationStates.GO_ON)
                 && request.isManual();
