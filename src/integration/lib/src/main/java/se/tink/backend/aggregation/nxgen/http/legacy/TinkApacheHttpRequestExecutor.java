c1a933e45e5330ef2de36df1240d4536181e50e0
Xiaoqiang Chen <xiaoqiang.chen@tink.se>
2020-12-19 20:13:28 +0100

    refactor(aggregation): java style

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/legacy/TinkApacheHttpRequestExecutor.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/legacy/TinkApacheHttpRequestExecutor.java
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.google.common.base.Strings;
-import com.google.common.collect.ImmutableSet;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.OffsetDateTime;
