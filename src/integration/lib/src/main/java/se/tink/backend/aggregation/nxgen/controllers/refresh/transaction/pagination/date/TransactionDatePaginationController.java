a69ee9b66258eae8016d81eee9cc24f6a1e66ac6
Daniel Obrębski <daniel.obrebski@tink.se>
2020-12-30 16:08:23 +0100

    feat(DatePaginationController): add builder

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/date/TransactionDatePaginationController.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/date/TransactionDatePaginationController.java
@@ -1,15 +1,14 @@
 package se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date;
 
 import com.google.common.base.Preconditions;
-import java.lang.invoke.MethodHandles;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Collection;
 import java.util.Date;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date.ActualLocalDateTimeSource;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date.LocalDateTimeSource;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
@@ -18,33 +17,36 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.paginat
 import se.tink.backend.aggregation.nxgen.core.account.Account;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 
+@Slf4j
 public class TransactionDatePaginationController<A extends Account>
         implements TransactionPaginator<A> {
-    private static final Logger logger =
-            LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     private static final int DEFAULT_MAX_CONSECUTIVE_EMPTY_PAGES = 4;
     private static final int DEFAULT_DAYS_TO_FETCH = 89;
 
     private final TransactionDatePaginator<A> paginator;
-
-    private LocalDateTime fromDateTime;
-    private LocalDateTime toDateTime;
-    private int consecutiveEmptyPages = 0;
     private final int consecutiveEmptyPagesLimit;
     private final ChronoUnit unitToFetch;
     private final int amountToFetch;
     private final LocalDateTimeSource localDateTimeSource;
+    private ZoneId zoneId = ZoneId.of("CET");
 
+    private LocalDateTime fromDateTime;
+    private LocalDateTime toDateTime;
+    private int consecutiveEmptyPages = 0;
+
+    @Deprecated
     public TransactionDatePaginationController(TransactionDatePaginator<A> paginator) {
         this(paginator, DEFAULT_MAX_CONSECUTIVE_EMPTY_PAGES);
     }
 
+    @Deprecated
     public TransactionDatePaginationController(
             TransactionDatePaginator<A> paginator, int consecutiveEmptyPagesLimit) {
         this(paginator, consecutiveEmptyPagesLimit, DEFAULT_DAYS_TO_FETCH, ChronoUnit.DAYS);
     }
 
+    @Deprecated
     public TransactionDatePaginationController(
             TransactionDatePaginator<A> paginator,
             int consecutiveEmptyPagesLimit,
@@ -58,6 +60,7 @@ public class TransactionDatePaginationController<A extends Account>
                 new ActualLocalDateTimeSource());
     }
 
+    @Deprecated
     public TransactionDatePaginationController(
             TransactionDatePaginator<A> paginator,
             int consecutiveEmptyPagesLimit,
@@ -70,6 +73,7 @@ public class TransactionDatePaginationController<A extends Account>
                 localDateTimeSource);
     }
 
+    @Deprecated
     public TransactionDatePaginationController(
             TransactionDatePaginator<A> paginator,
             int consecutiveEmptyPagesLimit,
@@ -87,6 +91,15 @@ public class TransactionDatePaginationController<A extends Account>
                         || unitToFetch == ChronoUnit.WEEKS
                         || unitToFetch == ChronoUnit.MONTHS,
                 "Invalid time unit for pagination: " + unitToFetch.toString());
+    }
+
+    private TransactionDatePaginationController(Builder<A> builder) {
+        this.paginator = builder.paginator;
+        this.consecutiveEmptyPagesLimit = builder.consecutiveEmptyPagesLimit;
+        this.unitToFetch = builder.unitToFetch;
+        this.amountToFetch = builder.amountToFetch;
+        this.localDateTimeSource = builder.localDateTimeSource;
+        this.zoneId = builder.zoneId;
     }
 
     @Override
@@ -109,7 +122,7 @@ public class TransactionDatePaginationController<A extends Account>
         if (transactions.isEmpty() && !response.canFetchMore().isPresent()) {
             // Override canFetchMore with consecutive check.
 
-            logger.info(
+            log.info(
                     String.format(
                             "Couldn't find any transactions for account with accountNumber: %s",
                             account.getAccountNumber()));
@@ -119,7 +132,7 @@ public class TransactionDatePaginationController<A extends Account>
                     consecutiveEmptyPages < consecutiveEmptyPagesLimit);
         }
 
-        logger.info(
+        log.info(
                 String.format(
                         "Fetched %s transactions for account with accountNumber: %s",
                         transactions.size(), account.getAccountNumber()));
@@ -136,7 +149,7 @@ public class TransactionDatePaginationController<A extends Account>
     }
 
     private Date convertToDate(LocalDateTime dateTime) {
-        return Date.from(dateTime.atZone(ZoneId.of("CET")).toInstant());
+        return Date.from(dateTime.atZone(zoneId).toInstant());
     }
 
     private LocalDateTime calculateToDate() {
@@ -161,6 +174,50 @@ public class TransactionDatePaginationController<A extends Account>
             default:
                 throw new IllegalStateException(
                         "Unsupported pagination unit: " + unitToFetch.toString());
+        }
+    }
+
+    public static class Builder<A extends Account> {
+
+        @NonNull private TransactionDatePaginator<A> paginator;
+        private int consecutiveEmptyPagesLimit = DEFAULT_MAX_CONSECUTIVE_EMPTY_PAGES;
+        private ChronoUnit unitToFetch = ChronoUnit.DAYS;
+        private int amountToFetch = DEFAULT_DAYS_TO_FETCH;
+        private LocalDateTimeSource localDateTimeSource = new ActualLocalDateTimeSource();
+        private ZoneId zoneId = ZoneId.of("CET");
+
+        public Builder<A> setPaginator(TransactionDatePaginator<A> paginator) {
+            this.paginator = paginator;
+            return this;
+        }
+
+        public Builder<A> setConsecutiveEmptyPagesLimit(int consecutiveEmptyPagesLimit) {
+            this.consecutiveEmptyPagesLimit = consecutiveEmptyPagesLimit;
+            return this;
+        }
+
+        public Builder<A> setUnitToFetch(ChronoUnit unitToFetch) {
+            this.unitToFetch = unitToFetch;
+            return this;
+        }
+
+        public Builder<A> setAmountToFetch(int amountToFetch) {
+            this.amountToFetch = amountToFetch;
+            return this;
+        }
+
+        public Builder<A> setLocalDateTimeSource(LocalDateTimeSource localDateTimeSource) {
+            this.localDateTimeSource = localDateTimeSource;
+            return this;
+        }
+
+        public Builder<A> setZoneId(ZoneId zoneId) {
+            this.zoneId = zoneId;
+            return this;
+        }
+
+        public TransactionDatePaginationController<A> build() {
+            return new TransactionDatePaginationController<>(this);
         }
     }
 }
