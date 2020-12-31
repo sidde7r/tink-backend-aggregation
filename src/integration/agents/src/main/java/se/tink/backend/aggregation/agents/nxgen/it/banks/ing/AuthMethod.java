883389fd7fa06c9d73233c3c8ed99a18c4450344
Sebastian Brzozowski <sebastian.brzozowski@tink.se>
2020-12-31 10:41:14 +0100

    refactor(ing:it): remove dead agent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/banks/ing/AuthMethod.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.it.banks.ing;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public enum AuthMethod {
-    @JsonProperty("smartcode")
-    SMART_CODE
-}
