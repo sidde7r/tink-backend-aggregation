2bf5772ca66ea12ded501a8746e9898383bd3702
Jan Sameryt <jan.sameryt@tink.com>
2021-01-08 10:50:01 +0100

    fix(sparebank): now using deserializer for transactionDate
    [ITE-1981]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/fetcher/card/entities/CardTransactionEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/fetcher/card/entities/CardTransactionEntity.java
@@ -1,8 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sparebank.fetcher.card.entities;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import java.time.LocalDate;
 import lombok.Getter;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sparebank.fetcher.transactionalaccount.entities.BalanceAmountEntity;
@@ -18,7 +16,7 @@ public class CardTransactionEntity {
     @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate bookingDate;
 
-    @JsonSerialize(using = LocalDateSerializer.class)
+    @JsonDeserialize(using = LocalDateDeserializer.class)
     private LocalDate transactionDate;
 
     private BalanceAmountEntity transactionAmount;
