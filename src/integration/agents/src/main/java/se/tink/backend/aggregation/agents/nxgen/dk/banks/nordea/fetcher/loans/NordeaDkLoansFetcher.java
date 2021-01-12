61b0b232e654f023f1d89f5cb6209d605bbd2d22
Mateusz Wiski <mateusz.wiski@tink.com>
2021-01-12 12:01:03 +0100

    chore(NordeaRE, DK): fetch loans transactions, fix interest rate
    [ITE-1312]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/fetcher/loans/NordeaDkLoansFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/fetcher/loans/NordeaDkLoansFetcher.java
@@ -1,16 +1,28 @@
 package se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.loans;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.NordeaDkApiClient;
+import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.NordeaDkConstants;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.loans.entities.LoanEntity;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.loans.rpc.LoanDetailsResponse;
+import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.transactionalaccount.entities.TransactionEntity;
+import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.transactionalaccount.rpc.TransactionsResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginator;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginatorResponse;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginatorResponseImpl;
 import se.tink.backend.aggregation.nxgen.core.account.loan.LoanAccount;
+import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
 
+@Slf4j
 @RequiredArgsConstructor
-public class NordeaDkLoansFetcher implements AccountFetcher<LoanAccount> {
+public class NordeaDkLoansFetcher
+        implements AccountFetcher<LoanAccount>, TransactionKeyPaginator<LoanAccount, String> {
 
     private final NordeaDkApiClient apiClient;
 
@@ -20,6 +32,42 @@ public class NordeaDkLoansFetcher implements AccountFetcher<LoanAccount> {
                 .map(LoanEntity::getLoanId)
                 .map(apiClient::getLoanDetails)
                 .map(LoanDetailsResponse::toTinkLoanAccount)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public TransactionKeyPaginatorResponse<String> getTransactionsFor(
+            LoanAccount account, String continuationKey) {
+        TransactionsResponse transactionsResponse;
+        try {
+            transactionsResponse =
+                    apiClient.getAccountTransactions(
+                            account.getApiIdentifier(),
+                            account.getFromTemporaryStorage(
+                                    NordeaDkConstants.StorageKeys.PRODUCT_CODE),
+                            continuationKey);
+
+            log.info(
+                    "[Nordea DK] Successfully fetched loan transactions. Loan type: {}, loan productCode: {}",
+                    account.getDetails().getType(),
+                    account.getFromTemporaryStorage(NordeaDkConstants.StorageKeys.PRODUCT_CODE));
+
+            return new TransactionKeyPaginatorResponseImpl<>(
+                    getTransactions(transactionsResponse),
+                    transactionsResponse.getContinuationKey());
+        } catch (HttpResponseException e) {
+            log.info(
+                    "[Nordea DK] Failed to fetch loan transactions. Loan type: {}, loan productCode: {} ",
+                    account.getDetails().getType(),
+                    account.getFromTemporaryStorage(NordeaDkConstants.StorageKeys.PRODUCT_CODE),
+                    e);
+            return TransactionKeyPaginatorResponseImpl.createEmpty();
+        }
+    }
+
+    List<Transaction> getTransactions(TransactionsResponse transactionsResponse) {
+        return transactionsResponse.getTransactions().stream()
+                .map(TransactionEntity::toTinkTransaction)
                 .collect(Collectors.toList());
     }
 }
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/fetcher/transactionalaccount/rpc/TransactionsResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/nordea/fetcher/transactionalaccount/rpc/TransactionsResponse.java
@@ -3,6 +3,7 @@ package se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.transac
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.util.Collections;
 import java.util.List;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.nordea.fetcher.transactionalaccount.entities.TransactionEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
@@ -14,7 +15,7 @@ public class TransactionsResponse {
     private String continuationKey;
 
     @JsonProperty("result")
-    private List<TransactionEntity> transactions;
+    private List<TransactionEntity> transactions = Collections.emptyList();
 
     public String getContinuationKey() {
         return continuationKey;
