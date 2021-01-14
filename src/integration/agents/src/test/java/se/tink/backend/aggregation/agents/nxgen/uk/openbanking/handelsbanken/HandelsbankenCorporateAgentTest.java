d0af580a7f2a7265368fc72dde884824c04d36ff
Jakub Badysiak <jakub.badysiak@tink.se>
2021-01-14 15:42:38 +0100

    feat(uk/handelsbanken): add personal and corporate agents
    [IFD-1548]

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/handelsbanken/HandelsbankenCorporateAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/handelsbanken/HandelsbankenCorporateAgentTest.java
@@ -3,11 +3,11 @@ package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.handelsbanken;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
-public class HandelsbankenAgentTest {
+public class HandelsbankenCorporateAgentTest {
 
     @Test
     public void testRefresh() throws Exception {
-        new AgentIntegrationTest.Builder("uk", "uk-handelsbanken-ob")
+        new AgentIntegrationTest.Builder("uk", "uk-handelsbanken-corporate-ob")
                 .addCredentialField("accessToken", "MV9QUk9GSUxFLUZJX1BSSVZBVEUz")
                 .loadCredentialsBefore(false)
                 .saveCredentialsAfter(false)
