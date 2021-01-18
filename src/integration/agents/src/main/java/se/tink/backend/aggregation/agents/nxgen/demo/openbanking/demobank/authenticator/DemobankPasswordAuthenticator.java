c15e6c0c1b176a532abb3891bdef2225fb646b40
Richard Stephens <richard.stephens@tink.se>
2021-01-18 13:52:28 +0100

    fix(demobank): password authenticator throws login error

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/openbanking/demobank/authenticator/DemobankPasswordAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/openbanking/demobank/authenticator/DemobankPasswordAuthenticator.java
@@ -2,9 +2,11 @@ package se.tink.backend.aggregation.agents.nxgen.demo.openbanking.demobank.authe
 
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
+import se.tink.backend.aggregation.agents.exceptions.errors.LoginError;
 import se.tink.backend.aggregation.agents.nxgen.demo.openbanking.demobank.DemobankApiClient;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.password.PasswordAuthenticator;
 import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
 
 public class DemobankPasswordAuthenticator implements PasswordAuthenticator {
     public DemobankApiClient apiClient;
@@ -17,7 +19,11 @@ public class DemobankPasswordAuthenticator implements PasswordAuthenticator {
     public void authenticate(String username, String password)
             throws AuthenticationException, AuthorizationException {
 
-        OAuth2Token oAuth2Token = apiClient.login(username, password);
-        apiClient.setTokenToStorage(oAuth2Token);
+        try {
+            OAuth2Token oAuth2Token = apiClient.login(username, password);
+            apiClient.setTokenToStorage(oAuth2Token);
+        } catch (HttpResponseException e) {
+            throw LoginError.INCORRECT_CREDENTIALS.exception(e);
+        }
     }
 }
