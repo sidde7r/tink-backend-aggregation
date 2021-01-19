57d78959ae70a579b72cb565161bf24502a774bb
Mateusz Konstanciak <mateusz.konstanciak@tink.se>
2021-01-19 09:59:04 +0100

    fix(Axa): Fix Axa OB authorize URL
    Axa migrated to the new XS2A solution requiring extra call during authorize.
    
    [MINI-581]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/axa/AxaAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/axa/AxaAgent.java
@@ -4,7 +4,9 @@ import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capa
 
 import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
+import se.tink.backend.aggregation.agents.nxgen.be.openbanking.axa.authenticator.AxaAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.Xs2aDevelopersTransactionalAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.xs2adevelopers.authenticator.Xs2aDevelopersAuthenticator;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 
 @AgentCapabilities({CHECKING_ACCOUNTS})
@@ -13,5 +15,16 @@ public final class AxaAgent extends Xs2aDevelopersTransactionalAgent {
     @Inject
     public AxaAgent(AgentComponentProvider componentProvider) {
         super(componentProvider, "https://api-dailybanking.axabank.be");
+    }
+
+    @Override
+    protected Xs2aDevelopersAuthenticator constructXs2aAuthenticator(
+            AgentComponentProvider componentProvider) {
+        return new AxaAuthenticator(
+                apiClient,
+                persistentStorage,
+                configuration,
+                componentProvider.getLocalDateTimeSource(),
+                credentials);
     }
 }
