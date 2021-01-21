7152527ff42cf8817bcfc259deaee46912e69401
Sai He <sai.he@tink.se>
2021-01-21 11:02:07 +0100

    feat(UK/PIS): To create a new signer for PIS Payload

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/ais/base/jwt/signer/PayloadEncodedSecretServiceJwtSigner.java
@@ -0,0 +1,33 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.jwt.signer;
+
+import com.nimbusds.jose.crypto.RSASSASigner;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.configuration.UkOpenBankingConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.jwt.signer.iface.JwtSigner;
+import se.tink.backend.aggregation.agents.utils.crypto.RSA;
+import se.tink.backend.aggregation.agents.utils.encoding.EncodingUtils;
+
+@RequiredArgsConstructor
+public class PayloadEncodedSecretServiceJwtSigner implements JwtSigner {
+    private final UkOpenBankingConfiguration configuration;
+
+    @Override
+    public String sign(
+            JwtSigner.Algorithm algorithm,
+            Map<String, Object> headerClaims,
+            Map<String, Object> payloadClaims,
+            boolean detachedPayload) {
+        RSASSASigner signer =
+                new RSASSASigner(
+                        RSA.getPrivateKeyFromBytes(
+                                EncodingUtils.decodeBase64String(configuration.getSigningKey())));
+        return TinkJwtPaymentSigner.builder(configuration::getSigningKeyId, signer)
+                .withAlgorithm(algorithm)
+                .withHeaderClaims(headerClaims)
+                .withPayloadClaims(payloadClaims)
+                .withDetachedPayload(detachedPayload)
+                .build()
+                .sign();
+    }
+}
