c6760dfbf064a6a016ac3ae330e424862b338a96
Sai He <sai.he@tink.se>
2021-01-22 13:31:32 +0100

    fix(UK/Revolut): To fix Revolut fake null debtor

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/pis/common/UkOpenBankingPaymentHelperTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/pis/common/UkOpenBankingPaymentHelperTest.java
@@ -5,10 +5,14 @@ import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbank
 import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.pis.UkOpenBankingPaymentTestFixtures.createFutureDatePayment;
 import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.pis.UkOpenBankingPaymentTestFixtures.createTodayPayment;
 
+import java.net.URI;
 import java.time.Clock;
 import org.junit.Before;
 import org.junit.Test;
+import se.tink.libraries.account.AccountIdentifier;
 import se.tink.libraries.payment.enums.PaymentType;
+import se.tink.libraries.payment.rpc.Creditor;
+import se.tink.libraries.payment.rpc.Debtor;
 import se.tink.libraries.payment.rpc.Payment;
 
 public class UkOpenBankingPaymentHelperTest {
@@ -42,5 +46,32 @@ public class UkOpenBankingPaymentHelperTest {
 
         // then
         assertThat(paymentType).isEqualTo(PaymentType.DOMESTIC_FUTURE);
+    }
+
+    @Test
+    public void testRevolutDomesticPaymentWithEmptyDebtor() {
+        Payment payment =
+                new Payment.Builder()
+                        .withDebtor(new Debtor(null))
+                        .withCreditor(
+                                new Creditor(
+                                        AccountIdentifier.create(
+                                                URI.create("sort-code://12345612345678"))))
+                        .build();
+        assertThat(UkOpenBankingPaymentHelper.getPaymentType(payment))
+                .isEqualTo(PaymentType.DOMESTIC);
+    }
+
+    @Test
+    public void testRevolutSepaPaymentWithEmptyDebtor() {
+        Payment payment =
+                new Payment.Builder()
+                        .withDebtor(new Debtor(null))
+                        .withCreditor(
+                                new Creditor(
+                                        AccountIdentifier.create(
+                                                URI.create("iban://SE4550000000058398257466"))))
+                        .build();
+        assertThat(UkOpenBankingPaymentHelper.getPaymentType(payment)).isEqualTo(PaymentType.SEPA);
     }
 }
