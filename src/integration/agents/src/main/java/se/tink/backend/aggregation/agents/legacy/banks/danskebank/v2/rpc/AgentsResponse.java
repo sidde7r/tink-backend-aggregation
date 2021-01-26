7bb637f21cf131bb1c9d7962f69f2fdaac03f425
Nazri Abdullah <nazri.abdullah@tink.se>
2021-01-26 20:06:50 +0800

    chore(danskebank): removed legacy codes

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/danskebank/v2/rpc/AgentsResponse.java
+++
@@ -1,6 +0,0 @@
-package se.tink.backend.aggregation.agents.banks.danskebank.v2.rpc;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class AgentsResponse extends AbstractResponse {}
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/danskebank/v2/rpc/VerifyBankIdRequest.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.banks.danskebank.v2.rpc;
-
-public class VerifyBankIdRequest extends BankIdRequest<VerifyBankIdModuleInput> {
-    public VerifyBankIdRequest(String orderReference) {
-        super(new VerifyBankIdModuleInput(orderReference));
-    }
-}
