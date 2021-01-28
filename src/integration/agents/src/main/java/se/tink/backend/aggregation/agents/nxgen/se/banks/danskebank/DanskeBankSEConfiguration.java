ad6670fff1d53705657aa48c500d97ce0ef5c68b
Andreas Olausson <andreas.olausson@tink.se>
2021-01-28 11:26:04 +0100

    docs(danskebank/SE): document product codes

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/danskebank/DanskeBankSEConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/danskebank/DanskeBankSEConfiguration.java
@@ -83,29 +83,59 @@ public class DanskeBankSEConfiguration implements DanskeBankConfiguration {
 
     @Override
     public List<String> getCheckingAccountTypes() {
+        // Documenting here what the product codes mean.
+        // Commenting is done on the form:
+        // <accountProduct>: "<accountName>" - "<'translated' accountName>"
+        // Some accounts seem to have attributes like a checking account,
+        // for instance: access to credit and debit card, but in the bank app, they are usually
+        // displayed as savings accounts.
+        // For consistency reasons, we'll map those as they are mapped in the bank app.
         return ImmutableList.<String>builder()
+                // 2AA: "Byggnadskredit" - "Building credit"
                 .add("2AA")
+                // 2AB: "Depåkonto" - "Depot account"
                 .add("2AB")
+                // 2AE: Missing logs for this product code
                 .add("2AE")
+                // 2AF: "Företagskonto" - "Company account"
                 .add("2AF")
+                // 2A3: "Mastercard Guld" - "Mastercard Gold"
                 .add("2A3")
+                // 2A4: "Företagskort" - "Company card"
                 .add("2A4")
+                // 2BA: "Finanskonto" - "Finance account"
                 .add("2BA")
+                // 2BM: "Privatkonto special" - "Private account special"
                 .add("2BM")
+                // 2B4: "Danske start" - "Danske start account"
                 .add("2B4")
+                // 2B5: "Servicekonto" - "Service account"
                 .add("2B5")
+                // 2B6: "Baskonto" - "Base account"
                 .add("2B6")
+                // 2B7: "Danske Exklusiv" - "Danske Exclusive"
                 .add("2B7")
+                // 2B8: "Mastercard Platinum"
                 .add("2B8")
+                // 2B9: "World Elite" - Credit card
                 .add("2B9")
+                // 2B0: "Danske Plus" - Credit card
                 .add("2B0")
+                // 2CY: "Mastercard Direkt" - "Mastercard Direct"
                 .add("2CY")
+                // 2CZ: "Mastercard Direkt Ung" - "Mastercard Direct Young"
                 .add("2CZ")
+                // 2DE: "Gårdskonto" - "Farm account" with access to credit.
                 .add("2DE")
+                // 2EH: "<USERNAME> Spar" - Savings account that the user has named.
                 .add("2EH")
+                // 2EX: "Danske konto" - "Danske account"
                 .add("2EX")
+                // 3BG: "Danske Business ONE"
                 .add("3BG")
+                // 3BH: "Danske Business PLUS"
                 .add("3BH")
+                // 62C: "Valutakonto" - "Currency account"
                 .add("62C")
                 .build();
     }
@@ -113,19 +143,38 @@ public class DanskeBankSEConfiguration implements DanskeBankConfiguration {
     @Override
     public List<String> getSavingsAccountTypes() {
         return ImmutableList.<String>builder()
+                // 2AN: "Specialinlåning" - "Special deposits"
                 .add("2AN")
+                // 2BP: "Sparkonto" - "Savings account".
+                // An account with access to both credit and debit card.
+                // However, many users have named this account personally and it's by default named
+                // as "Savings account" from the bank.
+                // It's most likely a savings account at Danske and we should map it accordingly.
                 .add("2BP")
+                // 2BS: "Skogskonto" - "Forest account"
                 .add("2BS")
+                // 2CF: "Indivudellt pensionssparkonto" - "Individual pension account"
                 .add("2CF")
+                // 2C2: "Sparkonto XL" - "Savings account XL"
                 .add("2C2")
+                // 2DC: "Investeringssparkonto"/"Individuellt sparkonto"
+                // - "Investment savings account/Individual savings account"
                 .add("2DC")
+                // 2DD: "Investeringssparkonto val." - "Investment savings account"
                 .add("2DD")
+                // 2DH: "AKP ISK" - "Investment savings account"
                 .add("2DH")
+                // 2DI: "June" - Account with access to credit
                 .add("2DI")
+                // 2ED: "Fastränteplacering" - "Fixed-rate investment"
                 .add("2ED")
+                // 2EK: "Sparkonto XL Kampanj" - "Savings account XL campaign"
                 .add("2EK")
+                // 2SF: "Sparkonto Företag" - Savings account Company"
                 .add("2SF")
+                // 3CA: "HSB Bosparkonto" - "HSB home-saving account"
                 .add("3CA")
+                // 3CB: "HSB Fasträntekonto" - "HSB fixed-rate account"
                 .add("3CB")
                 .build();
     }
@@ -133,20 +182,35 @@ public class DanskeBankSEConfiguration implements DanskeBankConfiguration {
     @Override
     public Map<String, LoanDetails.Type> getLoanAccountTypes() {
         return ImmutableMap.<String, LoanDetails.Type>builder()
+                // 2AT: "Företagslån (02)" - "Company loan"
                 .put("2AT", LoanDetails.Type.OTHER)
+                // 2AS: "Skogslån" - "Forest loan"
                 .put("2AS", LoanDetails.Type.LAND)
+                // 2CK: "Privatbostadslån" - "Private home loan"
                 .put("2CK", LoanDetails.Type.MORTGAGE)
+                // 2CL: "Privatlån" - "Privateloan"
                 .put("2CL", LoanDetails.Type.BLANCO)
+                // 2C6: "Direktlån" - "Direct loan"
                 .put("2C6", LoanDetails.Type.OTHER)
+                // 2DG: "Fasträntelån" - "Fixed-rate loan"
                 .put("2DG", LoanDetails.Type.OTHER)
+                // 2DL: "Handpenning/Överbryggningslån" - "Down payment/Bridging loan"
                 .put("2DL", LoanDetails.Type.OTHER)
+                // 3AC: "Bolån" - "Mortgage loan"
                 .put("3AC", LoanDetails.Type.MORTGAGE)
+                // 3AN: "Bolån Premium" - "Mortgage loan Premium"
                 .put("3AN", LoanDetails.Type.MORTGAGE)
+                // 3AS: "Företagslån (03)" - "Company loan"
                 .put("3AS", LoanDetails.Type.OTHER)
+                // 3AT: "Företagslån (04)" - "Company loan"
                 .put("3AT", LoanDetails.Type.OTHER)
+                // 3BJ: "Bolån Fast Hypotek" - "Mortgage loan fixed
                 .put("3BJ", LoanDetails.Type.MORTGAGE)
+                // 3BL: "Bolån Online Hypotek" - "Mortgage loan online"
                 .put("3BL", LoanDetails.Type.MORTGAGE)
+                // 3BM: "Bolån" - Mortgage loan"
                 .put("3BM", LoanDetails.Type.MORTGAGE)
+                // 3BK: "Bolån Premium Hypotek" - Mortgage loan Premium"
                 .put("3BK", LoanDetails.Type.MORTGAGE)
                 .build();
     }
