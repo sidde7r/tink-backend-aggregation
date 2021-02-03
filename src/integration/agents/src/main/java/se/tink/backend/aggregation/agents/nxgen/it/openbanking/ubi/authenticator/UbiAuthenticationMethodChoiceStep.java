5403a12892fc3ff736344493a5a7db84b7c09c8f
Daniel Obrębski <daniel.obrebski@tink.se>
2021-02-03 10:18:28 +0100

    feat(UBI/OB): add select option to handle (no)bankapp
    [ITE-2095]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/ubi/authenticator/UbiAuthenticationMethodChoiceStep.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/ubi/authenticator/UbiAuthenticationMethodChoiceStep.java
@@ -1,7 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.it.openbanking.ubi.authenticator;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.agents.rpc.SelectOption;
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.authenticator.CbiThirdPartyAppAuthenticationStep;
@@ -13,8 +17,8 @@ import se.tink.backend.aggregation.nxgen.controllers.authentication.progressive.
 import se.tink.libraries.i18n.Catalog;
 import se.tink.libraries.i18n.LocalizableKey;
 
+@RequiredArgsConstructor
 public class UbiAuthenticationMethodChoiceStep implements AuthenticationStep {
-    private final Catalog catalog;
 
     private static final LocalizableKey DESCRIPTION =
             new LocalizableKey("Do you have UBI Banca mobile app installed on this phone?");
@@ -27,9 +31,10 @@ public class UbiAuthenticationMethodChoiceStep implements AuthenticationStep {
     private static final LocalizableKey PATTERN_ERROR_MSG =
             new LocalizableKey("The value you entered is not valid.");
 
-    public UbiAuthenticationMethodChoiceStep(Catalog catalog) {
-        this.catalog = catalog;
-    }
+    private static final LocalizableKey YES = new LocalizableKey("Yes");
+    private static final LocalizableKey NO = new LocalizableKey("No");
+
+    private final Catalog catalog;
 
     @Override
     public AuthenticationStepResponse execute(AuthenticationRequest request)
@@ -60,7 +65,14 @@ public class UbiAuthenticationMethodChoiceStep implements AuthenticationStep {
                 .maxLength(3)
                 .pattern(PATTERN)
                 .patternError(catalog.getString(PATTERN_ERROR_MSG))
+                .selectOptions(prepareSelectOptions())
                 .build();
+    }
+
+    private List<SelectOption> prepareSelectOptions() {
+        SelectOption selectOptionNoBankApp = new SelectOption(catalog.getString(NO), "n");
+        SelectOption selectOptionBankApp = new SelectOption(catalog.getString(YES), "y");
+        return Arrays.asList(selectOptionNoBankApp, selectOptionBankApp);
     }
 
     private boolean shouldUseRedirectFlow(String chosenMethod) {
