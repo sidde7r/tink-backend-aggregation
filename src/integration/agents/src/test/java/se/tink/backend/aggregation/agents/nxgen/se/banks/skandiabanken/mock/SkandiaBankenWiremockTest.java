60980b5b46713cd9d4f5c131815235bad8d46003
Annica Alstermark <annica.alstermark@tink.se>
2021-02-05 17:01:28 +0100

    feat(Skandia): Add wiremock test
    Don't pay too much attention to the mapped account types, the reason this test is created with so many accounts is to fix the account type mapping.

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/skandiabanken/mock/SkandiaBankenWiremockTest.java
@@ -0,0 +1,38 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.skandiabanken.mock;
+
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonFileParser;
+import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
+import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockrefresh.AgentWireMockRefreshTest;
+import se.tink.libraries.credentials.service.RefreshableItem;
+import se.tink.libraries.enums.MarketCode;
+
+public class SkandiaBankenWiremockTest {
+
+    @Test
+    public void testRefresh() throws Exception {
+
+        // given
+        final String wireMockFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/skandiabanken/mock/resources/skandiabanken-mock-traffic.aap";
+        final String contractFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/skandiabanken/mock/resources/skandiabanken-contract.json";
+
+        final AgentWireMockRefreshTest agentWireMockRefreshTest =
+                AgentWireMockRefreshTest.builder(
+                                MarketCode.SE, "skandiabanken-ssn-bankid", wireMockFilePath)
+                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                        .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
+                        .build();
+
+        final AgentContractEntity expected =
+                new AgentContractEntitiesJsonFileParser()
+                        .parseContractOnBasisOfFile(contractFilePath);
+
+        // when
+        agentWireMockRefreshTest.executeRefresh();
+
+        // then
+        agentWireMockRefreshTest.assertExpectedData(expected);
+    }
+}
