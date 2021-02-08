9ff949812b35c85b3c28bc70b1bb807f0575af25
Jan Ossowski <jan.ossowski@tink.se>
2021-02-08 14:43:44 +0100

    fix(poppankki): disable credit card fetching

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/poppankki/PopPankkiAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/poppankki/PopPankkiAgent.java
@@ -1,9 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.fi.banks.poppankki;
 
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CHECKING_ACCOUNTS;
-import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CREDIT_CARDS;
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.LOANS;
 
+import java.util.Collections;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
 import se.tink.backend.aggregation.agents.contexts.agent.AgentContext;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.samlink.SamlinkAgent;
@@ -11,7 +13,7 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.samlink.S
 import se.tink.backend.aggregation.configuration.signaturekeypair.SignatureKeyPair;
 import se.tink.libraries.credentials.service.CredentialsRequest;
 
-@AgentCapabilities({CHECKING_ACCOUNTS, CREDIT_CARDS, LOANS})
+@AgentCapabilities({CHECKING_ACCOUNTS, LOANS})
 public final class PopPankkiAgent extends SamlinkAgent {
 
     public PopPankkiAgent(
@@ -22,5 +24,15 @@ public final class PopPankkiAgent extends SamlinkAgent {
                 signatureKeyPair,
                 new SamlinkV2Configuration(
                         PopPankkiConstants.Url.BASE, PopPankkiConstants.Header.CLIENT_APP_VALUE));
+    }
+
+    @Override
+    public FetchAccountsResponse fetchCreditCardAccounts() {
+        return new FetchAccountsResponse(Collections.emptyList());
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchCreditCardTransactions() {
+        return new FetchTransactionsResponse(Collections.emptyMap());
     }
 }
