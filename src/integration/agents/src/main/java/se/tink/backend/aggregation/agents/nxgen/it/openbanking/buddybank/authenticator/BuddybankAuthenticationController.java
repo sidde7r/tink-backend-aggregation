bd85ceea3e27ada403b570f1af5fe392f45bea64
Johannes Elgh <johannes.elgh@tink.se>
2021-02-09 11:29:37 +0100

    fix(IT/buddybank): migrate to use SuppInfoController iface
    instead of SupplementalRequester

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/buddybank/authenticator/BuddybankAuthenticationController.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/buddybank/authenticator/BuddybankAuthenticationController.java
@@ -5,28 +5,25 @@ import com.github.rholder.retry.Retryer;
 import com.github.rholder.retry.RetryerBuilder;
 import com.github.rholder.retry.StopStrategies;
 import com.github.rholder.retry.WaitStrategies;
-import java.util.Collections;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import se.tink.backend.agents.rpc.Credentials;
 import se.tink.backend.agents.rpc.Field;
-import se.tink.backend.aggregation.agents.contexts.SupplementalRequester;
 import se.tink.backend.aggregation.agents.nxgen.it.openbanking.buddybank.authenticator.rpc.BuddybankCreateConsentResponse;
 import se.tink.backend.aggregation.agents.utils.berlingroup.consent.ConsentDetailsResponse;
 import se.tink.backend.aggregation.agents.utils.supplementalfields.CommonFields;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.utils.StrongAuthenticationState;
-import se.tink.backend.aggregationcontroller.v1.rpc.enums.CredentialsStatus;
+import se.tink.backend.aggregation.nxgen.controllers.utils.SupplementalInformationController;
 import se.tink.libraries.i18n.Catalog;
 import se.tink.libraries.i18n.LocalizableKey;
 import se.tink.libraries.i18n.LocalizableParametrizedKey;
-import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class BuddybankAuthenticationController implements Authenticator {
     private final BuddybankAuthenticator authenticator;
     private final StrongAuthenticationState strongAuthenticationState;
-    private final SupplementalRequester supplementalRequester;
+    private final SupplementalInformationController supplementalInformationController;
     private final Catalog catalog;
     private static final LocalizableKey DESCRIPTION = new LocalizableKey("description");
     private static final String FIELD_KEY = "name";
@@ -41,11 +38,11 @@ public class BuddybankAuthenticationController implements Authenticator {
     public BuddybankAuthenticationController(
             BuddybankAuthenticator authenticator,
             StrongAuthenticationState strongAuthenticationState,
-            SupplementalRequester supplementalRequester,
+            SupplementalInformationController supplementalInformationController,
             Catalog catalog) {
         this.authenticator = authenticator;
         this.strongAuthenticationState = strongAuthenticationState;
-        this.supplementalRequester = supplementalRequester;
+        this.supplementalInformationController = supplementalInformationController;
         this.catalog = catalog;
     }
 
@@ -53,12 +50,11 @@ public class BuddybankAuthenticationController implements Authenticator {
     public void authenticate(Credentials credentials) {
         BuddybankCreateConsentResponse consentStatus =
                 authenticator.createConsentRequest(strongAuthenticationState.getState());
-        displayVerificationCode(credentials, consentStatus.getPsuMessage());
+        displayVerificationCode(consentStatus.getPsuMessage());
         awaitConsentResponse();
     }
 
-    private void displayVerificationCode(Credentials credentials, String psuMessage) {
-
+    private void displayVerificationCode(String psuMessage) {
         Field field =
                 CommonFields.Information.build(
                         FIELD_KEY,
@@ -66,11 +62,7 @@ public class BuddybankAuthenticationController implements Authenticator {
                         catalog.getString(VALUE),
                         catalog.getString(HELP_TEXT, psuMessage));
 
-        credentials.setSupplementalInformation(
-                SerializationUtils.serializeToString(Collections.singletonList(field)));
-        credentials.setStatus(CredentialsStatus.AWAITING_SUPPLEMENTAL_INFORMATION);
-
-        supplementalRequester.requestSupplementalInformation(credentials, false);
+        supplementalInformationController.askSupplementalInformationAsync(field);
     }
 
     private void awaitConsentResponse() {
