71c662bdb84827ba6bae422ccf7aa037ccc28ef3
Jesús A. Álvarez <jesus.alvarez@tink.se>
2021-02-10 15:33:27 +0100

    fix(handelsbanken): mask authToken from logs
    not strictly necessary, authToken on its own is not sufficient to access data (cookies from the session are needed too)

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/handelsbanken/HandelsbankenSessionStorage.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/handelsbanken/HandelsbankenSessionStorage.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken;
 
+import java.util.Collections;
 import java.util.Optional;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.authenticator.rpc.ApplicationEntryPointResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.fetcher.creditcard.entities.HandelsbankenCreditCard;
@@ -24,7 +25,8 @@ public class HandelsbankenSessionStorage {
 
     public void persist(ApplicationEntryPointResponse applicationEntryPoint) {
         persist(HandelsbankenConstants.Storage.APPLICATION_ENTRY_POINT, applicationEntryPoint);
-        logMasker.mask(applicationEntryPoint.getAuthToken());
+        logMasker.addNewSensitiveValuesToMasker(
+                Collections.singleton(applicationEntryPoint.getAuthToken()));
     }
 
     public Optional<ApplicationEntryPointResponse> applicationEntryPoint() {
