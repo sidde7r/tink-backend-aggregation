a30063829150213b5185eadde8e35113e317995e
Nazri Bin Abdullah <nazri.abdullah@tink.com>
2021-02-11 15:19:27 +0800

    feat(amex-ob): changed storage format

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/amex/AmericanExpressUtils.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/amex/AmericanExpressUtils.java
@@ -1,9 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.amex;
 
 import java.math.BigDecimal;
-import java.util.Date;
 import se.tink.libraries.amount.ExactCurrencyAmount;
-import se.tink.libraries.date.ThreadSafeDateFormat;
 
 public class AmericanExpressUtils {
 
@@ -11,13 +9,8 @@ public class AmericanExpressUtils {
         throw new IllegalStateException("Utility class");
     }
 
-    public static String createAndGetStorageString(Date fromDate, Date toDate) {
-        return String.format(
-                "transactions?%s=%s&%s=%s",
-                "start_date",
-                ThreadSafeDateFormat.FORMATTER_DAILY.format(fromDate),
-                "end_date",
-                ThreadSafeDateFormat.FORMATTER_DAILY.format(toDate));
+    public static String createAndGetStorageString(String key) {
+        return String.format("transactions?%s=%s", "end_date", key);
     }
 
     public static String formatAccountId(String accountId) {
