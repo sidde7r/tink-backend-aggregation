848ce4c5cc70706214641e23a57a22eb2f76cc0d
Nazri Bin Abdullah <nazri.abdullah@tink.com>
2021-02-11 15:20:16 +0800

    feat(amex-ob): changed pagination type

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/amex/dto/TransactionResponseFormatted.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/amex/dto/TransactionResponseFormatted.java
@@ -1,18 +1,27 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.amex.dto;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginatorResponse;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 
-public class TransactionResponseFormatted implements PaginatorResponse {
+public class TransactionResponseFormatted implements TransactionKeyPaginatorResponse<String> {
 
     private List<TransactionDto> transactions;
+    private final LocalDate nextEndDate;
 
-    public TransactionResponseFormatted(List<TransactionDto> transactions) {
+    public TransactionResponseFormatted(List<TransactionDto> transactions, LocalDate nextEndDate) {
         this.transactions = transactions;
+        this.nextEndDate = nextEndDate;
+    }
+
+    @Override
+    public String nextKey() {
+        return nextEndDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 
     @Override
@@ -24,6 +33,6 @@ public class TransactionResponseFormatted implements PaginatorResponse {
 
     @Override
     public Optional<Boolean> canFetchMore() {
-        return Optional.empty();
+        return Optional.of(nextEndDate != null);
     }
 }
