b2833493364810d41976c8b2e97e2fb5e9f830ea
Nazri Bin Abdullah <nazri.abdullah@tink.com>
2021-02-11 15:12:56 +0800

    feat(amex-ob): added response entities

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/amex/dto/StatementPeriodsDto.java
@@ -0,0 +1,14 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.amex.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.util.List;
+import lombok.Data;
+import se.tink.backend.aggregation.annotations.JsonObject;
+
+@JsonObject
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@Data
+public class StatementPeriodsDto {
+    private List<StatementDto> statementPeriods;
+}
