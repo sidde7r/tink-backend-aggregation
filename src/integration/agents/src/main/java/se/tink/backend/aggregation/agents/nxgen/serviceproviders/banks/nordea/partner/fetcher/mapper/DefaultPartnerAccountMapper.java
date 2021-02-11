a92c60a67ef4a9d72a9c5cfd078c912cf8e4949d
Jesús A. Álvarez <jesus.alvarez@tink.se>
2021-02-11 12:12:29 +0100

    refactor(nordeapartner): common mapping to DefaultPartnerAccountMapper

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/nordea/partner/fetcher/mapper/DefaultPartnerAccountMapper.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/nordea/partner/fetcher/mapper/DefaultPartnerAccountMapper.java
@@ -1,6 +1,14 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.fetcher.mapper;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.NordeaPartnerConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.fetcher.transactional.entity.AccountEntity;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.balance.BalanceModule;
@@ -10,9 +18,24 @@ import se.tink.libraries.account.AccountIdentifier;
 import se.tink.libraries.account.AccountIdentifier.Type;
 import se.tink.libraries.amount.ExactCurrencyAmount;
 
+@Slf4j
 public class DefaultPartnerAccountMapper implements NordeaPartnerAccountMapper {
     @Override
     public Optional<TransactionalAccount> toTinkTransactionalAccount(AccountEntity account) {
+        if (Strings.isNullOrEmpty(account.getIban())
+                || Strings.isNullOrEmpty(account.getCategory())
+                || Strings.isNullOrEmpty(account.getCurrency())) {
+            log.warn("Ignoring account with no IBAN, category and/or currency");
+            return Optional.empty();
+        }
+
+        final List<AccountIdentifier> identifiers = getAccountIdentifiers(account);
+        Preconditions.checkState(
+                identifiers.size() != 0, "Account must have at least one identifier");
+        final String formattedAccountNumber =
+                MoreObjects.firstNonNull(
+                        Strings.emptyToNull(account.getDisplayAccountNumber()),
+                        identifiers.get(0).getIdentifier());
         return TransactionalAccount.nxBuilder()
                 .withTypeAndFlagsFrom(
                         NordeaPartnerConstants.TRANSACTIONAL_ACCOUNT_TYPE_MAPPER,
@@ -20,17 +43,26 @@ public class DefaultPartnerAccountMapper implements NordeaPartnerAccountMapper {
                 .withBalance(
                         BalanceModule.of(
                                 ExactCurrencyAmount.of(
-                                        account.getAvailableBalance(), account.getCurrency())))
+                                        MoreObjects.firstNonNull(
+                                                account.getAvailableBalance(), BigDecimal.ZERO),
+                                        account.getCurrency())))
                 .withId(
                         IdModule.builder()
                                 .withUniqueIdentifier(account.getIban())
-                                .withAccountNumber(account.getDisplayAccountNumber())
-                                .withAccountName(account.getNickname())
-                                .addIdentifier(
-                                        AccountIdentifier.create(Type.IBAN, account.getIban()))
+                                .withAccountNumber(identifiers.get(0).getIdentifier())
+                                .withAccountName(
+                                        MoreObjects.firstNonNull(
+                                                Strings.emptyToNull(account.getNickname()),
+                                                formattedAccountNumber))
+                                .addIdentifiers(identifiers)
                                 .build())
                 .addHolderName(account.getHolderName())
                 .setApiIdentifier(account.getAccountId())
                 .build();
+    }
+
+    @JsonIgnore
+    protected List<AccountIdentifier> getAccountIdentifiers(AccountEntity account) {
+        return Collections.singletonList(AccountIdentifier.create(Type.IBAN, account.getIban()));
     }
 }
