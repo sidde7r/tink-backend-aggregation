51fc9ae10589902314f4f1e8e326c66591e665d9
Jesús A. Álvarez <jesus.alvarez@tink.se>
2021-02-17 18:16:39 +0100

    fix(swedbank): exclude pension accounts from savings
    this excludes specific pension accounts from being parsed twice, once as savings and another as investments
    this is done specifically for type IP-SPAR, other types are already ignored from savings because they don't have balance in the engagement overview endpoint
    
    duplicates will need to be deleted from database

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/fetchers/investment/rpc/PensionPortfoliosResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/fetchers/investment/rpc/PensionPortfoliosResponse.java
@@ -3,9 +3,11 @@ package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.fetchers.inve
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.fetchers.investment.entities.OccupationalPensionInsurancesEntity;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.fetchers.investment.entities.PensionInsuranceEntity;
 import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.fetchers.investment.entities.PrivatePensionInsurancesEntity;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.rpc.AbstractAccountEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
@@ -21,15 +23,9 @@ public class PensionPortfoliosResponse {
         return occupationalPensionInsurances;
     }
 
-    /**
-     * Temporary method to know if a user has holdings so that we can look at the responses in the
-     * S3 logs.
-     *
-     * @return True if holdings list is non-empty for any of the pension accounts.
-     */
     @JsonIgnore
-    public boolean hasPensionHoldings() {
-        List<PensionInsuranceEntity> allPensionInsurances = new ArrayList<>();
+    private List<PensionInsuranceEntity> getAllPensionInsurances() {
+        final List<PensionInsuranceEntity> allPensionInsurances = new ArrayList<>();
 
         if (privatePensionInsurances != null) {
             allPensionInsurances.addAll(privatePensionInsurances.getPensionInsurances());
@@ -39,7 +35,30 @@ public class PensionPortfoliosResponse {
             allPensionInsurances.addAll(occupationalPensionInsurances.getPensionInsurances());
         }
 
-        return allPensionInsurances.stream()
+        return allPensionInsurances;
+    }
+
+    /**
+     * Temporary method to know if a user has holdings so that we can look at the responses in the
+     * S3 logs.
+     *
+     * @return True if holdings list is non-empty for any of the pension accounts.
+     */
+    @JsonIgnore
+    public boolean hasPensionHoldings() {
+        return getAllPensionInsurances().stream()
                 .anyMatch(PensionInsuranceEntity::hasNonEmptyHoldingsList);
+    }
+
+    /**
+     * Pension accounts appear both under pension portfolios and engagement overview (as savings
+     * accounts). The the transactional account fetcher should exclude them so they are not
+     * duplicated.
+     */
+    @JsonIgnore
+    public List<String> getPensionAccountNumbers() {
+        return getAllPensionInsurances().stream()
+                .map(AbstractAccountEntity::getFullyFormattedNumber)
+                .collect(Collectors.toList());
     }
 }
