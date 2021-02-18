1292365e386917e29ec5145b6574c1647bb142c3
Ashfaq Haider <ashfaq.haider@tink.se>
2021-02-18 14:47:46 +0100

    chore(InternalStatus): Add internalStatus to constructors

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/exceptions/payment/PaymentAuthorizationCancelledByUserException.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/exceptions/payment/PaymentAuthorizationCancelledByUserException.java
@@ -1,12 +1,14 @@
 package se.tink.backend.aggregation.agents.exceptions.payment;
 
 import se.tink.backend.aggregation.agents.exceptions.entity.ErrorEntity;
+import se.tink.libraries.signableoperation.enums.InternalStatus;
 
 public class PaymentAuthorizationCancelledByUserException extends PaymentAuthorizationException {
     public static final String MESSAGE =
             "Authorisation of payment was cancelled. Please try again.";
 
-    public PaymentAuthorizationCancelledByUserException(ErrorEntity errorEntity) {
-        super(errorEntity, MESSAGE);
+    public PaymentAuthorizationCancelledByUserException(
+            ErrorEntity errorEntity, InternalStatus internalStatus) {
+        super(errorEntity, MESSAGE, internalStatus);
     }
 }
--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/exceptions/payment/PaymentAuthorizationTimeOutException.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/exceptions/payment/PaymentAuthorizationTimeOutException.java
@@ -1,19 +1,21 @@
 package se.tink.backend.aggregation.agents.exceptions.payment;
 
 import se.tink.backend.aggregation.agents.exceptions.entity.ErrorEntity;
+import se.tink.libraries.signableoperation.enums.InternalStatus;
 
 public class PaymentAuthorizationTimeOutException extends PaymentAuthorizationException {
     public static final String MESSAGE = "Authorisation of payment timed out. Please try again.";
 
-    public PaymentAuthorizationTimeOutException(String message) {
-        super(message);
+    public PaymentAuthorizationTimeOutException(String message, InternalStatus internalStatus) {
+        super(message, internalStatus);
     }
 
-    public PaymentAuthorizationTimeOutException() {
-        this(MESSAGE);
+    public PaymentAuthorizationTimeOutException(InternalStatus internalStatus) {
+        this(MESSAGE, internalStatus);
     }
 
-    public PaymentAuthorizationTimeOutException(ErrorEntity errorEntity) {
-        super(errorEntity, MESSAGE);
+    public PaymentAuthorizationTimeOutException(
+            ErrorEntity errorEntity, InternalStatus internalStatus) {
+        super(errorEntity, MESSAGE, internalStatus);
     }
 }
