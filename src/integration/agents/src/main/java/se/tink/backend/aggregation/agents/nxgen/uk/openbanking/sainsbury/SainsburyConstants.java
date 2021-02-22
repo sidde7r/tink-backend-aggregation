eb0116b513902b034eee29a7efcce37846b01165
Pavlo Yeromenko <pavlo.yeromenko@tink.com>
2021-02-22 23:00:14 +0100

    chore(uk/sainsbury): business agent OB setup with Refresh test

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/sainsbury/SainsburyConstants.java
@@ -0,0 +1,10 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.sainsbury;
+
+public final class SainsburyConstants {
+    public static final String ORGANISATION_ID = "0015800001ZEc2JAAT";
+
+    public static final String AIS_API_URL =
+            "https://ob.sainsburysbank.co.uk/open-banking/v3.1/aisp";
+    public static final String WELL_KNOWN_URL =
+            "https://iam.sainsburysbank.co.uk/sso/oauth2/realms/root/realms/general/.well-known/openid-configuration";
+}
