bd43c31be39dc7ebf3eb420d2d2196f6da89c763
Andreas Olausson <andreas.olausson@tink.se>
2021-02-22 16:06:04 +0100

    feat(danskebank): add iban as identifier
    Since 'fetchAccountDetails' requires a account number as parameter, a HashMap was created to connect the account number with responding AccountDetailsReponse. This response is passed as a parameter which is used for creating the iban identifier.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/danskebank/fetchers/DanskeBankTransactionalAccountFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/danskebank/fetchers/DanskeBankTransactionalAccountFetcher.java
@@ -3,20 +3,26 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskeba
 import com.google.common.collect.ImmutableList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import org.apache.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.DanskeBankApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.DanskeBankConfiguration;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.DanskeBankPredicates;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.fetchers.mapper.AccountEntityMapper;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.fetchers.rpc.AccountDetailsRequest;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.fetchers.rpc.AccountDetailsResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.fetchers.rpc.AccountEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.fetchers.rpc.ListAccountsRequest;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.fetchers.rpc.ListAccountsResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
 
 public class DanskeBankTransactionalAccountFetcher implements AccountFetcher<TransactionalAccount> {
 
@@ -43,6 +49,13 @@ public class DanskeBankTransactionalAccountFetcher implements AccountFetcher<Tra
                         ListAccountsRequest.createFromLanguageCode(
                                 configuration.getLanguageCode()));
 
+        List<AccountEntity> accountEntities = listAccounts.getAccounts();
+
+        Map<String, AccountDetailsResponse> accountDetails = new HashMap<>();
+        for (AccountEntity accountEntity : accountEntities) {
+            accountDetails.put(accountEntity.getAccountNoExt(), fetchAccountDetails(accountEntity));
+        }
+
         logDuplicatedAccountNoExt(listAccounts.getAccounts());
 
         return ImmutableList.<TransactionalAccount>builder()
@@ -50,15 +63,43 @@ public class DanskeBankTransactionalAccountFetcher implements AccountFetcher<Tra
                 .addAll(
                         accountEntityMapper.toTinkCheckingAccounts(
                                 configuration.getCheckingAccountTypes(),
-                                listAccounts.getAccounts()))
+                                listAccounts.getAccounts(),
+                                accountDetails))
                 .addAll(
                         accountEntityMapper.toTinkSavingsAccounts(
-                                configuration, listAccounts.getAccounts()))
+                                configuration, listAccounts.getAccounts(), accountDetails))
                 .build();
+    }
+
+    private AccountDetailsResponse fetchAccountDetails(AccountEntity accountEntity) {
+        // Using "EN" as languageCode to be consistent with fetchLoanAccountDetails().
+        try {
+            return apiClient.fetchAccountDetails(
+                    new AccountDetailsRequest(accountEntity.getAccountNoInt(), "EN"));
+        } catch (HttpResponseException e) {
+            // Sometimes we receive 500 response that has a body of AccountDetailsResponse
+            if (e.getResponse().getStatus() == HttpStatus.SC_INTERNAL_SERVER_ERROR
+                    && e.getResponse().hasBody()) {
+                try {
+                    return e.getResponse().getBody(AccountDetailsResponse.class);
+                } catch (RuntimeException re) {
+                    log.info("Failed to map exception body into AccountDetailsResponse. ", e);
+                }
+            }
+            log.info("Failed to fetch account details. ", e);
+        }
+        return new AccountDetailsResponse();
     }
 
     private List<TransactionalAccount> logAndGetTransactionalAccountsOfUnknownType(
             ListAccountsResponse listAccounts) {
+        List<AccountEntity> accountEntities = listAccounts.getAccounts();
+
+        Map<String, AccountDetailsResponse> accountDetails = new HashMap<>();
+        for (AccountEntity accountEntity : accountEntities) {
+            accountDetails.put(accountEntity.getAccountNoExt(), fetchAccountDetails(accountEntity));
+        }
+
         return listAccounts.getAccounts().stream()
                 .filter(DanskeBankPredicates.CREDIT_CARDS.negate())
                 .filter(accountEntity -> !accountEntity.isLoanAccount())
@@ -76,7 +117,11 @@ public class DanskeBankTransactionalAccountFetcher implements AccountFetcher<Tra
                                         "Account: apiIdentifier = {}, accountProduct = {}",
                                         accountEntity.getAccountNoInt(),
                                         accountEntity.getAccountProduct()))
-                .map(accountEntityMapper::toUnknownAccount)
+                .map(
+                        accountEntity ->
+                                accountEntityMapper.toUnknownAccount(
+                                        accountEntity,
+                                        accountDetails.get(accountEntity.getAccountNoExt())))
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .collect(Collectors.toList());
