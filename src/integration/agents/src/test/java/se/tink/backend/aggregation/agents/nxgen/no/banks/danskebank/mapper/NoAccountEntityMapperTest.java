5590843942e5e63efab57f3bcfd61283b5634a45
Andreas Olausson <andreas.olausson@tink.se>
2021-02-23 10:35:24 +0100

    test(danskebank/NO): update test for iban identifier
    Updating the NoAccountEntityMapperTest since adding iban as identifer requires the AccountDetailsResponse to be passed as a parameter.

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/no/banks/danskebank/mapper/NoAccountEntityMapperTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/no/banks/danskebank/mapper/NoAccountEntityMapperTest.java
@@ -7,7 +7,9 @@ import static org.mockito.Mockito.mock;
 
 import org.junit.Before;
 import org.junit.Test;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.DanskeBankApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.DanskeBankConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.fetchers.rpc.AccountDetailsResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.danskebank.fetchers.rpc.AccountEntity;
 import se.tink.backend.aggregation.compliance.account_capabilities.AccountCapabilities;
 import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
@@ -17,26 +19,34 @@ import se.tink.libraries.serialization.utils.SerializationUtils;
 public class NoAccountEntityMapperTest {
 
     private static final String BBAN = "bban";
+    private static final String IBAN = "iban";
+    private static final String IBAN_NUMBER = "NO6402401234567";
     private static final String BANK_IDENTIFIER = "bankIdentifier";
     private static final String ACCOUNT_EXT_NO = "12345678901";
     private static final String ACCOUNT_INT_NO = "1234567890";
 
+    private DanskeBankApiClient apiClient;
     private DanskeBankConfiguration configuration;
     private NoAccountEntityMapper noAccountEntityMapper;
     private AccountEntity accountEntity;
+    private AccountDetailsResponse accountDetailsResponse;
 
     @Before
     public void setUp() {
+        apiClient = mock(DanskeBankApiClient.class);
         noAccountEntityMapper = new NoAccountEntityMapper();
         configuration = getDanskeBankConfiguration();
         accountEntity = getAccountEntity();
+        accountDetailsResponse = getAccountDetailsReponse();
     }
 
     @Test
     public void checkingAccountShouldHaveBbanAsUniqueIdentifier() {
         // given & when
         TransactionalAccount result =
-                noAccountEntityMapper.toCheckingAccount(accountEntity).orElse(null);
+                noAccountEntityMapper
+                        .toCheckingAccount(accountEntity, accountDetailsResponse)
+                        .orElse(null);
 
         // then
         assert result != null;
@@ -45,9 +55,23 @@ public class NoAccountEntityMapperTest {
 
     private void assertResultHasProperFieldsValues(TransactionalAccount result) {
         assertThat(result.getIdModule().getUniqueId()).isEqualTo(ACCOUNT_EXT_NO);
-        assertThat(result.getIdentifiers().size()).isEqualTo(1);
-        assertThat(result.getIdentifiers().get(0).getIdentifier()).isEqualTo(ACCOUNT_EXT_NO);
-        assertThat(result.getIdentifiers().get(0).getType().toString()).isEqualTo(BBAN);
+        assertThat(result.getIdentifiers().size()).isEqualTo(2);
+        assertThat(
+                        result.getIdentifiers().stream()
+                                .filter(id -> id.getIdentifier().equals(ACCOUNT_EXT_NO))
+                                .filter(id -> id.getType().toString().equals(BBAN))
+                                .findFirst()
+                                .get()
+                                .getIdentifier())
+                .isNotEmpty();
+        assertThat(
+                        result.getIdentifiers().stream()
+                                .filter(id -> id.getIdentifier().equals(IBAN_NUMBER))
+                                .filter(id -> id.getType().toString().equals(IBAN))
+                                .findFirst()
+                                .get()
+                                .getIdentifier())
+                .isNotEmpty();
         assertThat(result.getAccountNumber()).isEqualTo(ACCOUNT_EXT_NO);
         assertThat(result.getFromTemporaryStorage(BANK_IDENTIFIER)).isEqualTo(ACCOUNT_INT_NO);
         assertThat(result.getApiIdentifier()).isEqualTo(ACCOUNT_INT_NO);
@@ -57,7 +81,9 @@ public class NoAccountEntityMapperTest {
     public void savingsAccountShouldHaveBbanAsUniqueIdentifier() {
         // given
         TransactionalAccount result =
-                noAccountEntityMapper.toSavingsAccount(configuration, accountEntity).orElse(null);
+                noAccountEntityMapper
+                        .toSavingsAccount(configuration, accountEntity, accountDetailsResponse)
+                        .orElse(null);
 
         // then
         assert result != null;
@@ -104,5 +130,24 @@ public class NoAccountEntityMapperTest {
                         + "    \"balance\": -1234.45\n"
                         + "}",
                 AccountEntity.class);
+    }
+
+    private AccountDetailsResponse getAccountDetailsReponse() {
+        return SerializationUtils.deserializeFromString(
+                "{"
+                        + "\"traceId\":null,"
+                        + "\"statusCode\":200,"
+                        + "\"responseCode\":200,"
+                        + "\"feeAmount\":0,"
+                        + "\"feeCurrency\":\"EUR\","
+                        + "\"accountType\":\"Danske account\","
+                        + "\"accountNumber\":null,"
+                        + "\"registrationNumber\":null,"
+                        + "\"iban\":\""
+                        + IBAN_NUMBER
+                        + "\","
+                        + "\"bic\":\"DABANO22"
+                        + "\"}",
+                AccountDetailsResponse.class);
     }
 }
