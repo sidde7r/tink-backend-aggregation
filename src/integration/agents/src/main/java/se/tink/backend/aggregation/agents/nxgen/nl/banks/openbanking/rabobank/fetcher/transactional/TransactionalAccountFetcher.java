1a6b81cce9e06e0b7b6436940d1ab9848eafefbe
Krzysztof Korolczuk <krzysztof.korolczuk@tink.se>
2021-03-03 08:53:11 +0100

    refactor(nl/rabobank): remove usage of deprecated builder

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/rabobank/fetcher/transactional/TransactionalAccountFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/rabobank/fetcher/transactional/TransactionalAccountFetcher.java
@@ -4,8 +4,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.rabobank.RabobankApiClient;
 import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.rabobank.fetcher.rpc.TransactionalAccountsResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
@@ -13,7 +11,6 @@ import se.tink.backend.aggregation.nxgen.core.account.transactional.Transactiona
 
 public class TransactionalAccountFetcher implements AccountFetcher<TransactionalAccount> {
 
-    private static final Logger logger = LoggerFactory.getLogger(TransactionalAccountFetcher.class);
     private final RabobankApiClient apiClient;
 
     public TransactionalAccountFetcher(final RabobankApiClient apiClient) {
@@ -22,11 +19,12 @@ public class TransactionalAccountFetcher implements AccountFetcher<Transactional
 
     @Override
     public Collection<TransactionalAccount> fetchAccounts() {
-
         return Optional.ofNullable(apiClient.fetchAccounts())
                 .map(TransactionalAccountsResponse::getAccounts).orElseGet(Collections::emptyList)
                 .stream()
                 .map(acc -> acc.toCheckingAccount(apiClient.getBalance(acc.getResourceId())))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
                 .collect(Collectors.toList());
     }
 }
