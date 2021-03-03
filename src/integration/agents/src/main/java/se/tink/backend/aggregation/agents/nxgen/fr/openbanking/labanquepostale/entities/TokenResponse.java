96f193fa62adf281c824b18b35a0a6eb0c3a9cda
Krzysztof Korolczuk <krzysztof.korolczuk@tink.se>
2021-03-03 13:30:17 +0100

    refactor(OpenBankProject): remove unused legacy code

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/labanquepostale/entities/TokenResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/labanquepostale/entities/TokenResponse.java
@@ -1,4 +1,4 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.openbankproject.authenticator.rpc;
+package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.labanquepostale.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import se.tink.backend.aggregation.annotations.JsonObject;
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/openbankproject/transactionalaccount/rpc/FetchAccountResponse.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.openbankproject.transactionalaccount.rpc;
-
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.openbankproject.transactionalaccount.entities.accounts.AccountEntity;
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class FetchAccountResponse extends AccountEntity {}
