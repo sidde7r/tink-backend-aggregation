621018351af6127040eee2cf4da38a8f17d4b26a
Paulina Słowik <paulina.slowik@tink.com>
2021-03-03 15:19:27 +0100

    chore(hellobank): solving problem with retrieving transactions
    [ITE-2235]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/hellobank/HelloBankAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/hellobank/HelloBankAgent.java
@@ -6,7 +6,11 @@ import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capa
 import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.fetcher.transactionalaccount.CbiGlobeTransactionalAccountFetcher;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcherController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionPagePaginationController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
 
 @AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS})
 public final class HelloBankAgent extends CbiGlobeAgent {
@@ -14,5 +18,19 @@ public final class HelloBankAgent extends CbiGlobeAgent {
     @Inject
     public HelloBankAgent(AgentComponentProvider agentComponentProvider) {
         super(agentComponentProvider);
+    }
+
+    @Override
+    protected TransactionalAccountRefreshController getTransactionalAccountRefreshController() {
+        final CbiGlobeTransactionalAccountFetcher accountFetcher =
+                CbiGlobeTransactionalAccountFetcher.createFromBooked(apiClient, persistentStorage);
+
+        return new TransactionalAccountRefreshController(
+                metricRefreshController,
+                updateController,
+                accountFetcher,
+                new TransactionFetcherController<>(
+                        transactionPaginationHelper,
+                        new TransactionPagePaginationController<>(accountFetcher, 1)));
     }
 }
