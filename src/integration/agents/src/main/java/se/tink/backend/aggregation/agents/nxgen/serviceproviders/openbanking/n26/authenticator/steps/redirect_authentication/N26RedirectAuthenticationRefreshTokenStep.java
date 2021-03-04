513ac58b510c1e124303a65f5a3bd26996c851d2
Lukasz Binkiewicz <lukasz.binkiewicz@tink.se>
2021-03-04 15:12:02 +0100

    fix(N26): Changing handling of session expired

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/n26/authenticator/steps/redirect_authentication/N26RedirectAuthenticationRefreshTokenStep.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/n26/authenticator/steps/redirect_authentication/N26RedirectAuthenticationRefreshTokenStep.java
@@ -1,11 +1,10 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.n26.authenticator.steps.redirect_authentication;
 
 import lombok.extern.slf4j.Slf4j;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.n26.authenticator.steps.fetch_authorization_url.N26FetchAuthorizationUrlStep;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.n26.authenticator.steps.validate_consent.N26AutoAuthValidateConsentStep;
-import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.AgentAuthenticationProcessStepIdentifier;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.request.AgentProceedNextStepAuthenticationRequest;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.result.AgentAuthenticationResult;
+import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.result.AgentFailedAuthenticationResult;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.result.AgentProceedNextStepAuthenticationResult;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.result.AgentSucceededAuthenticationResult;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.steps.AgentAuthenticationProcessStep;
@@ -13,6 +12,7 @@ import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.redirect.RedirectAuthenticationRefreshTokenStep;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.redirect.RedirectRefreshTokenCall;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.common.authentication.RefreshableAccessTokenValidator;
+import se.tink.backend.aggregation.agentsplatform.agentsframework.error.SessionExpiredError;
 
 @Slf4j
 public class N26RedirectAuthenticationRefreshTokenStep
@@ -37,12 +37,9 @@ public class N26RedirectAuthenticationRefreshTokenStep
         if (authenticationResult instanceof AgentSucceededAuthenticationResult) {
             return new AgentProceedNextStepAuthenticationResult(
                     AgentAuthenticationProcessStep.identifier(N26AutoAuthValidateConsentStep.class),
-                    authenticationProcessRequest.getAuthenticationPersistedData());
+                    authenticationResult.getAuthenticationPersistedData());
         } else if (authenticationResult instanceof AgentProceedNextStepAuthenticationResult) {
-            return new AgentProceedNextStepAuthenticationResult(
-                    AgentAuthenticationProcessStepIdentifier.of(
-                            N26FetchAuthorizationUrlStep.class.getSimpleName()),
-                    authenticationProcessRequest.getAuthenticationPersistedData());
+            return new AgentFailedAuthenticationResult(new SessionExpiredError(), null);
         }
         log.error("Unknown state after refreshing access token");
         throw new IllegalStateException(
