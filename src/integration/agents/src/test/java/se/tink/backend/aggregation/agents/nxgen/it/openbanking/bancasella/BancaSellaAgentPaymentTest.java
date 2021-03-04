c468c75c275511cb2e9258de1676c939df838438
vishwanath sharma <vishwanath.sharma@tink.se>
2021-03-04 17:25:32 +0100

    feat(bancasella): manual agent test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/bancasella/BancaSellaAgentPaymentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/bancasella/BancaSellaAgentPaymentTest.java
@@ -13,6 +13,10 @@ import se.tink.libraries.amount.ExactCurrencyAmount;
 import se.tink.libraries.payment.rpc.Creditor;
 import se.tink.libraries.payment.rpc.Debtor;
 import se.tink.libraries.payment.rpc.Payment;
+import se.tink.libraries.transfer.enums.RemittanceInformationType;
+import se.tink.libraries.transfer.rpc.ExecutionRule;
+import se.tink.libraries.transfer.rpc.Frequency;
+import se.tink.libraries.transfer.rpc.PaymentServiceType;
 import se.tink.libraries.transfer.rpc.RemittanceInformation;
 
 @Ignore
@@ -41,12 +45,32 @@ public class BancaSellaAgentPaymentTest {
         manager.before();
         creditorDebtorManager.before();
 
-        builder.build().testTinkLinkPayment(createRealDomesticPayment());
+        builder.build().testTinkLinkPayment(createRealDomesticPayment().build());
     }
 
-    private Payment createRealDomesticPayment() {
+    @Test
+    public void testRecurringPayments() throws Exception {
+        manager.before();
+        creditorDebtorManager.before();
+
+        builder.build().testTinkLinkPayment(createRealDomesticRecurringPayment().build());
+    }
+
+    private Payment.Builder createRealDomesticRecurringPayment() {
+        Payment.Builder recurringPayment = createRealDomesticPayment();
+        recurringPayment.withPaymentServiceType(PaymentServiceType.PERIODIC);
+        recurringPayment.withFrequency(Frequency.MONTHLY);
+        recurringPayment.withStartDate(LocalDate.now().plusDays(1));
+        recurringPayment.withEndDate(LocalDate.now().plusMonths(3));
+        recurringPayment.withExecutionRule(ExecutionRule.PRECEEDING);
+
+        return recurringPayment;
+    }
+
+    private Payment.Builder createRealDomesticPayment() {
         RemittanceInformation remittanceInformation = new RemittanceInformation();
         remittanceInformation.setValue("BancaSella");
+        remittanceInformation.setType(RemittanceInformationType.UNSTRUCTURED);
         AccountIdentifier creditorAccountIdentifier =
                 new IbanIdentifier(
                         creditorDebtorManager.get(BancaSellaAgentPaymentTest.Arg.CREDITOR_ACCOUNT));
@@ -67,8 +91,7 @@ public class BancaSellaAgentPaymentTest {
                 .withExactCurrencyAmount(amount)
                 .withExecutionDate(executionDate)
                 .withCurrency(currency)
-                .withRemittanceInformation(remittanceInformation)
-                .build();
+                .withRemittanceInformation(remittanceInformation);
     }
 
     private enum Arg implements ArgumentManager.ArgumentManagerEnum {
