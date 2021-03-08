ba3aa01006205b5e679dd5613d89c07f5b623402
Lukasz Binkiewicz <lukasz.binkiewicz@tink.se>
2021-03-08 12:08:12 +0100

    fix(FR/labanquepostale): Handle timeout response

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/labanquepostale/authenticator/LaBanquePostaleAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/labanquepostale/authenticator/LaBanquePostaleAuthenticator.java
@@ -1,9 +1,14 @@
 package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.labanquepostale.authenticator;
 
+import java.util.Map;
+import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceException;
+import se.tink.backend.aggregation.agents.exceptions.errors.ThirdPartyAppError;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.labanquepostale.LaBanquePostaleApiClient;
+import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.labanquepostale.LaBanquePostaleConstants.ErrorMessages;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.authenticator.BerlinGroupAuthenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2Authenticator;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.constants.OAuth2Constants.CallbackParams;
 import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
 import se.tink.backend.aggregation.nxgen.http.url.URL;
 import se.tink.backend.aggregation.nxgen.storage.SessionStorage;
@@ -29,5 +34,14 @@ public class LaBanquePostaleAuthenticator extends BerlinGroupAuthenticator
     @Override
     public OAuth2Token exchangeAuthorizationCode(String code) throws BankServiceException {
         return apiClient.getToken(code);
+    }
+
+    @Override
+    public void handleSpecificCallbackDataError(Map<String, String> callbackData)
+            throws AuthenticationException {
+        String errorType = callbackData.get(CallbackParams.ERROR);
+        if (ErrorMessages.TIME_OUT.equals(errorType)) {
+            throw ThirdPartyAppError.TIMED_OUT.exception();
+        }
     }
 }
