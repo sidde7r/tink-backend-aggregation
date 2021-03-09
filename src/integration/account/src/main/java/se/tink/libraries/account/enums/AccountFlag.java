32f5bcc7f3fa7a9575b9785c66db984ccab6cdfd
Andreas Olausson <andreas.olausson@tink.se>
2021-03-09 11:57:40 +0100

    feat(AccountFlag): add DEPOT_CASH_BALANCE as account flag

--- src/integration/account/src/main/java/se/tink/libraries/account/enums/AccountFlag.java
+++ src/integration/account/src/main/java/se/tink/libraries/account/enums/AccountFlag.java
@@ -3,5 +3,9 @@ package se.tink.libraries.account.enums;
 public enum AccountFlag {
     BUSINESS,
     MANDATE,
-    PSD2_PAYMENT_ACCOUNT
+    PSD2_PAYMENT_ACCOUNT,
+    // DEPOT_CASH_BALANCE - A flag added for displaying deposit accounts to investment accounts.
+    // Those deposit accounts are usually parsed as savings accounts.
+    // This is introduced due to a request from Avanza.
+    DEPOT_CASH_BALANCE
 }
