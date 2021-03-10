24fd9e0674c15bbcb765851d96143d7d338f23be
Ashfaq Haider <ashfaq.haider@tink.se>
2021-03-10 16:27:11 +0100

    feat(Swedbank OB): add Transfer and PIS caps

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/swedbank/SwedbankAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/swedbank/SwedbankAgent.java
@@ -1,18 +1,30 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.swedbank;
 
-import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CHECKING_ACCOUNTS;
-import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.SAVINGS_ACCOUNTS;
-
 import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
+import se.tink.backend.aggregation.agents.agentcapabilities.AgentPisCapability;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModules;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.swedbank.SwedbankBaseAgent;
+import se.tink.backend.aggregation.client.provider_configuration.rpc.Capability;
+import se.tink.backend.aggregation.client.provider_configuration.rpc.PisCapability;
 import se.tink.backend.aggregation.eidassigner.QsealcSigner;
 import se.tink.backend.aggregation.eidassigner.module.QSealcSignerModuleRSASHA256;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 
 @AgentDependencyModules(modules = QSealcSignerModuleRSASHA256.class)
-@AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS})
+@AgentCapabilities({
+    Capability.CHECKING_ACCOUNTS,
+    Capability.SAVINGS_ACCOUNTS,
+    Capability.TRANSFERS
+})
+@AgentPisCapability(
+        capabilities = {
+            PisCapability.PIS_SE_BANK_TRANSFERS,
+            PisCapability.PIS_SE_BG,
+            PisCapability.PIS_SE_PG,
+            PisCapability.PIS_FUTURE_DATE
+        },
+        markets = {"SE"})
 public class SwedbankAgent extends SwedbankBaseAgent {
 
     @Inject
