0d0f08f96d0ee2ed20519eb508a1f00f47335693
vishwanath sharma <vishwanath.sharma@tink.se>
2021-03-11 13:17:31 +0100

    fix(bpm): recurring payment test fix
    Fixed BPM Agent test for recurring payments.
    Refactored code to be more optimized and removed lots of code duplication.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/executor/payment/entities/InstructedAmountEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/executor/payment/entities/InstructedAmountEntity.java
@@ -1,35 +1,16 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.executor.payment.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.backend.aggregation.nxgen.controllers.payment.PaymentRequest;
 
 @JsonObject
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class InstructedAmountEntity {
 
     private String currency;
     private String amount;
-
-    @JsonIgnore
-    public static InstructedAmountEntity of(PaymentRequest paymentRequest) {
-        return new InstructedAmountEntity(
-                paymentRequest.getPayment().getExactCurrencyAmount().getCurrencyCode(),
-                String.valueOf(
-                        paymentRequest.getPayment().getExactCurrencyAmount().getDoubleValue()));
-    }
-
-    public InstructedAmountEntity() {}
-
-    public InstructedAmountEntity(String currency, String amount) {
-        this.currency = currency;
-        this.amount = amount;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public String getAmount() {
-        return amount;
-    }
 }
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/executor/payment/rpc/CreateRecurringPaymentRequest.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/executor/payment/rpc/CreateRecurringPaymentRequest.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.executor.payment.rpc;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,11 @@ import se.tink.backend.aggregation.annotations.JsonObject;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CreateRecurringPaymentRequest extends CreatePaymentRequest {
-    String startDate;
-    String endDate;
-    String frequency;
-    String executionRule;
+    private String startDate;
+    private String endDate;
+    private String frequency;
+    private String executionRule;
+    private String dayOfExecution;
 }
