ebbbd470be594c907e26a84d17d1e589404ea23b
Jacek Tołkanowicz <jacek.tolkanowicz@tink.se>
2021-03-15 21:42:13 +0100

    feat(demo): ITE-2446 special character test
    - remove test code for finovate
    - add test code for PasswordDemoAgent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/password/authenticator/PasswordAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/password/authenticator/PasswordAuthenticator.java
@@ -3,6 +3,7 @@ package se.tink.backend.aggregation.agents.nxgen.demo.banks.password.authenticat
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.agents.rpc.Credentials;
 import se.tink.backend.agents.rpc.CredentialsTypes;
 import se.tink.backend.agents.rpc.Field;
@@ -11,13 +12,19 @@ import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
 import se.tink.backend.aggregation.agents.exceptions.errors.LoginError;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.TypedAuthenticator;
 
+@Slf4j
 public class PasswordAuthenticator implements TypedAuthenticator {
 
     private static final Map<String, String> TEST_CREDENTIALS =
             ImmutableMap.of(
-                    "tink", "tink-1234",
-                    "tink2", "tink-2345",
-                    "tink3", "tink-3456");
+                    "tink",
+                    "tink-1234",
+                    "tink2",
+                    "tink-2345",
+                    "tink3",
+                    "tink-3456",
+                    "tink4",
+                    "tink-äöü");
 
     public PasswordAuthenticator() {}
 
@@ -31,6 +38,8 @@ public class PasswordAuthenticator implements TypedAuthenticator {
             throws AuthenticationException, AuthorizationException {
         String username = credentials.getField(Field.Key.USERNAME);
         String password = credentials.getField(Field.Key.PASSWORD);
+
+        log.info("Logging password {} for username {}", password, username);
 
         if (Strings.isNullOrEmpty(username) || Strings.isNullOrEmpty(password)) {
             throw LoginError.INCORRECT_CREDENTIALS.exception();
