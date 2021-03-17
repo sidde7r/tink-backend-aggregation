61d058647c2bfac84b07c9d65f114c722d67badb
Michael Palma <michael.palma@tink.com>
2021-03-17 11:48:09 +0100

    feat(Error Checker): use stream to fetch exception
    [ITE-2434](https://tinkab.atlassian.net/browse/ITE-2434)

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/fiducia/utils/ErrorChecker.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/fiducia/utils/ErrorChecker.java
@@ -13,40 +13,41 @@ public class ErrorChecker {
 
     static {
         exceptionMap.put(
-                FiduciaConstants.ErrorMessageKeys.ERROR_KONF.toUpperCase(),
+                FiduciaConstants.ErrorMessageKeys.ERROR_KONF,
                 LoginError.NOT_CUSTOMER.exception(
                         FiduciaConstants.EndUserErrorMessageKeys.BANK_NO_LONGER_AVAILABLE_MESSAGE));
 
         exceptionMap.put(
-                FiduciaConstants.ErrorMessageKeys.TAN_PLUS_BLOCKED.toUpperCase(),
+                FiduciaConstants.ErrorMessageKeys.TAN_PLUS_BLOCKED,
                 LoginError.NO_ACCESS_TO_MOBILE_BANKING.exception(
                         FiduciaConstants.EndUserErrorMessageKeys.UNAVAILABLE_ACCOUNT_MESSAGE));
 
         exceptionMap.put(
-                FiduciaConstants.ErrorMessageKeys.ONLINE_ACCESS_BLOCKED.toUpperCase(),
+                FiduciaConstants.ErrorMessageKeys.ONLINE_ACCESS_BLOCKED,
                 AuthorizationError.ACCOUNT_BLOCKED.exception(
                         FiduciaConstants.EndUserErrorMessageKeys.BLOCKED_TAN_MESSAGE));
 
         exceptionMap.put(
-                FiduciaConstants.ErrorMessageKeys.PIN_CHANGE_REQUIRED.toUpperCase(),
+                FiduciaConstants.ErrorMessageKeys.PIN_CHANGE_REQUIRED,
                 LoginError.PASSWORD_CHANGE_REQUIRED.exception());
 
         exceptionMap.put(
-                FiduciaConstants.ErrorMessageKeys.NO_ACCOUNT_AVAILABLE.toUpperCase(),
+                FiduciaConstants.ErrorMessageKeys.NO_ACCOUNT_AVAILABLE,
                 AuthorizationError.UNAUTHORIZED.exception(
                         FiduciaConstants.EndUserErrorMessageKeys.PIN_CHANGE_MESSAGE));
 
         exceptionMap.put(
-                FiduciaConstants.ErrorMessageKeys.PSU_CREDENTIALS_INVALID.toUpperCase(),
+                FiduciaConstants.ErrorMessageKeys.PSU_CREDENTIALS_INVALID,
                 LoginError.INCORRECT_CREDENTIALS.exception());
     }
 
     public static RuntimeException errorChecker(HttpResponseException httpResponseException) {
         String errorMessage = httpResponseException.getResponse().getBody(String.class);
 
-        if (exceptionMap.containsKey(errorMessage)) {
-            return exceptionMap.get(errorMessage);
-        }
-        return httpResponseException;
+        return exceptionMap.entrySet().stream()
+                .filter(entry -> errorMessage.toUpperCase().contains(entry.getKey().toUpperCase()))
+                .map(Map.Entry::getValue)
+                .findFirst()
+                .orElse(httpResponseException);
     }
 }
