4cd975b4f9be1c89f5ad601db947a7243b86aec3
Pavlo Yeromenko <pavlo.yeromenko@tink.com>
2021-03-19 14:13:47 +0100

    feat(UK/CaterAllen): business agent setup

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/caterallen/CaterAllenConstants.java
@@ -0,0 +1,9 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.caterallen;
+
+public final class CaterAllenConstants {
+    public static final String ORGANISATION_ID = "0015800001HQQrUAAX";
+    public static final String AIS_API_URL =
+            "https://developer.caterallen.co.uk/open-banking/v3.1/aisp";
+    public static final String WELL_KNOWN_URL =
+            "https://developer.caterallen.co.uk/.well-known/openid-configuration";
+}
---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/caterallen/CaterAllenAgentTest.java
@@ -0,0 +1,19 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.caterallen;
+
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+
+public class CaterAllenAgentTest {
+
+    @Test
+    public void testRefresh() throws Exception {
+        new AgentIntegrationTest.Builder("uk", "uk-caterallen-ob")
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(true)
+                .expectLoggedIn(false)
+                .setAppId("tink")
+                .setFinancialInstitutionId("2345494d2df5400a9118dcbed7d0cb9e")
+                .build()
+                .testRefresh();
+    }
+}
