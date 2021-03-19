354cddc9a4fefd0adfd5c6903162c771fd0f59a9
Jan Sameryt <jan.sameryt@tink.com>
2021-03-19 11:46:59 +0100

    fix(IT/cbi): package location for filters subpackage
    [ITE-2465]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/filter/CbiGlobeRetryFilter.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/filter/CbiGlobeRetryFilter.java
@@ -1,21 +1,21 @@
-package src.integration.agents.src.main.java.se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.filter;
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.filter;
 
 import org.apache.http.HttpStatus;
 import se.tink.backend.aggregation.nxgen.http.filter.filters.randomretry.AbstractRandomRetryFilter;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
 
-public class CBIGlobeRetryFilter extends AbstractRandomRetryFilter {
+public class CbiGlobeRetryFilter extends AbstractRandomRetryFilter {
 
     /**
      * @param maxNumRetries Number of additional retries to be performed.
      * @param retrySleepMilliseconds Time in milliseconds that will be spent sleeping between
      */
-    public CBIGlobeRetryFilter(int maxNumRetries, long retrySleepMilliseconds) {
+    public CbiGlobeRetryFilter(int maxNumRetries, long retrySleepMilliseconds) {
         super(maxNumRetries, retrySleepMilliseconds);
     }
 
     @Override
-    protected boolean shouldRetry(
-            se.tink.backend.aggregation.nxgen.http.response.HttpResponse response) {
+    protected boolean shouldRetry(HttpResponse response) {
         return response.getStatus() == HttpStatus.SC_BAD_GATEWAY
                 || response.getStatus() == HttpStatus.SC_INTERNAL_SERVER_ERROR
                 || response.getStatus() == HttpStatus.SC_GATEWAY_TIMEOUT
