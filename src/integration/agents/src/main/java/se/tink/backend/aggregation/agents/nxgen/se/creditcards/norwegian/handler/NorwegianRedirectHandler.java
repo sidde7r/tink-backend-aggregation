2bcf28a61fd37e244b3128d77ba7341d92a00b8b
Niclas Albinsson <niclas.albinsson@tink.se>
2021-03-22 15:10:02 +0100

    chore(norwegian): add logs

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/norwegian/handler/NorwegianRedirectHandler.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/norwegian/handler/NorwegianRedirectHandler.java
@@ -1,23 +1,24 @@
 package se.tink.backend.aggregation.agents.nxgen.se.creditcards.norwegian.handler;
 
-import java.net.URI;
 import java.util.List;
 import org.apache.http.Header;
 import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpRequestWrapper;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.cookie.Cookie;
 import org.apache.http.cookie.CookieOrigin;
 import org.apache.http.cookie.MalformedCookieException;
 import org.apache.http.impl.cookie.BestMatchSpec;
 import org.apache.http.protocol.HttpContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.http.redirect.handler.RedirectHandler;
 
 public class NorwegianRedirectHandler extends RedirectHandler {
     TinkHttpClient client;
+    private static final Logger log = LoggerFactory.getLogger(NorwegianRedirectHandler.class);
 
     public NorwegianRedirectHandler(TinkHttpClient client) {
         this.client = client;
@@ -26,13 +27,17 @@ public class NorwegianRedirectHandler extends RedirectHandler {
     @Override
     public boolean allowRedirect(HttpRequest request, HttpResponse response, HttpContext context) {
 
-        URI uri = ((HttpRequestWrapper) request).getURI();
         HttpHost targetHost = ((HttpClientContext) context).getTargetHost();
+        log.info("Target Uri: " + targetHost.getHostName());
+        log.info("Target Port: " + targetHost.getPort());
+        log.info("RequestLine URI: " + request.getRequestLine().getUri());
+        log.info("Cookie path: " + ((HttpClientContext) context).getCookieOrigin().getPath());
+        log.info("Cookie host: " + ((HttpClientContext) context).getCookieOrigin().getHost());
         CookieOrigin cookieOrigin =
                 new CookieOrigin(
                         targetHost.getHostName(),
                         getPort(targetHost),
-                        uri.getPath(),
+                        request.getRequestLine().getUri(),
                         targetHost.getSchemeName().equalsIgnoreCase("https"));
         Header[] headers = response.getHeaders("Set-cookie");
         if (headers != null) {
