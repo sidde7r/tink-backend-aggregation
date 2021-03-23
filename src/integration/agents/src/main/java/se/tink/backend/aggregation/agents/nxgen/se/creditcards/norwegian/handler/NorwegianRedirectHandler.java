bb2889f0a7197da9121227bc6a12d456f660d576
Niclas Albinsson <niclas.albinsson@tink.se>
2021-03-23 11:51:47 +0100

    chore(norwegian): more cookie logs

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/norwegian/handler/NorwegianRedirectHandler.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/norwegian/handler/NorwegianRedirectHandler.java
@@ -1,6 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.se.creditcards.norwegian.handler;
 
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.Header;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
@@ -10,14 +11,12 @@ import org.apache.http.cookie.CookieOrigin;
 import org.apache.http.cookie.MalformedCookieException;
 import org.apache.http.impl.cookie.BestMatchSpec;
 import org.apache.http.protocol.HttpContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.http.redirect.handler.RedirectHandler;
 
+@Slf4j
 public class NorwegianRedirectHandler extends RedirectHandler {
     TinkHttpClient client;
-    private static final Logger log = LoggerFactory.getLogger(NorwegianRedirectHandler.class);
 
     public NorwegianRedirectHandler(TinkHttpClient client) {
         this.client = client;
@@ -40,13 +39,25 @@ public class NorwegianRedirectHandler extends RedirectHandler {
                             + cookie.getPath()
                             + " GetSecure : "
                             + cookie.isSecure());
+
             for (Header header : headers) {
                 try {
 
                     List<Cookie> parse = new BestMatchSpec().parse(header, cookieOrigin);
+                    for (Cookie co : parse) {
+                        log.info(
+                                "Get Cookie Name: "
+                                        + co.getName()
+                                        + (co.getExpiryDate() != null
+                                                ? co.getExpiryDate().toString()
+                                                : "No Expiry Date"));
+                    }
                     client.addCookie(parse.toArray(new Cookie[0]));
+                    Cookie cookie1 = client.getCookies().get(client.getCookies().size() - 1);
+                    logStuff(cookie1);
                 } catch (MalformedCookieException e) {
                     // NOP
+                    log.error("Error: " + e.getMessage());
                 }
             }
         }
@@ -62,5 +73,15 @@ public class NorwegianRedirectHandler extends RedirectHandler {
             return 443;
         }
         throw new IllegalStateException("Illegal schema");
+    }
+
+    private void logStuff(Cookie cookie1) {
+        log.info(
+                "Cookie Name: "
+                        + cookie1.getName()
+                        + " Get expire date: "
+                        + (cookie1.getExpiryDate() != null
+                                ? cookie1.getExpiryDate().toString()
+                                : "No Expiry Date"));
     }
 }
