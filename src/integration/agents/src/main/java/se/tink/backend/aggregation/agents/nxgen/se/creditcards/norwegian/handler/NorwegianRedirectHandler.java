d6312276bd02fdaa7de2350dc4343c31e781e981
Niclas Albinsson <niclas.albinsson@tink.se>
2021-03-23 09:10:24 +0100

    chore(norwegian): add logs for cookie

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/norwegian/handler/NorwegianRedirectHandler.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/norwegian/handler/NorwegianRedirectHandler.java
@@ -10,11 +10,14 @@ import org.apache.http.cookie.CookieOrigin;
 import org.apache.http.cookie.MalformedCookieException;
 import org.apache.http.impl.cookie.BestMatchSpec;
 import org.apache.http.protocol.HttpContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.http.redirect.handler.RedirectHandler;
 
 public class NorwegianRedirectHandler extends RedirectHandler {
     TinkHttpClient client;
+    private static final Logger log = LoggerFactory.getLogger(NorwegianRedirectHandler.class);
 
     public NorwegianRedirectHandler(TinkHttpClient client) {
         this.client = client;
@@ -25,9 +28,22 @@ public class NorwegianRedirectHandler extends RedirectHandler {
         CookieOrigin cookie = ((HttpClientContext) context).getCookieOrigin();
         Header[] headers = response.getHeaders("Set-cookie");
         if (headers != null) {
+            CookieOrigin cookieOrigin =
+                    new CookieOrigin(
+                            cookie.getHost(), getPort(cookie), cookie.getPath(), cookie.isSecure());
+            log.info(
+                    "CookieHost: "
+                            + cookie.getHost()
+                            + " GetPort: "
+                            + getPort(cookie)
+                            + " GetPath "
+                            + cookie.getPath()
+                            + " GetSecure : "
+                            + cookie.isSecure());
             for (Header header : headers) {
                 try {
-                    List<Cookie> parse = new BestMatchSpec().parse(header, cookie);
+
+                    List<Cookie> parse = new BestMatchSpec().parse(header, cookieOrigin);
                     client.addCookie(parse.toArray(new Cookie[0]));
                 } catch (MalformedCookieException e) {
                     // NOP
@@ -35,5 +51,16 @@ public class NorwegianRedirectHandler extends RedirectHandler {
             }
         }
         return true;
+    }
+
+    private int getPort(CookieOrigin host) {
+
+        if (host.getPort() > 0) {
+            return host.getPort();
+        }
+        if (host.isSecure()) {
+            return 443;
+        }
+        throw new IllegalStateException("Illegal schema");
     }
 }
