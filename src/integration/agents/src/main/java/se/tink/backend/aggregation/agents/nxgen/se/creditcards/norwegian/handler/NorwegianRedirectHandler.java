a625714b28cde3805242a82ad960de12862d9839
Niclas Albinsson <niclas.albinsson@tink.se>
2021-03-25 10:17:09 +0100

    chore(norwegian): remove logs

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/norwegian/handler/NorwegianRedirectHandler.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/norwegian/handler/NorwegianRedirectHandler.java
@@ -1,23 +1,22 @@
 package se.tink.backend.aggregation.agents.nxgen.se.creditcards.norwegian.handler;
 
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.http.Header;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.protocol.HttpClientContext;
 import org.apache.http.cookie.Cookie;
-import org.apache.http.cookie.CookieOrigin;
 import org.apache.http.cookie.MalformedCookieException;
 import org.apache.http.impl.cookie.RFC2965Spec;
 import org.apache.http.protocol.HttpContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.http.redirect.handler.RedirectHandler;
 
-@Slf4j
 public class NorwegianRedirectHandler extends RedirectHandler {
     TinkHttpClient client;
-    private String noDate = "No Expiry Date";
+    private static final Logger log = LoggerFactory.getLogger(NorwegianRedirectHandler.class);
 
     public NorwegianRedirectHandler(TinkHttpClient client) {
         this.client = client;
@@ -25,77 +24,20 @@ public class NorwegianRedirectHandler extends RedirectHandler {
 
     @Override
     public boolean allowRedirect(HttpRequest request, HttpResponse response, HttpContext context) {
-
         Header[] headers = response.getHeaders("Set-cookie");
         if (headers != null) {
-            CookieOrigin cookieOrigin = ((HttpClientContext) context).getCookieOrigin();
-
-            log.info(
-                    "CookieHost: "
-                            + cookieOrigin.getHost()
-                            + " GetPort: "
-                            + getPort(cookieOrigin)
-                            + " GetPath "
-                            + cookieOrigin.getPath()
-                            + " GetSecure : "
-                            + cookieOrigin.isSecure());
-
             for (Header header : headers) {
                 try {
 
-                    List<Cookie> parse = new RFC2965Spec().parse(header, cookieOrigin);
-                    for (Cookie co : parse) {
-                        log.info(
-                                "Get Cookie Name: "
-                                        + co.getName()
-                                        + (co.getExpiryDate() != null
-                                                ? co.getExpiryDate().toString()
-                                                : noDate));
-                    }
-
+                    List<Cookie> parse =
+                            new RFC2965Spec()
+                                    .parse(header, ((HttpClientContext) context).getCookieOrigin());
                     client.addCookie(parse.toArray(new Cookie[0]));
-                    showAllCookies(client.getCookies());
-                    Cookie cookie1 = client.getCookies().get(client.getCookies().size() - 1);
-                    logStuff(cookie1);
                 } catch (MalformedCookieException e) {
-                    // NOP
                     log.error("Error: " + e.getMessage());
                 }
             }
         }
         return true;
-    }
-
-    private void showAllCookies(List<Cookie> cookies) {
-        String str = "Cookies : ";
-        for (Cookie cookie : cookies) {
-            str += cookie.getName() + " Expire date = " + getExpireDate(cookie) + " : ";
-        }
-        log.info(str);
-    }
-
-    private String getExpireDate(Cookie cookie) {
-        return cookie.getExpiryDate() != null ? cookie.getExpiryDate().toString() : noDate;
-    }
-
-    private int getPort(CookieOrigin host) {
-
-        if (host.getPort() > 0) {
-            return host.getPort();
-        }
-        if (host.isSecure()) {
-            return 443;
-        }
-        throw new IllegalStateException("Illegal schema");
-    }
-
-    private void logStuff(Cookie cookie1) {
-        log.info(
-                "Cookie Name: "
-                        + cookie1.getName()
-                        + " Get expire date: "
-                        + (cookie1.getExpiryDate() != null
-                                ? cookie1.getExpiryDate().toString()
-                                : noDate));
     }
 }
