847b1ae0f0fadcf1ef7efc8aff7826702a192951
Nazri Bin Abdullah <nazri.abdullah@tink.com>
2021-03-31 01:36:11 +0800

    chore(berlingroup): renamed balance mapper class

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/berlingroup/BalanceMapper.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/berlingroup/BalanceMapper.java
@@ -1,5 +1,19 @@
 package se.tink.backend.aggregation.agents.utils.berlingroup;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Ordering;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.ComparatorUtils;
+import se.tink.libraries.amount.ExactCurrencyAmount;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import static se.tink.backend.aggregation.agents.utils.berlingroup.BalanceType.CLOSING_AVAILABLE;
 import static se.tink.backend.aggregation.agents.utils.berlingroup.BalanceType.CLOSING_BOOKED;
 import static se.tink.backend.aggregation.agents.utils.berlingroup.BalanceType.EXPECTED;
@@ -8,19 +22,6 @@ import static se.tink.backend.aggregation.agents.utils.berlingroup.BalanceType.I
 import static se.tink.backend.aggregation.agents.utils.berlingroup.BalanceType.INTERIM_BOOKED;
 import static se.tink.backend.aggregation.agents.utils.berlingroup.BalanceType.OPENING_BOOKED;
 import static se.tink.backend.aggregation.agents.utils.berlingroup.BalanceType.PREVIOUSLY_CLOSED_BOOKED;
-
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Ordering;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.ComparatorUtils;
-import se.tink.libraries.amount.ExactCurrencyAmount;
 
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -38,20 +39,19 @@ public class BerlinGroupBalanceMapper {
     private static final List<BalanceType> AVAILABLE_BALANCE_PREFERRED_TYPES =
             ImmutableList.of(INTERIM_AVAILABLE, EXPECTED, FORWARD_AVAILABLE);
 
-    public static ExactCurrencyAmount getBookedBalance(List<BalanceEntity> balances) {
+    public static ExactCurrencyAmount getBookedBalance(List<BalanceMappable> balances) {
         // Booked balance is required, so we cannot continue with no way to determine it.
         if (balances.isEmpty()) {
             throw new IllegalArgumentException(
                     "Cannot determine booked balance from empty list of balances.");
         }
-        logUnknownTypes(balances, "getBookedBalance");
 
         // We are only interested in balances without included credit limit, to determine the true
         // booked balance on the account.
-        Optional<BalanceEntity> balanceEntity =
+        Optional<BalanceMappable> balanceEntity =
                 pickByPriority(
                         balances.stream()
-                                .filter(x -> !Boolean.TRUE.equals(x.getCreditLimitIncluded()))
+                                .filter(b -> !b.isCreditLimitIncluded())
                                 .collect(Collectors.toList()),
                         BOOKED_BALANCE_PREFERRED_TYPES);
 
@@ -65,27 +65,27 @@ public class BerlinGroupBalanceMapper {
     }
 
     public static Optional<ExactCurrencyAmount> getAvailableBalance(List<BalanceEntity> balances) {
-        logUnknownTypes(balances, "getAvailableBalance");
 
         // We are only interested in balances without included credit limit
-        Optional<BalanceEntity> balanceEntity =
+        Optional<BalanceMappable> balanceEntity =
                 pickByPriority(
                         balances.stream()
-                                .filter(x -> !Boolean.TRUE.equals(x.getCreditLimitIncluded()))
+                                .filter(b -> !b.isCreditLimitIncluded())
                                 .collect(Collectors.toList()),
                         AVAILABLE_BALANCE_PREFERRED_TYPES);
-        return balanceEntity.map(BalanceEntity::toTinkAmount);
+        return balanceEntity.map(BalanceMappable::toTinkAmount);
     }
 
-    public static Optional<ExactCurrencyAmount> getCreditLimit(List<BalanceEntity> balances) {
+    public static Optional<ExactCurrencyAmount> getCreditLimit(List<BalanceMappable> balances) {
         // In this method we only log the unknown types, but proceed with all of the balances.
         // Credit limit should appear the same in all types of balances, so there is no one type
         // that is better than other
-        logUnknownTypes(balances, "getCreditLimit");
 
-        Map<String, List<BalanceEntity>> balancesGroupedByType =
-                balances.stream().collect(Collectors.groupingBy(BalanceEntity::getBalanceType));
-        List<List<BalanceEntity>> listOfMultipleBalancesPerType =
+        Map<BalanceType, List<BalanceMappable>> balancesGroupedByType =
+                balances.stream()
+                        .filter(b -> b.getBalanceType().isPresent())
+                        .collect(Collectors.groupingBy(b -> b.getBalanceType().get()));
+        List<List<BalanceMappable>> listOfMultipleBalancesPerType =
                 balancesGroupedByType.values().stream()
                         .filter(x -> x.size() > 1)
                         .collect(Collectors.toList());
@@ -103,7 +103,7 @@ public class BerlinGroupBalanceMapper {
     }
 
     private static boolean isProperPairForDeterminingCreditLimit(
-            List<BalanceEntity> balancesOfSameType) {
+            List<BalanceMappable> balancesOfSameType) {
         if (balancesOfSameType.size() != 2) {
             log.info(
                     "Found more than two balances of type {}. Skipping trying to determine creditLimit out of them.",
@@ -111,48 +111,29 @@ public class BerlinGroupBalanceMapper {
             return false;
         }
         // No flag in balance object == flag set to false. Based on berlin group docs.
-        Boolean firstFlag = balancesOfSameType.get(0).getCreditLimitIncluded();
-        Boolean secondFlag = balancesOfSameType.get(1).getCreditLimitIncluded();
+        Boolean firstFlag = balancesOfSameType.get(0).isCreditLimitIncluded();
+        Boolean secondFlag = balancesOfSameType.get(1).isCreditLimitIncluded();
         return (Boolean.TRUE.equals(firstFlag) && !Boolean.TRUE.equals(secondFlag))
                 || (Boolean.TRUE.equals(secondFlag) && !Boolean.TRUE.equals(firstFlag));
     }
 
-    private static ExactCurrencyAmount calculateCreditLimit(List<BalanceEntity> balances) {
+    private static ExactCurrencyAmount calculateCreditLimit(List<BalanceMappable> balances) {
         // This method assumes that provided balances list is proper, ie. was earlier passed through
         // `isProperPairForDeterminingCreditLimit`
         return balances.get(0).toTinkAmount().subtract(balances.get(1).toTinkAmount()).abs();
     }
 
-    private static void logUnknownTypes(List<BalanceEntity> balances, String sourceOperation) {
-        List<String> unknownTypes =
-                balances.stream()
-                        .map(BalanceEntity::getBalanceType)
-                        .filter(x -> !BalanceType.isKnownType(x))
-                        .collect(Collectors.toList());
-        if (!unknownTypes.isEmpty()) {
-            log.warn(
-                    "Found balance entities with unknown balanceType during "
-                            + sourceOperation
-                            + ": "
-                            + String.join(", ", unknownTypes));
-        }
-    }
-
-    private static Optional<BalanceEntity> pickByPriority(
-            List<BalanceEntity> balances, List<BalanceType> priorityList) {
-        Comparator<BalanceEntity> priorityComparator =
+    private static Optional<BalanceMappable> pickByPriority(
+            List<BalanceMappable> balances, List<BalanceType> priorityList) {
+        Comparator<BalanceMappable> priorityComparator =
                 ComparatorUtils.transformedComparator(
-                        Ordering.explicit(priorityList),
-                        input -> BalanceType.findByStringType(input.getBalanceType()).get());
+                        Ordering.explicit(priorityList), input -> input.getBalanceType().get());
 
         // Filter out the unknown types, so the priority pick can behave as it should.
         // Comparator defined above would not be able to handle the unknown types.
         return balances.stream()
-                .filter(x -> BalanceType.isKnownType(x.getBalanceType()))
-                .filter(
-                        b ->
-                                priorityList.contains(
-                                        BalanceType.findByStringType(b.getBalanceType()).get()))
+                .filter(x -> x.getBalanceType().isPresent())
+                .filter(b -> priorityList.contains(b.getBalanceType().get()))
                 .min(priorityComparator);
     }
 }
