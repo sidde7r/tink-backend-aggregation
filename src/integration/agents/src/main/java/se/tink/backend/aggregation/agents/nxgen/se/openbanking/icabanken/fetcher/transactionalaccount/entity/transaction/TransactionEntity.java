80a2ce58f32ecaa81399e07154f0965f345948f7
Annica Alstermark <annica.alstermark@tink.se>
2021-04-13 14:26:31 +0200

    feat(ICA/OB): Extend trx parsing for trx history product

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/icabanken/fetcher/transactionalaccount/entity/transaction/TransactionEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/icabanken/fetcher/transactionalaccount/entity/transaction/TransactionEntity.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import java.time.LocalDate;
 import java.util.List;
+import se.tink.backend.aggregation.agents.models.TransactionExternalSystemIdType;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.nxgen.core.transaction.AggregationTransaction.Builder;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
+import se.tink.backend.aggregation.nxgen.core.transaction.TransactionDates;
+import se.tink.libraries.chrono.AvailableDateInformation;
 
 @JsonObject
 public class TransactionEntity {
@@ -26,11 +30,24 @@ public class TransactionEntity {
     private String transactionText;
 
     public Transaction toTinkTransaction(boolean isPending) {
-        return Transaction.builder()
-                .setPending(isPending)
-                .setDate(bookingDate)
-                .setAmount(transactionAmount.toAmount())
-                .setDescription(transactionText)
+        Builder builder =
+                Transaction.builder()
+                        .setPending(isPending)
+                        .setDate(bookingDate)
+                        .setAmount(transactionAmount.toAmount())
+                        .setDescription(transactionText)
+                        .setTransactionDates(getTinkTransactionDates())
+                        .addExternalSystemIds(
+                                TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID,
+                                entryReference);
+
+        return (Transaction) builder.build();
+    }
+
+    private TransactionDates getTinkTransactionDates() {
+        return TransactionDates.builder()
+                .setValueDate(new AvailableDateInformation().setDate(valueDate))
+                .setBookingDate(new AvailableDateInformation().setDate(bookingDate))
                 .build();
     }
 }
