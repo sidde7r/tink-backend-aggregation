85a2914792ffc28556a6cb830fb633ece3581526
Bartlomiej Swieczak <bartlomiej.swieczak@tink.se>
2021-04-14 15:12:23 +0200

    feat(uk/eidas-proxy): Delete unused fields in UkOpenBankingConfiguration

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/ais/base/module/UkOpenBankingEUFlowModule.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/ais/base/module/UkOpenBankingEUFlowModule.java
@@ -3,28 +3,19 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.uk
 import com.google.inject.AbstractModule;
 import com.google.inject.Inject;
 import com.google.inject.Provides;
+import com.google.inject.Scopes;
 import com.google.inject.Singleton;
 import se.tink.backend.aggregation.agents.agent.Agent;
 import se.tink.backend.aggregation.agents.contexts.CompositeAgentContext;
 import se.tink.backend.aggregation.agents.module.agentclass.AgentClass;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.UkOpenBankingFlowFacade;
-import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
 import se.tink.backend.aggregation.eidassigner.identity.EidasIdentity;
-import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 
 public final class UkOpenBankingEUFlowModule extends AbstractModule {
 
-    @Inject
-    @Singleton
-    @Provides
-    public UkOpenBankingFlowFacade ukOpenBankingFlowFacade(
-            CompositeAgentContext context,
-            AgentComponentProvider agentComponentProvider,
-            AgentsServiceConfiguration agentsServiceConfiguration,
-            EidasIdentity eidasIdentity) {
-        return new EidasProxyFlow(
-                        context, agentComponentProvider, agentsServiceConfiguration, eidasIdentity)
-                .get();
+    @Override
+    protected void configure() {
+        bind(UkOpenBankingFlowFacade.class).toProvider(EidasProxyFlow.class).in(Scopes.SINGLETON);
     }
 
     @Inject
