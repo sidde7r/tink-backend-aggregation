b2c6b915ae67e70b61b2d7bc4950a1b94c289674
Annica Alstermark <annica.alstermark@tink.se>
2021-04-15 14:09:57 +0200

    feat(SEBCards/OB): Extend trx parsing for trx history product

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbase/fetcher/cardaccounts/entities/TransactionEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbase/fetcher/cardaccounts/entities/TransactionEntity.java
@@ -1,17 +1,28 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbase.fetcher.cardaccounts.entities;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.google.common.base.Strings;
-import java.util.Date;
+import java.time.LocalDate;
+import java.util.Objects;
+import se.tink.backend.aggregation.agents.models.TransactionExternalSystemIdType;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCard;
+import se.tink.backend.aggregation.nxgen.core.transaction.AggregationTransaction.Builder;
 import se.tink.backend.aggregation.nxgen.core.transaction.CreditCardTransaction;
+import se.tink.backend.aggregation.nxgen.core.transaction.TransactionDates;
+import se.tink.backend.aggregation.utils.json.deserializers.LocalDateDeserializer;
+import se.tink.libraries.chrono.AvailableDateInformation;
 
 @JsonObject
 public class TransactionEntity {
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private Date bookingDate;
+    /** Description: Booking date expressed as ISODate in the YYYY-MM-DD format */
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate bookingDate;
+
+    /** Description: Value date expressed as ISODate in the YYYY-MM-DD format */
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    private LocalDate valueDate;
 
     private String cardAcceptorCity;
     private String cardAcceptorCountryCode;
@@ -25,23 +36,40 @@ public class TransactionEntity {
     private TransactionAmountEntity transactionAmount;
     private String transactionDetails;
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private Date valueDate;
-
     public String getMaskedPan() {
         return Strings.emptyToNull(maskedPan);
     }
+
     public String getNameOnCard() {
         return Strings.emptyToNull(nameOnCard);
     }
 
     public CreditCardTransaction toTinkTransaction(boolean isPending) {
-        return CreditCardTransaction.builder()
-                .setAmount(transactionAmount.getTinkAmount())
-                .setCreditCard(CreditCard.create(getNameOnCard(), getMaskedPan()))
-                .setDate(valueDate)
-                .setDescription(transactionDetails)
-                .setPending(isPending)
-                .build();
+        Builder builder =
+                CreditCardTransaction.builder()
+                        .setAmount(transactionAmount.getTinkAmount())
+                        .setCreditCard(CreditCard.create(getNameOnCard(), getMaskedPan()))
+                        .setPending(isPending)
+                        .setDate(valueDate)
+                        .setDescription(transactionDetails)
+                        .setTransactionDates(getTinkTransactionDates())
+                        .addExternalSystemIds(
+                                TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID,
+                                cardTransactionId)
+                        .setProprietaryFinancialInstitutionType(proprietaryBankTransactionCode);
+
+        return (CreditCardTransaction) builder.build();
+    }
+
+    private TransactionDates getTinkTransactionDates() {
+        TransactionDates.Builder builder = TransactionDates.builder();
+
+        builder.setValueDate(new AvailableDateInformation().setDate(valueDate));
+
+        if (Objects.nonNull(bookingDate)) {
+            builder.setBookingDate(new AvailableDateInformation().setDate(bookingDate));
+        }
+
+        return builder.build();
     }
 }
