305a71308451bbba0f0eb800c74169c77ba41e6f
Nazri Bin Abdullah <nazri.abdullah@tink.com>
2021-04-15 09:57:22 +0200

    feat(nl-knab-ob): added realtime balance

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/knab/fetcher/rpc/BalancesResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/knab/fetcher/rpc/BalancesResponse.java
@@ -1,26 +1,14 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.openbanking.knab.fetcher.rpc;
 
 import java.util.List;
-import org.apache.commons.collections4.ListUtils;
+import lombok.Getter;
 import se.tink.backend.aggregation.agents.nxgen.nl.openbanking.knab.fetcher.entity.AccountEntity;
 import se.tink.backend.aggregation.agents.nxgen.nl.openbanking.knab.fetcher.entity.BalanceEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.libraries.amount.ExactCurrencyAmount;
 
+@Getter
 @JsonObject
 public class BalancesResponse {
     private AccountEntity account;
     private List<BalanceEntity> balances;
-
-    public ExactCurrencyAmount getBalance() {
-        return ListUtils.emptyIfNull(balances).stream()
-                .filter(BalanceEntity::isInterimBooked)
-                .findAny()
-                .map(balanceEntity -> balanceEntity.getBalanceAmount().toTinkAmount())
-                .orElseGet(this::getDefaultAmount);
-    }
-
-    private ExactCurrencyAmount getDefaultAmount() {
-        return ExactCurrencyAmount.zero(account.getCurrency());
-    }
 }
