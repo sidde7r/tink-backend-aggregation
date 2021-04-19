6ce1838bad20dfc7110512b142aacec167d254fd
Daniel Obrębski <daniel.obrebski@tink.se>
2021-04-19 15:40:37 +0200

    fix(DNB): small fix for no_accounts state
    no_accounts state is returned on 403 not 400.
    
    [ITE-1774]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/dnb/DnbApiClient.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/dnb/DnbApiClient.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.no.openbanking.dnb;
 
+import java.util.Arrays;
 import java.util.Date;
 import javax.ws.rs.core.MediaType;
 import lombok.AllArgsConstructor;
@@ -55,7 +56,7 @@ public class DnbApiClient {
     }
 
     private void handleKnownCreateConsentErrors(HttpResponseException httpException) {
-        if (httpException.getResponse().getStatus() == 400
+        if (Arrays.asList(400, 403).contains(httpException.getResponse().getStatus())
                 && httpException.getResponse().hasBody()) {
             String body = httpException.getResponse().getBody(String.class);
             if (body.contains(ErrorMessages.WRONG_PSUID)) {
