9b956387f3938e973f721acb3d17193b7971e7e9
Annica Alstermark <annica.alstermark@tink.se>
2021-04-20 16:56:51 +0200

    chore(ICS/OB): Rate limit to 0.05rps
    An attempt to reduce the access exceeded errors we get every BG refresh.

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/ratelimit/RateLimitedExecutorService.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/ratelimit/RateLimitedExecutorService.java
@@ -34,6 +34,7 @@ public class RateLimitedExecutorService implements Managed {
                     .put("nxgen.dk.banks.danskebank.DanskeBankDKAgent", 0.05)
                     .put("nxgen.no.banks.danskebank.DanskeBankNOAgent", 0.05)
                     .put("nxgen.se.openbanking.nordea.NordeaSeAgent", 0.05)
+                    .put("nxgen.nl.creditcards.ICS.ICSAgent", 0.05)
                     .build();
 
     private final MetricRegistry metricRegistry;
