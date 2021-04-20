47b10b8e7d8e610bf1b918cea07615aeb02b1d6a
Andreas Olausson <andreas.olausson@tink.se>
2021-04-20 14:51:23 +0200

    log(avanza): remove logging of holder name

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/brokers/avanza/authenticator/AvanzaBankIdAuthenticator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/brokers/avanza/authenticator/AvanzaBankIdAuthenticator.java
@@ -157,13 +157,7 @@ public class AvanzaBankIdAuthenticator implements BankIdAuthenticator<BankIdInit
         maskAuthCredentialsFromLogging(bankIdCompleteResponse);
         putAuthCredentialsInAuthSessionStorage(bankIdCompleteResponse);
 
-        try {
-            // The try/catch is only used temporarily to log if we fail at fetching the holder name.
-            // This should be removed at the latest 2020-05-01.
-            storeHolderNameIfAvailable();
-        } catch (Exception e) {
-            LOGGER.info("Could not fetch holder name with the exception: ", e);
-        }
+        storeHolderNameIfAvailable();
     }
 
     private void storeHolderNameIfAvailable() {
