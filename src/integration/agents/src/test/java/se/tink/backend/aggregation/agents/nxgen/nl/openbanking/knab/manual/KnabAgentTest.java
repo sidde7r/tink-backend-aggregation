9603ccf455cc2b442120894753800e668be20908
Farrokh Bolandi <farrokh.bolandi@tink.se>
2021-04-21 15:36:06 +0200

    refactor(nl/knab): proper test packaging

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/knab/manual/KnabAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/knab/manual/KnabAgentTest.java
@@ -1,4 +1,4 @@
-package se.tink.backend.aggregation.agents.nxgen.nl.openbanking.knab;
+package se.tink.backend.aggregation.agents.nxgen.nl.openbanking.knab.manual;
 
 import org.junit.AfterClass;
 import org.junit.Before;
