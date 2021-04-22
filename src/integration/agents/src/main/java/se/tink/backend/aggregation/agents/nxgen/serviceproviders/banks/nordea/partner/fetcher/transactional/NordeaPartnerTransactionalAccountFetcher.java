64b7356fddc65db6a793ad58ff4fbc77f806a9cd
Jesús A. Álvarez <jesus.alvarez@tink.se>
2021-04-22 12:00:06 +0200

    feat(nordeapartner): ignore error 502 when fetching transactions
    generates non-actionable alerts, and doesn't impact refresh success rate

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/nordea/partner/fetcher/transactional/NordeaPartnerTransactionalAccountFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/nordea/partner/fetcher/transactional/NordeaPartnerTransactionalAccountFetcher.java
@@ -3,6 +3,8 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.p
 import java.time.LocalDate;
 import java.util.Collection;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceException;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.NordeaPartnerApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.fetcher.mapper.NordeaPartnerAccountMapper;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.nordea.partner.fetcher.transactional.model.NordeaPartnerTransactionalPaginatorResponse;
@@ -13,6 +15,7 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.paginat
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 
 @RequiredArgsConstructor
+@Slf4j
 public class NordeaPartnerTransactionalAccountFetcher
         implements AccountFetcher<TransactionalAccount>,
                 TransactionKeyPaginator<TransactionalAccount, String> {
@@ -29,12 +32,18 @@ public class NordeaPartnerTransactionalAccountFetcher
     @Override
     public TransactionKeyPaginatorResponse<String> getTransactionsFor(
             TransactionalAccount account, String key) {
-        final AccountTransactionsResponse response =
-                apiClient.fetchAccountTransaction(account.getApiIdentifier(), key);
+        try {
+            final AccountTransactionsResponse response =
+                    apiClient.fetchAccountTransaction(account.getApiIdentifier(), key);
 
-        return new NordeaPartnerTransactionalPaginatorResponse(
-                response.getTinkTransactions(),
-                response.getContinuationKey(),
-                response.canFetchMore(dateLimit));
+            return new NordeaPartnerTransactionalPaginatorResponse(
+                    response.getTinkTransactions(),
+                    response.getContinuationKey(),
+                    response.canFetchMore(dateLimit));
+        } catch (BankServiceException e) {
+            // don't raise non-actionable alert for frequent 502 and 503 errors
+            log.warn("Ignoring bank service error when fetching transactions: " + e.getMessage());
+            return NordeaPartnerTransactionalPaginatorResponse.createEmpty();
+        }
     }
 }
