0ae6e1fe9dd28fa4b390b18a7d463624b2e8c139
Pavlo Yeromenko <pavlo.yeromenko@tink.com>
2021-04-23 15:36:44 +0200

    feat(UK/Creation): setup business agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/creation/CreationBusinessAgent.java
@@ -0,0 +1,49 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.creation;
+
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CHECKING_ACCOUNTS;
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CREDIT_CARDS;
+
+import com.google.inject.Inject;
+import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
+import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForDecoupledMode;
+import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForProductionMode;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.UkOpenBankingBaseAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.UkOpenBankingFlowFacade;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.entities.AccountOwnershipType;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.interfaces.UkOpenBankingAis;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.interfaces.UkOpenBankingAisConfig;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.module.UkOpenBankingFlowModule;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.module.UkOpenBankingLocalKeySignerModuleForDecoupledMode;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.v31.UkOpenBankingAisConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.v31.UkOpenBankingV31Ais;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
+
+@AgentDependencyModulesForProductionMode(modules = UkOpenBankingFlowModule.class)
+@AgentDependencyModulesForDecoupledMode(
+        modules = UkOpenBankingLocalKeySignerModuleForDecoupledMode.class)
+@AgentCapabilities({CHECKING_ACCOUNTS, CREDIT_CARDS})
+public class CreationBusinessAgent extends UkOpenBankingBaseAgent {
+
+    private static final UkOpenBankingAisConfig aisConfig;
+
+    static {
+        aisConfig =
+                UkOpenBankingAisConfiguration.builder()
+                        .withAllowedAccountOwnershipType(AccountOwnershipType.BUSINESS)
+                        .withOrganisationId(CreationConstants.ORGANISATION_ID)
+                        .withWellKnownURL(CreationConstants.WELL_KNOWN_URL)
+                        .withApiBaseURL(CreationConstants.AIS_API_URL)
+                        .build();
+    }
+
+    @Inject
+    public CreationBusinessAgent(
+            AgentComponentProvider componentProvider, UkOpenBankingFlowFacade flowFacade) {
+        super(componentProvider, flowFacade, aisConfig);
+    }
+
+    @Override
+    protected UkOpenBankingAis makeAis() {
+        return new UkOpenBankingV31Ais(aisConfig, persistentStorage, localDateTimeSource);
+    }
+}
