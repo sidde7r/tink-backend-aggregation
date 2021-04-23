db3ddec2ad8bde0be8e00b61c5d0cd19174d99b4
Filip Behnke <filip.behnke@tink.com>
2021-04-23 09:08:51 +0200

    chore(PAYM-965): Add frequencies for recurring payments

--- src/integration/transfer/src/main/java/se/tink/libraries/transfer/rpc/Frequency.java
+++ src/integration/transfer/src/main/java/se/tink/libraries/transfer/rpc/Frequency.java
@@ -1,11 +1,16 @@
 package se.tink.libraries.transfer.rpc;
 
 public enum Frequency {
+    DAILY("Daily"),
     WEEKLY("Weekly"),
+    EVERY_TWO_WEEKS("EveryTwoWeeks"),
     MONTHLY("Monthly"),
-    QUARTERLY("Quarterly");
+    EVERY_TWO_MONTHS("EveryTwoMonths"),
+    QUARTERLY("Quarterly"),
+    SEMI_ANNUAL("SemiAnnual"),
+    ANNUAL("Annual");
 
-    private Frequency(String value) {
+    Frequency(String value) {
         this.value = value;
     }
 
