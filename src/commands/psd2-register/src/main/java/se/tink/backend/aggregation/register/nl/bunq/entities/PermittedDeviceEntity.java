4371450e09cef1948e5afd26e29f80613514ff65
Krzysztof Korolczuk <krzysztof.korolczuk@tink.se>
2021-04-26 12:13:22 +0200

    refactor: remove legacy registrations

--- src/commands/psd2-register/src/main/java/se/tink/backend/aggregation/register/nl/bunq/entities/PermittedDeviceEntity.java
+++
@@ -1,9 +0,0 @@
-package se.tink.backend.aggregation.register.nl.bunq.entities;
-
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class PermittedDeviceEntity {
-    private String description;
-    private String ip;
-}
