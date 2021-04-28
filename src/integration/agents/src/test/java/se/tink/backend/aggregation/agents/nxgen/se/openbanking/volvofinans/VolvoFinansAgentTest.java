9992212501b015c0a092d2352a0fccea19e68db1
Shivam Saini <shivam.Saini@tink.com>
2021-04-28 16:32:51 +0200

    refactor(se/volvofinans): Tag @Ignore no longer needed for manual test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/volvofinans/VolvoFinansAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/volvofinans/VolvoFinansAgentTest.java
@@ -1,11 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.volvofinans;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
-@Ignore
 public class VolvoFinansAgentTest {
 
     private AgentIntegrationTest.Builder builder;
