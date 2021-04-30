58d06d801d754380e87df541c359804517447c3a
Malgorzata Kosecka <malgorzata.kosecka@tink.se>
2021-04-30 16:23:09 +0200

    feat(uk/ulster): update organisationId

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/ulster/UlsterConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/ulster/UlsterConstants.java
@@ -2,7 +2,7 @@ package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.ulster;
 
 final class UlsterConstants {
 
-    public static final String ORG_ID = "0015800000jfxrpAAA";
+    public static final String ORG_ID = "0015800000jfwxXAAQ";
 
     public static final String AIS_API_URL = "https://api.ulsterbank.co.uk/open-banking/v3.1/aisp";
     public static final String PIS_API_URL = "https://api.ulsterbank.co.uk/open-banking/v3.1/pisp";
