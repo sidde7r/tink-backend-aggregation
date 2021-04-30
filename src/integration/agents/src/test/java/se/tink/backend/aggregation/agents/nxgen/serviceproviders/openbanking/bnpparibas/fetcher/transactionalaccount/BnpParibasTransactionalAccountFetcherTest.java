672e1fc8d65b3d9d8ca50d5ae6d8b276bea2b403
Lukasz Binkiewicz <lukasz.binkiewicz@tink.se>
2021-04-30 10:04:50 +0200

    fix(FR/BnpParibas): Change account name

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/bnpparibas/fetcher/transactionalaccount/BnpParibasTransactionalAccountFetcherTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/bnpparibas/fetcher/transactionalaccount/BnpParibasTransactionalAccountFetcherTest.java
@@ -1,24 +1,20 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bnpparibas.fetcher.transactionalaccount;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.any;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bnpparibas.BnpParibasApiBaseClient;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bnpparibas.fetcher.transactionalaccount.entities.AccountsItemEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bnpparibas.fetcher.transactionalaccount.rpc.AccountsResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bnpparibas.fetcher.transactionalaccount.rpc.BalanceResponse;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class BnpParibasTransactionalAccountFetcherTest {
 
@@ -45,27 +41,15 @@ public class BnpParibasTransactionalAccountFetcherTest {
                 bnpParibasTransactionalAccountFetcher.fetchAccounts();
 
         // then
-        assertNotNull(resp);
-        assertEquals(0, resp.size());
+        assertThat(resp).isNotNull();
+        assertThat(resp).isEmpty();
     }
 
     @Test
     public void shouldReturnObjectOnFetchAccounts() {
         // given
-        AccountsItemEntity accountsItemEntity = mock(AccountsItemEntity.class);
-        when(accountsItemEntity.isCheckingAccount()).thenReturn(true);
-        List<AccountsItemEntity> list = new ArrayList<>();
-        list.add(accountsItemEntity);
-        TransactionalAccount transactionalAccount = mock(TransactionalAccount.class);
-        Optional<TransactionalAccount> optionalTransactionalAccount =
-                Optional.ofNullable(transactionalAccount);
-
-        BalanceResponse balanceResponse = mock(BalanceResponse.class);
-
-        when(accountsItemEntity.toTinkAccount(any())).thenReturn(optionalTransactionalAccount);
-        when(apiClient.getBalance(anyString())).thenReturn(balanceResponse);
-        when(accountsResponse.getAccounts()).thenReturn(list);
-        when(apiClient.fetchAccounts()).thenReturn(accountsResponse);
+        when(apiClient.getBalance(anyString())).thenReturn(loadBalanceResponse());
+        when(apiClient.fetchAccounts()).thenReturn(loadAccountsResponse());
 
         BnpParibasTransactionalAccountFetcher bnpParibasTransactionalAccountFetcher =
                 new BnpParibasTransactionalAccountFetcher(apiClient);
@@ -75,8 +59,69 @@ public class BnpParibasTransactionalAccountFetcherTest {
                 bnpParibasTransactionalAccountFetcher.fetchAccounts();
 
         // then
-        assertNotNull(resp);
-        assertTrue(resp.size() > 0);
-        assertTrue(resp.contains(transactionalAccount));
+        assertThat(resp).isNotNull();
+        assertThat(resp).isNotEmpty();
+        TransactionalAccount account = new ArrayList<>(resp).get(0);
+        assertThat(account.getName()).isEqualTo("Compte de chèques");
+        assertThat(account.getAccountNumber()).isEqualTo("iban");
+        assertThat(account.getApiIdentifier()).isEqualTo("resourceId");
+        assertThat(account.getExactBalance().getExactValue()).isEqualTo(BigDecimal.valueOf(24.55));
+    }
+
+    private BalanceResponse loadBalanceResponse() {
+        String data =
+                "{\n"
+                        + "  \"balances\": [\n"
+                        + "    {\n"
+                        + "      \"name\": \"SOLDE COMPTABLE au 29/04/2021\",\n"
+                        + "      \"referenceDate\": \"2021-04-29\",\n"
+                        + "      \"balanceAmount\": {\n"
+                        + "        \"amount\": \"24.55\",\n"
+                        + "        \"currency\": \"EUR\"\n"
+                        + "      },\n"
+                        + "      \"balanceType\": \"CLBD\"\n"
+                        + "    }\n"
+                        + "  ],\n"
+                        + "  \"_links\": {\n"
+                        + "    \"self\": {\n"
+                        + "      \"href\": \"/accounts/resourceId/balances\"\n"
+                        + "    }\n"
+                        + "  }\n"
+                        + "}";
+        return SerializationUtils.deserializeFromString(data, BalanceResponse.class);
+    }
+
+    private AccountsResponse loadAccountsResponse() {
+        String data =
+                "{\n"
+                        + "  \"accounts\": [\n"
+                        + "    {\n"
+                        + "      \"resourceId\": \"resourceId\",\n"
+                        + "      \"accountId\": {\n"
+                        + "        \"iban\": \"iban\",\n"
+                        + "        \"currency\": \"EUR\"\n"
+                        + "      },\n"
+                        + "      \"name\": \"Compte de chèques ****1234\",\n"
+                        + "      \"details\": \"Bnp Paribas\",\n"
+                        + "      \"usage\": \"PRIV\",\n"
+                        + "      \"cashAccountType\": \"CACC\",\n"
+                        + "      \"psuStatus\": \"Account Holder\",\n"
+                        + "      \"_links\": {\n"
+                        + "        \"balances\": {\n"
+                        + "          \"href\": \"/accounts/resourceId/balances\"\n"
+                        + "        },\n"
+                        + "        \"transactions\": {\n"
+                        + "          \"href\": \"/accounts/resourceId/transactions\"\n"
+                        + "        }\n"
+                        + "      }\n"
+                        + "    }\n"
+                        + "  ],\n"
+                        + "  \"_links\": {\n"
+                        + "    \"self\": {\n"
+                        + "      \"href\": \"/accounts\"\n"
+                        + "    }\n"
+                        + "  }\n"
+                        + "}";
+        return SerializationUtils.deserializeFromString(data, AccountsResponse.class);
     }
 }
