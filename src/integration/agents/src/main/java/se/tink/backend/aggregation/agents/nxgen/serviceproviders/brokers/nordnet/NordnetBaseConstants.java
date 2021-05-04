9fbfb3b7063a33a024a4336efdd0376b98980e84
Jesús A. Álvarez <jesus.alvarez@tink.com>
2021-05-04 12:07:13 +0200

    refactor(nordnet): remove unused constants

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/brokers/nordnet/NordnetBaseConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/brokers/nordnet/NordnetBaseConstants.java
@@ -2,7 +2,6 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.brokers.nordne
 
 import java.util.regex.Pattern;
 import se.tink.backend.agents.rpc.AccountTypes;
-import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.constants.OAuth2Constants.PersistentStorageKeys;
 import se.tink.backend.aggregation.nxgen.core.account.TypeMapper;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.portfolio.PortfolioModule;
 import se.tink.backend.aggregation.nxgen.http.form.Form;
@@ -14,19 +13,13 @@ public class NordnetBaseConstants {
         public static final String BASE = "https://www.nordnet.se";
         public static final String INIT_LOGIN = BASE + "/api/2/login";
         public static final String BASIC_LOGIN = BASE + "/api/2/authentication/basic/login";
-        public static final String INIT_AUTHORIZE = BASE + "/oauth2/authorize";
         public static final String BANKID_START =
                 BASE + "/api/2/authentication/eid/se/bankid/start";
         public static final String BANKID_POLL = BASE + "/api/2/authentication/eid/se/bankid/poll";
-        public static final String OAUTH2_TOKEN = BASE + "/oauth2/token";
         public static final String CUSTOMER_INFO = BASE + "/api/2/customers/contact_info";
         public static final String ACCOUNTS = BASE + "/api/2/accounts";
         public static final String ACCOUNT_INFO = BASE + "/api/2/accounts/{account-id}/info";
         public static final String POSITIONS = BASE + "/api/2/accounts/{positions-id}/positions";
-    }
-
-    public static class InitLogin {
-        public static final String AUTHENTICATED = "authenticated";
     }
 
     public static class BankIdStatus {
@@ -41,20 +34,11 @@ public class NordnetBaseConstants {
     }
 
     public static class StorageKeys {
-
-        public static final String CLIENT_SECRET = "clientSecret";
-        public static final String OAUTH2_TOKEN = PersistentStorageKeys.OAUTH_2_TOKEN;
         public static final String ACCOUNTS = "accounts";
         public static final String SESSION_KEY = "session_key";
     }
 
     public static class FormKeys {
-        public static final String GRANT_TYPE = "grant_type";
-        public static final String CLIENT_ID = "client_id";
-        public static final String CLIENT_SECRET = "client_secret";
-        public static final String REDIRECT_URI = "redirect_uri";
-        public static final String RESPONSE_TYPE = "response_type";
-        public static final String SSN = "national_registration_number";
         public static final String USERNAME = "username";
         public static final String PASSWORD = "password";
         public static final String SERVICE = "service";
@@ -63,8 +47,6 @@ public class NordnetBaseConstants {
     }
 
     public static class FormValues {
-        public static final String GRANT_TYPE = "authorization_code";
-        public static final String CLIENT_SECRET = "6C2B9862-7FEE-CBACE053-3757570ADDEF";
         public static final String ANONYMOUS = "<<anonymous>>";
         public static final String COUNTRY_SE = "SE";
         public static final String LANG_EN = "en";
@@ -79,37 +61,23 @@ public class NordnetBaseConstants {
     }
 
     public static final class HeaderKeys {
-        public static final String NTAG = "ntag";
-        public static final String LOCATION = "Location";
-        public static final String APPLICATION_XML_Q = "application/xml;q=0.9";
-        public static final String GENERIC_MEDIA_TYPE = "*/*;q=0.8";
-        public static final String REFERER = "Referer";
-        public static final String BEARER = "Bearer ";
         public static final String BASIC = "Basic ";
     }
 
     public static final class HeaderValues {
-        public static final String NO_NTAG_RECEIVED_YET = "NO_NTAG_RECEIVED_YET";
         public static final String USER_AGENT =
                 "Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148";
         public static final String NORDNET_AGENT = "Nordnet/4 CFNetwork/1220.1 Darwin/20.3.0";
         public static final String REACT_NATIVE_AGENT =
                 "Nordnet React Native App/ios-14.4.2/12.3.0";
-        public static final String REFERER =
-                "https://classic.nordnet.se/mux/login/startSE.html?clearEndpoint=0&intent=mobile_ios_2&state=signin";
     }
 
     public static class QueryKeys {
-        public static final String AUTH_TYPE = "authType";
         public static final String INCLUDE_INSTRUMENT_LOAN = "include_instrument_loans";
     }
 
     public static class QueryValues {
-        public static final String AUTH_TYPE = "signin";
         public static final String CLIENT_ID = "MOBILE_IOS_2";
-        public static final String RESPONSE_TYPE = "code";
-        public static final String REDIRECT_URI = "nordnet-react://oauth2/authorize-callback";
-        public static final String B = "1";
         public static final String TRUE = "true";
     }
 
