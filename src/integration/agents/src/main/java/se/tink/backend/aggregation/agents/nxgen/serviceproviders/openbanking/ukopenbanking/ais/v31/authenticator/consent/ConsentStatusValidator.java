f0a7539f443f967dd0aa3eca14ba1d02cffd455d
Jakub Kalicki <jakub.kalicki@tink.com>
2021-05-04 10:46:12 +0200

    refactor(uk): Extract methods in consent status validator

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/ais/v31/authenticator/consent/ConsentStatusValidator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/ais/v31/authenticator/consent/ConsentStatusValidator.java
@@ -17,30 +17,37 @@ public class ConsentStatusValidator {
         this.storage = storage;
     }
 
-    public void validate() {
-        String consentId =
-                storage.get(
-                                UkOpenBankingV31Constants.PersistentStorageKeys
-                                        .AIS_ACCOUNT_CONSENT_ID,
-                                String.class)
-                        .orElse(StringUtils.EMPTY);
+    public boolean validate() {
+        String consentId = restoreConsentId();
+        checkIfMarkedWithErrorFlag(consentId);
+        checkIfAuthorised(consentId);
 
-        // To be removed when consent management becomes stable
-        if (consentId.equals(OpenIdAuthenticatorConstants.CONSENT_ERROR_OCCURRED)) {
-            cleanUpAndExpireSession(
-                    consentId,
-                    "These credentials were marked with CONSENT_ERROR_OCCURRED flag in the past. Expiring the session.");
-        }
+        return true;
+    }
 
+    private String restoreConsentId() {
+        return storage.get(
+                        UkOpenBankingV31Constants.PersistentStorageKeys.AIS_ACCOUNT_CONSENT_ID,
+                        String.class)
+                .orElse(StringUtils.EMPTY);
+    }
+
+    private void checkIfAuthorised(String consentId) {
         if (StringUtils.isNotEmpty(consentId) && isNotAuthorised(consentId)) {
             cleanUpAndExpireSession(consentId, "Invalid consent status. Expiring the session.");
         }
     }
 
+    // TODO: To be removed when consent management becomes stable
+    private void checkIfMarkedWithErrorFlag(String consentId) {
+        if (consentId.equals(OpenIdAuthenticatorConstants.CONSENT_ERROR_OCCURRED)) {
+            cleanUpAndExpireSession(
+                    consentId,
+                    "These credentials were marked with CONSENT_ERROR_OCCURRED flag in the past. Expiring the session.");
+        }
+    }
+
     private boolean isNotAuthorised(String consentId) {
-
-        // PLACEHOLDER: Add permissions vs refresh items validation
-
         return apiClient.fetchConsent(consentId).getData().isNotAuthorised();
     }
 
