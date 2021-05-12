d57a92b58dca917f8f02b3b7a324f183a794c58c
Nazri Bin Abdullah <nazri.abdullah@tink.com>
2021-05-12 18:08:50 +0200

    fix(nl-knab-ob): added retry and gate time out filter

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/knab/filter/KnabRetryFilter.java
@@ -0,0 +1,21 @@
+package se.tink.backend.aggregation.agents.nxgen.nl.openbanking.knab.filter;
+
+import org.apache.http.HttpStatus;
+import se.tink.backend.aggregation.nxgen.http.filter.filters.randomretry.AbstractRandomRetryFilter;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
+
+public class KnabRetryFilter extends AbstractRandomRetryFilter {
+
+    /**
+     * @param maxNumRetries Number of additional retries to be performed.
+     * @param retrySleepMilliseconds Time in milliseconds that will be spent sleeping between
+     */
+    public KnabRetryFilter(int maxNumRetries, long retrySleepMilliseconds) {
+        super(maxNumRetries, retrySleepMilliseconds);
+    }
+
+    @Override
+    protected boolean shouldRetry(HttpResponse response) {
+        return response.getStatus() == HttpStatus.SC_GATEWAY_TIMEOUT;
+    }
+}
