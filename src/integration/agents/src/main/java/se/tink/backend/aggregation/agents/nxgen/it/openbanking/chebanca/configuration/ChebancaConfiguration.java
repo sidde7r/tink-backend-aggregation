ef4de4d5fa29b3dae1308fe5d61a97c050c19d84
tymon zietek <tymon.zietek@tink.se>
2021-05-12 12:22:20 +0200

    feat(chebanca): adjust configuration to new authentication
    [NZG-290]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/chebanca/configuration/ChebancaConfiguration.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/chebanca/configuration/ChebancaConfiguration.java
@@ -4,44 +4,62 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaDescription;
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaExamples;
 import com.kjetland.jackson.jsonSchema.annotations.JsonSchemaTitle;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.annotations.Secret;
 import se.tink.backend.aggregation.annotations.SensitiveSecret;
 import se.tink.backend.aggregation.configuration.agents.ClientConfiguration;
-import se.tink.backend.aggregation.configuration.agents.ClientIdConfiguration;
-import se.tink.backend.aggregation.configuration.agents.ClientSecretsConfiguration;
 
 @JsonObject
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ChebancaConfiguration implements ClientConfiguration {
-
-    @JsonProperty @Secret @ClientIdConfiguration private String clientId;
-
-    @JsonProperty @SensitiveSecret @ClientSecretsConfiguration private String clientSecret;
+    @Secret
+    @JsonProperty
+    @JsonSchemaTitle("Application Id for manual refresh")
+    @JsonSchemaExamples("TEST_TPP_APP_01")
+    @JsonSchemaDescription(
+            "Application Id registered in CheBanca portal with OAuth2 grant type Authorization Code")
+    private String manualRefreshApplicationId;
 
     @Secret
     @JsonProperty
-    @JsonSchemaTitle("Application Id")
-    @JsonSchemaExamples("TEST_TPP_APP_01")
-    @JsonSchemaDescription("Application Id registered in CheBanca portal")
-    private String applicationId;
+    @JsonSchemaTitle("Client Id")
+    @JsonSchemaDescription("Client Id of application for background refresh")
+    @JsonSchemaExamples("555d5513cb123456789050b90f06a18b21234567890a9cce977c5513734d3f41")
+    private String manualRefreshClientId;
 
-    public ChebancaConfiguration() {}
+    @SensitiveSecret
+    @JsonProperty
+    @JsonSchemaTitle("Client Secret of application for manual refresh")
+    @JsonSchemaDescription(
+            "The client secret is a secret known only to the application and the authorization server.")
+    @JsonSchemaExamples("555d5513cb123456789050b90f06a18b21234567890a9cce977c5513734d3f41")
+    private String manualRefreshClientSecret;
 
-    public ChebancaConfiguration(String clientId, String clientSecret, String applicationId) {
-        this.clientId = clientId;
-        this.clientSecret = clientSecret;
-        this.applicationId = applicationId;
-    }
+    @Secret
+    @JsonProperty
+    @JsonSchemaTitle("Application Id for background refresh")
+    @JsonSchemaExamples("TEST_TPP_APP_02")
+    @JsonSchemaDescription(
+            "Application Id registered in CheBanca portal with OAuth2 grant type Client Credentials")
+    private String autoRefreshApplicationId;
 
-    public String getApplicationId() {
-        return applicationId;
-    }
+    @Secret
+    @JsonProperty
+    @JsonSchemaTitle("Client Id")
+    @JsonSchemaDescription("Client Id of application for background refresh")
+    @JsonSchemaExamples("555d5513cb123456789050b90f06a18b21234567890a9cce977c5513734d3f41")
+    private String autoRefreshClientId;
 
-    public String getClientId() {
-        return clientId;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
-    }
+    @SensitiveSecret
+    @JsonProperty
+    @JsonSchemaDescription(
+            "The client secret is a secret known only to the application and the authorization server.")
+    @JsonSchemaTitle("Client Secret of application for background refresh")
+    @JsonSchemaExamples("555d5513cb123456789050b90f06a18b21234567890a9cce977c5513734d3f41")
+    private String autoRefreshClientSecret;
 }
