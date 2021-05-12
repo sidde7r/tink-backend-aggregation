a77cb065ff678088db0bf6bd7a7e2e7ded79ba89
tymon zietek <tymon.zietek@tink.se>
2021-05-12 12:26:27 +0200

    test(chebanca): adds and refactor unit tests

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/chebanca/fetcher/transactionalaccount/ChebancaTransactionalAccountFetcherTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/chebanca/fetcher/transactionalaccount/ChebancaTransactionalAccountFetcherTest.java
@@ -6,38 +6,43 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.nio.file.Paths;
 import java.util.Collection;
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
-import se.tink.backend.agents.rpc.Credentials;
 import se.tink.backend.aggregation.agents.nxgen.it.openbanking.chebanca.ChebancaApiClient;
 import se.tink.backend.aggregation.agents.nxgen.it.openbanking.chebanca.exception.UnsuccessfulApiCallException;
-import se.tink.backend.aggregation.agents.nxgen.it.openbanking.chebanca.fetcher.transactionalaccount.data.TransactionalAccountTestData;
-import se.tink.backend.aggregation.agents.nxgen.it.openbanking.chebanca.fetcher.transactionalaccount.rpc.ConsentAuthorizationResponse;
-import se.tink.backend.aggregation.agents.nxgen.it.openbanking.chebanca.fetcher.transactionalaccount.rpc.ConsentResponse;
-import se.tink.backend.aggregation.agents.nxgen.it.openbanking.chebanca.fetcher.transactionalaccount.rpc.CustomerIdResponse;
 import se.tink.backend.aggregation.agents.nxgen.it.openbanking.chebanca.fetcher.transactionalaccount.rpc.GetAccountsResponse;
 import se.tink.backend.aggregation.agents.nxgen.it.openbanking.chebanca.fetcher.transactionalaccount.rpc.GetBalancesResponse;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
+import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class ChebancaTransactionalAccountFetcherTest {
 
     private static final int ERROR_RESPONSE_CODE = 500;
     private static final int SUCCESSFUL_RESPONSE_CODE = 200;
+    private static final String TEST_DATA_PATH =
+            "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/chebanca/resources";
 
     private ChebancaApiClient apiClient;
-    private ChebancaConsentManualApproveController consentController =
-            mock(ChebancaConsentManualApproveController.class);
-    private Credentials credentials = new Credentials();
     private HttpResponse erroneousResponse = getErroneousResponse();
+
+    @Before
+    public void initApiClientMockForPositiveScenario() {
+        apiClient = mock(ChebancaApiClient.class);
+        HttpResponse getAccountsResponse = getAccountsResponse();
+        when(apiClient.getAccounts()).thenReturn(getAccountsResponse);
+        HttpResponse balanceResponse = getBalanceResponse();
+        when(apiClient.getBalances(any())).thenReturn(balanceResponse);
+    }
 
     @Test
     public void shouldReturnProperNumberOfAccounts() {
         // given
         ChebancaTransactionalAccountFetcher fetcher =
-                new ChebancaTransactionalAccountFetcher(apiClient, consentController);
+                new ChebancaTransactionalAccountFetcher(apiClient);
         // when
         Collection<TransactionalAccount> accounts = fetcher.fetchAccounts();
 
@@ -46,75 +51,11 @@ public class ChebancaTransactionalAccountFetcherTest {
     }
 
     @Test
-    public void shouldThrowIfCreateConsentFailed() {
-        // given
-        when(apiClient.createConsent(any())).thenReturn(erroneousResponse);
-        ChebancaTransactionalAccountFetcher fetcher =
-                new ChebancaTransactionalAccountFetcher(apiClient, consentController);
-        // when
-        Throwable thrown = catchThrowable(fetcher::fetchAccounts);
-
-        // then
-        Assertions.assertThat(thrown)
-                .isInstanceOf(UnsuccessfulApiCallException.class)
-                .hasMessage(
-                        "Could not create consent. Error response code: " + ERROR_RESPONSE_CODE);
-    }
-
-    @Test
-    public void shouldThrowIfAuthorizeConsentFailed() {
-        // given
-        when(apiClient.authorizeConsent(any())).thenReturn(erroneousResponse);
-        ChebancaTransactionalAccountFetcher fetcher =
-                new ChebancaTransactionalAccountFetcher(apiClient, consentController);
-        // when
-        Throwable thrown = catchThrowable(fetcher::fetchAccounts);
-
-        // then
-        Assertions.assertThat(thrown)
-                .isInstanceOf(UnsuccessfulApiCallException.class)
-                .hasMessage(
-                        "Could not authorize consent. Error response code: " + ERROR_RESPONSE_CODE);
-    }
-
-    @Test
-    public void shouldThrowIfConfirmConsentFailed() {
-        // given
-        when(apiClient.confirmConsent(any())).thenReturn(erroneousResponse);
-        ChebancaTransactionalAccountFetcher fetcher =
-                new ChebancaTransactionalAccountFetcher(apiClient, consentController);
-        // when
-        Throwable thrown = catchThrowable(fetcher::fetchAccounts);
-
-        // then
-        Assertions.assertThat(thrown)
-                .isInstanceOf(UnsuccessfulApiCallException.class)
-                .hasMessage(
-                        "Could not confirm consent. Error response code: " + ERROR_RESPONSE_CODE);
-    }
-
-    @Test
-    public void shouldThrowIfGetCustomerIdFailed() {
-        // given
-        when(apiClient.getCustomerId()).thenReturn(erroneousResponse);
-        ChebancaTransactionalAccountFetcher fetcher =
-                new ChebancaTransactionalAccountFetcher(apiClient, consentController);
-        // when
-        Throwable thrown = catchThrowable(fetcher::fetchAccounts);
-
-        // then
-        Assertions.assertThat(thrown)
-                .isInstanceOf(UnsuccessfulApiCallException.class)
-                .hasMessage(
-                        "Could not get customer id. Error response code: " + ERROR_RESPONSE_CODE);
-    }
-
-    @Test
     public void shouldThrowIfGetBalancesFailed() {
         // given
         when(apiClient.getBalances(any())).thenReturn(erroneousResponse);
         ChebancaTransactionalAccountFetcher fetcher =
-                new ChebancaTransactionalAccountFetcher(apiClient, consentController);
+                new ChebancaTransactionalAccountFetcher(apiClient);
         // when
         Throwable thrown = catchThrowable(fetcher::fetchAccounts);
 
@@ -130,7 +71,7 @@ public class ChebancaTransactionalAccountFetcherTest {
         // given
         when(apiClient.getAccounts()).thenReturn(erroneousResponse);
         ChebancaTransactionalAccountFetcher fetcher =
-                new ChebancaTransactionalAccountFetcher(apiClient, consentController);
+                new ChebancaTransactionalAccountFetcher(apiClient);
         // when
         Throwable thrown = catchThrowable(fetcher::fetchAccounts);
 
@@ -141,72 +82,32 @@ public class ChebancaTransactionalAccountFetcherTest {
                         "Could not fetch accounts. Error response code: " + ERROR_RESPONSE_CODE);
     }
 
-    @Before
-    public void initApiClientMockForPositiveScenario() {
-        apiClient = mock(ChebancaApiClient.class);
-        HttpResponse createConsentResponse = getMockedCreateConsentResponse();
-        when(apiClient.createConsent(any())).thenReturn(createConsentResponse);
-        HttpResponse authorizeConsentResponse = getMockedAuthorizeConsentResponse();
-        when(apiClient.authorizeConsent(any())).thenReturn(authorizeConsentResponse);
-        HttpResponse confirmConsentResponse = getMockedConfirmConsentResponse();
-        when(apiClient.confirmConsent(any())).thenReturn(confirmConsentResponse);
-        HttpResponse getAccountsResponse = getAccountsResponse();
-        when(apiClient.getAccounts()).thenReturn(getAccountsResponse);
-        HttpResponse customerIdResponse = getMockedCustomerIdResponse();
-        when(apiClient.getCustomerId()).thenReturn(customerIdResponse);
-        HttpResponse balanceResponse = getBalanceResponse();
-        when(apiClient.getBalances(any())).thenReturn(balanceResponse);
-    }
-
     private HttpResponse getErroneousResponse() {
         HttpResponse response = mock(HttpResponse.class);
         when(response.getStatus()).thenReturn(ERROR_RESPONSE_CODE);
         return response;
     }
 
-    private HttpResponse getMockedCreateConsentResponse() {
-        HttpResponse response = mock(HttpResponse.class);
-        when(response.getStatus()).thenReturn(SUCCESSFUL_RESPONSE_CODE);
-        when(response.getBody(ConsentResponse.class))
-                .thenReturn(TransactionalAccountTestData.getCreateConsentResponse());
-        return response;
-    }
-
-    private HttpResponse getMockedAuthorizeConsentResponse() {
-        HttpResponse response = mock(HttpResponse.class);
-        when(response.getStatus()).thenReturn(SUCCESSFUL_RESPONSE_CODE);
-        when(response.getBody(ConsentAuthorizationResponse.class))
-                .thenReturn(TransactionalAccountTestData.getAuthorizeConsentResponse());
-        return response;
-    }
-
-    private HttpResponse getMockedConfirmConsentResponse() {
-        HttpResponse response = mock(HttpResponse.class);
-        when(response.getStatus()).thenReturn(SUCCESSFUL_RESPONSE_CODE);
-        return response;
-    }
-
-    private HttpResponse getMockedCustomerIdResponse() {
-        HttpResponse response = mock(HttpResponse.class);
-        when(response.getStatus()).thenReturn(SUCCESSFUL_RESPONSE_CODE);
-        when(response.getBody(CustomerIdResponse.class))
-                .thenReturn(TransactionalAccountTestData.getCustomerId());
-        return response;
-    }
-
     private HttpResponse getBalanceResponse() {
+        GetBalancesResponse balancesResponse =
+                SerializationUtils.deserializeFromString(
+                        Paths.get(TEST_DATA_PATH, "balances_response.json").toFile(),
+                        GetBalancesResponse.class);
         HttpResponse response = mock(HttpResponse.class);
         when(response.getStatus()).thenReturn(SUCCESSFUL_RESPONSE_CODE);
-        when(response.getBody(GetBalancesResponse.class))
-                .thenReturn(TransactionalAccountTestData.getBalances());
+        when(response.getBody(GetBalancesResponse.class)).thenReturn(balancesResponse);
         return response;
     }
 
     private HttpResponse getAccountsResponse() {
+        GetAccountsResponse accountsResponse =
+                SerializationUtils.deserializeFromString(
+                        Paths.get(TEST_DATA_PATH, "accounts_response.json").toFile(),
+                        GetAccountsResponse.class);
+
         HttpResponse response = mock(HttpResponse.class);
         when(response.getStatus()).thenReturn(SUCCESSFUL_RESPONSE_CODE);
-        when(response.getBody(GetAccountsResponse.class))
-                .thenReturn(TransactionalAccountTestData.getAccountsResponse());
+        when(response.getBody(GetAccountsResponse.class)).thenReturn(accountsResponse);
         return response;
     }
 }
