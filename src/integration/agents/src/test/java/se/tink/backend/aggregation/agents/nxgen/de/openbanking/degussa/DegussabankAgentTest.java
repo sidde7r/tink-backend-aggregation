7fce582dc73f68af4b796b71ff1aa36423459745
vishwanath sharma <vishwanath.sharma@tink.se>
2021-05-27 23:45:05 +0200

    feat(agent): Degussa agent Test

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/degussa/DegussabankAgentTest.java
@@ -0,0 +1,42 @@
+package se.tink.backend.aggregation.agents.nxgen.de.openbanking.degussa;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+import se.tink.backend.aggregation.agents.framework.ArgumentManager;
+
+public class DegussabankAgentTest {
+    private final ArgumentManager<ArgumentManager.UsernamePasswordArgumentEnum> manager =
+            new ArgumentManager<>(ArgumentManager.UsernamePasswordArgumentEnum.values());
+    private AgentIntegrationTest.Builder builder;
+
+    @Before
+    public void setup() {
+        manager.before();
+        builder =
+                new AgentIntegrationTest.Builder("de", "de-degussabank-ob")
+                        .addCredentialField(
+                                Field.Key.USERNAME,
+                                manager.get(ArgumentManager.UsernamePasswordArgumentEnum.USERNAME))
+                        .addCredentialField(
+                                Field.Key.PASSWORD,
+                                manager.get(ArgumentManager.UsernamePasswordArgumentEnum.PASSWORD))
+                        .loadCredentialsBefore(false)
+                        .expectLoggedIn(false)
+                        .saveCredentialsAfter(false)
+                        .setAppId("tink")
+                        .setFinancialInstitutionId("degussabank");
+    }
+
+    @Test
+    public void testRefresh() throws Exception {
+        builder.build().testRefresh();
+    }
+
+    @AfterClass
+    public static void cleanup() {
+        ArgumentManager.afterClass();
+    }
+}
