a88fe97c6b19589eca77d191dd77d0bcd5967869
Schahin Rajab <schahin.rajab@tink.com>
2021-06-01 14:34:07 +0200

    fix(fi/spankki): use locale as api language
    https://tinkab.atlassian.net/browse/ITE-2736

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/spankki/v2/entities/ClientInfoEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/banks/spankki/v2/entities/ClientInfoEntity.java
@@ -1,17 +1,19 @@
 package se.tink.backend.aggregation.agents.nxgen.fi.banks.spankki.v2.entities;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.RequiredArgsConstructor;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.spankki.v2.SpankkiConstants;
 import se.tink.backend.aggregation.agents.nxgen.fi.banks.spankki.v2.SpankkiConstants.Request;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
+@RequiredArgsConstructor
 public class ClientInfoEntity {
     @JsonProperty
     private String platformName = SpankkiConstants.DEVICE_PROFILE.getOs().toLowerCase();
 
     @JsonProperty private String appVersion = Request.CLIENT_INFO_APP_VERSION;
-    @JsonProperty private String lang = Request.CLIENT_INFO_LANG;
+    @JsonProperty private final String lang;
 
     @JsonProperty
     private String platformType = SpankkiConstants.DEVICE_PROFILE.getOs().toLowerCase();
