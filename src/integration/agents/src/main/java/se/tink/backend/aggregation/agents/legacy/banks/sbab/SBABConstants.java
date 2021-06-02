5e2a2c3628b5ab27cf942c387c30196d40dafa89
Niclas Albinsson <niclas.albinsson@tink.se>
2021-06-02 12:10:53 +0200

    chore(sbab/legacy): remove all non used endpoint

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/sbab/SBABConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/legacy/banks/sbab/SBABConstants.java
@@ -2,25 +2,14 @@ package se.tink.backend.aggregation.agents.banks.sbab;
 
 public class SBABConstants {
     public static final String INTEGRATION_NAME = "sbab";
-    public static final String IS_MORTGAGE_SWITCH_PROVIDER_TEST = "isSwitchMortgageProviderTest";
     public static final String TRUE = "true";
 
     public static class Url {
         public static final String HOST = "https://secure.sbab.se";
-        private static final String TRANSFER_BASE_ENDPOINT = HOST + "api/transfer-facade-service/";
 
         public static final String GRAPGQL_URL = HOST + "/beta/api/bb-bff/graphql";
         public static final String IDENTITY_URL =
                 HOST + "/beta/api/bb-bff/document/dataportability";
-        public static final String SAVED_RECIPIENTS = TRANSFER_BASE_ENDPOINT + "saved-recipients";
-        public static final String TRANSFER_ACCOUNTS = TRANSFER_BASE_ENDPOINT + "accounts";
-        public static final String VALIDATE_TRANSFER =
-                TRANSFER_BASE_ENDPOINT + "transfers/validate";
-        public static final String CONFIRM_TRANSFER = TRANSFER_BASE_ENDPOINT + "transfers";
-        public static final String VALIDATE_RECIPIENT =
-                TRANSFER_BASE_ENDPOINT + "saved-recipients/validate";
-        public static final String INIT_SIGN_PROCESS = TRANSFER_BASE_ENDPOINT + "signature/process";
-        public static final String SIGN_TRANSFER = TRANSFER_BASE_ENDPOINT + "signature/";
 
         public static final String OVERVIEW_URL = HOST + "/beta?rev=1621409868887";
         public static final String BANKID_BASE_URL = HOST + "/ib-login/api/v1/";
@@ -42,25 +31,10 @@ public class SBABConstants {
         public static final String DEP = "privat";
         public static final String AUTH_MECH = "PW_MBID";
         public static final String AUTH_DEVICE = "SAME";
-        public static final String REV = "1621407819416";
-    }
-
-    public static class TransferStatus {
-        public static final String PENDING = "PENDING";
-        public static final String COMPLETE = "COMPLETE";
-        public static final String UNKNOWN = "UNKNOWN";
-    }
-
-    public static class ErrorCode {
-        public static final int INVALID_ACCOUNT_NUMBER = 1200;
-        public static final int INVALID_ACCOUNT_NUMBER_LEN = 2001;
-        public static final int INVALID_TRANSACTION_DATE = 2006;
-        public static final int EXCESS_TRANSFER_AMOUNT = 2002;
     }
 
     public static class BankId {
         public static final int BANKID_MAX_ATTEMPTS = 90;
-        public static final String BANKID_AUTH_METHOD = "MOBILE_BANK_ID";
 
         public static final String CANCELED = "CANCELED";
         public static final String STARTED = "STARTED";
@@ -77,7 +51,6 @@ public class SBABConstants {
         public static final String SAVINGS_ACCOUNTS_QUERY = "SavingsAccountsQuery";
         public static final String LOAN_ACCOUNTS_QUERY = "CollateralsQuery";
         public static final String SAVINGS_ACCOUNTS_DETAILS_QUERY = "SavingsOverviewQuery";
-        public static final String LOAN_ACCOUNTS_DETAILS_QUERY = "SavingsOverviewQuery";
     }
 
     public static class Query {
