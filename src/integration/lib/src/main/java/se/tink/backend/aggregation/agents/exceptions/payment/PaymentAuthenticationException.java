84e961997813f086cd13e6594f6dfc5433b935ec
Shankey Jain <shankey.jain@tink.se>
2021-06-03 15:17:47 +0200

    feat(PaymentAuthenticationException): Adding internalStatus const

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/exceptions/payment/PaymentAuthenticationException.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/exceptions/payment/PaymentAuthenticationException.java
@@ -1,7 +1,13 @@
 package se.tink.backend.aggregation.agents.exceptions.payment;
 
+import se.tink.libraries.signableoperation.enums.InternalStatus;
+
 public class PaymentAuthenticationException extends PaymentException {
     public static final String DEFAULT_MESSAGE = "Payment authentication failed.";
+
+    public PaymentAuthenticationException(InternalStatus internalStatus) {
+        super(DEFAULT_MESSAGE, internalStatus);
+    }
 
     public PaymentAuthenticationException(String message, Throwable cause) {
         super(message, cause);
