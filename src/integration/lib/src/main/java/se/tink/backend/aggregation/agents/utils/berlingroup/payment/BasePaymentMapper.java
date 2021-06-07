d1903041cb0bdd34952ff03674850f724af711d6
tymon zietek <tymon.zietek@tink.se>
2021-06-07 12:19:08 +0200

    refactor(berlin group payments): adds base payment mapper

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/berlingroup/payment/BasePaymentMapper.java
@@ -0,0 +1,75 @@
+package se.tink.backend.aggregation.agents.utils.berlingroup.payment;
+
+import static java.util.Objects.nonNull;
+
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.utils.berlingroup.payment.entities.AccountEntity;
+import se.tink.backend.aggregation.agents.utils.berlingroup.payment.entities.AmountEntity;
+import se.tink.backend.aggregation.agents.utils.berlingroup.payment.rpc.CreatePaymentRequest;
+import se.tink.backend.aggregation.agents.utils.berlingroup.payment.rpc.CreateRecurringPaymentRequest;
+import se.tink.backend.aggregation.agents.utils.remittanceinformation.RemittanceInformationValidator;
+import se.tink.libraries.payment.rpc.Payment;
+import se.tink.libraries.transfer.enums.RemittanceInformationType;
+import se.tink.libraries.transfer.rpc.RemittanceInformation;
+
+public class BasePaymentMapper implements PaymentMapper<CreatePaymentRequest> {
+
+    @Override
+    public CreatePaymentRequest getPaymentRequest(Payment payment) {
+        return CreatePaymentRequest.builder()
+                .creditorAccount(getCreditorAccountEntity(payment))
+                .debtorAccount(getDebtorAccountEntity(payment))
+                .instructedAmount(getAmountEntity(payment))
+                .creditorName(payment.getCreditor().getName())
+                .remittanceInformationUnstructured(getUnstructuredRemittance(payment))
+                .requestedExecutionDate(payment.getExecutionDate())
+                .build();
+    }
+
+    @Override
+    public CreatePaymentRequest getRecurringPaymentRequest(Payment payment) {
+        return CreateRecurringPaymentRequest.builder()
+                .creditorAccount(getCreditorAccountEntity(payment))
+                .debtorAccount(getDebtorAccountEntity(payment))
+                .instructedAmount(getAmountEntity(payment))
+                .creditorName(payment.getCreditor().getName())
+                .remittanceInformationUnstructured(getUnstructuredRemittance(payment))
+                .frequency(payment.getFrequency().toString())
+                .startDate(payment.getStartDate())
+                // optional attributes
+                .endDate(payment.getEndDate())
+                .executionRule("preceeding")
+                .dayOfExecution(
+                        nonNull(payment.getDayOfExecution())
+                                ? String.valueOf(payment.getDayOfExecution())
+                                : null)
+                .build();
+    }
+
+    private AmountEntity getAmountEntity(Payment payment) {
+        return new AmountEntity(
+                String.valueOf(payment.getExactCurrencyAmount().getDoubleValue()),
+                payment.getExactCurrencyAmount().getCurrencyCode());
+    }
+
+    private String getUnstructuredRemittance(Payment payment) {
+        RemittanceInformation remittanceInformation = payment.getRemittanceInformation();
+
+        RemittanceInformationValidator.validateSupportedRemittanceInformationTypesOrThrow(
+                remittanceInformation, null, RemittanceInformationType.UNSTRUCTURED);
+
+        return Optional.ofNullable(remittanceInformation.getValue()).orElse("");
+    }
+
+    protected AccountEntity getDebtorAccountEntity(Payment payment) {
+        return getAccountEntity(payment.getDebtor().getAccountNumber());
+    }
+
+    protected AccountEntity getCreditorAccountEntity(Payment payment) {
+        return getAccountEntity(payment.getCreditor().getAccountNumber());
+    }
+
+    protected AccountEntity getAccountEntity(String accountNumber) {
+        return new AccountEntity(accountNumber);
+    }
+}
