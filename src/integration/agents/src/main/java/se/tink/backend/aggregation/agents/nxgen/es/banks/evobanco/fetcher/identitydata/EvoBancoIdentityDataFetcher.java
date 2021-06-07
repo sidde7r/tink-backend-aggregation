f5d33777d721c83a1e320e88f2a8856e078bca34
Jakub Badysiak <jakub.badysiak@tink.se>
2021-06-07 16:57:18 +0200

    fix(es/evobanco): handle invalid otp code
    [IFD-2508]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/evobanco/fetcher/identitydata/EvoBancoIdentityDataFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/evobanco/fetcher/identitydata/EvoBancoIdentityDataFetcher.java
@@ -2,10 +2,17 @@ package se.tink.backend.aggregation.agents.nxgen.es.banks.evobanco.fetcher.ident
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import se.tink.backend.aggregation.agents.exceptions.errors.LoginError;
+import se.tink.backend.aggregation.agents.exceptions.errors.SupplementalInfoError;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.evobanco.EvoBancoApiClient;
+import se.tink.backend.aggregation.agents.nxgen.es.banks.evobanco.EvoBancoConstants.ErrorCodes;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.evobanco.fetcher.identitydata.entities.EvoBancoIdentityEntity;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.evobanco.fetcher.identitydata.rpc.EvoBancoIdentityDataResponse;
+import se.tink.backend.aggregation.agents.nxgen.es.banks.evobanco.filter.entity.EvoBancoErrorResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.identitydata.IdentityDataFetcher;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
 import se.tink.libraries.identitydata.IdentityData;
 import se.tink.libraries.identitydata.countries.EsIdentityData;
 
@@ -21,7 +28,14 @@ public class EvoBancoIdentityDataFetcher implements IdentityDataFetcher {
 
     @Override
     public IdentityData fetchIdentityData() {
-        EvoBancoIdentityDataResponse identityDataResponse = apiClient.fetchIdentityData();
+        HttpResponse response = null;
+        try {
+            response = apiClient.fetchIdentityData();
+        } catch (HttpResponseException e) {
+            mapHttpError(e.getResponse());
+        }
+        EvoBancoIdentityDataResponse identityDataResponse =
+                Objects.requireNonNull(response).getBody(EvoBancoIdentityDataResponse.class);
         EvoBancoIdentityEntity identityEntity =
                 identityDataResponse.getClientData().getIdentityEntity();
         return EsIdentityData.builder()
@@ -31,5 +45,20 @@ public class EvoBancoIdentityDataFetcher implements IdentityDataFetcher {
                 .addSurnameElement(identityEntity.getSecondSurname())
                 .setDateOfBirth(LocalDate.parse(identityEntity.getDateOfBirth(), DATE_FORMATTER))
                 .build();
+    }
+
+    private void mapHttpError(HttpResponse response) {
+        EvoBancoErrorResponse errorResponse = response.getBody(EvoBancoErrorResponse.class);
+
+        if (ErrorCodes.INVALID_TOKEN.equals(errorResponse.getResponse().getCode())) {
+            throw SupplementalInfoError.NO_VALID_CODE.exception();
+        }
+        String message =
+                String.format(
+                        "Unknown error: httpStatus %s, code %s, message %s",
+                        response.getStatus(),
+                        errorResponse.getResponse().getCode(),
+                        errorResponse.getResponse().getMessage());
+        throw LoginError.DEFAULT_MESSAGE.exception(message);
     }
 }
--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/evobanco/fetcher/identitydata/EvoBancoIdentityFetcherTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/es/banks/evobanco/fetcher/identitydata/EvoBancoIdentityFetcherTest.java
@@ -1,5 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.es.banks.evobanco.fetcher.identitydata;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -7,8 +10,12 @@ import java.nio.file.Paths;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import se.tink.backend.aggregation.agents.exceptions.SupplementalInfoException;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.evobanco.EvoBancoApiClient;
 import se.tink.backend.aggregation.agents.nxgen.es.banks.evobanco.fetcher.identitydata.rpc.EvoBancoIdentityDataResponse;
+import se.tink.backend.aggregation.agents.nxgen.es.banks.evobanco.filter.entity.EvoBancoErrorResponse;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
+import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
 import se.tink.libraries.identitydata.IdentityData;
 import se.tink.libraries.serialization.utils.SerializationUtils;
 
@@ -27,11 +34,9 @@ public class EvoBancoIdentityFetcherTest {
     @Test
     public void shouldFetchIdentityData() {
         // given
-        when(apiClient.fetchIdentityData())
-                .thenReturn(
-                        SerializationUtils.deserializeFromString(
-                                Paths.get(TEST_DATA_PATH, "identity_data.json").toFile(),
-                                EvoBancoIdentityDataResponse.class));
+        HttpResponse response = mockResponse();
+
+        when(apiClient.fetchIdentityData()).thenReturn(response);
 
         // when
         IdentityData identityData = evoBancoIdentityDataFetcher.fetchIdentityData();
@@ -39,5 +44,47 @@ public class EvoBancoIdentityFetcherTest {
         // then
         Assert.assertEquals(identityData.getDateOfBirth().toString(), "1999-01-01");
         Assert.assertEquals(identityData.getFullName(), "ROBERTO ALEJANDRO VALDES");
+    }
+
+    @Test
+    public void shouldHandleSessionException() {
+        // given
+        HttpResponseException response = mockResponseException();
+
+        when(apiClient.fetchIdentityData()).thenThrow(response);
+
+        // when
+        Throwable throwable = catchThrowable(() -> evoBancoIdentityDataFetcher.fetchIdentityData());
+
+        // expected
+        assertThat(throwable).isExactlyInstanceOf(SupplementalInfoException.class);
+    }
+
+    private HttpResponse mockResponse() {
+        EvoBancoIdentityDataResponse evoBancoIdentityDataResponse =
+                SerializationUtils.deserializeFromString(
+                        Paths.get(TEST_DATA_PATH, "identity_data.json").toFile(),
+                        EvoBancoIdentityDataResponse.class);
+
+        HttpResponse response = mock(HttpResponse.class);
+        given(response.getStatus()).willReturn(200);
+        given(response.getBody(EvoBancoIdentityDataResponse.class))
+                .willReturn(evoBancoIdentityDataResponse);
+
+        return response;
+    }
+
+    private HttpResponseException mockResponseException() {
+        HttpResponse mocked = mock(HttpResponse.class);
+        when(mocked.getStatus()).thenReturn(401);
+        HttpResponseException exception = new HttpResponseException(null, mocked);
+
+        when(exception.getResponse().getBody(EvoBancoErrorResponse.class))
+                .thenReturn(
+                        SerializationUtils.deserializeFromString(
+                                "{\"response\":{\"message\":\"Token de acceso inv?lido\",\"codigo\":\"KO\"}}",
+                                EvoBancoErrorResponse.class));
+
+        return exception;
     }
 }
