64de86871478d777e1344898e593f6edc7587737
Gustaf Svanerud <gustaf.svanerud@tink.se>
2021-06-07 16:26:41 +0200

    feat(metric): add requesting cluster info to response metric

--- src/libraries/jersey_log/src/main/java/se/tink/libraries/jersey/logging/ResponseTimerFilter.java
+++ src/libraries/jersey_log/src/main/java/se/tink/libraries/jersey/logging/ResponseTimerFilter.java
@@ -1,5 +1,6 @@
 package se.tink.libraries.jersey.logging;
 
+import com.google.common.base.Strings;
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerRequestFilter;
 import com.sun.jersey.spi.container.ContainerResponse;
@@ -16,6 +17,9 @@ import se.tink.libraries.metrics.types.timers.Timer;
 class ResponseTimerFilter
         implements ContainerRequestFilter, ContainerResponseFilter, ResourceFilter {
 
+    private static final String CLUSTER_NAME_HEADER = "x-tink-cluster-name";
+    private static final String CLUSTER_ENVIRONMENT_HEADER = "x-tink-cluster-environment";
+    private static final String EMPTY_LABEL = "undefined";
     private static final Logger log = LoggerFactory.getLogger(ResponseTimerFilter.class);
     private static final String timerContextKey = "timerContext";
     private final MetricRegistry metricRegistry;
@@ -55,7 +59,14 @@ class ResponseTimerFilter
                                     teamOwnership
                                             .map(TeamOwnership::value)
                                             .map(Enum::name)
-                                            .orElse(""));
+                                            .orElse(""))
+                            .label(
+                                    "request_cluster",
+                                    safeGetLabel(request.getHeaderValue(CLUSTER_NAME_HEADER)))
+                            .label(
+                                    "request_environment",
+                                    safeGetLabel(
+                                            request.getHeaderValue(CLUSTER_ENVIRONMENT_HEADER)));
             this.metricRegistry.timer(metricId).time(timerContext).stop();
         }
         return response;
@@ -69,5 +80,9 @@ class ResponseTimerFilter
     @Override
     public ContainerResponseFilter getResponseFilter() {
         return this;
+    }
+
+    private static String safeGetLabel(String header) {
+        return Optional.ofNullable(Strings.emptyToNull(header)).orElse(EMPTY_LABEL);
     }
 }
