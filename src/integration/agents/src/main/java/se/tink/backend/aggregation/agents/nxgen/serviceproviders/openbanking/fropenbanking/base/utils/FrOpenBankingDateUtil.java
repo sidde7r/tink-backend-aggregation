091206246e35fc1b3bcb384f7dd7e45f79200d56
Shankey Jain <shankey.jain@tink.se>
2021-06-07 13:53:56 +0200

    feat(FrBase): Support for DateUtil

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/fropenbanking/base/utils/FrOpenBankingDateUtil.java
@@ -0,0 +1,33 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.fropenbanking.base.utils;
+
+import java.time.Clock;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.TimeZone;
+import se.tink.libraries.date.CountryDateHelper;
+
+public class FrOpenBankingDateUtil {
+
+    private FrOpenBankingDateUtil() {}
+
+    private static final ZoneId DEFAULT_ZONE_ID = ZoneId.of("Europe/Paris");
+    private static final Locale DEFAULT_LOCALE = new Locale("fr_FR", "FR");
+    private static final CountryDateHelper dateHelper =
+            new CountryDateHelper(DEFAULT_LOCALE, TimeZone.getTimeZone(DEFAULT_ZONE_ID));
+
+    public static void setClock(Clock clock) {
+        dateHelper.setClock(clock);
+    }
+
+    public static LocalDate getExecutionDate(LocalDate localDate) {
+        return Optional.ofNullable(localDate)
+                .orElse(dateHelper.getCurrentOrNextBusinessDay(dateHelper.getNowAsLocalDate()));
+    }
+
+    public static LocalDateTime getCreationDate() {
+        return LocalDateTime.now(DEFAULT_ZONE_ID);
+    }
+}
