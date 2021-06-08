21dc755dd01a8aac85875a068ac6f0c80ace5775
Schahin Rajab <schahin.rajab@tink.com>
2021-06-08 11:48:31 +0200

    fix(dk/jyske): handle null account response

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/jyskebank/fetcher/transactionalaccount/JyskeBankAccountFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/jyskebank/fetcher/transactionalaccount/JyskeBankAccountFetcher.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
+import org.apache.commons.collections4.ListUtils;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.jyskebank.JyskeBankApiClient;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.jyskebank.JyskeConstants.Storage;
 import se.tink.backend.aggregation.agents.nxgen.dk.banks.jyskebank.fetcher.transactionalaccount.entities.AccountsEntity;
@@ -25,7 +26,7 @@ public class JyskeBankAccountFetcher
     public Collection<TransactionalAccount> fetchAccounts() {
         final AccountResponse accountResponse = apiClient.fetchAccounts();
 
-        return accountResponse.stream()
+        return ListUtils.emptyIfNull(accountResponse).stream()
                 .filter(AccountsEntity::isTransactionalAccount)
                 .map(AccountsEntity::toTinkAccount)
                 .filter(Optional::isPresent)
