83106f06e66a7aa5f298a05c12606798aee9ab94
Schahin Rajab <schahin.rajab@tink.com>
2021-06-08 21:44:53 +0200

    test(dk/jyske): add test for credit card fetcher
    mock test for credit card account fetching and transactions

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/jyskebank/JyskeBankTestData.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/jyskebank/JyskeBankTestData.java
@@ -4,6 +4,18 @@ public class JyskeBankTestData {
     private static final String TEST_DATA_PATH =
             "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/jyskebank/resources/";
 
+    public static class CreditCardTestData {
+        private static final String CREDIT_CARD_TEST_DATA_DIR = TEST_DATA_PATH + "creditcard/";
+
+        public static final String CREDIT_CARD_ACCOUNTS_FILE =
+                CREDIT_CARD_TEST_DATA_DIR + "credit_card_accounts.json";
+        public static final String CREDIT_CARD_TRANSACTIONS_PAGE_1_FILE =
+                CREDIT_CARD_TEST_DATA_DIR + "credit_card_transactions_page_1.json";
+        public static final String CREDIT_CARD_TRANSACTIONS_PAGE_2_FILE =
+                CREDIT_CARD_TEST_DATA_DIR + "credit_card_transactions_page_2.json";
+        public static final String CREDIT_CARD_1_ID = "1234:1234567";
+    }
+
     public static class InvestmentTestData {
         private static final String INVESTMENT_TEST_DATA_DIR = TEST_DATA_PATH + "investment/";
 
