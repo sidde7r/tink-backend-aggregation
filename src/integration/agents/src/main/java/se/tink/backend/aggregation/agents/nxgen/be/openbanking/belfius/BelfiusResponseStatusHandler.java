0bcb7bad02282744e550400d7e743dd42469b3bd
Lukasz Binkiewicz <lukasz.binkiewicz@tink.se>
2021-06-14 09:26:35 +0200

    fix(BE/Belfius): Add handling for internal_server_error
    BelfiusResponseStatusHandler is responsible for detecting errors in responses. New rule for internal_server_error has been added.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/belfius/BelfiusResponseStatusHandler.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/be/openbanking/belfius/BelfiusResponseStatusHandler.java
@@ -2,6 +2,7 @@ package se.tink.backend.aggregation.agents.nxgen.be.openbanking.belfius;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceError;
 import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
 import se.tink.backend.aggregation.nxgen.http.DefaultResponseStatusHandler;
 import se.tink.backend.aggregation.nxgen.http.request.HttpRequest;
@@ -27,6 +28,9 @@ public class BelfiusResponseStatusHandler extends DefaultResponseStatusHandler {
             persistentStorage.clear();
             throw SessionError.SESSION_EXPIRED.exception(
                     "User\\System has deactivated the consent");
+        } else if (httpResponse.getStatus() == 500
+                && httpResponse.getBody(String.class).contains("internal_server_error")) {
+            throw BankServiceError.BANK_SIDE_FAILURE.exception();
         }
         super.handleResponse(httpRequest, httpResponse);
     }
