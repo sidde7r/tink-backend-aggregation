1bcb6742b730ed320924cc4c3ba585f505bf63aa
Jan Ossowski <jan.ossowski@tink.se>
2021-06-17 01:57:00 +0200

    feat(cmcic): add getting default language from locale

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/openbanking/targobank/TargoBankAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/openbanking/targobank/TargoBankAgent.java
@@ -1,6 +1,8 @@
 package se.tink.backend.aggregation.agents.nxgen.es.openbanking.targobank;
 
 import com.google.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModules;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cmcic.CmcicAgent;
@@ -13,6 +15,20 @@ import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponen
 @AgentCapabilities(generateFromImplementedExecutors = true)
 public final class TargoBankAgent extends CmcicAgent {
 
+    private static final String AUTH_URL_FORMAT =
+            "https://www.targobank.es/oauth2/%s/banque/oauth2_authorization.aspx";
+    private static final Map<String, String> SUPPORTED_LANGUAGES = new HashMap<>();
+    private static final String PRIMARY_LANGUAGE = "es";
+
+    // Targobank requires "cat" which is ISO639-3 code for Catalan,
+    // while all the other languages for Targo and other CMCIC banks are in ISO639-1 format.
+    // Therefore, this beautiful hack is introduced.
+    static {
+        SUPPORTED_LANGUAGES.put("ca", "cat");
+        SUPPORTED_LANGUAGES.put("es", "es");
+        SUPPORTED_LANGUAGES.put("fr", "fr");
+    }
+
     @Inject
     public TargoBankAgent(AgentComponentProvider componentProvider, QsealcSigner qsealcSigner) {
         super(
@@ -21,6 +37,15 @@ public final class TargoBankAgent extends CmcicAgent {
                 new CmcicAgentConfig(
                         "https://oauth2-apiii.e-i.com",
                         "/targobank-es/",
-                        "https://www.targobank.es/oauth2/es/banque/oauth2_authorization.aspx"));
+                        getLocalizedAuthUrl(componentProvider)));
+    }
+
+    private static String getLocalizedAuthUrl(AgentComponentProvider componentProvider) {
+        String preferredLanguage =
+                CmcicAgent.getPreferredLanguage(
+                        componentProvider.getCredentialsRequest().getUser().getLocale(),
+                        SUPPORTED_LANGUAGES.keySet(),
+                        PRIMARY_LANGUAGE);
+        return String.format(AUTH_URL_FORMAT, SUPPORTED_LANGUAGES.get(preferredLanguage));
     }
 }
