89053e7156946635f9094ffb571fdd9bedab9541
Artur Ciskowski <artur.ciskowski@tink.se>
2021-06-16 18:42:13 +0200

    fix(PT/bancoctt): add retry filter for consent status not final

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/bancoctt/BancoCttAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/bancoctt/BancoCttAgent.java
@@ -7,6 +7,7 @@ import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModules;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsProgressiveBaseAgent;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.filter.ConsentStatusRetryFilter;
 import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
 import se.tink.backend.aggregation.eidassigner.QsealcSigner;
 import se.tink.backend.aggregation.eidassigner.module.QSealcSignerModuleRSASHA256;
@@ -22,5 +23,6 @@ public final class BancoCttAgent extends SibsProgressiveBaseAgent {
             AgentsServiceConfiguration configuration,
             QsealcSigner qsealcSigner) {
         super(agentComponentProvider, configuration, qsealcSigner);
+        client.addFilter(new ConsentStatusRetryFilter());
     }
 }
