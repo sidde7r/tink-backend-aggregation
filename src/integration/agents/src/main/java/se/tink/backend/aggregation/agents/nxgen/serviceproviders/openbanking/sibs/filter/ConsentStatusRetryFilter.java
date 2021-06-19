1301ae987964cb2718caf14f6d7fcada660480c3
Lukasz Binkiewicz <lukasz.binkiewicz@tink.se>
2021-06-19 09:24:44 +0200

    fix(PT/BancoCtt): Fixing filter
    The current implementation breaks the flow and we get exception when calling method 'ConsentStatus.valueOf'. By adding FilterOrder this filter will be the last on the list and it will allow for other filters to process first

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/filter/ConsentStatusRetryFilter.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/filter/ConsentStatusRetryFilter.java
@@ -3,12 +3,14 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.si
 import java.util.Optional;
 import java.util.regex.Pattern;
 import javax.net.ssl.SSLHandshakeException;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.authenticator.entity.ConsentStatus;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.authenticator.rpc.ConsentStatusResponse;
 import se.tink.backend.aggregation.nxgen.http.exceptions.client.HttpClientException;
+import se.tink.backend.aggregation.nxgen.http.filter.engine.FilterOrder;
+import se.tink.backend.aggregation.nxgen.http.filter.engine.FilterPhases;
 import se.tink.backend.aggregation.nxgen.http.filter.filters.retry.AbstractRetryFilter;
 import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
 
+@FilterOrder(category = FilterPhases.REQUEST_HANDLE, order = Integer.MAX_VALUE)
 public class ConsentStatusRetryFilter extends AbstractRetryFilter {
 
     public static final Pattern URL_PATTERN = Pattern.compile(".*/consent.*/status");
@@ -34,10 +36,7 @@ public class ConsentStatusRetryFilter extends AbstractRetryFilter {
     }
 
     private boolean isConsentStatusNotFinal(HttpResponse response) {
-        return Optional.ofNullable(response.getBody(ConsentStatusResponse.class))
-                .map(ConsentStatusResponse::getConsentStatus)
-                .map(ConsentStatus::isNotFinalStatus)
-                .orElse(false);
+        return response.getBody(ConsentStatusResponse.class).getConsentStatus().isNotFinalStatus();
     }
 
     @Override
