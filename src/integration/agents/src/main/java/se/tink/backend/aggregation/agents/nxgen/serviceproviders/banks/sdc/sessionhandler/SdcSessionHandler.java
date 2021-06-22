c9a766da21606a733322864dc5ce17cb22538b50
Daniel Obrębski <daniel.obrebski@tink.se>
2021-06-22 23:20:00 +0200

    fix(sdc/session): don't throw session expired on narrow exceptions

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/sessionhandler/SdcSessionHandler.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/sdc/sessionhandler/SdcSessionHandler.java
@@ -1,11 +1,9 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.sessionhandler;
 
-import org.apache.http.HttpStatus;
 import se.tink.backend.aggregation.agents.exceptions.SessionException;
 import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.sdc.SdcApiClient;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
-import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
 
 public class SdcSessionHandler implements SessionHandler {
 
@@ -24,12 +22,8 @@ public class SdcSessionHandler implements SessionHandler {
     public void keepAlive() throws SessionException {
         try {
             bankClient.fetchAgreements();
-        } catch (HttpResponseException hre) {
-            final int status = hre.getResponse().getStatus();
-            if (status == HttpStatus.SC_UNAUTHORIZED || status == HttpStatus.SC_FORBIDDEN) {
-                throw SessionError.SESSION_EXPIRED.exception(hre);
-            }
-            throw hre;
+        } catch (Exception e) {
+            throw SessionError.SESSION_EXPIRED.exception(e);
         }
     }
 }
