983d7fe50d489612097386ab37007fdc89344749
Mateusz Wiski <mateusz.wiski@tink.com>
2021-07-01 13:29:00 +0200

    fix(dk/NemID): correct the order of NemId methods
    [ITE-2843]

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/nemid/ss/steps/choosemethod/NemIdChoose2FAMethodField.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/authentication/multifactor/nemid/ss/steps/choosemethod/NemIdChoose2FAMethodField.java
@@ -1,7 +1,5 @@
 package se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.nemid.ss.steps.choosemethod;
 
-import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.AccessLevel;
@@ -21,15 +19,14 @@ class NemIdChoose2FAMethodField {
     private static final LocalizableKey DESCRIPTION =
             new LocalizableKey("Select how you want to confirm the NemID authentication");
 
-    public static Field build(Catalog catalog, Collection<NemId2FAMethod> nemId2FAMethods) {
+    public static Field build(Catalog catalog, List<NemId2FAMethod> nemId2FAMethods) {
         return CommonFields.Selection.build(
                 catalog, DESCRIPTION, prepareSelectOptions(catalog, nemId2FAMethods));
     }
 
     private static List<SelectOption> prepareSelectOptions(
-            Catalog catalog, Collection<NemId2FAMethod> nemId2FAMethods) {
+            Catalog catalog, List<NemId2FAMethod> nemId2FAMethods) {
         return nemId2FAMethods.stream()
-                .sorted(Comparator.comparing(NemId2FAMethod::getSupplementalInfoOrder))
                 .map(method -> prepareSelectOption(catalog, method))
                 .collect(Collectors.toList());
     }
