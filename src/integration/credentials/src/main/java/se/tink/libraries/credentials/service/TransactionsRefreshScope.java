198db396a79aa35af09b4290b3a3fe35ccec82b4
Wojciech Fiszer <wojciech.fiszer@tink.com>
2021-07-02 09:40:17 +0200

    refactor(as): Moved date limit logic to TransactionsRefreshScope

--- src/integration/credentials/src/main/java/se/tink/libraries/credentials/service/TransactionsRefreshScope.java
+++ src/integration/credentials/src/main/java/se/tink/libraries/credentials/service/TransactionsRefreshScope.java
@@ -1,10 +1,15 @@
 package se.tink.libraries.credentials.service;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import se.tink.libraries.account.AccountIdentifier;
 
 @Getter
 @Setter
@@ -18,4 +23,33 @@ public class TransactionsRefreshScope {
 
     /** It is a set of account transaction refresh scope objects. */
     private Set<AccountTransactionsRefreshScope> accounts;
+
+    @JsonIgnore
+    public Optional<LocalDate> getTransactionBookedDateGteForAccountIdentifiers(
+            Collection<AccountIdentifier> identifiers) {
+
+        Optional<LocalDate> defaultLimit = Optional.ofNullable(transactionBookedDateGte);
+        if (accounts == null || accounts.isEmpty()) {
+            return defaultLimit;
+        }
+
+        Optional<LocalDate> accountLimit =
+                accounts.stream()
+                        .filter(
+                                it -> {
+                                    Set<AccountIdentifier> accountScopeIdentifiers =
+                                            it.getAccountIdentifiers().stream()
+                                                    .map(AccountIdentifier::createOrThrow)
+                                                    .collect(Collectors.toSet());
+                                    return accountScopeIdentifiers.removeAll(identifiers);
+                                })
+                        .findAny()
+                        .map(AccountTransactionsRefreshScope::getTransactionBookedDateGte);
+
+        if (accountLimit.isPresent()) {
+            return accountLimit;
+        }
+
+        return defaultLimit;
+    }
 }
--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/RefreshScopeTransactionPaginationHelper.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/RefreshScopeTransactionPaginationHelper.java
@@ -2,18 +2,14 @@ package se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagina
 
 import java.time.LocalDate;
 import java.time.ZoneOffset;
-import java.util.Collection;
 import java.util.Date;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.aggregation.nxgen.core.account.Account;
-import se.tink.libraries.account.AccountIdentifier;
-import se.tink.libraries.credentials.service.AccountTransactionsRefreshScope;
 import se.tink.libraries.credentials.service.RefreshScope;
+import se.tink.libraries.credentials.service.TransactionsRefreshScope;
 
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 @Slf4j
@@ -32,53 +28,11 @@ public class RefreshScopeTransactionPaginationHelper extends TransactionPaginati
             return Optional.empty();
         }
 
-        Optional<Date> defaultLimit =
-                Optional.ofNullable(refreshScope.getTransactions().getTransactionBookedDateGte())
-                        .map(RefreshScopeTransactionPaginationHelper::localDateToDate);
-        if (refreshScope.getTransactions().getAccounts() == null
-                || refreshScope.getTransactions().getAccounts().isEmpty()) {
-            log.info("Returning all transactions date limit {}", defaultLimit);
-            return defaultLimit;
-        }
-
-        Optional<AccountTransactionsRefreshScope> accountRefreshScope =
-                getAccountTransactionsRefreshScope(account);
-
-        if (accountRefreshScope.isPresent()
-                && accountRefreshScope.get().getTransactionBookedDateGte() != null) {
-            Date accountLimit =
-                    localDateToDate(accountRefreshScope.get().getTransactionBookedDateGte());
-            log.info("Returning account transactions date limit {}", accountLimit);
-            return Optional.of(accountLimit);
-        }
-
-        log.info("Returning all transactions date limit {}", defaultLimit);
-        return defaultLimit;
-    }
-
-    private Optional<AccountTransactionsRefreshScope> getAccountTransactionsRefreshScope(
-            Account account) {
-        if (refreshScope == null
-                || refreshScope.getTransactions() == null
-                || refreshScope.getTransactions().getAccounts() == null) {
-            return Optional.empty();
-        }
-        return refreshScope.getTransactions().getAccounts().stream()
-                .filter(
-                        it ->
-                                accountTransactionsRefreshScopeContainsAnyIdentifier(
-                                        it, account.getIdentifiers()))
-                .findAny();
-    }
-
-    private static boolean accountTransactionsRefreshScopeContainsAnyIdentifier(
-            AccountTransactionsRefreshScope scope,
-            Collection<AccountIdentifier> accountIdentifiers) {
-        Set<AccountIdentifier> refreshScopeAccountIdentifiers =
-                scope.getAccountIdentifiers().stream()
-                        .map(AccountIdentifier::createOrThrow)
-                        .collect(Collectors.toSet());
-        return refreshScopeAccountIdentifiers.removeAll(accountIdentifiers);
+        TransactionsRefreshScope transactionsRefreshScope = refreshScope.getTransactions();
+        Optional<LocalDate> limit =
+                transactionsRefreshScope.getTransactionBookedDateGteForAccountIdentifiers(
+                        account.getIdentifiers());
+        return limit.map(RefreshScopeTransactionPaginationHelper::localDateToDate);
     }
 
     private static Date localDateToDate(LocalDate localDate) {
