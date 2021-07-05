d83e156f03659c1a8bc7cd46605cd717f6dc5481
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-07-05 14:27:13 +0200

    feat(lv-luminor): added LV agent for luminor

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/lv/openbanking/luminor/LuminorLVAgent.java
@@ -0,0 +1,20 @@
+package se.tink.backend.aggregation.agents.nxgen.lv.openbanking.luminor;
+
+import com.google.inject.Inject;
+import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.LuminorBaseAgent;
+import se.tink.backend.aggregation.client.provider_configuration.rpc.Capability;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
+
+@AgentCapabilities({
+    Capability.CHECKING_ACCOUNTS,
+    Capability.SAVINGS_ACCOUNTS,
+    Capability.TRANSFERS
+})
+public class LuminorLVAgent extends LuminorBaseAgent {
+
+    @Inject
+    public LuminorLVAgent(AgentComponentProvider componentProvider) {
+        super(componentProvider);
+    }
+}
