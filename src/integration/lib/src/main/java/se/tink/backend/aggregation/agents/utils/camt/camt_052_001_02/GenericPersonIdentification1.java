0b023a34865a1e24c68c2f51303c8b3548cfe139
Berk Gedik <berk.gedik@tink.com>
2021-07-05 20:34:43 +0200

    refactor(java11): updated camt deps

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/camt/camt_052_001_02/GenericPersonIdentification1.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/camt/camt_052_001_02/GenericPersonIdentification1.java
@@ -8,10 +8,10 @@
 
 package se.tink.backend.aggregation.agents.utils.camt.camt_052_001_02;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlType;
 
 /**
  * Java class for GenericPersonIdentification1 complex type.
