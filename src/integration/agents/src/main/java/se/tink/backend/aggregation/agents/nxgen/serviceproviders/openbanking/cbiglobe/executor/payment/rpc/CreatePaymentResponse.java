63b555105ea6adb95ab0c4dd32e37188df78bda4
vishwanath sharma <vishwanath.sharma@tink.se>
2021-07-07 11:35:41 +0200

    fix(payment): BPM recurring payment

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/executor/payment/rpc/CreatePaymentResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/cbiglobe/executor/payment/rpc/CreatePaymentResponse.java
@@ -6,10 +6,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.executor.payment.entities.LinksEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.executor.payment.enums.CbiGlobePaymentStatus;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.controllers.payment.PaymentResponse;
+import se.tink.libraries.payment.enums.PaymentStatus;
 import se.tink.libraries.payment.rpc.Payment;
 
 @JsonObject
@@ -30,9 +32,18 @@ public class CreatePaymentResponse {
 
     @JsonIgnore
     public PaymentResponse toTinkPaymentResponse(Payment tinkPayment) {
-        tinkPayment.setStatus(
-                CbiGlobePaymentStatus.mapToTinkPaymentStatus(
-                        CbiGlobePaymentStatus.fromString(transactionStatus)));
+        // In case of BPM's special handling, payment is explicitly marked as signed
+        // in case of RCVD, hence we dont want to again map transactionStatus
+        // because it will be null and lead to CREATED.
+        if (!(PaymentStatus.SIGNED.equals(tinkPayment.getStatus())
+                && CbiGlobeConstants.PSUAuthenticationStatus.AUTHENTICATED.equalsIgnoreCase(
+                        psuAuthenticationStatus)
+                && CbiGlobeConstants.PSUAuthenticationStatus.VERIFIED.equalsIgnoreCase(
+                        scaStatus))) {
+            tinkPayment.setStatus(
+                    CbiGlobePaymentStatus.mapToTinkPaymentStatus(
+                            CbiGlobePaymentStatus.fromString(transactionStatus)));
+        }
         if (paymentId != null) {
             tinkPayment.setUniqueId(paymentId); // bank Unique payment Id
         }
