20dcc79b794181e7213e6910096f56c9f1e9b20e
vishwanath sharma <vishwanath.sharma@tink.se>
2021-07-08 12:34:48 +0200

    fix(payment): adapted for wiremock

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/bpm/BpmAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/it/openbanking/bpm/BpmAgent.java
@@ -13,21 +13,14 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbi
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiGlobeApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.cbiglobe.CbiStorageProvider;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
-import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date.LocalDateTimeSource;
-import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.randomness.RandomValueGenerator;
 
 @AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS, TRANSFERS})
 @AgentPisCapability(capabilities = {SEPA_CREDIT_TRANSFER, PIS_SEPA_RECURRING_PAYMENTS})
 public final class BpmAgent extends CbiGlobeAgent {
 
-    private final RandomValueGenerator randomValueGenerator;
-    private final LocalDateTimeSource localDateTimeSource;
-
     @Inject
     public BpmAgent(AgentComponentProvider agentComponentProvider) {
         super(agentComponentProvider);
-        this.randomValueGenerator = agentComponentProvider.getRandomValueGenerator();
-        this.localDateTimeSource = agentComponentProvider.getLocalDateTimeSource();
     }
 
     @Override
