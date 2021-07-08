86a1b295267c6e76e6a01bebff188dbd95d30dfb
Berk Gedik <berk.gedik@tink.com>
2021-07-09 01:00:41 +0200

    test(auth-abort): implemented fake cache

--- src/libraries/cache/src/main/java/se/tink/libraries/cache/FakeCacheClient.java
+++ src/libraries/cache/src/main/java/se/tink/libraries/cache/FakeCacheClient.java
@@ -1,42 +1,34 @@
 package se.tink.libraries.cache;
 
 import com.google.common.util.concurrent.Futures;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Future;
 
 public class FakeCacheClient implements CacheClient {
 
-    private Map<String, Object> cache;
+    private final Map<String, Object> cache = new ConcurrentHashMap<>();
 
-    public FakeCacheClient() {
-        this.cache = new HashMap<>();
-    }
-
-    private String getInternalKey(CacheScope scope, String key) {
+    private static String getInternalKey(CacheScope scope, String key) {
         return scope.toString() + "-" + key;
     }
 
     @Override
     public Future<?> set(CacheScope scope, String key, int expiredTime, Object object) {
-        this.cache.put(getInternalKey(scope, key), object);
+        cache.put(getInternalKey(scope, key), object);
         return Futures.immediateFuture(object);
     }
 
     @Override
     public Object get(CacheScope scope, String key) {
-        return cache.containsKey(getInternalKey(scope, key))
-                ? this.cache.get(getInternalKey(scope, key))
-                : null;
+        return cache.get(getInternalKey(scope, key));
     }
 
     @Override
     public Future<Boolean> delete(CacheScope scope, String key) {
-        throw new UnsupportedOperationException("Not implemented");
+        return Futures.immediateFuture(cache.remove(getInternalKey(scope, key)) != null);
     }
 
     @Override
-    public void shutdown() {
-        throw new UnsupportedOperationException("Not implemented");
-    }
+    public void shutdown() {}
 }
