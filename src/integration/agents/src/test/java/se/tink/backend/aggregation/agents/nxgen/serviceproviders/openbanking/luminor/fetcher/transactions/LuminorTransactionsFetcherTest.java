c72809133c1fa0fb7a413f3693d5be65d544c409
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-07-12 19:39:49 +0200

    fix(baltics/luminor): fixed test
    refactored test to work for refatored transactionFetcher

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/transactions/LuminorTransactionsFetcherTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/transactions/LuminorTransactionsFetcherTest.java
@@ -4,10 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.util.Collections;
-import java.util.List;
+import java.util.Objects;
+import org.junit.Assert;
 import org.junit.Test;
+import se.tink.backend.aggregation.agents.models.TransactionDateType;
+import se.tink.backend.aggregation.agents.models.TransactionExternalSystemIdType;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.fetcher.TransactionsResponse;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 import se.tink.libraries.amount.ExactCurrencyAmount;
@@ -27,19 +28,26 @@ public class LuminorTransactionsFetcherTest {
                         Paths.get(TEST_DATA_PATH, "transactions_response.json").toFile(),
                         TransactionsResponse.class);
         // when
-        Transaction result = (Transaction) transactionsResponse.getTinkTransactions().get(0);
+        Transaction result =
+                transactionsResponse.getTinkTransactions().stream().findFirst().orElse(null);
 
         // then
-        assertThat(result)
-                .isEqualToComparingFieldByFieldRecursively(getExpectedAccountsResponse().get(0));
-    }
-
-    private List<Transaction> getExpectedAccountsResponse() {
-        return Collections.singletonList(
-                Transaction.builder()
-                        .setDate(LocalDate.of(2020, 3, 15))
-                        .setAmount(new ExactCurrencyAmount(BigDecimal.valueOf(152.45), "EUR"))
-                        .setDescription("Invoice x29876")
-                        .build());
+        Assert.assertEquals(
+                new ExactCurrencyAmount(BigDecimal.valueOf(152.45), "EUR"),
+                Objects.requireNonNull(result).getAmount());
+        Assert.assertEquals(("Invoice x29876"), Objects.requireNonNull(result).getDescription());
+        Assert.assertEquals(
+                ("SCOR"), Objects.requireNonNull(result).getProprietaryFinancialInstitutionType());
+        Assert.assertEquals(
+                ("RF18539007547034"), Objects.requireNonNull(result).getTransactionReference());
+        Assert.assertEquals(("Jonas Jonaitis"), Objects.requireNonNull(result).getMerchantName());
+        Assert.assertEquals(1, Objects.requireNonNull(result).getExternalSystemIds().size());
+        assertThat(result.getExternalSystemIds())
+                .containsKey(TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID);
+        assertThat(result.getExternalSystemIds()).containsValue("40367613");
+        Assert.assertEquals(
+                2, Objects.requireNonNull(result).getTransactionDates().getDates().size());
+        assertThat(result.getTransactionDates().getDates().get(0).getType())
+                .isEqualTo(TransactionDateType.VALUE_DATE);
     }
 }
