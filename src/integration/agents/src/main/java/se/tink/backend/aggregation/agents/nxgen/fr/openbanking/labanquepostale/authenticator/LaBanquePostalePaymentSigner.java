bc1ee785c541bff67f000d5027fdcba6c380ec20
Sai He <sai.he@tink.se>
2021-07-12 11:42:56 +0200

    chore(FR/LBG): To add a debug logger

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/labanquepostale/authenticator/LaBanquePostalePaymentSigner.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/labanquepostale/authenticator/LaBanquePostalePaymentSigner.java
@@ -2,11 +2,13 @@ package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.labanquepostale.
 
 import java.util.Map;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.aggregation.agents.exceptions.payment.PaymentAuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.payment.PaymentAuthorizationException;
 import se.tink.backend.aggregation.agents.exceptions.payment.PaymentRejectedException;
 import se.tink.libraries.signableoperation.enums.InternalStatus;
 
+@Slf4j
 public class LaBanquePostalePaymentSigner {
     private static final String PSU_AUTHORIZATION_FACTOR_KEY = "psuAuthenticationFactor";
     String paymentAuthorizationUrl;
@@ -35,6 +37,7 @@ public class LaBanquePostalePaymentSigner {
         // Related to @SupplementaryDataEntity
         if (!callback.containsKey(PSU_AUTHORIZATION_FACTOR_KEY)
                 || callback.getOrDefault("error", "").equalsIgnoreCase("authentication_error")) {
+            callback.forEach((k, v) -> log.info(k + " : " + v));
             throw new PaymentAuthorizationException(
                     "The Authorization failed during SCA",
                     InternalStatus.PAYMENT_AUTHORIZATION_FAILED);
