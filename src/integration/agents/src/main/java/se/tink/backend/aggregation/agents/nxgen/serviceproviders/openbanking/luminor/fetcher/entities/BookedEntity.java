7fb11c052359658e36894a54a422fc912e28b36c
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-07-12 19:06:59 +0200

    fix(baltics/luminor): refactoring transactions
    to populate more data points for transactions

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/entities/BookedEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/entities/BookedEntity.java
@@ -5,37 +5,36 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
 import java.util.Optional;
 import lombok.Getter;
+import se.tink.backend.aggregation.agents.models.TransactionExternalSystemIdType;
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.backend.aggregation.nxgen.core.transaction.AggregationTransaction;
 import se.tink.backend.aggregation.nxgen.core.transaction.AggregationTransaction.Builder;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 import se.tink.backend.aggregation.nxgen.core.transaction.TransactionDates;
 import se.tink.libraries.chrono.AvailableDateInformation;
 
 @JsonObject
+@Getter
 public class BookedEntity {
-    @Getter String transactionId;
-    @Getter String creditorName;
-    @Getter String creditorAccount;
+    String transactionId;
+    String creditorName;
+    IbanAccountEntity creditorAccount;
+
     @Getter String debtorName;
-    @Getter String debtorAccount;
+    IbanAccountEntity debtorAccount;
 
     @JsonProperty("transactionAmount")
-    @Getter
     TransactionAmountEntity transactionAccountEntity;
 
-    @Getter LocalDate bookingDate;
-    @Getter LocalDate valueDate;
-    @Getter String bankTransactionCode;
-    @Getter String remittanceInformationUnstructured;
+    LocalDate bookingDate;
+    LocalDate valueDate;
+    String bankTransactionCode;
+    String remittanceInformationUnstructured;
 
     @JsonProperty("remittanceInformationStructured")
-    @Getter
     RemittanceInformationStructuredEntity remittanceInformationStructuredEntity;
 
     @JsonIgnore
-    public AggregationTransaction toTinkTransaction() {
-
+    public Transaction toTinkTransaction() {
         Builder builder =
                 Transaction.builder()
                         .setAmount(getTransactionAccountEntity().toAmount())
@@ -45,13 +44,20 @@ public class BookedEntity {
                                         .orElse(
                                                 getRemittanceInformationStructuredEntity()
                                                         .reference))
-                        .setPending(false);
-        /* .setTransactionDates(
-        getTinkTransactionDates(getBookingDate(), getValueDate()));*/
-        return (AggregationTransaction) builder.build();
+                        .setPending(false)
+                        .setTransactionDates(getTinkTransactionDates())
+                        .addExternalSystemIds(
+                                TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID,
+                                transactionId)
+                        .setMerchantName(creditorName)
+                        .setProprietaryFinancialInstitutionType(
+                                getRemittanceInformationStructuredEntity().getReferenceType())
+                        .setTransactionReference(
+                                getRemittanceInformationStructuredEntity().getReference());
+        return (Transaction) builder.build();
     }
 
-    public TransactionDates getTinkTransactionDates(LocalDate valueDate, LocalDate bookingDate) {
+    public TransactionDates getTinkTransactionDates() {
         TransactionDates.Builder builder = TransactionDates.builder();
 
         if (valueDate != null) {
