60197eb3ca329d1cf76b33f1e1f162ec7a45c381
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-07-12 19:12:39 +0200

    fix(baltics/luminor): added accountHolderName to test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/accounts/LuminorAccountFetcherTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/accounts/LuminorAccountFetcherTest.java
@@ -4,8 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.LuminorConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.LuminorConstants.StorageKeys;
@@ -33,36 +31,32 @@ public class LuminorAccountFetcherTest {
                         AccountsResponse.class);
         // when
         TransactionalAccount result =
-                accountsResponse.getAccounts().get(0).toTinkAccount().orElse(null);
+                accountsResponse.getAccounts().get(0).toTinkAccount("John Doe").orElse(null);
 
         // then
-        assertThat(result)
-                .isEqualToComparingFieldByFieldRecursively(getExpectedAccountsResponse().get(0));
+        assertThat(result).isEqualToComparingFieldByFieldRecursively(getExpectedAccountsResponse());
     }
 
-    private List<TransactionalAccount> getExpectedAccountsResponse() {
-        return Collections.singletonList(
-                TransactionalAccount.nxBuilder()
-                        .withTypeAndFlagsFrom(
-                                LuminorConstants.ACCOUNT_TYPE_MAPPER, "Payment card account")
-                        .withBalance(
-                                BalanceModule.of(
-                                        new ExactCurrencyAmount(BigDecimal.valueOf(152.45), "EUR")))
-                        .withId(
-                                IdModule.builder()
-                                        .withUniqueIdentifier("LT594010049500047594")
-                                        .withAccountNumber(
-                                                "A01A77CCB3F8EE9F4B56E0C26EAAB19AD36A9741F1560DF57B71B0C287794542")
-                                        .withAccountName("My main account")
-                                        .addIdentifier(new IbanIdentifier("LT594010049500047594"))
-                                        .build())
-                        .putInTemporaryStorage(StorageKeys.ACCOUNT_ID, "LT594010049500047594")
-                        .setApiIdentifier(
-                                "A01A77CCB3F8EE9F4B56E0C26EAAB19AD36A9741F1560DF57B71B0C287794542")
-                        .setBankIdentifier(
-                                "A01A77CCB3F8EE9F4B56E0C26EAAB19AD36A9741F1560DF57B71B0C287794542")
-                        .addAccountFlags(AccountFlag.PSD2_PAYMENT_ACCOUNT)
-                        .build()
-                        .orElseThrow(IllegalStateException::new));
+    private TransactionalAccount getExpectedAccountsResponse() {
+        return TransactionalAccount.nxBuilder()
+                .withTypeAndFlagsFrom(LuminorConstants.ACCOUNT_TYPE_MAPPER, "Payment card account")
+                .withBalance(
+                        BalanceModule.of(
+                                new ExactCurrencyAmount(BigDecimal.valueOf(152.45), "EUR")))
+                .withId(
+                        IdModule.builder()
+                                .withUniqueIdentifier("LT594010049500047594")
+                                .withAccountNumber(
+                                        "A01A77CCB3F8EE9F4B56E0C26EAAB19AD36A9741F1560DF57B71B0C287794542")
+                                .withAccountName("My main account")
+                                .addIdentifier(new IbanIdentifier("LT594010049500047594"))
+                                .build())
+                .putInTemporaryStorage(StorageKeys.ACCOUNT_ID, "LT594010049500047594")
+                .setApiIdentifier(
+                        "A01A77CCB3F8EE9F4B56E0C26EAAB19AD36A9741F1560DF57B71B0C287794542")
+                .addAccountFlags(AccountFlag.PSD2_PAYMENT_ACCOUNT)
+                .addHolderName("John Doe")
+                .build()
+                .orElseThrow(IllegalStateException::new);
     }
 }
