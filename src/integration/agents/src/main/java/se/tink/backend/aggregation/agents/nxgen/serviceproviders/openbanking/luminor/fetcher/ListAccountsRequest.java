73abce4f870906b234ebd189a5a6fd5e20bfe006
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-07-12 18:52:52 +0200

    fix(baltics/luminor): removed unused class

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/ListAccountsRequest.java
+++
@@ -1,6 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.fetcher;
-
-import lombok.NoArgsConstructor;
-
-@NoArgsConstructor
-public class ListAccountsRequest {}
