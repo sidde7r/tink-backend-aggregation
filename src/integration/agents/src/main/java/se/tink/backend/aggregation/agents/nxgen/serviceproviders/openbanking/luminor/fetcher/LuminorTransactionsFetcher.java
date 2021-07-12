7131bc2b6a53a49c367b3d1ea0fa2c4f63960240
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-07-12 19:35:15 +0200

    fix(baltics/luminor): refactor fetcher
    refactored to use paginatorResponse

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/LuminorTransactionsFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/LuminorTransactionsFetcher.java
@@ -1,27 +1,48 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.fetcher;
 
-import java.time.LocalDate;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import lombok.RequiredArgsConstructor;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.LuminorApiClient;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcher;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponse;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.PaginatorResponseImpl;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginator;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
-import se.tink.backend.aggregation.nxgen.core.transaction.AggregationTransaction;
 
-public class LuminorTransactionsFetcher implements TransactionFetcher<TransactionalAccount> {
+@RequiredArgsConstructor
+public class LuminorTransactionsFetcher implements TransactionDatePaginator<TransactionalAccount> {
     private final LuminorApiClient apiClient;
 
-    public LuminorTransactionsFetcher(LuminorApiClient apiClient) {
-        this.apiClient = apiClient;
+    @Override
+    public PaginatorResponse getTransactionsFor(
+            TransactionalAccount account, Date fromDate, Date toDate) {
+        Date today90 = get90DaysBackOnly();
+
+        if (fromDate.before(today90)) {
+            return PaginatorResponseImpl.createEmpty(false);
+        }
+        String formattedFromDate = formatDate(fromDate);
+        String formattedToDate = formatDate(toDate);
+        return apiClient.getTransactions(
+                account.getAccountNumber(), formattedFromDate, formattedToDate);
     }
 
-    @Override
-    public List<AggregationTransaction> fetchTransactionsFor(TransactionalAccount account) {
-        LocalDate toDate = LocalDate.now().minusDays(90);
-        LocalDate fromDate = LocalDate.now();
+    private Date get90DaysBackOnly() {
+        Date today = new Date();
+        Calendar cal = new GregorianCalendar();
+        cal.setTime(today);
+        cal.add(Calendar.DAY_OF_MONTH, -90);
+        return cal.getTime();
+    }
 
-        TransactionsResponse transactionsResponse =
-                apiClient.getTransactions(account.getAccountNumber(), toDate, fromDate);
-
-        return transactionsResponse.getTinkTransactions();
+    private String formatDate(Date date) {
+        final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return dateFormatter.format(date);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
