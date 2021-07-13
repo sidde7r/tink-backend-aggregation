ff7eb349df3f773b1f75205843984f076a4ffecf
Schahin Rajab <schahin.rajab@tink.com>
2021-07-13 16:25:00 +0200

    test(dk/jyske): fetch pension accounts
    fetch pension accounts in investment test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/jyskebank/JyskeBankTestData.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/dk/banks/jyskebank/JyskeBankTestData.java
@@ -22,6 +22,8 @@ public class JyskeBankTestData {
     public static class InvestmentTestData {
         private static final String INVESTMENT_TEST_DATA_DIR = TEST_DATA_PATH + "investment/";
 
+        public static final String PENSION_ACCOUNTS_FILE =
+                INVESTMENT_TEST_DATA_DIR + "pension_accounts.json";
         public static final String INVESTMENT_ACCOUNTS_FILE =
                 INVESTMENT_TEST_DATA_DIR + "investment_accounts.json";
         public static final String INVESTMENT_1_ID = "501287123";
