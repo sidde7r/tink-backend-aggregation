7a5a7b4120034e3f3dd12a6a1c93dce292d09cf6
Shivam Saini <shivam.saini@tink.com>
2021-07-15 12:52:38 +0200

    refactor(ee/seb): corrected formatting

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/ee/openbanking/seb/SebEEConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/ee/openbanking/seb/SebEEConstants.java
@@ -2,7 +2,6 @@ package se.tink.backend.aggregation.agents.nxgen.ee.openbanking.seb;
 
 public class SebEEConstants {
 
-  public static final String BIC = "EEUHEE2X";
-  public static final String MARKET_CODE = "EE";
-
+    public static final String BIC = "EEUHEE2X";
+    public static final String MARKET_CODE = "EE";
 }
