5cba14bc9c277567a6eb9319b7b325a1c27796be
Shivam Saini <shivam.saini@tink.com>
2021-07-15 13:41:37 +0200

    feat(lv/seb): added SEB agent for LV market

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/lv/openbanking/seb/SebLVAgent.java
@@ -0,0 +1,58 @@
+package se.tink.backend.aggregation.agents.nxgen.lv.openbanking.seb;
+
+import com.google.inject.Inject;
+import se.tink.backend.aggregation.agents.FetchAccountsResponse;
+import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
+import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
+import se.tink.backend.aggregation.agents.RefreshSavingsAccountsExecutor;
+import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbalticsbase.SebBalticsBaseAgent;
+import se.tink.backend.aggregation.client.provider_configuration.rpc.Capability;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
+import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
+import se.tink.backend.aggregation.nxgen.http.filter.filters.AccessExceededFilter;
+
+@AgentCapabilities({Capability.CHECKING_ACCOUNTS, Capability.SAVINGS_ACCOUNTS})
+public class SebLVAgent extends SebBalticsBaseAgent<SebLVApiClient>
+        implements RefreshCheckingAccountsExecutor, RefreshSavingsAccountsExecutor {
+
+    private final TransactionalAccountRefreshController transactionalAccountRefreshController;
+
+    @Inject
+    protected SebLVAgent(AgentComponentProvider componentProvider) {
+        super(componentProvider);
+        configureHttpClient(client);
+        this.apiClient = new SebLVApiClient(client, persistentStorage, request);
+        this.transactionalAccountRefreshController = getTransactionalAccountRefreshController();
+    }
+
+    private void configureHttpClient(TinkHttpClient client) {
+        client.addFilter(new AccessExceededFilter());
+    }
+
+    @Override
+    protected SebLVApiClient getApiClient() {
+        return this.apiClient;
+    }
+
+    @Override
+    public FetchAccountsResponse fetchCheckingAccounts() {
+        return transactionalAccountRefreshController.fetchCheckingAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchCheckingTransactions() {
+        return transactionalAccountRefreshController.fetchCheckingTransactions();
+    }
+
+    @Override
+    public FetchAccountsResponse fetchSavingsAccounts() {
+        return transactionalAccountRefreshController.fetchSavingsAccounts();
+    }
+
+    @Override
+    public FetchTransactionsResponse fetchSavingsTransactions() {
+        return transactionalAccountRefreshController.fetchSavingsTransactions();
+    }
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/lv/openbanking/seb/SebLVConstants.java
@@ -0,0 +1,7 @@
+package se.tink.backend.aggregation.agents.nxgen.lv.openbanking.seb;
+
+public class SebLVConstants {
+
+    public static final String BIC = "UNLALV2X";
+    public static final String MARKET_CODE = "LV";
+}
