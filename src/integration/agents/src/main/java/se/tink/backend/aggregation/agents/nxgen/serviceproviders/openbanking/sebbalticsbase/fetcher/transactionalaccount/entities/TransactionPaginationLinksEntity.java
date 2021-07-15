7cf75fb17743ac96a8b68f20d7696a1c019db171
Shivam Saini <shivam.saini@tink.com>
2021-07-15 12:48:55 +0200

    refactor(baltic/seb): added json annotation

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbalticsbase/fetcher/transactionalaccount/entities/TransactionPaginationLinksEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbalticsbase/fetcher/transactionalaccount/entities/TransactionPaginationLinksEntity.java
@@ -2,7 +2,9 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.se
 
 import com.google.common.base.Strings;
 import se.tink.backend.aggregation.agents.Href;
+import se.tink.backend.aggregation.annotations.JsonObject;
 
+@JsonObject
 public class TransactionPaginationLinksEntity {
 
     private Href self;
