7a4d24827a0d9c66206a4e338ade7c2a3d1f35b0
Daniel Obrębski <daniel.obrebski@tink.se>
2021-07-15 18:24:46 +0200

    feat(pl/agents): add common structures and api client
    [ITE-2640]

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/polishapi/common/BasePolishApiPostClient.java
@@ -0,0 +1,108 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.common;
+
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Headers.HeaderKeys.ACCEPT;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Headers.HeaderKeys.ACCEPT_CHARSET;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Headers.HeaderKeys.ACCEPT_ENCODING;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Headers.HeaderKeys.ACCEPT_LANGUAGE;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Headers.HeaderKeys.DATE;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Headers.HeaderKeys.X_IBM_CLIENT_ID;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Headers.HeaderKeys.X_IBM_CLIENT_SECRET;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Headers.HeaderValues.ACCEPT_CHARSET_VAL;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Headers.HeaderValues.GetClient.PSU_USER_AGENT_VAL;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Localization.DATE_TIME_FORMATTER_HEADERS;
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Localization.DATE_TIME_FORMATTER_REQUEST_HEADERS;
+
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import javax.ws.rs.core.MediaType;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.common.dto.requests.RequestHeaderEntity;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiPersistentStorage;
+import se.tink.backend.aggregation.configuration.agents.AgentConfiguration;
+import se.tink.backend.aggregation.configuration.agents.utils.CertificateUtils;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
+import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
+import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
+import se.tink.backend.aggregation.nxgen.http.filter.filterable.request.RequestBuilder;
+import se.tink.backend.aggregation.nxgen.http.url.URL;
+
+@RequiredArgsConstructor
+public class BasePolishApiPostClient {
+
+    private final TinkHttpClient httpClient;
+    private final AgentComponentProvider agentComponentProvider;
+    protected final AgentConfiguration<PolishApiConfiguration> configuration;
+    protected final PolishApiPersistentStorage persistentStorage;
+
+    protected RequestBuilder getRequestWithBaseHeaders(
+            URL requestUrl, ZonedDateTime requestTime, OAuth2Token token) {
+        PolishApiConfiguration apiConfiguration = configuration.getProviderSpecificConfiguration();
+        RequestBuilder header =
+                httpClient
+                        .request(requestUrl)
+                        .header(ACCEPT, MediaType.APPLICATION_JSON)
+                        .header(ACCEPT_CHARSET, ACCEPT_CHARSET_VAL)
+                        .header(ACCEPT_ENCODING, "deflate")
+                        .header(ACCEPT_LANGUAGE, getLanguageCode())
+                        .header(DATE, requestTime.format(DATE_TIME_FORMATTER_HEADERS))
+                        .header(X_IBM_CLIENT_ID, apiConfiguration.getApiKey())
+                        .header(X_IBM_CLIENT_SECRET, apiConfiguration.getClientSecret());
+        if (token != null) {
+            header.addBearerToken(token);
+        }
+        return header;
+    }
+
+    @SneakyThrows
+    protected RequestHeaderEntity getRequestHeaderEntity(
+            String requestId, ZonedDateTime requestTime, String token) {
+        String apiKey = configuration.getProviderSpecificConfiguration().getApiKey();
+        RequestHeaderEntity.RequestHeaderEntityBuilder<?, ?> builder =
+                RequestHeaderEntity.builder()
+                        .apiKey(apiKey)
+                        .clientId(apiKey)
+                        .callbackURL(configuration.getRedirectUrl())
+                        .isDirectPsu(isUserPresent())
+                        .sendDate(requestTime.format(DATE_TIME_FORMATTER_REQUEST_HEADERS))
+                        .tppId(
+                                CertificateUtils.getOrganizationIdentifier(
+                                        configuration.getQsealc()))
+                        .requestId(requestId)
+                        .isCompanyContext(false)
+                        .isCorporateContext(false)
+                        .token(token);
+        if (isUserPresent()) {
+            builder.ipAddress(agentComponentProvider.getCredentialsRequest().getOriginatingUserIp())
+                    .userAgent(PSU_USER_AGENT_VAL);
+        }
+        return builder.build();
+    }
+
+    protected String getUuid() {
+        return agentComponentProvider.getRandomValueGenerator().generateUUIDv1().toString();
+    }
+
+    protected boolean isUserPresent() {
+        return agentComponentProvider.getCredentialsRequest().getUserAvailability().isUserPresent();
+    }
+
+    protected String getAccessTokenFromStorage() {
+        return getTokenFromStorage().getAccessToken();
+    }
+
+    protected OAuth2Token getTokenFromStorage() {
+        return persistentStorage.getToken();
+    }
+
+    protected ZonedDateTime getNow() {
+        return agentComponentProvider.getLocalDateTimeSource().now().atZone(ZoneOffset.UTC);
+    }
+
+    private String getLanguageCode() {
+        return PolishApiConstants.Localization.getLanguageCode(
+                agentComponentProvider.getCredentialsRequest().getUser().getLocale());
+    }
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/polishapi/common/dto/requests/RequestHeaderEntity.java
@@ -0,0 +1,43 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.common.dto.requests;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+import se.tink.backend.aggregation.annotations.JsonObject;
+
+@JsonObject
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@SuperBuilder
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@ToString
+public class RequestHeaderEntity {
+    // common
+    String sendDate;
+    String ipAddress;
+    String userAgent;
+    String tppId;
+    String requestId;
+    String apiKey;
+    Boolean isDirectPsu;
+    String callbackURL;
+
+    // data fetch
+    String token;
+
+    @JsonProperty("client_id")
+    String clientId;
+
+    // Used for consent / authorize
+    String psuContextIdentifierValue;
+    String psuIdentifierType;
+    String psuContextIdentifierType;
+    String psuIdentifierValue;
+
+    Boolean isCompanyContext;
+    Boolean isCorporateContext;
+}
