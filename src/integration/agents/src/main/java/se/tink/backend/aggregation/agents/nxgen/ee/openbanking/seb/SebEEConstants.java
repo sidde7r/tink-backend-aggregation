12050066294b97f9b82e49200692a0a75b39a186
Shivam Saini <shivam.saini@tink.com>
2021-07-15 12:30:02 +0200

    refactor(ee/seb): moved constants to a separate class

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/ee/openbanking/seb/SebEEConstants.java
@@ -0,0 +1,8 @@
+package se.tink.backend.aggregation.agents.nxgen.ee.openbanking.seb;
+
+public class SebEEConstants {
+
+  public static final String BIC = "EEUHEE2X";
+  public static final String MARKET_CODE = "EE";
+
+}
