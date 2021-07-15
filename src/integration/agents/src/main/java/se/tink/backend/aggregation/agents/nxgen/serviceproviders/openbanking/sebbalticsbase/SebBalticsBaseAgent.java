af942eabfcd0aa39dadcfd55531d6d71e858cc2c
Shivam Saini <shivam.saini@tink.com>
2021-07-15 12:26:28 +0200

    refactor(baltic/seb): added transaction fetcherController to base agent

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbalticsbase/SebBalticsBaseAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbalticsbase/SebBalticsBaseAgent.java
@@ -1,28 +1,30 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbalticsbase;
 
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbalticsbase.authenticator.SebBalticsDecoupledAuthenticator;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbalticsbase.configuration.SebBlaticsConfiguration;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbalticsbase.session.SebBalticsSessionHandler;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbalticsbase.configuration.SebBalticsConfiguration;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbalticsbase.fetcher.transactionalaccount.SebBalticsTransactionFetcher;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbalticsbase.fetcher.transactionalaccount.SebBalticsTransactionalAccountFetcher;
 import se.tink.backend.aggregation.configuration.agents.AgentConfiguration;
 import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
 import se.tink.backend.aggregation.nxgen.agents.SubsequentProgressiveGenerationAgent;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
-import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2TokenStorage;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.progressive.StatelessProgressiveAuthenticator;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcherController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionKeyPaginationController;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
+import se.tink.backend.aggregation.nxgen.controllers.session.OAuth2TokenSessionHandler;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
 
 public abstract class SebBalticsBaseAgent<C extends SebBalticsBaseApiClient>
         extends SubsequentProgressiveGenerationAgent {
 
     protected C apiClient;
-    protected AgentConfiguration<SebBlaticsConfiguration> agentConfiguration;
-    protected SebBlaticsConfiguration sebConfiguration;
-    protected OAuth2TokenStorage tokenStorage;
+    protected AgentConfiguration<SebBalticsConfiguration> agentConfiguration;
+    protected SebBalticsConfiguration sebConfiguration;
 
     protected SebBalticsBaseAgent(AgentComponentProvider componentProvider) {
         super(componentProvider);
         this.apiClient = getApiClient();
-        this.tokenStorage = new OAuth2TokenStorage(persistentStorage, sessionStorage);
     }
 
     protected abstract C getApiClient();
@@ -33,7 +35,7 @@ public abstract class SebBalticsBaseAgent<C extends SebBalticsBaseApiClient>
 
         agentConfiguration =
                 getAgentConfigurationController()
-                        .getAgentConfiguration(SebBlaticsConfiguration.class);
+                        .getAgentConfiguration(SebBalticsConfiguration.class);
         sebConfiguration = agentConfiguration.getProviderSpecificConfiguration();
         apiClient.setConfiguration(sebConfiguration);
         client.setEidasProxy(configuration.getEidasProxy());
@@ -47,6 +49,21 @@ public abstract class SebBalticsBaseAgent<C extends SebBalticsBaseApiClient>
 
     @Override
     protected SessionHandler constructSessionHandler() {
-        return new SebBalticsSessionHandler(apiClient, persistentStorage);
+        return new OAuth2TokenSessionHandler(persistentStorage);
+    }
+
+    protected TransactionalAccountRefreshController getTransactionalAccountRefreshController() {
+        SebBalticsTransactionalAccountFetcher accountFetcher =
+                new SebBalticsTransactionalAccountFetcher(apiClient);
+
+        return new TransactionalAccountRefreshController(
+                metricRefreshController,
+                updateController,
+                accountFetcher,
+                new TransactionFetcherController<>(
+                        this.transactionPaginationHelper,
+                        new TransactionKeyPaginationController<>(
+                                new SebBalticsTransactionFetcher(
+                                        apiClient, transactionPaginationHelper))));
     }
 }
