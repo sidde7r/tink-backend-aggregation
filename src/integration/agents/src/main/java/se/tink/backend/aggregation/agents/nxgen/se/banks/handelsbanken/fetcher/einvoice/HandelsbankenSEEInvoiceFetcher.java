89345b4a0e92e62569964b936556194e025cd8a1
Piotr Miecielica <piotr.miecielica@tink.se>
2021-07-16 16:37:36 +0200

    revert(handelsbanken): Remove einvoice refreshing
    This reverts commit 683b963caccdab8edab1b5df546f8c0f00c8c58c.

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/fetcher/einvoice/HandelsbankenSEEInvoiceFetcher.java
@@ -0,0 +1,30 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.fetcher.einvoice;
+
+import java.util.Collection;
+import java.util.Collections;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.HandelsbankenSEApiClient;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.fetcher.einvoice.rpc.PendingEInvoicesResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.handelsbanken.HandelsbankenSessionStorage;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.einvoice.EInvoiceFetcher;
+import se.tink.libraries.transfer.rpc.Transfer;
+
+public class HandelsbankenSEEInvoiceFetcher implements EInvoiceFetcher {
+
+    private final HandelsbankenSEApiClient client;
+    private final HandelsbankenSessionStorage sessionStorage;
+
+    public HandelsbankenSEEInvoiceFetcher(
+            HandelsbankenSEApiClient client, HandelsbankenSessionStorage sessionStorage) {
+        this.client = client;
+        this.sessionStorage = sessionStorage;
+    }
+
+    @Override
+    public Collection<Transfer> fetchEInvoices() {
+        return sessionStorage
+                .applicationEntryPoint()
+                .map(client::pendingEInvoices)
+                .map(PendingEInvoicesResponse::toTinkTransfers)
+                .orElseGet(Collections::emptyList);
+    }
+}
---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/fetcher/einvoice/entities/EInvoicesGrouped.java
@@ -0,0 +1,23 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.fetcher.einvoice.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.entities.EInvoice;
+import se.tink.backend.aggregation.annotations.JsonObject;
+
+@JsonObject
+public class EInvoicesGrouped {
+    private String type;
+    private String name;
+    private String number;
+
+    @JsonProperty("eInvoiceDTO")
+    private List<EInvoice> eInvoices;
+
+    private boolean mapped;
+    private boolean empty;
+
+    public List<EInvoice> getEInvoices() {
+        return eInvoices;
+    }
+}
