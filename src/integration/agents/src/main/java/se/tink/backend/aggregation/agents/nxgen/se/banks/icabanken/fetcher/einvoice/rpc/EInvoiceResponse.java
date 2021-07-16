8cc97d35648111fbca186380437ea561df3f5283
Piotr Miecielica <piotr.miecielica@tink.se>
2021-07-16 16:38:51 +0200

    revert(icabanken): Remove einvoice refreshing
    This reverts commit 7f5c29328122a0a25e3150a09d5b8adc87abbc14.

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/icabanken/fetcher/einvoice/rpc/EInvoiceResponse.java
@@ -0,0 +1,8 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.icabanken.fetcher.einvoice.rpc;
+
+import se.tink.backend.aggregation.agents.nxgen.se.banks.icabanken.fetcher.einvoice.entities.EInvoiceBodyEntity;
+import se.tink.backend.aggregation.agents.nxgen.se.banks.icabanken.rpc.BaseResponse;
+import se.tink.backend.aggregation.annotations.JsonObject;
+
+@JsonObject
+public class EInvoiceResponse extends BaseResponse<EInvoiceBodyEntity> {}
