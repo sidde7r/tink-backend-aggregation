61658d2fb317fc599613328e9ed6d41911f6e21e
Malgorzata Kosecka <malgorzata.kosecka@tink.se>
2021-07-19 17:22:25 +0200

    feat(uk/newday): add second batch of newday agents

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/argos/ArgosAgentTest.java
@@ -0,0 +1,19 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.argos;
+
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
+
+public class ArgosAgentTest {
+
+    @Test
+    public void test() throws Exception {
+        new AgentIntegrationTest.Builder("uk", "uk-newday-argos-ob")
+                .loadCredentialsBefore(false)
+                .saveCredentialsAfter(true)
+                .expectLoggedIn(false)
+                .setFinancialInstitutionId("bdda0dbc38e749f298f30acb224b2d28")
+                .setAppId("tink")
+                .build()
+                .testRefresh();
+    }
+}
