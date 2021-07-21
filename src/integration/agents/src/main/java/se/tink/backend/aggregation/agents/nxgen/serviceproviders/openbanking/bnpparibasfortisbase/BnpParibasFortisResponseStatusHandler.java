df74928ebe9a0b4f49d5ef1f0a381dbdb9703591
Grzegorz Ciosmak <grzegorz.ciosmak@tink.com>
2021-07-21 10:24:27 +0200

    refactor(BnpParibasFortis): extract error check to separate method
    [MINI-1121]
    The result of comments on the previous PR.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/bnpparibasfortisbase/BnpParibasFortisResponseStatusHandler.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/bnpparibasfortisbase/BnpParibasFortisResponseStatusHandler.java
@@ -1,7 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bnpparibasfortisbase;
 
 import lombok.extern.slf4j.Slf4j;
-import se.tink.backend.aggregation.agents.exceptions.errors.AuthorizationError;
+import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.bnpparibasfortisbase.BnpParibasFortisBaseConstants.FormValues;
 import se.tink.backend.aggregation.nxgen.http.DefaultResponseStatusHandler;
 import se.tink.backend.aggregation.nxgen.http.request.HttpRequest;
@@ -10,16 +10,20 @@ import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
 @Slf4j
 public class BnpParibasFortisResponseStatusHandler extends DefaultResponseStatusHandler {
 
-    @Override
-    public void handleResponse(HttpRequest httpRequest, HttpResponse httpResponse) {
+    private void checkAccessNotAllowedError(HttpResponse httpResponse) {
         if (httpResponse.getStatus() == 400
                 && httpResponse.getBody(String.class).contains(FormValues.ACCESS_NOT_ALLOWED)) {
             log.info(
                     "Response status {} : {}",
                     httpResponse.getStatus(),
                     httpResponse.getBody(String.class));
-            throw AuthorizationError.UNAUTHORIZED.exception();
+            throw SessionError.SESSION_EXPIRED.exception();
         }
+    }
+
+    @Override
+    public void handleResponse(HttpRequest httpRequest, HttpResponse httpResponse) {
+        checkAccessNotAllowedError(httpResponse);
         super.handleResponse(httpRequest, httpResponse);
     }
 }
