3aa0d4b3b7a4aa4892d8fe927c56ce01160e6c51
Artur Ciskowski <artur.ciskowski@tink.se>
2021-07-21 17:51:42 +0200

    fix(PT/SIBS): revert realtime balance

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/fetcher/transactionalaccount/SibsTransactionalAccountFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/fetcher/transactionalaccount/SibsTransactionalAccountFetcher.java
@@ -4,11 +4,12 @@ import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsBaseApiClient;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.entities.account.AccountEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.rpc.AccountsResponse;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.rpc.BalancesResponse;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
+import se.tink.libraries.amount.ExactCurrencyAmount;
 
 public class SibsTransactionalAccountFetcher implements AccountFetcher<TransactionalAccount> {
 
@@ -30,8 +31,17 @@ public class SibsTransactionalAccountFetcher implements AccountFetcher<Transacti
     }
 
     protected Optional<TransactionalAccount> toTinkAccount(AccountEntity accountEntity) {
-        BalancesResponse balanceResponse = apiClient.getAccountBalances(accountEntity.getId());
+        ExactCurrencyAmount balanceAmount =
+                apiClient.getAccountBalances(accountEntity.getId()).getBalances().stream()
+                        .findFirst()
+                        .orElseThrow(
+                                () ->
+                                        new IllegalStateException(
+                                                SibsConstants.ErrorMessages.NO_BALANCE))
+                        .getInterimAvailable()
+                        .getAmount()
+                        .toTinkAmount();
 
-        return accountEntity.toTinkAccount(balanceResponse);
+        return accountEntity.toTinkAccount(balanceAmount);
     }
 }
