bee469b13664aacec15bbfcbe4f7c2f138fe2717
Shivam Saini <shivam.saini@tink.com>
2021-07-22 12:34:46 +0200

    chore(baltic/seb): supplement fields for smartId in baltic

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/utils/supplementalfields/BalticFields.java
@@ -0,0 +1,28 @@
+package se.tink.backend.aggregation.agents.utils.supplementalfields;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.libraries.i18n.Catalog;
+import se.tink.libraries.i18n.LocalizableKey;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BalticFields {
+
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    public static class BankIdReferenceInfo {
+        private static final String FIELD_KEY = "name";
+        private static final LocalizableKey DESCRIPTION = new LocalizableKey("Challenge code");
+        private static final LocalizableKey HELP_TEXT =
+                new LocalizableKey(
+                        "Open SmartID on your mobile device, check that the challenge codes match and enter the PIN. Then continue the connection.");
+
+        public static Field build(Catalog catalog, String challengeCode) {
+            return CommonFields.Information.build(
+                    FIELD_KEY,
+                    catalog.getString(DESCRIPTION),
+                    challengeCode,
+                    catalog.getString(HELP_TEXT));
+        }
+    }
+}
