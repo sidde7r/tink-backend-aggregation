0a1a12d5883047458f39f5758aa7fc0a564a6667
Shivam Saini <shivam.saini@tink.com>
2021-07-22 11:49:53 +0200

    refactor(baltic/seb): Common agent for baltic can handle all 3 markets

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/lt/openbanking/seb/SebLTConstants.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.lt.openbanking.seb;
-
-public class SebLTConstants {
-
-    public static final String BIC = "CBVILT2X";
-    public static final String MARKET_CODE = "LT";
-}
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/lv/openbanking/seb/SebLVConstants.java
+++
@@ -1,7 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.lv.openbanking.seb;
-
-public class SebLVConstants {
-
-    public static final String BIC = "UNLALV2X";
-    public static final String MARKET_CODE = "LV";
-}
