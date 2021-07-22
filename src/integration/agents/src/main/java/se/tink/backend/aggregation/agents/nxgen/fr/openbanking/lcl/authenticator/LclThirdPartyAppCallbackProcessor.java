5c2851fbe0ed2452a75cdebd09c61ded4ec0c6e5
Lukasz Binkiewicz <lukasz.binkiewicz@tink.se>
2021-07-22 09:43:37 +0200

    fix(FR/Lcl): Handle new errors & add filter

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/lcl/authenticator/LclThirdPartyAppCallbackProcessor.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/lcl/authenticator/LclThirdPartyAppCallbackProcessor.java
@@ -1,8 +1,11 @@
 package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.lcl.authenticator;
 
 import java.util.Map;
+import java.util.Optional;
+import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceError;
 import se.tink.backend.aggregation.agents.exceptions.errors.AuthorizationError;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.constants.OAuth2Constants;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.constants.OAuth2Constants.ErrorType;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2based.OAuth2ThirdPartyAppRequestParamsProvider;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2based.ThirdPartyAppCallbackProcessor;
 
@@ -15,14 +18,21 @@ public class LclThirdPartyAppCallbackProcessor extends ThirdPartyAppCallbackProc
 
     @Override
     public String processError(String error, Map<String, String> callbackData) {
+        Optional.ofNullable(error).map(ErrorType::getErrorType).ifPresent(this::processErrorType);
         getCallbackElement(callbackData, OAuth2Constants.CallbackParams.ERROR_DESCRIPTION)
-                .ifPresent(this::process);
+                .ifPresent(this::processErrorDescription);
         return super.processError(error, callbackData);
     }
 
-    private void process(String errorDescription) {
+    private void processErrorDescription(String errorDescription) {
         if (errorDescription.contains("PSU access denied")) {
             throw AuthorizationError.UNAUTHORIZED.exception();
+        }
+    }
+
+    private void processErrorType(ErrorType errorType) {
+        if (ErrorType.SERVER_ERROR.equals(errorType)) {
+            throw BankServiceError.BANK_SIDE_FAILURE.exception();
         }
     }
 }
