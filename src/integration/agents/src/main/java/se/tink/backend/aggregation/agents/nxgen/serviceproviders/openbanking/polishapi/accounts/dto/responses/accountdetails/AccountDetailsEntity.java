77825148f7fff9af4d0356966be63f7ad34b1332
Daniel Obrębski <daniel.obrebski@tink.se>
2021-07-26 10:30:42 +0200

    fix(pl/agents): handle no owner name situation
    [ITE-2640]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/polishapi/accounts/dto/responses/accountdetails/AccountDetailsEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/polishapi/accounts/dto/responses/accountdetails/AccountDetailsEntity.java
@@ -3,6 +3,7 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.po
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import java.util.List;
 import java.util.Optional;
@@ -19,9 +20,11 @@ import se.tink.backend.aggregation.nxgen.core.account.AccountHolderType;
 import se.tink.backend.aggregation.nxgen.core.account.AccountTypeMapper;
 import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
 import se.tink.backend.aggregation.nxgen.core.account.entity.Party;
+import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.creditcard.CreditCardBuildStep;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.balance.BalanceModule;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.creditcard.CreditCardModule;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.id.IdModule;
+import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.transactional.TransactionalBuildStep;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccountType;
 import se.tink.backend.aggregation.source_info.AccountSourceInfo;
@@ -86,36 +89,41 @@ public class AccountDetailsEntity {
 
     @JsonIgnore
     public Optional<TransactionalAccount> toTinkAccount(AccountTypeMapper accountTypeMapper) {
-        return TransactionalAccount.nxBuilder()
-                .withPatternTypeAndFlagsFrom(
-                        accountTypeMapper,
-                        getConcatOfDescriptionAndCode(),
-                        TransactionalAccountType.CHECKING)
-                .withBalance(
-                        BalanceModule.builder()
-                                .withBalance(
-                                        ExactCurrencyAmount.of(
-                                                ObjectUtils.firstNonNull(
-                                                        bookingBalance, availableBalance),
-                                                currency))
-                                .setAvailableBalance(
-                                        ExactCurrencyAmount.of(
-                                                ObjectUtils.firstNonNull(
-                                                        availableBalance, bookingBalance),
-                                                currency))
-                                .build())
-                .withId(buildIdModule())
-                .addParties(new Party(getOwnerName(), getRole()))
-                .setHolderType(AccountHolderType.PERSONAL)
-                .setApiIdentifier(apiAccountId)
-                .setBankIdentifier(accountNumber)
-                .sourceInfo(
-                        AccountSourceInfo.builder()
-                                .bankAccountType(accountTypeName)
-                                .bankProductCode(accountType.getCode())
-                                .bankProductName(accountType.getDescription())
-                                .build())
-                .build();
+        TransactionalBuildStep transactionalBuildStep =
+                TransactionalAccount.nxBuilder()
+                        .withPatternTypeAndFlagsFrom(
+                                accountTypeMapper,
+                                getConcatOfDescriptionAndCode(),
+                                TransactionalAccountType.CHECKING)
+                        .withBalance(
+                                BalanceModule.builder()
+                                        .withBalance(
+                                                ExactCurrencyAmount.of(
+                                                        ObjectUtils.firstNonNull(
+                                                                bookingBalance, availableBalance),
+                                                        currency))
+                                        .setAvailableBalance(
+                                                ExactCurrencyAmount.of(
+                                                        ObjectUtils.firstNonNull(
+                                                                availableBalance, bookingBalance),
+                                                        currency))
+                                        .build())
+                        .withId(buildIdModule())
+                        .setHolderType(AccountHolderType.PERSONAL)
+                        .setApiIdentifier(apiAccountId)
+                        .setBankIdentifier(accountNumber)
+                        .sourceInfo(
+                                AccountSourceInfo.builder()
+                                        .bankAccountType(accountTypeName)
+                                        .bankProductCode(accountType.getCode())
+                                        .bankProductName(accountType.getDescription())
+                                        .build());
+
+        if (getOwnerName() != null) {
+            transactionalBuildStep.addParties(new Party(getOwnerName(), getRole()));
+        }
+
+        return transactionalBuildStep.build();
     }
 
     private String getConcatOfDescriptionAndCode() {
@@ -124,36 +132,44 @@ public class AccountDetailsEntity {
 
     @JsonIgnore
     public CreditCardAccount toTinkCreditCardAccount() {
-        return CreditCardAccount.nxBuilder()
-                .withCardDetails(
-                        CreditCardModule.builder()
-                                .withCardNumber(accountNumber)
-                                .withBalance(ExactCurrencyAmount.of(availableBalance, currency))
-                                .withAvailableCredit(
-                                        ExactCurrencyAmount.zero(
-                                                currency)) // API does not return available credit
-                                .withCardAlias(accountType.getDescription())
-                                .build())
-                .withPaymentAccountFlag()
-                .withId(buildIdModule())
-                .addParties(new Party(getOwnerName(), getRole()))
-                .setHolderType(AccountHolderType.PERSONAL)
-                .setApiIdentifier(apiAccountId)
-                .setBankIdentifier(accountNumber)
-                .sourceInfo(
-                        AccountSourceInfo.builder()
-                                .bankAccountType(accountTypeName)
-                                .bankProductCode(accountType.getCode())
-                                .bankProductName(accountType.getDescription())
-                                .build())
-                .build();
+        CreditCardBuildStep creditCardBuildStep =
+                CreditCardAccount.nxBuilder()
+                        .withCardDetails(
+                                CreditCardModule.builder()
+                                        .withCardNumber(accountNumber)
+                                        .withBalance(
+                                                ExactCurrencyAmount.of(availableBalance, currency))
+                                        .withAvailableCredit(
+                                                ExactCurrencyAmount.zero(
+                                                        currency)) // API does not return available
+                                        // credit
+                                        .withCardAlias(accountType.getDescription())
+                                        .build())
+                        .withPaymentAccountFlag()
+                        .withId(buildIdModule())
+                        .setHolderType(AccountHolderType.PERSONAL)
+                        .setApiIdentifier(apiAccountId)
+                        .setBankIdentifier(accountNumber)
+                        .sourceInfo(
+                                AccountSourceInfo.builder()
+                                        .bankAccountType(accountTypeName)
+                                        .bankProductCode(accountType.getCode())
+                                        .bankProductName(accountType.getDescription())
+                                        .build());
+        if (getOwnerName() != null) {
+            creditCardBuildStep.addParties(new Party(getOwnerName(), getRole()));
+        }
+        return creditCardBuildStep.build();
     }
 
     private IdModule buildIdModule() {
         return IdModule.builder()
                 .withUniqueIdentifier(accountNumber)
                 .withAccountNumber(accountNumber)
-                .withAccountName(ObjectUtils.firstNonNull(accountNameClient, accountTypeName))
+                .withAccountName(
+                        ObjectUtils.firstNonNull(
+                                Strings.emptyToNull(accountNameClient),
+                                Strings.emptyToNull(accountTypeName)))
                 .addIdentifiers(getIdentifiers())
                 .build();
     }
