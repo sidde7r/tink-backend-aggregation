e3d8812ba3ccea8958ccf4ff1892cee3de3f8e2e
Pawel Szalawinski <pawel.szalawinski@tink.com>
2021-07-26 13:25:47 +0200

    feat(NL/Abnamro/consent): created consent lib

---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/consent/generators/nlob/abnamro/AbnAmroConsentGenerator.java
@@ -0,0 +1,55 @@
+package se.tink.backend.aggregation.agents.consent.generators.nlob.abnamro;
+
+import com.google.common.collect.Sets;
+import java.util.Collections;
+import java.util.Set;
+import se.tink.backend.aggregation.agents.consent.ConsentGenerator;
+import se.tink.backend.aggregation.agents.consent.ToScopes;
+import se.tink.backend.aggregation.agents.consent.suppliers.ItemsSupplier;
+import se.tink.backend.aggregation.agents.consent.suppliers.ScopesSupplier;
+import se.tink.libraries.credentials.service.CredentialsRequest;
+import se.tink.libraries.credentials.service.RefreshableItem;
+
+public class AbnAmroConsentGenerator implements ConsentGenerator<String> {
+
+    private final ScopesSupplier<RefreshableItem, AbnAmroScope> scopesSupplier;
+
+    private static final ToScopes<RefreshableItem, AbnAmroScope> itemToScopes =
+            item -> {
+                switch (item) {
+                    case CHECKING_ACCOUNTS:
+                        return Sets.newHashSet(
+                                AbnAmroScope.READ_ACCOUNTS, AbnAmroScope.READ_ACCOUNTS_DETAILS);
+                    case SAVING_ACCOUNTS:
+                    case CREDITCARD_ACCOUNTS:
+                    case LOAN_ACCOUNTS:
+                    case INVESTMENT_ACCOUNTS:
+                    case CHECKING_TRANSACTIONS:
+                        return Sets.newHashSet(AbnAmroScope.READ_TRANSACTIONS_HISTORY);
+                    case SAVING_TRANSACTIONS:
+                    case CREDITCARD_TRANSACTIONS:
+                    case LOAN_TRANSACTIONS:
+                    case INVESTMENT_TRANSACTIONS:
+                    case IDENTITY_DATA:
+                    case LIST_BENEFICIARIES:
+                    case TRANSFER_DESTINATIONS:
+                    default:
+                        return Collections.emptySet();
+                }
+            };
+
+    public AbnAmroConsentGenerator(CredentialsRequest request, Set<AbnAmroScope> availableScopes) {
+        this.scopesSupplier =
+                new ScopesSupplier<>(ItemsSupplier.get(request), availableScopes, itemToScopes);
+    }
+
+    public static AbnAmroConsentGenerator of(
+            CredentialsRequest request, Set<AbnAmroScope> availableScopes) {
+        return new AbnAmroConsentGenerator(request, availableScopes);
+    }
+
+    @Override
+    public String generate() {
+        return String.join(" ", scopesSupplier.getStrings());
+    }
+}
---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/consent/generators/nlob/abnamro/AbnAmroScope.java
@@ -0,0 +1,20 @@
+package se.tink.backend.aggregation.agents.consent.generators.nlob.abnamro;
+
+import se.tink.backend.aggregation.agents.consent.Scope;
+
+public enum AbnAmroScope implements Scope {
+    READ_ACCOUNTS("psd2:account:balance:read"),
+    READ_ACCOUNTS_DETAILS("psd2:account:details:read"),
+    READ_TRANSACTIONS_HISTORY("psd2:account:transaction:read");
+
+    private final String value;
+
+    AbnAmroScope(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+}
