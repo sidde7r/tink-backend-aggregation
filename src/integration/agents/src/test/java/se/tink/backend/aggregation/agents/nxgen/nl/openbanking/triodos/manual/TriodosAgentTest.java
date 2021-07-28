26f92f134b5ebaa6a4ef26b036030aca1dfbd077
Shivam Saini <shivam.saini@tink.com>
2021-07-28 12:36:20 +0200

    refactor(nl/triodos): PSUs will select accounts during auth flow

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/triodos/manual/TriodosAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/triodos/manual/TriodosAgentTest.java
@@ -5,22 +5,15 @@ import org.junit.Before;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 import se.tink.backend.aggregation.agents.framework.ArgumentManager;
-import se.tink.backend.aggregation.agents.framework.ArgumentManager.IbanArgumentEnum;
-import se.tink.backend.aggregation.agents.nxgen.nl.openbanking.triodos.TriodosConstants.CredentialKeys;
 
 public class TriodosAgentTest {
 
     private AgentIntegrationTest.Builder builder;
-    private final ArgumentManager<IbanArgumentEnum> manager =
-            new ArgumentManager<>(IbanArgumentEnum.values());
 
     @Before
     public void setup() {
-        manager.before();
         builder =
                 new AgentIntegrationTest.Builder("nl", "nl-triodos-ob")
-                        .addCredentialField(
-                                CredentialKeys.IBANS, manager.get(IbanArgumentEnum.IBAN))
                         .setFinancialInstitutionId("triodos")
                         .setAppId("tink")
                         .setClusterId("oxford-preprod")
