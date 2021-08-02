8d0b9f29d273d54388a78568b35adbf90a1a2f44
Grzegorz Ciosmak <grzegorz.ciosmak@tink.com>
2021-08-02 16:00:15 +0200

    fix(CreditAgricole): Protection against user data reset
    MINI-1237
    A condition  to prevent the userID and partnerId data from being lost during authentication has been added

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/banks/creditagricole/authenticator/CreditAgricoleException.java
@@ -0,0 +1,8 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.banks.creditagricole.authenticator;
+
+public class CreditAgricoleException extends RuntimeException {
+
+    public CreditAgricoleException(final String message) {
+        super(message);
+    }
+}
