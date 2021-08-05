d9fc4f173e0a6a616be21edb4b54dac06bd34be4
Pawel Szalawinski <pawel.szalawinski@tink.com>
2021-08-05 08:44:51 +0200

    fix(OB/SE/NordeaBusiness): removed unecessary class

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/nordeabase/configuration/NordeaConfiguration.java
+++
@@ -1,9 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.nordeabase.configuration;
-
-public abstract class NordeaConfiguration {
-    public abstract String getBaseUrl();
-
-    public abstract String getFirstApiVersion();
-
-    public abstract String getSecondApiVersion();
-}
