3ae4c07e553692646f26a23ea81583b09aaaebad
Michael Palma <michael.palma@tink.com>
2021-08-09 14:00:49 +0200

    test(Sparebank OB): add wiremock auth & refresh
    [ITE-2712](https://tinkab.atlassian.net/browse/ITE-2712)

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/wiremock/SparebankWiremockAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/wiremock/SparebankWiremockAgentTest.java
@@ -1,9 +1,18 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sparebank.wiremock;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import com.google.gson.Gson;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonFileParser;
 import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
 import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockrefresh.AgentWireMockRefreshTest;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sparebank.fetcher.card.rpc.CardResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sparebank.fetcher.transactionalaccount.rpc.AccountResponse;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sparebank.util.AccountTestData;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sparebank.wiremock.module.SparebankWiremockTestModule;
 import se.tink.backend.aggregation.configuration.AgentsServiceConfigurationReader;
 import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
 import se.tink.libraries.credentials.service.RefreshableItem;
@@ -14,30 +23,114 @@ public class SparebankWiremockAgentTest {
     private static final String CONFIGURATION_PATH =
             "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/wiremock/resources/configuration.yml";
 
+    private static final String contractFilePath =
+            "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/wiremock/resources/agent-contract.json";
+
+    private static final AccountResponse SAMPLE_ACCOUNT_RESPONSE =
+            AccountTestData.getAccountResponse();
+
     @Test
-    public void testRefresh() throws Exception {
+    public void testManualAuthentication() throws Exception {
 
         // given
         final String wireMockServerFilePath =
-                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/wiremock/resources/wiremock-sparebank-ob.aap";
-        final String contractFilePath =
-                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/wiremock/resources/agent-contract.json";
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/wiremock/resources/manual-authentication-sparebank-ob.aap";
 
         final AgentsServiceConfiguration configuration =
                 AgentsServiceConfigurationReader.read(CONFIGURATION_PATH);
 
         final AgentWireMockRefreshTest agentWireMockRefreshTest =
-                AgentWireMockRefreshTest.builder(
-                                MarketCode.NO, "no-sparebank1sr-ob", wireMockServerFilePath)
-                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
-                        .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
-                        .withConfigurationFile(configuration)
+                AgentWireMockRefreshTest.nxBuilder()
+                        .withMarketCode(MarketCode.NO)
+                        .withProviderName("no-sparebank1sr-ob")
+                        .withWireMockFilePath(wireMockServerFilePath)
+                        .withConfigFile(configuration)
+                        .testFullAuthentication()
+                        .testOnlyAuthentication()
+                        .withAgentTestModule(new SparebankWiremockTestModule())
+                        .addCallbackData("psu-id", "PSU_ID")
+                        .addCallbackData("tpp-session-id", "SESSION_ID")
                         .build();
 
-        final AgentContractEntity expected =
+        assertThatCode(agentWireMockRefreshTest::executeRefresh).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void testAutoRefresh() throws Exception {
+
+        // given
+        final String wireMockServerFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/wiremock/resources/auto-refresh-sparebank-ob.aap";
+
+        final AgentsServiceConfiguration configuration =
+                AgentsServiceConfigurationReader.read(CONFIGURATION_PATH);
+
+        Gson gson = new Gson();
+        String sampleAcount = gson.toJson(SAMPLE_ACCOUNT_RESPONSE);
+
+        final AgentWireMockRefreshTest agentWireMockRefreshTest =
+                AgentWireMockRefreshTest.nxBuilder()
+                        .withMarketCode(MarketCode.NO)
+                        .withProviderName("no-sparebank1sr-ob")
+                        .withWireMockFilePath(wireMockServerFilePath)
+                        .withConfigFile(configuration)
+                        .testAutoAuthentication()
+                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                        .withAgentTestModule(new SparebankWiremockTestModule())
+                        .addPersistentStorageData("SESSION_ID", "XXXX_SESSION")
+                        .addPersistentStorageData("PSU_ID", "XXXX_ID")
+                        .addPersistentStorageData("CONSENT_CREATED_TIMESTAMP", 1628165339000L)
+                        .addPersistentStorageData("accounts", sampleAcount)
+                        .addPersistentStorageData("cards", CardResponse.empty())
+                        .build();
+
+        AgentContractEntity expected =
                 new AgentContractEntitiesJsonFileParser()
                         .parseContractOnBasisOfFile(contractFilePath);
+        // when
+        agentWireMockRefreshTest.executeRefresh();
 
+        // then
+        agentWireMockRefreshTest.assertExpectedData(expected);
+    }
+
+    @Test
+    public void testManualRefresh() throws Exception {
+
+        // given
+        final String wireMockServerFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/wiremock/resources/manual-refresh-sparebank-ob.aap";
+
+        final AgentsServiceConfiguration configuration =
+                AgentsServiceConfigurationReader.read(CONFIGURATION_PATH);
+
+        Gson gson = new Gson();
+        String sampleAcount = gson.toJson(SAMPLE_ACCOUNT_RESPONSE);
+
+        final AgentWireMockRefreshTest agentWireMockRefreshTest =
+                AgentWireMockRefreshTest.nxBuilder()
+                        .withMarketCode(MarketCode.NO)
+                        .withProviderName("no-sparebank1sr-ob")
+                        .withWireMockFilePath(wireMockServerFilePath)
+                        .withConfigFile(configuration)
+                        .testFullAuthentication()
+                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                        .withAgentTestModule(new SparebankWiremockTestModule())
+                        .addCallbackData("psu-id", "PSU_ID")
+                        .addCallbackData("tpp-session-id", "SESSION_ID")
+                        .addCallbackData("toDate", LocalDateTime.now().toString())
+                        .addPersistentStorageData("SESSION_ID", "XXXX_SESSION")
+                        .addPersistentStorageData("PSU_ID", "XXXX_ID")
+                        .addPersistentStorageData(
+                                "CONSENT_CREATED_TIMESTAMP",
+                                LocalDateTime.now().toEpochSecond(ZoneOffset.UTC))
+                        .addPersistentStorageData("accounts", sampleAcount)
+                        .addPersistentStorageData("cards", CardResponse.empty())
+                        .build();
+
+        AgentContractEntity expected =
+                new AgentContractEntitiesJsonFileParser()
+                        .parseContractOnBasisOfFile(contractFilePath);
         // when
         agentWireMockRefreshTest.executeRefresh();
 
---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sparebank/wiremock/module/SparebankWiremockTestModule.java
@@ -0,0 +1,14 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sparebank.wiremock.module;
+
+import com.google.inject.Scopes;
+import se.tink.backend.aggregation.agents.module.loader.TestModule;
+import se.tink.backend.aggregation.eidassigner.FakeQsealcSigner;
+import se.tink.backend.aggregation.eidassigner.QsealcSigner;
+
+public final class SparebankWiremockTestModule extends TestModule {
+
+    @Override
+    protected void configure() {
+        bind(QsealcSigner.class).to(FakeQsealcSigner.class).in(Scopes.SINGLETON);
+    }
+}
