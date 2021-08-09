947922f2a338bfa9cf9b942f7bfc240b5f1532dd
Lukasz Binkiewicz <lukasz.binkiewicz@tink.se>
2021-08-09 11:57:09 +0200

    feat(PT/BancoCtt): Add payments capability
    [PENG-22]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/bancoctt/BancoCttAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/pt/openbanking/bancoctt/BancoCttAgent.java
@@ -2,18 +2,27 @@ package se.tink.backend.aggregation.agents.nxgen.pt.openbanking.bancoctt;
 
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CHECKING_ACCOUNTS;
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.SAVINGS_ACCOUNTS;
+import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.TRANSFERS;
 
 import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
+import se.tink.backend.aggregation.agents.agentcapabilities.AgentPisCapability;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModules;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.common.signature.QSealSignatureProvider;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsModule;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sibs.SibsProgressiveBaseAgent;
+import se.tink.backend.aggregation.client.provider_configuration.rpc.PisCapability;
 import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 
 @AgentDependencyModules(modules = SibsModule.class)
-@AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS})
+@AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS, TRANSFERS})
+@AgentPisCapability(
+        capabilities = {
+            PisCapability.SEPA_CREDIT_TRANSFER,
+            PisCapability.PIS_FUTURE_DATE,
+            PisCapability.SEPA_INSTANT_CREDIT_TRANSFER
+        })
 public final class BancoCttAgent extends SibsProgressiveBaseAgent {
 
     @Inject
