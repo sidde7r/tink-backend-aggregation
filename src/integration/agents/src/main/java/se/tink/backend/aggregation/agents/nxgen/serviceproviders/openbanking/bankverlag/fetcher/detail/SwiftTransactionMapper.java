37af1de2547c2e3765df04fa397ba17fd7df6595
Jan Sameryt <jan.sameryt@tink.com>
2021-08-10 15:38:04 +0200

    refactor(degussa): review improvements
    [NZG-751]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/bankverlag/fetcher/detail/SwiftTransactionMapper.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/bankverlag/fetcher/detail/SwiftTransactionMapper.java
@@ -21,11 +21,23 @@ public class SwiftTransactionMapper {
     private static final Pattern RE_MT940_HEADER_PATTERN =
             Pattern.compile("(^\\d{6})(\\d{4})?(D|C|RC|RD)\\D?(\\d*,\\d*)(N.*)");
     private static final Pattern SEGMENT_PATTERN = Pattern.compile("(\\d\\d)(.*)");
+    private static final Pattern SEGMENT_SEPARATOR_PATTERN = Pattern.compile("\\?");
+
+    private static final int MAX_LENGHT_SINGLE_SEGMENT = 27;
+    private static final int TRANSACTION_DESCRIPTION_MINIMAL_KEY = 20;
+    private static final int TRANSACTION_DESCRIPTION_MAXIMUM_KEY = 29;
 
     private static final String REF_CUSTOMER = "KREF+";
     private static final String REF_MANDATE = "MREF+";
     private static final String REF_E2E = "EREF+";
     private static final String REF_SEPA = "SVWZ+";
+
+    private static final String TRANSACTION_CREDIT = "C";
+
+    private static final String MT940_HEADER_FIELD = ":61:";
+    private static final String MT940_DETAILS_FIELD = ":86:";
+
+    private static final String FIELD_START_CHAR = ":";
 
     @AllArgsConstructor
     protected static class RawMT940Transaction {
@@ -39,9 +51,6 @@ public class SwiftTransactionMapper {
         private String value;
     }
 
-    private static final String MT940_HEADER_FIELD = ":61:";
-    private static final String MT940_DETAILS_FIELD = ":86:";
-
     public List<AggregationTransaction> parse(String rawMT940) {
         List<RawMT940Transaction> rawTransactions = extractRawTransactions(rawMT940);
         return rawTransactions.stream().map(this::toTinkTransaction).collect(Collectors.toList());
@@ -51,11 +60,10 @@ public class SwiftTransactionMapper {
         List<RawMT940Transaction> transactions = new ArrayList<>();
 
         try (Scanner scanner = new Scanner(rawMT940); ) {
-            String line;
             while (scanner.hasNextLine()) {
-                line = scanner.nextLine();
+                String line = scanner.nextLine();
                 while (line.startsWith(MT940_HEADER_FIELD)) {
-                    String header = line.substring(4);
+                    String header = line.substring(MT940_HEADER_FIELD.length());
 
                     StringBuilder detailsBuilder = new StringBuilder();
                     line = extractRawTransactionDetails(scanner, detailsBuilder);
@@ -72,25 +80,29 @@ public class SwiftTransactionMapper {
         if (!scanner.hasNextLine()) {
             return "";
         }
-
-        // Skip any line that doesn't start the :86: section
-        String line = scanner.nextLine();
-        while (!line.startsWith(MT940_DETAILS_FIELD)) {
-            line = scanner.nextLine();
-        }
+        // We are not interested in any other section but :86:
+        String line = skipLinesUntilTransactionDetailsField(scanner);
 
         // Collect content of :86: section for further processing
-        detailsBuilder.append(line.substring(4));
-        while (scanner.hasNextLine() && !(line = scanner.nextLine()).startsWith(":")) {
+        detailsBuilder.append(line.substring(MT940_DETAILS_FIELD.length()));
+        while (scanner.hasNextLine() && !(line = scanner.nextLine()).startsWith(FIELD_START_CHAR)) {
             detailsBuilder.append(line);
         }
 
-        // We need to return already consumed line from scanner for futher processing. It could be
+        // We need to return already consumed line from scanner for further processing. It could be
         // the :61:
         return line;
     }
 
-    protected AggregationTransaction toTinkTransaction(RawMT940Transaction rawMT940Transaction) {
+    private String skipLinesUntilTransactionDetailsField(Scanner scanner) {
+        String line = scanner.nextLine();
+        while (!line.startsWith(MT940_DETAILS_FIELD)) {
+            line = scanner.nextLine();
+        }
+        return line;
+    }
+
+    private AggregationTransaction toTinkTransaction(RawMT940Transaction rawMT940Transaction) {
         return Transaction.builder()
                 .setAmount(
                         ExactCurrencyAmount.of(
@@ -101,11 +113,11 @@ public class SwiftTransactionMapper {
                 .build();
     }
 
-    protected BigDecimal getAmount(RawMT940Transaction rawMT940Transaction) {
+    private BigDecimal getAmount(RawMT940Transaction rawMT940Transaction) {
         Matcher matcher = RE_MT940_HEADER_PATTERN.matcher(rawMT940Transaction.header);
         boolean isNegative = true;
         if (matcher.find()) {
-            if (matcher.group(3).contains("C")) {
+            if (matcher.group(3).contains(TRANSACTION_CREDIT)) {
                 isNegative = false;
             }
             BigDecimal value = new BigDecimal(matcher.group(4).replace(",", "."));
@@ -115,12 +127,12 @@ public class SwiftTransactionMapper {
         }
     }
 
-    protected Date getDate(RawMT940Transaction rawMT940Transaction) {
+    private Date getDate(RawMT940Transaction rawMT940Transaction) {
         LocalDate date = SwiftDateParser.parseDate(rawMT940Transaction.header);
         return SwiftDateParser.toDate(date);
     }
 
-    protected String getReference(RawMT940Transaction rawMT940Transaction) {
+    private String getReference(RawMT940Transaction rawMT940Transaction) {
         Matcher matcher = RE_MT940_HEADER_PATTERN.matcher(rawMT940Transaction.header);
         if (matcher.find()) {
             String referenceGroup = matcher.group(5);
@@ -132,7 +144,7 @@ public class SwiftTransactionMapper {
         }
     }
 
-    protected String getDescription(RawMT940Transaction rawMT940Transaction) {
+    private String getDescription(RawMT940Transaction rawMT940Transaction) {
         List<Segment> segments =
                 getSegments(rawMT940Transaction).stream()
                         .filter(segment -> isRelevant(segment.key))
@@ -148,7 +160,8 @@ public class SwiftTransactionMapper {
             // If this value did not hit 27, we probably need a separator.
             // If transformed value was empty, we do not want a separator, as it leads to multiple
             // spaces in a row.
-            if (segment.value.length() < 27 && !transformedValue.equals("")) {
+            if (segment.value.length() < MAX_LENGHT_SINGLE_SEGMENT
+                    && !transformedValue.equals("")) {
                 builder.append(" ");
             }
         }
@@ -156,9 +169,10 @@ public class SwiftTransactionMapper {
         return builder.toString().trim();
     }
 
-    protected List<Segment> getSegments(RawMT940Transaction rawMT940Transaction) {
+    private List<Segment> getSegments(RawMT940Transaction rawMT940Transaction) {
         List<Segment> segments = new ArrayList<>();
-        String[] rawSegments = rawMT940Transaction.details.split("\\?");
+
+        String[] rawSegments = SEGMENT_SEPARATOR_PATTERN.split(rawMT940Transaction.details);
 
         for (String rawSegment : rawSegments) {
             Matcher matcher = SEGMENT_PATTERN.matcher(rawSegment);
@@ -176,7 +190,8 @@ public class SwiftTransactionMapper {
         // ?2x codes contain transaction description
         // ?3x codes contain bank information
         // For now we are only interested in transaction description
-        return key >= 20 && key <= 29;
+        return key >= TRANSACTION_DESCRIPTION_MINIMAL_KEY
+                && key <= TRANSACTION_DESCRIPTION_MAXIMUM_KEY;
     }
 
     private String transformValue(String value) {
