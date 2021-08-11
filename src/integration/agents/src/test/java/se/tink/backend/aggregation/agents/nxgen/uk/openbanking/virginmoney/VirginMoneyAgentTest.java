879b60856521b69111b69a588ed301ee87d2e027
Pavlo Yeromenko <pavlo.yeromenko@tink.com>
2021-08-11 11:53:14 +0200

    test(uk/virginmoney): add testRefresh

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/virginmoney/VirginMoneyAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/virginmoney/VirginMoneyAgentTest.java
@@ -1,17 +1,18 @@
-package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.virginmoney;
+package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.virginmoney;
 
 import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.AgentIntegrationTest;
 
 public class VirginMoneyAgentTest {
+
     @Test
     public void testRefresh() throws Exception {
         new AgentIntegrationTest.Builder("uk", "uk-virginmoney-ob")
                 .loadCredentialsBefore(false)
-                .saveCredentialsAfter(true)
+                .saveCredentialsAfter(false)
                 .expectLoggedIn(false)
-                .setFinancialInstitutionId("a56a4a134088494ebd1d9188bb2e6a7d")
                 .setAppId("tink")
+                .setFinancialInstitutionId("a56a4a134088494ebd1d9188bb2e6a7d")
                 .build()
                 .testRefresh();
     }
