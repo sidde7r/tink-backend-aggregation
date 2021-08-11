ca75fed8fac1b1031aec7fa332c7f9b05b64210c
Nazri Bin Abdullah <nazri.abdullah@tink.com>
2021-08-11 13:48:30 +0200

    fix(sparbankerna-ob): updated psu identity to accept null for personalId

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/swedbank/authenticator/entities/PsuDataEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/swedbank/authenticator/entities/PsuDataEntity.java
@@ -1,11 +1,13 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.swedbank.authenticator.entities;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class PsuDataEntity {
 
     @JsonProperty("bankID")
