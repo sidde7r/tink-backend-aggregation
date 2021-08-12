245b6a6827db5a068d93a11ff90d6938aba74ca7
Tomasz Rapusta <tomasz.rapusta@tink.com>
2021-08-12 10:16:42 +0200

    fix(FR/LCL): Move to next day

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/lcl/payment/LclPaymentDatePolicy.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/lcl/payment/LclPaymentDatePolicy.java
@@ -3,7 +3,6 @@ package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.lcl.payment;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.fropenbanking.base.FrOpenBankingPaymentDatePolicy;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.fropenbanking.base.entities.AccountEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.fropenbanking.base.utils.FrOpenBankingDateUtil;
 import se.tink.libraries.payment.rpc.Payment;
 
@@ -14,12 +13,7 @@ public class LclPaymentDatePolicy extends FrOpenBankingPaymentDatePolicy {
         if (payment.getExecutionDate() != null) {
             return payment.getExecutionDate();
         }
-        AccountEntity creditor = AccountEntity.creditorOf(payment);
         LocalDateTime created = FrOpenBankingDateUtil.getCreationDate();
-        if (creditor.isMonacoIban() || creditor.isFrenchIban()) {
-            return created.toLocalDate();
-        } else {
-            return created.plusDays(1L).toLocalDate();
-        }
+        return created.plusDays(1L).toLocalDate();
     }
 }
