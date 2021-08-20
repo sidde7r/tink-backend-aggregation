7e56e07df77f89e48a01ceb0766b31d4c6f91097
Blazej Szum <blazej.szum@tink.se>
2021-08-20 10:20:40 +0200

    feat(freeze): count number of merges
    CODEFREEZE_OVERRIDE

--- .buildkite/check-code-freeze.py
+++ .buildkite/check-code-freeze.py
@@ -4,6 +4,7 @@ import os, sys
 
 commit_message = os.getenv('BUILDKITE_MESSAGE')
 if os.getenv("BUILDKITE_BRANCH") == 'staging':
-    if commit_message.count('Merge #') != commit_message.count('CODEFREEZE_OVERRIDE'):
+    merge_info_line = commit_message.split("\n")[0]
+    if merge_info_line.count('#') != commit_message.count('CODEFREEZE_OVERRIDE'):
         os.system('echo "A code freeze is in effect. Please see #reliability-taskforce for more information" | buildkite-agent annotate')
         sys.exit(1)
