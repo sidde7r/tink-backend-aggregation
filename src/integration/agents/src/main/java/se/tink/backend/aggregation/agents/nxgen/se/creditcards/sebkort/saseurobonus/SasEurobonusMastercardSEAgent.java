6b0ac4e3c1702794267134ae7f430dd44ec4ae29
Nazri Bin Abdullah <nazri.abdullah@tink.com>
2021-08-28 00:53:22 +0200

    refactor(creditcards/sebkort): replaced with agentComponentProvider

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/sebkort/saseurobonus/SasEurobonusMastercardSEAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/creditcards/sebkort/saseurobonus/SasEurobonusMastercardSEAgent.java
@@ -3,16 +3,15 @@ package se.tink.backend.aggregation.agents.nxgen.se.creditcards.sebkort.saseurob
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.CREDIT_CARDS;
 import static se.tink.backend.aggregation.client.provider_configuration.rpc.Capability.IDENTITY_DATA;
 
+import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
-import se.tink.backend.aggregation.agents.contexts.agent.AgentContext;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.creditcards.sebkort.SebKortAgent;
-import se.tink.backend.aggregation.configuration.signaturekeypair.SignatureKeyPair;
-import se.tink.libraries.credentials.service.CredentialsRequest;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 
 @AgentCapabilities({CREDIT_CARDS, IDENTITY_DATA})
 public final class SasEurobonusMastercardSEAgent extends SebKortAgent {
-    public SasEurobonusMastercardSEAgent(
-            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
-        super(request, context, signatureKeyPair, new SasEurobonusMastercardSEConfiguration());
+    @Inject
+    public SasEurobonusMastercardSEAgent(AgentComponentProvider componentProvider) {
+        super(componentProvider, new SasEurobonusMastercardSEConfiguration());
     }
 }
