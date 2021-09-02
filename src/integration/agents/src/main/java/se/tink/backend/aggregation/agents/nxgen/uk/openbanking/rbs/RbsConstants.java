40db392ba03e7fd9c24f554b38b2c29736fc7a5d
Malgorzata Kosecka <malgorzata.kosecka@tink.se>
2021-09-02 12:02:22 +0200

    feat(us/rbsuniversal): add new agent CODEFREEZE_OVERRIDE

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/rbs/RbsConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/rbs/RbsConstants.java
@@ -9,6 +9,8 @@ final class RbsConstants {
             "https://personal.secure1.rbs.co.uk/.well-known/openid-configuration";
     static final String CORPORATE_WELL_KNOWN_URL =
             "https://corporate.secure1.rbs.co.uk/.well-known/openid-configuration";
+    static final String UNIVERSAL_LINKS_WELL_KNOWN_URL =
+            "https://secure1.rbs.co.uk/.well-known/openid-configuration";
     static final String CLEAR_SPEND_WELL_KNOWN_URL =
             "https://clearspend.secure1.rbs.co.uk/.well-known/openid-configuration";
 }
