7ce381cd38ce0addac817253e699aa4c383cda6e
Annica Alstermark <annica.alstermark@tink.se>
2021-09-02 19:41:30 +0200

    test(Volksbank): Add test case for expired refresh token
    Asserts that the agent throws SESSION_EXPIRED when receiving a 400 response from the bank with the error description "Access or Refresh token expired".

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/wiremock/VolksbankWiremockTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/wiremock/VolksbankWiremockTest.java
@@ -1,11 +1,15 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.wiremock;
 
 import org.junit.Test;
+import se.tink.backend.aggregation.agents.exceptions.SessionException;
 import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonFileParser;
 import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
 import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockrefresh.AgentWireMockRefreshTest;
+import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.VolksbankConstants;
 import se.tink.backend.aggregation.configuration.AgentsServiceConfigurationReader;
 import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.constants.OAuth2Constants.PersistentStorageKeys;
+import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
 import se.tink.libraries.credentials.service.RefreshableItem;
 import se.tink.libraries.enums.MarketCode;
 
@@ -126,5 +130,40 @@ public class VolksbankWiremockTest {
 
         // then
         agentWireMockRefreshTest.assertExpectedData(expected);
+    }
+
+    /**
+     * Volksbank agent has the same logic for handling expired tokens, it's redundant to have a test
+     * for each provider.
+     */
+    @Test(expected = SessionException.class)
+    public void testSnsBankRefreshTokenExpired() throws Exception {
+
+        final String configurationPath =
+                basePath + "volksbank/wiremock/resources/snsbank-config.yml";
+        final String wireMockServerFilePath =
+                basePath + "volksbank/wiremock/resources/nl-snsbank-ob-refresh-token-expired.aap";
+
+        final AgentsServiceConfiguration configuration =
+                AgentsServiceConfigurationReader.read(configurationPath);
+
+        final AgentWireMockRefreshTest agentWireMockRefreshTest =
+                AgentWireMockRefreshTest.nxBuilder()
+                        .withMarketCode(MarketCode.NL)
+                        .withProviderName("nl-snsbank-oauth2")
+                        .withWireMockFilePath(wireMockServerFilePath)
+                        .withConfigFile(configuration)
+                        .testAutoAuthentication()
+                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                        .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
+                        .addPersistentStorageData(
+                                VolksbankConstants.Storage.CONSENT, "dummy_consent_id")
+                        .addPersistentStorageData(
+                                PersistentStorageKeys.OAUTH_2_TOKEN,
+                                OAuth2Token.createBearer(
+                                        "test_access_token", "test_refresh_token", 0))
+                        .build();
+
+        agentWireMockRefreshTest.executeRefresh();
     }
 }
