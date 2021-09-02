a75ffc0ce315b40318c0d119dec72c42564c64e3
Malgorzata Kosecka <malgorzata.kosecka@tink.se>
2021-09-02 12:03:14 +0200

    feat(us/ulsteruniversal): add new agent CODEFREEZE_OVERRIDE

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/ulster/UlsterConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/ulster/UlsterConstants.java
@@ -10,6 +10,8 @@ final class UlsterConstants {
             "https://personal.secure1.ulsterbank.co.uk/.well-known/openid-configuration";
     public static final String CORPORATE_WELL_KNOWN_URL =
             "https://corporate.secure1.ulsterbank.co.uk/.well-known/openid-configuration";
+    public static final String UNIVERSAL_LINKS_WELL_KNOWN_URL =
+            "https://secure1.ulsterbank.co.uk/.well-known/openid-configuration";
     public static final String CLEAR_SPEND_WELL_KNOWN_URL =
             "https://clearspend.secure1.ulsterbank.co.uk/.well-known/openid-configuration";
 }
