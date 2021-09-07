6f0dd59dda2e0575b4d1f93575fef903bceccac9
Jakub Badysiak <jakub.badysiak@tink.se>
2021-09-07 22:16:58 +0200

    fix(uk/starling): throw session error CODEFREEZE_OVERRIDE
    [IFD-2810]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/starling/auth/steps/StarlingRefreshTokenStep.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/starling/auth/steps/StarlingRefreshTokenStep.java
@@ -2,14 +2,12 @@ package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.auth.st
 
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import se.tink.backend.aggregation.agents.exceptions.SessionException;
-import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.AgentAuthenticationPersistedData;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.AgentAuthenticationProcessState;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.request.AgentProceedNextStepAuthenticationRequest;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.result.AgentAuthenticationResult;
+import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.result.AgentFailedAuthenticationResult;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.process.result.AgentSucceededAuthenticationResult;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.redirect.AgentRefreshableAccessTokenAuthenticationPersistedData;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.authentication.redirect.AgentRefreshableAccessTokenAuthenticationPersistedDataAccessorFactory;
@@ -20,6 +18,7 @@ import se.tink.backend.aggregation.agentsplatform.agentsframework.common.AgentEx
 import se.tink.backend.aggregation.agentsplatform.agentsframework.common.authentication.RefreshableAccessToken;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.common.authentication.RefreshableAccessTokenValidator;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.common.authentication.TokenExpirationDateHelper;
+import se.tink.backend.aggregation.agentsplatform.agentsframework.error.SessionExpiredError;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.http.AuthenticationPersistedDataCookieStoreAccessorFactory;
 import se.tink.backend.aggregation.agentsplatform.agentsframework.http.ExternalApiCallResult;
 
@@ -48,7 +47,6 @@ public class StarlingRefreshTokenStep extends RedirectAuthenticationRefreshToken
         this.tokensValidator = tokensValidator;
     }
 
-    @SneakyThrows
     @Override
     public AgentAuthenticationResult execute(
             AgentProceedNextStepAuthenticationRequest authRequest) {
@@ -69,23 +67,23 @@ public class StarlingRefreshTokenStep extends RedirectAuthenticationRefreshToken
                         accessToken ->
                                 tryToRefreshAccessToken(
                                         accessToken, persistedData, authProcessState, clientInfo))
-                .orElseThrow(this::throwSessionExpiredError);
+                .orElseGet(this::prepareFailedAuthResult);
     }
 
     private AgentAuthenticationResult tryToRefreshAccessToken(
             RefreshableAccessToken accessToken,
             AgentAuthenticationPersistedData persistedData,
             AgentAuthenticationProcessState authProcessState,
-            AgentExtendedClientInfo clientInfo) throws SessionException {
+            AgentExtendedClientInfo clientInfo) {
 
         if (isNotRefreshable(accessToken)) {
-            throwSessionExpiredError();
+            return prepareFailedAuthResult();
         }
 
         return executeRefreshTokenCall(accessToken, persistedData, authProcessState, clientInfo)
                 .filter(this::isRefreshTokenPresent)
                 .map(token -> prepareSuccessAuthResult(token, persistedData))
-                .orElseThrow(this::throwSessionExpiredError);
+                .orElseGet(this::prepareFailedAuthResult);
     }
 
     private boolean isNotRefreshable(RefreshableAccessToken refreshableAccessToken) {
@@ -96,8 +94,8 @@ public class StarlingRefreshTokenStep extends RedirectAuthenticationRefreshToken
         return token.getRefreshToken() != null;
     }
 
-    private SessionException throwSessionExpiredError() {
-        throw SessionError.SESSION_EXPIRED.exception();
+    private AgentAuthenticationResult prepareFailedAuthResult() {
+        return new AgentFailedAuthenticationResult(new SessionExpiredError(), null);
     }
 
     private AgentAuthenticationResult prepareSuccessAuthResult(
