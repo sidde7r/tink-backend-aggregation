4438bd8938727832d1c7ef347ddd17768f73396f
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-09-10 11:38:40 +0200

    test(baltics/luminor): added unit tests
    - Added unit tests for account fetching for when there are accounts and when there are no accounts in response
    - Added unit tests for transactions fetching for when there are no transactions, when there are and when there are transactions but are too old to get paginated
    
    code_freeze_aug_2021

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/accounts/LuminorAccountFetcherTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/accounts/LuminorAccountFetcherTest.java
@@ -1,13 +1,22 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.fetcher.accounts;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
 import java.nio.file.Paths;
+import java.util.Collection;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.LuminorApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.LuminorConstants;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.LuminorConstants.StorageKeys;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.fetcher.LuminorAccountFetcher;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.luminor.fetcher.rpc.AccountsResponse;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date.ConstantLocalDateTimeSource;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date.LocalDateTimeSource;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.balance.BalanceModule;
 import se.tink.backend.aggregation.nxgen.core.account.nxbuilders.modules.id.IdModule;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
@@ -18,11 +27,21 @@ import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class LuminorAccountFetcherTest {
 
+    LuminorApiClient client;
+    LuminorAccountFetcher fetcher;
+    LocalDateTimeSource localDateTimeSource;
     private static final String TEST_DATA_PATH =
             "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/luminor/fetcher/accounts/resources";
 
+    @Before
+    public void setup() {
+        client = mock(LuminorApiClient.class);
+        localDateTimeSource = new ConstantLocalDateTimeSource();
+        fetcher = new LuminorAccountFetcher(client);
+    }
+
     @Test
-    public void shouldMapToTinkAccount() {
+    public void shouldMapToTinkAccountWhenThereAreAccounts() {
 
         // given
         AccountsResponse accountsResponse =
@@ -37,6 +56,30 @@ public class LuminorAccountFetcherTest {
         assertThat(result).isEqualToComparingFieldByFieldRecursively(getExpectedAccountsResponse());
     }
 
+    @Test
+    public void shouldFetchAccounts() {
+        AccountsResponse accountsResponse =
+                SerializationUtils.deserializeFromString(
+                        Paths.get(TEST_DATA_PATH, "accounts_response.json").toFile(),
+                        AccountsResponse.class);
+        when(client.getAccounts()).thenReturn(accountsResponse);
+
+        Collection<TransactionalAccount> result = fetcher.fetchAccounts();
+        Assert.assertFalse(result.isEmpty());
+    }
+
+    @Test
+    public void shouldReturnEmptyListIfThereAreNoAccounts() {
+        AccountsResponse accountsResponse =
+                SerializationUtils.deserializeFromString(
+                        Paths.get(TEST_DATA_PATH, "no_accounts_response.json").toFile(),
+                        AccountsResponse.class);
+        when(client.getAccounts()).thenReturn(accountsResponse);
+
+        Collection<TransactionalAccount> result = fetcher.fetchAccounts();
+        Assert.assertTrue(result.isEmpty());
+    }
+
     private TransactionalAccount getExpectedAccountsResponse() {
         return TransactionalAccount.nxBuilder()
                 .withTypeAndFlagsFrom(LuminorConstants.ACCOUNT_TYPE_MAPPER, "Payment card account")
@@ -45,15 +88,15 @@ public class LuminorAccountFetcherTest {
                                 new ExactCurrencyAmount(BigDecimal.valueOf(152.45), "EUR")))
                 .withId(
                         IdModule.builder()
-                                .withUniqueIdentifier("LT594010049500047594")
+                                .withUniqueIdentifier("LT000000000000000094")
                                 .withAccountNumber(
-                                        "A01A77CCB3F8EE9F4B56E0C26EAAB19AD36A9741F1560DF57B71B0C287794542")
+                                        "A01A77CCB3F8EE9F4B56E0C26THISISAFAKEACCOUNT60DF57B71B0C28779XXXX")
                                 .withAccountName("My main account")
-                                .addIdentifier(new IbanIdentifier("LT594010049500047594"))
+                                .addIdentifier(new IbanIdentifier("LT000000000000000094"))
                                 .build())
-                .putInTemporaryStorage(StorageKeys.ACCOUNT_ID, "LT594010049500047594")
+                .putInTemporaryStorage(StorageKeys.ACCOUNT_ID, "LT000000000000000094")
                 .setApiIdentifier(
-                        "A01A77CCB3F8EE9F4B56E0C26EAAB19AD36A9741F1560DF57B71B0C287794542")
+                        "A01A77CCB3F8EE9F4B56E0C26THISISAFAKEACCOUNT60DF57B71B0C28779XXXX")
                 .addAccountFlags(AccountFlag.PSD2_PAYMENT_ACCOUNT)
                 .addHolderName("Esbjorn Fakename")
                 .build()
