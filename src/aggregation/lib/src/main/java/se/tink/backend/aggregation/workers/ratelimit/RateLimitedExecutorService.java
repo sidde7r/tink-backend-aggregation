68d4c1ec3d72ce51ce3b7363f5aa8b5a2911655b
Damian Baciur <damian.baciur@tink.se>
2021-09-15 14:24:37 +0200

    fix(fi-spankki-ob): Set rate limit to 150rpm + unmask rate limit headers
    [ITE-2743]
    
    At the time of making this commit, S-Pannki has 2 rate limits:
    - 10 requests / second = 600 request / minute
    - 150 requests / minute
    Our current rate is 0.1 which gives 10 requests / second assuming we have 100 pods.
    To meet S-Pankki requirements, we have to reduce our rate 4 times to 0.025.

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/ratelimit/RateLimitedExecutorService.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/ratelimit/RateLimitedExecutorService.java
@@ -42,6 +42,7 @@ public class RateLimitedExecutorService implements Managed {
                     .put("nxgen.nl.openbanking.knab.KnabAgent", 0.05)
                     .put("nxgen.uk.openbanking.nationwide.NationwideV31Agent", 0.05)
                     .put("nxgen.demo.openbanking.demobank.DemobankAgent", 2.)
+                    .put("nxgen.fi.openbanking.spankki.SPankkiAgent", 0.025)
                     .build();
 
     private final MetricRegistry metricRegistry;
