d110d7a291304995dc6c191a9b7cf0dac9c3a6bb
Jakub Kalicki <jakub.kalicki@tink.com>
2021-09-15 17:27:52 +0200

    fix(uk/silicon): Update base URI
    [IFD-2532]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/siliconvalley/SiliconValleyConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/siliconvalley/SiliconValleyConstants.java
@@ -4,7 +4,7 @@ public class SiliconValleyConstants {
 
     public static final String ORGANISATION_ID = "0015800001ZEZ23AAH";
 
-    public static final String AIS_API_URL = "https://openbanking.svb.com/v3.1/api";
+    public static final String AIS_API_URL = "https://openbanking.svb.com/open-banking/v3.1/aisp";
 
     public static final String WELL_KNOWN_URL =
             "https://api.svb.com/.well-known/openid-configuration";
