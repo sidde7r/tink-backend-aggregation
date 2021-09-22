5aa8197a336a3a4d53d3cc7081b31cfaf6f5f648
Ahmed Hussein <ahmed.hussein@tink.se>
2021-09-22 17:30:12 +0200

    feat(demobank): add trxn mutability

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/openbanking/demobank/fetcher/transactionalaccount/entities/TransactionEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/openbanking/demobank/fetcher/transactionalaccount/entities/TransactionEntity.java
@@ -3,14 +3,18 @@ package se.tink.backend.aggregation.agents.nxgen.demo.openbanking.demobank.fetch
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.base.Strings;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Objects;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 import se.tink.libraries.amount.ExactCurrencyAmount;
 
 @JsonObject
 public class TransactionEntity {
+    private static final String MUTABLE_VALUE = "MUTABLE";
+
     @JsonProperty("id")
     private Integer id;
 
@@ -29,14 +33,26 @@ public class TransactionEntity {
     @JsonProperty("currency")
     private String currency;
 
+    @JsonProperty("mutability")
+    private String mutability;
+
     @JsonIgnore
     public Transaction toTinkTransaction() {
-        return Transaction.builder()
-                .setAmount(exactCurrencyAmount())
-                .setDescription(description)
-                .setDate(date)
-                .setPending(pending)
-                .build();
+        return (Transaction)
+                Transaction.builder()
+                        .setAmount(exactCurrencyAmount())
+                        .setDescription(description)
+                        .setDate(date)
+                        .setPending(pending)
+                        .setMutable(isMutable())
+                        .build();
+    }
+
+    private Boolean isMutable() {
+        if (Strings.isNullOrEmpty(mutability)) {
+            return null;
+        }
+        return Objects.equals(MUTABLE_VALUE, mutability);
     }
 
     public ExactCurrencyAmount exactCurrencyAmount() {
