833acdc9eb9d2b2a37da73f2bb6d43a2fd4c9554
Daniel Obrębski <daniel.obrebski@tink.se>
2021-09-27 16:43:27 +0200

    fix(fi/OP): fix transaction building and map new fields
    [ITE-3137]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/opbank/fetcher/entities/TransactionEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/opbank/fetcher/entities/TransactionEntity.java
@@ -1,14 +1,23 @@
 package se.tink.backend.aggregation.agents.nxgen.fi.openbanking.opbank.fetcher.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.common.base.Strings;
 import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import se.tink.backend.aggregation.agents.models.TransactionExternalSystemIdType;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
+import se.tink.backend.aggregation.nxgen.core.transaction.TransactionDates;
 import se.tink.libraries.amount.ExactCurrencyAmount;
+import se.tink.libraries.chrono.AvailableDateInformation;
 import se.tink.libraries.date.ThreadSafeDateFormat;
 
 @JsonObject
+@Getter
+@Slf4j
 public class TransactionEntity {
     private String archiveId;
     private String message;
@@ -22,57 +31,15 @@ public class TransactionEntity {
     private String proprietaryTransactionDescription;
     private PayerEntity payer;
 
-    public String getArchiveId() {
-        return archiveId;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public String getReference() {
-        return reference;
-    }
-
-    public String getAmount() {
-        return amount;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public String getBookingDate() {
-        return bookingDate;
-    }
-
-    public String getValueDate() {
-        return valueDate;
-    }
-
-    public String getPaymentDate() {
-        return paymentDate;
-    }
-
-    public RecipientEntity getRecipient() {
-        return recipient;
-    }
-
-    public String getProprietaryTransactionDescription() {
-        return proprietaryTransactionDescription;
-    }
-
-    public PayerEntity getPayer() {
-        return payer;
-    }
-
     private String getDescription() {
-        if (payer != null) {
+        if (payer != null && StringUtils.isNotBlank(payer.getName())) {
             return payer.getName();
-        } else if (recipient != null) {
+        } else if (recipient != null && StringUtils.isNotBlank(recipient.getName())) {
             return recipient.getName();
-        } else if (!Strings.isNullOrEmpty(message)) {
+        } else if (StringUtils.isNotBlank(message)) {
             return message;
+        } else if (StringUtils.isNotBlank(proprietaryTransactionDescription)) {
+            return proprietaryTransactionDescription;
         }
         return "";
     }
@@ -84,9 +51,38 @@ public class TransactionEntity {
                     .setDescription(getDescription())
                     .setDate(ThreadSafeDateFormat.FORMATTER_DAILY.parse(bookingDate))
                     .setAmount(ExactCurrencyAmount.of(amount, currency))
+                    .setTransactionDates(getTransactionDates())
+                    .setTransactionReference(reference)
+                    .setRawDetails(this)
+                    .addExternalSystemIds(
+                            TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID,
+                            archiveId)
                     .build();
         } catch (ParseException e) {
             throw new IllegalStateException("Parsing error with date");
         }
+    }
+
+    private TransactionDates getTransactionDates() {
+        TransactionDates.Builder builder = TransactionDates.builder();
+        try {
+            if (bookingDate != null) {
+                builder.setBookingDate(new AvailableDateInformation(LocalDate.parse(bookingDate)));
+            }
+            if (valueDate != null) {
+                builder.setValueDate(new AvailableDateInformation(LocalDate.parse(valueDate)));
+            }
+            if (paymentDate != null) {
+                builder.setExecutionDate(
+                        new AvailableDateInformation(LocalDate.parse(paymentDate)));
+            }
+        } catch (DateTimeParseException e) {
+            log.error(
+                    "[OP] Date format changed on API side expected format is YYYY-MM-DD, review the logs.",
+                    e);
+            throw new IllegalStateException("Parsing error with date");
+        }
+
+        return builder.build();
     }
 }
--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/opbank/fetcher/entities/TransactionEntityTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/opbank/fetcher/entities/TransactionEntityTest.java
@@ -2,17 +2,24 @@ package se.tink.backend.aggregation.agents.nxgen.fi.openbanking.opbank.fetcher.e
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.LocalDate;
+import java.util.Collections;
 import org.junit.Test;
+import se.tink.backend.aggregation.agents.models.TransactionExternalSystemIdType;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
+import se.tink.backend.aggregation.nxgen.core.transaction.TransactionDates;
 import se.tink.libraries.amount.ExactCurrencyAmount;
+import se.tink.libraries.chrono.AvailableDateInformation;
 import se.tink.libraries.serialization.utils.SerializationUtils;
 
 public class TransactionEntityTest {
 
+    private static final String TRANSACTION_RESPONSE_WITH_PAYER_AS_STRING =
+            "{\"archiveId\":\"ARCHIVE_ID\",\"message\":\"MESSAGE\",\"reference\":\"00000000003591297866\",\"amount\":\"50.0\",\"currency\":\"EUR\",\"bookingDate\":\"2000-10-10\",\"valueDate\":\"2020-03-19\",\"paymentDate\":null,\"recipient\":null,\"proprietaryTransactionDescription\":\"CARD\",\"payer\":{\"name\":\"PAYER\"}}";
+
     private static final TransactionEntity TRANSACTION_RESPONSE_WITH_PAYER_PRESENT =
             SerializationUtils.deserializeFromString(
-                    " { \"archiveId\" : \"ARCHIVE_ID\", \"message\": \"MESSAGE\", \"amount\": \"50.0\", \"currency\" : \"EUR\" , \"bookingDate\" : \"2000-10-10\", \"valueDate\" : \"2020-03-19\", \"payer\": { \"name\" : \"PAYER\" }, \"proprietaryTransactionDescription\" : \"CARD\" }",
-                    TransactionEntity.class);
+                    TRANSACTION_RESPONSE_WITH_PAYER_AS_STRING, TransactionEntity.class);
     private static final TransactionEntity TRANSACTION_RESPONSE_WITH_RECIPIENT_PRESENT =
             SerializationUtils.deserializeFromString(
                     " { \"archiveId\" : \"ARCHIVE_ID\", \"message\": \"MESSAGE\", \"amount\": \"50.0\", \"currency\" : \"EUR\" , \"bookingDate\" : \"2020-05-05\", \"valueDate\" : \"2020-03-19\", \"recipient\": { \"name\" : \"RECIPIENT\" }, \"proprietaryTransactionDescription\" : \"CARD\" }",
@@ -23,9 +30,14 @@ public class TransactionEntityTest {
                             " { \"archiveId\" : \"ARCHIVE_ID\", \"message\": \"MESSAGE\", \"amount\": \"50.0\", \"currency\" : \"EUR\" , \"bookingDate\" : \"2020-05-05\", \"valueDate\" : \"2020-03-19\", \"proprietaryTransactionDescription\" : \"CARD\" }",
                             TransactionEntity.class);
     private static final TransactionEntity
-            TRANSACTION_RESPONSE_WITHOUT_PAYER_AND_RECIPIENT_AND_MESSAGE_PRESENT =
+            TRANSACTION_RESPONSE_WITHOUT_PAYER_AND_RECIPIENT_AND_MESSAGE_AND_PROPRIETARY_TRANSACTION_DESCRIPTION =
                     SerializationUtils.deserializeFromString(
-                            " { \"archiveId\" : \"ARCHIVE_ID\", \"amount\": \"50.0\", \"currency\" : \"EUR\" , \"bookingDate\" : \"2020-05-05\", \"valueDate\" : \"2020-03-19\", \"proprietaryTransactionDescription\" : \"CARD\" }",
+                            " { \"archiveId\" : \"ARCHIVE_ID\", \"amount\": \"50.0\", \"currency\" : \"EUR\" , \"bookingDate\" : \"2020-05-05\", \"valueDate\" : \"2020-03-19\", \"proprietaryTransactionDescription\" : \"\" }",
+                            TransactionEntity.class);
+    private static final TransactionEntity
+            TRANSACTION_RESPONSE_WITHOUT_PAYER_AND_RECIPIENT_AND_MESSAGE_AND_PROPRIETARY_TRANSACTION_DESCRIPTION_PRESENT =
+                    SerializationUtils.deserializeFromString(
+                            "{\"archiveId\": \"ARCHIVE_ID\", \"message\": \"\", \"amount\": \"-50.00\", \"currency\": \"EUR\", \"bookingDate\": \"2020-10-09\", \"valueDate\": \"2020-10-09\", \"paymentDate\": \"2020-10-09\", \"recipient\": {\"name\": \"\", \"address\": []}, \"proprietaryTransactionDescription\": \"PTD\"}",
                             TransactionEntity.class);
     private static final TransactionEntity TRANSACTION_RESPONSE_WITH_INVALID_DATE =
             SerializationUtils.deserializeFromString(
@@ -34,37 +46,81 @@ public class TransactionEntityTest {
 
     @Test
     public void shouldUsePayerAsDescriptionWhenPayerPresentInResponse() {
+        // given & when
         Transaction transaction = TRANSACTION_RESPONSE_WITH_PAYER_PRESENT.toTinkTransaction();
+
+        // then
         assertThat(transaction.getDescription()).isEqualTo("PAYER");
     }
 
     @Test
     public void shouldUseRecipientAsDescriptionWhenPayerNotPresentInResponse() {
+        // given & when
         Transaction transaction = TRANSACTION_RESPONSE_WITH_RECIPIENT_PRESENT.toTinkTransaction();
+
+        // then
         assertThat(transaction.getDescription()).isEqualTo("RECIPIENT");
     }
 
     @Test
     public void shouldUseMessageAsDescriptionWhenPayerAndRecipientNotPresentInResponse() {
+        // given & when
         Transaction transaction =
                 TRANSACTION_RESPONSE_WITHOUT_PAYER_AND_RECIPIENT_PRESENT.toTinkTransaction();
+
+        // then
         assertThat(transaction.getDescription()).isEqualTo("MESSAGE");
     }
 
     @Test
-    public void shouldSetEmptyDescriptionWhenAllEligibleFieldsAreMissing() {
+    public void
+            shouldUseProprietaryTransactionDescriptionAsDescriptionWhenPayerAndRecipientNotPresentInResponse() {
+        // given & when
         Transaction transaction =
-                TRANSACTION_RESPONSE_WITHOUT_PAYER_AND_RECIPIENT_AND_MESSAGE_PRESENT
+                TRANSACTION_RESPONSE_WITHOUT_PAYER_AND_RECIPIENT_AND_MESSAGE_AND_PROPRIETARY_TRANSACTION_DESCRIPTION_PRESENT
                         .toTinkTransaction();
-        assertThat(transaction.getDescription()).isEqualTo("");
+
+        // then
+        assertThat(transaction.getDescription()).isEqualTo("PTD");
+    }
+
+    @Test
+    public void shouldSetEmptyDescriptionWhenAllEligibleFieldsAreMissing() {
+        // given & when
+        Transaction transaction =
+                TRANSACTION_RESPONSE_WITHOUT_PAYER_AND_RECIPIENT_AND_MESSAGE_AND_PROPRIETARY_TRANSACTION_DESCRIPTION
+                        .toTinkTransaction();
+
+        // then
+        assertThat(transaction.getDescription()).isEmpty();
     }
 
     @Test
     public void shouldProperlyMapIntoTinkTransaction() {
+        // given & when
         Transaction transaction = TRANSACTION_RESPONSE_WITH_PAYER_PRESENT.toTinkTransaction();
-        assertThat(transaction.getDescription()).isEqualTo("PAYER");
+
+        // then
         assertThat(transaction.getDate().toString()).isEqualTo("Tue Oct 10 10:00:00 UTC 2000");
+        assertThat(transaction.getDescription()).isEqualTo("PAYER");
         assertThat(transaction.getExactAmount()).isEqualTo(ExactCurrencyAmount.inEUR(50.0));
+        assertThat(transaction.getExternalSystemIds())
+                .isEqualTo(
+                        Collections.singletonMap(
+                                TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID,
+                                "ARCHIVE_ID"));
+        assertThat(transaction.getTransactionDates())
+                .usingRecursiveComparison()
+                .isEqualTo(
+                        TransactionDates.builder()
+                                .setBookingDate(
+                                        new AvailableDateInformation(LocalDate.parse("2000-10-10")))
+                                .setValueDate(
+                                        new AvailableDateInformation(LocalDate.parse("2020-03-19")))
+                                .build());
+        assertThat(transaction.getTransactionReference()).isEqualTo("00000000003591297866");
+        assertThat(transaction.getRawDetails())
+                .isEqualTo(TRANSACTION_RESPONSE_WITH_PAYER_AS_STRING);
     }
 
     @Test(expected = IllegalStateException.class)
