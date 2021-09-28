80aac2627f6a7b5ffbace39ef49a6b1a607c3994
Sai He <sai.he@tink.se>
2021-09-29 00:00:33 +0200

    test(SHB/RE): To add full wiremock coverage

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/executor/utilities/HandelsbankenDateUtils.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/executor/utilities/HandelsbankenDateUtils.java
@@ -1,21 +1,20 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.executor.utilities;
 
+import java.time.Clock;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.Locale;
 import java.util.TimeZone;
+import org.assertj.core.util.VisibleForTesting;
 import se.tink.libraries.date.CountryDateHelper;
 import se.tink.libraries.date.ThreadSafeDateFormat;
 
 public class HandelsbankenDateUtils {
 
-    private final CountryDateHelper dateHelper;
-
-    public HandelsbankenDateUtils() {
-        ZoneId defaultZoneId = ZoneId.of("CET");
-        Locale defaultLocale = new Locale("sv", "SE");
-        dateHelper = new CountryDateHelper(defaultLocale, TimeZone.getTimeZone(defaultZoneId));
-    }
+    private static final ZoneId DEFAULT_ZONE_ID = ZoneId.of("CET");
+    private static final Locale DEFAULT_LOCALE = new Locale("sv", "SE");
+    private static final CountryDateHelper dateHelper =
+            new CountryDateHelper(DEFAULT_LOCALE, TimeZone.getTimeZone(DEFAULT_ZONE_ID));
 
     public String getTransferDateForInternalTransfer(Date date) {
         return ThreadSafeDateFormat.FORMATTER_DAILY.format(
@@ -31,5 +30,10 @@ public class HandelsbankenDateUtils {
     public String getTransferDateForBgPg(Date date) {
         return ThreadSafeDateFormat.FORMATTER_DAILY.format(
                 dateHelper.getProvidedDateOrBestPossibleDate(date, 00, 00));
+    }
+
+    @VisibleForTesting
+    static void setClockForTesting(Clock clockForTesting) {
+        dateHelper.setClock(clockForTesting);
     }
 }
--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/executor/utilities/HandelsbankenDateUtilsTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/executor/utilities/HandelsbankenDateUtilsTest.java
@@ -2,6 +2,9 @@ package se.tink.backend.aggregation.agents.nxgen.se.banks.handelsbanken.executor
 
 import static org.junit.Assert.*;
 
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
@@ -13,7 +16,7 @@ public class HandelsbankenDateUtilsTest {
     public void testPaymentDateIsNotChanged_when_ExplicitlySet() {
         // given
         HandelsbankenDateUtils handelsbankenDateUtils = new HandelsbankenDateUtils();
-        LocalDate localDate = LocalDate.of(2020, 05, 29);
+        LocalDate localDate = LocalDate.of(2020, 5, 29);
         final Date executionDate = Date.from(localDate.atStartOfDay(ZoneId.of("CET")).toInstant());
 
         // when
@@ -21,5 +24,27 @@ public class HandelsbankenDateUtilsTest {
 
         // then
         assertEquals(paymentDate, localDate.toString());
+    }
+
+    @Test
+    public void testGetTransferDateForBgPg_SameBusinessDate_IfBusinessDateBeforeCutOff()
+            throws Exception {
+        String instantExpected = "2020-01-23T09:15:30Z";
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        Date date = formatter.parse(instantExpected);
+        HandelsbankenDateUtils.setClockForTesting(
+                Clock.fixed(Instant.parse(instantExpected), ZoneId.of("CET")));
+        HandelsbankenDateUtils handelsbankenDateUtils = new HandelsbankenDateUtils();
+        assertEquals("2020-01-23", handelsbankenDateUtils.getTransferDateForBgPg(date));
+    }
+
+    @Test
+    public void
+            testGetTransferDateForExternalA2A_DifferentBusinessDate_IfBusinessDateAfterCutOff() {
+        String instantExpected = "2020-01-23T19:15:30Z";
+        HandelsbankenDateUtils.setClockForTesting(
+                Clock.fixed(Instant.parse(instantExpected), ZoneId.of("CET")));
+        HandelsbankenDateUtils handelsbankenDateUtils = new HandelsbankenDateUtils();
+        assertEquals("2020-01-24", handelsbankenDateUtils.getTransferDateForExternalTransfer(null));
     }
 }
