2341c89a5db3f0a4cbc3995637f67244209d8219
Jan Sameryt <jan.sameryt@tink.com>
2021-09-29 15:57:59 +0200

    refactor(errors): of Exception/Factory class
    [DEM-3]
    
    This commit merges functionalities of Factory/Exception class, to provide (hopefully) even more user friendly experience.

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/exceptions/connectivity/ConnectivityException.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/exceptions/connectivity/ConnectivityException.java
@@ -2,18 +2,70 @@ package se.tink.backend.aggregation.agents.exceptions.connectivity;
 
 import lombok.Getter;
 import lombok.NonNull;
+import lombok.With;
 import se.tink.connectivity.errors.ConnectivityError;
+import se.tink.connectivity.errors.ConnectivityErrorDetails;
+import se.tink.connectivity.errors.ConnectivityErrorType;
 import se.tink.libraries.i18n.LocalizableKey;
 
 public final class ConnectivityException extends RuntimeException {
 
-    private final LocalizableKey userMessage;
-    @Getter private final ConnectivityError error;
-    private final Throwable cause;
-    private final String internalMessage;
+    private static final AccountInformationErrorDefaultMessageProvider
+            ACCOUNT_INFORMATION_ERROR_DEFAULT_MESSAGE_PROVIDER =
+                    new AccountInformationErrorDefaultMessageProvider();
+    private static final AuthorizationErrorDefaultMessageProvider
+            AUTHORIZATION_ERROR_DEFAULT_MESSAGE_PROVIDER =
+                    new AuthorizationErrorDefaultMessageProvider();
+    private static final ProviderErrorDefaultMessageProvider
+            PROVIDER_ERROR_DEFAULT_MESSAGE_PROVIDER = new ProviderErrorDefaultMessageProvider();
+    private static final TinkSideErrorDefaultMessageProvider
+            TINK_SIDE_ERROR_DEFAULT_MESSAGE_PROVIDER = new TinkSideErrorDefaultMessageProvider();
+    private static final UserLoginErrorDefaultMessageProvider
+            USER_LOGIN_ERROR_DEFAULT_MESSAGE_PROVIDER = new UserLoginErrorDefaultMessageProvider();
 
-    ConnectivityException(ConnectivityError error) {
-        this(error, new LocalizableKey(error.getDisplayMessage()), null, null);
+    @Getter private final ConnectivityError error;
+    @With private final LocalizableKey userMessage;
+    @With private final Throwable cause;
+    @With private final String internalMessage;
+
+    public ConnectivityException(ConnectivityErrorDetails.AccountInformationErrors reason) {
+        this(
+                ConnectivityErrorType.ACCOUNT_INFORMATION_ERROR,
+                reason.name(),
+                ACCOUNT_INFORMATION_ERROR_DEFAULT_MESSAGE_PROVIDER.provide(reason));
+    }
+
+    public ConnectivityException(ConnectivityErrorDetails.AuthorizationErrors reason) {
+        this(
+                ConnectivityErrorType.AUTHORIZATION_ERROR,
+                reason.name(),
+                AUTHORIZATION_ERROR_DEFAULT_MESSAGE_PROVIDER.provide(reason));
+    }
+
+    public ConnectivityException(ConnectivityErrorDetails.ProviderErrors reason) {
+        this(
+                ConnectivityErrorType.PROVIDER_ERROR,
+                reason.name(),
+                PROVIDER_ERROR_DEFAULT_MESSAGE_PROVIDER.provide(reason));
+    }
+
+    public ConnectivityException(ConnectivityErrorDetails.TinkSideErrors reason) {
+        this(
+                ConnectivityErrorType.TINK_SIDE_ERROR,
+                reason.name(),
+                TINK_SIDE_ERROR_DEFAULT_MESSAGE_PROVIDER.provide(reason));
+    }
+
+    public ConnectivityException(ConnectivityErrorDetails.UserLoginErrors reason) {
+        this(
+                ConnectivityErrorType.USER_LOGIN_ERROR,
+                reason.name(),
+                USER_LOGIN_ERROR_DEFAULT_MESSAGE_PROVIDER.provide(reason));
+    }
+
+    private ConnectivityException(
+            ConnectivityErrorType errorType, String reasonName, LocalizableKey userMessage) {
+        this(constructConnectivityError(errorType, reasonName, userMessage), null, null, null);
     }
 
     private ConnectivityException(
@@ -21,28 +73,26 @@ public final class ConnectivityException extends RuntimeException {
             LocalizableKey userMessage,
             Throwable cause,
             String internalMessage) {
-        super(getMessage(error, internalMessage), cause);
+        super(decideOnMessage(error, internalMessage), cause);
         this.error = error;
         this.userMessage = userMessage;
         this.cause = cause;
         this.internalMessage = internalMessage;
     }
 
-    private static String getMessage(@NonNull ConnectivityError error, String internalMessage) {
+    private static String decideOnMessage(
+            @NonNull ConnectivityError error, String internalMessage) {
         return internalMessage != null
                 ? internalMessage
                 : String.format("Cause: %s.%s", error.getType(), error.getDetails().getReason());
     }
 
-    public ConnectivityException withUserMessage(LocalizableKey userMessage) {
-        return new ConnectivityException(this.error, userMessage, this.cause, this.internalMessage);
-    }
-
-    public ConnectivityException withCause(Throwable cause) {
-        return new ConnectivityException(this.error, this.userMessage, cause, this.internalMessage);
-    }
-
-    public ConnectivityException withExceptionMessage(String internalMessage) {
-        return new ConnectivityException(this.error, this.userMessage, this.cause, internalMessage);
+    private static ConnectivityError constructConnectivityError(
+            ConnectivityErrorType errorType, String reasonName, LocalizableKey userMessage) {
+        return ConnectivityError.newBuilder()
+                .setType(errorType)
+                .setDetails(ConnectivityErrorDetails.newBuilder().setReason(reasonName).build())
+                .setDisplayMessage(userMessage.get())
+                .build();
     }
 }
