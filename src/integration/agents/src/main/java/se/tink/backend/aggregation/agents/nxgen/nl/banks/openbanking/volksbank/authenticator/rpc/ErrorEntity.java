1eb6f6389a009df91babba31555b0d40800608b2
Nazri Bin Abdullah <nazri.abdullah@tink.com>
2021-10-03 23:24:35 +0200

    refactor(nl/volksbank): reduced complexity for error response component

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/authenticator/rpc/ErrorEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/banks/openbanking/volksbank/authenticator/rpc/ErrorEntity.java
@@ -1,35 +1,24 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.authenticator.rpc;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
 import se.tink.backend.aggregation.agents.nxgen.nl.banks.openbanking.volksbank.VolksbankConstants.ErrorCodes;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
+@Getter
 @JsonObject
-public class ErrorResponse {
+public class ErrorEntity {
 
     private String category;
     private String code;
     private String text;
 
-    public String getCategory() {
-        return category;
+    @JsonIgnore
+    public boolean isConsentExpiredOrInvalid() {
+        return ErrorCodes.CONSENT_STATUS.contains(code);
     }
 
-    public String getCode() {
-        return code;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public boolean isConsentExpired() {
-        return code.equalsIgnoreCase(ErrorCodes.CONSENT_EXPIRED);
-    }
-
-    public boolean isConsentInvalid() {
-        return code.equalsIgnoreCase(ErrorCodes.CONSENT_INVALID);
-    }
-
+    @JsonIgnore
     public boolean isServiceBlocked() {
         return code.equalsIgnoreCase(ErrorCodes.SERVICE_BLOCKED);
     }
