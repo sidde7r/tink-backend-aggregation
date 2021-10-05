4903c331684b00d7f3b45d30cebf0a6a4575d5f8
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-10-05 17:10:16 +0200

    refactor(SE/SWEDBANK): refactored class
    - Added getter annotation
    - Removed unused method
    
    code_freeze_aug_2021

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/serviceprovider/rpc/SelectedProfileResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/swedbank/serviceprovider/rpc/SelectedProfileResponse.java
@@ -1,45 +1,15 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.swedbank.serviceprovider.rpc;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.util.Map;
-import java.util.Optional;
+import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
+@Getter
 public class SelectedProfileResponse {
     private SelectedProfileEntity selectedProfile;
     private Map<String, MenuItemLinkEntity> menuItems;
     private boolean mobile;
     private boolean nibprimaryUser;
     private String cacheGroup;
-
-    public SelectedProfileEntity getSelectedProfile() {
-        return selectedProfile;
-    }
-
-    public Map<String, MenuItemLinkEntity> getMenuItems() {
-        return menuItems;
-    }
-
-    public boolean isMobile() {
-        return mobile;
-    }
-
-    public boolean isNibprimaryUser() {
-        return nibprimaryUser;
-    }
-
-    public String getCacheGroup() {
-        return cacheGroup;
-    }
-
-    @JsonIgnore
-    public Optional<MenuItemLinkEntity> getMenuItem(String menuItemIdentifier) {
-        if (menuItems == null) {
-            throw new IllegalStateException();
-        }
-
-        return Optional.ofNullable(menuItems.get(menuItemIdentifier))
-                .filter(MenuItemLinkEntity::isAuthorized);
-    }
 }
