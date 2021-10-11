469b4f23bcf3bbc2acf482d2bbbd90fc378c8a58
Berk Gedik <berk.gedik@tink.com>
2021-10-11 23:46:00 +0200

    test(event): Wiremock test for event emission on SEB OB

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/seb/wiremock/SEBWireMockRawBankDataEventEmissionTest.java
@@ -0,0 +1,109 @@
+package se.tink.backend.aggregation.agents.nxgen.se.openbanking.seb.wiremock;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import se.tink.backend.agents.rpc.Field;
+import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonFileParser;
+import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
+import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockpayment.AgentWireMockPaymentTest;
+import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockpayment.command.PaymentCommand;
+import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockrefresh.AgentWireMockRefreshTest;
+import se.tink.backend.aggregation.configuration.AgentsServiceConfigurationReader;
+import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
+import se.tink.eventproducerservice.events.grpc.RawBankDataTrackerEventProto.RawBankDataTrackerEvent;
+import se.tink.libraries.account.AccountIdentifier;
+import se.tink.libraries.account.enums.AccountIdentifierType;
+import se.tink.libraries.amount.ExactCurrencyAmount;
+import se.tink.libraries.credentials.service.RefreshableItem;
+import se.tink.libraries.enums.MarketCode;
+import se.tink.libraries.payment.rpc.Creditor;
+import se.tink.libraries.payment.rpc.Debtor;
+import se.tink.libraries.payment.rpc.Payment;
+import se.tink.libraries.transfer.enums.RemittanceInformationType;
+import se.tink.libraries.transfer.rpc.RemittanceInformation;
+
+public class SEBWireMockRawBankDataEventEmissionTest {
+
+    private static final String CONFIGURATION_PATH =
+            "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/seb/wiremock/resources/configuration.yml";
+
+    @Test
+    public void testRawBankDataEventsAreEmittedAndAgentDoesNotCrashInRefresh() throws Exception {
+
+        // given
+        final String wireMockServerFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/seb/wiremock/resources/wireMock-seb-ob.aap";
+        final String contractFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/seb/wiremock/resources/agent-contract.json";
+
+        final AgentsServiceConfiguration configuration =
+                AgentsServiceConfigurationReader.read(CONFIGURATION_PATH);
+
+        final AgentContractEntity expected =
+                new AgentContractEntitiesJsonFileParser()
+                        .parseContractOnBasisOfFile(contractFilePath);
+
+        final AgentWireMockRefreshTest agentWireMockRefreshTest =
+                AgentWireMockRefreshTest.builder(MarketCode.SE, "se-seb-ob", wireMockServerFilePath)
+                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                        .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
+                        .withConfigurationFile(configuration)
+                        .build();
+
+        // when
+        agentWireMockRefreshTest.executeRefresh();
+
+        // then
+        agentWireMockRefreshTest.assertExpectedData(expected);
+        List<RawBankDataTrackerEvent> messageList =
+                agentWireMockRefreshTest.getEmittedRawBankDataEvents();
+        Assert.assertTrue(messageList.size() > 0);
+    }
+
+    @Test
+    public void testRawBankDataEventsAreEmittedAndAgentDoesNotCrashInPayment() throws Exception {
+
+        // given
+        final String wireMockFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/seb/wiremock/resources/wiremock-seb-ob-pis-pg.aap";
+        final AgentsServiceConfiguration configuration =
+                AgentsServiceConfigurationReader.read(CONFIGURATION_PATH);
+
+        final AgentWireMockPaymentTest agentWireMockPaymentTest =
+                AgentWireMockPaymentTest.builder(MarketCode.SE, "se-seb-ob", wireMockFilePath)
+                        .withConfigurationFile(configuration)
+                        .withHttpDebugTrace()
+                        .withPayment(createMockedDomesticPGPayment())
+                        .addCredentialField(Field.Key.USERNAME.getFieldKey(), "197710120000")
+                        .buildWithLogin(PaymentCommand.class);
+
+        agentWireMockPaymentTest.executePayment();
+        Assert.assertTrue(true);
+        List<RawBankDataTrackerEvent> messageList =
+                agentWireMockPaymentTest.getEmittedRawBankDataEvents();
+        Assert.assertTrue(messageList.size() > 0);
+    }
+
+    private Payment createMockedDomesticPGPayment() {
+        RemittanceInformation remittanceInformation = new RemittanceInformation();
+        remittanceInformation.setType(RemittanceInformationType.OCR);
+        remittanceInformation.setValue("1047514784933");
+
+        return new Payment.Builder()
+                .withCreditor(
+                        new Creditor(
+                                AccountIdentifier.create(AccountIdentifierType.SE_PG, "5768353"),
+                                "Tink"))
+                .withDebtor(
+                        new Debtor(
+                                AccountIdentifier.create(
+                                        AccountIdentifierType.IBAN, "SE4550000000058398257466")))
+                .withExactCurrencyAmount(ExactCurrencyAmount.inSEK(585.57))
+                .withCurrency("SEK")
+                .withRemittanceInformation(remittanceInformation)
+                .withExecutionDate(LocalDate.parse("2021-01-21"))
+                .build();
+    }
+}
