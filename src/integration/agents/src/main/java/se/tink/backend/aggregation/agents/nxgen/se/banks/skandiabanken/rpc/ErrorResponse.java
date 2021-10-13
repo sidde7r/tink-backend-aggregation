561f1e272069ff930f89fb1bf1cd11090455916a
Annica Alstermark <annica.alstermark@tink.se>
2021-10-13 10:46:40 +0200

    refactor(Skandia/RE): Remove unsused method/constants

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/skandiabanken/rpc/ErrorResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/skandiabanken/rpc/ErrorResponse.java
@@ -1,9 +1,5 @@
 package se.tink.backend.aggregation.agents.nxgen.se.banks.skandiabanken.rpc;
 
-import static se.tink.backend.aggregation.agents.nxgen.se.banks.skandiabanken.SkandiaBankenConstants.ErrorMessages.ERROR_CODE_MISSING_CREDIT_CARDS;
-import static se.tink.backend.aggregation.agents.nxgen.se.banks.skandiabanken.SkandiaBankenConstants.ErrorMessages.ERROR_MESSAGE_COMMUNICATION_FAILED;
-import static se.tink.backend.aggregation.agents.nxgen.se.banks.skandiabanken.SkandiaBankenConstants.ErrorMessages.STATUS_MESSAGE_INTERNAL_SERVER_ERROR;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import se.tink.backend.aggregation.annotations.JsonObject;
@@ -25,15 +21,5 @@ public class ErrorResponse {
     @JsonIgnore
     public boolean isUnauthorized() {
         return statusMessage.equalsIgnoreCase("Unauthorized");
-    }
-
-    public boolean isNoCreditCardsError() {
-        if (statusCode == 500
-                && STATUS_MESSAGE_INTERNAL_SERVER_ERROR.equalsIgnoreCase(statusMessage)
-                && ERROR_CODE_MISSING_CREDIT_CARDS.equalsIgnoreCase(errorCode)
-                && ERROR_MESSAGE_COMMUNICATION_FAILED.equalsIgnoreCase(errorMessage)) {
-            return true;
-        }
-        return false;
     }
 }
