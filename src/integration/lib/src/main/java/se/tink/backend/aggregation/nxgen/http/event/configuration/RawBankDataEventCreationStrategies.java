b617418bd72a70079cabf85416ac3b6628c7a27a
Berk Gedik <berk.gedik@tink.com>
2021-10-19 12:18:52 +0200

    refactor(event): use new date masking strategy

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/event/configuration/RawBankDataEventCreationStrategies.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/event/configuration/RawBankDataEventCreationStrategies.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.nxgen.http.event.configuration;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import lombok.Builder;
@@ -8,6 +9,7 @@ import se.tink.backend.aggregation.nxgen.http.event.masking.keys.MaskKeysWithNum
 import se.tink.backend.aggregation.nxgen.http.event.masking.keys.RawBankDataKeyValueMaskingStrategy;
 import se.tink.backend.aggregation.nxgen.http.event.masking.values.MaskAllValueMaskingStrategy;
 import se.tink.backend.aggregation.nxgen.http.event.masking.values.RawBankDataFieldValueMaskingStrategy;
+import se.tink.backend.aggregation.nxgen.http.event.masking.values.UnmaskDateValueMaskingStrategy;
 import se.tink.backend.aggregation.nxgen.http.event.type_detection.DummyFieldTypeDetectionStrategy;
 import se.tink.backend.aggregation.nxgen.http.event.type_detection.RawBankDataFieldTypeDetectionStrategy;
 
@@ -24,7 +26,9 @@ public class RawBankDataEventCreationStrategies {
                 .keyMaskingStrategies(
                         Collections.singletonList(new MaskKeysWithNumericValuesStrategy()))
                 .valueMaskingStrategies(
-                        Collections.singletonList(new MaskAllValueMaskingStrategy()))
+                        Arrays.asList(
+                                new UnmaskDateValueMaskingStrategy(),
+                                new MaskAllValueMaskingStrategy()))
                 .fieldTypeDetectionStrategies(
                         Collections.singletonList(new DummyFieldTypeDetectionStrategy()))
                 .build();
