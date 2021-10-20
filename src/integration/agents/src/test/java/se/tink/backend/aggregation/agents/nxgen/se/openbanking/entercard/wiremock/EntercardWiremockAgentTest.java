347688ca0b1cf29a74fe2f9c1eea3fb7860dfcb2
Elena Kolpakova <elena.kolpakova@tink.com>
2021-10-20 23:25:53 +0200

    test(se/coop): add wiremock test

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/entercard/wiremock/EntercardWiremockAgentTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/entercard/wiremock/EntercardWiremockAgentTest.java
@@ -21,18 +21,26 @@ public class EntercardWiremockAgentTest {
                 "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/entercard/wiremock/resources/remember_wiremock.aap";
         final String contractFilePath =
                 "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/entercard/wiremock/resources/remember_agent_contract.json";
-        final AgentsServiceConfiguration configuration =
-                AgentsServiceConfigurationReader.read(CONFIGURATION_PATH);
 
         final AgentWireMockRefreshTest agentWireMockRefreshTest =
-                AgentWireMockRefreshTest.builder(
-                                MarketCode.SE, "se-remembermastercard-ob", wireMockServerFilePath)
-                        .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
-                        .addCredentialField(Field.Key.USERNAME.getFieldKey(), MOCKED_SSN)
-                        .addCallbackData("code", "dummyCode")
-                        .withConfigurationFile(configuration)
-                        .addRefreshableItems()
-                        .build();
+                buildWiremockRefreshTest(wireMockServerFilePath, "se-remembermastercard-ob");
+        final AgentContractEntity expected =
+                new AgentContractEntitiesJsonFileParser()
+                        .parseContractOnBasisOfFile(contractFilePath);
+
+        agentWireMockRefreshTest.executeRefresh();
+        agentWireMockRefreshTest.assertExpectedData(expected);
+    }
+
+    @Test
+    public void testCoopSeRefresh() throws Exception {
+        final String wireMockServerFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/entercard/wiremock/resources/coop_wiremock.aap";
+        final String contractFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/entercard/wiremock/resources/coop_agent_contract.json";
+
+        final AgentWireMockRefreshTest agentWireMockRefreshTest =
+                buildWiremockRefreshTest(wireMockServerFilePath, "se-coop-ob");
 
         final AgentContractEntity expected =
                 new AgentContractEntitiesJsonFileParser()
@@ -41,5 +49,23 @@ public class EntercardWiremockAgentTest {
         agentWireMockRefreshTest.executeRefresh();
 
         agentWireMockRefreshTest.assertExpectedData(expected);
+    }
+
+    private AgentWireMockRefreshTest buildWiremockRefreshTest(
+            String wireMockServerFilePath, String providerName) throws Exception {
+
+        final AgentsServiceConfiguration configuration =
+                AgentsServiceConfigurationReader.read(CONFIGURATION_PATH);
+
+        return AgentWireMockRefreshTest.nxBuilder()
+                .withMarketCode(MarketCode.SE)
+                .withProviderName(providerName)
+                .withWireMockFilePath(wireMockServerFilePath)
+                .withConfigFile(configuration)
+                .testFullAuthentication()
+                .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
+                .addCredentialField(Field.Key.USERNAME.getFieldKey(), MOCKED_SSN)
+                .addCallbackData("code", "dummyCode")
+                .build();
     }
 }
