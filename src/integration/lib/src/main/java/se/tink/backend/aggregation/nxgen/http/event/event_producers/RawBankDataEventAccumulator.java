66fe63c21cc0b534d6ef607155c11dfb449866d6
Berk Gedik <berk.gedik@tink.com>
2021-10-20 11:44:50 +0200

    chore(event): removed unnecessary log

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/event/event_producers/RawBankDataEventAccumulator.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/event/event_producers/RawBankDataEventAccumulator.java
@@ -4,19 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import lombok.extern.slf4j.Slf4j;
 import se.tink.eventproducerservice.events.grpc.RawBankDataTrackerEventProto;
 import se.tink.eventproducerservice.events.grpc.RawBankDataTrackerEventProto.RawBankDataTrackerEvent;
 import se.tink.eventproducerservice.events.grpc.RawBankDataTrackerEventProto.RawBankDataTrackerEventBankField;
 
-@Slf4j
 public class RawBankDataEventAccumulator {
 
     private final List<RawBankDataTrackerEvent> eventList = new ArrayList<>();
-    private long totalProcessingTime = 0;
 
     public void addEvent(RawBankDataTrackerEvent event) {
-        long startTime = System.currentTimeMillis();
         RawBankDataTrackerEvent.Builder builder =
                 RawBankDataTrackerEvent.newBuilder()
                         .setTimestamp(event.getTimestamp())
@@ -45,14 +41,9 @@ public class RawBankDataEventAccumulator {
         }
         RawBankDataTrackerEvent aggregatedEvent = builder.build();
         this.eventList.add(aggregatedEvent);
-        long endTime = System.currentTimeMillis();
-        this.totalProcessingTime += endTime - startTime;
     }
 
     public List<RawBankDataTrackerEvent> getEventList() {
-        log.info(
-                "[RawBankDataEventAccumulator] Spent {} milliseconds for raw bank data event processing",
-                this.totalProcessingTime);
         return eventList;
     }
 }
