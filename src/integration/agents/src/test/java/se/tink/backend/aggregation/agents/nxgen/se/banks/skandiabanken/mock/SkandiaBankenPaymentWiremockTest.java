1e1ed77d1776c9c6c18eeb1bb39a35b2cead74c2
Andreas Olausson <andreas.olausson@tink.se>
2021-10-21 10:04:05 +0200

    test(Skandia/RE): add PIS wiremock with future execution date
    [TC-5303] Adding test for future BankGiro payment

---
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/skandiabanken/mock/SkandiaBankenPaymentWiremockTest.java
@@ -0,0 +1,61 @@
+package se.tink.backend.aggregation.agents.nxgen.se.banks.skandiabanken.mock;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import org.junit.Assert;
+import org.junit.Test;
+import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockpayment.AgentWireMockPaymentTest;
+import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockpayment.command.TransferCommand;
+import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.constants.OAuth2Constants;
+import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
+import se.tink.libraries.account.identifiers.BankGiroIdentifier;
+import se.tink.libraries.account.identifiers.SwedishIdentifier;
+import se.tink.libraries.amount.ExactCurrencyAmount;
+import se.tink.libraries.enums.MarketCode;
+import se.tink.libraries.serialization.utils.SerializationUtils;
+import se.tink.libraries.transfer.enums.TransferType;
+import se.tink.libraries.transfer.rpc.RemittanceInformation;
+import se.tink.libraries.transfer.rpc.Transfer;
+
+public class SkandiaBankenPaymentWiremockTest {
+    private static final ZoneId DEFAULT_ZONE_ID = ZoneId.of("CET");
+
+    private static final String RESOURCE_PACKAGE =
+            "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/se/banks/skandiabanken/mock/resources/";
+
+    @Test
+    public void testBankGiroPaymentWithFutureExecutionDate() throws Exception {
+        Transfer transfer = new Transfer();
+        transfer.setSource(new SwedishIdentifier("91599999999"));
+        transfer.setDestination(new BankGiroIdentifier("9999999"));
+        transfer.setAmount(ExactCurrencyAmount.inSEK(1));
+        transfer.setType(TransferType.PAYMENT);
+        transfer.setSourceMessage("Tink source");
+        transfer.setDueDate(
+                Date.from(LocalDate.of(2021, 10, 30).atStartOfDay(DEFAULT_ZONE_ID).toInstant()));
+        RemittanceInformation remittanceInformation = new RemittanceInformation();
+        remittanceInformation.setValue("Some value");
+
+        transfer.setRemittanceInformation(remittanceInformation);
+
+        final String wiremockFilePath =
+                RESOURCE_PACKAGE + "skandiabanken-mock-pis-future-successful.aap";
+
+        AgentWireMockPaymentTest agentWireMockPaymentTest =
+                AgentWireMockPaymentTest.builder(
+                                MarketCode.SE, "skandiabanken-ssn-bankid", wiremockFilePath)
+                        .addPersistentStorageData(
+                                OAuth2Constants.PersistentStorageKeys.OAUTH_2_TOKEN, getToken())
+                        .withTransfer(transfer)
+                        .buildWithLogin(TransferCommand.class);
+
+        agentWireMockPaymentTest.executePayment();
+        Assert.assertTrue(true);
+    }
+
+    private String getToken() {
+        return SerializationUtils.serializeToString(
+                OAuth2Token.create("Bearer", "accessToken", "refreshToken", 900));
+    }
+}
