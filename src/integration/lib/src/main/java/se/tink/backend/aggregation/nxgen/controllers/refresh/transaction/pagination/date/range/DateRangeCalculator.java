6cfc57fc7d3a24c8a8c2ebdaccfd04fb79d2faff
Jakub Kalicki <jakub.kalicki@tink.com>
2021-10-22 12:56:55 +0200

    feat(transaction): Add new methods to date range calculator
    [IFD-3025]

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/date/range/DateRangeCalculator.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/date/range/DateRangeCalculator.java
@@ -6,12 +6,15 @@ import java.time.Period;
 import java.time.ZoneOffset;
 import lombok.RequiredArgsConstructor;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date.LocalDateTimeSource;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.TransactionPaginationHelper;
+import se.tink.backend.aggregation.nxgen.core.account.Account;
 
 @RequiredArgsConstructor
-public class DateRangeCalculator {
+public class DateRangeCalculator<ACCOUNT extends Account> {
 
     private final LocalDateTimeSource timeSource;
     private final ZoneOffset offset;
+    private final TransactionPaginationHelper helper;
 
     public OffsetDateTime calculateTo(OffsetDateTime from) {
         if (from == null) {
@@ -21,14 +24,25 @@ public class DateRangeCalculator {
         return from.minusDays(1).with(LocalTime.MAX);
     }
 
+    public OffsetDateTime calculateFromAsStartOfTheDay(OffsetDateTime to, Period period) {
+        return to.minus(period).with(LocalTime.MIN);
+    }
+
     public OffsetDateTime calculateFromAsStartOfTheDayWithLimit(
             OffsetDateTime to, Period period, OffsetDateTime limit) {
-        OffsetDateTime from = to.minus(period).with(LocalTime.MIN);
+        OffsetDateTime from = calculateFromAsStartOfTheDay(to, period);
 
         if (from.isBefore(limit)) {
             return limit;
         }
 
         return from;
+    }
+
+    public OffsetDateTime applyCertainDateLimit(ACCOUNT account, OffsetDateTime proposed) {
+        return helper.getTransactionDateLimit(account)
+                .map(certainDate -> new java.sql.Date(certainDate.getTime()).toLocalDate())
+                .map(certainLocalDate -> certainLocalDate.atStartOfDay().atOffset(offset))
+                .orElse(proposed);
     }
 }
--- src/integration/lib/src/test/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/date/range/DateRangeCalculatorTest.java
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/nxgen/controllers/refresh/transaction/pagination/date/range/DateRangeCalculatorTest.java
@@ -1,32 +1,44 @@
 package se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.range;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.Period;
 import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date.ConstantLocalDateTimeSource;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date.LocalDateTimeSource;
+import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.TransactionPaginationHelper;
+import se.tink.backend.aggregation.nxgen.core.account.Account;
 
+@RunWith(MockitoJUnitRunner.class)
 public class DateRangeCalculatorTest {
 
-    private DateRangeCalculator calculator;
-    private LocalDateTimeSource localDateTimeSource;
+    @Mock private TransactionPaginationHelper helper;
+    @Mock private Account account;
+
+    private LocalDateTimeSource constantDateTimeSource;
+    private DateRangeCalculator<Account> calculator;
 
     @Before
     public void setUp() throws Exception {
-        localDateTimeSource = new ConstantLocalDateTimeSource();
-        calculator = new DateRangeCalculator(new ConstantLocalDateTimeSource(), ZoneOffset.UTC);
+        constantDateTimeSource = new ConstantLocalDateTimeSource();
+        calculator = new DateRangeCalculator<>(constantDateTimeSource, ZoneOffset.UTC, helper);
     }
 
     @Test
     public void shouldCalculateToDateAsNow() {
         // given
-        OffsetDateTime now = localDateTimeSource.now().atOffset(ZoneOffset.UTC);
+        OffsetDateTime now = constantDateTimeSource.now().atOffset(ZoneOffset.UTC);
 
         // when
         OffsetDateTime toDateTime = calculator.calculateTo(null);
@@ -49,6 +61,21 @@ public class DateRangeCalculatorTest {
                 .isEqualTo(
                         LocalDateTime.of(2021, 1, 14, 23, 59, 59, 999999999)
                                 .atOffset(ZoneOffset.UTC));
+    }
+
+    @Test
+    public void shouldCalculateFromAsStartOfTheDayCorrectly() {
+        // given
+        OffsetDateTime toDateTime =
+                LocalDate.of(2021, 1, 15).atStartOfDay().atOffset(ZoneOffset.UTC);
+        Period range = Period.ofWeeks(1);
+
+        // when
+        OffsetDateTime fromDateTime = calculator.calculateFromAsStartOfTheDay(toDateTime, range);
+
+        // then
+        assertThat(fromDateTime)
+                .isEqualTo(LocalDate.of(2021, 1, 8).atStartOfDay().atOffset(ZoneOffset.UTC));
     }
 
     @Test
@@ -82,5 +109,47 @@ public class DateRangeCalculatorTest {
         // then
         assertThat(fromDateTime)
                 .isEqualTo(LocalDate.of(2021, 1, 8).atStartOfDay().atOffset(ZoneOffset.UTC));
+    }
+
+    @Test
+    public void shouldApplyCertainDateLimit() {
+        // given
+        OffsetDateTime proposedFromDateTime =
+                LocalDate.of(2021, 1, 15).atStartOfDay().atOffset(ZoneOffset.UTC);
+        given(helper.getTransactionDateLimit(account))
+                .willReturn(optionalDate(2021, 1, 20, 13, 0, 0, ZoneOffset.UTC));
+
+        // when
+        OffsetDateTime fromDateTime =
+                calculator.applyCertainDateLimit(account, proposedFromDateTime);
+
+        // then
+        assertThat(fromDateTime)
+                .isEqualTo(LocalDate.of(2021, 1, 20).atStartOfDay().atOffset(ZoneOffset.UTC));
+    }
+
+    @Test
+    public void shouldNotApplyCertainDateLimit() {
+        // given
+        OffsetDateTime proposedFromDateTime =
+                LocalDate.of(2021, 1, 15).atStartOfDay().atOffset(ZoneOffset.UTC);
+        given(helper.getTransactionDateLimit(account)).willReturn(Optional.empty());
+
+        // when
+        OffsetDateTime fromDateTime =
+                calculator.applyCertainDateLimit(account, proposedFromDateTime);
+
+        // then
+        assertThat(fromDateTime).isEqualTo(proposedFromDateTime);
+    }
+
+    private Optional<Date> optionalDate(
+            int year, int month, int day, int hour, int min, int sec, ZoneOffset offset) {
+        return Optional.of(
+                Date.from(
+                        LocalDate.of(year, month, day)
+                                .atTime(hour, min, sec)
+                                .atOffset(offset)
+                                .toInstant()));
     }
 }
