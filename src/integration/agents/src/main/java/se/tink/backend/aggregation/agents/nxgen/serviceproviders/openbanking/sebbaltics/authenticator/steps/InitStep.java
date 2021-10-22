3ebc46488468bae9d6c0ce327500fe2f4ba440b0
Shivam Saini <shivam.Saini@tink.com>
2021-10-22 17:35:27 +0200

    refactor(baltic/seb-ob): update internal error messages
    [TC-4565]: Since all the exceptions are AUTHENTICATION_ERROR.exception(), providing internal error messages will make them more clear for different status responses.

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbaltics/authenticator/steps/InitStep.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbaltics/authenticator/steps/InitStep.java
@@ -98,10 +98,11 @@ public class InitStep implements AuthenticationStep {
                     break;
                 case AuthStatus.FAILED:
                     log.info("Authentication failed");
-                    throw ThirdPartyAppError.AUTHENTICATION_ERROR.exception();
+                    throw ThirdPartyAppError.AUTHENTICATION_ERROR.exception(
+                            "Authentication failed");
                 default:
                     log.warn(String.format("Unknown status (%s)", status));
-                    throw ThirdPartyAppError.AUTHENTICATION_ERROR.exception();
+                    throw ThirdPartyAppError.AUTHENTICATION_ERROR.exception("Unknown status");
             }
 
             Uninterruptibles.sleepUninterruptibly(
