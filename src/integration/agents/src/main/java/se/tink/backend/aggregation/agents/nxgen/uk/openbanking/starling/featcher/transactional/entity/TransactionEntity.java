3c8e09959c53d810a8cd82ffe0b6bb97f7820f0f
Pavlo Yeromenko <pavlo.yeromenko@tink.com>
2021-10-25 17:02:09 +0200

    chore(UK/Starling): add merchantName mapping

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/starling/featcher/transactional/entity/TransactionEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/starling/featcher/transactional/entity/TransactionEntity.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.StarlingConstants.TransactionDirections;
 import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.nxgen.core.transaction.AggregationTransaction.Builder;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 import se.tink.libraries.amount.ExactCurrencyAmount;
 
@@ -19,6 +19,10 @@ public class TransactionEntity {
 
     private static final List<String> pendingStatusList =
             new ArrayList<>(Arrays.asList("pending", "retrying"));
+
+    private static final String OUT_DIRECTION = "OUT";
+
+    private static final String MERCHANT_COUNTER_PARTY_TYPE = "MERCHANT";
 
     private String feedItemUid;
     private String categoryUid;
@@ -40,24 +44,31 @@ public class TransactionEntity {
     private String sourceSubType;
     private String status;
     private String reference;
+    private String counterPartyType;
+    private String counterPartyName;
     private String country;
     private String userNote;
 
     public Transaction toTinkTransaction() {
+
         ExactCurrencyAmount transactionAmount = amount.toExactCurrencyAmount();
 
-        if (direction.equals(TransactionDirections.OUT)) {
+        if (direction.equals(OUT_DIRECTION)) {
             transactionAmount = transactionAmount.negate();
         }
 
-        return (Transaction)
+        Builder builder =
                 Transaction.builder()
                         .setAmount(transactionAmount)
                         .setDate(transactionTime)
                         .setDescription(reference)
                         .setPending(isPending())
-                        .setProprietaryFinancialInstitutionType(source)
-                        .build();
+                        .setProprietaryFinancialInstitutionType(source);
+
+        if (isMerchantCounterPartyType()) {
+            builder.setMerchantName(counterPartyName);
+        }
+        return (Transaction) builder.build();
     }
 
     public String getStatus() {
@@ -72,5 +83,10 @@ public class TransactionEntity {
     @JsonIgnore
     private boolean isPending() {
         return pendingStatusList.contains(getStatus().toLowerCase());
+    }
+
+    @JsonIgnore
+    private boolean isMerchantCounterPartyType() {
+        return MERCHANT_COUNTER_PARTY_TYPE.equals(counterPartyType);
     }
 }
