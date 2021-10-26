4c1c1acd4fdc0d825803a739863ad7b22e93fd8d
Pawel Szalawinski <pawel.szalawinski@tink.com>
2021-10-26 16:51:20 +0200

    chore(OB/SE/Sebbusiness): updated trx history

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/business/seb/fetcher/transactionalaccount/entities/PendingEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/business/seb/fetcher/transactionalaccount/entities/PendingEntity.java
@@ -1,12 +1,17 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.business.seb.fetcher.transactionalaccount.entities;
 
+import static se.tink.backend.aggregation.agents.nxgen.se.openbanking.business.seb.SebConstants.Urls.PROVIDER_MARKET;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
+import se.tink.backend.aggregation.agents.models.TransactionExternalSystemIdType;
 import se.tink.backend.aggregation.annotations.JsonObject;
 import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
 
 @JsonObject
 public class PendingEntity {
+
+    private String transactionId;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date valueDate;
@@ -17,12 +22,20 @@ public class PendingEntity {
 
     private String pendingType;
 
+    private String remittanceInformationStructuredReference;
+
     public Transaction toTinkTransaction() {
-        return Transaction.builder()
-                .setAmount(transactionAmount.getAmount())
-                .setDate(valueDate)
-                .setDescription(creditorName)
-                .setPending(true)
-                .build();
+        return (Transaction)
+                Transaction.builder()
+                        .setAmount(transactionAmount.getAmount())
+                        .setDate(valueDate)
+                        .setDescription(creditorName)
+                        .setPending(true)
+                        .addExternalSystemIds(
+                                TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID,
+                                transactionId)
+                        .setTransactionReference(remittanceInformationStructuredReference)
+                        .setProviderMarket(PROVIDER_MARKET)
+                        .build();
     }
 }
