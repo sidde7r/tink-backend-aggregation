39bda66f82ba229f97d0e66c638993091f2ea9d4
Berk Gedik <berk.gedik@tink.com>
2021-10-27 18:30:34 +0200

    feat(UKOB): skeleton for computing real booked balance at the end

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/SendAccountsToUpdateServiceAgentWorkerCommand.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/SendAccountsToUpdateServiceAgentWorkerCommand.java
@@ -2,8 +2,13 @@ package se.tink.backend.aggregation.workers.commands;
 
 import com.google.common.collect.Lists;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.tink.backend.agents.rpc.AccountBalanceType;
+import se.tink.backend.aggregation.agents.models.Transaction;
 import se.tink.backend.aggregation.workers.commands.metrics.MetricsCommand;
 import se.tink.backend.aggregation.workers.context.AgentWorkerCommandContext;
 import se.tink.backend.aggregation.workers.metrics.AgentWorkerCommandMetricState;
@@ -11,6 +16,8 @@ import se.tink.backend.aggregation.workers.metrics.MetricAction;
 import se.tink.backend.aggregation.workers.operation.AgentWorkerCommand;
 import se.tink.backend.aggregation.workers.operation.AgentWorkerCommandResult;
 import se.tink.backend.aggregation.workers.operation.type.AgentWorkerOperationMetricType;
+import se.tink.libraries.account_data_cache.AccountData;
+import se.tink.libraries.amount.ExactCurrencyAmount;
 import se.tink.libraries.metrics.core.MetricId;
 
 public class SendAccountsToUpdateServiceAgentWorkerCommand extends AgentWorkerCommand
@@ -53,6 +60,36 @@ public class SendAccountsToUpdateServiceAgentWorkerCommand extends AgentWorkerCo
                    skipping is fine)
                 */
                 //
+
+                // TODO (AAP-1566): Example implementation, check it and implement properly
+                for (AccountData account : context.getAccountDataCache().getFilteredAccountData()) {
+                    if (Objects.isNull(account.getAccount().getExactBalance())
+                            && account.getTransactions().size() > 0) {
+                        // TODO (AAP-1566): Also check that we have necessary ISO-balances in
+                        // granular
+                        // balances, otherwise don't execute the computation.
+                        Map<AccountBalanceType, ExactCurrencyAmount> granularBalances =
+                                account.getAccount().getGranularAccountBalances();
+                        log.info(
+                                "[BOOKED BALANCE AUTO COMPUTATION] Computation started for an account");
+                        ExactCurrencyAmount computedBookedBalance =
+                                ExactCurrencyAmount.of(
+                                        account.getAccount().getAvailableBalance().getExactValue(),
+                                        account.getAccount()
+                                                .getAvailableBalance()
+                                                .getCurrencyCode());
+                        List<Transaction> pendingTransactions =
+                                account.getTransactions().stream()
+                                        .filter(Transaction::isPending)
+                                        .collect(Collectors.toList());
+                        for (Transaction transaction : pendingTransactions) {
+                            computedBookedBalance.subtract(transaction.getTransactionAmount());
+                        }
+                        account.getAccount().setExactBalance(computedBookedBalance);
+                        // TODO (AAP-1566): Check if we should also call
+                        // account.getAccount().setBalance(...);
+                    }
+                }
             } catch (Exception e) {
                 log.warn("Couldn't execute the balance computation");
             }
