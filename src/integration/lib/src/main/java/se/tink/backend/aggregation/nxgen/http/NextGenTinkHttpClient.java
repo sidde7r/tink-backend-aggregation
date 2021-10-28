a0f9719c266ed148a9a9f830e412b27d6fe77122
Berk Gedik <berk.gedik@tink.com>
2021-10-28 15:52:05 +0200

    refactor: Avoid NPE

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/NextGenTinkHttpClient.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/NextGenTinkHttpClient.java
@@ -322,13 +322,17 @@ public class NextGenTinkHttpClient extends NextGenFilterable<TinkHttpClient>
         if (Objects.nonNull(this.rawBankDataEventProducer)
                 && Objects.nonNull(rawBankDataEventAccumulator)
                 && Objects.nonNull(correlationId)) {
+            String providerName = "UNKNOWN";
+            if (Objects.nonNull(this.provider) && Objects.nonNull(this.provider.getName())) {
+                providerName = this.provider.getName();
+            }
             this.rawBankDataEventProducerInterceptor =
                     new RawBankDataEventProducerInterceptor(
                             rawBankDataEventProducer,
                             rawBankDataEventAccumulator,
                             refreshableItemInProgressSupplier,
                             correlationId,
-                            Optional.ofNullable(this.provider.getName()).orElse("Unknown"),
+                            providerName,
                             new RandomStickyDecisionMakerRawBankDataEventCreationTriggerStrategy(
                                     RAW_BANK_DATA_EVENT_EMISSION_RATE));
             addFilter(this.rawBankDataEventProducerInterceptor);
