be941a1acce8df15a7cedf200da5c4a60c59f4af
Berk Gedik <berk.gedik@tink.com>
2021-10-28 13:33:30 +0200

    feat(event): detect fields with BBAN value

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/event/type_detection/RawBankDataFieldTypeDetectionStrategy.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/event/type_detection/RawBankDataFieldTypeDetectionStrategy.java
@@ -11,4 +11,16 @@ public interface RawBankDataFieldTypeDetectionStrategy {
 
     RawBankDataTrackerEventBankFieldType getType(
             List<FieldPathPart> fieldPath, String value, JsonNodeType type);
+
+    static boolean isStringANumber(String str) {
+        if (str == null) {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
---
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/event/type_detection/account_identifier_detectors/BbanFieldTypeDetectionStrategy.java
@@ -0,0 +1,28 @@
+package se.tink.backend.aggregation.nxgen.http.event.type_detection.account_identifier_detectors;
+
+import com.fasterxml.jackson.databind.node.JsonNodeType;
+import java.util.List;
+import se.tink.backend.aggregation.nxgen.http.event.event_producers.pojo.FieldPathPart;
+import se.tink.backend.aggregation.nxgen.http.event.type_detection.RawBankDataFieldTypeDetectionStrategy;
+import se.tink.eventproducerservice.events.grpc.RawBankDataTrackerEventProto.RawBankDataTrackerEventBankFieldType;
+
+public class BbanFieldTypeDetectionStrategy implements RawBankDataFieldTypeDetectionStrategy {
+
+    @Override
+    public boolean isTypeMatched(List<FieldPathPart> fieldPath, String value, JsonNodeType type) {
+        boolean isBbanNameAppearsInFieldPath =
+                fieldPath.stream()
+                        .anyMatch(part -> part.getKeyName().trim().equalsIgnoreCase("bban"));
+        boolean isNumeric =
+                JsonNodeType.NUMBER.equals(type)
+                        || (JsonNodeType.STRING.equals(type)
+                                && RawBankDataFieldTypeDetectionStrategy.isStringANumber(value));
+        return isBbanNameAppearsInFieldPath && isNumeric;
+    }
+
+    @Override
+    public RawBankDataTrackerEventBankFieldType getType(
+            List<FieldPathPart> fieldPath, String value, JsonNodeType type) {
+        return RawBankDataTrackerEventBankFieldType.BBAN;
+    }
+}
