cec6fcbe93fc94399f27dec9e1556e04475e61ca
Daniel Obrębski <daniel.obrebski@tink.se>
2021-11-01 13:41:30 +0100

    chore(pl/errors): added handling of httpClientException
    [ITE-3268]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/polishapi/errorhandling/PolishApiErrorHandler.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/polishapi/errorhandling/PolishApiErrorHandler.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.errorhandling;
 
+import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
 import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.configuration.PolishApiConstants.Logs.LOG_TAG;
 import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.errorhandling.PolishApiErrors.DAILY_REQUEST_LIMIT_REACHED;
 import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.errorhandling.PolishApiErrors.INVALID_GRANT;
@@ -24,6 +25,7 @@ import org.apache.http.HttpStatus;
 import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceError;
 import se.tink.backend.aggregation.agents.exceptions.errors.SessionError;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.polishapi.errorhandling.dto.responses.ErrorResponse;
+import se.tink.backend.aggregation.nxgen.http.exceptions.client.HttpClientException;
 import se.tink.backend.aggregation.nxgen.http.filter.filterable.request.RequestBuilder;
 import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
 import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
@@ -41,6 +43,10 @@ public final class PolishApiErrorHandler {
                     HttpStatus.SC_SERVICE_UNAVAILABLE,
                     HttpStatus.SC_BAD_GATEWAY);
 
+    private static final ImmutableList<String> RETRYABLE_BANK_SIDE_ISSUES =
+            ImmutableList.of(
+                    "connection reset", "connect timed out", "read timed out", "failed to respond");
+
     public enum RequestType {
         GET,
         POST
@@ -55,6 +61,8 @@ public final class PolishApiErrorHandler {
             Throwable cause = e.getCause();
             if (cause instanceof HttpResponseException) {
                 handleHttpResponseException((HttpResponseException) cause);
+            } else if (cause instanceof HttpClientException) {
+                handleHttpClientException((HttpClientException) cause);
             } else {
                 throw new RuntimeException(
                         LOG_TAG + " Error handler - Something bad happened when calling bank API",
@@ -80,18 +88,28 @@ public final class PolishApiErrorHandler {
 
     private static <T> Retryer<T> prepareRetryer() {
         return RetryerBuilder.<T>newBuilder()
-                .retryIfException(
-                        e ->
-                                e instanceof HttpResponseException
-                                        && (RETRYABLE_STATUSES.contains(
-                                                ((HttpResponseException) e)
-                                                        .getResponse()
-                                                        .getStatus())))
+                .retryIfException(PolishApiErrorHandler::shouldRetryRequest)
                 .withWaitStrategy(
                         WaitStrategies.exponentialWait(MAX_TIME_WAIT_IN_S, TimeUnit.SECONDS))
                 .withStopStrategy(StopStrategies.stopAfterAttempt(MAX_NUM_OF_ATTEMPTS))
                 .withRetryListener(getListener())
                 .build();
+    }
+
+    private static boolean shouldRetryRequest(Throwable e) {
+        return (e instanceof HttpResponseException
+                        && isRetryableStatusReturnedByBank((HttpResponseException) e))
+                || (e instanceof HttpClientException
+                        && isBankSideHttpClientException((HttpClientException) e));
+    }
+
+    private static boolean isRetryableStatusReturnedByBank(HttpResponseException e) {
+        return RETRYABLE_STATUSES.contains(e.getResponse().getStatus());
+    }
+
+    private static boolean isBankSideHttpClientException(HttpClientException e) {
+        return RETRYABLE_BANK_SIDE_ISSUES.stream()
+                .anyMatch((failure -> containsIgnoreCase(e.getMessage(), failure)));
     }
 
     private static RetryListener getListener() {
@@ -135,7 +153,7 @@ public final class PolishApiErrorHandler {
     }
 
     private static void handleBankSideIssues(HttpResponseException e, ErrorResponse error) {
-        if (RETRYABLE_STATUSES.contains(e.getResponse().getStatus())) {
+        if (isRetryableStatusReturnedByBank(e)) {
             throw BankServiceError.BANK_SIDE_FAILURE.exception(error.getMessage());
         } else if (TOO_MANY_REQUESTS_STATUS == e.getResponse().getStatus()
                 || error.getMessage().contains(DAILY_REQUEST_LIMIT_REACHED)) {
@@ -163,6 +181,13 @@ public final class PolishApiErrorHandler {
     private static void handleNotImplemented(ErrorResponse error) {
         if (error.getMessage().toLowerCase().contains(NOT_IMPLEMENTED)) {
             throw new TransactionTypeNotSupportedException();
+        }
+    }
+
+    private static void handleHttpClientException(HttpClientException e) {
+        log.error("{} Error handler - HttpClientException occurred...", LOG_TAG);
+        if (isBankSideHttpClientException(e)) {
+            throw BankServiceError.BANK_SIDE_FAILURE.exception(e);
         }
     }
 }
