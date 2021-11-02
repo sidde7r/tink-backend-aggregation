2eb15c49f9bdf671183240cc712fd23d7d36e628
Nazri Bin Abdullah <nazri.abdullah@tink.com>
2021-11-02 23:30:45 +0100

    refactor(se-lansforsakringar-ob): added a check for response body type

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/lansforsakringar/filter/ServerFaultFilter.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/lansforsakringar/filter/ServerFaultFilter.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.lansforsakringar.filter;
 
+import javax.ws.rs.core.MediaType;
 import org.apache.http.HttpStatus;
 import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceError;
 import se.tink.backend.aggregation.agents.nxgen.se.openbanking.lansforsakringar.rpc.FaultResponse;
@@ -14,15 +15,25 @@ public class ServerFaultFilter extends Filter {
     @Override
     public HttpResponse handle(HttpRequest httpRequest)
             throws HttpClientException, HttpResponseException {
-        final HttpResponse response = nextFilter(httpRequest);
 
-        if (response.getStatus() == HttpStatus.SC_UNAUTHORIZED && response.hasBody()) {
+        try {
+            return nextFilter(httpRequest);
+
+        } catch (HttpResponseException e) {
+            final HttpResponse response = e.getResponse();
+
+            throwServerFaultError(response);
+            throw e;
+        }
+    }
+
+    private void throwServerFaultError(HttpResponse response) {
+        if (response.getStatus() == HttpStatus.SC_UNAUTHORIZED
+                && MediaType.APPLICATION_JSON_TYPE.isCompatible(response.getType())) {
             final FaultResponse faultResponse = response.getBody(FaultResponse.class);
             if (faultResponse.isServerFault()) {
                 throw BankServiceError.BANK_SIDE_FAILURE.exception();
             }
         }
-
-        return response;
     }
 }
