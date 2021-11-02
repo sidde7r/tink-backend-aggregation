daa7da731d22c2ae0c9e6485457397459704b54a
Berk Gedik <berk.gedik@tink.com>
2021-11-02 11:37:58 +0100

    fix(event): change order of type detectors
    This is because some dates are assumed to be a sort-code with the current type detector ordering

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/event/configuration/RawBankDataEventCreationStrategies.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/http/event/configuration/RawBankDataEventCreationStrategies.java
@@ -48,13 +48,13 @@ public class RawBankDataEventCreationStrategies {
                 .fieldTypeDetectionStrategies(
                         // order is important!
                         Arrays.asList(
+                                new DateFieldTypeDetectionStrategy(),
                                 new BooleanFieldTypeDetectionStrategy(),
                                 new IbanFieldTypeDetectionStrategy(),
                                 new SortCodeFieldTypeDetectionStrategy(),
                                 new BbanFieldTypeDetectionStrategy(),
                                 new IntegerFieldTypeDetectionStrategy(),
                                 new DoubleFieldTypeDetectionStrategy(),
-                                new DateFieldTypeDetectionStrategy(),
                                 new MaskedPanFieldTypeDetectionStrategy(),
                                 new StringFieldTypeDetectionStrategy(),
                                 new DummyFieldTypeDetectionStrategy()))
