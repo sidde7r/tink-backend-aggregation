da7545ca3d0df5ee94a60ca443b7cad91023df15
Elena Kolpakova <elena.kolpakova@tink.com>
2021-11-08 13:17:30 +0100

    feat(Handelsbanken/re): update device-model constant

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/HandelsbankenSEConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/banks/handelsbanken/HandelsbankenSEConstants.java
@@ -67,7 +67,7 @@ public class HandelsbankenSEConstants {
 
     public static final class Headers {
         public static final String APP_VERSION = "3.6:11.4";
-        public static final String DEVICE_MODEL = "IOS-11.4.1,8.5.0,iPhone9.3,SEPRIV";
+        public static final String DEVICE_MODEL = "IOS-14.4.1,11.2.0,iPhone8.1,SEPRIV";
     }
 
     public static class Accounts {
