b543db1938f3713a1aae0b348a006a73a510bca4
Aneta Zelik <aneta.zelik@tink.com>
2021-11-09 13:13:09 +0100

    chore(ConstantLocalDateTimeSource): ConstantLocalDateTimeSource refactor
    [PENG-338]

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/agents/componentproviders/generated/date/ConstantLocalDateTimeSource.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/nxgen/agents/componentproviders/generated/date/ConstantLocalDateTimeSource.java
@@ -1,6 +1,5 @@
 package se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.date;
 
-import java.time.Clock;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -9,17 +8,18 @@ import java.time.ZoneOffset;
 
 public final class ConstantLocalDateTimeSource implements LocalDateTimeSource {
 
-    private static final Instant CONSTANT_INSTANT =
-            LocalDate.of(1992, 4, 10).atStartOfDay(ZoneOffset.UTC).toInstant();
+    private static final LocalDate CONSTANT_DATE = LocalDate.of(1992, 4, 10);
+    private static final Instant CONSTANT_UTC_INSTANT =
+            CONSTANT_DATE.atStartOfDay(ZoneOffset.UTC).toInstant();
 
     @Override
     public LocalDateTime now(ZoneId zoneId) {
-        return LocalDateTime.ofInstant(getInstant(), zoneId);
+        return LocalDateTime.ofInstant(getInstant(zoneId), zoneId);
     }
 
     @Override
     public Instant getInstant(ZoneId zoneId) {
-        return Instant.now(Clock.fixed(CONSTANT_INSTANT, zoneId));
+        return CONSTANT_DATE.atStartOfDay(zoneId).toInstant();
     }
 
     @Override
@@ -29,6 +29,6 @@ public final class ConstantLocalDateTimeSource implements LocalDateTimeSource {
 
     @Override
     public Instant getInstant() {
-        return CONSTANT_INSTANT;
+        return CONSTANT_UTC_INSTANT;
     }
 }
