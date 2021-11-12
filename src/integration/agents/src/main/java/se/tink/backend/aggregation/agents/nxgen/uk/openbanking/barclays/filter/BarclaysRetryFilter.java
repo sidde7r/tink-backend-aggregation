a5f5de8611aef1c7ad2a985444a721cfb3e3a6e3
Berk Gedik <berk.gedik@tink.com>
2021-11-12 09:17:42 +0100

    chore(barclays): update comment

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/barclays/filter/BarclaysRetryFilter.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/barclays/filter/BarclaysRetryFilter.java
@@ -4,8 +4,8 @@ import se.tink.backend.aggregation.nxgen.http.filter.filters.retry.AbstractRetry
 import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
 import se.tink.backend.aggregation.nxgen.http.response.HttpResponseException;
 
-// Currently Barclays is returning a lot of 502 for our BG refreshes, we believe this is due to
-// rate limiting. To test this I had to override isRateLimitResponse method with 502 status.
+// Currently Barclays is returning a lot of 502 for our BG refreshes, we are using
+// BarclaysRetryFilter to overcome this problem.
 // Ticket on UKOB service desk with detail: OBSD-26678
 public class BarclaysRetryFilter extends AbstractRetryFilter {
 
