23832a98e9d74b23a26d10ae46926a3cdb0a0327
Johannes Elgh <johannes.elgh@tink.se>
2021-11-12 14:19:40 +0100

    fix(imaginbank): stop using deprecated 'Amount' class

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/imaginbank/fetcher/entities/BalanceEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/imaginbank/fetcher/entities/BalanceEntity.java
@@ -2,13 +2,21 @@ package se.tink.backend.aggregation.agents.nxgen.es.banks.imaginbank.fetcher.ent
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.libraries.amount.Amount;
+import se.tink.libraries.amount.ExactCurrencyAmount;
 
 @JsonObject
-public class BalanceEntity extends Amount {
+public class BalanceEntity {
+
+    private String currency;
+    private double value;
 
     public BalanceEntity(
             @JsonProperty("moneda") String currency, @JsonProperty("importe") double value) {
-        super(currency, value);
+        this.currency = currency;
+        this.value = value;
+    }
+
+    public ExactCurrencyAmount toExactCurrencyAmount() {
+        return ExactCurrencyAmount.of(value, currency);
     }
 }
