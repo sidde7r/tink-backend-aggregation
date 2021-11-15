3d9077e73a0b81ff32b29061863d9fae5a39fabc
Jonas Renold <jonas.renold@tink.se>
2021-11-15 11:10:14 +0100

    feat(agent/test): local dev provider overrides for testing
    Allow developers to override / specify providers locally in this repo during testing.

--- src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/framework/provider/ProviderConfigurationUtil.java
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/framework/provider/ProviderConfigurationUtil.java
@@ -1,8 +1,11 @@
 package se.tink.backend.aggregation.agents.framework.provider;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import se.tink.backend.agents.rpc.Provider;
 import se.tink.backend.aggregation.configuration.ProviderConfig;
 
 public final class ProviderConfigurationUtil {
@@ -16,7 +19,8 @@ public final class ProviderConfigurationUtil {
         File providersFile = new File(providersFilePath);
         final ObjectMapper mapper = new ObjectMapper();
         try {
-            return mapper.readValue(providersFile, ProviderConfig.class);
+            ProviderConfig providerConfig = mapper.readValue(providersFile, ProviderConfig.class);
+            return applyProviderOverrides(providerConfig);
         } catch (IOException e) {
             throw new IllegalStateException(e);
         }
@@ -24,5 +28,34 @@ public final class ProviderConfigurationUtil {
 
     private static String escapeMarket(String market) {
         return market.replaceAll("[^a-zA-Z]", "");
+    }
+
+    private static ProviderConfig applyProviderOverrides(ProviderConfig providerConfig) {
+        // Overrides file is an array of complete providers
+        File overridesFile = new File("etc/provider-override-development.json");
+        if (!overridesFile.exists()) {
+            return providerConfig;
+        }
+
+        final String market = providerConfig.getMarket();
+        final ObjectMapper mapper = new ObjectMapper();
+        try {
+            List<Provider> providers =
+                    mapper.readValue(overridesFile, new TypeReference<List<Provider>>() {});
+            providers.stream()
+                    .filter(provider -> market.equals(provider.getMarket()))
+                    .forEach(provider -> addProviderOverride(providerConfig, provider));
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
+        return providerConfig;
+    }
+
+    private static void addProviderOverride(ProviderConfig providerConfig, Provider newProvider) {
+        providerConfig
+                .getProviders()
+                .removeIf(oldProvider -> oldProvider.getName().equals(newProvider.getName()));
+        providerConfig.getProviders().add(newProvider);
     }
 }
