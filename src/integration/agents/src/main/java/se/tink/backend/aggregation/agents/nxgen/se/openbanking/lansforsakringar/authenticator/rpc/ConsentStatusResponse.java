be6d5c52954a7826e0718478e01d7bc0c3147f0a
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-11-17 10:53:42 +0100

    fix(se/lf): added check for rejected consent
    - Added a check for when consentstatus is rejected, this is a status that is returned if the user is not customer at the bank
    - Moved valid string to constant class
    TC-2662

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/lansforsakringar/authenticator/rpc/ConsentStatusResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/lansforsakringar/authenticator/rpc/ConsentStatusResponse.java
@@ -1,15 +1,25 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.lansforsakringar.authenticator.rpc;
 
+import static se.tink.backend.aggregation.agents.nxgen.se.openbanking.lansforsakringar.LansforsakringarConstants.ConsentStatus.VALID;
+
+import lombok.Getter;
+import se.tink.backend.aggregation.agents.exceptions.LoginException;
+import se.tink.backend.aggregation.agents.exceptions.errors.LoginError;
+import se.tink.backend.aggregation.agents.nxgen.se.openbanking.lansforsakringar.LansforsakringarConstants.ConsentStatus;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
+@Getter
 public class ConsentStatusResponse {
-
-    private static final String VALID = "valid";
-
     private String consentStatus;
 
     public boolean isConsentValid() {
         return VALID.equalsIgnoreCase(consentStatus);
+    }
+
+    public void checkConsentRejected() throws LoginException {
+        if (consentStatus.equalsIgnoreCase(ConsentStatus.REJECTED)) {
+            throw LoginError.NOT_CUSTOMER.exception();
+        }
     }
 }
