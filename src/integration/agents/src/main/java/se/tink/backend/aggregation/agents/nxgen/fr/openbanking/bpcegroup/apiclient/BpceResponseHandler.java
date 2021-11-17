6af30864f78683839b2b170560a6b83128631a16
Mateusz Pedzich <mateusz.pedzich@tink.se>
2021-11-17 23:51:12 +0100

    fix(fr/bpce): handling html errors
    [IC-688]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/bpcegroup/apiclient/BpceResponseHandler.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/bpcegroup/apiclient/BpceResponseHandler.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bpcegroup.apiclient;
 
+import javax.ws.rs.core.MultivaluedMap;
 import se.tink.backend.aggregation.agents.exceptions.bankservice.BankServiceError;
 import se.tink.backend.aggregation.agents.exceptions.errors.LoginError;
 import se.tink.backend.aggregation.nxgen.http.DefaultResponseStatusHandler;
@@ -9,10 +10,14 @@ import se.tink.backend.aggregation.nxgen.http.response.HttpResponse;
 public class BpceResponseHandler extends DefaultResponseStatusHandler {
 
     private static final String TECHNICAL_ERROR_MSG = "ERREUR Technique";
+    private static final String PAGE_ERROR_HEADER = "Page_Erreur";
+    private static final String OCTET_STREAM_CONTENT_TYPE = "application/octet-stream";
 
     @Override
     public void handleResponse(HttpRequest httpRequest, HttpResponse httpResponse) {
-        if (isBankInternalError(httpResponse) || isBankTechnicalError(httpResponse)) {
+        if (isBankInternalError(httpResponse)
+                || isBankTechnicalError(httpResponse)
+                || isHtmlErrorPage(httpResponse)) {
             throw BankServiceError.BANK_SIDE_FAILURE.exception();
         } else if (isNoAvailableAccountsError(httpResponse)) {
             throw LoginError.NO_ACCOUNTS.exception();
@@ -33,5 +38,12 @@ public class BpceResponseHandler extends DefaultResponseStatusHandler {
     private boolean isBankTechnicalError(HttpResponse response) {
         return response.getStatus() == 503
                 && response.getBody(String.class).equals(TECHNICAL_ERROR_MSG);
+    }
+
+    private boolean isHtmlErrorPage(HttpResponse response) {
+        MultivaluedMap<String, String> headers = response.getHeaders();
+        String contentTypeValue = headers.getFirst("Content-Type");
+        return headers.containsKey(PAGE_ERROR_HEADER)
+                && OCTET_STREAM_CONTENT_TYPE.equals(contentTypeValue);
     }
 }
