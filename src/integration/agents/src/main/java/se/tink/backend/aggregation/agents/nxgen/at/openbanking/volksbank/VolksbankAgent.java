f02bd03125c60ff383251ddd5f234413f41f936c
Johannes Elgh <johannes.elgh@tink.se>
2021-11-18 17:04:16 +0100

    refactor(at/volksbank-ob): convert to inject ctor

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/volksbank/VolksbankAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/at/openbanking/volksbank/VolksbankAgent.java
@@ -1,18 +1,18 @@
 package se.tink.backend.aggregation.agents.nxgen.at.openbanking.volksbank;
 
+import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.FetchAccountsResponse;
 import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
 import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
 import se.tink.backend.aggregation.agents.RefreshSavingsAccountsExecutor;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
-import se.tink.backend.aggregation.agents.contexts.agent.AgentContext;
 import se.tink.backend.aggregation.agents.nxgen.at.openbanking.volksbank.authenticator.VolksbankAuthenticationController;
 import se.tink.backend.aggregation.agents.nxgen.at.openbanking.volksbank.authenticator.VolksbankAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.at.openbanking.volksbank.configuration.VolksbankConfiguration;
 import se.tink.backend.aggregation.agents.nxgen.at.openbanking.volksbank.fetcher.transactionalaccount.VolksbankTransactionalAccountFetcher;
 import se.tink.backend.aggregation.agents.nxgen.at.openbanking.volksbank.fetcher.transactionalaccount.VolksbankTransactionalAccountTransactionFetcher;
-import se.tink.backend.aggregation.configuration.signaturekeypair.SignatureKeyPair;
 import se.tink.backend.aggregation.nxgen.agents.NextGenerationAgent;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.ThirdPartyAppAuthenticationController;
@@ -20,7 +20,6 @@ import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.Transac
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.TransactionDatePaginationController;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
-import se.tink.libraries.credentials.service.CredentialsRequest;
 
 @AgentCapabilities(generateFromImplementedExecutors = true)
 public final class VolksbankAgent extends NextGenerationAgent
@@ -30,9 +29,9 @@ public final class VolksbankAgent extends NextGenerationAgent
     private final VolksbankApiClient apiClient;
     private final TransactionalAccountRefreshController transactionalAccountRefreshController;
 
-    public VolksbankAgent(
-            CredentialsRequest request, AgentContext context, SignatureKeyPair signatureKeyPair) {
-        super(request, context, signatureKeyPair);
+    @Inject
+    public VolksbankAgent(AgentComponentProvider componentProvider) {
+        super(componentProvider);
 
         apiClient = new VolksbankApiClient(client, persistentStorage, sessionStorage, credentials);
         clientName = request.getProvider().getPayload();
