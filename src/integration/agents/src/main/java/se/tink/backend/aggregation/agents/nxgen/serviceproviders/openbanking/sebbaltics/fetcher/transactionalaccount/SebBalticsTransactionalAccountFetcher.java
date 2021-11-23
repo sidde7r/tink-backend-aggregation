9bbba71a2d623c277b7aae24219390033b4cf1ae
Andreas Olausson <andreas.olausson@tink.se>
2021-11-23 16:36:25 +0100

    refactor(SEB/Baltics): use AllArgsConstructor

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbaltics/fetcher/transactionalaccount/SebBalticsTransactionalAccountFetcher.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sebbaltics/fetcher/transactionalaccount/SebBalticsTransactionalAccountFetcher.java
@@ -1,17 +1,15 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbaltics.fetcher.transactionalaccount;
 
 import java.util.Collection;
+import lombok.AllArgsConstructor;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.sebbaltics.SebBalticsApiClient;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.AccountFetcher;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 
+@AllArgsConstructor
 public class SebBalticsTransactionalAccountFetcher implements AccountFetcher<TransactionalAccount> {
 
     private final SebBalticsApiClient apiClient;
-
-    public SebBalticsTransactionalAccountFetcher(SebBalticsApiClient apiClient) {
-        this.apiClient = apiClient;
-    }
 
     @Override
     public Collection<TransactionalAccount> fetchAccounts() {
