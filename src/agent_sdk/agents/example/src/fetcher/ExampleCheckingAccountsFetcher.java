d18af1b043ffc649cb582ba80f21dd1c40703336
Jonas Renold <jonas.renold@tink.se>
2021-11-30 13:32:35 +0100

    fix(agent_sdk): update example/playground agent

--- src/agent_sdk/agents/example/src/fetcher/ExampleCheckingAccountsFetcher.java
+++ src/agent_sdk/agents/example/src/fetcher/ExampleCheckingAccountsFetcher.java
@@ -2,53 +2,12 @@ package se.tink.agent.agents.example.fetcher;
 
 import java.util.List;
 import se.tink.agent.sdk.fetching.accounts.CheckingAccountsFetcher;
-import se.tink.agent.sdk.models.account.CheckingAccount;
-import se.tink.agent.sdk.models.account.CreditCardAccount;
-import se.tink.libraries.account.AccountIdentifier;
-import se.tink.libraries.account.enums.AccountIdentifierType;
+import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 
 public class ExampleCheckingAccountsFetcher implements CheckingAccountsFetcher {
 
     @Override
-    public List<CheckingAccount> fetchAccounts() {
-
-        CheckingAccount.builder()
-                .bankReference("test")
-                .identifier(
-                        AccountIdentifier.create(
-                                AccountIdentifierType.IBAN, "SE12312421421", "Sparkonto"))
-                .name(null)
-                .unknownCapabilities()
-                .noHolderInformation()
-                .balance(null)
-                .noCredit()
-                .noInterestRate()
-                .build();
-
-        CheckingAccount testAcc =
-                CheckingAccount.builder()
-                        .bankReference("someIdentifier")
-                        .identifier(null)
-                        .name("hej")
-                        .unknownCapabilities()
-                        .noHolderInformation()
-                        .balance(null)
-                        .noCredit()
-                        .noInterestRate()
-                        .build();
-
-        CreditCardAccount account =
-                CreditCardAccount.builder()
-                        .bankReference("test")
-                        .cardNumber("HELLU")
-                        .name("VISA")
-                        .unknownCapabilities()
-                        .holder(null)
-                        .balance(null)
-                        .credit(null)
-                        .noInterestRate()
-                        .build();
-
+    public List<TransactionalAccount> fetchAccounts() {
         return null;
     }
 }
