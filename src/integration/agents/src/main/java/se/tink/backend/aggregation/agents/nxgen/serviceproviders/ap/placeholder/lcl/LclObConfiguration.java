ea26247bb99eed7d9066f96df10e05a2834496ce
Piotr Cebo <piotr.cebo@tink.com>
2021-11-30 08:14:48 +0100

    chore(LCL): lcl placeholder agent

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/ap/placeholder/lcl/LclObConfiguration.java
@@ -0,0 +1,37 @@
+package se.tink.backend.aggregation.agents.nxgen.serviceproviders.ap.placeholder.lcl;
+
+import static com.google.common.base.Strings.*;
+
+import com.google.common.base.Preconditions;
+import se.tink.backend.aggregation.annotations.JsonObject;
+import se.tink.backend.aggregation.annotations.Secret;
+
+/**
+ * This is a configuration class to make other processes able to discover the secrets configuration
+ * and to be compatible. E.g.: processes like upload secrets require this kind of class to work
+ * properly. This Configuration class belongs to a dummy agent.
+ */
+@JsonObject
+public class LclObConfiguration {
+
+    private static final String INVALID_CONFIGURATION_MESSAGE =
+            "Invalid Configuration: %s cannot be empty or null";
+
+    @Secret private String clientId;
+    @Secret private String qsealcKeyId;
+
+    public String getClientId() {
+        Preconditions.checkNotNull(
+                emptyToNull(clientId), String.format(INVALID_CONFIGURATION_MESSAGE, "Client ID"));
+
+        return clientId;
+    }
+
+    public String getClientSecret() {
+        Preconditions.checkNotNull(
+                emptyToNull(qsealcKeyId),
+                String.format(INVALID_CONFIGURATION_MESSAGE, "QSealc key id"));
+
+        return qsealcKeyId;
+    }
+}
