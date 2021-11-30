ec6b147a9d2400916281c75ee2f7804baf827d84
Annica Alstermark <annica.alstermark@tink.se>
2021-11-30 17:17:37 +0100

    fix(agent_sdk): update identity data fetcher
    Use the integration/identity_data model instead of previous internal model (deleted in same commit).

--- src/agent_sdk/agents/example/src/fetcher/ExampleIdentityDataFetcher.java
+++ src/agent_sdk/agents/example/src/fetcher/ExampleIdentityDataFetcher.java
@@ -1,7 +1,7 @@
 package se.tink.agent.agents.example.fetcher;
 
 import se.tink.agent.sdk.fetching.identity_data.IdentityDataFetcher;
-import se.tink.agent.sdk.models.identity_data.IdentityData;
+import se.tink.libraries.identitydata.IdentityData;
 
 public class ExampleIdentityDataFetcher implements IdentityDataFetcher {
     @Override
