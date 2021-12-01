63ea5299bf13bb9885b233177e868ef0ceee86ad
Johannes Elgh <johannes.elgh@tink.se>
2021-12-01 15:45:54 +0100

    refactor(transfer): removed unused interface

--- src/integration/transfer/src/main/java/se/tink/libraries/transfer/iface/UuidIdentifiable.java
+++
@@ -1,9 +0,0 @@
-package se.tink.libraries.transfer.iface;
-
-import java.util.UUID;
-
-public interface UuidIdentifiable {
-
-    /** @return A unique UUID that is considered the ID of the instance */
-    UUID getId();
-}
