aed62586c1cce9720abb5eed2ec1bdf88d2b105c
Jonas Renold <jonas.renold@tink.se>
2021-12-01 14:29:49 +0100

    fix(agent_sdk): use Field instead of placeholder
    `String` was used as a placeholder before Field was available.

--- src/agent_sdk/sdk/src/environment/MultifactorAuthenticationState.java
+++ src/agent_sdk/sdk/src/environment/MultifactorAuthenticationState.java
@@ -3,11 +3,12 @@ package se.tink.agent.sdk.environment;
 import com.google.common.collect.ImmutableList;
 import se.tink.agent.sdk.user_interaction.ThirdPartyAppInfo;
 import se.tink.agent.sdk.user_interaction.UserInteraction;
+import se.tink.backend.agents.rpc.Field;
 
 public interface MultifactorAuthenticationState {
     String getState();
 
     UserInteraction<ThirdPartyAppInfo> intoUserInteraction(ThirdPartyAppInfo appInfo);
-    // TODO: fields type instead of String.
-    UserInteraction<ImmutableList<String>> intoUserInteraction(ImmutableList<String> fields);
+
+    UserInteraction<ImmutableList<Field>> intoUserInteraction(ImmutableList<Field> fields);
 }
