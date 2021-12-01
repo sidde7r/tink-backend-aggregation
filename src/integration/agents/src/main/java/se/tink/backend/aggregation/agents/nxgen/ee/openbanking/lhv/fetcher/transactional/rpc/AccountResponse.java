8a7902e46aa92d1f184b3bf34811b7e52c9a56e0
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-12-01 16:15:49 +0100

    fix(ee/LHV): added jsonobject annotations
    TC-5537

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/ee/openbanking/lhv/fetcher/transactional/rpc/AccountResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/ee/openbanking/lhv/fetcher/transactional/rpc/AccountResponse.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.Getter;
 import se.tink.backend.aggregation.agents.nxgen.ee.openbanking.lhv.fetcher.transactional.entities.AccountEntity;
+import se.tink.backend.aggregation.annotations.JsonObject;
 
 @Getter
+@JsonObject
 public class AccountResponse {
     @JsonProperty("accounts")
     private List<AccountEntity> accountList;
