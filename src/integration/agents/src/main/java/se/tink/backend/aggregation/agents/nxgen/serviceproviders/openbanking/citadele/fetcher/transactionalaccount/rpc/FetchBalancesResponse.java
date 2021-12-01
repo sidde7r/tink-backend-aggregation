deb6090d7538cf9bb5776127f7e887bf353bee93
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-12-01 10:41:44 +0100

    fix(baltics/citadele): use correct class
    TC-5538

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/citadele/fetcher/transactionalaccount/rpc/FetchBalancesResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/citadele/fetcher/transactionalaccount/rpc/FetchBalancesResponse.java
@@ -2,12 +2,11 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ci
 
 import java.util.List;
 import lombok.Getter;
-import se.tink.backend.aggregation.agents.utils.berlingroup.BalanceEntity;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.citadele.fetcher.transactionalaccount.entities.account.BalanceEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
 @Getter
 public class FetchBalancesResponse {
-
     private List<BalanceEntity> balances;
 }
