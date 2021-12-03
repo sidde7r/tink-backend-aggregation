a69834558031d9af6173163de0f725977afb9522
Jonas Renold <jonas.renold@tink.se>
2021-12-03 11:40:44 +0100

    refactor(agent_sdk): reintroduce environment
    Move the classes that are exposed to the agent's ctor to a dedicated package (environment).

--- src/agent_sdk/sdk/src/environment/Operation.java
+++ src/agent_sdk/sdk/src/environment/Operation.java
@@ -1,5 +1,7 @@
-package se.tink.agent.sdk.operation;
+package se.tink.agent.sdk.environment;
 
+import se.tink.agent.sdk.operation.Provider;
+import se.tink.agent.sdk.operation.StaticBankCredentials;
 import se.tink.agent.sdk.storage.Storage;
 import se.tink.backend.aggregation.nxgen.controllers.configuration.iface.AgentConfigurationControllerable;
 
