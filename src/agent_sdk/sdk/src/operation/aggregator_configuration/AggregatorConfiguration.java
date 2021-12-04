b921dc1ce48075bff81dc6130420c7c71e1e14b2
Jonas Renold <jonas.renold@tink.se>
2021-12-04 10:03:08 +0100

    fix(agent_sdk): fixup aggregator_config
    Add build file and cleanup interface.

--- src/agent_sdk/sdk/src/operation/aggregator_configuration/AggregatorConfiguration.java
+++ src/agent_sdk/sdk/src/operation/aggregator_configuration/AggregatorConfiguration.java
@@ -1,24 +1,18 @@
 package se.tink.agent.sdk.operation.aggregator_configuration;
 
-import java.util.Optional;
-
 public interface AggregatorConfiguration {
     String getQwaCertificate();
 
     String getQsealCertificate();
 
-    // TODO: should these return `Optional<T>` or `T` + blow up if not set? (the second option is
-    // how it works today)
-    Optional<String> getRedirectUrl();
+    String getRedirectUrl();
 
-    Optional<String> getClientId();
+    String getClientId();
 
-    Optional<String> getClientSecret();
+    String getClientSecret();
 
-    Optional<String> getKeyId();
+    String getKeyId();
 
-    // TODO: should return `Optional<T>` or `T` + blow up if not set? (the second option is how it
-    // works today)
     <T extends NonStandardAggregatorConfiguration> T getNonStandardConfiguration(
             final Class<T> configClass);
 }
