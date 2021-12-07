9b18b3b9af4ccf9c2377c8f99566f3f1b06de08d
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-12-07 16:47:25 +0100

    chore(se/lf): added getter annotation
    TC-5155

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/lansforsakringar/authenticator/rpc/ConsentResponse.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/se/openbanking/lansforsakringar/authenticator/rpc/ConsentResponse.java
@@ -1,40 +1,18 @@
 package se.tink.backend.aggregation.agents.nxgen.se.openbanking.lansforsakringar.authenticator.rpc;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 import se.tink.backend.aggregation.agents.nxgen.se.openbanking.lansforsakringar.authenticator.entities.LinksEntity;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
+@Getter
 public class ConsentResponse {
-
     private String scaStatus;
-
     private String authorisationId;
-
     private String consentId;
+    private String authorisationCode;
 
     @JsonProperty("_links")
     private LinksEntity links;
-
-    private String consentStatus;
-
-    public String getScaStatus() {
-        return scaStatus;
-    }
-
-    public String getAuthorisationId() {
-        return authorisationId;
-    }
-
-    public String getConsentId() {
-        return consentId;
-    }
-
-    public LinksEntity getLinks() {
-        return links;
-    }
-
-    public String getConsentStatus() {
-        return consentStatus;
-    }
 }
