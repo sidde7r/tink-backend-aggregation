6a395dbd09f3aeb975ab8fe47897309a381c32c7
Jonas Renold <jonas.renold@tink.se>
2021-12-08 09:51:01 +0100

    feat(agent_sdk): add user information to operation

--- src/agent_sdk/sdk/src/environment/Operation.java
+++ src/agent_sdk/sdk/src/environment/Operation.java
@@ -2,12 +2,15 @@ package se.tink.agent.sdk.environment;
 
 import se.tink.agent.sdk.operation.Provider;
 import se.tink.agent.sdk.operation.StaticBankCredentials;
+import se.tink.agent.sdk.operation.User;
 import se.tink.agent.sdk.operation.aggregator_configuration.AggregatorConfiguration;
 import se.tink.agent.sdk.storage.Storage;
 import se.tink.backend.aggregation.configuration.eidas.proxy.EidasProxyConfiguration;
 
 public interface Operation {
     Storage getAgentStorage();
+
+    User getUser();
 
     Provider getProvider();
 
