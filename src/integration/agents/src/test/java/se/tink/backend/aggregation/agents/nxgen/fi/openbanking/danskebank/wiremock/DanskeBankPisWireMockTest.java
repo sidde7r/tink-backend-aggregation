0a742d65d2c9281616c33aafb7d48d6265332dc3
Pawel Szalawinski <pawel.szalawinski@tink.com>
2021-12-08 15:29:42 +0100

    refactor(FI/DanskeBank): refactored wiremock

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/danskebank/wiremock/DanskeBankPisWireMockTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/danskebank/wiremock/DanskeBankPisWireMockTest.java
@@ -20,51 +20,51 @@ import se.tink.libraries.transfer.rpc.RemittanceInformation;
 
 public class DanskeBankPisWireMockTest {
 
-  @Test
-  public void testStandardPayment() throws Exception {
+    @Test
+    public void testStandardPayment() throws Exception {
 
-    //given
-    final String configurationPath =
-        "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/danskebank/wiremock/resources/configuration.yml";
-    final String wireMockFilePath =
-        "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/danskebank/wiremock/resources/domestic_payment.aap";
+        // given
+        final String configurationPath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/danskebank/wiremock/resources/configuration.yml";
+        final String wireMockFilePath =
+                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/danskebank/wiremock/resources/domestic_payment.aap";
 
-    final AgentsServiceConfiguration configuration =
-        AgentsServiceConfigurationReader.read(configurationPath);
+        final AgentsServiceConfiguration configuration =
+                AgentsServiceConfigurationReader.read(configurationPath);
 
-    final AgentWireMockPaymentTest agentWireMockPaymentTest =
-        AgentWireMockPaymentTest.builder(FI, "fi-danskebank-ob", wireMockFilePath)
-            .withConfigurationFile(configuration)
-            .withPayment(createDomesticPayment())
-            .withHttpDebugTrace()
-            .addCallbackData("code", "dummyCode")
-            .withAgentModule(new DanskeBankWireMockTestModule())
-            .buildWithoutLogin(PaymentCommand.class);
+        final AgentWireMockPaymentTest agentWireMockPaymentTest =
+                AgentWireMockPaymentTest.builder(FI, "fi-danskebank-ob", wireMockFilePath)
+                        .withConfigurationFile(configuration)
+                        .withPayment(createDomesticPayment())
+                        .withHttpDebugTrace()
+                        .addCallbackData("code", "dummyCode")
+                        .withAgentModule(new DanskeBankWireMockTestModule())
+                        .buildWithoutLogin(PaymentCommand.class);
 
-    agentWireMockPaymentTest.executePayment();
-  }
+        agentWireMockPaymentTest.executePayment();
+    }
 
-  private Payment createDomesticPayment() {
-    RemittanceInformation remittanceInformation = new RemittanceInformation();
-    remittanceInformation.setValue("Tink Test-2021-12-06--13-46-51");
-    remittanceInformation.setType(RemittanceInformationType.UNSTRUCTURED);
-    //ibans are mocked
-    AccountIdentifier creditorAccountIdentifier = new IbanIdentifier("FI3126598515147343");
-    Creditor creditor = new Creditor(creditorAccountIdentifier, "Tink Test");
-    AccountIdentifier debtorAccountIdentifier = new IbanIdentifier("FI9189512712449566");
-    Debtor debtor = new Debtor(debtorAccountIdentifier);
+    private Payment createDomesticPayment() {
+        RemittanceInformation remittanceInformation = new RemittanceInformation();
+        remittanceInformation.setValue("Tink Test-2021-12-06--13-46-51");
+        remittanceInformation.setType(RemittanceInformationType.UNSTRUCTURED);
+        // ibans are mocked
+        AccountIdentifier creditorAccountIdentifier = new IbanIdentifier("FI3126598515147343");
+        Creditor creditor = new Creditor(creditorAccountIdentifier, "Tink Test");
+        AccountIdentifier debtorAccountIdentifier = new IbanIdentifier("FI9189512712449566");
+        Debtor debtor = new Debtor(debtorAccountIdentifier);
 
-    ExactCurrencyAmount amount = ExactCurrencyAmount.inEUR(1.00);
-    String currency = "EUR";
+        ExactCurrencyAmount amount = ExactCurrencyAmount.inEUR(1.00);
+        String currency = "EUR";
 
-    return new Payment.Builder()
-        .withUniqueId("258b967552ae47579f183b57514f1413")
-        .withCreditor(creditor)
-        .withDebtor(debtor)
-        .withExactCurrencyAmount(amount)
-        .withCurrency(currency)
-        .withRemittanceInformation(remittanceInformation)
-        .withPaymentScheme(PaymentScheme.SEPA_CREDIT_TRANSFER)
-        .build();
-  }
+        return new Payment.Builder()
+                .withUniqueId("258b967552ae47579f183b57514f1413")
+                .withCreditor(creditor)
+                .withDebtor(debtor)
+                .withExactCurrencyAmount(amount)
+                .withCurrency(currency)
+                .withRemittanceInformation(remittanceInformation)
+                .withPaymentScheme(PaymentScheme.SEPA_CREDIT_TRANSFER)
+                .build();
+    }
 }
