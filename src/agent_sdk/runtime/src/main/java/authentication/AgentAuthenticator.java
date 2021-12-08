a5a99796677c1e4d879bf43515b8ec0f3d42ef4c
Jonas Renold <jonas.renold@tink.se>
2021-12-08 11:15:33 +0100

    feat(agent_sdk): add runtime support for oauth2swedish decoupled

--- src/agent_sdk/runtime/src/main/java/authentication/AgentAuthenticator.java
+++ src/agent_sdk/runtime/src/main/java/authentication/AgentAuthenticator.java
@@ -5,6 +5,7 @@ import se.tink.agent.runtime.authentication.processes.AuthenticationProcess;
 import se.tink.agent.runtime.authentication.processes.generic.GenericAuthenticationProcess;
 import se.tink.agent.runtime.authentication.processes.oauth2.Oauth2AuthenticationProcess;
 import se.tink.agent.runtime.authentication.processes.oauth2_decoupled_app.Oauth2DecoupledAppAuthenticationProcess;
+import se.tink.agent.runtime.authentication.processes.oauth2_decoupled_swedish_mobile_bankid.Oauth2DecoupledSwedishMobileBankIdAuthenticationProcess;
 import se.tink.agent.runtime.authentication.processes.swedish_mobile_bankid.SwedishMobileBankIdAuthenticationProcess;
 import se.tink.agent.runtime.authentication.processes.thirdparty_app.ThirdPartyAppAuthenticationProcess;
 import se.tink.agent.runtime.authentication.processes.username_password.UsernameAndPasswordAuthenticationProcess;
@@ -30,7 +31,8 @@ public class AgentAuthenticator {
                         new Oauth2DecoupledAppAuthenticationProcess(),
                         new ThirdPartyAppAuthenticationProcess(),
                         new SwedishMobileBankIdAuthenticationProcess(),
-                        new UsernameAndPasswordAuthenticationProcess());
+                        new UsernameAndPasswordAuthenticationProcess(),
+                        new Oauth2DecoupledSwedishMobileBankIdAuthenticationProcess());
     }
 
     public void authenticate(String request) {}
---
+++ src/agent_sdk/runtime/src/main/java/authentication/processes/oauth2_decoupled_swedish_mobile_bankid/Oauth2DecoupledSwedishMobileBankIdAuthenticationProcess.java
@@ -0,0 +1,59 @@
+package se.tink.agent.runtime.authentication.processes.oauth2_decoupled_swedish_mobile_bankid;
+
+import java.util.Optional;
+import se.tink.agent.runtime.authentication.processes.AuthenticationProcess;
+import se.tink.agent.runtime.instance.AgentInstance;
+import se.tink.agent.sdk.authentication.authenticators.generic.AuthenticationFlow;
+import se.tink.agent.sdk.authentication.authenticators.oauth2.steps.Oauth2ValidateOrRefreshAccessTokenStep;
+import se.tink.agent.sdk.authentication.authenticators.oauth2_decoupled_app.steps.Oauth2FetchAccessToken;
+import se.tink.agent.sdk.authentication.authenticators.oauth2_decoupled_swedish_mobile_bankid.Oauth2DecoupledSwedishMobileBankIdAuthenticator;
+import se.tink.agent.sdk.authentication.authenticators.swedish_mobile_bankid.steps.SwedishMobileBankIdOpenAppStep;
+import se.tink.agent.sdk.authentication.authenticators.thirdparty_app.steps.ThirdPartyAppInitStep;
+import se.tink.agent.sdk.authentication.authenticators.thirdparty_app.steps.ThirdPartyAppPollStep;
+import se.tink.agent.sdk.authentication.common_steps.VerifyBankConnectionStep;
+import se.tink.agent.sdk.authentication.existing_consent.ExistingConsentStep;
+import se.tink.agent.sdk.authentication.features.AuthenticateOauth2DecoupledSwedishMobileBankId;
+import se.tink.agent.sdk.authentication.new_consent.NewConsentStep;
+import se.tink.agent.sdk.utils.Sleeper;
+
+public class Oauth2DecoupledSwedishMobileBankIdAuthenticationProcess
+        implements AuthenticationProcess<Oauth2DecoupledSwedishMobileBankIdAuthenticator> {
+    @Override
+    public Optional<Oauth2DecoupledSwedishMobileBankIdAuthenticator> instantiateAuthenticator(
+            AgentInstance agentInstance) {
+        return agentInstance
+                .instanceOf(AuthenticateOauth2DecoupledSwedishMobileBankId.class)
+                .map(AuthenticateOauth2DecoupledSwedishMobileBankId::authenticator);
+    }
+
+    @Override
+    public AuthenticationFlow<NewConsentStep> getNewConsentFlow(
+            Oauth2DecoupledSwedishMobileBankIdAuthenticator authenticator) {
+        Sleeper sleeper = null;
+
+        return AuthenticationFlow.builder(
+                        new ThirdPartyAppInitStep(
+                                authenticator, SwedishMobileBankIdOpenAppStep.class))
+                .addStep(
+                        new SwedishMobileBankIdOpenAppStep(
+                                authenticator, ThirdPartyAppPollStep.class))
+                .addStep(
+                        new ThirdPartyAppPollStep(
+                                authenticator,
+                                sleeper,
+                                SwedishMobileBankIdOpenAppStep.class,
+                                Oauth2FetchAccessToken.class))
+                .addStep(new Oauth2FetchAccessToken(authenticator))
+                .build();
+    }
+
+    @Override
+    public AuthenticationFlow<ExistingConsentStep> getUseExistingConsentFlow(
+            Oauth2DecoupledSwedishMobileBankIdAuthenticator authenticator) {
+        return AuthenticationFlow.builder(
+                        new Oauth2ValidateOrRefreshAccessTokenStep(
+                                authenticator, VerifyBankConnectionStep.class))
+                .addStep(new VerifyBankConnectionStep(authenticator))
+                .build();
+    }
+}
