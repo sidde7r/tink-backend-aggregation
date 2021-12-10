4ad3934cb32cccf7d9f8b959a0c8f74055756939
Rebecca Kullenius <rebecca.kullenius@tink.com>
2021-12-10 13:40:32 +0100

    test(ee/lhv): updated unit test
    - Bank app is using creditorname/debtorname as description , so my chnage is mimicing the bank app so that the user reconizes herself. Sometimes creditorname debtorname is null then we should fetch remittanceunstructured
    
    - Updated unit test to test new description logic
    
    TC-5537

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/ee/openbanking/lhv/fetcher/entities/BookedTransactionEntityTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/ee/openbanking/lhv/fetcher/entities/BookedTransactionEntityTest.java
@@ -11,16 +11,16 @@ import se.tink.libraries.serialization.utils.SerializationUtils;
 public class BookedTransactionEntityTest {
 
     @Test
-    public void shouldMapTransactionsCorrectly() {
+    public void shouldMapDebitTransactionsCorrectly() {
         BookedTransactionEntity bookedTransactionEntity =
                 SerializationUtils.deserializeFromString(
-                        getTransactions(), BookedTransactionEntity.class);
+                        getDebitTransaction(), BookedTransactionEntity.class);
 
         Transaction result = bookedTransactionEntity.toTinkTransaction();
 
         Assert.assertFalse(result.isPending());
         Assert.assertEquals("202106281378178164-302887546", result.getTransactionReference());
-        Assert.assertEquals("Payment", result.getDescription());
+        Assert.assertEquals("Jane Doe", result.getDescription());
         Assert.assertEquals(BigDecimal.valueOf(-75.55), result.getAmount().getExactValue());
         Assert.assertEquals("EUR", result.getAmount().getCurrencyCode());
         Assert.assertEquals(
@@ -30,7 +30,47 @@ public class BookedTransactionEntityTest {
         Assert.assertEquals("Tue Jan 28 11:00:00 UTC 1992", result.getDate().toString());
     }
 
-    private String getTransactions() {
+    @Test
+    public void shouldMapCreditTransactionsCorrectly() {
+        BookedTransactionEntity bookedTransactionEntity =
+                SerializationUtils.deserializeFromString(
+                        getCreditTransaction(), BookedTransactionEntity.class);
+
+        Transaction result = bookedTransactionEntity.toTinkTransaction();
+
+        Assert.assertFalse(result.isPending());
+        Assert.assertEquals("202106281378178164-302887546", result.getTransactionReference());
+        Assert.assertEquals("Esbjorn", result.getDescription());
+        Assert.assertEquals(BigDecimal.valueOf(5005.55), result.getAmount().getExactValue());
+        Assert.assertEquals("EUR", result.getAmount().getCurrencyCode());
+        Assert.assertEquals(
+                "202106281378178164-302887546",
+                result.getExternalSystemIds()
+                        .get(TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID));
+        Assert.assertEquals("Tue Jan 28 11:00:00 UTC 1992", result.getDate().toString());
+    }
+
+    @Test
+    public void shouldMapCreditTransactionsCorrectlyWhenDebtorNameIsNull() {
+        BookedTransactionEntity bookedTransactionEntity =
+                SerializationUtils.deserializeFromString(
+                        getCreditorNameNullTransaction(), BookedTransactionEntity.class);
+
+        Transaction result = bookedTransactionEntity.toTinkTransaction();
+
+        Assert.assertFalse(result.isPending());
+        Assert.assertEquals("202106281378178164-302887546", result.getTransactionReference());
+        Assert.assertEquals("Swish", result.getDescription());
+        Assert.assertEquals(BigDecimal.valueOf(5005.55), result.getAmount().getExactValue());
+        Assert.assertEquals("EUR", result.getAmount().getCurrencyCode());
+        Assert.assertEquals(
+                "202106281378178164-302887546",
+                result.getExternalSystemIds()
+                        .get(TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID));
+        Assert.assertEquals("Tue Jan 28 11:00:00 UTC 1992", result.getDate().toString());
+    }
+
+    private String getDebitTransaction() {
         return "{\"entryReference\":\"202106281378178164-302887546\","
                 + "\"bookingDate\":\"1992-01-28\","
                 + "\"transactionAmount\":"
@@ -42,33 +82,37 @@ public class BookedTransactionEntityTest {
                 + "\"debtorName\":\"Esbjorn\","
                 + "\"debtorAccount\":"
                 + "{\"iban\":\"EE787700771005998888\"},"
-                + "\"remittanceInformationUnstructured\":\"Payment\"},"
-                + "{\"entryReference\":\"202106301378679415-303289656\","
-                + "\"bookingDate\":\"1992-01-30\","
+                + "\"remittanceInformationUnstructured\":\"Payment\"}"
+                + "}";
+    }
+
+    private String getCreditTransaction() {
+        return "{\"entryReference\":\"202106281378178164-302887546\","
+                + "\"bookingDate\":\"1992-01-28\","
                 + "\"transactionAmount\":"
                 + "{\"currency\":\"EUR\","
-                + "\"amount\":\"660.00\"},"
+                + "\"amount\":\"5005.55\"},"
                 + "\"creditorName\":\"Jane Doe\","
                 + "\"creditorAccount\":"
-                + "{\"iban\":\"EE787700771005998888\"},"
+                + "{\"iban\":\"EE347700771005934444\"},"
                 + "\"debtorName\":\"Esbjorn\","
                 + "\"debtorAccount\":"
-                + "{\"iban\":\"EE347700771005934444\"},"
-                + "\"remittanceInformationUnstructured\":\"1\","
-                + "\"remittanceInformationStructured\":\"1\"},"
-                + "{\"entryReference\":\"202106301378706076-303310033\","
-                + "\"bookingDate\":\"1992-01-30\","
+                + "{\"iban\":\"EE787700771005998888\"},"
+                + "\"remittanceInformationUnstructured\":\"Swish\"}"
+                + "}";
+    }
+
+    private String getCreditorNameNullTransaction() {
+        return "{\"entryReference\":\"202106281378178164-302887546\","
+                + "\"bookingDate\":\"1992-01-28\","
                 + "\"transactionAmount\":"
                 + "{\"currency\":\"EUR\","
-                + "\"amount\":\"500.00\"},"
-                + "\"creditorName\":\"Jane Doe\","
+                + "\"amount\":\"5005.55\"},"
                 + "\"creditorAccount\":"
-                + "{\"iban\":\"EE787700771005998888\"},"
-                + "\"debtorName\":\"Esbjorn\","
-                + "\"debtorAccount\":"
                 + "{\"iban\":\"EE347700771005934444\"},"
-                + "\"remittanceInformationUnstructured\":\"1\","
-                + "\"remittanceInformationStructured\":\"1\"}"
+                + "\"debtorAccount\":"
+                + "{\"iban\":\"EE787700771005998888\"},"
+                + "\"remittanceInformationUnstructured\":\"Swish\"}"
                 + "}";
     }
 }
