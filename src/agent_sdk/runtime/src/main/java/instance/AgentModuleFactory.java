959f119639c516a773809f093e65c20576ea888e
Jonas Renold <jonas.renold@tink.se>
2021-12-13 13:03:57 +0100

    fix(agent_sdk): remove unused runtime classes
    These were just temporary iterations.

--- src/agent_sdk/runtime/src/main/java/instance/AgentModuleFactory.java
+++
@@ -1,8 +0,0 @@
-package se.tink.agent.runtime.instance;
-
-import com.google.common.collect.ImmutableSet;
-import com.google.inject.Module;
-
-public interface AgentModuleFactory {
-    ImmutableSet<Module> getAgentModules();
-}
