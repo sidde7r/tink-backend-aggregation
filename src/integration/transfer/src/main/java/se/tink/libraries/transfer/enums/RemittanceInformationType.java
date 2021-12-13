c0cd6dba94b8e57a1608b1ecb868b0619cbe3c9f
Jan Sroczynski <jan.sroczynski@tink.com>
2021-12-13 13:33:50 +0100

    feat(PAYM-3096): support new RemittanceInformation types

--- src/integration/transfer/src/main/java/se/tink/libraries/transfer/enums/RemittanceInformationType.java
+++ src/integration/transfer/src/main/java/se/tink/libraries/transfer/enums/RemittanceInformationType.java
@@ -3,5 +3,10 @@ package se.tink.libraries.transfer.enums;
 public enum RemittanceInformationType {
     UNSTRUCTURED,
     OCR,
-    REFERENCE
+    REFERENCE,
+    // RF is Finland-specific reference
+    RF,
+    INVOICE,
+    // KID (kundeidentifikasjon) - NO specific reference
+    KID
 }
