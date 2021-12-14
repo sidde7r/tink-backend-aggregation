e250ce908f10eb0a31c3c5bea03be8319cc61167
Michal Gwiazda <michal.gwiazda@tink.com>
2021-12-14 09:42:18 +0100

    feat(nl/triodos): add TRANSFER_ACCOUNT_EXTERNAL type
    [MINI-1598]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/berlingroup/fetcher/transactionalaccount/entities/DebtorAccountEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/berlingroup/fetcher/transactionalaccount/entities/DebtorAccountEntity.java
@@ -1,12 +1,13 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.berlingroup.fetcher.transactionalaccount.entities;
 
+import lombok.Getter;
 import se.tink.backend.aggregation.annotations.JsonObject;
 
 @JsonObject
 public class DebtorAccountEntity {
     private String bban;
     private String currency;
-    private String iban;
+    @Getter private String iban;
     private String maskedPan;
     private String msisdn;
     private String pan;
