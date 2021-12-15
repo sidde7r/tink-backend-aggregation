708312517539266b4d97102465935cee3d163472
Jakub Kalicki <jakub.kalicki@tink.com>
2021-12-15 14:25:26 +0100

    log(uk/transaction): Print info about final from date

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/ais/base/fetcher/hybrid/UkObDateCalculator.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/ais/base/fetcher/hybrid/UkObDateCalculator.java
@@ -3,11 +3,13 @@ package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.uk
 import java.time.OffsetDateTime;
 import java.time.Period;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.ScaExpirationValidator;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.ScaStatus;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.date.range.DateRangeCalculator;
 import se.tink.backend.aggregation.nxgen.core.account.Account;
 
+@Slf4j
 @RequiredArgsConstructor
 public class UkObDateCalculator<ACCOUNT extends Account> {
 
@@ -35,15 +37,26 @@ public class UkObDateCalculator<ACCOUNT extends Account> {
             case VALID:
                 finalFrom =
                         calculator.calculateFromAsStartOfTheDay(toDateTime, maxPeriodForValidSca);
+                log.info(
+                        "[TRANSACTION FETCHING] SCA valid. Proposed finalFromDateTime: {}",
+                        finalFrom);
                 break;
             case EXPIRED:
             default:
                 finalFrom =
                         calculator.calculateFromAsStartOfTheDay(toDateTime, maxPeriodForExpiredSca);
+                log.info(
+                        "[TRANSACTION FETCHING] SCA expired. Proposed finalFromDateTime: {}",
+                        finalFrom);
                 break;
         }
 
-        return calculator.applyCertainDateLimit(account, finalFrom);
+        OffsetDateTime finalFromDate = calculator.applyCertainDateLimit(account, finalFrom);
+        log.info(
+                "[TRANSACTION FETCHING] finalFromDateTime after applying certain date limit: {}",
+                finalFromDate);
+
+        return finalFromDate;
     }
 
     private void evaluateScaStatusIfUnknown() {
