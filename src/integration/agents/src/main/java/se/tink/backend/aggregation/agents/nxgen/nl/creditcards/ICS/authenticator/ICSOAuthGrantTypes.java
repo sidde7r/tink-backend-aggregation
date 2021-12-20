d2f82fd35a3f1683804a4a1ba4654cfe2575aa86
Szymon Jaranowski <szymon.jaranowski@tink.com>
2021-12-20 16:24:45 +0100

    feat(nl/ics): remove unused json property
    [MINI-1599]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/creditcards/ICS/authenticator/ICSOAuthGrantTypes.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/nl/creditcards/ICS/authenticator/ICSOAuthGrantTypes.java
@@ -1,17 +1,13 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.creditcards.ICS.authenticator;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum ICSOAuthGrantTypes {
-    @JsonProperty("client_credentials")
     CLIENT_CREDENTIALS("client_credentials"),
 
-    @JsonProperty("authorization_code")
     AUTHORIZATION_CODE("authorization_code"),
 
-    @JsonProperty("refresh_token")
     REFRESH_TOKEN("refresh_token");
 
     private final String value;
