7a47675ac1397fa0d632ba33c693ea3267e0a124
Jonas Renold <jonas.renold@tink.se>
2021-12-28 11:22:49 +0100

    chore(QSealCSigner): deprecate old methods
    The new methods with alg as argument are meant to replace the old ones. Deprecate them until all usages have been moved.

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/eidassigner/QsealcSigner.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/eidassigner/QsealcSigner.java
@@ -1,10 +1,16 @@
 package se.tink.backend.aggregation.eidassigner;
 
 public interface QsealcSigner {
+    /** @deprecated use {@link #getSignatureBase64(QsealcAlg, byte[])} instead. */
+    @Deprecated
     String getSignatureBase64(byte[] signingData);
 
+    /** @deprecated use {@link #getJWSToken(QsealcAlg, byte[])} instead. */
+    @Deprecated
     String getJWSToken(byte[] jwsTokenData);
 
+    /** @deprecated use {@link #getSignature(QsealcAlg, byte[])} instead. */
+    @Deprecated
     byte[] getSignature(byte[] signingData);
 
     String getSignatureBase64(QsealcAlg algorithm, byte[] dataToSign);
