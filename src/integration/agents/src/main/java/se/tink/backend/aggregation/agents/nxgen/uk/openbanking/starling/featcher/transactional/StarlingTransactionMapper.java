b177a3ee90eed96f5d26390b5b53afb9239aa09b
Jakub Badysiak <jakub.badysiak@tink.se>
2022-01-04 17:41:00 +0100

    fix(uk/starling): added mapping for transaction

---
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/starling/featcher/transactional/StarlingTransactionMapper.java
@@ -0,0 +1,89 @@
+package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.featcher.transactional;
+
+import static se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.UkOpenBankingV31Constants.Time.DEFAULT_OFFSET;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import se.tink.backend.aggregation.agents.models.TransactionExternalSystemIdType;
+import se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.featcher.transactional.entity.TransactionEntity;
+import se.tink.backend.aggregation.nxgen.core.transaction.AggregationTransaction.Builder;
+import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
+import se.tink.backend.aggregation.nxgen.core.transaction.TransactionDates;
+import se.tink.libraries.amount.ExactCurrencyAmount;
+import se.tink.libraries.chrono.AvailableDateInformation;
+import se.tink.libraries.enums.MarketCode;
+
+@Slf4j
+public final class StarlingTransactionMapper {
+
+    public static Transaction toTinkTransaction(TransactionEntity transaction) {
+
+        ExactCurrencyAmount transactionAmount = transaction.getAmount().toExactCurrencyAmount();
+
+        if (transaction.isOutDirection()) {
+            transactionAmount = transactionAmount.negate();
+        }
+
+        Builder builder =
+                Transaction.builder()
+                        .setAmount(transactionAmount)
+                        .setDescription(transaction.getReference())
+                        .setPending(transaction.isPending())
+                        .setProprietaryFinancialInstitutionType(transaction.getSource())
+                        .setProviderMarket(String.valueOf(MarketCode.UK))
+                        .setTransactionDates(getTransactionDates(transaction));
+
+        if (transaction.hasSettlementTime()) {
+            builder.setDate(getDateOfTransaction(transaction.getSettlementTime()));
+        } else {
+            builder.setDate(getDateOfTransaction(transaction.getTransactionTime()));
+        }
+
+        if (transaction.hasFeedItemUid()) {
+            builder.addExternalSystemIds(
+                    TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID,
+                    transaction.getFeedItemUid());
+        }
+
+        if (transaction.isMerchantCounterPartyType()) {
+            builder.setMerchantName(transaction.getCounterPartyName());
+        }
+        return (Transaction) builder.build();
+    }
+
+    private static TransactionDates getTransactionDates(TransactionEntity transaction) {
+        TransactionDates.Builder transactionDates =
+                TransactionDates.builder()
+                        .setTransactionDate(
+                                prepareTransactionDate(transaction.getTransactionTime()));
+
+        if (transaction.hasSettlementTime()) {
+            transactionDates.setBookingDate(
+                    prepareTransactionDate(transaction.getSettlementTime()));
+        }
+
+        return transactionDates.build();
+    }
+
+    private static AvailableDateInformation prepareTransactionDate(Instant transactionDateTime) {
+        return new AvailableDateInformation()
+                .setDate(LocalDateTime.ofInstant(transactionDateTime, DEFAULT_OFFSET).toLocalDate())
+                .setInstant(transactionDateTime);
+    }
+
+    private static Date getDateOfTransaction(Instant instant) {
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            return simpleDateFormat.parse(instant.toString());
+        } catch (ParseException e) {
+            log.warn(
+                    "[StarlingTransactionMapper] Issue with parsing date of transaction {}",
+                    instant);
+            return Date.from(instant);
+        }
+    }
+}
--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/starling/featcher/transactional/entity/TransactionEntity.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/starling/featcher/transactional/entity/TransactionEntity.java
@@ -1,42 +1,27 @@
 package se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.featcher.transactional.entity;
 
+import static se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.StarlingConstants.TransactionMapping.MERCHANT_COUNTER_PARTY_TYPE;
+import static se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.StarlingConstants.TransactionMapping.OUT_DIRECTION;
+import static se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.StarlingConstants.TransactionMapping.PENDING_STATUS_LIST;
+import static se.tink.backend.aggregation.agents.nxgen.uk.openbanking.starling.StarlingConstants.TransactionMapping.RELEVANT_STATUS_LIST;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import se.tink.backend.aggregation.agents.models.TransactionExternalSystemIdType;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.TransactionDateMapper;
+import lombok.Getter;
+import lombok.Setter;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.UkObInstantDeserializer;
 import se.tink.backend.aggregation.annotations.JsonObject;
-import se.tink.backend.aggregation.nxgen.core.transaction.AggregationTransaction.Builder;
-import se.tink.backend.aggregation.nxgen.core.transaction.Transaction;
-import se.tink.backend.aggregation.nxgen.core.transaction.TransactionDates;
-import se.tink.libraries.amount.ExactCurrencyAmount;
-import se.tink.libraries.enums.MarketCode;
 
 @JsonObject
+@Getter
+@Setter
 public class TransactionEntity {
-
-    private static final List<String> relevantStatusList =
-            new ArrayList<>(Arrays.asList("settled", "pending", "retrying"));
-
-    private static final List<String> pendingStatusList =
-            new ArrayList<>(Arrays.asList("pending", "retrying"));
-
-    private static final String OUT_DIRECTION = "OUT";
-
-    private static final String MERCHANT_COUNTER_PARTY_TYPE = "MERCHANT";
 
     private String feedItemUid;
     private String categoryUid;
     private AmountEntity amount;
     private AmountEntity sourceAmount;
-
     private String direction;
 
     @JsonDeserialize(using = UkObInstantDeserializer.class)
@@ -57,91 +42,33 @@ public class TransactionEntity {
     private String country;
     private String userNote;
 
-    public Transaction toTinkTransaction() {
-
-        ExactCurrencyAmount transactionAmount = amount.toExactCurrencyAmount();
-
-        if (direction.equals(OUT_DIRECTION)) {
-            transactionAmount = transactionAmount.negate();
-        }
-
-        Builder builder =
-                Transaction.builder()
-                        .setAmount(transactionAmount)
-                        .setDescription(reference)
-                        .setPending(isPending())
-                        .setProprietaryFinancialInstitutionType(source)
-                        .setProviderMarket(String.valueOf(MarketCode.UK))
-                        .setTransactionDates(getTransactionDates());
-
-        if (hasSettlementTime()) {
-            builder.setDate(getDateOfTransaction(settlementTime));
-        } else {
-            builder.setDate(getDateOfTransaction(transactionTime));
-        }
-
-        if (hasFeedItemUid()) {
-            builder.addExternalSystemIds(
-                    TransactionExternalSystemIdType.PROVIDER_GIVEN_TRANSACTION_ID, feedItemUid);
-        }
-
-        if (isMerchantCounterPartyType()) {
-            builder.setMerchantName(counterPartyName);
-        }
-        return (Transaction) builder.build();
-    }
-
-    @JsonIgnore
-    private Date getDateOfTransaction(Instant instant) {
-        try {
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-            return simpleDateFormat.parse(instant.toString());
-        } catch (ParseException e) {
-            return Date.from(instant);
-        }
-    }
-
-    @JsonIgnore
-    private TransactionDates getTransactionDates() {
-        TransactionDates.Builder transactionDates =
-                TransactionDates.builder()
-                        .setTransactionDate(
-                                TransactionDateMapper.prepareTransactionDate(transactionTime));
-
-        if (hasSettlementTime()) {
-            transactionDates.setBookingDate(
-                    TransactionDateMapper.prepareTransactionDate(settlementTime));
-        }
-
-        return transactionDates.build();
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
     @JsonIgnore
     public boolean isRelevant() {
-        return relevantStatusList.contains(getStatus().toLowerCase());
+        return RELEVANT_STATUS_LIST.contains(status.toLowerCase());
     }
 
     @JsonIgnore
-    private boolean isPending() {
-        return pendingStatusList.contains(getStatus().toLowerCase());
+    public boolean isPending() {
+        return PENDING_STATUS_LIST.contains(status.toLowerCase());
     }
 
     @JsonIgnore
-    private boolean isMerchantCounterPartyType() {
+    public boolean isMerchantCounterPartyType() {
         return MERCHANT_COUNTER_PARTY_TYPE.equals(counterPartyType);
     }
 
     @JsonIgnore
-    private boolean hasSettlementTime() {
+    public boolean isOutDirection() {
+        return OUT_DIRECTION.equals(direction);
+    }
+
+    @JsonIgnore
+    public boolean hasSettlementTime() {
         return settlementTime != null;
     }
 
     @JsonIgnore
-    private boolean hasFeedItemUid() {
+    public boolean hasFeedItemUid() {
         return feedItemUid != null;
     }
 }
