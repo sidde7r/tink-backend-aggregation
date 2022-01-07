994087e044338f11cdb6d1a27af2912c9310c8dc
Jesús A. Álvarez <jesus.alvarez@tink.com>
2022-01-07 17:57:27 +0100

    feat(har): print HAR from wiremock tests

--- src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/framework/compositeagenttest/base/CompositeAgentTest.java
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/framework/compositeagenttest/base/CompositeAgentTest.java
@@ -2,10 +2,15 @@ package se.tink.backend.aggregation.agents.framework.compositeagenttest.base;
 
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Set;
+import java.util.function.Function;
 import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.aggregation.agents.framework.context.NewAgentTestContext;
-import se.tink.backend.aggregation.logmasker.LogMasker;
+import se.tink.libraries.se.tink.libraries.har_logger.src.logger.HarMasker;
+import se.tink.libraries.se.tink.libraries.har_logger.src.logger.HarMaskerImpl;
 
 @Slf4j
 public final class CompositeAgentTest {
@@ -47,9 +52,15 @@ public final class CompositeAgentTest {
     }
 
     private String printHttpDebugTrace() {
-        final LogMasker logMasker = context.getLogMasker();
-        final String maskedLog = logMasker.mask(context.getRawHttpTrafficLogger().toString());
-
-        return "\n" + "===== HTTP DEBUG TRACE LOG =====\n" + maskedLog + "\n";
+        try {
+            final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            final HarMasker masker = new HarMaskerImpl(x -> true, Function.identity());
+            context.getHarLogCollector().writeHar(outputStream, masker);
+            final String logHar = outputStream.toString(StandardCharsets.UTF_8.name());
+            return "\n" + "===== HTTP DEBUG TRACE LOG =====\n" + logHar + "\n";
+        } catch (IOException e) {
+            log.error("Could not serialize har: ", e);
+            return null;
+        }
     }
 }
