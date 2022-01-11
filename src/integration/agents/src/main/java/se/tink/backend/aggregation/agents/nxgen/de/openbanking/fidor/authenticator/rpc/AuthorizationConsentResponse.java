827f5818c4dca48e9082df33f532263bb3f551e7
Jan Sameryt <jan.sameryt@tink.com>
2022-01-11 12:41:10 +0100

    refactor(DE): drop dead code
    [NZG-1086]
    
    This commits removes some OB agents that were never really used, are old, and not of good quality

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/de/openbanking/fidor/authenticator/rpc/AuthorizationConsentResponse.java
+++
@@ -1,8 +0,0 @@
-package se.tink.backend.aggregation.agents.nxgen.de.openbanking.fidor.authenticator.rpc;
-
-import se.tink.backend.aggregation.annotations.JsonObject;
-
-@JsonObject
-public class AuthorizationConsentResponse {
-    private String scaStatus;
-}
