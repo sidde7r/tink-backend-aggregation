29239bbad51421a957a80b0168fe7933a27068ef
Jonas Renold <jonas.renold@tink.se>
2022-01-12 11:32:12 +0100

    feat(agent_sdk): expose qsealcSigner in utilities

--- src/agent_sdk/runtime/src/main/java/environment/UtilitiesImpl.java
+++ src/agent_sdk/runtime/src/main/java/environment/UtilitiesImpl.java
@@ -5,6 +5,7 @@ import se.tink.agent.sdk.utils.RandomGenerator;
 import se.tink.agent.sdk.utils.Sleeper;
 import se.tink.agent.sdk.utils.SupplementalInformationHelper;
 import se.tink.agent.sdk.utils.TimeGenerator;
+import se.tink.agent.sdk.utils.signer.QsealcSigner;
 import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
 
 public class UtilitiesImpl implements Utilities {
@@ -13,18 +14,21 @@ public class UtilitiesImpl implements Utilities {
     private final Sleeper sleeper;
     private final TinkHttpClient tinkHttpClient;
     private final SupplementalInformationHelper supplementalInformationHelper;
+    private final QsealcSigner qsealcSigner;
 
     public UtilitiesImpl(
             RandomGenerator randomGenerator,
             TimeGenerator timeGenerator,
             Sleeper sleeper,
             TinkHttpClient tinkHttpClient,
-            SupplementalInformationHelper supplementalInformationHelper) {
+            SupplementalInformationHelper supplementalInformationHelper,
+            QsealcSigner qsealcSigner) {
         this.randomGenerator = randomGenerator;
         this.timeGenerator = timeGenerator;
         this.sleeper = sleeper;
         this.tinkHttpClient = tinkHttpClient;
         this.supplementalInformationHelper = supplementalInformationHelper;
+        this.qsealcSigner = qsealcSigner;
     }
 
     @Override
@@ -50,5 +54,10 @@ public class UtilitiesImpl implements Utilities {
     @Override
     public SupplementalInformationHelper getSupplementalInformationHelper() {
         return supplementalInformationHelper;
+    }
+
+    @Override
+    public QsealcSigner getQsealcSigner() {
+        return qsealcSigner;
     }
 }
