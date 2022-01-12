c8107c4f855b11b53e96e40355ecfe1c5eaec1d3
Jonas Renold <jonas.renold@tink.se>
2022-01-12 11:28:31 +0100

    feat(agent_sdk): qsealc signer util
    A utility class to sign data with the aggregators qseal certificate. The `Signature` class (result) is generalized and can be used by any signer method.
    It's purposely decoupled from the qsealcsigner client (which soon will become a library) so that the library can change without affecting the agents.

---
+++ src/agent_sdk/sdk/src/utils/signer/QsealcAlgorithm.java
@@ -0,0 +1,7 @@
+package se.tink.agent.sdk.utils.signer;
+
+public enum QsealcAlgorithm {
+    RSA_SHA256,
+    PSS_SHA256,
+    JWT_RSA_SHA256
+}
---
+++ src/agent_sdk/sdk/src/utils/signer/Signature.java
@@ -0,0 +1,40 @@
+package se.tink.agent.sdk.utils.signer;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class Signature {
+    private static final Base64.Encoder BASE64_ENCODER = Base64.getEncoder();
+    private static final Base64.Encoder BASE64_URL_ENCODER = Base64.getUrlEncoder();
+
+    private final byte[] signatureBytes;
+
+    private Signature(byte[] signatureBytes) {
+        this.signatureBytes = signatureBytes;
+    }
+
+    public byte[] getBytes() {
+        return this.signatureBytes;
+    }
+
+    public String getString(Charset charset) {
+        return new String(this.signatureBytes, charset);
+    }
+
+    public String getString() {
+        return getString(StandardCharsets.US_ASCII);
+    }
+
+    public String getBase64Encoded() {
+        return BASE64_ENCODER.encodeToString(this.signatureBytes);
+    }
+
+    public String getBase64UrlEncoded() {
+        return BASE64_URL_ENCODER.encodeToString(this.signatureBytes);
+    }
+
+    public static Signature create(byte[] signatureBytes) {
+        return new Signature(signatureBytes);
+    }
+}
