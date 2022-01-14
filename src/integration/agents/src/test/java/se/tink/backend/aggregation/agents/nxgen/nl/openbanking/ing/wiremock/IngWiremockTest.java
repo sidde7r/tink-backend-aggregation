164fc26f621725d8cf61345372bda1b2bc854668
Jonas Renold <jonas.renold@tink.se>
2022-01-14 12:38:30 +0100

    fix(nl/ing): use new QsealcSigner

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/wiremock/IngWiremockTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/wiremock/IngWiremockTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonFileParser;
 import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
 import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockrefresh.AgentWireMockRefreshTest;
-import se.tink.backend.aggregation.agents.nxgen.nl.openbanking.ing.wiremock.module.IngWiremockTestModule;
 import se.tink.backend.aggregation.configuration.AgentsServiceConfigurationReader;
 import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
 import se.tink.libraries.credentials.service.RefreshableItem;
@@ -35,7 +34,6 @@ public class IngWiremockTest {
                         .addRefreshableItems(RefreshableItem.allRefreshableItemsAsArray())
                         .addRefreshableItems(RefreshableItem.IDENTITY_DATA)
                         .addCallbackData("code", "dummyCode")
-                        .withAgentTestModule(new IngWiremockTestModule())
                         .build();
 
         final AgentContractEntity expected =
