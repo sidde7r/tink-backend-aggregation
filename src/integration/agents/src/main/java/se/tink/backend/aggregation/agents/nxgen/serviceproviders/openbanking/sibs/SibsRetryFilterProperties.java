f1f43cfe7f958d4c6655a2e474ddac0668088107
Mateusz Wiski <mateusz.wiski@tink.com>
2022-01-14 08:53:25 +0100

    fix(pt/sibs): add more retries and prolong time between them
    [MINI-1472]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/SibsRetryFilterProperties.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/sibs/SibsRetryFilterProperties.java
@@ -6,5 +6,5 @@ import lombok.Value;
 public class SibsRetryFilterProperties {
     int maxNumRetries;
     int retrySleepMilliseconds;
-    int serviceInvalidMaxNumRetries;
+    int serviceInvalidRetrySleepMilliseconds;
 }
