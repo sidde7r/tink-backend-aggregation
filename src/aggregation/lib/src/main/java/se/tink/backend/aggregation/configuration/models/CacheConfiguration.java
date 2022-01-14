55341217774b9b9d6f54f9e32ef25fdc84d26e6a
Piotr Cebo <piotr.cebo@tink.com>
2022-01-14 12:09:41 +0100

    chore(config): fix config + add replica client log creation

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/configuration/models/CacheConfiguration.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/configuration/models/CacheConfiguration.java
@@ -1,13 +1,12 @@
 package se.tink.backend.aggregation.configuration.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.google.common.collect.Lists;
 import java.util.List;
 
 public class CacheConfiguration {
     @JsonProperty private String hosts;
     @JsonProperty private boolean enabled = true;
-    @JsonProperty private List<String> mirroredClusters = Lists.newArrayList();
+    @JsonProperty private List<String> mirroredClusters;
 
     public boolean isEnabled() {
         return enabled;
