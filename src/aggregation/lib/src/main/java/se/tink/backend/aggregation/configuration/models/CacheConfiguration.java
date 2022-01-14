f79efc1340fa3c377e27bdd19a7b45e9cd6c6355
Piotr Cebo <piotr.cebo@tink.com>
2022-01-14 15:55:46 +0100

    revert(cache): rever breaking changes

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/configuration/models/CacheConfiguration.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/configuration/models/CacheConfiguration.java
@@ -1,12 +1,13 @@
 package se.tink.backend.aggregation.configuration.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.common.collect.Lists;
 import java.util.List;
 
 public class CacheConfiguration {
     @JsonProperty private String hosts;
     @JsonProperty private boolean enabled = true;
-    @JsonProperty private List<String> mirroredClusters;
+    @JsonProperty private List<String> mirroredClusters = Lists.newArrayList();
 
     public boolean isEnabled() {
         return enabled;
