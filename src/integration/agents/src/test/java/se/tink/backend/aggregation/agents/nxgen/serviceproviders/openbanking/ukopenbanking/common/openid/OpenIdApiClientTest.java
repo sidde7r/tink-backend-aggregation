2095236df59669ecd038c57c9dc8f2b4676953a7
Hoang My Nguyen <hoang.my-nguyen@tink.com>
2022-01-17 14:44:17 +0100

    refactor(uk/authmethod): change error handling tokenEndpointAuthMethod

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/common/openid/OpenIdApiClientTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/common/openid/OpenIdApiClientTest.java
@@ -1,6 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -15,13 +16,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
+import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.configuration.ClientInfo;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.configuration.SoftwareStatementAssertion;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.jwt.signer.iface.JwtSigner;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.rpc.TokenRequestForm;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.rpc.WellKnownResponse;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.randomness.RandomValueGenerator;
 import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.http.filter.filterable.request.RequestBuilder;
 import se.tink.backend.aggregation.nxgen.http.url.URL;
+import se.tink.libraries.serialization.utils.SerializationUtils;
 
 @RunWith(MockitoJUnitRunner.class)
 public class OpenIdApiClientTest {
@@ -64,9 +69,33 @@ public class OpenIdApiClientTest {
         assertThat(jwkPublicKeys.get().get("external").getFormat()).isEqualTo("X.509");
     }
 
+    @Test
+    public void shouldThrowAuthorizationErrorWhenPreferredTokenEndpointAuthMethodNotFound() {
+        // given
+        TokenRequestForm requestForm = new TokenRequestForm();
+        WellKnownResponse wellKnownConfiguration =
+                createWellKnownResponseWithoutTokenEndpointAuthMethod();
+        // when
+        Throwable throwable =
+                catchThrowable(
+                        () ->
+                                apiClient.handleFormAuthentication(
+                                        requestForm, wellKnownConfiguration));
+        // then
+        assertThat(throwable)
+                .isExactlyInstanceOf(AuthorizationException.class)
+                .hasMessage("[OpenIdApiClient]: Preferred token endpoint auth method not found.");
+    }
+
     private RequestBuilder mockRequestBuilder(String contents) {
         RequestBuilder toReturn = mock(RequestBuilder.class);
         when(toReturn.get(eq(String.class))).thenReturn(contents);
         return toReturn;
+    }
+
+    private WellKnownResponse createWellKnownResponseWithoutTokenEndpointAuthMethod() {
+        return SerializationUtils.deserializeFromString(
+                "        {\"version\":\"3.0\",\"issuer\":\"https:\\/\\/oauth.tiaa.barclays.com\",\"authorization_endpoint\":\"https:\\/\\/oauth.tiaa.barclays.com\\/BarclaysCorporate\\/as\\/authorization.oauth2\",\"token_endpoint\":\"https:\\/\\/token.tiaa.barclays.com\\/as\\/token.oauth2\",\"jwks_uri\":\"https:\\/\\/keystore.openbanking.org.uk\\/0015800000jfAW1AAM\\/2SPmPNUU6KrtcgutPlkfBd.jwks\",\"scopes_supported\":[\"accounts\",\"payments\",\"fundsconfirmations\",\"openid\"],\"claims_supported\":[\"openbanking_intent_id\",\"http://openbanking.org.uk/refresh_token_expires_at\",\"sub\",\"acr\"],\"response_types_supported\":[\"code id_token\"],\"response_modes_supported\":[\"fragment\",\"query\",\"form_post\"],\"grant_types_supported\":[\"implicit\",\"authorization_code\",\"refresh_token\",\"client_credentials\"],\"subject_types_supported\":[\"public\"],\"id_token_signing_alg_values_supported\":[\"PS256\"],\"token_endpoint_auth_signing_alg_values_supported\":[\"PS256\"],\"token_endpoint_auth_methods_supported\":[\"any\"],\"claim_types_supported\":[\"normal\"],\"claims_parameter_supported\":true,\"request_parameter_supported\":true,\"request_object_signing_alg_values_supported\":[\"PS256\"],\"request_object_encryption_alg_values_supported\":[],\"request_object_encryption_enc_values_supported\":[],\"request_uri_parameter_supported\":false,\"tls_client_certificate_bound_access_tokens\":true}\n",
+                WellKnownResponse.class);
     }
 }
