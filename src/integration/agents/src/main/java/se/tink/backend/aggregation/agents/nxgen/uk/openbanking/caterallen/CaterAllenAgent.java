e1bc0ecf55498d7ba8b794cb7b6c780d37dd9346
Malgorzata Kosecka <malgorzata.kosecka@tink.se>
2022-01-18 18:01:51 +0100

    refactor(uk/caterallen): remove unused unleash

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/caterallen/CaterAllenAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/caterallen/CaterAllenAgent.java
@@ -4,13 +4,10 @@ import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.CH
 import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.SAVINGS_ACCOUNTS;
 
 import com.google.inject.Inject;
-import lombok.extern.slf4j.Slf4j;
-import no.finn.unleash.UnleashContext;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForDecoupledMode;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForProductionMode;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.UkOpenBankingBaseAgent;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.UkOpenBankingApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.UkOpenBankingFlowFacade;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.authenticator.UkOpenBankingAisAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.consent.ConsentStatusValidator;
@@ -22,18 +19,11 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.uko
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.v31.UkOpenBankingV31Ais;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.v31.authenticator.UkOpenBankingAisAuthenticationController;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.OpenIdAuthenticationValidator;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.configuration.ClientInfo;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.configuration.SoftwareStatementAssertion;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.jwt.signer.iface.JwtSigner;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticator;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.ThirdPartyAppAuthenticationController;
-import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
-import se.tink.libraries.unleash.model.Toggle;
-import se.tink.libraries.unleash.strategies.aggregation.providersidsandexcludeappids.Constants;
 
-@Slf4j
 @AgentDependencyModulesForProductionMode(modules = UkOpenBankingFlowModule.class)
 @AgentDependencyModulesForDecoupledMode(
         modules = UkOpenBankingLocalKeySignerModuleForDecoupledMode.class)
@@ -41,7 +31,6 @@ import se.tink.libraries.unleash.strategies.aggregation.providersidsandexcludeap
 public class CaterAllenAgent extends UkOpenBankingBaseAgent {
 
     private static final UkOpenBankingAisConfig aisConfig;
-    private final AgentComponentProvider componentProvider;
 
     static {
         aisConfig =
@@ -56,61 +45,12 @@ public class CaterAllenAgent extends UkOpenBankingBaseAgent {
     public CaterAllenAgent(
             AgentComponentProvider componentProvider, UkOpenBankingFlowFacade flowFacade) {
         super(componentProvider, flowFacade, aisConfig);
-        this.componentProvider = componentProvider;
     }
 
     @Override
     public Authenticator constructAuthenticator() {
         UkOpenBankingAisAuthenticationController authController = createUkObAuthController();
         return createAutoAuthController(authController);
-    }
-
-    @Override
-    protected UkOpenBankingApiClient createApiClient(
-            TinkHttpClient httpClient,
-            JwtSigner signer,
-            SoftwareStatementAssertion softwareStatement,
-            String redirectUrl,
-            ClientInfo providerConfiguration) {
-
-        Toggle toggle =
-                Toggle.of("uk-caterallen-additional-header")
-                        .context(
-                                UnleashContext.builder()
-                                        .addProperty(
-                                                Constants.Context.PROVIDER_NAME.getValue(),
-                                                "uk-caterallen-ob")
-                                        .addProperty(
-                                                Constants.Context.APP_ID.getValue(),
-                                                componentProvider.getContext().getAppId())
-                                        .build())
-                        .build();
-
-        if (componentProvider.getUnleashClient().isToggleEnabled(toggle)) {
-            log.info("[CaterAllenApiClient used with additional header]");
-            return new CaterAllenApiClient(
-                    httpClient,
-                    signer,
-                    softwareStatement,
-                    redirectUrl,
-                    providerConfiguration,
-                    randomValueGenerator,
-                    persistentStorage,
-                    aisConfig,
-                    componentProvider);
-        } else {
-            log.info("[UkOpenBankingApiClient used]");
-            return new UkOpenBankingApiClient(
-                    httpClient,
-                    signer,
-                    softwareStatement,
-                    redirectUrl,
-                    providerConfiguration,
-                    randomValueGenerator,
-                    persistentStorage,
-                    aisConfig,
-                    componentProvider);
-        }
     }
 
     @Override
