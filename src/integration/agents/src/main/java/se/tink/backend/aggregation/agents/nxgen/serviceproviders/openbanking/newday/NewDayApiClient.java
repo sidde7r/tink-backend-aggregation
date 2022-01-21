a014399e718de3ca74a21e9054eb62854ede2252
Malgorzata Kosecka <malgorzata.kosecka@tink.se>
2022-01-21 16:56:25 +0100

    revert(uk/newday): fix for jwks link
    [IFD-2762]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/newday/NewDayApiClient.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/newday/NewDayApiClient.java
@@ -1,11 +1,7 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.newday;
 
-import java.security.PublicKey;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.UkOpenBankingApiClient;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.interfaces.UkOpenBankingAisConfig;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.OpenIdConstants.Scopes;
@@ -13,7 +9,6 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.uko
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.configuration.SoftwareStatementAssertion;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.entities.ClientMode;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.jwt.signer.iface.JwtSigner;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.rpc.JsonWebKeySet;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.rpc.TokenRequestForm;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.rpc.TokenResponse;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.rpc.WellKnownResponse;
@@ -22,18 +17,8 @@ import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.ran
 import se.tink.backend.aggregation.nxgen.core.authentication.OAuth2Token;
 import se.tink.backend.aggregation.nxgen.http.client.TinkHttpClient;
 import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
-import se.tink.libraries.serialization.utils.SerializationUtils;
 
-/* This class was implemented to bypass NewDay mistake
- *   they return jwksUri as http link which doesn't work
- *   changing it to https allow us to get proper response.
- *   This class should be remove when bank will change this link in their well-known.
- *   Ticket with link for issue created on UK service desk:
- *   TODO IFD-2762
- */
 public class NewDayApiClient extends UkOpenBankingApiClient {
-
-    private Map<String, PublicKey> cachedJwkPublicKeys;
 
     public NewDayApiClient(
             TinkHttpClient httpClient,
@@ -55,28 +40,6 @@ public class NewDayApiClient extends UkOpenBankingApiClient {
                 persistentStorage,
                 aisConfig,
                 componentProvider);
-    }
-
-    @Override
-    public Optional<Map<String, PublicKey>> getJwkPublicKeys() {
-        if (Objects.nonNull(cachedJwkPublicKeys)) {
-            return Optional.of(cachedJwkPublicKeys);
-        }
-
-        String jwksUri =
-                getWellKnownConfiguration().getJwksUri().toString().replace("http", "https");
-
-        String response = httpClient.request(jwksUri).get(String.class);
-
-        JsonWebKeySet jsonWebKeySet =
-                SerializationUtils.deserializeFromString(response, JsonWebKeySet.class);
-
-        if (jsonWebKeySet == null) {
-            return Optional.empty();
-        }
-
-        cachedJwkPublicKeys = jsonWebKeySet.getAllKeysMap();
-        return Optional.ofNullable(cachedJwkPublicKeys);
     }
 
     /*
