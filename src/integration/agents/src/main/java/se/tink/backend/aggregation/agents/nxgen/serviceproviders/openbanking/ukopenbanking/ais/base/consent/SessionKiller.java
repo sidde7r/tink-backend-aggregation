06ed422e6643b85fe37b79a30ffbdb0ab5194012
Grzegorz Zacharski <grzegorz.zacharski@tink.com>
2022-01-21 16:39:31 +0100

    refactor(uk/ob): simplify implementation of SessionKiller
    [IFD-3368]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/ais/base/consent/SessionKiller.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/ukopenbanking/ais/base/consent/SessionKiller.java
@@ -1,15 +1,20 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.consent;
 
 import se.tink.backend.aggregation.agents.exceptions.SessionException;
-import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.UkOpenBankingV31Constants.PersistentStorageKeys;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.storage.AuthenticationDataStorage;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.storage.ConsentDataStorage;
 import se.tink.backend.aggregation.nxgen.storage.PersistentStorage;
 
 public interface SessionKiller {
 
     static void cleanUpAndExpireSession(PersistentStorage storage, SessionException exception) {
-        storage.remove(PersistentStorageKeys.AIS_ACCOUNT_CONSENT_ID);
-        storage.remove(PersistentStorageKeys.AIS_ACCESS_TOKEN);
-        storage.remove(PersistentStorageKeys.AIS_ACCOUNT_CONSENT_CREATION_DATE);
+        AuthenticationDataStorage authDataStorage = new AuthenticationDataStorage(storage);
+        ConsentDataStorage consentDataStorage = new ConsentDataStorage(storage);
+
+        consentDataStorage.removeConsentId();
+        authDataStorage.removeAccessToken();
+        authDataStorage.removeStrongAuthenticationTime();
+
         throw exception;
     }
 }
