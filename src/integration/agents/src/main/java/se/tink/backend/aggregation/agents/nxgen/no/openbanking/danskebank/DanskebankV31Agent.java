aad017bab9d20a66e3fc74f475dc359a09e3897f
Elena Kolpakova <elena.kolpakova@tink.com>
2022-01-24 15:42:13 +0100

    feat(no/Dankebank/OB): add PIS

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/danskebank/DanskebankV31Agent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/no/openbanking/danskebank/DanskebankV31Agent.java
@@ -3,14 +3,19 @@ package se.tink.backend.aggregation.agents.nxgen.no.openbanking.danskebank;
 import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.CHECKING_ACCOUNTS;
 import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.CREDIT_CARDS;
 import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.SAVINGS_ACCOUNTS;
+import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.TRANSFERS;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
+import se.tink.backend.aggregation.agents.agentcapabilities.AgentPisCapability;
+import se.tink.backend.aggregation.agents.agentcapabilities.PisCapability;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForDecoupledMode;
 import se.tink.backend.aggregation.agents.module.annotation.AgentDependencyModulesForProductionMode;
 import se.tink.backend.aggregation.agents.nxgen.no.openbanking.danskebank.mapper.DanskeNoIdentifierMapper;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.danskebank.DanskeBankV31EUBaseAgent;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.danskebank.DanskebankV31Constant.Url.V31;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.danskebank.configuration.DanskeBankPisConfiguration;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.danskebank.configuration.DanskebankAisConfiguration;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.danskebank.signer.DanskeOpenBankingModule;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.base.UkOpenBankingFlowFacade;
@@ -19,14 +24,23 @@ import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.uko
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.v31.mapper.creditcards.CreditCardAccountMapper;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.v31.mapper.transactionalaccounts.TransactionalAccountBalanceMapper;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.ais.v31.mapper.transactionalaccounts.TransactionalAccountMapper;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.common.openid.jwt.signer.iface.JwtSigner;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.pis.common.filter.UkOpenBankingPisRequestFilter;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.pis.common.signature.UkOpenBankingJwtSignatureHelper;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.pis.common.signature.UkOpenBankingPs256MinimalSignatureCreator;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.pis.common.signature.UkOpenBankingPs256SignatureCreator;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.pis.common.signature.UkOpenBankingRs256SignatureCreator;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.ukopenbanking.pis.storage.UkOpenBankingPaymentStorage;
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
+import se.tink.backend.aggregation.nxgen.agents.componentproviders.generated.randomness.RandomValueGenerator;
 import se.tink.libraries.enums.MarketCode;
 import se.tink.libraries.mapper.PrioritizedValueExtractor;
 
 @AgentDependencyModulesForProductionMode(modules = DanskeOpenBankingModule.class)
 @AgentDependencyModulesForDecoupledMode(
         modules = UkOpenBankingLocalKeySignerModuleForDecoupledMode.class)
-@AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS, CREDIT_CARDS})
+@AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS, CREDIT_CARDS, TRANSFERS})
+@AgentPisCapability(capabilities = {PisCapability.NORWEGIAN_DOMESTIC_CREDIT_TRANSFER})
 public final class DanskebankV31Agent extends DanskeBankV31EUBaseAgent {
 
     private static final UkOpenBankingAisConfig aisConfig;
@@ -45,6 +59,14 @@ public final class DanskebankV31Agent extends DanskeBankV31EUBaseAgent {
                 componentProvider,
                 flowFacade,
                 aisConfig,
+                new DanskeBankPisConfiguration.Builder(
+                                V31.getPisBaseUrl().toString(), MarketCode.NO)
+                        .withWellKnownURL(V31.getWellKnownUrl(MarketCode.NO))
+                        .build(),
+                createPisRequestFilter(
+                        new UkOpenBankingPs256MinimalSignatureCreator(flowFacade.getJwtSinger()),
+                        flowFacade.getJwtSinger(),
+                        componentProvider.getRandomValueGenerator()),
                 getCreditCardAccountMapperWithDanskeIdentifierMapper(),
                 getTransactionalAccountMapperWithDanskeIdentifierMapper());
     }
@@ -62,5 +84,21 @@ public final class DanskebankV31Agent extends DanskeBankV31EUBaseAgent {
         return new TransactionalAccountMapper(
                 new TransactionalAccountBalanceMapper(valueExtractor),
                 new DanskeNoIdentifierMapper(valueExtractor));
+    }
+
+    protected static UkOpenBankingPisRequestFilter createPisRequestFilter(
+            UkOpenBankingPs256SignatureCreator ps256SignatureCreator,
+            JwtSigner jwtSigner,
+            RandomValueGenerator randomValueGenerator) {
+        final UkOpenBankingPaymentStorage paymentStorage = new UkOpenBankingPaymentStorage();
+        final UkOpenBankingJwtSignatureHelper jwtSignatureHelper =
+                new UkOpenBankingJwtSignatureHelper(
+                        new ObjectMapper(),
+                        paymentStorage,
+                        new UkOpenBankingRs256SignatureCreator(jwtSigner),
+                        ps256SignatureCreator);
+
+        return new UkOpenBankingPisRequestFilter(
+                jwtSignatureHelper, paymentStorage, randomValueGenerator);
     }
 }
