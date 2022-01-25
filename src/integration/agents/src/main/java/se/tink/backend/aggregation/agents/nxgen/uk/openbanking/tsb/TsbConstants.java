5c7c10292af408a79364ff47942958e37d9a7385
Jakub Kalicki <jakub.kalicki@tink.com>
2022-01-25 12:54:03 +0100

    fix(uk/tsb): Increase timeout limit as a workaround
    [IFD-3402]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/tsb/TsbConstants.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/uk/openbanking/tsb/TsbConstants.java
@@ -8,4 +8,6 @@ final class TsbConstants {
     public static final String PIS_API_URL = "https://apis.tsb.co.uk/apis/open-banking/v3.1/pisp";
     public static final String WELL_KNOWN_URL =
             "https://apis.tsb.co.uk/apis/open-banking/v3.1/.well-known/openid-configuration";
+
+    public static final int TIMEOUT_LIMIT_MS = 60000;
 }
