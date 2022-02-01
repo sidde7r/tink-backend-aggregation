42b8bf3ca73be36fca1009a1827c387b549e8c20
Jonas Renold <jonas.renold@tink.se>
2022-02-01 11:33:59 +0100

    docs(agent_sdk): add javadoc to interface method

--- src/agent_sdk/sdk/src/steppable_execution/execution_flow/ExecutionFlow.java
+++ src/agent_sdk/sdk/src/steppable_execution/execution_flow/ExecutionFlow.java
@@ -5,5 +5,10 @@ import javax.annotation.Nullable;
 import se.tink.agent.sdk.steppable_execution.base_step.BaseStep;
 
 public interface ExecutionFlow<T, R> {
+    /**
+     * @param stepId The step identifier
+     * @return Optional.empty() if the identifier could not be found among the registered steps,
+     *     otherwise Optional.of(step)
+     */
     Optional<BaseStep<T, R>> getStep(@Nullable String stepId);
 }
