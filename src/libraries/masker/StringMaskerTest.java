0b21d86946758081e0c4b58f2b4a036976035933
Jakub Kalicki <jakub.kalicki@tink.com>
2022-02-01 12:39:19 +0100

    refactor(masker): Rename test methods

--- src/libraries/masker/StringMaskerTest.java
+++ src/libraries/masker/StringMaskerTest.java
@@ -7,20 +7,20 @@ import org.junit.Test;
 public class StringMaskerTest {
 
     @Test
-    public void shouldNotMaskNullAndEmptyString() {
+    public void shouldNotStarMaskNullAndEmptyString() {
         assertThat(StringMasker.starMaskBeginningOfString(null)).isEqualTo(null);
         assertThat(StringMasker.starMaskBeginningOfString("")).isEqualTo("");
     }
 
     @Test
-    public void shouldMaskAllStringIfLengthSmallerThat9() {
+    public void shouldStarMaskAllCharactersIfLengthUpTo8() {
         assertThat(StringMasker.starMaskBeginningOfString("1")).isEqualTo("*");
         assertThat(StringMasker.starMaskBeginningOfString("123")).isEqualTo("***");
         assertThat(StringMasker.starMaskBeginningOfString("12345678")).isEqualTo("********");
     }
 
     @Test
-    public void shouldLeave4LastDigitsWhenLengthLongerThan9() {
+    public void shouldStarMaskStringExceptLast4CharactersIfLongerThan8() {
         assertThat(StringMasker.starMaskBeginningOfString("123456789")).isEqualTo("*****6789");
         assertThat(StringMasker.starMaskBeginningOfString("12345678911131517"))
                 .isEqualTo("*************1517");
