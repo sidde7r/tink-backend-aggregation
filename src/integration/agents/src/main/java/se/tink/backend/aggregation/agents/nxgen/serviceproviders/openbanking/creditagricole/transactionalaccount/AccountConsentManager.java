c61d7a93bca28e3bd91fbd224d7d094433e4bd2d
Mateusz Pedzich <mateusz.pedzich@tink.se>
2022-02-02 14:37:08 +0100

    fix(fr/CA): sending always all accounts in consent request
    [PENG-601]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/creditagricole/transactionalaccount/AccountConsentManager.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/serviceproviders/openbanking/creditagricole/transactionalaccount/AccountConsentManager.java
@@ -1,25 +1,30 @@
 package se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.creditagricole.transactionalaccount;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.creditagricole.transactionalaccount.apiclient.CreditAgricoleBaseApiClient;
+import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.creditagricole.transactionalaccount.entities.AccountEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.creditagricole.transactionalaccount.entities.AccountIdEntity;
 import se.tink.backend.aggregation.agents.nxgen.serviceproviders.openbanking.creditagricole.transactionalaccount.rpc.GetAccountsResponse;
 
+/**
+ * Whenever sending consents, we should always put ALL accounts in the request. New consent is
+ * overriding old one and not putting all can result in lack of resources.
+ */
 @Slf4j
 public class AccountConsentManager {
 
     private final CreditAgricoleBaseApiClient apiClient;
-    private final AtomicBoolean wasConsentSent = new AtomicBoolean();
+    private boolean wasConsentSent;
 
     public AccountConsentManager(CreditAgricoleBaseApiClient apiClient) {
         this.apiClient = apiClient;
     }
 
     boolean prepareConsentsIfNeeded(@NonNull GetAccountsResponse accountsResponse) {
-        if (wasConsentSent.get()) {
+        if (wasConsentSent) {
             log.info("[ACCOUNT CONSENT MANAGER] Consents were already prepared before");
             return false;
         }
@@ -31,11 +36,13 @@ public class AccountConsentManager {
             return false;
         }
 
-        log.info(
-                "[ACCOUNT CONSENT MANAGER] Sending consents for {} accounts",
-                accountsWithoutConsent.size());
-        apiClient.putConsents(accountsWithoutConsent);
-        wasConsentSent.set(true);
+        List<AccountIdEntity> allAccountIds =
+                accountsResponse.getAccounts().stream()
+                        .map(AccountEntity::getAccountId)
+                        .collect(Collectors.toList());
+        log.info("[ACCOUNT CONSENT MANAGER] Sending consent for {} accounts", allAccountIds.size());
+        apiClient.putConsents(allAccountIds);
+        wasConsentSent = true;
         return true;
     }
 }
