2d9a2f69f9bf932247e6732845254f92b2f80d5e
Annica Alstermark <annica.alstermark@tink.se>
2022-02-04 11:20:30 +0100

    refactor(agent_sdk): Use lombok constructor

--- src/agent_sdk/runtime/src/environment/AgentEnvironment.java
+++ src/agent_sdk/runtime/src/environment/AgentEnvironment.java
@@ -1,16 +1,13 @@
 package src.agent_sdk.runtime.src.environment;
 
+import lombok.AllArgsConstructor;
 import se.tink.agent.sdk.environment.Operation;
 import se.tink.agent.sdk.environment.Utilities;
 
+@AllArgsConstructor
 public class AgentEnvironment {
     private final Operation operation;
     private final Utilities utilities;
-
-    public AgentEnvironment(Operation operation, Utilities utilities) {
-        this.operation = operation;
-        this.utilities = utilities;
-    }
 
     public Operation getOperation() {
         return operation;
