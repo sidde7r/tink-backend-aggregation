cc53b78387124041e32e44ba794158e64c3059ab
Mateusz Wiski <mateusz.wiski@tink.com>
2022-02-09 16:25:54 +0100

    feat(ingbase/ob): add wiremock for instant SEPA
    [MINI-1823]

--- src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/wiremock/IngWiremockTest.java
+++ src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/wiremock/IngWiremockTest.java
@@ -1,28 +1,51 @@
 package se.tink.backend.aggregation.agents.nxgen.nl.openbanking.ing.wiremock;
 
+import io.dropwizard.configuration.ConfigurationException;
+import java.io.IOException;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import se.tink.backend.aggregation.agents.framework.assertions.AgentContractEntitiesJsonFileParser;
 import se.tink.backend.aggregation.agents.framework.assertions.entities.AgentContractEntity;
+import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockpayment.AgentWireMockPaymentTest;
+import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockpayment.command.PaymentCommand;
 import se.tink.backend.aggregation.agents.framework.compositeagenttest.wiremockrefresh.AgentWireMockRefreshTest;
 import se.tink.backend.aggregation.configuration.AgentsServiceConfigurationReader;
 import se.tink.backend.aggregation.configuration.agentsservice.AgentsServiceConfiguration;
+import se.tink.libraries.account.identifiers.IbanIdentifier;
+import se.tink.libraries.amount.ExactCurrencyAmount;
 import se.tink.libraries.credentials.service.RefreshableItem;
 import se.tink.libraries.enums.MarketCode;
+import se.tink.libraries.payment.rpc.Creditor;
+import se.tink.libraries.payment.rpc.Debtor;
+import se.tink.libraries.payment.rpc.Payment;
+import se.tink.libraries.payments.common.model.PaymentScheme;
+import se.tink.libraries.transfer.enums.RemittanceInformationType;
+import se.tink.libraries.transfer.rpc.RemittanceInformation;
 
+@RunWith(JUnitParamsRunner.class)
 public class IngWiremockTest {
+
+    private static final String RESOURCE_PATH =
+            "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/wiremock/resources/";
+    private static final String CONFIGURATION_PATH = RESOURCE_PATH + "configuration.yml";
+
+    private final AgentsServiceConfiguration configuration = getConfiguration();
+
+    private static AgentsServiceConfiguration getConfiguration() {
+        try {
+            return AgentsServiceConfigurationReader.read(CONFIGURATION_PATH);
+        } catch (IOException | ConfigurationException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
     @Test
     public void testRefresh() throws Exception {
-        final String configurationPath =
-                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/wiremock/resources/configuration.yml";
-        final String wireMockServerFilePath =
-                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/wiremock/resources/nl-ing-ob-ais.aap";
+        final String wireMockServerFilePath = RESOURCE_PATH + "nl-ing-ob-ais.aap";
 
-        final String contractFilePath =
-                "src/integration/agents/src/test/java/se/tink/backend/aggregation/agents/nxgen/nl/openbanking/ing/wiremock/resources/agent-contract.json";
-
-        final AgentsServiceConfiguration configuration =
-                AgentsServiceConfigurationReader.read(configurationPath);
+        final String contractFilePath = RESOURCE_PATH + "agent-contract.json";
 
         final AgentWireMockRefreshTest agentWireMockRefreshTest =
                 AgentWireMockRefreshTest.nxBuilder()
@@ -45,5 +68,50 @@ public class IngWiremockTest {
 
         // then
         agentWireMockRefreshTest.assertExpectedData(expected);
+    }
+
+    @Test
+    @Parameters
+    public void testInstantSepaPayment(Debtor debtor, String wiremockFile) throws Exception {
+        // given
+        final String wireMockFilePath = RESOURCE_PATH + wiremockFile;
+
+        final AgentWireMockPaymentTest agentWireMockPaymentTest =
+                AgentWireMockPaymentTest.builder(MarketCode.NL, "nl-ing-ob", wireMockFilePath)
+                        .withConfigurationFile(configuration)
+                        .withPayment(
+                                createMockedPayment(
+                                        PaymentScheme.SEPA_INSTANT_CREDIT_TRANSFER, debtor))
+                        .buildWithoutLogin(PaymentCommand.class);
+
+        // expect
+        agentWireMockPaymentTest.executePayment();
+    }
+
+    private Payment createMockedPayment(PaymentScheme paymentScheme, Debtor debtor) {
+        String currency = "EUR";
+        ExactCurrencyAmount amount = ExactCurrencyAmount.of("1.01", currency);
+        RemittanceInformation remittanceInformation = new RemittanceInformation();
+        remittanceInformation.setType(RemittanceInformationType.UNSTRUCTURED);
+        remittanceInformation.setValue("test value");
+
+        return new Payment.Builder()
+                .withCreditor(
+                        new Creditor(new IbanIdentifier("NL98INGB1234567890"), "Payment Creditor"))
+                .withDebtor(debtor)
+                .withExactCurrencyAmount(amount)
+                .withCurrency(currency)
+                .withRemittanceInformation(remittanceInformation)
+                .withUniqueId("TEST_PAYMENT_ID")
+                .withPaymentScheme(paymentScheme)
+                .build();
+    }
+
+    @SuppressWarnings("unused")
+    private Object[] parametersForTestInstantSepaPayment() {
+        return new Object[][] {
+            {new Debtor(new IbanIdentifier("NL21INGB0987654321")), "nl-ing-pis_instant_sepa.aap"},
+            {null, "nl-ing-pis_instant_sepa_without_debtor.aap"},
+        };
     }
 }
