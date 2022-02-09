63dbb4db5ca76b20b4ac5f4c4fd00d5b169d166a
Annica Alstermark <annica.alstermark@tink.se>
2022-02-09 10:26:57 +0100

    feat(agent factory): Add creation of the agent SDK AgentInstance

--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/agentfactory/FakeAgentFactoryImpl.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/agentfactory/FakeAgentFactoryImpl.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.agentfactory;
 
+import se.tink.agent.runtime.instance.AgentInstance;
 import se.tink.backend.aggregation.agents.agent.Agent;
 import se.tink.backend.aggregation.agents.agentfactory.iface.AgentFactory;
 import se.tink.backend.aggregation.agents.contexts.agent.AgentContext;
@@ -9,6 +10,12 @@ public class FakeAgentFactoryImpl implements AgentFactory {
 
     @Override
     public Agent create(CredentialsRequest request, AgentContext context)
+            throws ReflectiveOperationException {
+        return null;
+    }
+
+    @Override
+    public AgentInstance createAgentSdkInstance(CredentialsRequest request, AgentContext context)
             throws ReflectiveOperationException {
         return null;
     }
--- src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/agentfactory/impl/AgentFactory.java
+++ src/integration/lib/src/main/java/se/tink/backend/aggregation/agents/agentfactory/impl/AgentFactory.java
@@ -1,5 +1,6 @@
 package se.tink.backend.aggregation.agents.agentfactory.iface;
 
+import se.tink.agent.runtime.instance.AgentInstance;
 import se.tink.backend.aggregation.agents.agent.Agent;
 import se.tink.backend.aggregation.agents.contexts.agent.AgentContext;
 import se.tink.libraries.credentials.service.CredentialsRequest;
@@ -9,5 +10,9 @@ public interface AgentFactory {
 
     /** Creates an agent from a credentials request and an agent context. */
     Agent create(CredentialsRequest request, AgentContext context)
+            throws ReflectiveOperationException;
+
+    /** Creates an agent SDK instance from a credentials request and an agent context. */
+    AgentInstance createAgentSdkInstance(CredentialsRequest request, AgentContext context)
             throws ReflectiveOperationException;
 }
