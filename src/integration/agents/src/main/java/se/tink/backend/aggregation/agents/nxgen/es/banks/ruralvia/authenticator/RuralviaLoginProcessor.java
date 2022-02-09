10457f80de2dcb1fcc6a255a0326f0662c98ea94
Sebastian Brzozowski <sebastian.brzozowski@tink.com>
2022-02-09 21:18:23 +0100

    feat(ruralvia): moving the search for validation JS alerts

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/ruralvia/authenticator/RuralviaLoginProcessor.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/es/banks/ruralvia/authenticator/RuralviaLoginProcessor.java
@@ -3,13 +3,16 @@ package se.tink.backend.aggregation.agents.nxgen.es.banks.ruralvia.authenticator
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebElement;
 import se.tink.backend.agents.rpc.Credentials;
 import se.tink.backend.agents.rpc.Field;
 import se.tink.backend.aggregation.agents.exceptions.AuthenticationException;
 import se.tink.backend.aggregation.agents.exceptions.AuthorizationException;
+import se.tink.backend.aggregation.agents.exceptions.connectivity.ConnectivityException;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.progressive.AuthenticationStepResponse;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.step.CallbackProcessorEmpty;
+import se.tink.connectivity.errors.ConnectivityErrorDetails;
 import se.tink.integration.webdriver.WebDriverWrapper;
 
 @AllArgsConstructor
@@ -24,7 +27,7 @@ public class RuralviaLoginProcessor implements CallbackProcessorEmpty {
         WebElement loginForm = webDriver.findElement(By.id("form1"));
         List<WebElement> inputElements = loginForm.findElements(By.tagName("input"));
         fillInCredentials(inputElements);
-        loginForm.submit();
+        submitLoginForm(loginForm);
         return AuthenticationStepResponse.executeNextStep();
     }
 
@@ -32,5 +35,21 @@ public class RuralviaLoginProcessor implements CallbackProcessorEmpty {
         inputElements.get(0).sendKeys(credentials.getField(Field.Key.USERNAME));
         inputElements.get(1).sendKeys(credentials.getField(Field.Key.NATIONAL_ID_NUMBER));
         inputElements.get(2).sendKeys(credentials.getField(Field.Key.PASSWORD));
+    }
+
+    private void submitLoginForm(WebElement loginForm) {
+        loginForm.submit();
+        handleCredentialsContainsIllegalCharacterFromJavaScriptAlert();
+        new PageLoadWait(webDriver).waitFor(3);
+    }
+
+    private void handleCredentialsContainsIllegalCharacterFromJavaScriptAlert() {
+        try {
+            webDriver.switchTo().alert();
+            throw new ConnectivityException(
+                    ConnectivityErrorDetails.UserLoginErrors.STATIC_CREDENTIALS_INCORRECT);
+        } catch (NoAlertPresentException ex) {
+            // no action needed when there is no JS alert
+        }
     }
 }
