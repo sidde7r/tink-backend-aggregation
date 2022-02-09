fa4955b6302057f1f75e2a71f3ba7c66c1078d99
Paweł Drewek <pawel.drewek@tink.com>
2022-02-09 16:08:42 +0100

    chore(FR/BRED): remove capability

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/bredbanquepopulaire/BredBanquePopulaireAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fr/openbanking/bredbanquepopulaire/BredBanquePopulaireAgent.java
@@ -1,7 +1,6 @@
 package se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bredbanquepopulaire;
 
 import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.CHECKING_ACCOUNTS;
-import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.CREDIT_CARDS;
 import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.IDENTITY_DATA;
 
 import com.google.inject.Inject;
@@ -11,14 +10,12 @@ import se.tink.backend.aggregation.agents.FetchAccountsResponse;
 import se.tink.backend.aggregation.agents.FetchIdentityDataResponse;
 import se.tink.backend.aggregation.agents.FetchTransactionsResponse;
 import se.tink.backend.aggregation.agents.RefreshCheckingAccountsExecutor;
-import se.tink.backend.aggregation.agents.RefreshCreditCardAccountsExecutor;
 import se.tink.backend.aggregation.agents.RefreshIdentityDataExecutor;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bredbanquepopulaire.apiclient.BredBanquePopulaireApiClient;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bredbanquepopulaire.apiclient.BredBanquePopulaireResponseHandler;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bredbanquepopulaire.authenticator.BredBanquePopulaireAuthenticator;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bredbanquepopulaire.configuration.BredBanquePopulaireConfiguration;
-import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bredbanquepopulaire.fetcher.creditcard.BredBanquePopulaireCreditCardFetcher;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bredbanquepopulaire.fetcher.identity.BredBanquePopulaireIdentityDataFetcher;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bredbanquepopulaire.fetcher.transacitons.BredBanquePopulaireTransactionsFetcher;
 import se.tink.backend.aggregation.agents.nxgen.fr.openbanking.bredbanquepopulaire.fetcher.transactionalaccount.BredBanquePopulaireTransactionalAccountFetcher;
@@ -31,24 +28,19 @@ import se.tink.backend.aggregation.nxgen.controllers.authentication.Authenticato
 import se.tink.backend.aggregation.nxgen.controllers.authentication.automatic.AutoAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.ThirdPartyAppAuthenticationController;
 import se.tink.backend.aggregation.nxgen.controllers.authentication.multifactor.thirdpartyapp.oauth2.OAuth2AuthenticationController;
-import se.tink.backend.aggregation.nxgen.controllers.refresh.creditcard.CreditCardRefreshController;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.TransactionFetcherController;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transaction.pagination.page.TransactionPagePaginationController;
 import se.tink.backend.aggregation.nxgen.controllers.refresh.transactionalaccount.TransactionalAccountRefreshController;
 import se.tink.backend.aggregation.nxgen.controllers.session.SessionHandler;
-import se.tink.backend.aggregation.nxgen.core.account.creditcard.CreditCardAccount;
 import se.tink.backend.aggregation.nxgen.core.account.transactional.TransactionalAccount;
 
-@AgentCapabilities({CHECKING_ACCOUNTS, CREDIT_CARDS, IDENTITY_DATA})
+@AgentCapabilities({CHECKING_ACCOUNTS, IDENTITY_DATA})
 public final class BredBanquePopulaireAgent extends NextGenerationAgent
-        implements RefreshCheckingAccountsExecutor,
-                RefreshCreditCardAccountsExecutor,
-                RefreshIdentityDataExecutor {
+        implements RefreshCheckingAccountsExecutor, RefreshIdentityDataExecutor {
     private final AgentConfiguration<BredBanquePopulaireConfiguration> agentConfiguration;
     private final QsealcSigner qsealcSigner;
     private final BredBanquePopulaireApiClient apiClient;
     private final TransactionalAccountRefreshController transactionalAccountRefreshController;
-    private final CreditCardRefreshController creditCardRefreshController;
     private final BredBanquePopulaireIdentityDataFetcher identityDataFetcher;
 
     @Inject
@@ -72,7 +64,6 @@ public final class BredBanquePopulaireAgent extends NextGenerationAgent
                         componentProvider.getRandomValueGenerator(),
                         headerGenerator);
         this.transactionalAccountRefreshController = getTransactionalAccountRefreshController();
-        this.creditCardRefreshController = getCreditCardRefreshController();
         this.identityDataFetcher = new BredBanquePopulaireIdentityDataFetcher(apiClient);
     }
 
@@ -126,22 +117,6 @@ public final class BredBanquePopulaireAgent extends NextGenerationAgent
                         new TransactionPagePaginationController<>(transactionsFetcher, 0)));
     }
 
-    private CreditCardRefreshController getCreditCardRefreshController() {
-        final BredBanquePopulaireCreditCardFetcher creditCardFetcher =
-                new BredBanquePopulaireCreditCardFetcher(apiClient);
-
-        final BredBanquePopulaireTransactionsFetcher<CreditCardAccount> transactionsFetcher =
-                new BredBanquePopulaireTransactionsFetcher<>(apiClient);
-
-        return new CreditCardRefreshController(
-                metricRefreshController,
-                updateController,
-                creditCardFetcher,
-                new TransactionFetcherController<>(
-                        transactionPaginationHelper,
-                        new TransactionPagePaginationController<>(transactionsFetcher, 0)));
-    }
-
     @Override
     public FetchAccountsResponse fetchCheckingAccounts() {
         return transactionalAccountRefreshController.fetchCheckingAccounts();
@@ -150,16 +125,6 @@ public final class BredBanquePopulaireAgent extends NextGenerationAgent
     @Override
     public FetchTransactionsResponse fetchCheckingTransactions() {
         return transactionalAccountRefreshController.fetchCheckingTransactions();
-    }
-
-    @Override
-    public FetchAccountsResponse fetchCreditCardAccounts() {
-        return creditCardRefreshController.fetchCreditCardAccounts();
-    }
-
-    @Override
-    public FetchTransactionsResponse fetchCreditCardTransactions() {
-        return creditCardRefreshController.fetchCreditCardTransactions();
     }
 
     @Override
