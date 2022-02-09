bd9ff06b7518e17ddd4438294c599269fdebe372
Jonas Renold <jonas.renold@tink.se>
2022-02-09 15:45:45 +0100

    test(agent_sdk/payment): test payment getStatus timeout
    This test is setup in such a way that it will always break due to getStatus poll timeout (since the test agent always returns PENDING).

---
+++ src/agent_sdk/compatibility_layers/aggregation_service/test/payments/BulkPaymentInitiationGetStatusTimeoutTest.java
@@ -0,0 +1,70 @@
+package src.agent_sdk.compatibility_layers.aggregation_service.test.payments;
+
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.List;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import se.tink.agent.sdk.models.payments.PaymentStatus;
+import se.tink.agent.sdk.models.payments.bulk_payment_register_result.BulkPaymentRegisterResult;
+import se.tink.agent.sdk.models.payments.bulk_payment_sign_result.BulkPaymentSignResult;
+import se.tink.agent.sdk.models.payments.payment.Payment;
+import src.agent_sdk.compatibility_layers.aggregation_service.src.payments.report.PaymentInitiationReport;
+import src.agent_sdk.compatibility_layers.aggregation_service.src.payments.report.PaymentInitiationState;
+
+public class BulkPaymentInitiationGetStatusTimeoutTest {
+    @Test
+    public void testGetStatusTimeout() {
+        // Set up the agent to successfully register and sign the payment but to always return
+        // PENDING when polling for status. The payment initiation will be constructed with a very
+        // short polling timeout. We expect the final state of the payment to be PENDING.
+        BulkPaymentAgent agent =
+                new BulkPaymentAgent(
+                        List.of(
+                                BulkPaymentRegisterResult.builder()
+                                        .reference(PaymentInitiationTestHelper.PAYMENT_1_REF)
+                                        .noError()
+                                        .build()),
+                        List.of(
+                                BulkPaymentSignResult.builder()
+                                        .reference(PaymentInitiationTestHelper.PAYMENT_1_REF)
+                                        .status(PaymentStatus.PENDING)
+                                        .noDebtor()
+                                        .build()),
+                        List.of(
+                                BulkPaymentSignResult.builder()
+                                        .reference(PaymentInitiationTestHelper.PAYMENT_1_REF)
+                                        .status(PaymentStatus.PENDING)
+                                        .noDebtor()
+                                        .build()));
+
+        List<Payment> payments = List.of(PaymentInitiationTestHelper.PAYMENT_1);
+
+        PaymentInitiationReport paymentInitiationReport =
+                PaymentInitiationTestHelper.initiateBulkPayments(
+                        agent, Duration.ofMillis(1), payments);
+
+        // Assert that the agent processed the expected payments at every step.
+        MatcherAssert.assertThat(
+                payments,
+                Matchers.containsInAnyOrder(agent.getPaymentsAskedToRegister().toArray()));
+        MatcherAssert.assertThat(
+                payments, Matchers.containsInAnyOrder(agent.getPaymentsAskedToSign().toArray()));
+        MatcherAssert.assertThat(
+                new HashSet<>(payments),
+                Matchers.containsInAnyOrder(agent.getPaymentsAskedToGetStatus().toArray()));
+
+        // Assert that the final report contain the expected final statuses and/or errors.
+        List<PaymentInitiationState> expectedFinalPaymentStates =
+                List.of(
+                        PaymentInitiationState.builder()
+                                .paymentReference(PaymentInitiationTestHelper.PAYMENT_1_REF)
+                                .status(PaymentStatus.PENDING)
+                                .build());
+        MatcherAssert.assertThat(
+                expectedFinalPaymentStates,
+                Matchers.containsInAnyOrder(
+                        paymentInitiationReport.getFinalPaymentStates().toArray()));
+    }
+}
