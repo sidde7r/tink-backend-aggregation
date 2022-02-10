aea378a54d2b3961f98655b9ead67a8d46e55ce5
Piotr Cebo <piotr.cebo@tink.com>
2022-02-10 10:16:43 +0100

    chore(SQS): change queue size to 10

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/worker/AgentWorker.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/worker/AgentWorker.java
@@ -47,7 +47,7 @@ public class AgentWorker extends ManagedSafeStop {
 
     // Automatic Refreshes will be put on a persistent queue. This queue will be used as a buffer
     // only.
-    private static final int MAX_QUEUE_AUTOMATIC_REFRESH = 100;
+    private static final int MAX_QUEUE_AUTOMATIC_REFRESH = 10;
 
     /**
      * As of right now, the longest supplemental information timeout is 540 seconds. This is the max
