8c785d1fcd61253e8db100bc0dbdfd7983947bd0
Pawel Szalawinski <pawel.szalawinski@tink.com>
2022-02-11 10:55:26 +0100

    feat(samlink): removed savings capabilities from agents

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/samlink/saastopankki/SpAgent.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/fi/openbanking/samlink/saastopankki/SpAgent.java
@@ -2,7 +2,6 @@ package se.tink.backend.aggregation.agents.nxgen.fi.openbanking.samlink.saastopa
 
 import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.CHECKING_ACCOUNTS;
 import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.CREDIT_CARDS;
-import static se.tink.backend.aggregation.agents.agentcapabilities.Capability.SAVINGS_ACCOUNTS;
 
 import com.google.inject.Inject;
 import se.tink.backend.aggregation.agents.agentcapabilities.AgentCapabilities;
@@ -15,7 +14,7 @@ import se.tink.backend.aggregation.eidassigner.module.QSealcSignerModuleRSASHA25
 import se.tink.backend.aggregation.nxgen.agents.componentproviders.AgentComponentProvider;
 
 @AgentDependencyModules(modules = QSealcSignerModuleRSASHA256.class)
-@AgentCapabilities({CHECKING_ACCOUNTS, SAVINGS_ACCOUNTS, CREDIT_CARDS})
+@AgentCapabilities({CHECKING_ACCOUNTS, CREDIT_CARDS})
 public final class SpAgent extends SamlinkAgent {
 
     @Inject
