18afb383f4f43818c8975178493e5289e1a78a39
Jonas Renold <jonas.renold@tink.se>
2022-02-13 11:33:05 +0100

    feat(agent_sdk/payment/test): add beneficiary contract/report
    To be used by test agent impl and tests.

--- src/agent_sdk/compatibility_layers/aggregation_service/test/payments/test_agent/PaymentsTestContract.java
+++ src/agent_sdk/compatibility_layers/aggregation_service/test/payments/test_agent/PaymentsTestContract.java
@@ -1,18 +1,31 @@
 package src.agent_sdk.compatibility_layers.aggregation_service.test.payments.test_agent;
 
 import java.util.List;
+import java.util.Map;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Singular;
-import org.junit.Ignore;
+import org.apache.commons.lang3.tuple.Pair;
+import se.tink.agent.sdk.models.payments.BeneficiaryState;
+import se.tink.agent.sdk.models.payments.beneficiary.Beneficiary;
+import se.tink.agent.sdk.models.payments.beneficiary_register_result.BeneficiaryRegisterResult;
 import se.tink.agent.sdk.models.payments.bulk_payment_register_result.BulkPaymentRegisterResult;
 import se.tink.agent.sdk.models.payments.bulk_payment_sign_result.BulkPaymentSignResult;
+import se.tink.libraries.account.AccountIdentifier;
 
 @Builder
 @Getter
-@Ignore
 public class PaymentsTestContract {
     @Singular private final List<BulkPaymentRegisterResult> registerResults;
     @Singular private final List<BulkPaymentSignResult> signResults;
     @Singular private final List<BulkPaymentSignResult> signStatusResults;
+    @Singular private final Map<AccountIdentifier, List<Beneficiary>> fetchBeneficiaryResults;
+
+    @Singular
+    private final Map<Pair<AccountIdentifier, Beneficiary>, BeneficiaryRegisterResult>
+            registerBeneficiaryResults;
+
+    @Singular
+    private final Map<Pair<AccountIdentifier, Beneficiary>, BeneficiaryState>
+            signBeneficiaryResults;
 }
--- src/agent_sdk/compatibility_layers/aggregation_service/test/payments/test_agent/PaymentsTestExecutionReport.java
+++ src/agent_sdk/compatibility_layers/aggregation_service/test/payments/test_agent/PaymentsTestExecutionReport.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import org.junit.Ignore;
+import org.apache.commons.lang3.tuple.Pair;
+import se.tink.agent.sdk.models.payments.beneficiary.Beneficiary;
 import se.tink.agent.sdk.models.payments.payment.Payment;
+import se.tink.libraries.account.AccountIdentifier;
 
-@Ignore
 public class PaymentsTestExecutionReport {
     private final List<Payment> paymentsAskedToRegister = new ArrayList<>();
     private final List<Payment> paymentsAskedToSign = new ArrayList<>();
     private final Set<Payment> paymentsAskedToGetSignStatus = new HashSet<>();
+    private final List<AccountIdentifier> accountsAskedToFetchBeneficiariesFor = new ArrayList<>();
+    private final List<Pair<AccountIdentifier, Beneficiary>> beneficiariesAskedToRegister =
+            new ArrayList<>();
+    private final List<Pair<AccountIdentifier, Beneficiary>> beneficiariesAskedToSign =
+            new ArrayList<>();
 
     public List<Payment> getPaymentsAskedToRegister() {
         return this.paymentsAskedToRegister;
@@ -25,6 +31,18 @@ public class PaymentsTestExecutionReport {
         return this.paymentsAskedToGetSignStatus;
     }
 
+    public List<AccountIdentifier> getAccountsAskedToFetchBeneficiariesFor() {
+        return accountsAskedToFetchBeneficiariesFor;
+    }
+
+    public List<Pair<AccountIdentifier, Beneficiary>> getBeneficiariesAskedToRegister() {
+        return beneficiariesAskedToRegister;
+    }
+
+    public List<Pair<AccountIdentifier, Beneficiary>> getBeneficiariesAskedToSign() {
+        return beneficiariesAskedToSign;
+    }
+
     public void addPaymentsToRegister(List<Payment> payments) {
         this.paymentsAskedToRegister.addAll(payments);
     }
@@ -35,5 +53,19 @@ public class PaymentsTestExecutionReport {
 
     public void addPaymentsToGetSignStatus(List<Payment> payments) {
         this.paymentsAskedToGetSignStatus.addAll(payments);
+    }
+
+    public void addAccountToFetchBeneficiariesFor(AccountIdentifier debtorAccountIdentifier) {
+        this.accountsAskedToFetchBeneficiariesFor.add(debtorAccountIdentifier);
+    }
+
+    public void addBeneficiaryToRegister(
+            AccountIdentifier debtorAccountIdentifier, Beneficiary beneficiary) {
+        this.beneficiariesAskedToRegister.add(Pair.of(debtorAccountIdentifier, beneficiary));
+    }
+
+    public void addBeneficiaryToSign(
+            AccountIdentifier debtorAccountIdentifier, Beneficiary beneficiary) {
+        this.beneficiariesAskedToSign.add(Pair.of(debtorAccountIdentifier, beneficiary));
     }
 }
