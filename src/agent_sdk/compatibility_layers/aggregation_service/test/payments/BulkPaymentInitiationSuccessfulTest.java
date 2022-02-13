bff9c4276abaf0ece45872a7e56c05d3370cf99a
Jonas Renold <jonas.renold@tink.se>
2022-02-13 11:38:21 +0100

    fix(agent_sdk/payment/test): add beneficiary to successful test

--- src/agent_sdk/compatibility_layers/aggregation_service/test/payments/BulkPaymentInitiationSuccessfulTest.java
+++ src/agent_sdk/compatibility_layers/aggregation_service/test/payments/BulkPaymentInitiationSuccessfulTest.java
@@ -5,13 +5,15 @@ import java.util.List;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.Test;
+import se.tink.agent.sdk.models.payments.BeneficiaryState;
 import se.tink.agent.sdk.models.payments.PaymentStatus;
+import se.tink.agent.sdk.models.payments.beneficiary_register_result.BeneficiaryRegisterResult;
 import se.tink.agent.sdk.models.payments.bulk_payment_register_result.BulkPaymentRegisterResult;
 import se.tink.agent.sdk.models.payments.bulk_payment_sign_result.BulkPaymentSignResult;
 import se.tink.agent.sdk.models.payments.payment.Payment;
 import src.agent_sdk.compatibility_layers.aggregation_service.src.payments.report.PaymentInitiationReport;
 import src.agent_sdk.compatibility_layers.aggregation_service.src.payments.report.PaymentInitiationState;
-import src.agent_sdk.compatibility_layers.aggregation_service.test.payments.test_agent.BulkPaymentTestAgent;
+import src.agent_sdk.compatibility_layers.aggregation_service.test.payments.test_agent.BulkPaymentAndBeneficiaryTestAgent;
 import src.agent_sdk.compatibility_layers.aggregation_service.test.payments.test_agent.PaymentsTestContract;
 import src.agent_sdk.compatibility_layers.aggregation_service.test.payments.test_agent.PaymentsTestExecutionReport;
 
@@ -19,13 +21,38 @@ public class BulkPaymentInitiationSuccessfulTest {
 
     @Test
     public void testAllSuccessful() {
-        // Set up the agent to successfully register, sign (with PENDING status) and getStatus (with
-        // INITIATED_AND_EXECUTED status).
+        // Set up the agent to:
+        //  - Fetch 1 beneficiary (for payment 1)
+        //  - Successfully register and sign beneficiaries for payment 2 and 3
+        //  - Successfully register, sign (with PENDING status) and getSignStatus (with
+        // INITIATED_AND_EXECUTED status) for all payments.
+        // The final status for all payments should be INITIATED_AND_EXECUTED.
         PaymentsTestExecutionReport executionReport = new PaymentsTestExecutionReport();
-        BulkPaymentTestAgent agent =
-                new BulkPaymentTestAgent(
+        BulkPaymentAndBeneficiaryTestAgent agent =
+                new BulkPaymentAndBeneficiaryTestAgent(
                         executionReport,
                         PaymentsTestContract.builder()
+                                .fetchBeneficiaryResult(
+                                        PaymentInitiationTestHelper.DEBTOR_1_ACCOUNT_IDENTIFIER,
+                                        List.of(PaymentInitiationTestHelper.BENEFICIARY_ALICE))
+                                .registerBeneficiaryResult(
+                                        PaymentInitiationTestHelper.PAYMENT_2_BENEFICIARY,
+                                        BeneficiaryRegisterResult.builder()
+                                                .noError()
+                                                .noBankReference()
+                                                .build())
+                                .registerBeneficiaryResult(
+                                        PaymentInitiationTestHelper.PAYMENT_3_BENEFICIARY,
+                                        BeneficiaryRegisterResult.builder()
+                                                .noError()
+                                                .noBankReference()
+                                                .build())
+                                .signBeneficiaryResult(
+                                        PaymentInitiationTestHelper.PAYMENT_2_BENEFICIARY,
+                                        BeneficiaryState.successful())
+                                .signBeneficiaryResult(
+                                        PaymentInitiationTestHelper.PAYMENT_3_BENEFICIARY,
+                                        BeneficiaryState.successful())
                                 .registerResult(
                                         BulkPaymentRegisterResult.builder()
                                                 .reference(
@@ -98,6 +125,24 @@ public class BulkPaymentInitiationSuccessfulTest {
                 PaymentInitiationTestHelper.initiateBulkPayments(agent, payments);
 
         // Assert that the agent processed the expected payments at every step.
+        MatcherAssert.assertThat(
+                List.of(PaymentInitiationTestHelper.DEBTOR_1_ACCOUNT_IDENTIFIER),
+                Matchers.containsInAnyOrder(
+                        executionReport.getAccountsAskedToFetchBeneficiariesFor().toArray()));
+        MatcherAssert.assertThat(
+                List.of(
+                        PaymentInitiationTestHelper.PAYMENT_2_BENEFICIARY,
+                        PaymentInitiationTestHelper.PAYMENT_3_BENEFICIARY),
+                Matchers.containsInAnyOrder(
+                        executionReport.getBeneficiariesAskedToRegister().toArray()));
+
+        MatcherAssert.assertThat(
+                List.of(
+                        PaymentInitiationTestHelper.PAYMENT_2_BENEFICIARY,
+                        PaymentInitiationTestHelper.PAYMENT_3_BENEFICIARY),
+                Matchers.containsInAnyOrder(
+                        executionReport.getBeneficiariesAskedToSign().toArray()));
+
         MatcherAssert.assertThat(
                 payments,
                 Matchers.containsInAnyOrder(
