320f08634f9ff3fe1b5b53746d649b57e6b0ca15
Jonas Renold <jonas.renold@tink.se>
2022-02-14 15:03:03 +0100

    fix(wiremock/login): login with agent_sdk authenticator
    Added agent_sdk authentication to the wiremock's login command. It will login as normal (identical to before) if the agent does not support the new sdk authentication.
    
    Evaluated if the authentication succeeded similar to how the old `agent.login()` check is done, using an assert.

--- src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/framework/compositeagenttest/command/LoginCommand.java
+++ src/integration/lib/src/test/java/se/tink/backend/aggregation/agents/framework/compositeagenttest/command/LoginCommand.java
@@ -2,6 +2,10 @@ package se.tink.backend.aggregation.agents.framework.compositeagenttest.command;
 
 import com.google.inject.Inject;
 import org.junit.Assert;
+import se.tink.agent.compatibility_layers.aggregation_service.authentication.Authentication;
+import se.tink.agent.compatibility_layers.aggregation_service.authentication.report.AuthenticationReport;
+import se.tink.agent.runtime.instance.AgentInstance;
+import se.tink.agent.sdk.authentication.consent.ConsentStatus;
 import se.tink.backend.aggregation.agents.PersistentLogin;
 import se.tink.backend.aggregation.agents.agent.Agent;
 import se.tink.backend.aggregation.agents.agentplatform.authentication.AgentPlatformAuthenticationExecutor;
@@ -14,22 +18,51 @@ import se.tink.libraries.credentials.service.CredentialsRequest;
 
 public final class LoginCommand implements CompositeAgentTestCommand {
 
-    private final Agent agent;
+    private final AgentInstance agentInstance;
     private final CredentialsRequest credentialsRequest;
     private final SupplementalInformationController supplementalInformationController;
 
     @Inject
     private LoginCommand(
-            Agent agent,
+            AgentInstance agentInstance,
             CredentialsRequest credentialsRequest,
             SupplementalInformationController supplementalInformationController) {
-        this.agent = agent;
+        this.agentInstance = agentInstance;
         this.credentialsRequest = credentialsRequest;
         this.supplementalInformationController = supplementalInformationController;
     }
 
     @Override
     public void execute() throws Exception {
+        if (agentInstance.supportsAuthentication()) {
+            authenticateAgentSdk();
+        } else {
+            authenticateAgent();
+        }
+    }
+
+    private void authenticateAgentSdk() {
+        Authentication authentication =
+                new Authentication(supplementalInformationController, agentInstance);
+
+        AuthenticationReport authenticationReport =
+                authentication.initiate(credentialsRequest.isForceAuthenticate());
+
+        Assert.assertEquals(
+                "Agent could not login successfully.",
+                ConsentStatus.VALID,
+                authenticationReport.getConsentStatus());
+    }
+
+    private void authenticateAgent() throws Exception {
+        Agent agent =
+                agentInstance
+                        .instanceOf(Agent.class)
+                        .orElseThrow(
+                                () ->
+                                        new IllegalStateException(
+                                                "Agent does not implement Agent.class."));
+
         if (isLoggedIn(agent)) {
             return;
         }
@@ -38,17 +71,15 @@ public final class LoginCommand implements CompositeAgentTestCommand {
             new AgentPlatformAuthenticationExecutor()
                     .processAuthentication(
                             agent, credentialsRequest, supplementalInformationController);
-            return;
         } else if (agent instanceof ProgressiveAuthAgent) {
             final ProgressiveLoginExecutor executor =
                     new ProgressiveLoginExecutor(
                             supplementalInformationController, (ProgressiveAuthAgent) agent);
             executor.login(credentialsRequest);
-            return;
+        } else {
+            boolean loginSuccessful = agent.login();
+            Assert.assertTrue("Agent could not login successfully.", loginSuccessful);
         }
-
-        boolean loginSuccessful = agent.login();
-        Assert.assertTrue("Agent could not login successfully.", loginSuccessful);
     }
 
     private boolean isLoggedIn(Agent agent) throws Exception {
