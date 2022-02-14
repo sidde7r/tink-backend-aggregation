a4827a5128fe8eb9e2c13c50635c5086f40449c6
Johannes Elgh <johannes.elgh@tink.se>
2022-02-14 15:17:55 +0100

    feat(instantiate-AWC): in 0.1% of cases, run new create Agent method

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/InstantiateAgentWorkerCommand.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/InstantiateAgentWorkerCommand.java
@@ -1,6 +1,10 @@
 package se.tink.backend.aggregation.workers.commands;
 
+import java.util.Optional;
 import java.util.Random;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.tink.agent.runtime.instance.AgentInstance;
 import se.tink.backend.aggregation.agents.agent.Agent;
 import se.tink.backend.aggregation.workers.commands.state.InstantiateAgentWorkerCommandState;
 import se.tink.backend.aggregation.workers.context.AgentWorkerCommandContext;
@@ -8,8 +12,8 @@ import se.tink.backend.aggregation.workers.operation.AgentWorkerCommand;
 import se.tink.backend.aggregation.workers.operation.AgentWorkerCommandResult;
 
 public class InstantiateAgentWorkerCommand extends AgentWorkerCommand {
-
-    private static final double TEST_RATIO = 0.0; 
+    private static final Logger log = LoggerFactory.getLogger(InstantiateAgentWorkerCommand.class);
+    private static final double TEST_RATIO = 0.001; // 0.1%
     private InstantiateAgentWorkerCommandState state;
     private AgentWorkerCommandContext context;
     private final Random random;
@@ -32,6 +36,24 @@ public class InstantiateAgentWorkerCommand extends AgentWorkerCommand {
             createAndAddAgentOldWay();
             return AgentWorkerCommandResult.CONTINUE;
         }
+
+        // TESTING NEW PATH WORKS
+
+        log.info(
+                "[TESTING NEW INSTANTIATE AGENT] picking new instantiate agent path to test it works");
+
+        AgentInstance agentInstance =
+                state.getAgentFactory().createAgentSdkInstance(context.getRequest(), context);
+        Optional<Agent> agent = agentInstance.instanceOf(Agent.class);
+        if (agent.isPresent()) {
+            log.info("[TESTING NEW INSTANTIATE AGENT] instantiation works!");
+            context.setAgent(agent.get());
+        } else {
+            // Fallback
+            log.info("[TESTING NEW INSTANTIATE AGENT] it did not work, let's fallback!");
+            createAndAddAgentOldWay();
+        }
+
         return AgentWorkerCommandResult.CONTINUE;
     }
 
