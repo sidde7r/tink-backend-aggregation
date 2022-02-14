fda682cfcd17f302826aa6af8007505fd8c5412d
Johannes Elgh <johannes.elgh@tink.se>
2022-02-14 15:13:51 +0100

    refactor(instantiate-AWC): set up for testing a different path
    this commit doesn't change behavior

--- src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/InstantiateAgentWorkerCommand.java
+++ src/aggregation/lib/src/main/java/se/tink/backend/aggregation/workers/commands/InstantiateAgentWorkerCommand.java
@@ -1,5 +1,7 @@
 package se.tink.backend.aggregation.workers.commands;
 
+import java.util.Random;
+import se.tink.backend.aggregation.agents.agent.Agent;
 import se.tink.backend.aggregation.workers.commands.state.InstantiateAgentWorkerCommandState;
 import se.tink.backend.aggregation.workers.context.AgentWorkerCommandContext;
 import se.tink.backend.aggregation.workers.operation.AgentWorkerCommand;
@@ -7,13 +9,16 @@ import se.tink.backend.aggregation.workers.operation.AgentWorkerCommandResult;
 
 public class InstantiateAgentWorkerCommand extends AgentWorkerCommand {
 
+    private static final double TEST_RATIO = 0.0; 
     private InstantiateAgentWorkerCommandState state;
     private AgentWorkerCommandContext context;
+    private final Random random;
 
     public InstantiateAgentWorkerCommand(
             AgentWorkerCommandContext context, InstantiateAgentWorkerCommandState state) {
         this.context = context;
         this.state = state;
+        this.random = new Random();
     }
 
     @Override
@@ -21,12 +26,22 @@ public class InstantiateAgentWorkerCommand extends AgentWorkerCommand {
         state.doRightBeforeInstantiation(
                 context.getRequest().getProvider().getName(),
                 context.getRequest().getCredentials().getId());
-        context.setAgent(state.getAgentFactory().create(context.getRequest(), context));
+
+        if (random.nextDouble() >= TEST_RATIO) {
+            // REGULAR PATH
+            createAndAddAgentOldWay();
+            return AgentWorkerCommandResult.CONTINUE;
+        }
         return AgentWorkerCommandResult.CONTINUE;
     }
 
     @Override
     protected void doPostProcess() throws Exception {
         state.doAtInstantiationPostProcess();
+    }
+
+    private void createAndAddAgentOldWay() throws ReflectiveOperationException {
+        Agent agent = state.getAgentFactory().create(context.getRequest(), context);
+        context.setAgent(agent);
     }
 }
