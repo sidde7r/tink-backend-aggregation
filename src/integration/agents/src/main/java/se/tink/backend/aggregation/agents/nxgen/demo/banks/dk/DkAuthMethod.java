3de0336718a42f1257c8cb0ed8a863a895e8571c
Damian Baciur <damian.baciur@tink.se>
2022-02-16 11:53:53 +0100

    feat(MitID,Demo): Add support to choose auth method by number
    [ITE-3272]

--- src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/dk/DkAuthMethod.java
+++ src/integration/agents/src/main/java/se/tink/backend/aggregation/agents/nxgen/demo/banks/dk/DkAuthMethod.java
@@ -1,21 +1,35 @@
 package se.tink.backend.aggregation.agents.nxgen.demo.banks.dk;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 
 @Getter
 @RequiredArgsConstructor
 public enum DkAuthMethod {
-    NEM_ID("nemId"),
-    MIT_ID("mitId");
+    NEM_ID("nemId", "1"),
+    MIT_ID("mitId", "2");
 
-    private final String supplementalInfoKey;
+    private final List<String> supplementalInfoKeys;
 
-    public static DkAuthMethod getBySupplementalInfoKey(String key) {
-        return Stream.of(DkAuthMethod.values())
-                .filter(method -> method.getSupplementalInfoKey().equals(key))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException("Unknown method key: " + key));
+    DkAuthMethod(String... supplementalInfoKeys) {
+        this.supplementalInfoKeys = Arrays.asList(supplementalInfoKeys);
+    }
+
+    public static DkAuthMethod getBySupplementalInfoKey(String supplementalInfoKey) {
+        for (DkAuthMethod method : DkAuthMethod.values()) {
+            boolean hasMatchingKey =
+                    method.getSupplementalInfoKeys().stream()
+                            .anyMatch(
+                                    methodKey ->
+                                            StringUtils.equalsIgnoreCase(
+                                                    methodKey, supplementalInfoKey));
+            if (hasMatchingKey) {
+                return method;
+            }
+        }
+        throw new IllegalStateException("Unknown method key: " + supplementalInfoKey);
     }
 }
